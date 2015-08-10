519d0afa02dce8b38ed62789e8a6ac8b853e4a04
Wouter de Meijer <w.demeijer@gmail.com>
2015-08-10 13:19:07 +0200

    INFUND-151 - Added ApplicationStatus sql data file.

---
+++ setup-files/data-dump/ifs_application_status.sql
@@ -0,0 +1,8 @@
+/*
+-- Query: SELECT * FROM `ifs-data`.application_status
+-- Date: 2015-08-10 12:46
+*/
+INSERT INTO `application_status` (`id`,`name`) VALUES (1,'created');
+INSERT INTO `application_status` (`id`,`name`) VALUES (2,'submitted');
+INSERT INTO `application_status` (`id`,`name`) VALUES (3,'approved');
+INSERT INTO `application_status` (`id`,`name`) VALUES (4,'rejected');
