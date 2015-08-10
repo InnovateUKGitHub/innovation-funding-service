376e0943b1d0a1c44ee71070302612bcce732e28
Wouter de Meijer <w.demeijer@gmail.com>
2015-08-10 13:24:27 +0200

    INFUND-151 - Added ApplicationStatus and Application sql data file.

--- setup-files/data-dump/ifs_application.sql
+++ setup-files/data-dump/ifs_application.sql
@@ -1,37 +1,8 @@
--- MySQL dump 10.13  Distrib 5.6.24, for osx10.8 (x86_64)
---
--- Host: 127.0.0.1    Database: ifs
--- ------------------------------------------------------
--- Server version	5.6.25
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Dumping data for table `application`
---
-
-LOCK TABLES `application` WRITE;
-/*!40000 ALTER TABLE `application` DISABLE KEYS */;
-INSERT INTO `application` VALUES (1,'Rovel Additive Manufacturing Process',1,1),(2,'Providing sustainable childcare',1,2),(3,'Mobile Phone Data for Logistics Analytics',1,3),(4,'Using natural gas to heat homes',1,4);
-/*!40000 ALTER TABLE `application` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2015-07-31 11:06:50
+/*
+-- Query: SELECT * FROM `ifs-data`.application
+-- Date: 2015-08-10 13:22
+*/
+INSERT INTO `application` (`id`,`name`,`competition`,`application_status_id`) VALUES (1,'Rovel Additive Manufacturing Process',1,1);
+INSERT INTO `application` (`id`,`name`,`competition`,`application_status_id`) VALUES (2,'Providing sustainable childcare',1,2);
+INSERT INTO `application` (`id`,`name`,`competition`,`application_status_id`) VALUES (3,'Mobile Phone Data for Logistics Analytics',1,3);
+INSERT INTO `application` (`id`,`name`,`competition`,`application_status_id`) VALUES (4,'Using natural gas to heat homes',1,4);
