37ba96896bcfb59294c712cd9e5914fa23a393b4
Brent de Kok <brent@worth.systems>
2015-08-11 09:30:06 +0100

    INFUND-115 Build component: " Days left to submit "

--- ifs-web-service/src/main/resources/static/js/worth.js
+++ ifs-web-service/src/main/resources/static/js/worth.js
@@ -1,4 +1,4 @@
-/* 3. Progressive collapsibles written by @Heydonworks altered by Worth Systems
+/*  Progressive collapsibles written by @Heydonworks altered by Worth Systems
 -----------------------------------------------------------------------------------------
 */
 $(document).ready(function(){
@@ -29,4 +29,28 @@ $(document).ready(function(){
     });
   });
 
+});
+
+/* 
+Lea verou's SVG pie
+*/
+
+ $('.pie').each(function(index,pie) {
+  var p = parseFloat(pie.textContent);
+  var NS = "http://www.w3.org/2000/svg";
+  var svg = document.createElementNS(NS, "svg");
+  var circle = document.createElementNS(NS, "circle");
+  var title = document.createElementNS(NS, "title");
+  
+  circle.setAttribute("r", 16);
+  circle.setAttribute("cx", 16);
+  circle.setAttribute("cy", 16);
+  circle.setAttribute("stroke-dasharray", p + " 100");
+  
+  svg.setAttribute("viewBox", "0 0 32 32");
+  title.textContent = pie.textContent;
+  pie.textContent = '';
+  svg.appendChild(title);
+  svg.appendChild(circle);
+  pie.appendChild(svg);
 });
