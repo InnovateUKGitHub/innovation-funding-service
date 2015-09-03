fafdb556308ca648059c2b5d53752001cd7b33d8
Wouter de Meijer <w.demeijer@gmail.com>
2015-09-03 18:39:54 +0200

    DevOps - Ubuntu Gnome machine now doesn't need to reboot anymore for the gdm interface to start and Ubuntu Headless now starts without virtualbox UI.

--- robot-tests/ubuntu-vm-gnome/provision_script.sh
+++ robot-tests/ubuntu-vm-gnome/provision_script.sh
@@ -2,17 +2,41 @@
 
 #if [! -f /vagrant/data/install.txt ];
 #then
-sudo apt-get -y update
-sudo apt-get -y install gdm
-sudo dpkg-reconfigure gdm
-sudo apt-get -y install python-wxgtk2.8
-sudo apt-get -y install python-pip
-sudo pip install robotframework
-sudo pip install robotframework-selenium2library
-sudo pip install robotframework-ride
-sudo apt-get -y install gnome-terminal
-sudo apt-get -y install firefox
-sudo reboot
 
-	#fi
+echo "# Updating package repository"
+sudo apt-get -y update
+
+echo "# Installing gnome"
+sudo apt-get -y install gdm
+
+echo "# Configuring gnome"
+sudo dpkg-reconfigure gdm
+
+echo "# Installing python-wxgtk2.8"
+sudo apt-get -y install python-wxgtk2.8
+
+echo "# Installing python-pip"
+sudo apt-get -y install python-pip
+
+echo "# Installing robotframework"
+sudo pip install robotframework
+
+echo "# Installing robotframework-selenium2library"
+sudo pip install robotframework-selenium2library
+
+echo "# Installing robotframework-ride"
+sudo pip install robotframework-ride
+
+echo "# Installing terminal"
+sudo apt-get -y install gnome-terminal
+
+echo "# Installing firefox"
+sudo apt-get -y install firefox
+
+echo "# Starting gnome"
+sudo service gdm start
+
+echo "# Finished setting up RIDE virtual machine"
+
+#fi
 
--- robot-tests/ubuntu-vm-headless/provision_script.sh
+++ robot-tests/ubuntu-vm-headless/provision_script.sh
@@ -2,15 +2,33 @@
 
 #if [! -f /vagrant/data/install.txt ];
 #then
-sudo apt-get -y update
-sudo apt-get -y install xvfb
-sudo apt-get -y install python-wxgtk2.8
-sudo apt-get -y install python-pip
-sudo pip install robotframework
-sudo pip install robotframework-selenium2library
-sudo pip install robotframework-ride
-sudo apt-get -y install firefox
-sudo reboot
 
-	#fi
+echo "# Updating package repository"
+sudo apt-get -y update
+
+echo "# Installing xvfb"
+sudo apt-get -y install xvfb
+
+echo "# Installing python-wxgtk2.8"
+sudo apt-get -y install python-wxgtk2.8
+
+echo "# Installing python-pip"
+sudo apt-get -y install python-pip
+
+echo "# Installing robotframework"
+sudo pip install robotframework
+
+echo "# Installing robotframework-selenium2library"
+sudo pip install robotframework-selenium2library
+
+echo "# Installing robotframework-ride"
+sudo pip install robotframework-ride
+
+echo "# Installing terminal"
+sudo apt-get -y install gnome-terminal
+
+echo "# Installing firefox"
+sudo apt-get -y install firefox
+
+#fi
 
