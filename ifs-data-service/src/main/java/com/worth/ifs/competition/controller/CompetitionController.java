9ff9979db16ab1500cc24b8ffb898b58a267d885
Nuno Alexandre <nalexandre@worth.systems>
2015-09-15 17:59:59 +0200

    CompetitionController with organised imports and test code for the Process workflow.

--- ifs-data-service/src/main/java/com/worth/ifs/competition/controller/CompetitionController.java
+++ ifs-data-service/src/main/java/com/worth/ifs/competition/controller/CompetitionController.java
@@ -1,15 +1,15 @@
 package com.worth.ifs.competition.controller;
 
-import com.worth.ifs.application.repository.ApplicationStatusRepository;
-import com.worth.ifs.application.repository.ResponseRepository;
 import com.worth.ifs.competition.domain.Competition;
 import com.worth.ifs.competition.repository.CompetitionsRepository;
-import com.worth.ifs.user.repository.ProcessRoleRepository;
-import com.worth.ifs.user.repository.UserRepository;
+import com.worth.ifs.workflow.domain.*;
+import com.worth.ifs.workflow.domain.Process;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -21,14 +21,10 @@ import java.util.List;
 public class CompetitionController {
     @Autowired
     CompetitionsRepository repository;
+
     @Autowired
-    ProcessRoleRepository userAppRoleRepository;
-    @Autowired
-    ApplicationStatusRepository applicationStatusRepository;
-    @Autowired
-    UserRepository userRepository;
-    @Autowired
-    ResponseRepository responseRepository;
+    ProcessHandler processHandler;
+
 
     private final Log log = LogFactory.getLog(getClass());
 
@@ -45,6 +41,19 @@ public class CompetitionController {
 
     @RequestMapping("/findAll")
     public List<Competition> findAll() {
+
+
+        Process p  = new Process(ProcessEvent.ASSESSMENT_INVITATION, ProcessStatus.PENDING, new Long(3), new Long(4));
+        //Process(ProcessEvent event, ProcessStatus status, Long assigneeId, Long subjectId)
+        processHandler.saveProcess(p);
+
+        System.out.println("There are " + processHandler.getProcessesByAssigneeAndType(new Long(1), ProcessEvent.ASSESSMENT_INVITATION).size() + " @ with assignee 1 and event Assessment Invit");
+        System.out.println("There are " + processHandler.getProcessesByAssigneeAndType(new Long(3),ProcessEvent.ASSESSMENT_INVITATION).size() + " @ with assignee 3 and event Assessment Invit");
+
+
+
+
         return repository.findAll();
+
     }
 }
