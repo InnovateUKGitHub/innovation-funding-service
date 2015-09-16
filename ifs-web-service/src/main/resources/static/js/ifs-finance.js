da88346108f113b1a65f77dd8c3c06c1941c5332
Wouter de Meijer <w.demeijer@gmail.com>
2015-09-16 18:01:26 +0200

    INFUND-370 - The repeated fields for Labour, Materials and Subcontracting Costs totals are now calculated and formatted properly

--- ifs-web-service/src/main/resources/static/js/ifs-finance.js
+++ ifs-web-service/src/main/resources/static/js/ifs-finance.js
@@ -8,26 +8,40 @@ var worthIFSFinance = {
     },
     domReady : function(){
         if(worthIFS.isApplicationForm()){
-            worthIFSFinance.repeatingTotal();
-            worthIFSFinance.initFinanceCalculations();
-
+            worthIFSFinance.preProcessRepeatedFieldsForTotalElements();
+            worthIFSFinance.bindCalculationActionToFields();
         }
     },
-    repeatingTotal : function(){
+    preProcessRepeatedFieldsForTotalElements : function(){
         if(jQuery('[data-calculation-repeating-total]').length){
             $('[data-calculation-repeating-total]').each(function(index,value){
-                var inst = $(this);
-                var fields = inst.attr("data-calculation-repeating-total");
-                var attributes = [];
-                $(fields).each(function(){
-                    attributes.push('#'+$(this).attr('id'));
-                });
-
-                inst.attr('data-calculation-fields',attributes.join());
+                worthIFSFinance.preProcessRepeatedFieldsForTotalElement($(this));
             });
         }
     },
-    initFinanceCalculations : function(){
+    preProcessRepeatedFieldsForTotalElement : function(totalElement) {
+        var fieldSelector = worthIFSFinance.getFieldSelector(totalElement);
+        var repeatedFieldElements = worthIFSFinance.getFieldsBySelector(fieldSelector);
+        worthIFSFinance.addRepeatedFieldIdsToTotalElement(totalElement, repeatedFieldElements);
+    },
+
+    getFieldsBySelector : function(fieldSelector) {
+        var fields = [];
+
+        $(fieldSelector).each(function(){
+            fields.push('#'+$(this).attr('id'));
+        });
+
+        return fields;
+    },
+    getFieldSelector : function(totalElement) {
+        return totalElement.attr("data-calculation-repeating-total");
+    },
+    addRepeatedFieldIdsToTotalElement : function(totalElement, repeatedFieldElements) {
+        totalElement.attr('data-calculation-fields',repeatedFieldElements.join());
+    },
+
+    bindCalculationActionToFields : function(){
         $('[data-calculation-fields]').each(function(index,value){
                 var inst = $(this);
                 var fields = inst.attr("data-calculation-fields").split(',');
@@ -45,11 +59,17 @@ var worthIFSFinance = {
             var operation = element.attr('data-calculation-operations').split(',');
             var values = [];
             _.each(calcFields, function(field){
-                values.push(parseFloat($(field).val() || 0));
+                values.push(parseFloat($(field).attr("data-calculation-rawvalue") || $(field).val() || 0));
             });
 
-            var calculatedValue = worthIFSFinance.MathOperation[operation[0]](values[0],values[1]);
+            if(values.length === 1) {
+                var calculatedValue=values[0];
+            }
+            else {
+                var calculatedValue = worthIFSFinance.MathOperation[operation[0]](values[0],values[1]);
+            }
             //one operation and more values, all get the same operation
+
             if((operation.length == 1) && (values.length > 2)) {
                 for (i = 2; i < values.length; i++) { 
                     console.log('round:',i,typeof(operation[0]),operation[0],typeof(calculatedValue),calculatedValue,typeof(values[i]),values[i],values)
@@ -63,8 +83,19 @@ var worthIFSFinance = {
                     calculatedValue = worthIFSFinance.MathOperation[operation[i]](calculatedValue,values[i+1]);
                 }
             }
-            element.val(Math.round(calculatedValue));
+            element.attr("data-calculation-rawvalue",calculatedValue);
+            element.val(worthIFSFinance.formatCurrency(Math.round(calculatedValue)));
             element.trigger('change');
+    },
+
+    formatCurrency: function(total) {
+        total = Math.abs(total);
+        parsedFloat = parseFloat(total, 10);
+        tofixed = parsedFloat.toFixed();
+        replaced = tofixed.replace(/(\d)(?=(\d{3})+\b)/g, "$1,");
+        tostring= replaced.toString();
+
+        return tostring;
     }
 } 
 
