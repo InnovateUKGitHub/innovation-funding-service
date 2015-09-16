a240409db207cf1d25975304406e2e958040b038
Nuno Alexandre <nalexandre@worth.systems>
2015-09-16 18:23:14 +0200

    Added 3 table drops.

--- setup-files/data-dump/ifs-drop-tables.sql
+++ setup-files/data-dump/ifs-drop-tables.sql
@@ -29,7 +29,10 @@ DROP TABLE IF EXISTS competition_applications;
 DROP TABLE IF EXISTS process;
 DROP TABLE IF EXISTS user;
 DROP TABLE IF EXISTS user_application_role;
-DROP TABLE IF EXISTS process;
+DROP TABLE IF EXISTS assessment;
+DROP TABLE IF EXISTS assessment_assessments;
+DROP TABLE IF EXISTS response_assessment;
+
 DROP TABLE IF EXISTS process_event;
 DROP TABLE IF EXISTS process_role;
 DROP TABLE IF EXISTS process_type;
