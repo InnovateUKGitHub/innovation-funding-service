203ef2e15a9466427f47ab394656b7c9bce72eb7
Wouter de Meijer <w.demeijer@gmail.com>
2015-09-30 16:31:57 +0200

    Fixed ifs-reset-for-test script breaking the Application Form after execution

--- setup-files/data-dump/ifs-reset-for-test.sql
+++ setup-files/data-dump/ifs-reset-for-test.sql
@@ -1,4 +1,4 @@
 TRUNCATE question_status;
 UPDATE response SET value='';
-UPDATE cost SET cost=NULL, quantity=NULL,item=NULL;
+UPDATE cost SET cost=0, quantity=0,item='';
 UPDATE process SET status='PENDING' WHERE id='3' OR id='4';
