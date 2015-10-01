4adaadc9653fce683e6c79dce00de7df0106bdba
Wouter de Meijer <w.demeijer@gmail.com>
2015-10-01 12:48:59 +0200

    INFUND-440 - Updated Assessor tests and test reset SQL for new workflow changes

--- setup-files/data-dump/ifs-reset-for-test.sql
+++ setup-files/data-dump/ifs-reset-for-test.sql
@@ -1,5 +1,6 @@
 TRUNCATE question_status;
 UPDATE response SET value='';
 UPDATE cost SET cost=0, quantity=0,item='';
-UPDATE process SET status='PENDING' WHERE id='3' OR id='4';
+UPDATE process SET status='pending' WHERE id='3' OR id='4';
+UPDATE assessment SET submitted='Y' WHERE application=1;
 UPDATE assessment SET temp_recommended_value='EMPTY', recommendation_feedback='test' WHERE application=6
