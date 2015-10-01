c0edee97ef17205576ee150be3b615ed386a4bc0
Duncan Watson <duncan@hiveit.co.uk>
2015-10-01 12:07:16 +0100

    INFUND-345 - added autosave javascript to assessor feedback textareas

--- ifs-web-service/src/main/resources/static/js/ifs-assessment-feedback.js
+++ ifs-web-service/src/main/resources/static/js/ifs-assessment-feedback.js
@@ -3,23 +3,49 @@
 $(function() {
     "use strict";
 
-    var handleAssessorFeedbackFieldChange = function(parameterName) {
+    // TODO DW - consider suggesting as an alternative to the use of "typeWatch" plugin as used in the Application Form autosave javascript in ifs.js, as
+    // it is event delegation-based rather than needing to be registered directly on DOM elements, and is therefore kinder to pages that dynamically
+    // update their contents
+    var registerKeyupCallback = function(callback, keyupDelay) {
 
-        return function() {
-
+        return function(e) {
             var field = $(this);
-            var formUrl = field.closest('form').attr('action');
-            var feedbackContainer = field.closest('[data-response-id]');
-            var responseId = feedbackContainer.attr('data-response-id')
-                ;
-            $.ajax({
-                type: "PUT",
-                url: formUrl + '/response/' + responseId + '?' + parameterName + '=' + field.val()
-            });
+            var existingCountdown = field.data('keyupListener');
+            if (typeof existingCountdown !== 'undefined') {
+                window.clearTimeout(existingCountdown);
+            }
+            field.data('keyupListener', window.setTimeout(function() {
+                callback(field, e);
+                field.data('keyupListener', null);
+            }, keyupDelay));
+        };
+    };
+
+    var handleAssessorFeedbackFieldChangeOnField = function(parameterName, field) {
+        var formUrl = field.closest('form').attr('action');
+        var feedbackContainer = field.closest('[data-response-id]');
+        var responseId = feedbackContainer.attr('data-response-id')
+            ;
+        $.ajax({
+            type: "PUT",
+            url: formUrl + '/response/' + responseId + '?' + parameterName + '=' + field.val()
+        });
+    };
+
+    var handleAssessorFeedbackFieldChange = function(parameterName) {
+        return function() {
+            var field = $(this);
+            return handleAssessorFeedbackFieldChangeOnField(parameterName, field);
         };
     };
 
     $('body').on('change', '[id ^= "assessor-question-score-"]', handleAssessorFeedbackFieldChange('score'));
     $('body').on('change', '[id ^= "assessor-question-confirmation-"]', handleAssessorFeedbackFieldChange('confirmationAnswer'));
     $('body').on('change', '[id ^= "assessor-question-feedback-"]', handleAssessorFeedbackFieldChange('feedbackText'));
+
+    var updateAssessorFeedbackTextarea = function(field) {
+        handleAssessorFeedbackFieldChangeOnField('feedbackText', field);
+    };
+
+    $('body').on('keyup', '[id ^= "assessor-question-feedback-"]', registerKeyupCallback(updateAssessorFeedbackTextarea, 500));
 });
