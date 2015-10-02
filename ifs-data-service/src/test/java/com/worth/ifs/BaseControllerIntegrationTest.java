699f3b1a926cc0fef9d79d655c1b1ab8faa4fae4
Duncan Watson <duncan@hiveit.co.uk>
2015-10-02 16:36:07 +0100

    Added transactional controller integration test with rollback in ApplicationControllerIntegrationTest

--- ifs-data-service/src/test/java/com/worth/ifs/BaseControllerIntegrationTest.java
+++ ifs-data-service/src/test/java/com/worth/ifs/BaseControllerIntegrationTest.java
@@ -1,10 +1,12 @@
 package com.worth.ifs;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * Created by dwatson on 02/10/15.
  */
+@Transactional
 public abstract class BaseControllerIntegrationTest<ControllerType> extends BaseWebIntegrationTest {
 
     protected ControllerType controller;
