58711c634c2de0264371348fa44f8722b82a31ba
Duncan Watson <duncan@hiveit.co.uk>
2015-10-02 12:40:22 +0100

    Added examples of transactional readonly tests to the UserRepositoryTest class

--- ifs-data-service/src/test/java/com/worth/ifs/user/repository/UserRepositoryTest.java
+++ ifs-data-service/src/test/java/com/worth/ifs/user/repository/UserRepositoryTest.java
@@ -1,10 +1,14 @@
 package com.worth.ifs.user.repository;
 
 import com.worth.ifs.BaseRepositoryIntegrationTest;
+import com.worth.ifs.user.domain.ProcessRole;
 import com.worth.ifs.user.domain.User;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static java.util.Arrays.asList;
@@ -23,7 +27,7 @@ public class UserRepositoryTest extends BaseRepositoryIntegrationTest<UserReposi
     }
 
     @Test
-    public void test_userList() {
+    public void test_findAll() {
 
         //
         // Fetch the list of users
@@ -39,4 +43,48 @@ public class UserRepositoryTest extends BaseRepositoryIntegrationTest<UserReposi
         assertTrue(emailAddresses.containsAll(expectedUsers));
     }
 
+    @Test
+    @Transactional
+    @Rollback
+    public void test_createUser() {
+
+        //
+        // Create a new user
+        //
+        User newUser = repository.save(new User("New User", "new@example.com", "apassword", "worthsystemsandhiveittogetheratlast", "", new ArrayList<ProcessRole>()));
+        assertNotNull(newUser.getId());
+
+        //
+        // Fetch the list of users and assert that the count has increased and the new user is present in the list of expected users
+        //
+        List<User> users = repository.findAll();
+        assertEquals(7, users.size());
+        List<String> emailAddresses = users.stream().map(User::getEmail).collect(toList());
+        List<String> expectedUsers = asList("steve.smith@empire.com", "jessica.doe@ludlow.co.uk", "paul.plum@gmail.com", "competitions@innovateuk.gov.uk", "finance@innovateuk.gov.uk", "pete.tom@egg.com", "new@example.com");
+        assertTrue(emailAddresses.containsAll(expectedUsers));
+    }
+
+    @Test
+    @Transactional
+    @Rollback
+    public void test_deleteNewUser() {
+
+        //
+        // Create a new user
+        //
+        User newUser = repository.save(new User("New User", "new@example.com", "apassword", "worthsystemsandhiveittogetheratlast", "", new ArrayList<ProcessRole>()));
+
+        //
+        // and immediately delete them
+        //
+        repository.delete(newUser.getId());
+
+        //
+        // Fetch the list of users and assert that the user doesn't exist in this list
+        //
+        List<User> users = repository.findAll();
+        assertEquals(6, users.size());
+        List<String> emailAddresses = users.stream().map(User::getEmail).collect(toList());
+        assertFalse(emailAddresses.contains("new@example.com"));
+    }
 }
