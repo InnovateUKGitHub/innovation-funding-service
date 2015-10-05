1e1389c9647fc10d0bc4de7a10345684f7f575e0
Wouter de Meijer <w.demeijer@gmail.com>
2015-10-05 13:26:12 +0200

    Updated SQL script and content variables in Assessor tests to match new content for the tests to properly work.

--- setup-files/data-dump/ifs-reset-for-test.sql
+++ setup-files/data-dump/ifs-reset-for-test.sql
@@ -3,4 +3,5 @@ UPDATE response SET value='';
 UPDATE cost SET cost=0, quantity=0,item='';
 UPDATE process SET status='pending' WHERE id='3' OR id='4';
 UPDATE assessment SET submitted='Y' WHERE application=1;
+UPDATE assessment SET submitted='Y' WHERE process_id=2;
 UPDATE assessment SET temp_recommended_value='EMPTY', recommendation_feedback='test' WHERE application=6
