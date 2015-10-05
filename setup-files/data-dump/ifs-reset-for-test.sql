b2e2d0af76a388f9bef0c5e864ab91b080e71bc3
Wouter de Meijer <w.demeijer@gmail.com>
2015-10-05 14:12:11 +0200

    INFUND-354 - Added basic tests for the Application Summary sections and persistence of Application Review changes

--- setup-files/data-dump/ifs-reset-for-test.sql
+++ setup-files/data-dump/ifs-reset-for-test.sql
@@ -1,7 +1,8 @@
-TRUNCATE question_status;
-UPDATE response SET value='';
-UPDATE cost SET cost=0, quantity=0,item='';
-UPDATE process SET status='pending' WHERE id='3' OR id='4';
-UPDATE assessment SET submitted='Y' WHERE application=1;
-UPDATE assessment SET submitted='Y' WHERE process_id=2;
-UPDATE assessment SET temp_recommended_value='EMPTY', recommendation_feedback='test' WHERE application=6
+TRUNCATE question_status; 										# Remove all question statuses so no question is already assigned or marked as complete
+UPDATE response SET value=''; 									# Reset all responses to nothing so all questions start empty
+UPDATE cost SET cost=0, quantity=0,item=''; 					# reset all costs to zero for testing the application form
+UPDATE process SET status='pending' WHERE id='3' OR id='4'; 	# Set two applications up as pending for Assessor testing
+UPDATE assessment SET submitted='Y' WHERE application=1; 		# Set first application to submitted for Assessor testing
+UPDATE assessment SET submitted='Y' WHERE process_id=2;			# Set second application to submitted for Assessor testing
+UPDATE response SET assessment_feedback=NULL, assessment_score=NULL WHERE question_id='13' and application_id='5'; # Reset the first question feedback response for Assessor testing.
+UPDATE assessment SET temp_recommended_value='EMPTY', recommendation_feedback='test' WHERE application=6 # Reset the recommendation feedback for application 6 for Assessor testing.
