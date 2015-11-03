97f3387ffce6e13adc519298d66f164825e7cdd8
David Soff <david@soff.nl>
2015-11-03 17:34:38 +0000

    INFUND-560 add CostFieldTest

---
+++ ifs-data-service/src/test/java/com/worth/ifs/finance/domain/CostFieldTest.java
@@ -0,0 +1,27 @@
+package com.worth.ifs.finance.domain;
+
+        import org.junit.Assert;
+        import org.junit.Before;
+        import org.junit.Test;
+
+public class CostFieldTest {
+    Long id;
+    String title;
+    String type;
+    CostField costField;
+
+    @Before
+    public void setUp() throws Exception {
+        id = 123456L;
+        title = "field title";
+        type = "field type";
+        costField = new CostField(id, title, type);
+    }
+
+    @Test
+    public void sectionShouldReturnCorrectAttributeValues() throws Exception {
+        Assert.assertEquals(costField.getId(), id);
+        Assert.assertEquals(costField.getTitle(), title);
+        Assert.assertEquals(costField.getType(), type);
+    }
+}
