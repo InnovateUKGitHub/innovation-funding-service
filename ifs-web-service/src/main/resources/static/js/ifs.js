742b1a32e3e2c747319359a555a29dd30938084a
Brent de Kok <brent@worth.systems>
2015-11-10 14:20:58 +0100

    Small js tweaks, merged loader to ifs.js

--- ifs-web-service/src/main/resources/static/js/ifs.js
+++ ifs-web-service/src/main/resources/static/js/ifs.js
@@ -43,3 +43,35 @@ var IFS = {
   }
 };
 
+//util for kicking it off.
+var UTIL = (function(){
+  "use strict";
+  return {
+      fire : function(func,funcname, args){
+        var namespace = IFS;  // indicate your obj literal namespace here
+        funcname = (funcname === undefined) ? 'init' : funcname;
+        if (func !== '' && namespace[func] && typeof namespace[func][funcname] == 'function'){
+          namespace[func][funcname](args);
+        }
+      },
+      loadEvents : function(){
+        var bodyId = document.body.id;
+        // hit up common first.
+        UTIL.fire('common');
+        // do all the classes too.
+        jQuery.each(document.body.className.split(/\s+/),function(i,classnm){
+          UTIL.fire(classnm);
+          UTIL.fire(classnm,bodyId);
+        });
+        UTIL.fire('common','finalize');
+      }
+  };
+
+})();
+// kick it all off here 
+
+jQuery(document).ready(function(){
+    UTIL.loadEvents();
+});
+
+
