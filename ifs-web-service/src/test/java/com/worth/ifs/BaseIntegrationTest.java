a324f0136d10ed0b2329148149cc11c4f75de43f
richardpopple <richard@hiveit.co.uk>
2015-11-11 16:30:33 +0000

    Fixing the build due to common test hierarchy having dependencies on the data project that are not exposed outside the data project. Current solution is to have two test hierar

---
+++ ifs-web-service/src/test/java/com/worth/ifs/BaseIntegrationTest.java
@@ -0,0 +1,19 @@
+package com.worth.ifs;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * This is the base class for all integration tests against a configured Spring application.  Subclasses of this base can be
+ * of the form of either integration tests with a running server ({@link BaseWebIntegrationTest}) or without
+ * (e.g. {@link BaseRepositoryIntegrationTest}).
+ *
+ * Created by dwatson on 02/10/15.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Application.class)
+@TestPropertySource(locations="classpath:application-web-integration-test.properties")
+public abstract class BaseIntegrationTest {
+}
