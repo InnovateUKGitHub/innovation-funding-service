7630bad06eeac19fbf72498dcfe0b33a97366a37
Nico Bijl <nbijl@worth.systems>
2015-11-18 16:42:32 +0100

    INFUND-43 unit test for data-services, for not allowed save action.

--- ifs-data-service/src/test/java/com/worth/ifs/form/domain/FormInputTest.java
+++ ifs-data-service/src/test/java/com/worth/ifs/form/domain/FormInputTest.java
@@ -5,6 +5,8 @@ import com.worth.ifs.validator.EmailValidator;
 import com.worth.ifs.validator.NotEmptyValidator;
 import org.junit.Test;
 
+import java.util.HashSet;
+
 import static org.hamcrest.Matchers.hasItem;
 import static org.junit.Assert.*;
 
@@ -34,6 +36,31 @@ public class FormInputTest {
         assertThat(formInputType.getFormValidators(), hasItem(emailValidator));
         assertThat(formInputType.getFormValidators(), hasItem(notEmptyValidator));
         assertEquals(formInputType.getFormValidators(), Sets.newHashSet(emailValidator, notEmptyValidator));
+    }
+
+    @Test
+    public void test_formInputTypeAddValidatorSet() throws Exception {
+        Long id = 1L;
+        String title = "formInputTypeTitle";
+        FormInput formInputType = new FormInput();
+
+        String validatorTitle = "Email Validator";
+        Class clazz = EmailValidator.class;
+
+        FormValidator emailValidator = new FormValidator();
+        emailValidator.setTitle(validatorTitle);
+        emailValidator.setClass(clazz);
+
+        FormValidator notEmptyValidator = new FormValidator();
+        notEmptyValidator.setTitle(validatorTitle);
+        notEmptyValidator.setClass(NotEmptyValidator.class);
+
+        HashSet validators = new HashSet();
+        validators.add(emailValidator);
+        formInputType.setFormValidators(validators);
+
+        assertThat(formInputType.getFormValidators(), hasItem(emailValidator));
+        assertEquals(formInputType.getFormValidators(), Sets.newHashSet(emailValidator));
     }
 
 }
