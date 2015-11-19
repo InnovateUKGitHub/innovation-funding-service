d9c6c565c897dfceed715f4a016221e35c97eb73
Duncan Watson <duncan@hiveit.co.uk>
2015-11-19 11:43:22 +0000

    INFUND-660 - finishing ApplicationFinanceRestService refactor and testing

--- ifs-data-service/src/test/java/com/worth/ifs/finance/service/ApplicationFinanceRestServiceMocksTest.java
+++ ifs-data-service/src/test/java/com/worth/ifs/finance/service/ApplicationFinanceRestServiceMocksTest.java
@@ -5,9 +5,10 @@ import com.worth.ifs.finance.domain.ApplicationFinance;
 import org.junit.Test;
 import org.springframework.http.ResponseEntity;
 
+import java.util.List;
+
 import static com.worth.ifs.finance.builder.ApplicationFinanceBuilder.newApplicationFinance;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.*;
 import static org.mockito.Mockito.when;
 import static org.springframework.http.HttpMethod.GET;
 import static org.springframework.http.HttpStatus.OK;
@@ -39,4 +40,55 @@ public class ApplicationFinanceRestServiceMocksTest extends BaseRestServiceMocks
         assertNotNull(finance);
         assertEquals(returnedResponse, finance);
     }
+
+    @Test
+    public void test_getApplicationFinance_forApplicationIdAndOrganisationId_nullSafe() {
+        assertNull(service.getApplicationFinance(123L, null));
+        assertNull(service.getApplicationFinance(null, 456L));
+        assertNull(service.getApplicationFinance(null, null));
+    }
+
+    @Test
+    public void test_getApplicationFinances_forApplicationId() {
+
+        String expectedUrl = dataServicesUrl + applicationFinanceRestURL + "/findByApplication/123";
+        ApplicationFinance[] returnedResponse = newApplicationFinance().buildArray(3, ApplicationFinance.class);
+        ResponseEntity<ApplicationFinance[]> returnedEntity = new ResponseEntity<>(returnedResponse, OK);
+
+        when(mockRestTemplate.exchange(expectedUrl, GET, httpEntityForRestCall(), ApplicationFinance[].class)).thenReturn(returnedEntity);
+
+        List<ApplicationFinance> finances = service.getApplicationFinances(123L);
+        assertNotNull(finances);
+        assertEquals(returnedResponse[0], finances.get(0));
+        assertEquals(returnedResponse[1], finances.get(1));
+        assertEquals(returnedResponse[2], finances.get(2));
+    }
+
+
+    @Test
+    public void test_getApplicationFinances_forApplicationId_nullSafe() {
+        assertNull(service.getApplicationFinances(null));
+    }
+
+    @Test
+    public void test_addApplicationFinance_forApplicationIdAndOrganisationId() {
+
+        String expectedUrl = dataServicesUrl + applicationFinanceRestURL + "/add/123/456";
+        ApplicationFinance returnedResponse = newApplicationFinance().build();
+        ResponseEntity<ApplicationFinance> returnedEntity = new ResponseEntity<>(returnedResponse, OK);
+
+        when(mockRestTemplate.postForEntity(expectedUrl, httpEntityForRestCall(null), ApplicationFinance.class)).thenReturn(returnedEntity);
+
+        ApplicationFinance finance = service.addApplicationFinanceForOrganisation(123L, 456L);
+        assertNotNull(finance);
+        assertEquals(returnedResponse, finance);
+    }
+
+    @Test
+    public void test_addApplicationFinance_nullSafe() {
+        assertNull(service.addApplicationFinanceForOrganisation(123L, null));
+        assertNull(service.addApplicationFinanceForOrganisation(null, 456L));
+        assertNull(service.addApplicationFinanceForOrganisation(null, null));
+    }
+
 }
