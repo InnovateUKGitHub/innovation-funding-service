d13a2cbf79e7fb453c9ab2eac15eab522674f1ab
Duncan Watson <duncan@hiveit.co.uk>
2015-11-19 14:47:20 +0000

    INFUND-660 - refactored and tested SectionRestService

--- ifs-data-service/src/main/java/com/worth/ifs/application/service/SectionRestServiceImpl.java
+++ ifs-data-service/src/main/java/com/worth/ifs/application/service/SectionRestServiceImpl.java
@@ -3,12 +3,11 @@ package com.worth.ifs.application.service;
 import com.worth.ifs.application.domain.Section;
 import com.worth.ifs.commons.service.BaseRestService;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
-import java.util.Arrays;
 import java.util.List;
+
+import static java.util.Arrays.asList;
 
 /**
  * SectionRestServiceImpl is a utility for CRUD operations on {@link Section}.
@@ -20,24 +19,18 @@ public class SectionRestServiceImpl extends BaseRestService implements SectionRe
     @Value("${ifs.data.service.rest.section}")
     String sectionRestURL;
 
-
+    @Override
     public List<Long> getCompletedSectionIds(Long applicationId, Long organisationId) {
-        RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<Long[]> responseEntity = restTemplate.getForEntity(dataRestServiceURL + sectionRestURL + "/getCompletedSections/"+applicationId+"/"+organisationId, Long[].class);
-        Long[] sections =responseEntity.getBody();
-        return Arrays.asList(sections);
+        return asList(restGet(sectionRestURL + "/getCompletedSections/"+applicationId+"/"+organisationId, Long[].class));
     }
 
+    @Override
     public List<Long> getIncompletedSectionIds(Long applicationId) {
-        RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<Long[]> responseEntity = restTemplate.getForEntity(dataRestServiceURL + sectionRestURL + "/getIncompleteSections/"+applicationId, Long[].class);
-        Long[] sections =responseEntity.getBody();
-        return Arrays.asList(sections);
+        return asList(restGet(sectionRestURL + "/getIncompleteSections/"+applicationId, Long[].class));
     }
 
+    @Override
     public Section getSection(String name) {
-        RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<Section> responseEntity = restTemplate.getForEntity(dataRestServiceURL + sectionRestURL + "/findByName/"+name, Section.class);
-        return responseEntity.getBody();
+        return restGet(sectionRestURL + "/findByName/" + name, Section.class);
     }
 }
