3f061f20e9310734819b5d5728a3c7f331c5d950
Duncan Watson <duncan@hiveit.co.uk>
2015-11-23 23:04:00 +0000

    INFUND-660 - amended User equality checks to help permission=checking code pass - correct checking for assessor permissions around viewing and updating feedback

--- ifs-data-service/src/main/java/com/worth/ifs/security/FeedbackRules.java
+++ ifs-data-service/src/main/java/com/worth/ifs/security/FeedbackRules.java
@@ -2,21 +2,33 @@ package com.worth.ifs.security;
 
 
 import com.worth.ifs.assessment.dto.Feedback;
+import com.worth.ifs.user.domain.ProcessRole;
 import com.worth.ifs.user.domain.User;
+import com.worth.ifs.user.domain.UserRoleType;
+import com.worth.ifs.user.repository.ProcessRoleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 @PermissionRules
 public class FeedbackRules {
 
+    @Autowired
+    private ProcessRoleRepository processRoleRepository;
 
     @PermissionRule("READ")
-    public boolean hasPermission1(Feedback dto, User user){
-        return true;
+    public boolean hasPermission1(Feedback dto, User user) {
+        return isAssessorForApplication(dto, user);
     }
 
     @PermissionRule("UPDATE")
     public boolean hasPermission2(Feedback dto, User user){
-        return false;
+        return isAssessorForApplication(dto, user);
+    }
+
+    private boolean isAssessorForApplication(Feedback dto, User user) {
+        ProcessRole processRole = processRoleRepository.findOne(dto.getAssessorProcessRoleId());
+        return processRole.getRole().getName().equals(UserRoleType.ASSESSOR.getName()) &&
+                processRole.getUser().equals(user);
     }
 }
