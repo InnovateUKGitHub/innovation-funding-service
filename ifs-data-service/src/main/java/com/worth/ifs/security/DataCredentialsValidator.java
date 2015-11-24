1a86a83d296386208a7e1f1a38927b369bb9c772
Nico Bijl <nbijl@worth.systems>
2015-11-24 14:29:11 +0000

    Optimize java imports.

--- ifs-data-service/src/main/java/com/worth/ifs/security/DataCredentialsValidator.java
+++ ifs-data-service/src/main/java/com/worth/ifs/security/DataCredentialsValidator.java
@@ -3,7 +3,6 @@ package com.worth.ifs.security;
 import com.worth.ifs.commons.security.CredentialsValidator;
 import com.worth.ifs.user.controller.UserController;
 import com.worth.ifs.user.domain.User;
-import org.hibernate.Hibernate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
--- ifs-data-service/src/test/java/com/worth/ifs/BaseControllerMockMVCTest.java
+++ ifs-data-service/src/test/java/com/worth/ifs/BaseControllerMockMVCTest.java
@@ -1,24 +1,13 @@
 package com.worth.ifs;
 
-import com.worth.ifs.application.repository.ApplicationRepository;
-import com.worth.ifs.finance.repository.ApplicationFinanceRepository;
-import com.worth.ifs.user.repository.ProcessRoleRepository;
-import com.worth.ifs.user.repository.UserRepository;
 import org.junit.Before;
 import org.junit.Rule;
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 import org.springframework.restdocs.RestDocumentation;
-import org.springframework.restdocs.mockmvc.RestDocumentationResultHandler;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
-import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
-import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
-import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
 
 /**
  * This is the base class for testing Controllers using MockMVC in addition to standard Mockito mocks.  Using MockMVC
