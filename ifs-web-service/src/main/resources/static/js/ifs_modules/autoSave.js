4da66103a3b9cf6e57294a1c4257132e0e56b7a8
Brent de Kok <brent@worth.systems>
2015-12-14 14:00:16 +0100

    INFUND-1108 Single question per page front-end

--- ifs-web-service/src/main/resources/static/js/ifs_modules/autoSave.js
+++ ifs-web-service/src/main/resources/static/js/ifs_modules/autoSave.js
@@ -37,70 +37,74 @@ IFS.autoSave = (function(){
              var validationMessages = formGroup.find(".validation-messages").first();
              var formTextareaSaveInfo = formGroup.find('.textarea-save-info');
              var startAjaxTime= new Date().getTime();
+             var applicationId = jQuery("#application_id").val();
 
              if(formTextareaSaveInfo.length === 0){
                 formGroup.find('.textarea-footer').append('<span class="textarea-save-info" />');
                 formTextareaSaveInfo = formGroup.find('.textarea-save-info');
              }
 
-             jQuery.ajax({
-                 type: 'POST',
-                 url: "/application-form/saveFormElement",
-                 data: jsonObj,
-                 dataType: "json",
-                 beforeSend: function() {
-                    formTextareaSaveInfo.html('Saving...');
-                }
-             })
-             .done(function(data){
-                 var doneAjaxTime = new Date().getTime();
-                 var remainingWaitingTime = (IFS.autoSave.settings.minimumUpdateTime-(doneAjaxTime-startAjaxTime));
+             if(typeof(applicationId) !== 'undefined') {
+                    console.log(applicationId);
+                 jQuery.ajax({
+                     type: 'POST',
+                     url: '/application/'+applicationId+'/form/saveFormElement',
+                     data: jsonObj,
+                     dataType: "json",
+                     beforeSend: function() {
+                        formTextareaSaveInfo.html('Saving...');
+                    }
+                 })
+                 .done(function(data){
+                     var doneAjaxTime = new Date().getTime();
+                     var remainingWaitingTime = (IFS.autoSave.settings.minimumUpdateTime-(doneAjaxTime-startAjaxTime));
 
-                 // set the form-saved-state
-                 jQuery('.form-serialize-js').data('serializedFormState',formState);
-                 field.removeClass('error');
-                 formGroup.removeClass('error');
-                  
-                  //save message
-                 if(data.success == 'true'){
-                     setTimeout(function(){
-                         IFS.autoSave.removeValidationError(formGroup);
-                         formTextareaSaveInfo.html('Saved!');
-                     }, remainingWaitingTime);
-                 }else{
-                     setTimeout(function(){
-                         formTextareaSaveInfo.html("Invalid input, but saved anyway.");
-                         IFS.autoSave.removeValidationError(formGroup);
-                         jQuery.each(data.validation_errors, function(index, value){
-                             validationMessages.append('<span class="error-message">' + value + '</span>');
-                         });
-                         formGroup.addClass('error');
-                     }, remainingWaitingTime);
-                 }
-             }).fail(function(data) {
-                 var doneAjaxTime = new Date().getTime();
-                 var remainingWaitingTime = (IFS.autoSave.settings.minimumUpdateTime-(doneAjaxTime-startAjaxTime));
-
-                 setTimeout(function(){
-                     IFS.autoSave.removeValidationError(formGroup);
-
-                     var errorMessage = data.responseJSON.errorMessage;
-                     if (formGroup.length) {
-                         formGroup.addClass('error');
-                         if(formTextareaSaveInfo.length){
-                            formTextareaSaveInfo.html(errorMessage);
-                         }
-                         else {
-                            var label = formGroup.find('label').first();
-                            if (label.length) {
-                              validationMessages.append('<span class="error-message">' + errorMessage + '</span>');
-                            }
-                         }
-                     } else {
-                         field.addClass('error');
+                     // set the form-saved-state
+                     jQuery('.form-serialize-js').data('serializedFormState',formState);
+                     field.removeClass('error');
+                     formGroup.removeClass('error');
+                      
+                      //save message
+                     if(data.success == 'true'){
+                         setTimeout(function(){
+                             IFS.autoSave.removeValidationError(formGroup);
+                             formTextareaSaveInfo.html('Saved!');
+                         }, remainingWaitingTime);
+                     }else{
+                         setTimeout(function(){
+                             formTextareaSaveInfo.html("Invalid input, but saved anyway.");
+                             IFS.autoSave.removeValidationError(formGroup);
+                             jQuery.each(data.validation_errors, function(index, value){
+                                 validationMessages.append('<span class="error-message">' + value + '</span>');
+                             });
+                             formGroup.addClass('error');
+                         }, remainingWaitingTime);
                      }
-                 }, remainingWaitingTime);
-             });
+                 }).fail(function(data) {
+                     var doneAjaxTime = new Date().getTime();
+                     var remainingWaitingTime = (IFS.autoSave.settings.minimumUpdateTime-(doneAjaxTime-startAjaxTime));
+
+                     setTimeout(function(){
+                         IFS.autoSave.removeValidationError(formGroup);
+
+                         var errorMessage = data.responseJSON.errorMessage;
+                         if (formGroup.length) {
+                             formGroup.addClass('error');
+                             if(formTextareaSaveInfo.length){
+                                formTextareaSaveInfo.html(errorMessage);
+                             }
+                             else {
+                                var label = formGroup.find('label').first();
+                                if (label.length) {
+                                  validationMessages.append('<span class="error-message">' + errorMessage + '</span>');
+                                }
+                             }
+                         } else {
+                             field.addClass('error');
+                         }
+                     }, remainingWaitingTime);
+                 });
+             }
         },
         removeValidationError: function (formGroup){
             formGroup.removeClass('error');
