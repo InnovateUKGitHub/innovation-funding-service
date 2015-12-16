595e0a73c222d3e7c10ce24528aad9678debb855
Ravish Bhagdev <rav@hiveit.co.uk>
2015-12-16 10:32:27 +0000

    Adding unit test for new service

---
+++ ifs-data-service/src/test/java/com/worth/ifs/application/builder/QuestionStatusBuilder.java
@@ -0,0 +1,43 @@
+package com.worth.ifs.application.builder;
+
+import com.worth.ifs.BaseBuilder;
+import com.worth.ifs.application.domain.Application;
+import com.worth.ifs.application.domain.QuestionStatus;
+
+import java.util.List;
+import java.util.function.BiConsumer;
+
+import static com.worth.ifs.BuilderAmendFunctions.*;
+import static java.util.Collections.emptyList;
+
+public class QuestionStatusBuilder extends BaseBuilder<QuestionStatus, QuestionStatusBuilder> {
+
+    private QuestionStatusBuilder(List<BiConsumer<Integer, QuestionStatus>> newMultiActions) {
+        super(newMultiActions);
+    }
+
+    @Override
+    protected QuestionStatusBuilder createNewBuilderWithActions(List<BiConsumer<Integer, QuestionStatus>> actions) {
+        return new QuestionStatusBuilder(actions);
+    }
+
+    public static QuestionStatusBuilder newQuestionStatus() {
+        return new QuestionStatusBuilder(emptyList())
+                .with(uniqueIds());
+    }
+
+    public QuestionStatusBuilder withQuestionNumber(String value) {
+        return with(question -> setField("questionNumber", value, question));
+    }
+
+    public QuestionStatusBuilder withApplication(Application application) {
+        return with(questionStatus -> {
+            questionStatus.setApplication(application);
+        });
+    }
+
+    @Override
+    protected QuestionStatus createInitial() {
+        return new QuestionStatus();
+    }
+}
