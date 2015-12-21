963c5f0ea96ec56c4993c2c5e91201e528c1f052
Duncan Watson <duncan@hiveit.co.uk>
2015-12-21 10:49:38 +0000

    INFUND-1138 - more detail around argument checking in controller test

--- ifs-data-service/src/test/java/com/worth/ifs/application/controller/FormInputResponseFileUploadControllerTest.java
+++ ifs-data-service/src/test/java/com/worth/ifs/application/controller/FormInputResponseFileUploadControllerTest.java
@@ -8,16 +8,20 @@ import com.worth.ifs.transactional.ServiceSuccess;
 import com.worth.ifs.util.Either;
 import org.apache.commons.lang3.tuple.Pair;
 import org.junit.Test;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MvcResult;
 
 import java.io.File;
 import java.util.function.Supplier;
 
 import static com.worth.ifs.BuilderAmendFunctions.id;
+import static com.worth.ifs.LambdaMatcher.lambdaMatches;
 import static com.worth.ifs.file.resource.builders.FileEntryResourceBuilder.newFileEntryResource;
 import static com.worth.ifs.util.Either.right;
 import static java.util.Arrays.asList;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.mockito.Matchers.argThat;
 import static org.mockito.Matchers.isA;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -43,7 +47,19 @@ public class FormInputResponseFileUploadControllerTest extends BaseControllerMoc
         Either<ServiceFailure, ServiceSuccess<Pair<File, FormInputResponseFileEntryResource>>> successResponse =
                 right(new ServiceSuccess(Pair.of(new File(""), new FormInputResponseFileEntryResource(newFileEntryResource().with(id(1111L)).build(), 123L, 456L, 789L))));
 
-        when(applicationService.createFormInputResponseFileUpload(isA(FormInputResponseFileEntryResource.class), isA(Supplier.class))).thenReturn(successResponse);
+        FormInputResponseFileEntryResource resourceExpectations = argThat(lambdaMatches(resource -> {
+            assertEquals(123L, resource.getCompoundId().getFormInputId());
+            assertEquals(456L, resource.getCompoundId().getApplicationId());
+            assertEquals(789L, resource.getCompoundId().getProcessRoleId());
+
+            assertNull(resource.getFileEntryResource().getId());
+            assertEquals(1000, resource.getFileEntryResource().getFilesizeBytes());
+            assertEquals(MediaType.parseMediaType("application/pdf"), resource.getFileEntryResource().getMediaType());
+            assertEquals("original.pdf", resource.getFileEntryResource().getName());
+            return true;
+        }));
+
+        when(applicationService.createFormInputResponseFileUpload(resourceExpectations, isA(Supplier.class))).thenReturn(successResponse);
 
         MvcResult response = mockMvc.
                 perform(
