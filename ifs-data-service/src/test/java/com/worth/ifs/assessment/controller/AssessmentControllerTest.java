c3f9c12964d26823c17178e24c40da284463be84
richardpopple <richard@hiveit.co.uk>
2015-12-22 19:31:34 +0000

    INFUND-572 api tests

--- ifs-data-service/src/test/java/com/worth/ifs/assessment/controller/AssessmentControllerTest.java
+++ ifs-data-service/src/test/java/com/worth/ifs/assessment/controller/AssessmentControllerTest.java
@@ -1,10 +1,15 @@
 package com.worth.ifs.assessment.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.worth.ifs.BaseControllerMockMVCTest;
 import com.worth.ifs.assessment.domain.Assessment;
+import com.worth.ifs.assessment.domain.RecommendedValue;
 import com.worth.ifs.assessment.dto.Score;
 import com.worth.ifs.assessment.workflow.AssessmentWorkflowEventHandler;
+import com.worth.ifs.user.controller.ProcessRoleController;
+import com.worth.ifs.user.domain.ProcessRole;
 import com.worth.ifs.workflow.domain.ProcessOutcome;
 import org.junit.Test;
 import org.mockito.Mock;
@@ -14,8 +19,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static com.worth.ifs.BuilderAmendFunctions.description;
+import static com.worth.ifs.BuilderAmendFunctions.id;
 import static com.worth.ifs.assessment.builder.AssessmentBuilder.newAssessment;
 import static com.worth.ifs.assessment.builder.ProcessOutcomeBuilder.newProcessOutcome;
+import static com.worth.ifs.user.builder.ProcessRoleBuilder.newProcessRole;
 import static org.hamcrest.core.Is.is;
 import static org.mockito.Mockito.when;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
@@ -32,6 +39,9 @@ public class AssessmentControllerTest extends BaseControllerMockMVCTest {
 
     @Mock
     AssessmentWorkflowEventHandler assessmentWorkflowEventHandler;
+
+    @Mock
+    ProcessRoleController processRoleController;
 
     @Override
     protected AssessmentController supplyControllerUnderTest() {
@@ -105,12 +115,22 @@ public class AssessmentControllerTest extends BaseControllerMockMVCTest {
 
     @Test
     public void testAcceptAssessmentInvitation() throws Exception {
-
+        long assessmentId = 1L;
+        long processRoleId = 2L;
+        Assessment assessment = newAssessment().withId(assessmentId).build();
+        String json = new ObjectMapper().writeValueAsString(assessment);
+        when(assessmentHandler.getOneByProcessRole(processRoleId)).thenReturn(assessment);
+        mockMvc.perform(post(applicationControllerPath + "/acceptAssessmentInvitation/" + processRoleId)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json))
+                .andExpect(status().isOk())
+                .andDo(document("assessment/accept-invitation-assessment"));
     }
 
     @Test
     public void testRejectAssessmentInvitation() throws Exception {
         ProcessOutcome processOutcome = newProcessOutcome()
+                .with(id(null))
                 .with(description("a description"))
                 .withOutcome("conflict-of-interest")
                 .build();
@@ -129,11 +149,58 @@ public class AssessmentControllerTest extends BaseControllerMockMVCTest {
 
     @Test
     public void testSubmitAssessments() throws Exception {
+        long assessorId = 1L;
+        long assessmentId1 = 2L;
+        long assessmentId2 = 3L;
+        Assessment assessment1 = newAssessment().withId(assessmentId1).build();
+        Assessment assessment2 = newAssessment().withId(assessmentId2).build();
 
+
+        ObjectMapper mapper = new ObjectMapper();
+        ObjectNode jsonNode = mapper.createObjectNode();
+        jsonNode.put("assessorId", assessorId);
+        ArrayNode arrayNode = mapper.createArrayNode();
+        jsonNode.putArray("assessmentsToSubmit").add(assessmentId1).add(assessmentId2);
+        String json = mapper.writeValueAsString(jsonNode);
+
+        when(assessmentHandler.getOne(assessmentId1)).thenReturn(assessment1);
+        when(assessmentHandler.getOne(assessmentId2)).thenReturn(assessment2);
+
+        mockMvc.perform(post(applicationControllerPath + "/submitAssessments").contentType(MediaType.APPLICATION_JSON)
+                .content(json))
+                .andExpect(status().isOk())
+                .andExpect(content().string("true"))
+                .andDo(document("assessment/submit-assessments-assessment"));
     }
 
     @Test
     public void testSubmitAssessment() throws Exception {
 
+        long assessorId = 1L;
+        long assessmentId = 2L;
+        long applicationId = 3L;
+        long processRoleId = 4L;
+        String suitableValue = "yes";
+        Assessment assessment = newAssessment().withId(assessmentId).build();
+        ProcessRole processRole = newProcessRole().withId(processRoleId).build();
+
+        ObjectMapper mapper = new ObjectMapper();
+        ObjectNode jsonNode = mapper.createObjectNode()
+                .put("assessorId", assessorId)
+                .put("applicationId", applicationId)
+                .put("suitableValue", suitableValue)
+                .put("comments", "comments")
+                .put("suitableFeedback", "feedback");
+        String json = mapper.writeValueAsString(jsonNode);
+
+        when(processRoleController.findByUserApplication(assessorId, applicationId)).thenReturn(processRole);
+        when(assessmentHandler.getOneByProcessRole(processRole.getId())).thenReturn(assessment);
+        when(assessmentHandler.getRecommendedValueFromString(suitableValue)).thenReturn(RecommendedValue.YES);
+
+        mockMvc.perform(post(applicationControllerPath + "/saveAssessmentSummary").contentType(MediaType.APPLICATION_JSON)
+                .content(json))
+                .andExpect(status().isOk())
+                .andExpect(content().string("true"))
+                .andDo(document("assessment/submit-assessment-assessment"));
     }
 }
