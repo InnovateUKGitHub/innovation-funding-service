3c74abc8641e07acfdb194c1acf587a3332de3b4
Brent de Kok <brent@worth.systems>
2015-12-29 18:10:29 +0100

    Fixed location of error-messages in the registration section and did client side email validation

--- ifs-web-service/src/main/resources/static/js/ifs_modules/formValidation.js
+++ ifs-web-service/src/main/resources/static/js/ifs_modules/formValidation.js
@@ -12,6 +12,10 @@ IFS.formValidation = (function(){
                 field2 : '[name="retypedPassword"]',
                 messageInvalid : 'Passwords must match'
             },
+            email : {
+                fields : '[type="email"]',
+                messageInvalid : "Please enter a valid emailaddress"
+            },
             typeTimeout : 800
         },
         init : function(){
@@ -19,41 +23,67 @@ IFS.formValidation = (function(){
 
             IFS.formValidation.initPasswordCheck(); //checks if password and retyped password are equal
             IFS.formValidation.initNumberCheck();   //checks if it is a number by using jQuery.isNumeric (https://api.jquery.com/jQuery.isNumeric/)
+            IFS.formValidation.initEmailCheck();   //checks if the email is valid, the almost rfc compliant check. The same as the java check, see http://www.regular-expressions.info/email.html
         },
         initPasswordCheck : function(){
-            var passwordFields = s.password.field1+','+s.password.field2;
-
-            jQuery('body').on('change', passwordFields, function(){ 
-                IFS.formValidation.checkPasswords(jQuery(this));
-            });
-
-            jQuery('body').on('keyup', passwordFields , function(){ 
-                clearTimeout(window.IFS.formValidation_timer);
-                window.IFS.formValidation_timer = setTimeout(function(){ 
-                    IFS.formValidation.checkPasswords(jQuery(this)); 
-                }, s.typeTimeout);
-            });
-        },
-        initNumberCheck : function(){
-            jQuery('body').on('change', s.number.fields , function(){ 
-                IFS.formValidation.checkNumber(jQuery(this));
+            jQuery('body').on('change keyup', s.password.field1+','+s.password.field2, function(e){ 
+                if(e.type == 'keyup'){
+                    clearTimeout(window.IFS.formValidation_timer);
+                    window.IFS.formValidation_timer = setTimeout(function(){ 
+                        IFS.formValidation.checkPasswords(); 
+                    }, s.typeTimeout);
+                }
+                else {
+                    IFS.formValidation.checkPasswords();
+                }
             });
         },
         checkPasswords : function(){
             var pw1 = jQuery(s.password.field1);
             var pw2 = jQuery(s.password.field2);
 
-            if(pw1.val().length && pw2.val().length){
+            //if both are on the page and have content (.val)
+            if(pw1.length && pw2.length && pw1.val().length && pw2.val().length){
                 if(pw1.val() == pw2.val()){
                     IFS.formValidation.setValid(pw1,s.password.messageInvalid);
                     IFS.formValidation.setValid(pw2,s.password.messageInvalid);
-
                 }
                 else {
                     IFS.formValidation.setInvalid(pw1,s.password.messageInvalid);
                     IFS.formValidation.setInvalid(pw2,s.password.messageInvalid);
                 }
             }
+        },
+        initEmailCheck : function(){
+            jQuery('body').on('change keyup', s.email.fields , function(e){
+                var el = jQuery(e.target);
+
+                if(e.type == 'keyup'){
+                    clearTimeout(window.IFS.formValidation_timer);
+                    window.IFS.formValidation_timer = setTimeout(function(){ 
+                        IFS.formValidation.checkEmail(el); 
+                    }, s.typeTimeout);
+                }
+                else {
+                    IFS.formValidation.checkEmail(el);
+                }
+            });
+        },
+        checkEmail : function(field){
+            var email = field.val();
+            var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/i;
+            var validEmail = re.test(email);
+            if(validEmail){
+                IFS.formValidation.setValid(field,s.email.messageInvalid);
+            }
+            else {
+                IFS.formValidation.setInvalid(field,s.email.messageInvalid);
+            }
+        },
+        initNumberCheck : function(){
+            jQuery('body').on('change', s.number.fields , function(){ 
+                IFS.formValidation.checkNumber(jQuery(this));
+            });
         },
         checkNumber : function(field){
             if(jQuery.isNumeric(field.val())){
@@ -73,7 +103,6 @@ IFS.formValidation = (function(){
                     var html = '<span class="error-message">'+message+'</span>';
                     formGroup.find('label').first().append(html);
                 }
-
             }
         },
         setValid : function(field,message){
