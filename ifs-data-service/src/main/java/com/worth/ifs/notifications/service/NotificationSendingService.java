8e3f296e3027c5b41dcb3bc25446429c1f36d7e8
Duncan Watson <duncan@hiveit.co.uk>
2016-01-06 15:55:20 +0000

    INFUND-973 - added NotSecured annottaions

--- ifs-data-service/src/main/java/com/worth/ifs/notifications/service/NotificationSendingService.java
+++ ifs-data-service/src/main/java/com/worth/ifs/notifications/service/NotificationSendingService.java
@@ -2,13 +2,16 @@ package com.worth.ifs.notifications.service;
 
 import com.worth.ifs.notifications.resource.NotificationMedium;
 import com.worth.ifs.notifications.resource.NotificationResource;
+import com.worth.ifs.security.NotSecured;
 
 /**
  * Represents a Service that knows how to send a NotificationResponse out as a message via a particular NotificationMedium
  */
 public interface NotificationSendingService {
 
+    @NotSecured("NotificationSendingService to be used within the context of some other secured service (via NotificationService)")
     NotificationMedium getNotificationMedium();
 
+    @NotSecured("NotificationSendingService to be used within the context of some other secured service (via NotificationService)")
     void sendNotification(NotificationResource notification);
 }
