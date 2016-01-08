26130db8c861b591b472586618bac0ddcddec807
Brent de Kok <brent@worth.systems>
2016-01-08 14:31:22 +0100

    INFUND-1386 Reversed changes as I was breaking other finance sections. Now placed the toggle outside of the repeatable section. Also changed names of the repeatable finance sections as it can be used for whatever content.

--- ifs-web-service/src/main/resources/static/js/ifs_modules/conditionalForms.js
+++ ifs-web-service/src/main/resources/static/js/ifs_modules/conditionalForms.js
@@ -1,4 +1,4 @@
-//Conditional questions based on answers see: govuk-elements.herokuapp.com/form-elements/#form-toggle-content 
+//Conditional questions based on answers see: govuk-elements.herokuapp.com/form-elements/#form-toggle-content
 //
 //This code is a replacement for the GDS application.js code that was overly complex for what it did and didn't work on pageload
 //All behaviours and html are the same as the GDS html so no need to refactor html
@@ -9,29 +9,29 @@ IFS.conditionalForms = (function(){
         init : function(){
             jQuery('label[data-target]').each(function(){
                 var label = jQuery(this);
-                var dataTarget = label.attr('data-target');
-                var dataTargetEl = jQuery('#'+dataTarget);
+                var dataTarget = '#'+label.attr('data-target');
                 var inputEl = label.find('input[type="radio"],input[type="checkbox"]');
-                
+
                 //for having inverted show/hide
                 var isInverted = false;
                 if(label.attr('data-target-inverted')){
                     isInverted = true;
                 }
-                if(inputEl && dataTarget && dataTargetEl){
+                if(inputEl && dataTarget && dataTarget){
                     var groupName = inputEl.attr('name');
                     inputEl.attr('aria-controls',dataTarget);
                     //execute on pageload
-                    IFS.conditionalForms.toggleVisibility(inputEl,dataTargetEl,isInverted); 
+                    IFS.conditionalForms.toggleVisibility(inputEl,dataTarget,isInverted);
 
                     //execute on click
                     jQuery('input[name="'+groupName+'"]').on('click',function(){
-                        IFS.conditionalForms.toggleVisibility(inputEl,dataTargetEl,isInverted); 
+                        IFS.conditionalForms.toggleVisibility(inputEl,dataTarget,isInverted);
                     });
                 }
             });
         },
         toggleVisibility : function(input,target,isInverted){
+            target = jQuery(target);
             var radioStatus = input.is(':checked');
             if(isInverted) {
                 radioStatus = !radioStatus;
@@ -42,12 +42,9 @@ IFS.conditionalForms = (function(){
               target.attr('aria-hidden','false').removeClass('js-hidden');
             }
             else {
-              input.attr('aria-expanded','false');  
+              input.attr('aria-expanded','false');
               target.attr('aria-hidden','true');
             }
         }
     };
 })();
-
-
-
