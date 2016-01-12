3aad31a35952c76ea639bf96691cf738c2b4dc22
richardpopple <richard@hiveit.co.uk>
2016-01-12 11:38:39 +0000

    Hopefully a fix for inconsitent checksums

--- robot-tests/insertTestDataAndUpdatePradha.bat
+++ robot-tests/insertTestDataAndUpdatePradha.bat
@@ -2,5 +2,6 @@
 "C:\Program Files\MySQL\MySQL Server 5.7\bin\mysql.exe" -uroot -p ifs < testDataDump.sql
 cd ..\ifs-data-service\
 gradlew.bat  flywayMigrate -Pflyway.validateOnMigrate=false
+gradlew.bat flywayRepair
 
 
