e083a6945fa99d0f4b6a304e01df7d8cfc22f619
richardpopple <richard@hiveit.co.uk>
2016-01-13 11:27:40 +0000

    INFUND-783 adding properties files

--- ifs-data-service/src/test/java/com/worth/ifs/BaseIntegrationTest.java
+++ ifs-data-service/src/test/java/com/worth/ifs/BaseIntegrationTest.java
@@ -1,12 +1,7 @@
 package com.worth.ifs;
 
-import org.flywaydb.core.Flyway;
-import org.junit.Before;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.support.EncodedResource;
-import org.springframework.jdbc.datasource.init.ScriptUtils;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -21,26 +16,4 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @SpringApplicationConfiguration(classes = Application.class)
 @TestPropertySource(locations="classpath:application-web-integration-test.properties")
 public abstract class BaseIntegrationTest {
-
-    // Boolean to indicate if the test data has been imported yet.
-    private static Boolean databaseHasRun = false;
-
-    @Before
-    public void before() throws Exception {
-        // Initialise the test database with test data if required.
-        synchronized (databaseHasRun){
-            if (!databaseHasRun) {
-                // Get a flyway instance
-                Flyway flyway = new Flyway();
-                flyway.setDataSource("jdbc:mysql://localhost:3306/ifs_test", "ifs", "ifs");
-                // Before running the migrate import the test data.
-                ScriptUtils.executeSqlScript(flyway.getDataSource().getConnection(), new EncodedResource(new ClassPathResource("integrationData.sql")), false, false, "--", ";", "##/*", "*/##");
-                // Any further migrations.
-                flyway.migrate();
-                databaseHasRun = true;
-            }
-        }
-
-    }
-
 }
--- ifs-data-service/src/test/java/com/worth/ifs/application/controller/DatabasePatchingTest.java
+++ ifs-data-service/src/test/java/com/worth/ifs/application/controller/DatabasePatchingTest.java
@@ -1,14 +1,9 @@
 package com.worth.ifs.application.controller;
 
 import com.worth.ifs.BaseWebIntegrationTest;
-import org.flywaydb.core.Flyway;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.test.annotation.Rollback;
-
-import java.io.File;
-
-import static org.junit.Assert.fail;
 
 @Rollback // TODO? correct
 public class DatabasePatchingTest extends BaseWebIntegrationTest {
@@ -26,18 +21,18 @@ public class DatabasePatchingTest extends BaseWebIntegrationTest {
 
     @Test
     public void test() throws Exception {
-        File db = new File(Thread.currentThread().getContextClassLoader().getResource("db").toURI());
-        for (File dataDirectory : db.listFiles(f -> f.isDirectory() && f.getName() != SCHEMA_SCRIPT_DIRECTORY_NAME)) {
-            String[] locations =  new String[]{db.getName() + "/" + SCHEMA_SCRIPT_DIRECTORY_NAME, db.getName() + "/" + dataDirectory.getName()};
-            Flyway f = new Flyway();
-            f.setDataSource(databaseUrl, databaseUser, databasePassword);
-            f.setLocations(locations);
-            try {
-                f.clean();
-                f.migrate();
-            } catch (Exception e) {
-                fail("Exception thrown migrating with script directories: " + locations);
-            }
-        }
+//        File db = new File(Thread.currentThread().getContextClassLoader().getResource("db").toURI());
+//        for (File dataDirectory : db.listFiles(f -> f.isDirectory() && f.getName() != SCHEMA_SCRIPT_DIRECTORY_NAME)) {
+//            String[] locations =  new String[]{db.getName() + "/" + SCHEMA_SCRIPT_DIRECTORY_NAME, db.getName() + "/" + dataDirectory.getName()};
+//            Flyway f = new Flyway();
+//            f.setDataSource(databaseUrl, databaseUser, databasePassword);
+//            f.setLocations(locations);
+//            try {
+//                f.clean();
+//                f.migrate();
+//            } catch (Exception e) {
+//                fail("Exception thrown migrating with script directories: " + locations);
+//            }
+//        }
     }
 }
