351a2b66adadd8b70a2784a3ad72c5e4989b4d39
Brent de Kok <brent@worth.systems>
2016-01-13 11:39:32 +0000

    INFUND-1311  Add JavaScript to add/remove persons client-side through AJAX

---
+++ ifs-web-service/src/main/resources/static/js/ifs_pages/invite_contributors.js
@@ -0,0 +1,69 @@
+/*jshint multistr: true */
+IFS.invites = (function(){
+    "use strict";
+    var s;
+    return {
+      settings : {
+          formid : '.contributorsForm',
+          inputs :  '.contributorsForm input',
+          addRow : '.contributorsForm .add-another-row',
+          removeRow : '.contributorsForm .remove-another-row'
+      },
+      init : function(){
+          s = this.settings;
+          jQuery('body').on('change',s.inputs, function(){
+              IFS.invites.saveToCookie();
+          });
+          jQuery('body').on('click',s.addRow, function(e){
+              IFS.invites.addRow(e);
+          });
+          jQuery('body').on('click',s.removeRow, function(e){
+              IFS.invites.removeRow(e);
+          });
+      },
+      saveToCookie : function(){
+          //cookie is updated simply by having an ajax call go to the server
+          var data = (jQuery(s.formid).serialize());
+          jQuery.ajax({
+            url: window.location,
+            method: "POST",
+            dataType : 'json', //for requests headers so that the back-end can give a version without 302 redirect
+            data: data
+          });
+      },
+      addRow : function(e){
+          e.preventDefault();
+          var orgContainer = jQuery(e.target).closest('li');
+          var orgId = orgContainer.attr('data-invite-org');
+          var rowId = orgContainer.find('[data-invite-row]').length;
+          var html = '<tr data-invite-row>\
+                        <td><input type="text" value="" placeholder="name" name="organisationMap['+orgId+']['+rowId+'].personName" /></td>\
+                        <td><input type="text" value="" placeholder="name@company.co.uk" name="organisationMap['+orgId+']['+rowId+'].email" /></td>\
+                        <td class="alignright"><button value="'+orgId+'_'+rowId+'" name="remove_person" type="submit" class="remove-another-row buttonlink">Remove</button></td>\
+                      </tr>';
+          orgContainer.find('tbody').append(html);
+          IFS.invites.saveToCookie();
+      },
+      recountRows : function(){
+          jQuery('[data-invite-row]').each(function(index,value){
+              var orgId = jQuery(value).closest('li').attr('data-invite-org');
+
+              jQuery(value).find('input').each(function(){
+                  var input = jQuery(this);
+                  var oldName = input.attr('name');
+                  var newName = oldName.split('.');
+                  newName[0] = 'organisationMap['+orgId+']['+index+']';
+                  newName = newName.join('.');
+                  input.attr('name',newName);
+              });
+          });
+      },
+      removeRow : function(e){
+          e.preventDefault();
+          var button = jQuery(e.target);
+          button.closest('tr').remove();
+          IFS.invites.recountRows();
+          IFS.invites.saveToCookie();
+      }
+    };
+})();
