a26986be085d6b39ed6a92d27ec9a919a68323fc
richardpopple <richard@hiveit.co.uk>
2016-01-14 12:15:37 +0000

    INFUND-783 pressing the migrate button

--- ifs-data-service/src/main/resources/db/migration/V8_3__Update_Other_Funding.sql
+++
@@ -1,3 +0,0 @@
-UPDATE `question` SET `guidance_answer`='Please tell us if you have received, or will receive any other public sector funding for this project.' WHERE `id`='35';
-INSERT INTO `cost` (`cost`, `description`, `item`, `application_finance_id`, `question_id`) VALUES ('0', 'Other Funding', 'Yes', '1', '35');
-UPDATE `question` SET `mark_as_completed_enabled`=1 WHERE `id`='35';
