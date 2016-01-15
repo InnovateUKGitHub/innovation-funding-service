443e31a7bd288290d807edef63d045ebcc8bea26
Nico Bijl <nbijl@worth.systems>
2016-01-15 09:39:04 +0100

    INFUND-901 INFUND-1018 invite collaborators, changed data-services, save is now working.

--- ifs-data-service/src/main/java/com/worth/ifs/invite/service/InviteRestService.java
+++ ifs-data-service/src/main/java/com/worth/ifs/invite/service/InviteRestService.java
@@ -8,5 +8,5 @@ import java.util.List;
 
 public interface InviteRestService {
     public ResourceEnvelope<InviteOrganisationResource> createInvitesByInviteOrganisation(String organisationName, List<InviteResource> invites);
-
+    public ResourceEnvelope<InviteOrganisationResource> createInvitesByOrganisation(Long organisationId, List<InviteResource> invites);
 }
