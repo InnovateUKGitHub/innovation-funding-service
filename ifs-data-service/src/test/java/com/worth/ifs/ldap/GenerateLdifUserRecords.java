a6f8f051d62974cdbea521e0e34440ce275338c1
Duncan Watson <duncan@hiveit.co.uk>
2016-01-18 10:45:02 +0000

    INFUND-1265 - added base user sets for the 3 environments (to mirror flyway folder structure)

--- ifs-data-service/src/test/java/com/worth/ifs/ldap/GenerateLdifUserRecords.java
+++ ifs-data-service/src/test/java/com/worth/ifs/ldap/GenerateLdifUserRecords.java
@@ -8,48 +8,81 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import static com.worth.ifs.util.CollectionFunctions.simpleJoiner;
+import static com.worth.ifs.util.CollectionFunctions.simpleMap;
+import static java.util.Arrays.asList;
 import static java.util.stream.Collectors.joining;
 
 /**
- *
+ * Temporary
  */
 public class GenerateLdifUserRecords {
 
+    private static class LdapUserRecord {
 
-    public static String generateLdapUserRecord(String firstName, String lastName, String emailAddress, String title) throws URISyntaxException, IOException {
+        String firstName;
+        String lastName;
+        String emailAddress;
+        String title;
 
-        String fullName = firstName + " " + lastName;
-
-        Map<String, String> replacements = new HashMap<>();
-        replacements.put("fullName", fullName);
-        replacements.put("firstName", firstName);
-        replacements.put("lastName", lastName);
-        replacements.put("uid", firstName.toLowerCase() + "." + lastName.toLowerCase());
-        replacements.put("emailAddress", emailAddress);
-        replacements.put("title", title);
-
-        File template = new File(Thread.currentThread().getContextClassLoader().getResource("user_ldif_record_template.txt").toURI());
-        List<String> templateLines = Files.readAllLines(template.toPath());
-        String templateContents = templateLines.stream().collect(joining("\n"));
-
-        for (Map.Entry<String, String> replacement : replacements.entrySet()) {
-            templateContents = templateContents.replace("$" + replacement.getKey(), replacement.getValue());
+        public LdapUserRecord(String firstName, String lastName, String emailAddress, String title) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.emailAddress = emailAddress;
+            this.title = title;
         }
-
-        return templateContents;
     }
 
+    public static LdapUserRecord createLdapUserRecord(String firstName, String lastName, String emailAddress, String title) {
+        return new LdapUserRecord(firstName, lastName, emailAddress, title);
+    }
+
+    public static String generateLdapUserRecord(LdapUserRecord... userRecords) {
+
+        List<String> results = simpleMap(asList(userRecords), userRecord -> {
+
+            try {
+                String fullName = userRecord.firstName + " " + userRecord.lastName;
+
+                Map<String, String> replacements = new HashMap<>();
+                replacements.put("fullName", fullName);
+                replacements.put("firstName", userRecord.firstName);
+                replacements.put("lastName", userRecord.lastName);
+                replacements.put("uid", userRecord.firstName.toLowerCase() + "." + userRecord.lastName.toLowerCase());
+                replacements.put("emailAddress", userRecord.emailAddress);
+                replacements.put("title", userRecord.title);
+
+                File template = new File(Thread.currentThread().getContextClassLoader().getResource("user_ldif_record_template.txt").toURI());
+                List<String> templateLines = Files.readAllLines(template.toPath());
+                String templateContents = templateLines.stream().collect(joining("\n"));
+
+                for (Map.Entry<String, String> replacement : replacements.entrySet()) {
+                    templateContents = templateContents.replace("$" + replacement.getKey(), replacement.getValue());
+                }
+
+                return templateContents;
+
+            } catch (Exception e) {
+                throw new IllegalArgumentException(e);
+            }
+        });
+
+        return simpleJoiner(results, "\n\n\n");
+
+    }
+
+    // DEVELOPMENT, INTEGRATION ENVIRONMENT AND WEBTEST DATA
     public static void main(String[] args) throws IOException, URISyntaxException {
 
-        String recordSeparator = "\n\n# ==================================\n\n";
+        String contents = generateLdapUserRecord(
+                createLdapUserRecord("Steve", "Smith", "steve.smith@empire.com", "Mr"),
+                createLdapUserRecord("Jessica", "Doe", "jessica.doe@ludlow.co.uk", "Mrs"),
+                createLdapUserRecord("Professor", "Plum", "paul.plum@gmail.com", "Prof"),
+                createLdapUserRecord("Comp", "Exec (Competitions)", "competitions@innovateuk.gov.uk", "Mr"),
+                createLdapUserRecord("Project", "Finance Analyst (Finance)", "finance@innovateuk.gov.uk", "Mr"),
+                createLdapUserRecord("Pete", "Tom", "pete.tom@egg.com", "Mr"),
+                createLdapUserRecord("Felix", "Wilson", "felix.wilson@gmail.com", "Mr"));
 
-        System.out.println(generateLdapUserRecord("Steve", "Smith", "steve.smith@empire.com", "Mr") + recordSeparator);
-        System.out.println(generateLdapUserRecord("Jessica", "Doe", "jessica.doe@ludlow.co.uk", "Mrs") + recordSeparator);
-        System.out.println(generateLdapUserRecord("Professor", "Plum", "paul.plum@gmail.com", "Prof") + recordSeparator);
-        System.out.println(generateLdapUserRecord("Comp", "Exec (Competitions)", "competitions@innovateuk.gov.uk", "Mr") + recordSeparator);
-        System.out.println(generateLdapUserRecord("Project", "Finance Analyst (Finance)", "finance@innovateuk.gov.uk", "Mr") + recordSeparator);
-        System.out.println(generateLdapUserRecord("Pete", "Tom", "pete.tom@egg.com", "Mr") + recordSeparator);
-        System.out.println(generateLdapUserRecord("Felix", "Wilson", "felix.wilson@gmail.com", "Mr") + recordSeparator);
-
+        System.out.println(contents);
     }
 }
