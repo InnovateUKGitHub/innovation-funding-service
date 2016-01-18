4226767beb1021084b1ad4c2a681f791b990babe
Nico Bijl <nbijl@worth.systems>
2016-01-18 14:40:37 +0100

    INFUND-896 fixed build?

--- ifs-data-service/src/main/java/com/worth/ifs/invite/repository/InviteRepository.java
+++ ifs-data-service/src/main/java/com/worth/ifs/invite/repository/InviteRepository.java
@@ -2,6 +2,11 @@ package com.worth.ifs.invite.repository;
 
 import com.worth.ifs.invite.domain.Invite;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface InviteRepository extends PagingAndSortingRepository<Invite, Long> {
+
+    List<Invite> findByApplicationId(@Param("applicationId") Long applicationId);
 }
