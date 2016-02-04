e2852605f519da094ad792f7cc703aaf7a5f0c0e
Nico Bijl <nbijl@worth.systems>
2016-02-04 14:10:17 +0100

    INFUND-1332 added check if invited user already has a account, so account creation is not needed.

--- ifs-web-service/src/main/java/com/worth/ifs/application/AcceptInviteController.java
+++ ifs-web-service/src/main/java/com/worth/ifs/application/AcceptInviteController.java
@@ -1,11 +1,11 @@
 package com.worth.ifs.application;
 
-import com.worth.ifs.application.resource.ApplicationResource;
 import com.worth.ifs.competition.domain.Competition;
 import com.worth.ifs.invite.constant.InviteStatusConstants;
 import com.worth.ifs.invite.resource.InviteResource;
 import com.worth.ifs.invite.service.InviteRestService;
 import com.worth.ifs.login.LoginForm;
+import com.worth.ifs.user.resource.UserResource;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.List;
 import java.util.Optional;
 
 @Controller
@@ -27,29 +28,43 @@ public class AcceptInviteController extends AbstractApplicationController {
 
 
 
-    @RequestMapping(value = "/accept-invite/{applicationId}/{hash}", method = RequestMethod.GET)
+    @RequestMapping(value = "/accept-invite/{competitionId}/{hash}", method = RequestMethod.GET)
     public String displayContributors(
-            @PathVariable("applicationId") final Long applicationId,
+            @PathVariable("competitionId") final Long competitionId,
             @PathVariable("hash") final String hash,
             HttpServletRequest request,
             HttpServletResponse response,
             Model model) {
-        ApplicationResource application = applicationService.getById(applicationId);
-        Competition competition = competitionService.getById(application.getCompetition());
+//        ApplicationResource application = applicationService.getById(applicationId);
+
         Optional<InviteResource> invite = inviteRestService.getInviteByHash(hash);
 
 
-        model.addAttribute("loginForm", new LoginForm());
-        model.addAttribute("currentApplication", application);
-        model.addAttribute("currentCompetition", competition);
+
+//        model.addAttribute("currentApplication", application);
 
         if(invite.isPresent()){
             InviteResource inviteResource = invite.get();
             if(InviteStatusConstants.SEND.equals(inviteResource.getStatus())){
-                log.debug("Found the invite " + inviteResource.getEmail());
+                Competition competition = competitionService.getById(competitionId);
+                LoginForm loginForm = new LoginForm();
+
+
+                log.error("Found the invite " + inviteResource.getEmail());
                 log.debug("Found the invite " + inviteResource.getName());
                 log.debug("Found the invite " + inviteResource.getStatus().name());
 
+                // check if there already is a user with this emailaddress
+                List<UserResource> existingUsers = userService.findUserByEmail(inviteResource.getEmail());
+                if(existingUsers.isEmpty()){
+//                    model.addAttribute("emailAddressRegistered", "false");
+                }else{
+                    loginForm.setEmail(inviteResource.getEmail());
+                    model.addAttribute("emailAddressRegistered", "true");
+                }
+
+                model.addAttribute("loginForm", loginForm);
+                model.addAttribute("competitionName", competition.getName());
                 return "accept-invite";
             }else{
                 log.error("INVITE NOT FOUND");
