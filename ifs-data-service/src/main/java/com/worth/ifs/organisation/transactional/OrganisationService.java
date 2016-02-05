3d469f2c378d466be42b7795ef13f8b219c24c8b
Duncan Watson <duncan@hiveit.co.uk>
2016-02-05 16:15:12 +0000

    INFUND-1555 - tests fixes with OrganisationController rework

--- ifs-data-service/src/main/java/com/worth/ifs/organisation/transactional/OrganisationService.java
+++ ifs-data-service/src/main/java/com/worth/ifs/organisation/transactional/OrganisationService.java
@@ -13,8 +13,11 @@ import java.util.Set;
  * Represents operations surrounding the use of Organisations in the system
  */
 public interface OrganisationService {
+
+    @NotSecured("TODO DW - implement security when permissions matrix known")
     ServiceResult<Set<Organisation>> findByApplicationId(Long applicationId);
 
+    @NotSecured("TODO DW - implement security when permissions matrix known")
     ServiceResult<Organisation> findById(Long organisationId);
 
     @NotSecured("When creating a application, this methods is called before creating a user account, so there his no way to authenticate.")
