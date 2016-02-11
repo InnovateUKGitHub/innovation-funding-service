ab0847c4d32db223177559318a5468f46fc36311
richardpopple <richard@hiveit.co.uk>
2016-02-11 14:19:30 +0000

    INFUND-1680 invalidating caches

--- ifs-web-service/src/main/java/com/worth/ifs/cache/RestCacheMethodInterceptor.java
+++ ifs-web-service/src/main/java/com/worth/ifs/cache/RestCacheMethodInterceptor.java
@@ -6,18 +6,16 @@ import org.aopalliance.intercept.MethodInterceptor;
 import org.aopalliance.intercept.MethodInvocation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
 import static java.util.Arrays.asList;
-import static org.springframework.web.context.request.RequestAttributes.SCOPE_REQUEST;
-import static org.springframework.web.context.request.RequestContextHolder.getRequestAttributes;
 
 @Component
 public class RestCacheMethodInterceptor implements MethodInterceptor {
@@ -25,7 +23,16 @@ public class RestCacheMethodInterceptor implements MethodInterceptor {
     private static final Logger LOG = LoggerFactory.getLogger(RestCacheMethodInterceptor.class);
     private static final Cache<String, Map<Method, Map<List<Object>, Object>>> CACHE
             = CacheBuilder.newBuilder().expireAfterWrite(10, TimeUnit.SECONDS).build();
-    private static final String REQUEST_UID_KEY = "REQUEST_UID_KEY";
+
+
+    @Autowired
+    private UidSupplier uidSupplier;
+
+    public void invalidate(){
+        final String uid = uidSupplier.get();
+        CACHE.invalidate(uidSupplier.get());
+        LOG.debug("Invalidating: " + uid);
+    }
 
     @Override
     public Object invoke(MethodInvocation invocation) throws Throwable {
@@ -34,22 +41,20 @@ public class RestCacheMethodInterceptor implements MethodInterceptor {
         final Method method = invocation.getMethod();
 
         // Generate the Uid for this request if there isn't one
-        if (getRequestAttributes().getAttribute(REQUEST_UID_KEY, SCOPE_REQUEST) == null) {
-            getRequestAttributes().setAttribute(REQUEST_UID_KEY, UUID.randomUUID().toString(), SCOPE_REQUEST);
-        }
-        final String requestUid = (String) getRequestAttributes().getAttribute(REQUEST_UID_KEY, SCOPE_REQUEST);
-
-        final Map<Method, Map<List<Object>, Object>> methodMap = CACHE.get(requestUid, HashMap::new);
+        String uid = uidSupplier.get();
+        final Map<Method, Map<List<Object>, Object>> methodMap = CACHE.get(uid, HashMap::new);
         methodMap.putIfAbsent(method, new HashMap<>());
         final Map<List<Object>, Object> argsMap = methodMap.get(method);
         if (argsMap.containsKey(parameters)) { // null is an acceptable return argument
-            final Object cachedResult = argsMap.get(invocation.getArguments());
+            final Object cachedResult = argsMap.get(parameters);
             LOG.debug("Returning cached result: " + cachedResult);
-            System.out.println("qqRP returning cached");
+            return cachedResult;
         }
         final Object result = invocation.proceed();
         argsMap.put(parameters, result);
-        CACHE.put(requestUid, methodMap);
+        CACHE.put(uid, methodMap);
         return result;
     }
+
+
 }
