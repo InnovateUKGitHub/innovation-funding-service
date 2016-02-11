ad31f1bb51222523fb512d38f309111657b21fe9
Duncan Watson <duncan@hiveit.co.uk>
2016-02-11 11:10:40 +0000

    INFUND-1835 - refactored CostValueController

--- ifs-data-service/src/main/java/com/worth/ifs/finance/service/CostValueRestServiceImpl.java
+++ ifs-data-service/src/main/java/com/worth/ifs/finance/service/CostValueRestServiceImpl.java
@@ -1,5 +1,6 @@
 package com.worth.ifs.finance.service;
 
+import com.worth.ifs.commons.rest.RestResult;
 import com.worth.ifs.commons.service.BaseRestService;
 import com.worth.ifs.finance.resource.CostValueResource;
 import org.springframework.beans.factory.annotation.Value;
@@ -7,12 +8,12 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class CostValueRestServiceImpl extends BaseRestService implements CostValueRestService {
+
     @Value("${ifs.data.service.rest.costvalue}")
     private String restUrl;
 
-
     @Override
-    public CostValueResource findOne(Long id) {
-        return restGet(restUrl + "/" + id, CostValueResource.class);
+    public RestResult<CostValueResource> findOne(Long id) {
+        return getWithRestResult(restUrl + "/" + id, CostValueResource.class);
     }
 }
--- ifs-data-service/src/main/java/com/worth/ifs/finance/transactional/CostValueService.java
+++ ifs-data-service/src/main/java/com/worth/ifs/finance/transactional/CostValueService.java
@@ -1,10 +1,12 @@
 package com.worth.ifs.finance.transactional;
 
-import com.worth.ifs.finance.domain.CostValue;
+import com.worth.ifs.commons.service.ServiceResult;
 import com.worth.ifs.finance.domain.CostValueId;
+import com.worth.ifs.finance.resource.CostValueResource;
 import com.worth.ifs.security.NotSecured;
 
 public interface CostValueService {
+
     @NotSecured("TODO")
-    CostValue findOne(CostValueId id);
+    ServiceResult<CostValueResource> findOne(CostValueId id);
 }
