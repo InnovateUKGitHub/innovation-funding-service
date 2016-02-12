8400df47b5b0137effbafb51d9286e5547a515c4
Duncan Watson <duncan@hiveit.co.uk>
2016-02-12 13:39:41 +0000

    Fixing broken tests

--- ifs-data-service/src/test/java/com/worth/ifs/transactional/ServiceFailureTransactionRollbackAdvisorTestService.java
+++ ifs-data-service/src/test/java/com/worth/ifs/transactional/ServiceFailureTransactionRollbackAdvisorTestService.java
@@ -1,15 +1,20 @@
 package com.worth.ifs.transactional;
 
 import com.worth.ifs.commons.service.ServiceResult;
+import com.worth.ifs.security.NotSecured;
 
 /**
  *
  */
 public interface ServiceFailureTransactionRollbackAdvisorTestService {
 
+
+    @NotSecured("just a test method")
     ServiceResult<String> successfulMethod();
 
+    @NotSecured("just a test method")
     ServiceResult<String> failingMethod();
 
+    @NotSecured("just a test method")
     ServiceResult<String> exceptionThrowingMethod();
 }
