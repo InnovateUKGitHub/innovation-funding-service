fbaa8718cb400da83e333e36f490e0d06c560a3d
richardpopple <richard@hiveit.co.uk>
2016-02-23 15:16:51 +0000

    INFUND-936 more scripts to allow work on the shibboleth server

--- setup-files/scripts/shibboleth/build_and_deploy.sh
+++ setup-files/scripts/shibboleth/build_and_deploy.sh
@@ -7,6 +7,8 @@ if [ -z "$docker_id" ]; then
   echo "No Shibboleth Docker image currently running."
   exit 1
 fi
+docker exec $docker_id rm /tmp/_build_and_deploy.sh
+docker cp /home/richardpopple/Projects/ifs/Code/innovation-funding-service/setup-files/scripts/shibboleth/_build_and_deploy.sh $docker_id:/tmp/_build_and_deploy.sh
+docker exec $docker_id /tmp/_build_and_deploy.sh
 
-docker exec $docker_id rm -rf  /tmp/build_shibboleth.sh
 
