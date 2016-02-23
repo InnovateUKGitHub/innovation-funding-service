31438beb16b2110affab78e518e5bb23f1a23e72
Duncan Watson <duncan@hiveit.co.uk>
2016-02-23 22:38:04 +0000

    INFUND-1536 - reinstating login javascript from IFS in new Shib login page

--- robot-tests/run_tests_locally.sh
+++ robot-tests/run_tests_locally.sh
@@ -103,13 +103,13 @@ function startServers {
 function runTests {
     echo "**********RUN THE WEB TESTS**********"
     cd ${scriptDir}
-    pybot --outputdir target --pythonpath IFS_acceptance_tests/libs -v SERVER_BASE:$webBase -v PROTOCOL:'https://' -v VIRTUAL_DISPLAY:$useXvfb --exclude Failing --exclude Pending --name IFS $testDirectory
+    pabot --outputdir target --pythonpath IFS_acceptance_tests/libs -v SERVER_BASE:$webBase -v PROTOCOL:'https://' -v VIRTUAL_DISPLAY:$useXvfb --exclude Failing --exclude Pending --name IFS $testDirectory
 }
 
 function runHappyPathTests {
     echo "*********RUN THE HAPPY PATH TESTS ONLY*********"
     cd ${scriptDir}
-    pybot --outputdir target --pythonpath IFS_acceptance_tests/libs -v SERVER_BASE:$webBase -v PROTOCOL:'https://' -v VIRTUAL_DISPLAY:$useXvfb --include HappyPath --name IFS $testDirectory
+    pabot --outputdir target --pythonpath IFS_acceptance_tests/libs -v SERVER_BASE:$webBase -v PROTOCOL:'https://' -v VIRTUAL_DISPLAY:$useXvfb --include HappyPath --name IFS $testDirectory
 }
 
 cd "$(dirname "$0")"
