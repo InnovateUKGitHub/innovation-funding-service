f5a957df59c77d4a0f21456bd2a2fbdab0b8bf76
David Soff <dsoff@worth.systems>
2016-02-26 15:25:23 +0100

    INFUND-1913 removed Spring Data Rest/HAL browser as these exposed the repositories without any form of authorization. Also removed Lombok and spring hateoas as these are no longer in use

--- ifs-data-service/src/main/java/com/worth/ifs/application/controller/ApplicationStatusController.java
+++ ifs-data-service/src/main/java/com/worth/ifs/application/controller/ApplicationStatusController.java
@@ -3,14 +3,13 @@ package com.worth.ifs.application.controller;
 import com.worth.ifs.application.resource.ApplicationStatusResource;
 import com.worth.ifs.application.transactional.ApplicationStatusService;
 import com.worth.ifs.commons.rest.RestResult;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.hateoas.ExposesResourceFor;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@ExposesResourceFor(ApplicationStatusResource.class)
 @RequestMapping("/applicationstatus")
 public class ApplicationStatusController {
 
