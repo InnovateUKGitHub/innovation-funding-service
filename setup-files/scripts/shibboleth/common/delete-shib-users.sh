317feb17b9edf4b4125d83904c7b2a0b54f16387
richardpopple <richard@hiveit.co.uk>
2016-03-08 14:43:25 +0000

    INFUND-936 updates to scripts from new shib image

--- setup-files/scripts/shibboleth/common/delete-shib-users.sh
+++ setup-files/scripts/shibboleth/common/delete-shib-users.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 docker cp _delete-shib-users-remote.sh ifs-local-dev:/tmp/_delete-shib-users-remote.sh
-docker exec ifs-local-dev /tmp/delete-shib-users-remote.sh
+docker exec ifs-local-dev /tmp/_delete-shib-users-remote.sh
 
