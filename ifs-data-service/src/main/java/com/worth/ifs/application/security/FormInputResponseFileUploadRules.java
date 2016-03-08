3a2bba137c86d8b90b4cde8c1ceccdbe7d6dd211
Ravish Bhagdev <rav@hiveit.co.uk>
2016-03-08 14:49:31 +0000

    Fixing rules to allow normal applicants/collaborators to upload/remove files

--- ifs-data-service/src/main/java/com/worth/ifs/application/security/FormInputResponseFileUploadRules.java
+++ ifs-data-service/src/main/java/com/worth/ifs/application/security/FormInputResponseFileUploadRules.java
@@ -13,8 +13,10 @@ import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.Arrays;
 import java.util.List;
 
+import static com.worth.ifs.user.domain.UserRoleType.APPLICANT;
 import static com.worth.ifs.user.domain.UserRoleType.LEADAPPLICANT;
 
 /**
@@ -40,7 +42,7 @@ public class FormInputResponseFileUploadRules {
 
     @PermissionRule(value = "READ", description = "An Applicant can download a file for an answer to one of their own Applications")
     public boolean applicantCanDownloadFilesInResponsesForOwnApplication(FormInputResponseFileEntryResource fileEntry, User user) {
-        return userIsApplicantOnThisApplication(fileEntry.getCompoundId().getApplicationId(), user);
+        return userIsApplicantOnThisApplication(fileEntry, user);
     }
 
     private boolean userIsApplicantOnThisApplication(FormInputResponseFileEntryResource fileEntry, User user) {
@@ -48,13 +50,8 @@ public class FormInputResponseFileUploadRules {
     }
 
     private boolean userIsApplicantOnThisApplication(long applicationId, User user) {
-        List<Role> leadApplicantRoles = roleRepository.findByName(LEADAPPLICANT.getName());
-        if (leadApplicantRoles.isEmpty()) {
-            LOG.error("Could not find a Lead Applicant role");
-            return false;
-        }
-        Role leadApplicantRole = leadApplicantRoles.get(0);
-        List<ProcessRole> applicantProcessRoles = processRoleRepository.findByUserIdAndRoleAndApplicationId(user.getId(), leadApplicantRole, applicationId);
+        List<Role> allApplicantRoles = roleRepository.findByNameIn(Arrays.asList(APPLICANT.getName(), LEADAPPLICANT.getName()));
+        List<ProcessRole> applicantProcessRoles = processRoleRepository.findByUserIdAndRoleInAndApplicationId(user.getId(), allApplicantRoles, applicationId);
         return !applicantProcessRoles.isEmpty();
     }
 }
