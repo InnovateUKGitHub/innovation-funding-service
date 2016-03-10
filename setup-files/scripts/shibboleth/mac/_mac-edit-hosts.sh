67a97671763b97a024cc404362ac2156ea1c8b90
richardpopple <richard@hiveit.co.uk>
2016-03-10 11:30:03 +0000

    INFUND-936 adding a new entry to the hosts file for macs

--- setup-files/scripts/shibboleth/mac/_mac-edit-hosts.sh
+++ setup-files/scripts/shibboleth/mac/_mac-edit-hosts.sh
@@ -2,6 +2,7 @@
 source _mac-set-docker-vars.sh
 cp /etc/hosts /tmp/hostsbackup
 ip_address=$(docker-machine ip)
-cat /etc/hosts | grep -v 'ifs-local-dev' > /tmp/temphosts
+cat /etc/hosts | grep -v 'ifs-local-dev' | grep -v 'iuk-auth-localdev' > /tmp/temphosts
 echo "$ip_address  ifs-local-dev" >> /tmp/temphosts
+echo "$ip_address  iuk-auth-localdev" >> /tmp/temphosts
 sudo cp /tmp/temphosts /etc/hosts
