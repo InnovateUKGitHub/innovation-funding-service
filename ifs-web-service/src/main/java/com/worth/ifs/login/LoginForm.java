9a30025a3a2717498f00e3f6ec599399ef933feb
jonathan.lee <jonleeipl@gmail.com>
2016-03-14 09:07:22 +0000

    INFUND-1527: Adding regex validation to other email inputs, with constant to reduce duplication

--- ifs-web-service/src/main/java/com/worth/ifs/login/LoginForm.java
+++ ifs-web-service/src/main/java/com/worth/ifs/login/LoginForm.java
@@ -3,6 +3,8 @@ package com.worth.ifs.login;
 import org.hibernate.validator.constraints.Email;
 import org.hibernate.validator.constraints.NotEmpty;
 
+import com.worth.ifs.util.FormUtil;
+
 /**
  * This object is used for the loginForm. When the form is submitted the data is
  * injected into a LoginForm instance, so it is easy to use and you don't need to
@@ -12,7 +14,7 @@ import org.hibernate.validator.constraints.NotEmpty;
 public class LoginForm {
 
     @NotEmpty(message = "Please enter your e-mail address")
-    @Email(message = "Please enter a valid e-mail address")
+    @Email(regexp = FormUtil.EMAIL_DISALLOW_INVALID_CHARACTERS_REGEX, message = "Please enter a valid e-mail address")
     private String email;
     @NotEmpty(message = "Please enter your password")
     private String password;
---
+++ ifs-web-service/src/main/java/com/worth/ifs/util/FormUtil.java
@@ -0,0 +1,8 @@
+package com.worth.ifs.util;
+
+public class FormUtil {
+
+	public static final String EMAIL_DISALLOW_INVALID_CHARACTERS_REGEX = "^[^{}|]*$";
+	
+	private FormUtil() {}
+}
