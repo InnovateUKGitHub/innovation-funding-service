7cb1716715d47b1451175133d297e3de53caa813
Duncan Watson <duncan@hiveit.co.uk>
2016-03-14 12:00:30 +0000

    INFUND-936 - adding OS detection in scripts

--- robot-tests/run_tests_locally.sh
+++ robot-tests/run_tests_locally.sh
@@ -131,7 +131,17 @@ uploadFileDir=${scriptDir}"/upload_files"
 cd ../setup-files/scripts/shibboleth/common
 shibbolethCommonScriptsPath=$(pwd)
 echo "shibbolethCommonScriptsPath:        ${shibbolethCommonScriptsPath}"
-cd ../linux
+
+if [[ $OSTYPE == linux* ]]; then
+  os=linux
+elif [[ $OSTYPE == darwin* ]]; then
+  os=mac
+else
+  echo "Unable to determine a supported operating system for this script.  Currently only supported on Linux and Macs"
+  exit 1
+fi
+
+cd ../${os}
 shibbolethOsScriptsPath=$(pwd)
 echo "shibbolethOsScriptsPath:        ${shibbolethOsScriptsPath}"
 cd ../../../../ifs-data-service
