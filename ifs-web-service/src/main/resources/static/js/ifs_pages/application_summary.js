6ad876c15de2e39c0c56c4f5efbcd0031d781134
jonathan.lee <jonleeipl@gmail.com>
2016-03-15 08:51:06 +0000

    INFUND-1786: using additional submit button to get around the problem of modal behaviour not deactivated for inactive button

--- ifs-web-service/src/main/resources/static/js/ifs_pages/application_summary.js
+++ ifs-web-service/src/main/resources/static/js/ifs_pages/application_summary.js
@@ -7,16 +7,20 @@ IFS.application_summary = (function(){
         init: function(){
         	jQuery(document).find("#agree-to-terms-container").show();
         	jQuery(document).find('#submit-application-button').hide();
+        	jQuery(document).find('#disabled-submit-application-button').show();
         	jQuery(document).find('#agree-terms').attr('checked', false);
         	jQuery(document).on('change', '#agree-terms', IFS.application_summary.handleChangeAgreeTermsCheckbox);
         },
         handleChangeAgreeTermsCheckbox: function() {
         	var checkbox = jQuery(this);
         	var submitButton = jQuery(document).find('#submit-application-button');
+        	var dummyButton = jQuery(document).find('#disabled-submit-application-button');
         	if(checkbox.is(":checked")) {
         		submitButton.show();
+        		dummyButton.hide();
         	} else {
         		submitButton.hide();
+        		dummyButton.show();
         	}
         }
     };
