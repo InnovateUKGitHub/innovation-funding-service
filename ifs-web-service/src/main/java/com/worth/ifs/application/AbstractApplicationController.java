d398c325ae164bb532e0300683171c2a0c54a935
Nico Bijl <nbijl@worth.systems>
2016-03-22 11:49:24 +0100

    Adding constant instead of redefining Strings over and over again..

--- ifs-web-service/src/main/java/com/worth/ifs/application/AbstractApplicationController.java
+++ ifs-web-service/src/main/java/com/worth/ifs/application/AbstractApplicationController.java
@@ -47,9 +47,26 @@ import static com.worth.ifs.util.CollectionFunctions.simpleMap;
  * This object contains shared methods for all the Controllers related to the {@link ApplicationResource} data.
  */
 public abstract class AbstractApplicationController extends BaseController {
+    public static final String MARK_AS_COMPLETE = "mark_as_complete";
+    public static final String MARK_SECTION_AS_COMPLETE = "mark_section_as_complete";
+    public static final String MARK_SECTION_AS_INCOMPLETE = "mark_section_as_incomplete";
+    public static final String MARK_AS_INCOMPLETE = "mark_as_incomplete";
+    public static final String UPLOAD_FILE = "upload_file";
+    public static final String REMOVE_UPLOADED_FILE = "remove_uploaded_file";
+    public static final String ADD_COST = "add_cost";
+    public static final String REMOVE_COST = "remove_cost";
+    public static final String EDIT_QUESTION = "edit_question";
+    public static final String APPLICATION_FORM = "application-form";
+    public static final String MODEL_ATTRIBUTE_FORM = "form";
+    public static final String QUESTION_ID = "questionId";
+    public static final String APPLICATION_ID = "applicationId";
+    public static final String APPLICATION_BASE_URL = "/application/";
 
     public static final String ASSIGN_QUESTION_PARAM = "assign_question";
     public static final String FORM_MODEL_ATTRIBUTE = "form";
+    public static final String APPLICATION_START_DATE = "application.startDate";
+    public static final String QUESTION_URL = "/question/";
+    public static final String SECTION_URL = "/section/";
     private final Log log = LogFactory.getLog(getClass());
 
     @Autowired
