740d1bc9d024bb78e53afde9f865c3613b0ae312
Brent de Kok <brent@worth.systems>
2016-03-22 14:40:04 +0100

    INFUND-1786 Changes front-end code checkbox submit:
    * Removed second button which was doing almost the same thing
    * Removed all inline styles
    * Not always uncheck the checkbox on page refresh (leave previous value in browser)
    * Removed double ID which gave problems when selecting the label.
    * Added aria-disabled for better accessibility

--- ifs-web-service/src/main/resources/static/js/ifs_pages/application_summary.js
+++ ifs-web-service/src/main/resources/static/js/ifs_pages/application_summary.js
@@ -2,28 +2,36 @@
 // dependency on ifs_collpasible
 IFS.application_summary = (function(){
     "use strict";
-    
+    var s;
     return {
+        settings : {
+            submitButton : '#submit-application-button',
+            checkbox : '#agree-terms-page'
+        },
         init: function(){
-        	jQuery(document).find("#agree-to-terms-container").show();
-        	jQuery(document).find('#submit-application-button').hide();
-        	jQuery(document).find('#disabled-submit-application-button').show();
-        	jQuery(document).find('#agree-terms').attr('checked', false);
-        	jQuery(document).on('change', '#agree-terms', IFS.application_summary.handleChangeAgreeTermsCheckbox);
+          s =this.settings;
+          IFS.application_summary.handleChangeAgreeTermsCheckbox();
+          jQuery(document).on('change', s.checkbox, IFS.application_summary.handleChangeAgreeTermsCheckbox);
+          //if the submitbutton is disabled we prevent the default href action
+          jQuery('body').on('click',s.submitButton+'[aria-disabled]',function(e){ e.preventDefault();});
+        },
+        disableButton : function(){
+           var button = jQuery(s.submitButton);
+           var modal = button.attr('data-js-modal');
+           //remove the modal action and add aria-disabled and disabled styling
+           button.removeAttr('data-js-modal').attr({'data-js-modal-disabled':modal, 'aria-disabled': 'true'}).addClass('disabled');
+        },
+        enableButton : function(){
+            var button = jQuery(s.submitButton);
+            var modal = button.attr('data-js-modal-disabled');
+            button.removeAttr('data-js-modal-disabled aria-disabled').attr('data-js-modal',modal).removeClass('disabled');
         },
         handleChangeAgreeTermsCheckbox: function() {
-        	var checkbox = jQuery(this);
-        	var submitButton = jQuery(document).find('#submit-application-button');
-        	var dummyButton = jQuery(document).find('#disabled-submit-application-button');
-        	if(checkbox.is(":checked")) {
-        		submitButton.show();
-        		dummyButton.hide();
+        	if(jQuery(s.checkbox).prop("checked")) {
+            IFS.application_summary.enableButton();
         	} else {
-        		submitButton.hide();
-        		dummyButton.show();
+            IFS.application_summary.disableButton();
         	}
         }
     };
 })();
-
-
