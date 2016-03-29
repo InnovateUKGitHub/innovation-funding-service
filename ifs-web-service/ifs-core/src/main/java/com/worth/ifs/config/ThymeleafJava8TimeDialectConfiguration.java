b02caec9062bae460afac853d9f72028b960c849
Nico Bijl <nbijl@worth.systems>
2016-03-29 15:20:18 +0200

    INFUND-2313 moving configuration from ifs-application-service to ifs-core

--- ifs-web-service/ifs-core/src/main/java/com/worth/ifs/config/ThymeleafJava8TimeDialectConfiguration.java
+++ ifs-web-service/ifs-core/src/main/java/com/worth/ifs/config/ThymeleafJava8TimeDialectConfiguration.java
@@ -1,4 +1,4 @@
-package com.worth.ifs.configuration;
+package com.worth.ifs.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
