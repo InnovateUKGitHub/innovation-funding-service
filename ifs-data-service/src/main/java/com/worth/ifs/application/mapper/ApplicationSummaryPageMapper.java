b0bbe65f87d65ec939d08e68e3a5efe05a071bee
jonathan.lee <jonleeipl@gmail.com>
2016-03-29 17:06:20 +0100

    INFUND-2135 adding configuration of application summary path.  also updating POJOTest

--- ifs-data-service/src/main/java/com/worth/ifs/application/mapper/ApplicationSummaryPageMapper.java
+++ ifs-data-service/src/main/java/com/worth/ifs/application/mapper/ApplicationSummaryPageMapper.java
@@ -5,7 +5,6 @@ import org.springframework.data.domain.Page;
 
 import com.worth.ifs.application.domain.Application;
 import com.worth.ifs.application.resource.ApplicationSummaryPageResource;
-import com.worth.ifs.commons.mapper.BaseMapper;
 import com.worth.ifs.commons.mapper.GlobalMapperConfig;
 
 @Mapper(
@@ -14,23 +13,20 @@ import com.worth.ifs.commons.mapper.GlobalMapperConfig;
 			ApplicationSummaryMapper.class
 	    }
 	)
-public abstract class ApplicationSummaryPageMapper extends BaseMapper<Page<Application>, ApplicationSummaryPageResource, Long>{
+public abstract class ApplicationSummaryPageMapper {
 
-	@Override
 	public ApplicationSummaryPageResource mapToResource(Page<Application> domain){
 		return null; // TODO this is what i need to do some mapping. could i just implement it?  this mapstruct thing seems like a huge faff.
 	}
 
-	@Override
 	public Iterable<ApplicationSummaryPageResource> mapToResource(Iterable<Page<Application>> domain) {
 		return null;
 	}
 	
-	@Override
     public Page<Application> mapToDomain(ApplicationSummaryPageResource resource){
     	return null;
     }
-    @Override
+
     public Iterable<Page<Application>> mapToDomain(Iterable<ApplicationSummaryPageResource> resource){
     	return null;
     }
