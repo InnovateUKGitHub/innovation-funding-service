97187b0c622d43cd4534afd1fffeb5f07f730674
Ravish Bhagdev <rav@hiveit.co.uk>
2016-03-31 13:39:11 +0100

    adding mising service annotation

--- ifs-web-service/ifs-core/src/main/java/com/worth/ifs/user/service/CompAdminEmailServiceImpl.java
+++ ifs-web-service/ifs-core/src/main/java/com/worth/ifs/user/service/CompAdminEmailServiceImpl.java
@@ -3,7 +3,9 @@ package com.worth.ifs.user.service;
 import com.worth.ifs.commons.rest.RestResult;
 import com.worth.ifs.user.domain.CompAdminEmail;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class CompAdminEmailServiceImpl implements CompAdminEmailService {
 
     @Autowired
