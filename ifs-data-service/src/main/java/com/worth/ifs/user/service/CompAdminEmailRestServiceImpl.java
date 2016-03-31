64b415dab3e36a00cf51eea599b5304c54233fd8
Ravish Bhagdev <rav@hiveit.co.uk>
2016-03-31 13:55:02 +0100

    Adding missing @Service annotation

--- ifs-data-service/src/main/java/com/worth/ifs/user/service/CompAdminEmailRestServiceImpl.java
+++ ifs-data-service/src/main/java/com/worth/ifs/user/service/CompAdminEmailRestServiceImpl.java
@@ -4,11 +4,13 @@ import com.worth.ifs.commons.rest.RestResult;
 import com.worth.ifs.commons.service.BaseRestService;
 import com.worth.ifs.user.domain.CompAdminEmail;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
 import static com.worth.ifs.commons.error.CommonErrors.notFoundError;
 import static com.worth.ifs.commons.rest.RestResult.restFailure;
 
+@Service
 public class CompAdminEmailRestServiceImpl extends BaseRestService implements CompAdminEmailRestService {
 
     private String compAdminEmailRestURL;
