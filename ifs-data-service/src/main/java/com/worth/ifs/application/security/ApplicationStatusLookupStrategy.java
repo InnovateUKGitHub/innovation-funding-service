e6bb2704cb9dbd283a0d9de95eebf1482976b262
David Soff <dsoff@worth.systems>
2016-04-01 10:09:37 +0200

    Revert "Revert "INFUND-1614 finish securing ApplicationStatusService""
    3857ddfe786462d61a7e28c18a27350cdb115fdb

---
+++ ifs-data-service/src/main/java/com/worth/ifs/application/security/ApplicationStatusLookupStrategy.java
@@ -0,0 +1,32 @@
+package com.worth.ifs.application.security;
+
+import com.worth.ifs.application.domain.ApplicationStatus;
+import com.worth.ifs.application.mapper.ApplicationStatusMapper;
+import com.worth.ifs.application.repository.ApplicationStatusRepository;
+import com.worth.ifs.application.resource.ApplicationStatusResource;
+import com.worth.ifs.security.PermissionEntityLookupStrategies;
+import com.worth.ifs.security.PermissionEntityLookupStrategy;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+@PermissionEntityLookupStrategies
+public class ApplicationStatusLookupStrategy {
+    @Autowired
+    private ApplicationStatusRepository applicationStatusRepository;
+
+    @Autowired
+    private ApplicationStatusMapper applicationStatusMapper;
+
+    @PermissionEntityLookupStrategy
+    public ApplicationStatus getApplicationStatus(Long id){
+        return applicationStatusRepository.findOne(id);
+    }
+
+    @PermissionEntityLookupStrategy
+    public ApplicationStatusResource getApplicationStatusResource(Long id){
+        return applicationStatusMapper.mapToResource(applicationStatusRepository.findOne(id));
+    }
+
+}
---
+++ ifs-data-service/src/main/java/com/worth/ifs/application/security/ApplicationStatusRules.java
@@ -0,0 +1,18 @@
+package com.worth.ifs.application.security;
+
+import com.worth.ifs.application.domain.ApplicationStatus;
+import com.worth.ifs.security.PermissionRule;
+import com.worth.ifs.security.PermissionRules;
+import com.worth.ifs.user.domain.User;
+
+import org.springframework.stereotype.Component;
+
+@PermissionRules
+@Component
+public class ApplicationStatusRules {
+
+    @PermissionRule(value = "READ", description = "for now any authenticated user can read the applicationStatusses")
+    public boolean userCanReadApplicationStatus(ApplicationStatus applicationStatus, User user){
+        return true;
+    }
+}
