bd99a83052817460c7204b36bc6ad91131a23822
Duncan Watson <duncan@hiveit.co.uk>
2016-04-11 23:19:43 +0100

    INFUND-2217 - regex password exclusions

--- ifs-data-service/src/main/java/com/worth/ifs/user/transactional/PasswordPolicyValidator.java
+++ ifs-data-service/src/main/java/com/worth/ifs/user/transactional/PasswordPolicyValidator.java
@@ -4,7 +4,12 @@ import com.worth.ifs.user.resource.UserResource;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.function.BiFunction;
 import java.util.function.Function;
+import java.util.regex.Pattern;
+
+import static java.lang.String.format;
+import static java.util.regex.Pattern.CASE_INSENSITIVE;
 
 /**
  * A component that is able to enforce certain additional rules for the Innovate UK password policy
@@ -24,7 +29,7 @@ public class PasswordPolicyValidator {
         }
     }
 
-    private interface ExclusionRuleMutator extends Function<ExclusionRule, List<ExclusionRule>> {
+    private interface ExclusionRuleMutator extends BiFunction<ExclusionRule, UserResource, List<Pattern>> {
 
     }
 
@@ -34,8 +39,11 @@ public class PasswordPolicyValidator {
     private ExclusionRuleMutator numbersForLettersMutator = new ExclusionRuleMutator() {
 
         @Override
-        public List<ExclusionRule> apply(ExclusionRule exclusionRule) {
-            return exclusionRule.description;
+        public List<Pattern> apply(ExclusionRule exclusionRule, UserResource user) {
+
+            String currentExcludedWord = exclusionRule.exclusionSupplier.apply(user);
+            Pattern currentExcludedWordCaseInsensitive = Pattern.compile(format("(%s)", currentExcludedWord), CASE_INSENSITIVE);
+            return currentExcludedWord;
         }
     }
 }
