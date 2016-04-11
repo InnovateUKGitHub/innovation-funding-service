71cdb4fbcffa5ac3662f2673758c8417f5026693
Duncan Watson <duncan@hiveit.co.uk>
2016-04-11 22:43:24 +0100

    INFUND-gst

--- ifs-data-service/src/main/java/com/worth/ifs/user/transactional/PasswordPolicyValidator.java
+++ ifs-data-service/src/main/java/com/worth/ifs/user/transactional/PasswordPolicyValidator.java
@@ -3,6 +3,7 @@ package com.worth.ifs.user.transactional;
 import com.worth.ifs.user.resource.UserResource;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
 import java.util.function.Function;
 
 /**
@@ -17,12 +18,24 @@ public class PasswordPolicyValidator {
         private String description;
         private Function<UserResource, String> exclusionSupplier;
 
-        public ExclusionRule(String description) {
+        public ExclusionRule(String description, Function<UserResource, String> exclusionSupplier) {
             this.description = description;
+            this.exclusionSupplier = exclusionSupplier;
         }
     }
 
-    private class ExclusionRuleMutator {
+    private interface ExclusionRuleMutator extends Function<ExclusionRule, List<ExclusionRule>> {
 
+    }
+
+    private ExclusionRule containsFirstName = new ExclusionRule("PASSWORD_MUST_NOT_CONTAIN_FIRST_NAME", user -> user.getFirstName());
+    private ExclusionRule containsLastName = new ExclusionRule("PASSWORD_MUST_NOT_CONTAIN_LAST_NAME", user -> user.getLastName());
+
+    private ExclusionRuleMutator numbersForLettersMutator = new ExclusionRuleMutator() {
+
+        @Override
+        public List<ExclusionRule> apply(ExclusionRule exclusionRule) {
+            return exclusionRule.description;
+        }
     }
 }
