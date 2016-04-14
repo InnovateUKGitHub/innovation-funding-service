9a8b3345bdbb1621f4eabbb64d49ec2057e7aa22
richardpopple <richard@hiveit.co.uk>
2016-04-14 10:18:21 +0100

    INFUND-1633

--- ifs-data-service/src/main/java/com/worth/ifs/application/security/ApplicationRules.java
+++ ifs-data-service/src/main/java/com/worth/ifs/application/security/ApplicationRules.java
@@ -4,6 +4,7 @@ import com.worth.ifs.application.repository.ApplicationRepository;
 import com.worth.ifs.application.resource.ApplicationResource;
 import com.worth.ifs.security.PermissionRule;
 import com.worth.ifs.security.PermissionRules;
+import com.worth.ifs.security.SecurityRuleUtil;
 import com.worth.ifs.user.domain.ProcessRole;
 import com.worth.ifs.user.domain.Role;
 import com.worth.ifs.user.domain.UserRoleType;
@@ -18,7 +19,6 @@ import org.springframework.stereotype.Component;
 import java.util.Arrays;
 import java.util.List;
 
-import static com.worth.ifs.security.SecurityRuleUtil.isCompAdmin;
 import static com.worth.ifs.user.domain.UserRoleType.*;
 import static com.worth.ifs.util.CollectionFunctions.onlyElement;
 
@@ -36,18 +36,23 @@ public class ApplicationRules {
     @Autowired
     RoleRepository roleRepository;
 
-
-    // TODO RP
-    @PermissionRule(value = "READ_PARTICIPATION_PERCENTAGE", description = "TODO")
-    public boolean qqRP(final ApplicationResource applicationResource, UserResource user){
-        ProcessRole byUserIdAndApplicationId = processRoleRepository.findByUserIdAndApplicationId(user.getId(), applicationResource.getId());
-        return  byUserIdAndApplicationId != null;
+    @PermissionRule(value = "READ_PARTICIPATION_PERCENTAGE", description = "The consortium can see the participation percentage for their applications")
+    public boolean consortiumCanSeeTheParticipantPercentage(final ApplicationResource applicationResource, UserResource user) {
+        final boolean isLeadApplicant = checkRole(user, applicationResource.getId(), LEADAPPLICANT);
+        final boolean isCollaborator = checkRole(user, applicationResource.getId(), COLLABORATOR);
+        return isLeadApplicant || isCollaborator;
     }
 
+    @PermissionRule(value = "READ_PARTICIPATION_PERCENTAGE", description = "The assessor can see the participation percentage for applications they assess")
+    public boolean assessorCanSeeTheParticipantPercentageInApplicationsTheyAssess(final ApplicationResource applicationResource, UserResource user) {
+        final boolean isLeadApplicant = checkRole(user, applicationResource.getId(), LEADAPPLICANT);
+        final boolean isCollaborator = checkRole(user, applicationResource.getId(), COLLABORATOR);
+        return isLeadApplicant || isCollaborator;
+    }
 
-    @PermissionRule(value = "READ", description = "A user can see an applicationResource which they are connected to and if the application exists")
-    public boolean applicantCanSeeConnectedApplicationResource(ApplicationResource application, UserResource user) {
-        return isCompAdmin(user) || !(applicationExists(application) && !userIsConnectedToApplicationResource(application, user));
+    @PermissionRule(value = "READ_PARTICIPATION_PERCENTAGE", description = "The assessor can see the participation percentage for applications they assess")
+    public boolean compAdminCanSeeTheParticipantPercentageInApplications(final ApplicationResource applicationResource, UserResource user) {
+        return SecurityRuleUtil.isCompAdmin(user);
     }
 
     @PermissionRule(value = "UPDATE", description = "A user can update their own application if they are a lead applicant or collaborator of the application")
@@ -70,5 +75,10 @@ public class ApplicationRules {
     boolean applicationExists(ApplicationResource applicationResource) {
         Long id = applicationResource.getId();
         return id != null && applicationRepository.exists(id);
+    }
+
+    private boolean checkRole(UserResource user, Long applicationId, UserRoleType userRoleType) {
+        final ProcessRole processRole = processRoleRepository.findByUserIdAndApplicationId(user.getId(), applicationId);
+        return processRole != null && processRole.getRole().getName().equals(userRoleType.getName());
     }
 }
--- ifs-data-service/src/main/java/com/worth/ifs/finance/security/ApplicationFinancePermissionRules.java
+++ ifs-data-service/src/main/java/com/worth/ifs/finance/security/ApplicationFinancePermissionRules.java
@@ -35,21 +35,22 @@ public class ApplicationFinancePermissionRules {
     @Autowired
     private RoleRepository roleRepository;
 
-    @PermissionRule(value = "READ", description = "The consortium can see their finances of their own organisation")
-    public boolean consortiumCanSeeTheirOwnOrganisationFinances(final ApplicationFinanceResource applicationFinanceResource, final UserResource user) {
+    @PermissionRule(value = "READ", description = "The consortium can see the application finances of their own organisation")
+    public boolean consortiumCanSeeTheApplicationFinancesForTheirOrganisation(final ApplicationFinanceResource applicationFinanceResource, final UserResource user) {
         final boolean isLeadApplicant = checkRole(user, applicationFinanceResource.getApplication(), applicationFinanceResource.getOrganisation(), LEADAPPLICANT);
         final boolean isCollaborator = checkRole(user, applicationFinanceResource.getApplication(), applicationFinanceResource.getOrganisation(), COLLABORATOR);
+        final boolean isApplicant = checkRole(user, applicationFinanceResource.getApplication(), applicationFinanceResource.getOrganisation(), APPLICANT);
         return isLeadApplicant || isCollaborator;
     }
 
-    @PermissionRule(value = "READ", description = "An assessor can see their finances for organisations in the applications they assess")
-    public boolean assessorCanSeeTheFinancesForOrganisationsInApplicationsTheyAssess(final ApplicationFinanceResource applicationFinanceResource, final UserResource user){
+    @PermissionRule(value = "READ", description = "An assessor can see the application finances for organisations in the applications they assess")
+    public boolean assessorCanSeeTheApplicationFinanceForOrganisationsInApplicationsTheyAssess(final ApplicationFinanceResource applicationFinanceResource, final UserResource user){
         final boolean isAssessor = checkRole(user, applicationFinanceResource.getApplication(), applicationFinanceResource.getOrganisation(), ASSESSOR);
         return isAssessor;
     }
 
-    @PermissionRule(value = "READ", description = "A comp admin can see organisation finances for applications")
-    public boolean compAdminCanSeeTheFinancesForOrganisationsInApplications(final ApplicationFinanceResource applicationFinanceResource, final UserResource user){
+    @PermissionRule(value = "READ", description = "A comp admin can see application finances for organisations")
+    public boolean compAdminCanSeeApplicationFinancesForOrganisations(final ApplicationFinanceResource applicationFinanceResource, final UserResource user){
         return SecurityRuleUtil.isCompAdmin(user);
     }
 
