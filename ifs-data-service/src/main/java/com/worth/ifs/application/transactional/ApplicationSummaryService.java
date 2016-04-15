6b00c2a39a485ca2daf20615775ec9544ee4b907
jonathan.lee <jonleeipl@gmail.com>
2016-04-15 10:02:36 +0100

    INFUND-2411 following code review, making method to more relevant service

--- ifs-data-service/src/main/java/com/worth/ifs/application/transactional/ApplicationSummaryService.java
+++ ifs-data-service/src/main/java/com/worth/ifs/application/transactional/ApplicationSummaryService.java
@@ -1,11 +1,7 @@
 package com.worth.ifs.application.transactional;
 
-import java.util.Collection;
-import java.util.List;
-
 import org.springframework.security.access.prepost.PreAuthorize;
 
-import com.worth.ifs.application.domain.Application;
 import com.worth.ifs.application.resource.ApplicationSummaryPageResource;
 import com.worth.ifs.commons.service.ServiceResult;
 
@@ -19,8 +15,5 @@ public interface ApplicationSummaryService {
 	
 	@PreAuthorize("hasAuthority('comp_admin')")
 	ServiceResult<ApplicationSummaryPageResource> getNotSubmittedApplicationSummariesByCompetitionId(Long competitionId, int pageIndex, String sortBy);
-
-	@PreAuthorize("hasAuthority('comp_admin')")
-	ServiceResult<List<Application>> getApplicationSummariesByCompetitionIdAndStatus(Long competitionId, Collection<Long> applicationStatusId);
 
 }
