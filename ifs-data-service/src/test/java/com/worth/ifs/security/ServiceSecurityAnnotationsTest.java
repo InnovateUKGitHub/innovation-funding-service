a8b9fb899c1af534368be573fef5feb318443989
Duncan Watson <duncan@hiveit.co.uk>
2016-04-15 17:27:55 +0100

    INFUND-2233 and 2234 - amended CSV generation to order by entity and action key

--- ifs-data-service/src/test/java/com/worth/ifs/security/ServiceSecurityAnnotationsTest.java
+++ ifs-data-service/src/test/java/com/worth/ifs/security/ServiceSecurityAnnotationsTest.java
@@ -5,6 +5,7 @@ import com.worth.ifs.BaseIntegrationTest;
 import com.worth.ifs.commons.security.UidAuthenticationService;
 import com.worth.ifs.commons.service.BaseRestService;
 import com.worth.ifs.file.transactional.FileServiceImpl;
+import org.apache.commons.lang3.tuple.Triple;
 import org.junit.Test;
 import org.springframework.aop.framework.Advised;
 import org.springframework.aop.support.AopUtils;
@@ -26,9 +27,13 @@ import java.lang.reflect.Modifier;
 import java.util.*;
 
 import static com.worth.ifs.util.CollectionFunctions.simpleFilter;
+import static com.worth.ifs.util.CollectionFunctions.sort;
 import static org.junit.Assert.*;
 
-public class AllServicesAreAnnotatedTest extends BaseIntegrationTest {
+/**
+ * Tests around the Spring Security annotations on the Services and the Permission Rule framework as used by the CustomPermissionEvaluator
+ */
+public class ServiceSecurityAnnotationsTest extends BaseIntegrationTest {
 
     @Autowired
     private ApplicationContext context;
@@ -77,31 +82,56 @@ public class AllServicesAreAnnotatedTest extends BaseIntegrationTest {
         assertTrue("We should be checking at least one method for security annotations", totalMethodsChecked > 0);
     }
 
+    /**
+     * This test will generate a CSV of Permission Rules ordered by the Entities that they apply to.
+     *
+     * This is of the format "Entity", "Action", "Rule description"
+     *
+     * @throws IOException
+     */
     @Test
     public void generateLowLevelPermissionsDocumentation() throws IOException {
 
         CustomPermissionEvaluator evaluator = (CustomPermissionEvaluator) context.getBean("customPermissionEvaluator");
 
+        List<Triple<String, String, String>> permissionRuleRows = new ArrayList<>();
+
         CustomPermissionEvaluator.DtoClassToPermissionsToPermissionsMethods rulesMap =
                 (CustomPermissionEvaluator.DtoClassToPermissionsToPermissionsMethods) ReflectionTestUtils.getField(evaluator, "rulesMap");
 
-        CSVWriter writer = new CSVWriter(new FileWriter("permission-rules.csv"), '\t');
+        Comparator<Class<?>> simpleNameComparator = (clazz1, clazz2) -> clazz1.getSimpleName().compareTo(clazz2.getSimpleName());
+        List<Class<?>> orderedClasses = sort(rulesMap.keySet(), simpleNameComparator);
 
-        try {
+        orderedClasses.forEach(clazz -> {
 
-            writer.writeNext(new String[]{"Object", "Action", "Rule"});
+            Map<String, CustomPermissionEvaluator.ListOfMethods> rulesForClass = rulesMap.get(clazz);
+            List<String> orderedActions = sort(rulesForClass.keySet());
 
-            rulesMap.forEach((clazz, action) -> {
-                action.forEach((actionName, permissionRuleMethods) -> permissionRuleMethods.forEach(serviceAndMethod -> {
+            orderedActions.forEach(actionName -> {
 
-                    Object service = serviceAndMethod.getLeft();
+                CustomPermissionEvaluator.ListOfMethods permissionRuleMethodsForThisAction = rulesForClass.get(actionName);
+
+                permissionRuleMethodsForThisAction.forEach(serviceAndMethod -> {
+
                     Method ruleMethod = serviceAndMethod.getValue();
                     PermissionRule permissionRule = ruleMethod.getDeclaredAnnotation(PermissionRule.class);
 
-                    writer.writeNext(new String[]{clazz.getSimpleName(), actionName, permissionRule.description()});
-                }));
-            });
+                    final String finalClassName;
+                    if (clazz.getSimpleName().endsWith("Resource")) {
+                        finalClassName = clazz.getSimpleName().substring(0, clazz.getSimpleName().length() - "Resource".length());
+                    } else {
+                        finalClassName = clazz.getSimpleName();
+                    }
 
+                    permissionRuleRows.add(Triple.of(finalClassName, actionName, permissionRule.description()));
+                });
+            });
+        });
+
+        CSVWriter writer = new CSVWriter(new FileWriter("build/permission-rules.csv"), '\t');
+        try {
+            writer.writeNext(new String[]{"Entity", "Action", "Rule description"});
+            permissionRuleRows.forEach(row -> writer.writeNext(new String[] {row.getLeft(), row.getMiddle(), row.getRight()}));
         } finally {
             writer.close();
         }
