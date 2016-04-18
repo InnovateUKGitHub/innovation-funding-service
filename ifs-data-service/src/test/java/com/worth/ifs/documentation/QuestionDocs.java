11ad6ab714e44f4b63a13eddcdc13966f79efe3a
Rogier de Regt <rderegt@worthit.nl>
2016-04-18 09:57:21 +0100

    INFUND-1605 updated docs

--- ifs-data-service/src/test/java/com/worth/ifs/documentation/QuestionDocs.java
+++ ifs-data-service/src/test/java/com/worth/ifs/documentation/QuestionDocs.java
@@ -3,15 +3,25 @@ package com.worth.ifs.documentation;
 import com.worth.ifs.application.builder.QuestionBuilder;
 
 import com.worth.ifs.application.builder.QuestionResourceBuilder;
+import com.worth.ifs.application.domain.QuestionStatus;
+import com.worth.ifs.application.resource.QuestionStatusResource;
+import com.worth.ifs.application.resource.SectionResource;
 import org.springframework.restdocs.payload.FieldDescriptor;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 import static com.worth.ifs.application.builder.QuestionBuilder.newQuestion;
 import static com.worth.ifs.application.builder.QuestionResourceBuilder.newQuestionResource;
 import static com.worth.ifs.application.builder.QuestionStatusBuilder.newQuestionStatus;
+import static com.worth.ifs.application.builder.QuestionStatusResourceBuilder.newQuestionStatusResource;
 import static com.worth.ifs.application.builder.ResponseBuilder.newResponse;
 import static com.worth.ifs.application.builder.SectionBuilder.newSection;
+import static com.worth.ifs.application.builder.SectionResourceBuilder.newSectionResource;
 import static com.worth.ifs.competition.builder.CompetitionBuilder.newCompetition;
 import static com.worth.ifs.finance.builder.CostBuilder.newCost;
+import static com.worth.ifs.util.CollectionFunctions.simpleMap;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 
 public class QuestionDocs {
@@ -31,23 +41,13 @@ public class QuestionDocs {
         fieldWithPath("formInputs").description("list of the inputs used to answer the question"),
         fieldWithPath("assessorConfirmationQuestion").description("scope verification question for the assessor"),
         fieldWithPath("questionStatuses").description("List of question statuses"),
-        fieldWithPath("questionNumber").description("number of the question")
-    };
+        fieldWithPath("questionNumber").description("number of the question"),
+        fieldWithPath("section").description("Id of the section of which the question is part of").optional(),
+        fieldWithPath("competition").description("Id of the competition"),
+        fieldWithPath("responses").description("List of ids for the responses to the question").optional(),
+        fieldWithPath("costs").description("List of ids of the costs related to the finance questions").optional()
 
-    public static final QuestionBuilder questionBuilder = newQuestion()
-        .withId(1L)
-        .withName("question name")
-        .withShortName("name")
-        .withDescription("description")
-        .withGuidanceQuestion("guidance question")
-        .withPriority(1)
-        .withAssessorConfirmationQuestion("confirmation question")
-        .withCompetition(newCompetition().build())
-        .withSection(newSection().build())
-        .withResponses(newResponse().build(2))
-        .withQuestionStatuses(newQuestionStatus().build(2))
-        .withCosts(newCost().build(2))
-        .withQuestionNumber("1");
+    };
 
     public static final QuestionResourceBuilder questionBuilder = newQuestionResource()
             .withId(1L)
@@ -57,10 +57,10 @@ public class QuestionDocs {
             .withGuidanceQuestion("guidance question")
             .withPriority(1)
             .withAssessorConfirmationQuestion("confirmation question")
-            .withCompetition(newCompetition().build())
-            .withSection(newSection().build())
-            .withResponses(newResponse().build(2))
-            .withQuestionStatuses(newQuestionStatus().build(2))
-            .withCosts(newCost().build(2))
+            .withCompetition(1L)
+            .withSection(1L)
+            .withResponses(newSectionResource().build(2).stream().map(SectionResource::getId).collect(Collectors.toList()))
+            .withQuestionStatuses(newQuestionStatusResource().build(2).stream().map(QuestionStatusResource::getId).collect(Collectors.toList()))
+            .withCosts(Arrays.asList(1L))
             .withQuestionNumber("1");
 }
