56365436e439aa476fde9595cba78d3949a82daf
Duncan Watson <duncan@hiveit.co.uk>
2016-04-19 12:13:21 +0100

    INFUND-2555 - made all inner test services public for recording purposes

--- ifs-data-service/src/test/java/com/worth/ifs/user/service/UserServiceSecurityTest.java
+++ ifs-data-service/src/test/java/com/worth/ifs/user/service/UserServiceSecurityTest.java
@@ -1,6 +1,7 @@
 package com.worth.ifs.user.service;
 
 import com.worth.ifs.BaseServiceSecurityTest;
+import com.worth.ifs.commons.service.ServiceResult;
 import com.worth.ifs.token.domain.Token;
 import com.worth.ifs.token.security.TokenLookupStrategies;
 import com.worth.ifs.token.security.TokenPermissionRules;
@@ -9,7 +10,12 @@ import com.worth.ifs.user.security.UserPermissionRules;
 import com.worth.ifs.user.transactional.UserService;
 import org.junit.Before;
 import org.junit.Test;
+import org.springframework.security.access.method.P;
 
+import java.util.List;
+import java.util.Set;
+
+import static com.worth.ifs.commons.service.ServiceResult.serviceSuccess;
 import static com.worth.ifs.user.builder.UserResourceBuilder.newUserResource;
 import static org.mockito.Matchers.eq;
 import static org.mockito.Matchers.isA;
@@ -134,4 +140,54 @@ public class UserServiceSecurityTest extends BaseServiceSecurityTest<UserService
         return TestUserService.class;
     }
 
+    /**
+     * Test class for use in Service Security tests.
+     */
+    public static class TestUserService implements UserService {
+
+        @Override
+        public ServiceResult<UserResource> getUserResourceByUid(String uid) {
+            return serviceSuccess(newUserResource().build());
+        }
+
+        @Override
+        public ServiceResult<UserResource> getUserById(Long id) {
+            return serviceSuccess(newUserResource().build());
+        }
+
+        @Override
+        public ServiceResult<List<UserResource>> findAll() {
+            return serviceSuccess(newUserResource().build(2));
+        }
+
+        @Override
+        public ServiceResult<UserResource> findByEmail(String email) {
+            return serviceSuccess(newUserResource().build());
+        }
+
+        @Override
+        public ServiceResult<Set<UserResource>> findAssignableUsers(Long applicationId) {
+            return serviceSuccess(newUserResource().buildSet(2));
+        }
+
+        @Override
+        public ServiceResult<Set<UserResource>> findRelatedUsers(Long applicationId) {
+            return serviceSuccess(newUserResource().buildSet(2));
+        }
+
+        @Override
+        public ServiceResult<Void> sendPasswordResetNotification(@P("user") UserResource user) {
+            return null;
+        }
+
+        @Override
+        public ServiceResult<Void> checkPasswordResetHashValidity(@P("hash") String hash) {
+            return null;
+        }
+
+        @Override
+        public ServiceResult<Void> changePassword(@P("hash") String hash, String password) {
+            return null;
+        }
+    }
 }
