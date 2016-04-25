310f039158bb8efa8e404d5846dd042e93c63dd7
Duncan Watson <duncan@hiveit.co.uk>
2016-04-25 11:20:26 +0100

    INFUND-2565 - removed static autowiring to avoid non-GC-compatible fields

--- ifs-data-service/src/main/java/com/worth/ifs/validator/transactional/ValidatorServiceImpl.java
+++ ifs-data-service/src/main/java/com/worth/ifs/validator/transactional/ValidatorServiceImpl.java
@@ -20,10 +20,15 @@ import java.util.List;
  */
 @Service
 public class ValidatorServiceImpl extends BaseTransactionalService implements ValidatorService {
+
     @Autowired
     private FormInputResponseRepository formInputResponseRepository;
+
     @Autowired
-    CostService costService;
+    private CostService costService;
+
+    @Autowired
+    private ValidationUtil validationUtil;
 
     private static final Log LOG = LogFactory.getLog(ValidatorServiceImpl.class);
 
@@ -33,7 +38,7 @@ public class ValidatorServiceImpl extends BaseTransactionalService implements Va
         List<FormInputResponse> response = formInputResponseRepository.findByApplicationIdAndFormInputId(applicationId, formInputId);
         if(!response.isEmpty()) {
             for (FormInputResponse formInputResponse : response) {
-                results.add(ValidationUtil.validateResponse(formInputResponse, false));
+                results.add(validationUtil.validateResponse(formInputResponse, false));
             }
         }
         return results;
@@ -42,7 +47,7 @@ public class ValidatorServiceImpl extends BaseTransactionalService implements Va
     @Override
     public BindingResult validateFormInputResponse(Long applicationId, Long formInputId, Long markedAsCompleteById) {
         FormInputResponse response = formInputResponseRepository.findByApplicationIdAndUpdatedByIdAndFormInputId(applicationId, markedAsCompleteById, formInputId);
-        return ValidationUtil.validateResponse(response, false);
+        return validationUtil.validateResponse(response, false);
     }
 
 
@@ -52,7 +57,7 @@ public class ValidatorServiceImpl extends BaseTransactionalService implements Va
             return costService.financeDetails(applicationId, role.getOrganisation().getId()).andOnSuccess(financeDetails -> {
                 return costService.getCostItems(financeDetails.getId(), questionId).andOnSuccessReturn(costItems -> {
                     LOG.debug("=======Got Cost Items : count 2: "+costItems.size());
-                    return ValidationUtil.validateCostItem(costItems);
+                    return validationUtil.validateCostItem(costItems);
                 });
             });
         }).getSuccessObject();
