b2fb18a6ca6cdf9bd3c626a7e5100e7108a53dfa
Nico Bijl <nbijl@worth.systems>
2016-04-26 12:34:08 +0200

    INFUND-844 added max labour days per year.

---
+++ ifs-data-service/src/main/java/com/worth/ifs/validator/ConditionalMaxLabourDaysValidator.java
@@ -0,0 +1,33 @@
+package com.worth.ifs.validator;
+
+import com.worth.ifs.finance.resource.category.LabourCostCategory;
+import com.worth.ifs.finance.resource.cost.LabourCost;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class ConditionalMaxLabourDaysValidator  implements ConstraintValidator<ConditionalMaxLabourDays, LabourCost> {
+    private static final Log LOG = LogFactory.getLog(ConditionalMaxLabourDaysValidator.class);
+
+    @Override
+    public boolean isValid(final LabourCost value, final ConstraintValidatorContext ctx) {
+        if (value == null) {
+            return true;
+        }
+
+        if(StringUtils.isNotEmpty(value.getName())
+                && value.getName().equals(LabourCostCategory.WORKING_DAYS_KEY)){
+            if(value.getLabourDays() > 365){
+                // A year can not have more working days then 365...
+                return false;
+            }else{
+                return true;
+            }
+        }
+
+        return true;
+    }
+}
