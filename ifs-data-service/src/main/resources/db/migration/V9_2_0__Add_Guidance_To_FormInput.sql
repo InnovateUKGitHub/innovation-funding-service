ed4fe76e3e242c033a257159484106f3601fbc6e
David Soff <dsoff@worth.systems>
2016-05-06 12:28:32 +0200

    INFUND-2024 add migration to move guidance from question to form_input

--- ifs-data-service/src/main/resources/db/migration/V9_2_0__Add_Guidance_To_FormInput.sql
+++ ifs-data-service/src/main/resources/db/migration/V9_2_0__Add_Guidance_To_FormInput.sql
@@ -1,2 +1,11 @@
-ALTER TABLE `form_input` ADD `guidance_answer` longtext;
 ALTER TABLE `form_input` ADD `guidance_question` longtext;
+ALTER TABLE `form_input` ADD `guidance_answer` longtext;
+
+UPDATE form_input
+JOIN question_form_input ON form_input.id=question_form_input.form_input_id
+JOIN question ON question.id=question_form_input.question_id
+SET form_input.guidance_question=question.guidance_question,
+    form_input.guidance_answer=question.guidance_answer;
+
+ALTER TABLE `question` DROP COLUMN `guidance_question`;
+ALTER TABLE `question` DROP COLUMN `guidance_answer`;
