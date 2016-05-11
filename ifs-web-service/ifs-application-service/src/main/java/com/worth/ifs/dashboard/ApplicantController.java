bea0a4bdea2b7df8208a6937f6ed936ca6d055c0
jonathan.lee <jonleeipl@gmail.com>
2016-05-11 12:08:39 +0100

    INFUND-2604 displaying project setup section in dashboard

--- ifs-web-service/ifs-application-service/src/main/java/com/worth/ifs/dashboard/ApplicantController.java
+++ ifs-web-service/ifs-application-service/src/main/java/com/worth/ifs/dashboard/ApplicantController.java
@@ -1,5 +1,21 @@
 package com.worth.ifs.dashboard;
 
+import static com.worth.ifs.util.CollectionFunctions.combineLists;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.worth.ifs.application.constant.ApplicationStatusConstants;
 import com.worth.ifs.application.resource.ApplicationResource;
 import com.worth.ifs.application.resource.ApplicationStatusResource;
 import com.worth.ifs.application.service.ApplicationService;
@@ -11,18 +27,6 @@ import com.worth.ifs.user.domain.UserRoleType;
 import com.worth.ifs.user.resource.ProcessRoleResource;
 import com.worth.ifs.user.resource.UserResource;
 import com.worth.ifs.user.service.ProcessRoleService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-
-import javax.servlet.http.HttpServletRequest;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-
-import static com.worth.ifs.util.CollectionFunctions.combineLists;
 
 /**
  * This controller will handle requests related to the current applicant. So pages that are relative to that user,
@@ -56,19 +60,28 @@ public class ApplicantController {
         List<ApplicationResource> inProgress = applicationService.getInProgress(user.getId());
         List<ApplicationResource> finished = applicationService.getFinished(user.getId());
 
+        List<ApplicationResource> projectsInSetup = projectsInSetup(finished);
+        
         Map<Long, CompetitionResource> competitions = createCompetitionMap(inProgress, finished);
         Map<Long, ApplicationStatusResource> applicationStatusMap = createApplicationStatusMap(inProgress, finished);
 
         model.addAttribute("applicationsInProcess", inProgress);
         model.addAttribute("applicationsAssigned", getAssignedApplications(inProgress, user));
         model.addAttribute("applicationsFinished", finished);
+        model.addAttribute("projectsInSetup", projectsInSetup);
         model.addAttribute("competitions", competitions);
         model.addAttribute("applicationStatuses", applicationStatusMap);
 
         return "applicant-dashboard";
     }
 
-    /**
+    private List<ApplicationResource> projectsInSetup(List<ApplicationResource> finished) {
+		return finished.stream()
+				.filter(a -> ApplicationStatusConstants.APPROVED.getId().equals(a.getApplicationStatus()))
+				.collect(Collectors.toList());
+	}
+
+	/**
      * Get a list of application ids, where one of the questions is assigned to the current user. This is only for the
      * collaborators, since the leadapplicant is the default assignee.
      */
