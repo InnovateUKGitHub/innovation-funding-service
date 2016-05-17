a674f8a6f317799be0f9e773baa6128e40fe0606
jonathan.lee <jonleeipl@gmail.com>
2016-05-17 15:36:54 +0100

    INFUND-2609 initial commit outlining project finance role

---
+++ ifs-data-service/src/main/resources/db/migration/V16_0__Add_project_finance_role.sql
@@ -0,0 +1,1 @@
+INSERT IGNORE INTO `role` (`id`, `name`) VALUES (8,'project_finance');
---
+++ ifs-data-service/src/main/resources/db/setup/V16_2__Add_project_finance_emails.sql
@@ -0,0 +1,9 @@
+/** Adding actual users who will be project finance in prod **/
+INSERT  IGNORE INTO `project_finance_emails` (`email`) VALUES ('sarah.vodden@innovateuk.gov.uk');
+INSERT  IGNORE INTO `project_finance_emails` (`email`) VALUES ('barry.shaw@innovateuk.gov.uk');
+INSERT  IGNORE INTO `project_finance_emails` (`email`) VALUES ('adam.mapstone@innovateuk.gov.uk');
+INSERT  IGNORE INTO `project_finance_emails` (`email`) VALUES ('james.danek@innovateuk.gov.uk');
+INSERT  IGNORE INTO `project_finance_emails` (`email`) VALUES ('jessica.cook@innovateuk.gov.uk');
+INSERT  IGNORE INTO `project_finance_emails` (`email`) VALUES ('kelly.blake@innovateuk.gov.uk');
+INSERT  IGNORE INTO `project_finance_emails` (`email`) VALUES ('jonathan.else@innovateuk.gov.uk');
+INSERT  IGNORE INTO `project_finance_emails` (`email`) VALUES ('ellie.ricketts-jones@innovateuk.gov.uk');
