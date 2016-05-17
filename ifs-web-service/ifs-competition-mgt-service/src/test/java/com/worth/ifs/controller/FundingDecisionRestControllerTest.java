ba30d75af1239ca3b0d3117fd92a22e867b6cf3a
jonathan.lee <jonleeipl@gmail.com>
2016-05-17 15:55:07 +0100

    INFUND-2885 including test for funding decision autosave controller

--- ifs-web-service/ifs-competition-mgt-service/src/test/java/com/worth/ifs/controller/FundingDecisionRestControllerTest.java
+++ ifs-web-service/ifs-competition-mgt-service/src/test/java/com/worth/ifs/controller/FundingDecisionRestControllerTest.java
@@ -1,7 +1,7 @@
 package com.worth.ifs.controller;
 
-import static com.worth.ifs.util.MapFunctions.asMap;
 import static org.hamcrest.Matchers.is;
+import static org.mockito.Matchers.argThat;
 import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -11,12 +11,11 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import java.util.Map;
 
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
+import org.mockito.ArgumentMatcher;
 import org.mockito.Mock;
 import org.springframework.http.MediaType;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.worth.ifs.application.resource.FundingDecision;
 import com.worth.ifs.application.service.ApplicationFundingDecisionService;
 
@@ -36,21 +35,24 @@ public class FundingDecisionRestControllerTest extends BaseControllerMockMVCTest
     }
     
 	@Test
-	@Ignore
     public void saveFundingDecision() throws Exception {
     	
-		Map<String, String> request = asMap("applicationId", 3, "fundingDecision", "Y");
-		String jsonRequestBody = new ObjectMapper().writeValueAsString(request);
-		
     	mockMvc.perform(
     				post("/funding/123")
-    				.contentType(MediaType.APPLICATION_JSON)
-    				.accept(MediaType.APPLICATION_JSON)
-    				.content(jsonRequestBody)
+    				.contentType(MediaType.APPLICATION_FORM_URLENCODED)
+    				.param("applicationId", "3")
+    				.param("fundingDecision", "Y")
     			)
                 .andExpect(jsonPath("$.success", is("true")));
     	
-    	verify(applicationFundingDecisionService).saveApplicationFundingDecisionData(eq(123L), eq(asMap(3, FundingDecision.FUNDED)));
-    }
+    	verify(applicationFundingDecisionService).saveApplicationFundingDecisionData(eq(123L), argThat(new ArgumentMatcher<Map<Long, FundingDecision>> (){
 
+			@SuppressWarnings("unchecked")
+			@Override
+			public boolean matches(Object argument) {
+				Map<Long, FundingDecision> arg = (Map<Long, FundingDecision>) argument;
+				return arg.size() == 1 && arg.containsKey(3L) && FundingDecision.FUNDED.equals(arg.get(3L));
+			}}));
+    }
+	
 }
