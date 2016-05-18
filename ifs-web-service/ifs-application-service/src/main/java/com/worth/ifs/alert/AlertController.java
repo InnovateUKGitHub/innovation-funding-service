b7a5f4405b95e3a6a2c9006c80ec136113de12a3
Reinaud van Rumpt <rvanrumpt@worth.systems>
2016-05-18 16:51:15 +0200

    INFUND-2901 Controller that returns AlertMessages in JSON via the weblayer

---
+++ ifs-web-service/ifs-application-service/src/main/java/com/worth/ifs/alert/AlertController.java
@@ -0,0 +1,46 @@
+package com.worth.ifs.alert;
+
+import com.worth.ifs.alert.resource.AlertResource;
+import com.worth.ifs.alert.resource.AlertType;
+import com.worth.ifs.application.service.AlertService;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by rvanrumpt on 18/05/16.
+ */
+
+
+@Controller
+@RequestMapping("/alert")
+public class AlertController {
+
+    @Autowired
+    AlertService alertService;
+
+    private static Log LOG = LogFactory.getLog(AlertController.class);
+
+    @RequestMapping(value = "/findAllVisibleByType/{type}", method = RequestMethod.GET)
+    public @ResponseBody
+    List<AlertResource> getAlertByTypeJSON(@PathVariable("type") String type) {
+        List<AlertResource> alerts = new ArrayList();
+
+        try {
+            AlertType alertType = AlertType.valueOf(type.toUpperCase());
+            alerts = alertService.findAllVisibleByType(alertType);
+        } catch (IllegalArgumentException e) {
+            LOG.debug(e.getMessage());
+        }
+
+        return alerts;
+    }
+}
