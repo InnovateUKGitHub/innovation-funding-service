6b8ea77ff08ba71cc7b339c307a4458245f41473
Joe Hajas <jhajas@worth.systems>
2016-05-23 16:07:41 +0100

    INFUND-2707 As an applicant I want to get validation errors when I mark as complete the application details page with invalid inputs Added custom validation class

---
+++ ifs-data-service/src/main/java/com/worth/ifs/validator/ApplicationMarkAsCompleteValidator.java
@@ -0,0 +1,40 @@
+package com.worth.ifs.validator;
+
+import com.worth.ifs.application.domain.Application;
+import com.worth.ifs.form.domain.FormInputResponse;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.util.StringUtils;
+import org.springframework.validation.Errors;
+
+/**
+ * Validates the inputs in the application details, if valid on the markAsComplete action
+ */
+//@todo refactor, messages, conditions
+public class ApplicationMarkAsCompleteValidator extends BaseValidator{
+    private static final Log LOG = LogFactory.getLog(ApplicationMarkAsCompleteValidator.class);
+
+    @Override
+    public void validate(Object target, Errors errors) {
+        LOG.debug("do ApplicationMarkAsComplete Validation");
+
+        FormInputResponse response = (FormInputResponse) target;
+        Application application = response.getApplication();
+
+        if (StringUtils.isEmpty(application.getName())) {
+            LOG.debug("MarkAsComplete validation message for: " + application.getName());
+            errors.rejectValue("value", "response.emptyResponse", "Please enter some text");
+        }
+
+        if (StringUtils.isEmpty(application.getDurationInMonths()) || application.getDurationInMonths() < 0 || application.getDurationInMonths() > 99) {
+            LOG.debug("MarkAsComplete validation message for: " + application.getDurationInMonths());
+            errors.rejectValue("value", "response.emptyResponse", "Please enter some text");
+        }
+
+        if (StringUtils.isEmpty(application.getStartDate())) {
+           LOG.debug("MarkAsComplete validation message for: " + response.getId());
+         //   errors.rejectValue("value", "response.emptyResponse", "Please enter some text");
+        }
+
+    }
+}
