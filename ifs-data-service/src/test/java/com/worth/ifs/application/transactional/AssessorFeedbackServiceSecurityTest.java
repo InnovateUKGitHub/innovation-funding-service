80e4c089db8763d26eb77c63a85c5a4a95a3ef8b
jonathan.lee <jonleeipl@gmail.com>
2016-05-26 15:38:51 +0100

    INFUND-2672 ensuring security and documentation for submitting assessor feedback

--- ifs-data-service/src/test/java/com/worth/ifs/application/transactional/AssessorFeedbackServiceSecurityTest.java
+++ ifs-data-service/src/test/java/com/worth/ifs/application/transactional/AssessorFeedbackServiceSecurityTest.java
@@ -7,15 +7,27 @@ import com.worth.ifs.application.security.ApplicationLookupStrategy;
 import com.worth.ifs.application.security.ApplicationPermissionRules;
 import com.worth.ifs.commons.service.ServiceResult;
 import com.worth.ifs.file.resource.FileEntryResource;
+import com.worth.ifs.user.resource.RoleResource;
+import com.worth.ifs.user.resource.UserRoleType;
+
 import org.apache.commons.lang3.tuple.Pair;
 import org.junit.Before;
 import org.junit.Test;
+import org.springframework.security.access.AccessDeniedException;
 
 import java.io.InputStream;
+import java.util.List;
 import java.util.function.Supplier;
 
 import static com.worth.ifs.application.builder.ApplicationResourceBuilder.newApplicationResource;
 import static com.worth.ifs.file.resource.builders.FileEntryResourceBuilder.newFileEntryResource;
+import static com.worth.ifs.user.builder.RoleResourceBuilder.newRoleResource;
+import static com.worth.ifs.user.builder.UserResourceBuilder.newUserResource;
+import static com.worth.ifs.user.resource.UserRoleType.COMP_ADMIN;
+import static java.util.Arrays.asList;
+import static java.util.Collections.singletonList;
+import static java.util.stream.Collectors.toList;
+import static org.junit.Assert.fail;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -25,9 +37,11 @@ import static org.mockito.Mockito.when;
  */
 public class AssessorFeedbackServiceSecurityTest extends BaseServiceSecurityTest<AssessorFeedbackService> {
 
+	private static final Long COMPETITION_ID = 123L;
+	 
     private ApplicationPermissionRules rules;
     private ApplicationLookupStrategy lookupStrategy;
-
+    
     @Before
     public void lookupMockPermissionBeans() {
         rules = getMockPermissionRulesBean(ApplicationPermissionRules.class);
@@ -95,7 +109,107 @@ public class AssessorFeedbackServiceSecurityTest extends BaseServiceSecurityTest
             verify(rules).applicationTeamCanSeeAndDownloadPublishedAssessorFeedbackForTheirApplications(application, getLoggedInUser());
         });
     }
+    
+    @Test
+	public void test_assessorFeedbackUploaded_allowedIfGlobalCompAdminRole() {
 
+		RoleResource compAdminRole = newRoleResource().withType(COMP_ADMIN).build();
+		setLoggedInUser(newUserResource().withRolesGlobal(singletonList(compAdminRole)).build());
+		service.assessorFeedbackUploaded(COMPETITION_ID);
+	}
+
+	@Test
+	public void test_assessorFeedbackUploaded_deniedIfNotLoggedIn() {
+
+		setLoggedInUser(null);
+		try {
+			service.assessorFeedbackUploaded(COMPETITION_ID);
+			fail("Should not have been able to check if assessor feedback uploaded without first logging in");
+		} catch (AccessDeniedException e) {
+			// expected behaviour
+		}
+	}
+
+	@Test
+	public void test_assessorFeedbackUploaded_deniedIfNoGlobalRolesAtAll() {
+
+		try {
+			service.assessorFeedbackUploaded(COMPETITION_ID);
+			fail("Should not have been able to check if assessor feedback uploaded without the global comp admin role");
+		} catch (AccessDeniedException e) {
+			// expected behaviour
+		}
+	}
+
+	@Test
+	public void test_assessorFeedbackUploaded_deniedIfNotCorrectGlobalRoles() {
+
+		List<UserRoleType> nonCompAdminRoles = asList(UserRoleType.values()).stream().filter(type -> type != COMP_ADMIN)
+				.collect(toList());
+
+		nonCompAdminRoles.forEach(role -> {
+
+			setLoggedInUser(newUserResource().withRolesGlobal(singletonList(newRoleResource().withType(role).build())).build());
+
+			try {
+				service.assessorFeedbackUploaded(COMPETITION_ID);
+				fail("Should not have been able to check if assessor feedback uploaded without the global Comp Admin role");
+			} catch (AccessDeniedException e) {
+				// expected behaviour
+			}
+		});
+	}
+
+	@Test
+	public void test_submitAssessorFeedback_allowedIfGlobalCompAdminRole() {
+
+		RoleResource compAdminRole = newRoleResource().withType(COMP_ADMIN).build();
+		setLoggedInUser(newUserResource().withRolesGlobal(singletonList(compAdminRole)).build());
+		service.submitAssessorFeedback(COMPETITION_ID);
+	}
+
+	@Test
+	public void test_submitAssessorFeedback_deniedIfNotLoggedIn() {
+
+		setLoggedInUser(null);
+		try {
+			service.submitAssessorFeedback(COMPETITION_ID);
+			fail("Should not have been able to submit assessor feedback without first logging in");
+		} catch (AccessDeniedException e) {
+			// expected behaviour
+		}
+	}
+
+	@Test
+	public void test_submitAssessorFeedback_deniedIfNoGlobalRolesAtAll() {
+
+		try {
+			service.submitAssessorFeedback(COMPETITION_ID);
+			fail("Should not have been able to submit assessor feedback without the global comp admin role");
+		} catch (AccessDeniedException e) {
+			// expected behaviour
+		}
+	}
+
+	@Test
+	public void test_submitAssessorFeedback_deniedIfNotCorrectGlobalRoles() {
+
+		List<UserRoleType> nonCompAdminRoles = asList(UserRoleType.values()).stream().filter(type -> type != COMP_ADMIN)
+				.collect(toList());
+
+		nonCompAdminRoles.forEach(role -> {
+
+			setLoggedInUser(newUserResource().withRolesGlobal(singletonList(newRoleResource().withType(role).build())).build());
+
+			try {
+				service.submitAssessorFeedback(COMPETITION_ID);
+				fail("Should not have been able to submit assessor feedback without the global Comp Admin role");
+			} catch (AccessDeniedException e) {
+				// expected behaviour
+			}
+		});
+	}
+	
     @Override
     protected Class<? extends AssessorFeedbackService> getServiceClass() {
         return TestAssessorFeedbackService.class;
