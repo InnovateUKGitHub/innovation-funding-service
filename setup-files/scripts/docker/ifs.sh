ba1ba7431457e6c1cf9b87008b6c837fef5ab457
David Soff <dsoff@worth.systems>
2016-05-27 16:30:27 +0200

    INFUND-3094 Docker setup

---
+++ setup-files/scripts/docker/ifs.sh
@@ -0,0 +1,60 @@
+#!/bin/sh
+
+startingDir= pwd
+
+eval $(docker-machine env default)
+
+cd ../../../
+
+start() {
+    docker-compose up -d
+}
+
+stop() {
+    docker-compose stop
+}
+
+restart() {
+    docker-compose restart
+}
+
+rebuild() {
+    docker-compose stop
+    docker-compose up -d --build
+
+}
+redeploy() {
+    docker-compose exec $1 gradle cleanDeploy --daemon
+}
+
+noTest() {
+     docker-compose exec $1 gradle cleanDeploy -x test --daemon
+}
+
+
+case "$1" in
+        start)
+            start
+            ;;
+        stop)
+            stop
+            ;;
+        restart)
+            restart
+            ;;
+        rebuild)
+            rebuild $2
+            ;;
+        redeploy)
+            redeploy $2
+            ;;
+        notest)
+            noTest $2
+            ;;
+
+        *)
+            echo $"that will not work, please check the script to find out how to use it :D"
+            exit 1
+esac
+
+cd $startingDir
