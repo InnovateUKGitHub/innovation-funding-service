c9ec702d44a027082b06e670694dd5555e1c0151
Ravish Bhagdev <rav@hiveit.co.uk>
2016-05-31 13:17:46 +0100

    Fixed java doc comment

--- ifs-web-service/ifs-core/src/main/java/com/worth/ifs/application/service/ProjectServiceImpl.java
+++ ifs-web-service/ifs-core/src/main/java/com/worth/ifs/application/service/ProjectServiceImpl.java
@@ -1,14 +1,13 @@
 package com.worth.ifs.application.service;
 
-import com.worth.ifs.application.resource.ApplicationResource;
 import com.worth.ifs.project.resource.ProjectResource;
 import com.worth.ifs.project.service.ProjectRestService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
- * This class contains methods to retrieve and store {@link ApplicationResource} related data,
- * through the RestService {@link ApplicationRestService}.
+ * This class contains methods to retrieve and store {@link ProjectResource} related data,
+ * through the RestService {@link ProjectRestService}.
  */
 // TODO DW - INFUND-1555 - get service calls to return rest responses
 @Service
