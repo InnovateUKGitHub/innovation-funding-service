86963ad33b9305e53607ca514d0dc1a52ef2985c
jonathan.lee <jonleeipl@gmail.com>
2016-05-31 14:50:31 +0100

    INFUND-2613 project detail page

--- ifs-web-service/ifs-application-service/src/test/java/com/worth/ifs/project/ProjectDetailsControllerTest.java
+++ ifs-web-service/ifs-application-service/src/test/java/com/worth/ifs/project/ProjectDetailsControllerTest.java
@@ -1,23 +1,40 @@
 package com.worth.ifs.project;
 
-import com.worth.ifs.BaseControllerMockMVCTest;
-import com.worth.ifs.application.resource.ApplicationResource;
-import com.worth.ifs.competition.resource.CompetitionResource;
-import com.worth.ifs.project.resource.ProjectResource;
-import org.junit.Test;
-
 import static com.worth.ifs.application.builder.ApplicationResourceBuilder.newApplicationResource;
 import static com.worth.ifs.competition.builder.CompetitionResourceBuilder.newCompetitionResource;
 import static com.worth.ifs.project.builder.ProjectResourceBuilder.newProjectResource;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
-public class ProjectControllerTest extends BaseControllerMockMVCTest<ProjectController> {
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.MockitoAnnotations;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import com.worth.ifs.BaseControllerMockMVCTest;
+import com.worth.ifs.application.resource.ApplicationResource;
+import com.worth.ifs.competition.resource.CompetitionResource;
+import com.worth.ifs.project.resource.ProjectResource;
+import static java.util.Arrays.asList;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ProjectDetailsControllerTest extends BaseControllerMockMVCTest<ProjectDetailsController> {
+	
+	@Before
+	public void setUp() {
+		super.setUp();
+		setupInvites();
+		loginDefaultUser();
+		loggedInUser.setOrganisations(asList(8L));
+	}
 	
     @Override
-    protected ProjectController supplyControllerUnderTest() {
-        return new ProjectController();
+    protected ProjectDetailsController supplyControllerUnderTest() {
+        return new ProjectDetailsController();
     }
     
     @Test
@@ -32,11 +49,12 @@ public class ProjectControllerTest extends BaseControllerMockMVCTest<ProjectCont
         when(projectService.getById(projectId)).thenReturn(projectResource);
         when(competitionService.getById(applicationResource.getCompetition())).thenReturn(competitionResource);
 
-        mockMvc.perform(get("/project/{id}", projectId))
+        mockMvc.perform(get("/project/{id}/details", projectId))
                 .andExpect(status().isOk())
                 .andExpect(model().attribute("project", projectResource))
                 .andExpect(model().attribute("app", applicationResource))
                 .andExpect(model().attribute("competition", competitionResource))
-                .andExpect(view().name("project/overview"));
+                .andExpect(view().name("project/detail"));
     }
+
 }
