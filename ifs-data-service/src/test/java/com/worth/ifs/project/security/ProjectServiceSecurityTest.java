7ca3b34c7c78f1bf05480bb5096525c9399fa52c
Ravish Bhagdev <rav@hiveit.co.uk>
2016-05-31 16:03:57 +0100

    Adding security checks for creation of project from application

--- ifs-data-service/src/test/java/com/worth/ifs/project/security/ProjectServiceSecurityTest.java
+++ ifs-data-service/src/test/java/com/worth/ifs/project/security/ProjectServiceSecurityTest.java
@@ -8,12 +8,19 @@ import com.worth.ifs.project.transactional.ProjectService;
 import com.worth.ifs.user.resource.UserResource;
 import org.junit.Before;
 import org.junit.Test;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.access.method.P;
 
 import java.util.List;
 import java.util.Map;
 
 import static com.worth.ifs.project.builder.ProjectResourceBuilder.newProjectResource;
+import static com.worth.ifs.user.builder.RoleResourceBuilder.newRoleResource;
+import static com.worth.ifs.user.builder.UserResourceBuilder.newUserResource;
+import static com.worth.ifs.user.resource.UserRoleType.APPLICANT;
+import static com.worth.ifs.user.resource.UserRoleType.COMP_ADMIN;
+import static java.util.Collections.singletonList;
+import static junit.framework.TestCase.fail;
 import static org.mockito.Matchers.isA;
 import static org.mockito.Mockito.*;
 
@@ -44,6 +51,23 @@ public class ProjectServiceSecurityTest extends BaseServiceSecurityTest<ProjectS
                     verify(projectPermissionRules, times(1)).compAdminsCanViewProjects(isA(ProjectResource.class), isA(UserResource.class));
                 }
         );
+    }
+
+    @Test
+    public void test_createProjectFromApplicationAllowedIfCompAdminRole() {
+        setLoggedInUser(newUserResource().withRolesGlobal(singletonList(newRoleResource().withType(COMP_ADMIN).build())).build());
+        service.createProjectFromApplication(123L);
+    }
+
+    @Test
+    public void test_createProjectFromApplicationDeniedForApplicant() {
+        setLoggedInUser(newUserResource().withRolesGlobal(singletonList(newRoleResource().withType(APPLICANT).build())).build());
+        try {
+            service.createProjectFromApplication(123L);
+            fail("Should not have been able to create project from application as applicant");
+        } catch(AccessDeniedException ade){
+            //expected behaviour
+        }
     }
 
     @Override
