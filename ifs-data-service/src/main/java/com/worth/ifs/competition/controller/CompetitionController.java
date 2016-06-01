21d3c158e720e2ebf4b5eb7a433efb6145b1d726
Nico Bijl <nbijl@worth.systems>
2016-06-01 14:29:55 +0200

    INFUND-2945 changed after code review.

--- ifs-data-service/src/main/java/com/worth/ifs/competition/controller/CompetitionController.java
+++ ifs-data-service/src/main/java/com/worth/ifs/competition/controller/CompetitionController.java
@@ -6,12 +6,13 @@ import com.worth.ifs.competition.transactional.CompetitionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 /**
- * ApplicationController exposes Application data and operations through a REST API.
+ * CompetitionController exposes Competition data and operations through a REST API.
  */
 @RestController
 @RequestMapping("/competition")
@@ -33,7 +34,7 @@ public class CompetitionController {
     /**
      * Create a new competition object, and return it.
      */
-    @RequestMapping("/create")
+    @RequestMapping(value = "", method = RequestMethod.POST)
     public RestResult<CompetitionResource> create() {
         return competitionService.create().toGetResponse();
     }
