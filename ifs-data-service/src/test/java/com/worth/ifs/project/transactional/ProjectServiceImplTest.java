563b2487a787b64379859a187794486560f7ebf7
Duncan Watson <duncan@hiveit.co.uk>
2016-06-02 14:40:51 +0100

    INFUND-2614 - adding tests for ProjectService

---
+++ ifs-data-service/src/test/java/com/worth/ifs/project/transactional/ProjectServiceImplTest.java
@@ -0,0 +1,62 @@
+package com.worth.ifs.project.transactional;
+
+import com.worth.ifs.BaseServiceUnitTest;
+import com.worth.ifs.application.domain.Application;
+import com.worth.ifs.commons.service.ServiceResult;
+import com.worth.ifs.project.domain.Project;
+import com.worth.ifs.project.resource.ProjectResource;
+import org.junit.Test;
+
+import java.time.LocalDate;
+
+import static com.worth.ifs.LambdaMatcher.createLambdaMatcher;
+import static com.worth.ifs.application.builder.ApplicationBuilder.newApplication;
+import static com.worth.ifs.project.builder.ProjectBuilder.newProject;
+import static com.worth.ifs.project.builder.ProjectResourceBuilder.newProjectResource;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.when;
+
+public class ProjectServiceImplTest extends BaseServiceUnitTest<ProjectService> {
+
+    @Test
+    public void testCreateProjectFromApplication() {
+
+        Application application = newApplication().
+                withId(123L).
+                withName("My Application").
+                withDurationInMonths(5L).
+                withStartDate(LocalDate.of(2017, 3, 2)).
+                build();
+
+        ProjectResource newProjectResource = newProjectResource().build();
+
+        when(applicationRepositoryMock.findOne(123L)).thenReturn(application);
+
+        Project newProjectExpectations = createProjectExpectationsFromOriginalApplication(application);
+        Project savedProject = newProject().build();
+
+        when(projectRepositoryMock.save(newProjectExpectations)).thenReturn(savedProject);
+        when(projectMapperMock.mapToResource(savedProject)).thenReturn(newProjectResource);
+
+        ServiceResult<ProjectResource> project = service.createProjectFromApplication(123L);
+        assertTrue(project.isSuccess());
+        assertEquals(newProjectResource, project.getSuccessObject());
+    }
+
+    private Project createProjectExpectationsFromOriginalApplication(Application application) {
+
+        return createLambdaMatcher(project -> {
+            assertEquals(application.getId(), project.getId());
+            assertEquals(application.getName(), project.getName());
+            assertEquals(application.getDurationInMonths(), project.getDurationInMonths());
+            assertEquals(application.getStartDate(), project.getTargetStartDate());
+            assertNull(project.getProjectManager());
+            assertNull(project.getAddress());
+        });
+    }
+
+    @Override
+    protected ProjectService supplyServiceUnderTest() {
+        return new ProjectServiceImpl();
+    }
+}
