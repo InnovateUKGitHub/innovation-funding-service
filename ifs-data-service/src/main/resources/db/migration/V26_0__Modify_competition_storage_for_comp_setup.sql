32044df4ee2fefbc5eacbc05100c3a0f74c8e454
Nico Bijl <nbijl@worth.systems>
2016-06-03 15:30:15 +0200

    INFUND-3053 database migration numbers and comments

--- ifs-data-service/src/main/resources/db/migration/V26_0__Modify_competition_storage_for_comp_setup.sql
+++ ifs-data-service/src/main/resources/db/migration/V26_0__Modify_competition_storage_for_comp_setup.sql
@@ -1,42 +1,59 @@
+-- Add competition columns for saving data on competition setup.
+ALTER TABLE competition ADD COLUMN status VARCHAR(45) NULL DEFAULT NULL COMMENT '';
+ALTER TABLE competition ADD COLUMN budget_code VARCHAR(255);
+ALTER TABLE competition ADD COLUMN code VARCHAR(255);
+ALTER TABLE competition ADD COLUMN paf_code VARCHAR(255);
+ALTER TABLE competition ADD COLUMN executive_user_id BIGINT;
+ALTER TABLE competition ADD COLUMN lead_technologist_user_id BIGINT;
+ALTER TABLE competition ADD CONSTRAINT FK_oyhemo48a8fegie1npbk759s2 FOREIGN KEY (executive_user_id) REFERENCES user (id);
+ALTER TABLE competition ADD CONSTRAINT FK_7gcrp1ms5k4o9ehrci3uqx6hg FOREIGN KEY (lead_technologist_user_id) REFERENCES user (id);
 
-ALTER TABLE `competition`
-    ADD COLUMN `status` VARCHAR(45) NULL DEFAULT NULL COMMENT '' ;
 
-alter table competition add column budget_code varchar(255);
-alter table competition add column `code` varchar(255);
-alter table competition add column `paf_code` varchar(255);
-alter table competition add column executive_user_id bigint;
-alter table competition add column lead_technologist_user_id bigint;
-alter table competition add constraint FK_oyhemo48a8fegie1npbk759s2 foreign key (executive_user_id) references user (id);
-alter table competition add constraint FK_7gcrp1ms5k4o9ehrci3uqx6hg foreign key (lead_technologist_user_id) references user (id);
-
+-- Add competition Type.
 CREATE TABLE competition_type (
     id BIGINT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(255),
+    NAME VARCHAR(255),
     PRIMARY KEY (id)
-);
-alter table competition add column competition_type_id bigint;
-alter table competition add constraint FK_4ymkkm30gi0r9w65d1xuawyws foreign key (competition_type_id) references competition_type (id);
+)  ENGINE=INNODB DEFAULT CHARSET=UTF8;
+
+ALTER TABLE competition ADD COLUMN competition_type_id BIGINT;
+ALTER TABLE competition ADD CONSTRAINT FK_4ymkkm30gi0r9w65d1xuawyws FOREIGN KEY (competition_type_id) REFERENCES competition_type (id);
 
 
+-- Add Category structure, for example for Innovation Sector and Innovation Area.
 CREATE TABLE category (
     id BIGINT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(255),
+    NAME VARCHAR(255),
     type VARCHAR(255),
     parent_id BIGINT,
     PRIMARY KEY (id)
-);
+)  ENGINE=INNODB DEFAULT CHARSET=UTF8;
+
 CREATE TABLE category_link (
     id BIGINT NOT NULL AUTO_INCREMENT,
     class_name VARCHAR(255),
     class_pk BIGINT,
     category_id BIGINT,
     PRIMARY KEY (id)
-);
+)  ENGINE=INNODB DEFAULT CHARSET=UTF8;
 
-alter table category_link add constraint UK_3ba9x0jh5patrfe48pcb4twko unique (class_name, class_pk, category_id);
-alter table category add constraint FK_81thrbnb8c08gua7tvqj7xdqk foreign key (parent_id) references category (id);
-alter table category_link add constraint FK_3kai632y5lw33gxmec08p54s0 foreign key (category_id) references category (id);
+ALTER TABLE category_link ADD CONSTRAINT UK_3ba9x0jh5patrfe48pcb4twko UNIQUE (
+	class_name,
+	class_pk,
+	category_id
+	);
 
-create table milestone (id bigint not null auto_increment, date datetime, name varchar(255), competition_id bigint, primary key (id));
-alter table milestone add constraint FK_d2gmngr50hf7lkjv8s9mxhfms foreign key (competition_id) references competition (id);
+ALTER TABLE category ADD CONSTRAINT FK_81thrbnb8c08gua7tvqj7xdqk FOREIGN KEY (parent_id) REFERENCES category (id);
+ALTER TABLE category_link ADD CONSTRAINT FK_3kai632y5lw33gxmec08p54s0 FOREIGN KEY (category_id) REFERENCES category (id);
+
+
+-- Add Competition Milestones
+CREATE TABLE milestone (
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    DATE DATETIME,
+    NAME VARCHAR(255),
+    competition_id BIGINT,
+    PRIMARY KEY (id)
+)  ENGINE=INNODB DEFAULT CHARSET=UTF8;
+
+ALTER TABLE milestone ADD CONSTRAINT FK_d2gmngr50hf7lkjv8s9mxhfms FOREIGN KEY (competition_id) REFERENCES competition (id);
--- ifs-data-service/src/main/resources/db/webtest/V26_2__Add_competition_in_setup_status.sql
+++ ifs-data-service/src/main/resources/db/webtest/V26_2__Add_competition_in_setup_status.sql
@@ -1,14 +1,14 @@
 
 
-insert into competition (id, competition_type_id, status) values (6, 1, 'COMPETITION_SETUP');
+insert into competition (id, competition_type_id, status) values (7, 1, 'COMPETITION_SETUP');
 
 
-INSERT INTO `category_link` (`id`, `class_name`, `class_pk`, `category_id`) VALUES ('1', 'com.worth.ifs.competition.domain.Competition', '6', '1');
-INSERT INTO `category_link` (`id`, `class_name`, `class_pk`, `category_id`) VALUES ('2', 'com.worth.ifs.competition.domain.Competition', '6', '9');
+INSERT INTO `category_link` (`id`, `class_name`, `class_pk`, `category_id`) VALUES ('1', 'com.worth.ifs.competition.domain.Competition', '7', '1');
+INSERT INTO `category_link` (`id`, `class_name`, `class_pk`, `category_id`) VALUES ('2', 'com.worth.ifs.competition.domain.Competition', '7', '9');
 
 
-INSERT INTO `milestone` (`id`, `name`, `competition_id`) VALUES (NULL, 'Opening date', '6');
-INSERT INTO `milestone` (`id`, `name`, `competition_id`) VALUES (NULL, 'Briefing event', '6');
-INSERT INTO `milestone` (`id`, `name`, `competition_id`) VALUES (NULL, 'Submission date', '6');
-INSERT INTO `milestone` (`id`, `name`, `competition_id`) VALUES (NULL, 'Allocate assessors', '6');
-INSERT INTO `milestone` (`id`, `name`, `competition_id`) VALUES (NULL, 'Assessor briefing', '6');
+INSERT INTO `milestone` (`id`, `name`, `competition_id`) VALUES (NULL, 'Opening date', '7');
+INSERT INTO `milestone` (`id`, `name`, `competition_id`) VALUES (NULL, 'Briefing event', '7');
+INSERT INTO `milestone` (`id`, `name`, `competition_id`) VALUES (NULL, 'Submission date', '7');
+INSERT INTO `milestone` (`id`, `name`, `competition_id`) VALUES (NULL, 'Allocate assessors', '7');
+INSERT INTO `milestone` (`id`, `name`, `competition_id`) VALUES (NULL, 'Assessor briefing', '7');
