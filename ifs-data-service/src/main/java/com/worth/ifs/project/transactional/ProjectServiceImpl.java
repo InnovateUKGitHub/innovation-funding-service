b21c5c366010700114956b20d9fa1e55349f0386
Duncan Watson <duncan@hiveit.co.uk>
2016-06-06 11:48:03 +0100

    INFUND-3132 - adding persistence of project manager choice to service

--- ifs-data-service/src/main/java/com/worth/ifs/project/transactional/ProjectServiceImpl.java
+++ ifs-data-service/src/main/java/com/worth/ifs/project/transactional/ProjectServiceImpl.java
@@ -1,23 +1,5 @@
 package com.worth.ifs.project.transactional;
 
-import static com.worth.ifs.commons.error.CommonErrors.notFoundError;
-import static com.worth.ifs.commons.error.CommonFailureKeys.PROJECT_SETUP_DATE_MUST_BE_IN_THE_FUTURE;
-import static com.worth.ifs.commons.error.CommonFailureKeys.PROJECT_SETUP_DATE_MUST_START_ON_FIRST_DAY_OF_MONTH;
-import static com.worth.ifs.commons.error.CommonFailureKeys.PROJECT_MANAGER_MUST_BE_IN_LEAD_ORGANISATION;
-import static com.worth.ifs.commons.service.ServiceResult.serviceFailure;
-import static com.worth.ifs.commons.service.ServiceResult.serviceSuccess;
-import static com.worth.ifs.util.CollectionFunctions.simpleMap;
-import static com.worth.ifs.util.EntityLookupCallbacks.find;
-
-import java.time.LocalDate;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.method.P;
-import org.springframework.stereotype.Service;
-
 import com.worth.ifs.application.domain.Application;
 import com.worth.ifs.application.repository.ApplicationRepository;
 import com.worth.ifs.application.resource.FundingDecision;
@@ -30,6 +12,22 @@ import com.worth.ifs.project.resource.ProjectResource;
 import com.worth.ifs.transactional.BaseTransactionalService;
 import com.worth.ifs.user.domain.Organisation;
 import com.worth.ifs.user.domain.ProcessRole;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.method.P;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Map;
+
+import static com.worth.ifs.commons.error.CommonErrors.notFoundError;
+import static com.worth.ifs.commons.error.CommonFailureKeys.*;
+import static com.worth.ifs.commons.service.ServiceResult.serviceFailure;
+import static com.worth.ifs.commons.service.ServiceResult.serviceSuccess;
+import static com.worth.ifs.util.CollectionFunctions.simpleFilter;
+import static com.worth.ifs.util.CollectionFunctions.simpleMap;
+import static com.worth.ifs.util.EntityLookupCallbacks.find;
+import static com.worth.ifs.util.EntityLookupCallbacks.getOnlyElementOrFail;
 
 @Service
 public class ProjectServiceImpl extends BaseTransactionalService implements ProjectService {
@@ -62,10 +60,7 @@ public class ProjectServiceImpl extends BaseTransactionalService implements Proj
 	public ServiceResult<Void> setProjectManager(Long projectId, Long projectManagerId) {
 		 return getProject(projectId).
 				  andOnSuccess(project -> validateProjectManager(project, projectManagerId).
-				  andOnSuccess(() -> {
-					  //TODO set something on the project here
-					  project.setProjectManager(null);
-				  }));
+				  andOnSuccessReturnVoid(project::setProjectManager));
 	}
 
 	@Override
@@ -94,13 +89,16 @@ public class ProjectServiceImpl extends BaseTransactionalService implements Proj
         return serviceSuccess();
     }
     
-    private ServiceResult<Void> validateProjectManager(Project project, Long projectManagerId) {
-		Application application = applicationRepository.findOne(project.getId());
+    private ServiceResult<ProcessRole> validateProjectManager(Project project, Long projectManagerId) {
+
+        Application application = applicationRepository.findOne(project.getId());
 		Organisation leadPartner = application.getLeadOrganisation();
-		List<ProcessRole> leadPartnerProcessRoles = application.getProcessRoles().stream().filter(pr -> leadPartner.equals(pr.getOrganisation())).collect(Collectors.toList());
-		boolean match = leadPartnerProcessRoles.stream().anyMatch(lppr -> projectManagerId.equals(lppr.getUser().getId()));
-		if(match) {
-			return serviceSuccess();
+
+        List<ProcessRole> leadPartnerProcessRoles = simpleFilter(application.getProcessRoles(), pr -> leadPartner.equals(pr.getOrganisation()));
+        List<ProcessRole> matchingProcessRoles = simpleFilter(leadPartnerProcessRoles, lppr -> projectManagerId.equals(lppr.getUser().getId()));
+
+        if(!matchingProcessRoles.isEmpty()) {
+			return getOnlyElementOrFail(matchingProcessRoles).andOnSuccess(processRole -> serviceSuccess(processRole));
 		} else {
 			return serviceFailure(new Error(PROJECT_MANAGER_MUST_BE_IN_LEAD_ORGANISATION));
 		}
