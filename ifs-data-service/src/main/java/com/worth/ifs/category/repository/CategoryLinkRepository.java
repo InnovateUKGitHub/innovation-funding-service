4f9c1196e36620102928e15c33121e0e8d156117
Nico Bijl <nbijl@worth.systems>
2016-06-10 14:48:35 +0200

    INFUND-2982 code cleanup

--- ifs-data-service/src/main/java/com/worth/ifs/category/repository/CategoryLinkRepository.java
+++ ifs-data-service/src/main/java/com/worth/ifs/category/repository/CategoryLinkRepository.java
@@ -3,12 +3,11 @@ package com.worth.ifs.category.repository;
 import com.worth.ifs.category.domain.CategoryLink;
 import com.worth.ifs.category.resource.CategoryType;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 
 
 public interface CategoryLinkRepository extends CrudRepository<CategoryLink, Long> {
 
-    CategoryLink findByClassNameAndClassPk(@Param("className") String className, @Param("classPk") Long classPk);
-    CategoryLink findByClassNameAndClassPkAndCategory_Type(@Param("className") String className, @Param("classPk") Long classPk, @Param("type") CategoryType type);
+    CategoryLink findByClassNameAndClassPk(String className, Long classPk);
+    CategoryLink findByClassNameAndClassPkAndCategory_Type(String className, Long classPk, CategoryType type);
 
 }
--- ifs-data-service/src/main/java/com/worth/ifs/category/repository/CategoryRepository.java
+++ ifs-data-service/src/main/java/com/worth/ifs/category/repository/CategoryRepository.java
@@ -3,12 +3,11 @@ package com.worth.ifs.category.repository;
 import com.worth.ifs.category.domain.Category;
 import com.worth.ifs.category.resource.CategoryType;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 
 public interface CategoryRepository extends CrudRepository<Category, Long> {
-    Category findByTypeAndCategoryLinks_ClassNameAndCategoryLinks_ClassPk(@Param("type") CategoryType type, @Param("className") String className, @Param("classPk") Long classPk);
-    List<Category> findByType(@Param("type") CategoryType type);
+    Category findByTypeAndCategoryLinks_ClassNameAndCategoryLinks_ClassPk(CategoryType type, String className, Long classPk);
+    List<Category> findByType(CategoryType type);
 }
