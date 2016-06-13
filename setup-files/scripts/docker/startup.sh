e6c58e91dfd86fe41e23b192309c1eb70e51c4d9
richardpopple <richard@hiveit.co.uk>
2016-06-13 14:55:14 +0100

    INFUND-3094 fixing some errors in the linux scripts

--- setup-files/scripts/docker/startup.sh
+++ setup-files/scripts/docker/startup.sh
@@ -29,8 +29,8 @@ setHostFile(){
             ;;
         linux*)
             cp /etc/hosts /tmp/hostsbackup
-            ip_address=`docker inspect --format '{{ .NetworkSettings.IPAddress }}' ifs-local-dev`
-            database_ip_address=`docker inspect --format '{{ .NetworkSettings.IPAddress }}' ifs-data-service`
+            ip_address=`docker inspect --format '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' ifs-local-dev`
+            database_ip_address=`docker inspect --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' ifs-database`
             cat /etc/hosts | grep -v 'ifs-local-dev' | grep -v 'iuk-auth-localdev' > /tmp/temphosts
             echo "$ip_address  iuk-auth-localdev" >> /tmp/temphosts
             echo "$ip_address  ifs-local-dev" >> /tmp/temphosts
@@ -56,6 +56,7 @@ installShib
 docker-compose up -d
 sleep 2
 docker-compose exec mysql mysql -uroot -ppassword -e 'create database ifs_test'
+docker-compose exec mysql mysql -uroot -ppassword -e 'create database ifs'
 setHostFile
 ./gradlew -Pprofile=docker flywayClean flywayMigrate
 cd setup-files/scripts/docker
