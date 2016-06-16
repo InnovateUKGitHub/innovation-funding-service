ecdc05eefc0ee1ed83dd9cdcb65d0668ec1558f8
Reinaud van Rumpt <rvanrumpt@worth.systems>
2016-06-16 12:03:29 +0200

    INFUND-2982 Category and CategoryLink repository unit tests

--- ifs-data-service/src/main/java/com/worth/ifs/category/repository/CategoryLinkRepository.java
+++ ifs-data-service/src/main/java/com/worth/ifs/category/repository/CategoryLinkRepository.java
@@ -4,10 +4,12 @@ import com.worth.ifs.category.domain.CategoryLink;
 import com.worth.ifs.category.resource.CategoryType;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 
 public interface CategoryLinkRepository extends CrudRepository<CategoryLink, Long> {
 
-    CategoryLink findByClassNameAndClassPk(String className, Long classPk);
+    List<CategoryLink> findByClassNameAndClassPk(String className, Long classPk);
     CategoryLink findByClassNameAndClassPkAndCategory_Type(String className, Long classPk, CategoryType type);
 
 }
