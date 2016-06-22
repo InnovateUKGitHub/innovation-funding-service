6fe7b744cbad3e5f8864dfa08ba2190a3271f8ed
Reinaud van Rumpt <rvanrumpt@worth.systems>
2016-06-22 14:19:35 +0200

    INFUND-3652 Fill the correct innovation areas

--- ifs-data-service/src/main/resources/db/setup/V31_1__Add_competition_status_values.sql
+++ ifs-data-service/src/main/resources/db/setup/V31_1__Add_competition_status_values.sql
@@ -7,19 +7,41 @@ INSERT INTO `competition_type` (`name`, `state_aid`) VALUES ('Additive Manufactu
 -- ADD INNOVATION_SECTOR
 INSERT INTO `category` (id, `name`, `type`) VALUES (1, 'Health and life sciences', 'INNOVATION_SECTOR');
 INSERT INTO `category` (id, `name`, `type`) VALUES (2, 'Materials and manufacturing', 'INNOVATION_SECTOR');
-INSERT INTO `category` (id, `name`, `type`) VALUES (3, 'Emerging and enabling', 'INNOVATION_SECTOR');
-INSERT INTO `category` (id, `name`, `type`) VALUES (4, 'Infrastructure', 'INNOVATION_SECTOR');
+INSERT INTO `category` (id, `name`, `type`) VALUES (3, 'Emerging and enabling technologies', 'INNOVATION_SECTOR');
+INSERT INTO `category` (id, `name`, `type`) VALUES (4, 'Infrastructure systems', 'INNOVATION_SECTOR');
 
 -- ADD INNOVATION_AREA
-INSERT INTO `category` (`name`, `type`, `parent_id`) VALUES ('Transport', 'INNOVATION_AREA', '3');
-INSERT INTO `category` (`name`, `type`, `parent_id`) VALUES ('Energy', 'INNOVATION_AREA', '3');
-INSERT INTO `category` (`name`, `type`, `parent_id`) VALUES ('Urban living and built', 'INNOVATION_AREA', '3');
-INSERT INTO `category` (`name`, `type`, `parent_id`) VALUES ('Health and care', 'INNOVATION_AREA', '1');
-INSERT INTO `category` (`name`, `type`, `parent_id`) VALUES ('Agriculture and food', 'INNOVATION_AREA', '1');
-INSERT INTO `category` (`name`, `type`, `parent_id`) VALUES ('Biosciences', 'INNOVATION_AREA', '1');
-INSERT INTO `category` (`name`, `type`, `parent_id`) VALUES ('High value manufacturing', 'INNOVATION_AREA', '2');
+INSERT INTO `category` (`name`, `type`, `parent_id`) VALUES ('Earth Observation', 'INNOVATION_AREA', '3');
+INSERT INTO `category` (`name`, `type`, `parent_id`) VALUES ('Internet of Things', 'INNOVATION_AREA', '3');
+INSERT INTO `category` (`name`, `type`, `parent_id`) VALUES ('Data', 'INNOVATION_AREA', '3');
+INSERT INTO `category` (`name`, `type`, `parent_id`) VALUES ('Cyber Security', 'INNOVATION_AREA', '3');
+INSERT INTO `category` (`name`, `type`, `parent_id`) VALUES ('User Experience', 'INNOVATION_AREA', '3');
+INSERT INTO `category` (`name`, `type`, `parent_id`) VALUES ('Emerging Tech and Industries', 'INNOVATION_AREA', '3');
+INSERT INTO `category` (`name`, `type`, `parent_id`) VALUES ('Robotics and AS', 'INNOVATION_AREA', '3');
+INSERT INTO `category` (`name`, `type`, `parent_id`) VALUES ('Electronics, Sensors and photonics', 'INNOVATION_AREA', '3');
+INSERT INTO `category` (`name`, `type`, `parent_id`) VALUES ('Open', 'INNOVATION_AREA', '3');
+INSERT INTO `category` (`name`, `type`, `parent_id`) VALUES ('Design', 'INNOVATION_AREA', '3');
+INSERT INTO `category` (`name`, `type`, `parent_id`) VALUES ('Creative economy', 'INNOVATION_AREA', '3');
+
+INSERT INTO `category` (`name`, `type`, `parent_id`) VALUES ('Advanced Therapies', 'INNOVATION_AREA', '1');
+INSERT INTO `category` (`name`, `type`, `parent_id`) VALUES ('Precision Medicine', 'INNOVATION_AREA', '1');
+INSERT INTO `category` (`name`, `type`, `parent_id`) VALUES ('Medicines Technology', 'INNOVATION_AREA', '1');
+INSERT INTO `category` (`name`, `type`, `parent_id`) VALUES ('Bioscience', 'INNOVATION_AREA', '1');
+INSERT INTO `category` (`name`, `type`, `parent_id`) VALUES ('Agri Productivity', 'INNOVATION_AREA', '1');
+INSERT INTO `category` (`name`, `type`, `parent_id`) VALUES ('Enhanced Food Quality', 'INNOVATION_AREA', '1');
+
+INSERT INTO `category` (`name`, `type`, `parent_id`) VALUES ('Digital Manufacturing', 'INNOVATION_AREA', '2');
+INSERT INTO `category` (`name`, `type`, `parent_id`) VALUES ('Early Stage Manufacturing', 'INNOVATION_AREA', '2');
+INSERT INTO `category` (`name`, `type`, `parent_id`) VALUES ('Manufacturing Readiness', 'INNOVATION_AREA', '2');
 INSERT INTO `category` (`name`, `type`, `parent_id`) VALUES ('Resource efficiency', 'INNOVATION_AREA', '2');
-INSERT INTO `category` (`name`, `type`, `parent_id`) VALUES ('Advanced materials', 'INNOVATION_AREA', '2');
+INSERT INTO `category` (`name`, `type`, `parent_id`) VALUES ('Advanced Materials', 'INNOVATION_AREA', '2');
+
+INSERT INTO `category` (`name`, `type`, `parent_id`) VALUES ('Transport Systems', 'INNOVATION_AREA', '4');
+INSERT INTO `category` (`name`, `type`, `parent_id`) VALUES ('Urban living', 'INNOVATION_AREA', '4');
+INSERT INTO `category` (`name`, `type`, `parent_id`) VALUES ('Infrastructure', 'INNOVATION_AREA', '4');
+INSERT INTO `category` (`name`, `type`, `parent_id`) VALUES ('Energy Systems', 'INNOVATION_AREA', '4');
+INSERT INTO `category` (`name`, `type`, `parent_id`) VALUES ('Nuclear', 'INNOVATION_AREA', '4');
+INSERT INTO `category` (`name`, `type`, `parent_id`) VALUES ('Offshore Renewable Energy', 'INNOVATION_AREA', '4');
 
 
 INSERT INTO `role` (`id`, `name`) VALUES (12, 'competition_executive');
