0a6c6722a76ce43b46e8887ee23dbb6de446452e
Ravish Bhagdev <rav@hiveit.co.uk>
2016-06-30 12:36:29 +0100

    Adding experian fields to bank details

---
+++ ifs-data-service/src/main/resources/db/migration/V40_1__Add_experian_fields_to_bank_details.sql
@@ -0,0 +1,6 @@
+ALTER TABLE `bank_detail`
+  ADD COLUMN `company_name_score` TINYINT(1) NULL AFTER `organisation_address_id`,
+  ADD COLUMN `company_number_score` TINYINT(1) NULL AFTER `company_name_score`,
+  ADD COLUMN `account_number_score` TINYINT(1) NULL AFTER `company_number_score`,
+  ADD COLUMN `address_score` TINYINT(1) NULL AFTER `account_number_score`,
+  ADD COLUMN `manual_approval` TINYINT(1) NULL AFTER `address_score`;
