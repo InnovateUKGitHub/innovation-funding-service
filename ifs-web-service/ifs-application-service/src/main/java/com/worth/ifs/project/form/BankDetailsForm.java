3950adb921c65883547f08445f884c077ac13ae8
Brent de Kok <brent@worth.systems>
2016-07-01 15:55:35 +0200

    INFUND-3010 Bank Details, validation message

--- ifs-web-service/ifs-application-service/src/main/java/com/worth/ifs/project/form/BankDetailsForm.java
+++ ifs-web-service/ifs-application-service/src/main/java/com/worth/ifs/project/form/BankDetailsForm.java
@@ -6,11 +6,11 @@ import org.hibernate.validator.constraints.NotEmpty;
 import javax.validation.constraints.Pattern;
 
 public class BankDetailsForm extends ProjectDetailsAddressViewModelForm {
-    @NotEmpty(message="Please enter a valid sort code")
+    @NotEmpty(message="Please enter a sort code")
     @Pattern(regexp = "\\d{6}", message = "Please enter a valid sort code")
     private String sortCode;
 
-    @NotEmpty(message="Please enter a valid account number")
+    @NotEmpty(message="Please enter an account number")
     @Pattern(regexp = "\\d{8}", message = "Please enter a valid account number")
     private String accountNumber;
 
