60da7f7f8ba76f543ede4368d41b77806828353f
Joe Hajas <jhajas@worth.systems>
2016-07-01 13:58:50 +0100

    INFUND-2707 As an applicant I want to get validation errors when I mark as complete the application details page with invalid inputs Removed db script

--- ifs-data-service/src/main/resources/db/migration/V50_0__Add_application_mark_as_complete_validator.sql
+++
@@ -1,6 +0,0 @@
-/** New mark as complete validator for Application details section **/
-
-INSERT IGNORE INTO `form_validator` (`id`, `clazz_name`, `title`) VALUES (4,'com.worth.ifs.validator.ApplicationMarkAsCompleteValidator','ApplicationMarkAsCompleteValidator');
-INSERT IGNORE INTO `form_input_validator` (`form_input_id`, `form_validator_id` ) VALUES (9, 4);
-
-
