2d8856efce84c76b1dc6e37c126f98b7abde405b
Duncan Watson <duncan@hiveit.co.uk>
2016-07-08 10:12:48 +0100

    INFUND-3403 - comp failure in test fix

--- ifs-web-service/ifs-application-service/src/test/java/com/worth/ifs/form/service/FormInputResponseServiceImplTest.java
+++ ifs-web-service/ifs-application-service/src/test/java/com/worth/ifs/form/service/FormInputResponseServiceImplTest.java
@@ -1,7 +1,6 @@
 package com.worth.ifs.form.service;
 
 import com.worth.ifs.BaseUnitTestMocksTest;
-import com.worth.ifs.commons.error.Error;
 import com.worth.ifs.commons.rest.ValidationMessages;
 import com.worth.ifs.form.resource.FormInputResponseResource;
 import org.junit.Test;
@@ -14,7 +13,6 @@ import java.util.Map;
 import static com.worth.ifs.commons.error.Error.fieldError;
 import static com.worth.ifs.commons.rest.RestResult.restSuccess;
 import static com.worth.ifs.form.builder.FormInputResponseResourceBuilder.newFormInputResponseResource;
-import static java.util.Arrays.asList;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.when;
 import static org.springframework.http.HttpStatus.NOT_ACCEPTABLE;
@@ -56,7 +54,7 @@ public class FormInputResponseServiceImplTest extends BaseUnitTestMocksTest {
     @Test
     public void test_save() {
 
-        ValidationMessages validation = new ValidationMessages(fieldError("value", "an error", NOT_ACCEPTABLE), fieldError("value", "another error", NOT_ACCEPTABLE)));
+        ValidationMessages validation = new ValidationMessages(fieldError("value", "an error", NOT_ACCEPTABLE), fieldError("value", "another error", NOT_ACCEPTABLE));
 
         when(restServiceMock.saveQuestionResponse(123L, 456L, 789L, "A new value", false)).
                 thenReturn(restSuccess(validation));
