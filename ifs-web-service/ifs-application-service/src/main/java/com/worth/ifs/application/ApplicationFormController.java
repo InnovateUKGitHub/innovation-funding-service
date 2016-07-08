e9b1b2fe941d4e9ad5078866f39127974d669c7a
Joe Hajas <jhajas@worth.systems>
2016-07-08 10:31:22 +0100

    INFUND-3866 Duration field saves prev value when is emptied instead of showing an error

--- ifs-web-service/ifs-application-service/src/main/java/com/worth/ifs/application/ApplicationFormController.java
+++ ifs-web-service/ifs-application-service/src/main/java/com/worth/ifs/application/ApplicationFormController.java
@@ -805,8 +805,9 @@ public class ApplicationFormController extends AbstractApplicationController {
             }
         } else if (fieldName.startsWith("application.durationInMonths")) {
             Long durationInMonth = Long.valueOf(value);
-            if (durationInMonth == null || durationInMonth > 36L) {
+            if (durationInMonth < 1 || durationInMonth > 36L) {
                 errors.add("Please enter a valid duration between 1 and 36 months");
+                application.setDurationInMonths(durationInMonth);
             } else {
                 application.setDurationInMonths(durationInMonth);
                 applicationService.save(application);
