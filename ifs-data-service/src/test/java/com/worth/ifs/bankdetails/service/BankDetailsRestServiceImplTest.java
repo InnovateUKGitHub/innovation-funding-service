aaee92136b0770f12d75addbc6d6bee2ecc1e410
Ravish Bhagdev <rav@hiveit.co.uk>
2016-07-11 10:10:04 +0100

    Adding update bank details rest service unit test

--- ifs-data-service/src/test/java/com/worth/ifs/bankdetails/service/BankDetailsRestServiceImplTest.java
+++ ifs-data-service/src/test/java/com/worth/ifs/bankdetails/service/BankDetailsRestServiceImplTest.java
@@ -2,11 +2,14 @@ package com.worth.ifs.bankdetails.service;
 
 import com.worth.ifs.BaseRestServiceUnitTest;
 import com.worth.ifs.bankdetails.resource.BankDetailsResource;
+import com.worth.ifs.commons.rest.RestResult;
 import org.junit.Test;
 import org.springframework.test.util.ReflectionTestUtils;
 
 import static com.worth.ifs.bankdetails.builder.BankDetailsResourceBuilder.newBankDetailsResource;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.springframework.http.HttpStatus.OK;
 
 public class BankDetailsRestServiceImplTest extends BaseRestServiceUnitTest<BankDetailsRestServiceImpl> {
     final String projectRestURL = "/project";
@@ -35,5 +38,14 @@ public class BankDetailsRestServiceImplTest extends BaseRestServiceUnitTest<Bank
         setupGetWithRestResultExpectations(projectRestURL + "/" + projectId + "/bank-details?organisationId=" + organisationId, BankDetailsResource.class, returnedResponse);
         BankDetailsResource response = service.getBankDetailsByProjectAndOrganisation(projectId, organisationId).getSuccessObject();
         assertEquals(response, returnedResponse);
+    }
+
+    @Test
+    public void testUpdateBankDetails(){
+        Long projectId = 123L;
+        BankDetailsResource bankDetailsResource = newBankDetailsResource().build();
+        setupPostWithRestResultExpectations(projectRestURL + "/" + projectId + "/bank-details", bankDetailsResource, OK);
+        RestResult result = service.updateBankDetails(projectId, bankDetailsResource);
+        assertTrue(result.isSuccess());
     }
 }
