76718f9d8c77307aad3821919d88d53e288fdbd2
gauld <gauld@worth.systems>
2016-07-15 15:17:41 +0100

    IFUND-3809 Data services for Assessment status updates

--- ifs-data-service/src/main/java/com/worth/ifs/assessment/controller/AssessmentController.java
+++ ifs-data-service/src/main/java/com/worth/ifs/assessment/controller/AssessmentController.java
@@ -3,12 +3,15 @@ package com.worth.ifs.assessment.controller;
 import com.worth.ifs.assessment.resource.AssessmentResource;
 import com.worth.ifs.assessment.transactional.AssessmentService;
 import com.worth.ifs.commons.rest.RestResult;
+import com.worth.ifs.workflow.domain.ProcessOutcome;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import static org.springframework.web.bind.annotation.RequestMethod.PUT;
 
 /**
  * Exposes CRUD operations through a REST API to manage {@link com.worth.ifs.assessment.domain.Assessment} related data.
@@ -23,6 +26,11 @@ public class AssessmentController {
     @RequestMapping(value= "/{id}", method = GET)
     public RestResult<AssessmentResource> findById(@PathVariable("id") final Long id) {
         return assessmentService.findById(id).toGetResponse();
+    }
+
+    @RequestMapping(value= "/{id}", method = PUT)
+    public RestResult<Void> updateStatus(@PathVariable("id") final Long id,@RequestBody final ProcessOutcome processOutcome) {
+        return assessmentService.updateStatus(id, processOutcome).toPutResponse();
     }
 
 }
--- ifs-data-service/src/main/java/com/worth/ifs/assessment/transactional/AssessmentServiceImpl.java
+++ ifs-data-service/src/main/java/com/worth/ifs/assessment/transactional/AssessmentServiceImpl.java
@@ -3,13 +3,17 @@ package com.worth.ifs.assessment.transactional;
 import com.worth.ifs.assessment.domain.Assessment;
 import com.worth.ifs.assessment.mapper.AssessmentMapper;
 import com.worth.ifs.assessment.repository.AssessmentRepository;
+import com.worth.ifs.assessment.resource.AssessmentOutcomes;
 import com.worth.ifs.assessment.resource.AssessmentResource;
+import com.worth.ifs.assessment.workflow.AssessmentWorkflowEventHandler;
 import com.worth.ifs.commons.service.ServiceResult;
 import com.worth.ifs.transactional.BaseTransactionalService;
+import com.worth.ifs.workflow.domain.ProcessOutcome;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import static com.worth.ifs.commons.error.CommonErrors.notFoundError;
+import static com.worth.ifs.commons.service.ServiceResult.serviceSuccess;
 import static com.worth.ifs.util.EntityLookupCallbacks.find;
 
 /**
@@ -24,8 +28,22 @@ public class AssessmentServiceImpl extends BaseTransactionalService implements A
     @Autowired
     private AssessmentMapper assessmentMapper;
 
+    @Autowired
+    private AssessmentWorkflowEventHandler assessmentWorkflowEventHandler;
+
     @Override
     public ServiceResult<AssessmentResource> findById(final Long id) {
         return find(assessmentRepository.findOne(id), notFoundError(Assessment.class, id)).andOnSuccessReturn(assessmentMapper::mapToResource);
+    }
+
+    @Override
+    public ServiceResult<Void> updateStatus(Long assessmentId, ProcessOutcome processOutcome) {
+        Assessment assessment = assessmentRepository.findOne(assessmentId);
+
+        if (processOutcome.getOutcomeType().equals(AssessmentOutcomes.REJECT.getType())) {
+            assessmentWorkflowEventHandler.rejectInvitation(assessment.getProcessRole().getId(), assessment.getProcessStatus(), processOutcome);
+        }
+
+        return serviceSuccess();
     }
 }
