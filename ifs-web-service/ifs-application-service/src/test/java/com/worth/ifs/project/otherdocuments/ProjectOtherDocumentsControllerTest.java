bcea3b82c9edff0a59d05941cd7c4f9ba56c310e
Duncan Watson <duncan@hiveit.co.uk>
2016-07-19 13:38:38 +0100

    INFUND-3420 - adding tests for web controller

--- ifs-web-service/ifs-application-service/src/test/java/com/worth/ifs/project/otherdocuments/ProjectOtherDocumentsControllerTest.java
+++ ifs-web-service/ifs-application-service/src/test/java/com/worth/ifs/project/otherdocuments/ProjectOtherDocumentsControllerTest.java
@@ -1,6 +1,8 @@
 package com.worth.ifs.project.otherdocuments;
 
 import com.worth.ifs.BaseControllerMockMVCTest;
+import com.worth.ifs.file.controller.viewmodel.FileDetailsViewModel;
+import com.worth.ifs.file.resource.FileEntryResource;
 import com.worth.ifs.project.otherdocuments.controller.ProjectOtherDocumentsController;
 import com.worth.ifs.project.otherdocuments.form.ProjectOtherDocumentsForm;
 import com.worth.ifs.project.otherdocuments.viewmodel.ProjectOtherDocumentsViewModel;
@@ -12,6 +14,7 @@ import org.springframework.test.web.servlet.MvcResult;
 import java.util.List;
 import java.util.Optional;
 
+import static com.worth.ifs.file.resource.builders.FileEntryResourceBuilder.newFileEntryResource;
 import static com.worth.ifs.project.builder.ProjectResourceBuilder.newProjectResource;
 import static com.worth.ifs.user.builder.OrganisationResourceBuilder.newOrganisationResource;
 import static com.worth.ifs.util.CollectionFunctions.simpleMap;
@@ -32,8 +35,6 @@ public class ProjectOtherDocumentsControllerTest extends BaseControllerMockMVCTe
         List<OrganisationResource> partnerOrganisations = newOrganisationResource().build(3);
 
         when(projectService.getById(projectId)).thenReturn(project);
-
-
         when(projectService.getCollaborationAgreementFileDetails(projectId)).thenReturn(Optional.empty());
         when(projectService.getExploitationPlanFileDetails(projectId)).thenReturn(Optional.empty());
         when(projectService.getPartnerOrganisationsForProject(projectId)).thenReturn(partnerOrganisations);
@@ -79,8 +80,6 @@ public class ProjectOtherDocumentsControllerTest extends BaseControllerMockMVCTe
         List<OrganisationResource> partnerOrganisations = newOrganisationResource().build(3);
 
         when(projectService.getById(projectId)).thenReturn(project);
-
-
         when(projectService.getCollaborationAgreementFileDetails(projectId)).thenReturn(Optional.empty());
         when(projectService.getExploitationPlanFileDetails(projectId)).thenReturn(Optional.empty());
         when(projectService.getPartnerOrganisationsForProject(projectId)).thenReturn(partnerOrganisations);
@@ -111,6 +110,57 @@ public class ProjectOtherDocumentsControllerTest extends BaseControllerMockMVCTe
         assertFalse(model.isShowDocumentsBeingReviewedMessage());
         assertFalse(model.isShowRejectionMessages());
         assertFalse(model.isShowSubmitDocumentsButton());
+
+        // test the form for the file uploads
+        assertNull(form.getCollaborationAgreement());
+        assertNull(form.getExploitationPlan());
+    }
+
+    @Test
+    public void testViewOtherDocumentsPageWithExistingDocuments() throws Exception {
+
+        long projectId = 123L;
+
+        ProjectResource project = newProjectResource().withId(projectId).build();
+        List<OrganisationResource> partnerOrganisations = newOrganisationResource().build(3);
+
+        FileEntryResource existingCollaborationAgreement = newFileEntryResource().build();
+        FileEntryResource existingExplotationPlan = newFileEntryResource().build();
+
+        when(projectService.getById(projectId)).thenReturn(project);
+        when(projectService.getCollaborationAgreementFileDetails(projectId)).thenReturn(Optional.of(existingCollaborationAgreement));
+        when(projectService.getExploitationPlanFileDetails(projectId)).thenReturn(Optional.of(existingExplotationPlan));
+        when(projectService.getPartnerOrganisationsForProject(projectId)).thenReturn(partnerOrganisations);
+        when(projectService.isUserLeadPartner(projectId, loggedInUser.getId())).thenReturn(true);
+
+        MvcResult result = mockMvc.perform(get("/project/123/other-documents")).
+                andExpect(view().name("project/other-documents")).
+                andReturn();
+
+        ProjectOtherDocumentsViewModel model = (ProjectOtherDocumentsViewModel) result.getModelAndView().getModel().get("model");
+        ProjectOtherDocumentsForm form = (ProjectOtherDocumentsForm) result.getModelAndView().getModel().get("form");
+
+        FileDetailsViewModel expectedCollaborationAgreement = new FileDetailsViewModel(existingCollaborationAgreement);
+        FileDetailsViewModel expectedExploitationPlan = new FileDetailsViewModel(existingExplotationPlan);
+
+        // test the view model
+        assertEquals(project.getId(), model.getProjectId());
+        assertEquals(project.getName(), model.getProjectName());
+        assertEquals(expectedCollaborationAgreement, model.getCollaborationAgreementFileDetails());
+        assertEquals(expectedExploitationPlan, model.getExploitationPlanFileDetails());
+        assertEquals(emptyList(), model.getRejectionReasons());
+        assertEquals(simpleMap(partnerOrganisations, OrganisationResource::getName),
+                model.getPartnerOrganisationNames());
+
+        // test flags that help to drive the page
+        assertFalse(model.isReadOnly());
+        assertTrue(model.isEditable());
+        assertTrue(model.isLeadPartner());
+        assertTrue(model.isShowLeadPartnerGuidanceInformation());
+        assertFalse(model.isShowApprovedMessage());
+        assertFalse(model.isShowDocumentsBeingReviewedMessage());
+        assertFalse(model.isShowRejectionMessages());
+        assertTrue(model.isShowSubmitDocumentsButton());
 
         // test the form for the file uploads
         assertNull(form.getCollaborationAgreement());
