98a221963dfd565428b3e75a6dcf2485912b6512
Ravish Bhagdev <rav@hiveit.co.uk>
2016-07-22 11:08:45 +0100

    Adding a minor comment explaining placeholder thresholds

--- ifs-data-service/src/main/java/com/worth/ifs/bankdetails/resource/BankDetailsResource.java
+++ ifs-data-service/src/main/java/com/worth/ifs/bankdetails/resource/BankDetailsResource.java
@@ -112,6 +112,8 @@ public class BankDetailsResource {
 
     @JsonIgnore
     public boolean isApproved(){
+        // Note that this criteria is temporary and will be adusted when we decide on thresholds.
+        // It will likely be moved into a property file so it can be adjusted without code change.
         return manualApproval || (verified && registrationNumberMatched && companyNameScore > 6 && addressScore > 6);
     }
 
