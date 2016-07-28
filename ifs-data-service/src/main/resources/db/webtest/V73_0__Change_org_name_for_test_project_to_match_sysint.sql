00ca016183b7d15036916d019d71da972f230ce6
Ravish Bhagdev <rav@hiveit.co.uk>
2016-07-28 11:38:15 +0100

    Adding missing application finance record for newly added academic org (eggs)

--- ifs-data-service/src/main/resources/db/webtest/V73_0__Change_org_name_for_test_project_to_match_sysint.sql
+++ ifs-data-service/src/main/resources/db/webtest/V73_0__Change_org_name_for_test_project_to_match_sysint.sql
@@ -9,8 +9,21 @@ SELECT @applicationId := application_id FROM project WHERE id = @projectId;
 SELECT @peteTomId:= id FROM `user` WHERE `email` = 'pete.tom@egg.com';
 SELECT @eggsOrganisationId := id FROM organisation where name = 'EGGS';
 
+-- Make EGGS organisation
+
 -- Make Pete Tom a collaborator on "best riffs"
 INSERT IGNORE INTO `process_role` (`application_id`, `organisation_id`, `role_id`, `user_id`) VALUES (@applicationId, @eggsOrganisationId, @collaboratorRoleId, @peteTomId);
 
 -- Make Pete Tom a partner on "best riffs"
 INSERT IGNORE INTO `project_user` (`project_id`, `organisation_id`, `role_id`, `user_id`) VALUES (@projectId, @eggsOrganisationId, @partnerRoleId, @peteTomId);
+
+-- Insert missing application finance for new organisation(s) now added to project in steps above.
+-- NOTE: This is just re-execution of V39_5 because we are adding another org here.
+INSERT IGNORE INTO application_finance (application_id, organisation_id, organisation_size, finance_file_entry_id)
+  SELECT a.id, pu.organisation_id, 'SMALL', null FROM application a
+    JOIN project proj ON proj.application_id = a.id
+    JOIN project_user pu ON pu.project_id = proj.id
+  WHERE NOT EXISTS (
+      SELECT 1 FROM application_finance a2
+      WHERE a2.application_id = a.id AND a2.organisation_id = pu.organisation_id
+  );
