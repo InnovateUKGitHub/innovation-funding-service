dd89f327664e86744f9da604eea5812fbcf97ba0
Bronnyl Dias <bdias@worth.systems>
2016-07-29 19:00:43 +0100

    INFUND-3970-Partner-Spend-Profile-Page
    Added Junits for the ProjectSpendProfileController.
    
    Change-Id: I706573d9dcc022911456e5c7b6b659882ce42845

--- ifs-web-service/ifs-project-setup-service/src/main/java/com/worth/ifs/project/viewmodel/ProjectSpendProfileViewModel.java
+++ ifs-web-service/ifs-project-setup-service/src/main/java/com/worth/ifs/project/viewmodel/ProjectSpendProfileViewModel.java
@@ -1,6 +1,8 @@
 package com.worth.ifs.project.viewmodel;
 
 import com.worth.ifs.competition.resource.CompetitionResource;
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
 
 import java.time.LocalDate;
 
@@ -36,4 +38,28 @@ public class ProjectSpendProfileViewModel {
     public CompetitionResource getCompetition() {
         return competition;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+
+        if (o == null || getClass() != o.getClass()) return false;
+
+        ProjectSpendProfileViewModel viewModel = (ProjectSpendProfileViewModel) o;
+
+        return new EqualsBuilder()
+                .append(projectId, viewModel.projectId)
+                .append(targetProjectStartDate, viewModel.targetProjectStartDate)
+                .append(durationInMonths, viewModel.durationInMonths)
+                .append(competition, viewModel.competition)
+                .isEquals();
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder(17, 37)
+                .append(projectId)
+                .toHashCode();
+    }
 }
+
