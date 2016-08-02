389c5952d261eb6179680b4494020b9cd0d13c48
Bronnyl Dias <bdias@worth.systems>
2016-08-02 12:13:25 +0100

    INFUND-3764-View-Spend-Profile-ReadOnly
    1) Removed competition resource from the view model
    2) Added "project name" to the equals method of the view model
    3) Modified the junits to reflect the above changes
    
    Change-Id: Iefd9e0d982019818be4dd596b2485e657427c3a7

--- ifs-web-service/ifs-project-setup-service/src/test/java/com/worth/ifs/project/ProjectSpendProfileControllerTest.java
+++ ifs-web-service/ifs-project-setup-service/src/test/java/com/worth/ifs/project/ProjectSpendProfileControllerTest.java
@@ -1,20 +1,13 @@
 package com.worth.ifs.project;
 
 import com.worth.ifs.BaseControllerMockMVCTest;
-import com.worth.ifs.application.resource.ApplicationResource;
-import com.worth.ifs.competition.resource.CompetitionResource;
 import com.worth.ifs.project.resource.ProjectResource;
 import com.worth.ifs.project.viewmodel.ProjectSpendProfileViewModel;
 import org.junit.Test;
 
 import java.time.LocalDate;
 
-import static com.worth.ifs.application.builder.ApplicationResourceBuilder.newApplicationResource;
-import static com.worth.ifs.competition.builder.CompetitionResourceBuilder.newCompetitionResource;
 import static com.worth.ifs.project.builder.ProjectResourceBuilder.newProjectResource;
-import static org.mockito.Matchers.any;
-import static org.mockito.Mockito.never;
-import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -30,19 +23,16 @@ public class ProjectSpendProfileControllerTest extends BaseControllerMockMVCTest
     @Test
     public void viewSpendProfileSuccessfulViewModelPopulation() throws Exception {
 
-        CompetitionResource competitionResource = newCompetitionResource().withName("C1").build();
-        ApplicationResource applicationResource = newApplicationResource().withCompetition(competitionResource.getId()).build();
         ProjectResource projectResource = newProjectResource()
+                .withName("projectName1")
                 .withTargetStartDate(LocalDate.now())
                 .withDuration(3L)
-                .withApplication(applicationResource).build();
+                .build();
 
         when(projectService.getById(projectResource.getId())).thenReturn(projectResource);
-        when(applicationService.getById(projectResource.getApplication())).thenReturn(applicationResource);
-        when(competitionService.getById(applicationResource.getCompetition())).thenReturn(competitionResource);
 
         // Assert that the view model is populated with the correct values
-        ProjectSpendProfileViewModel viewModel = new ProjectSpendProfileViewModel(projectResource, competitionResource);
+        ProjectSpendProfileViewModel viewModel = new ProjectSpendProfileViewModel(projectResource);
 
         mockMvc.perform(get("/project/{projectId}/spend-profile", projectResource.getId()))
                 .andExpect(status().isOk())
@@ -60,10 +50,7 @@ public class ProjectSpendProfileControllerTest extends BaseControllerMockMVCTest
                 thenThrow(new RuntimeException());
 
         mockMvc.perform(get("/project/{projectId}/spend-profile", projectResource.getId()))
-                .andExpect(status().isInternalServerError());
-
-        verify(applicationService, never()).getById(any());
-        verify(competitionService, never()).getById(any());
-
+                .andExpect(status().isInternalServerError())
+                .andExpect(model().attributeDoesNotExist("model"));
     }
 }
