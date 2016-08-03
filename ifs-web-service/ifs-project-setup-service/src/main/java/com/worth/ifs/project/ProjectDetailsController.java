5a90fa48bf2ad4e9e84f745d02b3263e6a3f975b
David Soff <dsoff@worth.systems>
2016-08-03 16:26:41 +0200

    INFUND-2621 create controller methods for initial testing of the templates

--- ifs-web-service/ifs-project-setup-service/src/main/java/com/worth/ifs/project/ProjectDetailsController.java
+++ ifs-web-service/ifs-project-setup-service/src/main/java/com/worth/ifs/project/ProjectDetailsController.java
@@ -1,8 +1,20 @@
 package com.worth.ifs.project;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+import java.util.SortedSet;
+import java.util.TreeSet;
+import java.util.concurrent.ExecutionException;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+
+import javax.validation.Valid;
+
 import com.worth.ifs.address.resource.AddressResource;
 import com.worth.ifs.address.resource.OrganisationAddressType;
-import com.worth.ifs.form.AddressForm;
 import com.worth.ifs.application.resource.ApplicationResource;
 import com.worth.ifs.application.service.ApplicationService;
 import com.worth.ifs.application.service.CompetitionService;
@@ -10,38 +22,54 @@ import com.worth.ifs.commons.rest.RestResult;
 import com.worth.ifs.commons.service.ServiceResult;
 import com.worth.ifs.competition.resource.CompetitionResource;
 import com.worth.ifs.controller.ValidationHandler;
+import com.worth.ifs.form.AddressForm;
 import com.worth.ifs.organisation.resource.OrganisationAddressResource;
 import com.worth.ifs.organisation.service.OrganisationAddressRestService;
+import com.worth.ifs.project.consortiumoverview.viewmodel.ConsortiumPartnerStatus;
+import com.worth.ifs.project.consortiumoverview.viewmodel.LeadPartnerModel;
+import com.worth.ifs.project.consortiumoverview.viewmodel.ProjectConsortiumStatusViewModel;
+import com.worth.ifs.project.consortiumoverview.viewmodel.RegularPartnerModel;
 import com.worth.ifs.project.form.FinanceContactForm;
 import com.worth.ifs.project.form.ProjectManagerForm;
 import com.worth.ifs.project.resource.ProjectResource;
 import com.worth.ifs.project.resource.ProjectUserResource;
-import com.worth.ifs.project.viewmodel.*;
+import com.worth.ifs.project.viewmodel.ProjectDetailsAddressViewModel;
+import com.worth.ifs.project.viewmodel.ProjectDetailsAddressViewModelForm;
+import com.worth.ifs.project.viewmodel.ProjectDetailsStartDateForm;
+import com.worth.ifs.project.viewmodel.ProjectDetailsStartDateViewModel;
+import com.worth.ifs.project.viewmodel.ProjectDetailsViewModel;
 import com.worth.ifs.user.resource.OrganisationResource;
 import com.worth.ifs.user.resource.ProcessRoleResource;
 import com.worth.ifs.user.resource.UserResource;
 import com.worth.ifs.user.service.OrganisationRestService;
 import com.worth.ifs.user.service.ProcessRoleService;
 import com.worth.ifs.user.service.UserService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.validation.Valid;
-import java.time.LocalDate;
-import java.util.*;
-import java.util.concurrent.ExecutionException;
-import java.util.function.Supplier;
-import java.util.stream.Collectors;
-
-import static com.worth.ifs.address.resource.OrganisationAddressType.*;
+import static com.worth.ifs.address.resource.OrganisationAddressType.OPERATING;
+import static com.worth.ifs.address.resource.OrganisationAddressType.PROJECT;
+import static com.worth.ifs.address.resource.OrganisationAddressType.REGISTERED;
 import static com.worth.ifs.controller.ErrorToObjectErrorConverterFactory.asGlobalErrors;
 import static com.worth.ifs.controller.ErrorToObjectErrorConverterFactory.toField;
+import static com.worth.ifs.project.consortiumoverview.viewmodel.ConsortiumPartnerStatus.ACTION_REQUIRED;
+import static com.worth.ifs.project.consortiumoverview.viewmodel.ConsortiumPartnerStatus.COMPLETE;
+import static com.worth.ifs.project.consortiumoverview.viewmodel.ConsortiumPartnerStatus.PENDING;
 import static com.worth.ifs.user.resource.UserRoleType.PARTNER;
 import static com.worth.ifs.user.resource.UserRoleType.PROJECT_MANAGER;
-import static com.worth.ifs.util.CollectionFunctions.*;
+import static com.worth.ifs.util.CollectionFunctions.getOnlyElement;
+import static com.worth.ifs.util.CollectionFunctions.simpleFilter;
+import static com.worth.ifs.util.CollectionFunctions.simpleFindFirst;
+import static com.worth.ifs.util.CollectionFunctions.simpleMap;
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 /**
@@ -297,6 +325,38 @@ public class ProjectDetailsController extends AddressLookupBaseController {
     public String submitProjectDetails(@PathVariable("projectId") Long projectId) {
         projectService.setApplicationDetailsSubmitted(projectId).getSuccessObjectOrThrowException();
         return redirectToProjectDetails(projectId);
+    }
+
+    @RequestMapping(value="/{projectId}/team-status", method = GET)
+    public String viewProjectTeamStatus(Model model, @PathVariable("projectId") final Long projectId) {
+
+        model.addAttribute("model", getProjectTeamStatusViewModel(projectId));
+        return "project/consortium-status";
+    }
+
+    private ProjectConsortiumStatusViewModel getProjectTeamStatusViewModel(final Long projectId) {
+        OrganisationResource leadOrganisation = projectService.getLeadOrganisation(projectId);
+        List<OrganisationResource> otherOrganisations = projectService.getPartnerOrganisationsForProject(projectId);
+        ConsortiumPartnerStatus monitoringOfficerStatus = projectService.getMonitoringOfficerForProject(projectId).isPresent()? COMPLETE: PENDING;
+
+        final LeadPartnerModel leadPartnerModel = new LeadPartnerModel(
+            leadOrganisation.getName(),
+            ACTION_REQUIRED,
+            monitoringOfficerStatus,
+            ACTION_REQUIRED,
+            ACTION_REQUIRED,
+            ACTION_REQUIRED,
+            ACTION_REQUIRED,
+            ACTION_REQUIRED
+        );
+        final List<RegularPartnerModel> otherPartnersModels = simpleMap(otherOrganisations, partner -> createPartnerModel(projectId, partner));
+
+        return new ProjectConsortiumStatusViewModel(leadPartnerModel, otherPartnersModels);
+    }
+
+    private RegularPartnerModel createPartnerModel(final Long projectId, final OrganisationResource partner) {
+
+        return new RegularPartnerModel(partner.getName(), ACTION_REQUIRED, ACTION_REQUIRED, ACTION_REQUIRED, ACTION_REQUIRED);
     }
 
     private ProjectManagerForm populateOriginalProjectManagerForm(final Long projectId) {
