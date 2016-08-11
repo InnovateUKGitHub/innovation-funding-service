f6ee018bebd0f2ac4f006ed6727415e0651c531c
Duncan Watson <duncan@hiveit.co.uk>
2016-08-11 20:25:26 +0100

    INFUND-4395 - added security to calls for Project Finance user

--- ifs-data-service/src/main/java/com/worth/ifs/project/transactional/ProjectFinanceService.java
+++ ifs-data-service/src/main/java/com/worth/ifs/project/transactional/ProjectFinanceService.java
@@ -1,13 +1,16 @@
 package com.worth.ifs.project.transactional;
 
 import com.worth.ifs.commons.service.ServiceResult;
-import com.worth.ifs.security.NotSecured;
+import com.worth.ifs.project.resource.ProjectResource;
+import com.worth.ifs.security.SecuredBySpring;
+import org.springframework.security.access.prepost.PreAuthorize;
 
 /**
  * Service dealing with Project finance operations
  */
 public interface ProjectFinanceService {
 
-    @NotSecured(value = "TODO DW - to be secured", mustBeSecuredByOtherServices = false)
+    @PreAuthorize("hasAuthority('project_finance')")
+    @SecuredBySpring(value = "GENERATE_SPEND_PROFILE", securedType = ProjectResource.class, description = "A member of the internal Finance Team can generate a Spend Profile for any Project" )
     ServiceResult<Void> generateSpendProfile(Long projectId);
 }
---
+++ ifs-data-service/src/test/java/com/worth/ifs/project/security/ProjectFinanceServiceSecurityTest.java
@@ -0,0 +1,54 @@
+package com.worth.ifs.project.security;
+
+import com.worth.ifs.BaseServiceSecurityTest;
+import com.worth.ifs.commons.service.ServiceResult;
+import com.worth.ifs.project.transactional.ProjectFinanceService;
+import com.worth.ifs.user.resource.RoleResource;
+import com.worth.ifs.user.resource.UserResource;
+import com.worth.ifs.user.resource.UserRoleType;
+import org.junit.Test;
+import org.springframework.security.access.AccessDeniedException;
+
+import static com.worth.ifs.user.builder.RoleResourceBuilder.newRoleResource;
+import static com.worth.ifs.user.builder.UserResourceBuilder.newUserResource;
+import static com.worth.ifs.user.resource.UserRoleType.PROJECT_FINANCE;
+import static java.util.Arrays.asList;
+import static java.util.Collections.singletonList;
+import static junit.framework.TestCase.fail;
+
+public class ProjectFinanceServiceSecurityTest extends BaseServiceSecurityTest<ProjectFinanceService> {
+
+    @Test
+    public void testGenerateSpendProfile() {
+
+        asList(UserRoleType.values()).forEach(role -> {
+            RoleResource roleResource = newRoleResource().withType(role).build();
+            UserResource userWithRole = newUserResource().withRolesGlobal(singletonList(roleResource)).build();
+            setLoggedInUser(userWithRole);
+
+            if (PROJECT_FINANCE.equals(role)) {
+                service.generateSpendProfile(123L);
+            } else {
+                try {
+                    service.generateSpendProfile(123L);
+                    fail("Should have thrown an AccessDeniedException for any non-Finance Team members");
+                } catch (AccessDeniedException e) {
+                    // expected behaviour
+                }
+            }
+        });
+    }
+    @Override
+    protected Class<TestProjectFinanceService> getServiceClass() {
+        return TestProjectFinanceService.class;
+    }
+
+    public static class TestProjectFinanceService implements ProjectFinanceService {
+
+        @Override
+        public ServiceResult<Void> generateSpendProfile(Long projectId) {
+            return null;
+        }
+    }
+}
+
