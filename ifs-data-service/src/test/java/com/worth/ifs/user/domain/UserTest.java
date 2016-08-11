3a1de64b76d5653975810a0098f921ae577c222d
richardpopple <richard@hiveit.co.uk>
2016-08-11 16:33:34 +0100

    INFUND-3530 test

--- ifs-data-service/src/test/java/com/worth/ifs/user/domain/UserTest.java
+++ ifs-data-service/src/test/java/com/worth/ifs/user/domain/UserTest.java
@@ -1,11 +1,18 @@
 package com.worth.ifs.user.domain;
 
+import com.worth.ifs.user.builder.UserBuilder;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static com.worth.ifs.user.builder.OrganisationBuilder.newOrganisation;
+import static java.util.Arrays.asList;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 public class UserTest {
     User user;
@@ -46,4 +53,36 @@ public class UserTest {
         Assert.assertEquals(user.getImageUrl(),imageUrl);
         Assert.assertEquals(user.getUid(), uid);
     }
+
+
+
+    @Test
+    public void testAddOrganisations() {
+        Organisation org1 = newOrganisation().build();
+        Organisation org2 = newOrganisation().build();
+        User u = UserBuilder.newUser().build();
+        u.setOrganisations(new ArrayList<>(asList(org1, org2))); // Not using the builder as that uses method under test
+        // Check everything is as expected before testing
+        assertNotNull(u.getOrganisations());
+        assertEquals(2, u.getOrganisations().size());
+        assertTrue(u.getOrganisations().contains(org1));
+        assertTrue(u.getOrganisations().contains(org2));
+
+        // Add an org already existing - nothing should change
+        u.addUserOrganisation(org1);
+        assertNotNull(u.getOrganisations());
+        assertEquals(2, u.getOrganisations().size());
+        assertTrue(u.getOrganisations().contains(org1));
+        assertTrue(u.getOrganisations().contains(org2));
+
+        // Add an new org
+        Organisation org3 = newOrganisation().build();
+        u.addUserOrganisation(org3);
+        assertNotNull(u.getOrganisations());
+        assertEquals(3, u.getOrganisations().size());
+        assertTrue(u.getOrganisations().contains(org1));
+        assertTrue(u.getOrganisations().contains(org2));
+        assertTrue(u.getOrganisations().contains(org3));
+    }
+
 }
