39e9fc4b93b7e713102fe9a699df6b362abeb7ab
Tom Baldwin <tbaldwin@worth.systems>
2016-08-31 09:35:54 +0100

    INFUND-4169 - Additional controller tests for accepting/rejecting CompetitionInvites. New CompetitionParticipantBuilder.
    Former-commit-id: d84065039d2726dd54862265dc935bd0a6a2e544

--- ifs-data-service/src/test/java/com/worth/ifs/assessment/controller/CompetitionInviteControllerIntegrationTest.java
+++ ifs-data-service/src/test/java/com/worth/ifs/assessment/controller/CompetitionInviteControllerIntegrationTest.java
@@ -1,12 +1,17 @@
 package com.worth.ifs.assessment.controller;
 
+
 import com.worth.ifs.BaseControllerIntegrationTest;
 import com.worth.ifs.commons.rest.RestResult;
 import com.worth.ifs.competition.domain.Competition;
 import com.worth.ifs.competition.repository.CompetitionRepository;
 import com.worth.ifs.invite.domain.CompetitionInvite;
+import com.worth.ifs.invite.domain.CompetitionParticipant;
+import com.worth.ifs.invite.domain.RejectionReason;
 import com.worth.ifs.invite.repository.CompetitionInviteRepository;
+import com.worth.ifs.invite.repository.CompetitionParticipantRepository;
 import com.worth.ifs.invite.resource.CompetitionInviteResource;
+import com.worth.ifs.invite.resource.CompetitionRejectionResource;
 import com.worth.ifs.user.domain.User;
 import com.worth.ifs.user.repository.UserRepository;
 import org.junit.Before;
@@ -15,7 +20,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import static com.worth.ifs.BaseBuilderAmendFunctions.id;
 import static com.worth.ifs.assessment.builder.CompetitionInviteBuilder.newCompetitionInvite;
+import static com.worth.ifs.assessment.builder.CompetitionParticipantBuilder.newCompetitionParticipant;
 import static com.worth.ifs.commons.error.CommonErrors.notFoundError;
+import static com.worth.ifs.commons.error.CommonFailureKeys.*;
+import static com.worth.ifs.invite.builder.RejectionReasonResourceBuilder.newRejectionReasonResource;
+import static com.worth.ifs.invite.domain.CompetitionParticipantRole.ASSESSOR;
+import static com.worth.ifs.invite.domain.ParticipantStatus.PENDING;
 import static com.worth.ifs.security.SecuritySetter.swapOutForUser;
 import static com.worth.ifs.user.builder.UserBuilder.newUser;
 import static org.junit.Assert.*;
@@ -36,13 +46,16 @@ public class CompetitionInviteControllerIntegrationTest extends BaseControllerIn
     UserRepository userRepository;
 
     @Autowired
-    CompetitionRepository competitionRepository;
+    private CompetitionParticipantRepository competitionParticipantRepository;
+
+    @Autowired
+    private CompetitionRepository competitionRepository;
 
     private Competition competition;
 
     @Before
     public void setup() {
-        swapOutForUser(getSystemRegistrationUser());
+        setLoggedInUser(getSystemRegistrationUser());
 
         competition = competitionRepository.findOne(1L);
     }
@@ -171,5 +184,187 @@ public class CompetitionInviteControllerIntegrationTest extends BaseControllerIn
         RestResult<Boolean> serviceResult = controller.checkExistingUser("not exists hash");
         assertTrue(serviceResult.isFailure());
         assertTrue(serviceResult.getFailure().is(notFoundError(CompetitionInvite.class, "not exists hash")));
+    }
+
+    @Test
+    public void acceptInvite() throws Exception {
+        competitionParticipantRepository.save(newCompetitionParticipant()
+                .with(id(null))
+                .withStatus(PENDING)
+                .withRole(ASSESSOR)
+                .withCompetition(competition)
+                .withInvite(newCompetitionInvite()
+                        .with(id(null))
+                        .withName("name")
+                        .withEmail("no-user-exists@for-this.address")
+                        .withUser((User) null)
+                        .withHash("hash")
+                        .withCompetition(competition)
+                        .build())
+                .build());
+        controller.openInvite("hash");
+
+        RestResult<Void> serviceResult = controller.acceptInvite("hash");
+        assertTrue(serviceResult.isSuccess());
+    }
+
+    @Test
+    public void acceptInvite_hashNotExists() throws Exception {
+        RestResult<Void> serviceResult = controller.acceptInvite("hash not exists");
+        assertTrue(serviceResult.isFailure());
+        assertTrue(serviceResult.getFailure().is(notFoundError(CompetitionParticipant.class, "hash not exists")));
+    }
+
+    @Test
+    public void acceptInvite_notOpened() throws Exception {
+        competitionParticipantRepository.save(newCompetitionParticipant()
+                .with(id(null))
+                .withStatus(PENDING)
+                .withRole(ASSESSOR)
+                .withCompetition(competition)
+                .withInvite(newCompetitionInvite()
+                        .with(id(null))
+                        .withName("name")
+                        .withEmail("no-user-exists@for-this.address")
+                        .withUser((User) null)
+                        .withHash("hash")
+                        .withCompetition(competition)
+                        .build())
+                .build());
+
+        RestResult<Void> serviceResult = controller.acceptInvite("hash");
+        assertTrue(serviceResult.isFailure());
+        assertTrue(serviceResult.getFailure().is(COMPETITION_PARTICIPANT_CANNOT_ACCEPT_UNOPENED_INVITE));
+    }
+
+    @Test
+    public void acceptInvite_rejected() throws Exception {
+        competitionParticipantRepository.save(newCompetitionParticipant()
+                .with(id(null))
+                .withStatus(PENDING)
+                .withRole(ASSESSOR)
+                .withCompetition(competition)
+                .withInvite(newCompetitionInvite()
+                        .with(id(null))
+                        .withName("name")
+                        .withEmail("no-user-exists@for-this.address")
+                        .withUser((User) null)
+                        .withHash("hash")
+                        .withCompetition(competition)
+                        .build())
+                .build());
+        controller.openInvite("hash");
+
+        CompetitionRejectionResource competitionRejectionResource =
+                new CompetitionRejectionResource(newRejectionReasonResource().withId(1L).build(), "too busy");
+        RestResult<Void> serviceResult = controller.rejectInvite("hash", competitionRejectionResource);
+        assertTrue(serviceResult.isSuccess());
+        RestResult<Void> acceptResult = controller.acceptInvite("hash");
+
+        assertTrue(acceptResult.isFailure());
+        assertTrue(acceptResult.getFailure().is(COMPETITION_PARTICIPANT_CANNOT_ACCEPT_ALREADY_REJECTED_INVITE));
+    }
+
+    @Test
+    public void rejectInvite() throws Exception {
+        competitionParticipantRepository.save(newCompetitionParticipant()
+                .with(id(null))
+                .withStatus(PENDING)
+                .withRole(ASSESSOR)
+                .withCompetition(competition)
+                .withInvite(newCompetitionInvite()
+                        .with(id(null))
+                        .withName("name")
+                        .withEmail("no-user-exists@for-this.address")
+                        .withUser((User) null)
+                        .withHash("hash")
+                        .withCompetition(competition)
+                        .build())
+                .build());
+        controller.openInvite("hash");
+
+        CompetitionRejectionResource competitionRejectionResource =
+                new CompetitionRejectionResource(newRejectionReasonResource().withId(1L).build(), "too busy");
+        RestResult<Void> serviceResult = controller.rejectInvite("hash", competitionRejectionResource);
+        assertTrue(serviceResult.isSuccess());
+    }
+
+    @Test
+    public void rejectInvite_accepted() throws Exception {
+        competitionParticipantRepository.save(newCompetitionParticipant()
+                .with(id(null))
+                .withStatus(PENDING)
+                .withRole(ASSESSOR)
+                .withCompetition(competition)
+                .withInvite(newCompetitionInvite()
+                        .with(id(null))
+                        .withName("name")
+                        .withEmail("no-user-exists@for-this.address")
+                        .withUser((User) null)
+                        .withHash("hash")
+                        .withCompetition(competition)
+                        .build())
+                .build());
+        controller.openInvite("hash");
+
+        RestResult<Void> serviceResult = controller.acceptInvite("hash");
+        assertTrue(serviceResult.isSuccess());
+
+        CompetitionRejectionResource competitionRejectionResource =
+                new CompetitionRejectionResource(newRejectionReasonResource().withId(1L).build(), "too busy");
+        RestResult<Void> rejectResult = controller.rejectInvite("hash", competitionRejectionResource);
+
+        assertTrue(rejectResult.isFailure());
+        assertTrue(rejectResult.getFailure().is(COMPETITION_PARTICIPANT_CANNOT_REJECT_ALREADY_ACCEPTED_INVITE));
+    }
+
+    @Test
+    public void rejectInvite_notOpened() throws Exception {
+        competitionParticipantRepository.save(newCompetitionParticipant()
+                .with(id(null))
+                .withStatus(PENDING)
+                .withRole(ASSESSOR)
+                .withCompetition(competition)
+                .withInvite(newCompetitionInvite()
+                        .with(id(null))
+                        .withName("name")
+                        .withEmail("no-user-exists@for-this.address")
+                        .withUser((User) null)
+                        .withHash("hash")
+                        .withCompetition(competition)
+                        .build())
+                .build());
+
+        CompetitionRejectionResource competitionRejectionResource =
+                new CompetitionRejectionResource(newRejectionReasonResource().withId(1L).build(), "too busy");
+        RestResult<Void> serviceResult = controller.rejectInvite("hash", competitionRejectionResource);
+
+        assertTrue(serviceResult.isFailure());
+        assertTrue(serviceResult.getFailure().is(COMPETITION_PARTICIPANT_CANNOT_REJECT_UNOPENED_INVITE));
+    }
+
+    @Test
+    public void rejectInvite_unknownReason() throws Exception {
+        competitionParticipantRepository.save(newCompetitionParticipant()
+                .with(id(null))
+                .withStatus(PENDING)
+                .withRole(ASSESSOR)
+                .withCompetition(competition)
+                .withInvite(newCompetitionInvite()
+                        .with(id(null))
+                        .withName("name")
+                        .withEmail("no-user-exists@for-this.address")
+                        .withUser((User) null)
+                        .withHash("hash")
+                        .withCompetition(competition)
+                        .build())
+                .build());
+        controller.openInvite("hash");
+
+        CompetitionRejectionResource competitionRejectionResource =
+                new CompetitionRejectionResource(newRejectionReasonResource().withId(-1L).build(), "too busy");
+        RestResult<Void> serviceResult = controller.rejectInvite("hash", competitionRejectionResource);
+        assertTrue(serviceResult.isFailure());
+        assertTrue(serviceResult.getFailure().is(notFoundError(RejectionReason.class, -1L)));
     }
 }
