29a4108ce4195447c761514b6368dfa9ec54abcf
Ben Outram <ben@hiveit.co.uk>
2016-08-31 21:38:58 +0100

    INFUND-4175 Service was missing @Service annotation.
    Former-commit-id: f06232647dea02f051d8599f66162c257351aa05

--- ifs-data-service/src/main/java/com/worth/ifs/assessment/service/CompetitionParticipantRestServiceImpl.java
+++ ifs-data-service/src/main/java/com/worth/ifs/assessment/service/CompetitionParticipantRestServiceImpl.java
@@ -5,12 +5,14 @@ import com.worth.ifs.commons.service.BaseRestService;
 import com.worth.ifs.invite.resource.CompetitionParticipantResource;
 import com.worth.ifs.invite.resource.CompetitionParticipantRoleResource;
 import com.worth.ifs.invite.resource.ParticipantStatusResource;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 import static com.worth.ifs.commons.service.ParameterizedTypeReferences.competitionParticipantResourceListType;
 import static java.lang.String.format;
 
+@Service
 public class CompetitionParticipantRestServiceImpl extends BaseRestService implements CompetitionParticipantRestService {
 
     private static final String competitionParticipantRestUrl = "/competitionparticipant";
