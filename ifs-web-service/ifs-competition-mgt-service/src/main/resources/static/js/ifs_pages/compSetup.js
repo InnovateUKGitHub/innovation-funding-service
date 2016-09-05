a1f75f95017a209617f5eb59733a81b4fd9fda9a
Reinaud van Rumpt <rvanrumpt@worth.systems>
2016-09-05 13:48:46 +0100

    INFUND-4764 Cleaned up JS pull requests
    Former-commit-id: b8b10c17c7714651671b797db7b2bd0c5ae2909d

--- ifs-web-service/ifs-competition-mgt-service/src/main/resources/static/js/ifs_pages/compSetup.js
+++ ifs-web-service/ifs-competition-mgt-service/src/main/resources/static/js/ifs_pages/compSetup.js
@@ -15,15 +15,7 @@ IFS.competition_management.setup = (function(){
           IFS.competition_management.setup.handleStateAid();
         });
         jQuery("body.competition-management.competition-setup").on('change','[name="innovationSectorCategoryId"]',function(){
-           function waitUntilSaved() {
-             var waitUntilSavedTimer = setInterval(function() {
-               if(jQuery('[name="innovationSectorCategoryId"]').closest(".form-group").find('.autosave-info').html() == "Saved!") {
-                 IFS.competition_management.setup.handleInnovationSector(false);
-                 clearInterval(waitUntilSavedTimer);
-               }
-             }, 100);
-           }
-           waitUntilSaved();
+          IFS.competition_management.setup.handleInnovationSector(false);
         });
         IFS.competition_management.setup.innovationSectorOnPageLoad();
 
@@ -79,7 +71,11 @@ IFS.competition_management.setup = (function(){
                       }
                   });
                   if(!pageLoad) {
-                    innovationCategory.trigger('change');
+                    var innovationSectorElement = jQuery('[name="innovationSectorCategoryId"]');
+                    var innovationAreaElement = jQuery('[name="innovationAreaCategoryId"]');
+
+                    IFS.core.autoSave.fieldChanged(innovationSectorElement);
+                    IFS.core.autoSave.fieldChanged(innovationAreaElement);
                   }
               }
           });
