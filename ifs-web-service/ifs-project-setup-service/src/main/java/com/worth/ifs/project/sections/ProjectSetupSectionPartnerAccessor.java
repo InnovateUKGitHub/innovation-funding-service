86d08db0e5a299b5c8bff5d427337603953a0fb7
Duncan Watson <duncan@hiveit.co.uk>
2016-09-06 12:27:14 +0100

    INFUND-4428 - added skeleton for the checking of access to Project Setup status
    Former-commit-id: 3ead044fd8c4ae450fcd2327c9c5b0b76d8b0a83

---
+++ ifs-web-service/ifs-project-setup-service/src/main/java/com/worth/ifs/project/sections/ProjectSetupSectionPartnerAccessor.java
@@ -0,0 +1,55 @@
+package com.worth.ifs.project.sections;
+
+import com.worth.ifs.commons.error.exception.ForbiddenActionException;
+import com.worth.ifs.project.resource.ProjectResource;
+import com.worth.ifs.user.resource.OrganisationResource;
+import com.worth.ifs.user.resource.UserResource;
+
+/**
+ * This is a helper class for determining whether or not a given Project Setup section is available to access
+ */
+public class ProjectSetupSectionPartnerAccessor {
+
+    private ProjectSetupProgressChecker projectSetupProgressChecker = new ProjectSetupProgressChecker();
+
+    public void checkAccessToCompaniesHouseSection(ProjectResource project, UserResource user, OrganisationResource organisation) {
+
+        if (projectSetupProgressChecker.isBusinessOrganisationType(project, user, organisation)) {
+            return;
+        }
+
+        throwForbiddenException("Unable to access Companies House section if not a Business Organisation");
+    }
+
+    public void checkAccessToProjectDetailsSection(ProjectResource project, UserResource user, OrganisationResource organisation) {
+
+        if (!projectSetupProgressChecker.isBusinessOrganisationType(project, user, organisation)) {
+            return;
+        }
+
+        if (projectSetupProgressChecker.isCompaniesHouseDetailsComplete(project, user, organisation)) {
+            return;
+        }
+
+        throwForbiddenException("Unable to access Project Details section until Companies House details are complete for Organisation");
+    }
+
+    public void checkAccessToMonitoringOfficerSection(ProjectResource project, UserResource user, OrganisationResource organisation) {
+
+        if (projectSetupProgressChecker.isProjectDetailsComplete(project, user, organisation)) {
+            return;
+        }
+
+        throwForbiddenException("Unable to access Monitoring Officer section until Companies House details are complete for Organisation");
+    }
+
+    public void checkAccessToBankDetailsSection(ProjectResource project, UserResource user, OrganisationResource organisation) {
+
+        // TODO DW - discuss with Robin
+        throwForbiddenException("Unable to access Monitoring Officer section until Companies House details are complete for Organisation");
+    }
+
+    public void throwForbiddenException(String message) {
+        throw new ForbiddenActionException(message);
+    }
+}
---
+++ ifs-web-service/ifs-project-setup-service/src/test/java/com/worth/ifs/project/sections/ProjectSetupSectionsPartnerAccessorTest.java
@@ -0,0 +1,95 @@
+package com.worth.ifs.project.sections;
+
+import com.worth.ifs.BaseUnitTest;
+import com.worth.ifs.commons.error.exception.ForbiddenActionException;
+import com.worth.ifs.project.resource.ProjectResource;
+import com.worth.ifs.user.resource.OrganisationResource;
+import com.worth.ifs.user.resource.UserResource;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
+import java.util.function.Consumer;
+
+import static com.worth.ifs.project.builder.ProjectResourceBuilder.newProjectResource;
+import static com.worth.ifs.user.builder.OrganisationResourceBuilder.newOrganisationResource;
+import static com.worth.ifs.user.builder.UserResourceBuilder.newUserResource;
+import static java.util.Arrays.asList;
+import static org.mockito.Mockito.*;
+
+public class ProjectSetupSectionsPartnerAccessorTest extends BaseUnitTest {
+
+    @Mock
+    private ProjectSetupProgressChecker projectSetupProgressCheckerMock;
+
+    @InjectMocks
+    private ProjectSetupSectionPartnerAccessor accessor;
+
+    private ProjectResource project = newProjectResource().build();
+    private UserResource user = newUserResource().build();
+    private OrganisationResource organisation = newOrganisationResource().build();
+
+    @Test
+    public void testCheckAccessToCompaniesHouseSectionHappyPath() {
+
+        when(projectSetupProgressCheckerMock.isBusinessOrganisationType(project, user, organisation)).thenReturn(true);
+
+        accessor.checkAccessToCompaniesHouseSection(project, user, organisation);
+
+        verifyInteractions(mock -> mock.isBusinessOrganisationType(project, user, organisation));
+    }
+
+    @Test(expected = ForbiddenActionException.class)
+    public void testCheckAccessToCompaniesHouseSectionButOrganisationIsNotBusiness() {
+
+        when(projectSetupProgressCheckerMock.isBusinessOrganisationType(project, user, organisation)).thenReturn(false);
+
+        accessor.checkAccessToCompaniesHouseSection(project, user, organisation);
+
+        verifyInteractions(mock -> mock.isBusinessOrganisationType(project, user, organisation));
+    }
+
+    @Test
+    public void testCheckAccessToProjectDetailsSectionHappyPathForBusinessOrganisation() {
+
+        when(projectSetupProgressCheckerMock.isBusinessOrganisationType(project, user, organisation)).thenReturn(true);
+        when(projectSetupProgressCheckerMock.isCompaniesHouseDetailsComplete(project, user, organisation)).thenReturn(true);
+
+        accessor.checkAccessToProjectDetailsSection(project, user, organisation);
+
+        verifyInteractions(
+                mock -> mock.isBusinessOrganisationType(project, user, organisation),
+                mock -> mock.isCompaniesHouseDetailsComplete(project, user, organisation)
+        );
+    }
+
+    @Test
+    public void testCheckAccessToProjectDetailsSectionHappyPathForNonBusinessTypeOrganisation() {
+
+        when(projectSetupProgressCheckerMock.isBusinessOrganisationType(project, user, organisation)).thenReturn(false);
+
+        accessor.checkAccessToProjectDetailsSection(project, user, organisation);
+
+        verifyInteractions(mock -> mock.isBusinessOrganisationType(project, user, organisation));
+    }
+
+    @Test(expected = ForbiddenActionException.class)
+    public void testCheckAccessToProjectDetailsSectionButCompaniesHouseSectionIncomplete() {
+
+        when(projectSetupProgressCheckerMock.isBusinessOrganisationType(project, user, organisation)).thenReturn(true);
+        when(projectSetupProgressCheckerMock.isCompaniesHouseDetailsComplete(project, user, organisation)).thenReturn(false);
+
+        accessor.checkAccessToProjectDetailsSection(project, user, organisation);
+
+        verifyInteractions(
+                mock -> mock.isBusinessOrganisationType(project, user, organisation),
+                mock -> mock.isCompaniesHouseDetailsComplete(project, user, organisation)
+        );
+    }
+
+    @SafeVarargs
+    private final void verifyInteractions(Consumer<ProjectSetupProgressChecker>... verifiers) {
+        asList(verifiers).forEach(verifier -> verifier.accept(verify(projectSetupProgressCheckerMock)));
+        verifyNoMoreInteractions(projectSetupProgressCheckerMock);
+    }
+}
