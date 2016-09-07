2375b22fa655e1dc664b9d88237fe0d17aeb1203
Luke Harper <lukeharperipl@gmail.com>
2016-09-07 12:03:37 +0100

    INFUND-3240 Moving application print to web-core.
    Former-commit-id: f10df02e1e9a0c168421857dab48369a4877b610

--- ifs-web-service/ifs-application-service/src/main/java/com/worth/ifs/application/ApplicationController.java
+++ ifs-web-service/ifs-application-service/src/main/java/com/worth/ifs/application/ApplicationController.java
@@ -50,9 +50,6 @@ public class ApplicationController extends AbstractApplicationController {
     private ApplicationOverviewModelPopulator applicationOverviewModelPopulator;
 
     @Autowired
-    private OrganisationDetailsModelPopulator organisationDetailsModelPopulator;
-
-    @Autowired
     private AssessorFeedbackRestService assessorFeedbackRestService;
 
     public static String redirectToApplication(ApplicationResource application){
@@ -327,35 +324,6 @@ public class ApplicationController extends AbstractApplicationController {
     private void doAssignQuestion(@PathVariable("applicationId") Long applicationId, HttpServletRequest request, HttpServletResponse response) {
         assignQuestion(request, applicationId);
         cookieFlashMessageFilter.setFlashMessage(response, "assignedQuestion");
-    }
-
-    /**
-     * Printable version of the application
-     */
-    @RequestMapping(value="/{applicationId}/print")
-    public String print(@PathVariable("applicationId") final Long applicationId,
-            Model model, HttpServletRequest request) {
-        UserResource user = userAuthenticationService.getAuthenticatedUser(request);
-        ApplicationResource application = applicationService.getById(applicationId);
-        CompetitionResource competition = competitionService.getById(application.getCompetition());
-
-        List<FormInputResponseResource> responses = formInputResponseService.getByApplication(applicationId);
-        model.addAttribute("responses", formInputResponseService.mapFormInputResponsesToFormInput(responses));
-        model.addAttribute("currentApplication", application);
-        model.addAttribute("currentCompetition", competition);
-
-        List<ProcessRoleResource> userApplicationRoles = processRoleService.findProcessRolesByApplicationId(application.getId());
-        Optional<OrganisationResource> userOrganisation = getUserOrganisation(user.getId(), userApplicationRoles);
-        model.addAttribute("userOrganisation", userOrganisation.orElse(null));
-
-        organisationDetailsModelPopulator.populateModel(model, application.getId(), userApplicationRoles);
-        addQuestionsDetails(model, application, null);
-        addUserDetails(model, application, user.getId());
-        addApplicationInputs(application, model);
-        addMappedSectionsDetails(model, application, competition, Optional.empty(), userOrganisation);
-        financeOverviewModelManager.addFinanceDetails(model, competition.getId(), applicationId);
-
-        return "/application/print";
     }
 
     private void addApplicationAndSectionsInternalWithOrgDetails(final ApplicationResource application, final CompetitionResource competition, final Long userId, final Model model, final ApplicationForm form) {
