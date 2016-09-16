39d2394e45b09a2d6ead70c53573b6345fc4be82
gauld <gauld@worth.systems>
2016-09-16 17:18:57 +0100

    INFUND-5123 Added populator and view classes.
    Former-commit-id: 3a2d6011aad3cc47c35dfcaa5927e5a3b515c3db

--- ifs-web-service/ifs-assessment-service/src/main/java/com/worth/ifs/assessment/controller/AssessorRegistrationController.java
+++ ifs-web-service/ifs-assessment-service/src/main/java/com/worth/ifs/assessment/controller/AssessorRegistrationController.java
@@ -1,6 +1,11 @@
 package com.worth.ifs.assessment.controller;
 
+import com.worth.ifs.assessment.model.AssessorDeclarationModelPopulator;
+import com.worth.ifs.assessment.model.AssessorSkillsModelPopulator;
+import com.worth.ifs.assessment.model.AssessorTermsModelPopulator;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -11,23 +16,36 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping("/registration")
 public class AssessorRegistrationController {
 
+    @Autowired
+    private AssessorSkillsModelPopulator assessorSkillsModelPopulator;
+
+    @Autowired
+    private AssessorDeclarationModelPopulator assessorDeclarationModelPopulator;
+
+    @Autowired
+    private AssessorTermsModelPopulator assessorTermsModelPopulator;
+
+
     @RequestMapping(value = "register", method = RequestMethod.GET)
     public String register() {
         return "registration/register";
     }
 
     @RequestMapping(value = "skills", method = RequestMethod.GET)
-    public String skills() {
+    public String skills(Model model) {
+        model.addAttribute("model", assessorSkillsModelPopulator.populateModel());
         return "registration/innovation-areas";
     }
 
     @RequestMapping(value = "declaration", method = RequestMethod.GET)
-    public String declaration() {
+    public String declaration(Model model) {
+        model.addAttribute("model", assessorDeclarationModelPopulator.populateModel());
         return "registration/declaration-of-interest";
     }
 
     @RequestMapping(value = "terms", method = RequestMethod.GET)
-    public String terms() {
+    public String terms(Model model) {
+        model.addAttribute("model", assessorTermsModelPopulator.populateModel());
         return "registration/terms";
     }
 }
