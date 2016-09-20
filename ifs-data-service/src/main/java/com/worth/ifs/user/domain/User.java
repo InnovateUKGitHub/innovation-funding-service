886d1ae9ea2dd4fcaed319fa9a40a096bdc8b56c
Tom Baldwin <tbaldwin@worth.systems>
2016-09-20 16:41:24 +0100

    INFUND-5134 - UserResource now uses the Gender/Disability enums.  Gender/Disability are moved to the resource package for access by the web tier.
    Former-commit-id: ddee5998c961770587fb2f076b70739c7447b600

--- ifs-data-service/src/main/java/com/worth/ifs/user/domain/User.java
+++ ifs-data-service/src/main/java/com/worth/ifs/user/domain/User.java
@@ -2,6 +2,8 @@ package com.worth.ifs.user.domain;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.worth.ifs.user.resource.Disability;
+import com.worth.ifs.user.resource.Gender;
 import com.worth.ifs.user.resource.UserRoleType;
 import com.worth.ifs.user.resource.UserStatus;
 import org.apache.commons.lang3.builder.EqualsBuilder;
