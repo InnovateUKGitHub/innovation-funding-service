695b891123692555744b0164efc268caa32a7165
skistapur <sainath.kistapur@wtg.co.uk>
2016-09-23 14:51:25 +0100

    INFUND-4979: section template foreign key fix.
    Former-commit-id: bd49997bd3708300c8fdd5ea82300cc3b9bf4b78

--- ifs-data-service/src/main/resources/db/setup/V78_12_1__create_sector_competition_templates.sql
+++ ifs-data-service/src/main/resources/db/setup/V78_12_1__create_sector_competition_templates.sql
@@ -5,9 +5,11 @@ CHANGE COLUMN `id` `id` BIGINT(20) NOT NULL AUTO_INCREMENT ;
 ALTER TABLE `section_template` ADD CONSTRAINT `FK_st_ctid` FOREIGN KEY (`competition_template_id`) REFERENCES `competition_template` (`id`);
 
 ALTER TABLE `question_template` DROP FOREIGN KEY `FK_qqt_stid`;
+ALTER TABLE `section_template` DROP FOREIGN KEY `FK_st_pstid`;
 ALTER TABLE `section_template`
 CHANGE COLUMN `id` `id` BIGINT(20) NOT NULL AUTO_INCREMENT ;
-ALTER TABLE `question_template` ADD CONSTRAINT `FK_qqt_stid` FOREIGN KEY (`section_template_id`) REFERENCES `section_template` (`id`)
+ALTER TABLE `question_template` ADD CONSTRAINT `FK_qqt_stid` FOREIGN KEY (`section_template_id`) REFERENCES `section_template` (`id`);
+ALTER TABLE `section_template` ADD CONSTRAINT `FK_st_pstid` FOREIGN KEY (`parent_section_template_id`) REFERENCES `section_template` (`id`);
 
 ALTER TABLE `question_template`
 CHANGE COLUMN `id` `id` BIGINT(20) NOT NULL AUTO_INCREMENT ;
