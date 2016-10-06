01b50febfc44d3ac4da3b00761020514a76c650a
richard <richard@hiveit.co.uk>
2016-10-06 13:22:50 +0100

    INFUND-5192 tests
    Former-commit-id: 50fcd870de21d2c0283264de5bdf9e0a69140482

--- ifs-data-service/src/test/java/com/worth/ifs/project/finance/transactional/FinanceCheckServiceImplTest.java
+++ ifs-data-service/src/test/java/com/worth/ifs/project/finance/transactional/FinanceCheckServiceImplTest.java
@@ -1,14 +1,13 @@
 package com.worth.ifs.project.finance.transactional;
 
 import com.worth.ifs.BaseServiceUnitTest;
+import com.worth.ifs.BuilderAmendFunctions;
 import com.worth.ifs.application.domain.Application;
 import com.worth.ifs.commons.error.Error;
 import com.worth.ifs.commons.service.ServiceResult;
-import com.worth.ifs.project.builder.CostCategoryResourceBuilder;
-import com.worth.ifs.project.builder.CostGroupResourceBuilder;
-import com.worth.ifs.project.builder.CostResourceBuilder;
-import com.worth.ifs.project.builder.FinanceCheckResourceBuilder;
+import com.worth.ifs.project.builder.*;
 import com.worth.ifs.project.finance.domain.Cost;
+import com.worth.ifs.project.finance.domain.CostCategory;
 import com.worth.ifs.project.finance.domain.FinanceCheck;
 import com.worth.ifs.project.finance.domain.SpendProfile;
 import com.worth.ifs.project.finance.repository.FinanceCheckRepository;
@@ -18,6 +17,7 @@ import com.worth.ifs.project.finance.resource.CostResource;
 import com.worth.ifs.project.finance.resource.FinanceCheckResource;
 import com.worth.ifs.project.resource.ProjectOrganisationCompositeId;
 import com.worth.ifs.project.resource.SpendProfileTableResource;
+import com.worth.ifs.user.builder.OrganisationBuilder;
 import org.apache.poi.ss.formula.functions.Finance;
 import org.junit.Ignore;
 import org.junit.Test;
@@ -29,12 +29,19 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
+import static com.worth.ifs.BaseBuilderAmendFunctions.id;
 import static com.worth.ifs.LambdaMatcher.createLambdaMatcher;
 import static com.worth.ifs.commons.error.CommonErrors.notFoundError;
 import static com.worth.ifs.commons.error.CommonFailureKeys.SPEND_PROFILE_CONTAINS_FRACTIONS_IN_COST_FOR_SPECIFIED_CATEGORY_AND_MONTH;
 import static com.worth.ifs.commons.service.ServiceResult.serviceSuccess;
+import static com.worth.ifs.project.builder.CostBuilder.newCost;
+import static com.worth.ifs.project.builder.CostCategoryBuilder.newCostCategory;
 import static com.worth.ifs.project.builder.CostCategoryResourceBuilder.newCostCategoryResource;
+import static com.worth.ifs.project.builder.CostGroupBuilder.newCostGroup;
+import static com.worth.ifs.project.builder.FinanceCheckBuilder.newFinanceCheck;
 import static com.worth.ifs.project.builder.FinanceCheckResourceBuilder.newFinanceCheckResource;
+import static com.worth.ifs.project.builder.ProjectBuilder.newProject;
+import static com.worth.ifs.user.builder.OrganisationBuilder.newOrganisation;
 import static com.worth.ifs.util.MapFunctions.asMap;
 import static java.util.Arrays.asList;
 import static org.junit.Assert.assertEquals;
@@ -48,15 +55,51 @@ public class FinanceCheckServiceImplTest extends BaseServiceUnitTest<FinanceChec
     private FinanceCheckRepository financeCheckRepositoryMock;
 
     @Test
-    public void testSaveFinanceCheckNotFound() {
+    public void testGetByProjectAndOrganisationNotFound() {
+        // Set up
         Long projectId = 1L;
-        Long organisationId = 1L;
+        Long organisationId = 2L;
         ProjectOrganisationCompositeId compositeId = new ProjectOrganisationCompositeId(projectId, organisationId);
         when(financeCheckRepositoryMock.findByProjectIdAndOrganisationId(projectId, organisationId)).thenReturn(null);
+        // Method under test
         ServiceResult<FinanceCheckResource> result = service.getByProjectAndOrganisation(compositeId);
+        // Assertions
         assertTrue(result.isFailure());
         assertTrue(result.getFailure().is(notFoundError(FinanceCheck.class, compositeId)));
     }
+
+    @Test
+    public void testGetByProjectAndOrganisation() {
+        // Set up
+        Long projectId = 1L;
+        Long organisationId = 2L;
+        ProjectOrganisationCompositeId compositeId = new ProjectOrganisationCompositeId(projectId, organisationId);
+        FinanceCheck financeCheck = newFinanceCheck().
+                withProject(newProject().with(id(projectId)).build()).
+                withOrganisation(newOrganisation().with(id(organisationId)).build()).
+                withCostGroup(newCostGroup().
+                        withCosts(newCost().
+                                withValue("1", "2").
+                                withCostCategory(
+                                        newCostCategory().
+                                                withName("cat 1", "cat 2").
+                                                buildArray(2, CostCategory.class)).
+                                build(2)).
+                        build()).
+                build();
+        // Method under test
+        when(financeCheckRepositoryMock.findByProjectIdAndOrganisationId(projectId, organisationId)).thenReturn(financeCheck);
+        ServiceResult<FinanceCheckResource> result = service.getByProjectAndOrganisation(compositeId);
+        // Assertions - basically testing the deserialisation into resource objects
+        assertTrue(result.isSuccess());
+        assertEquals(financeCheck.getId(), result.getSuccessObject().getId());
+        assertEquals(financeCheck.getOrganisation().getId(), result.getSuccessObject().getOrganisation());
+        assertEquals(financeCheck.getProject().getId(), result.getSuccessObject().getProject());
+        assertEquals(financeCheck.getCostGroup().getId(), result.getSuccessObject().getCostGroup().getId());
+        assertEquals(financeCheck.getCostGroup().getCosts().size(), result.getSuccessObject().getCostGroup().getCosts().size());
+        assertEquals(financeCheck.getCostGroup().getCosts().get(0).getCostCategory().getId(), result.getSuccessObject().getCostGroup().getCosts().get(0).getCostCategory().getId());
+    }
+
 
     @Test
     public void testGetFinanceCheck() {
