23992ec57999c083d6b0c749f12d35b66a11233c
Wouter de Meijer <wdemeijer@worth.systems>
2016-10-06 09:55:01 +0200

    INFUND-5188 - Added security rule so rejected assessments cannot be opened by their assessor
    Former-commit-id: 7800017b7c0e268e29e34914d5d68f8bf33cff78

--- ifs-data-service/src/main/java/com/worth/ifs/assessment/security/AssessmentPermissionRules.java
+++ ifs-data-service/src/main/java/com/worth/ifs/assessment/security/AssessmentPermissionRules.java
@@ -1,11 +1,23 @@
 package com.worth.ifs.assessment.security;
 
+import com.worth.ifs.assessment.domain.Assessment;
+import com.worth.ifs.assessment.mapper.AssessmentMapper;
+import com.worth.ifs.assessment.repository.AssessmentRepository;
+import com.worth.ifs.assessment.resource.AssessmentOutcomes;
 import com.worth.ifs.assessment.resource.AssessmentResource;
+import com.worth.ifs.assessment.resource.AssessmentStates;
 import com.worth.ifs.security.BasePermissionRules;
 import com.worth.ifs.commons.security.PermissionRule;
 import com.worth.ifs.commons.security.PermissionRules;
+import com.worth.ifs.user.domain.User;
+import com.worth.ifs.user.mapper.UserMapper;
 import com.worth.ifs.user.resource.UserResource;
+import com.worth.ifs.workflow.domain.ActivityState;
+import com.worth.ifs.workflow.domain.ProcessOutcome;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.Optional;
 
 /**
  * Provides the permissions around CRUD operations for {@link com.worth.ifs.assessment.domain.Assessment} resources.
@@ -13,6 +25,11 @@ import org.springframework.stereotype.Component;
 @Component
 @PermissionRules
 public class AssessmentPermissionRules extends BasePermissionRules {
+    @Autowired
+    AssessmentMapper mapper;
+
+    @Autowired
+    AssessmentRepository assessmentRepository;
 
     @PermissionRule(value = "READ", description = "Assessors can read Assessments")
     public boolean userCanReadAssessment(final AssessmentResource assessment, final UserResource user) {
@@ -26,6 +43,17 @@ public class AssessmentPermissionRules extends BasePermissionRules {
 
     private boolean isAssessorForAssessment(final AssessmentResource assessment, final UserResource user) {
         Long assessmentUser = processRoleRepository.findOne(assessment.getProcessRole()).getUser().getId();
-        return user.getId().equals(assessmentUser);
+        return user.getId().equals(assessmentUser) && !assessorHasRejectedAssessment(assessment);
+    }
+
+    private boolean assessorHasRejectedAssessment(final AssessmentResource assessmentResource) {
+        Assessment assessment = assessmentRepository.findOne(assessmentResource.getId());
+
+        if(assessment.getLastOutcome()!=null) {
+            return assessment.getLastOutcome().getOutcomeType().equals(AssessmentOutcomes.REJECT.getType());
+        }
+        else {
+            return false;
+        }
     }
 }
