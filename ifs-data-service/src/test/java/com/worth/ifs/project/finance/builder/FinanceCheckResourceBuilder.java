7c61e93b361cf2a9fe31bba22191c2bcc39d9704
Ravish Bhagdev <rav@hiveit.co.uk>
2016-10-10 23:55:35 +0100

    INFUND-5192 Adding data layer controller unit tests, adding fc summary resource builder, moving builders into correct place
    Former-commit-id: eb1c1e493b9d5fa7b7b30117cf1e8170cbfbb1fe

--- ifs-data-service/src/test/java/com/worth/ifs/project/finance/builder/FinanceCheckResourceBuilder.java
+++ ifs-data-service/src/test/java/com/worth/ifs/project/finance/builder/FinanceCheckResourceBuilder.java
@@ -1,17 +1,12 @@
-package com.worth.ifs.project.builder;
+package com.worth.ifs.project.finance.builder;
 
 import com.worth.ifs.BaseBuilder;
-import com.worth.ifs.application.builder.ApplicationBuilder;
-import com.worth.ifs.project.finance.domain.FinanceCheck;
-import com.worth.ifs.project.finance.domain.SpendProfile;
 import com.worth.ifs.project.finance.resource.CostGroupResource;
 import com.worth.ifs.project.finance.resource.FinanceCheckResource;
-import com.worth.ifs.user.domain.Organisation;
 
 import java.util.List;
 import java.util.function.BiConsumer;
 
-import static com.worth.ifs.BaseBuilderAmendFunctions.idBasedNames;
 import static com.worth.ifs.BuilderAmendFunctions.setField;
 import static com.worth.ifs.BuilderAmendFunctions.uniqueIds;
 import static java.util.Collections.emptyList;
--- ifs-data-service/src/test/java/com/worth/ifs/project/finance/controller/FinanceCheckControllerTest.java
+++ ifs-data-service/src/test/java/com/worth/ifs/project/finance/controller/FinanceCheckControllerTest.java
@@ -1,24 +1,65 @@
 package com.worth.ifs.project.finance.controller;
 
 import com.worth.ifs.BaseControllerMockMVCTest;
+import com.worth.ifs.commons.rest.RestResult;
 import com.worth.ifs.project.controller.FinanceCheckController;
+import com.worth.ifs.project.finance.resource.FinanceCheckResource;
+import com.worth.ifs.project.finance.resource.FinanceCheckSummaryResource;
+import com.worth.ifs.project.finance.workflow.financechecks.resource.FinanceCheckProcessResource;
+import com.worth.ifs.project.resource.ProjectOrganisationCompositeId;
 import org.junit.Test;
+
+import static com.worth.ifs.commons.service.ServiceResult.serviceSuccess;
+import static com.worth.ifs.project.finance.builder.FinanceCheckResourceBuilder.newFinanceCheckResource;
+import static com.worth.ifs.project.finance.builder.FinanceCheckProcessResourceBuilder.newFinanceCheckProcessResource;
+import static com.worth.ifs.project.finance.builder.FinanceCheckSummaryResourceBuilder.newFinanceCheckSummaryResource;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.when;
 
 public class FinanceCheckControllerTest extends BaseControllerMockMVCTest<FinanceCheckController> {
 
     @Test
-    public void testGenerateFinanceCheck(){
-        // TODO RP
-    }
-
-    @Test
-    public void testSaveFinanceCheck() {
-        // TODO RP
-    }
-
-    @Test
     public void testGetFinanceCheck() {
-        // TODO RP
+        Long projectId = 123L;
+        Long organisationId = 456L;
+        ProjectOrganisationCompositeId projectOrganisationCompositeId = new ProjectOrganisationCompositeId(projectId, organisationId);
+        when(financeCheckServiceMock.getByProjectAndOrganisation(projectOrganisationCompositeId)).thenReturn(serviceSuccess(newFinanceCheckResource().build()));
+        RestResult<FinanceCheckResource> result = controller.getFinanceCheck(projectId, organisationId);
+        assertTrue(result.isSuccess());
+    }
+
+    @Test
+    public void testGetFinanceCheckSummary(){
+        Long projectId = 123L;
+        when(financeCheckServiceMock.getFinanceCheckSummary(projectId)).thenReturn(serviceSuccess(newFinanceCheckSummaryResource().build()));
+        RestResult<FinanceCheckSummaryResource> result = controller.getFinanceCheckSummary(projectId);
+        assertTrue(result.isSuccess());
+    }
+
+    @Test
+    public void testGetFinanceCheckApprovalStatus(){
+        Long projectId = 123L;
+        Long organisationId = 456L;
+        when(financeCheckServiceMock.getFinanceCheckApprovalStatus(projectId, organisationId)).thenReturn(serviceSuccess(newFinanceCheckProcessResource().build()));
+        RestResult<FinanceCheckProcessResource> result = controller.getFinanceCheckApprovalStatus(projectId, organisationId);
+        assertTrue(result.isSuccess());
+    }
+
+    @Test
+    public void testApproveFinanceCheck(){
+        Long projectId = 123L;
+        Long organisationId = 456L;
+        when(financeCheckServiceMock.approve(projectId, organisationId)).thenReturn(serviceSuccess());
+        RestResult<Void> result = controller.approveFinanceCheck(projectId, organisationId);
+        assertTrue(result.isSuccess());
+    }
+
+    @Test
+    public void testUpdateFinanceCheck() {
+        FinanceCheckResource financeCheckResource = newFinanceCheckResource().build();
+        when(financeCheckServiceMock.save(financeCheckResource)).thenReturn(serviceSuccess());
+        RestResult<Void> result = controller.updateFinanceCheck(financeCheckResource);
+        assertTrue(result.isSuccess());
     }
 
     @Override
