117d908b1e4706187882ce99c4121be4c405cc70
Wouter de Meijer <wdemeijer@worth.systems>
2016-10-11 15:52:36 +0200

    INFUND-5521 - Added temporary security annotation for ContractService getCurrent service function
    Former-commit-id: 3e172a177b49913bebde0ae25ad2f20f25aa6fa4

--- ifs-data-service/src/main/java/com/worth/ifs/user/transactional/ContractService.java
+++ ifs-data-service/src/main/java/com/worth/ifs/user/transactional/ContractService.java
@@ -1,5 +1,6 @@
 package com.worth.ifs.user.transactional;
 
+import com.worth.ifs.commons.security.NotSecured;
 import com.worth.ifs.commons.service.ServiceResult;
 import com.worth.ifs.user.resource.ContractResource;
 
@@ -7,5 +8,7 @@ import com.worth.ifs.user.resource.ContractResource;
  * Transactional and secured service providing operations around {@link com.worth.ifs.user.domain.Contract} data.
  */
 public interface ContractService {
+    //TODO: review security annotation
+    @NotSecured(value = "Still needs to be secured", mustBeSecuredByOtherServices = false)
     ServiceResult<ContractResource> getCurrent();
 }
