c2a3c0178234d7883382ade453e8594d3f96d867
glenn <gauld@worth.systems>
2016-10-11 11:18:15 +0100

    INFUND-5381 Updates to fix failing tests.
    Former-commit-id: 96a91bb8edb59e8525363be972dffa7eae4dbf18

--- ifs-data-service/src/test/java/com/worth/ifs/documentation/SpendProfileDocs.java
+++ ifs-data-service/src/test/java/com/worth/ifs/documentation/SpendProfileDocs.java
@@ -12,7 +12,7 @@ public class SpendProfileDocs {
             fieldWithPath("monthlyCostsPerCategoryMap").description("Map which holds costs per month per category on the Spend Profile page"),
             fieldWithPath("months[]").description("List of months covered in the Spend Profile"),
             fieldWithPath("markedAsComplete").description("Whether spend profile has been marked as complete or not"),
-            fieldWithPath("validationMessages").description("Validation messages pertaining to spend profile table"),
+            fieldWithPath("validationMessages").description("Validation messages pertaining to spend profile table")
     };
 
     public static final FieldDescriptor[] spendProfileCSVFields = {
