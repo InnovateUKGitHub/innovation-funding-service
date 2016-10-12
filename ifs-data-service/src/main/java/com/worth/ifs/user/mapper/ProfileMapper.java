4d7f452f525968c378278154f562b0df3c28788e
Ben Outram <ben@hiveit.co.uk>
2016-10-12 13:16:25 +0100

    INFUND-5381 Removing unused ProfileRepository.
    Former-commit-id: 938c468ea3044af96cfe51b04f8a42050aa35cc8

--- ifs-data-service/src/main/java/com/worth/ifs/user/mapper/ProfileMapper.java
+++ ifs-data-service/src/main/java/com/worth/ifs/user/mapper/ProfileMapper.java
@@ -4,28 +4,20 @@ import com.worth.ifs.address.mapper.AddressMapper;
 import com.worth.ifs.commons.mapper.BaseMapper;
 import com.worth.ifs.commons.mapper.GlobalMapperConfig;
 import com.worth.ifs.user.domain.Profile;
-import com.worth.ifs.user.domain.User;
 import com.worth.ifs.user.resource.ProfileResource;
-import com.worth.ifs.user.resource.UserResource;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Mappings;
 
 @Mapper(
-        config =  GlobalMapperConfig.class,
+        config = GlobalMapperConfig.class,
         uses = {
                 AddressMapper.class,
                 UserMapper.class,
                 ContractMapper.class
         }
 )
-public abstract class ProfileMapper extends BaseMapper<Profile, ProfileResource, Long> {
-
-    @Mappings({
-            @Mapping(target = "user", ignore = true ),
-    })
-    @Override
-    public abstract ProfileResource mapToResource(Profile domain);
+public abstract class ProfileMapper {
 
     public Long mapProfileToId(Profile object) {
         if (object == null) {
@@ -35,7 +27,18 @@ public abstract class ProfileMapper extends BaseMapper<Profile, ProfileResource,
     }
 
     public Profile createEntity() {
-        return createDefault(Profile.class);
+        return BaseMapper.createDefault(Profile.class);
     }
+
+    @Mappings({
+            @Mapping(target = "user", ignore = true),
+    })
+    public abstract ProfileResource mapToResource(Profile domain);
+
+    public abstract Iterable<ProfileResource> mapToResource(Iterable<Profile> domain);
+
+    public abstract Profile mapToDomain(ProfileResource resource);
+
+    public abstract Iterable<Profile> mapToDomain(Iterable<ProfileResource> resource);
 }
 
