ccab8c42082f0d4b6715d33b5974d381800e515a
Matt Lawson <github@lawlesscreation.com>
2016-10-12 13:20:48 +0100

    update script to re-number added/removed rows and added the error message dor required if fields
    Former-commit-id: 513622f0d40d92eda3c981ed73f86e6c2449cbaf

--- ifs-web-service/ifs-assessment-service/src/main/resources/static/js/ifs_modules/repeatableDeclarationRows.js
+++ ifs-web-service/ifs-assessment-service/src/main/resources/static/js/ifs_modules/repeatableDeclarationRows.js
@@ -53,7 +53,30 @@ IFS.assessment.repeatableDeclarationRows = (function() {
             jQuery(newRow).find('input').first().focus();
         },
         removeRow : function(el){
+            var rowParent = jQuery(el).closest('tbody');
+            var rows = [];
+
             jQuery(el).closest('tr').remove();
+
+            //re-number rows to ensure no empty/missing data is created server-side
+            rows = jQuery(rowParent).children();
+
+            if(rows.length !== 0){
+                jQuery(rows).each(function(rowIndex){
+                    var rowsInputs = jQuery(this).find('input');
+
+                    jQuery(rowsInputs).each(function(){
+                        //regex will replace 1 or more numbers in the string with the new index value
+                        var newId = jQuery(this).attr('id').replace(/\d+/g, rowIndex);
+                        var newName = jQuery(this).attr('name').replace(/\d+/g, rowIndex);
+
+                        jQuery(this).attr({
+                            'id': newId,
+                            'name': newName
+                        });
+                    });
+                });
+            }
         }
     };
 })();
