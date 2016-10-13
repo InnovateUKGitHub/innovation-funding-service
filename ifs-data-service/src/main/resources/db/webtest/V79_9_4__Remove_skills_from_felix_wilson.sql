13c59f0ba139255bc61b74653171ef1a5410b8ad
Wouter de Meijer <wdemeijer@worth.systems>
2016-10-13 17:27:25 +0200

    INFUND-5411 - Changed flyway patch to make use of user_id instead of id for selection
    Former-commit-id: 84ff47c9921776a2c7472ec68dbaf52370d5f3c9

--- ifs-data-service/src/main/resources/db/webtest/V79_9_4__Remove_skills_from_felix_wilson.sql
+++ ifs-data-service/src/main/resources/db/webtest/V79_9_4__Remove_skills_from_felix_wilson.sql
@@ -1,2 +1,2 @@
 -- Remove skills_areas and business_type entries from felix wilson
-UPDATE `profile` SET `skills_areas`=NULL, `business_type`=NULL WHERE `id`='2'
+UPDATE `profile` SET `skills_areas`=NULL, `business_type`=NULL WHERE `user_id`='9'
