a00f150811ff3255a2f93cb9a238ca49ec83cfd8
Luke Harper <lukeharperipl@gmail.com>
2016-10-13 10:13:08 +0100

    INFUND-5519 Fixing failing acc tests and removing imports.
    Former-commit-id: 5ce8717cb5470fece8c4c71c19292d35fcfe8ed8

--- ifs-web-service/ifs-project-setup-mgt-service/src/main/java/com/worth/ifs/bankdetails/form/ApproveBankDetailsForm.java
+++ ifs-web-service/ifs-project-setup-mgt-service/src/main/java/com/worth/ifs/bankdetails/form/ApproveBankDetailsForm.java
@@ -1,16 +1,5 @@
 package com.worth.ifs.bankdetails.form;
 
-import com.worth.ifs.address.resource.OrganisationAddressType;
-import com.worth.ifs.form.AddressForm;
-import org.apache.commons.lang3.builder.EqualsBuilder;
-import org.apache.commons.lang3.builder.HashCodeBuilder;
-import org.hibernate.validator.constraints.NotEmpty;
-
-import javax.validation.Valid;
-import javax.validation.constraints.Pattern;
-
-import static com.worth.ifs.address.resource.OrganisationAddressType.BANK_DETAILS;
-
 /**
  * Empty form class to provide errors to template if approving bank details fails.
  */
