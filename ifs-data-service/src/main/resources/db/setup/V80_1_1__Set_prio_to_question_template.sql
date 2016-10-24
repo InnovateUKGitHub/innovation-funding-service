8751657aa2ca6dddea46761349231efe69a07f29
Reinaud van Rumpt <rvanrumpt@worth.systems>
2016-10-24 14:59:54 +0100

    INFUND-5837 Applicationquestions ordering fix
    Former-commit-id: 4ad45653d21faaff22a9eb851839ac3f0fd5c084

---
+++ ifs-data-service/src/main/resources/db/setup/V80_1_1__Set_prio_to_question_template.sql
@@ -0,0 +1,4 @@
+UPDATE `question_template` SET `priority`='9' WHERE `id`='13';
+UPDATE `question_template` SET `priority`='10' WHERE `id`='14';
+UPDATE `question_template` SET `priority`='10' WHERE `id`='49';
+UPDATE `question_template` SET `priority`='9' WHERE `id`='48';
