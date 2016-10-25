f22bb2d631010cbe0bcb28c2f7c3c2e007a56c67
skistapur <sainath.kistapur@wtg.co.uk>
2016-10-25 16:34:21 +0100

    Temp commit
    Former-commit-id: f7298c6c10122216ae944345f000a2c2da2399b3

---
+++ ifs-web-service/ifs-competition-mgt-service/src/main/java/com/worth/ifs/competitionsetup/controller/CompetitionSetupApplicationController.java
@@ -0,0 +1,39 @@
+package com.worth.ifs.competitionsetup.controller;
+
+import com.worth.ifs.application.service.CompetitionService;
+import com.worth.ifs.competition.resource.CompetitionResource;
+import com.worth.ifs.competition.resource.CompetitionSetupSection;
+import com.worth.ifs.competitionsetup.service.CompetitionSetupService;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Controller to manage the Application Questions and it's sub-sections in the
+ * competition setup process
+ */
+@Controller
+@RequestMapping("/competition/setup/{competitionId}/section/application")
+public class CompetitionSetupApplicationController {
+
+    private static final Log LOG = LogFactory.getLog(CompetitionSetupApplicationController.class);
+    private static final String COMPETITION_ID_KEY = "competitionId";
+
+    @Autowired
+    private CompetitionSetupService competitionSetupService;
+
+    @Autowired
+    private CompetitionService competitionService;
+
+    @RequestMapping(value = "/lading-page", method = RequestMethod.GET)
+    public String initCompetitionSetupSection(Model model, @PathVariable(COMPETITION_ID_KEY) Long competitionId) {
+        CompetitionResource competitionResource = competitionService.getById(competitionId);
+        competitionSetupService.populateCompetitionSectionModelAttributes(model, competitionResource, CompetitionSetupSection.APPLICATION_FORM);
+        return "";
+    }
+}
