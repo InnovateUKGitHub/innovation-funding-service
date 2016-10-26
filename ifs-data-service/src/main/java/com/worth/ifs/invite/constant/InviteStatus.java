dc1ebc1bc3a6886181f6c436a841e94526653ef1
Wouter de Meijer <wdemeijer@worth.systems>
2016-10-26 12:37:35 +0200

    INFUND-5735 - Split up flyway patches, refactored enum to exclude id's and removed unnecessary CompetitionParticipantRoleConverter.
    Former-commit-id: c48298c25ecb211b9892673614eef69b13313797

--- ifs-data-service/src/main/java/com/worth/ifs/invite/constant/InviteStatus.java
+++ ifs-data-service/src/main/java/com/worth/ifs/invite/constant/InviteStatus.java
@@ -5,23 +5,7 @@ package com.worth.ifs.invite.constant;
  * The value of these entries are used when saving to and interpreting from the database.
  */
 public enum InviteStatus {
-    SENT(1, "SENT"),
-    CREATED(2, "CREATED"),
-    OPENED(3, "OPENED");
-
-    private long id;
-    private String name;
-
-    InviteStatus(long id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
+    SENT,
+    CREATED,
+    OPENED;
 }
--- ifs-data-service/src/main/java/com/worth/ifs/invite/domain/CompetitionParticipantRole.java
+++ ifs-data-service/src/main/java/com/worth/ifs/invite/domain/CompetitionParticipantRole.java
@@ -10,25 +10,6 @@ import static com.worth.ifs.commons.util.enums.Identifiable.toIdMap;
 /**
  * The role of a {@link CompetitionParticipant}.
  */
-public enum CompetitionParticipantRole implements ParticipantRole<Competition>, Identifiable {
-    ASSESSOR(1, "ASSESSOR");
-
-    private final long id;
-    private final String name;
-
-    private static final Map<Long, CompetitionParticipantRole> idMap = toIdMap(values());
-
-    CompetitionParticipantRole(final long id, final String name) {
-        this.id = id;
-        this.name = name;
-    }
-
-    @Override
-    public long getId() {
-        return id;
-    }
-
-    public static CompetitionParticipantRole getById(long id) {
-        return idMap.get(id);
-    }
+public enum CompetitionParticipantRole implements ParticipantRole<Competition> {
+    ASSESSOR;
 }
