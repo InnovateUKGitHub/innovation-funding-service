966fa4efc74ad45b326d011591412b9cbd1f114e
Nuno Alexandre <nalexandre@worth.systems>
2016-10-27 18:03:31 +0200

    Moving base rest base classes to API
    Former-commit-id: 65fc437472b07ee7132c9da680425ccb9622ffd3

--- ifs-rest-api-service/src/main/java/com/worth/ifs/commons/service/AnonymousUserRestTemplateAdaptor.java
+++ ifs-rest-api-service/src/main/java/com/worth/ifs/commons/service/AnonymousUserRestTemplateAdaptor.java
@@ -3,7 +3,6 @@ package com.worth.ifs.commons.service;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Component;
-
 import static com.worth.ifs.commons.security.UidAuthenticationService.AUTH_TOKEN;
 
 /**
