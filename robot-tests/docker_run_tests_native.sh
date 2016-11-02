26672cdec0f8731d36594bbd5dd813ef2891f636
Nicholas Tsim <nick@hiveit.co.uk>
2016-11-02 12:01:06 +0000

    INFUND-5960 - Fixes docker_run_tests_native.sh not copying the pretend virus file into the Docker container and provides much nicer presentation during execution
    Former-commit-id: 557049ffcb59f92211512c5d9e896a529642ddd9

--- robot-tests/docker_run_tests_native.sh
+++ robot-tests/docker_run_tests_native.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-function coloredEcho(){
+# Define some functions for later use
+
+function coloredEcho() {
     local exp=$1;
     local color=$2;
-    if ! [[ $color =~ '^[0-9]$' ]] ; then
-       case $(echo $color | tr '[:upper:]' '[:lower:]') in
+    if ! [[ ${color} =~ '^[0-9]$' ]] ; then
+       case $(echo ${color} | tr '[:upper:]' '[:lower:]') in
         black) color=0 ;;
         red) color=1 ;;
         green) color=2 ;;
@@ -15,140 +17,186 @@ function coloredEcho(){
         white|*) color=7 ;; # white or invalid color
        esac
     fi
-    tput setaf $color;
-    echo $exp;
+    tput setaf ${color};
+    echo ${exp};
     tput sgr0;
 }
 
-function clearDownFileRepository() {
-    echo "***********Deleting any uploaded files***************"
-    echo "storedFileFolder:   ${storedFileFolder}"
-    docker exec ifs_data_1  rm -rf ${storedFileFolder}
+function section() {
+    echo
+    coloredEcho "$1" green
+    echo
+}
 
-    echo "***********Deleting any holding for scan files***************"
-    echo "virusScanHoldingFolder: ${virusScanHoldingFolder}"
-    docker exec ifs_data_1  rm -rf ${virusScanHoldingFolder}
+function startContainers() {
+    section "=> STARTING DOCKER CONTAINERS"
 
-    echo "***********Deleting any quarantined files***************"
-    echo "virusScanQuarantinedFolder: ${virusScanQuarantinedFolder}"
-    docker exec ifs_data_1  rm -rf ${virusScanQuarantinedFolder}
-
-    echo "***********Deleting any scanned files***************"
-    echo "virusScanScannedFolder: ${virusScanScannedFolder}"
-    docker exec ifs_data_1  rm -rf ${virusScanScannedFolder}
+    cd ${dockerDir}
+    docker-compose -p ifs start
 }
 
 function addTestFiles() {
-    clearDownFileRepository
-    echo "***********Adding test files***************"
-    echo "***********Making the quarantined directory ***************"
+    section "=> CLEANING TEST FILE REPOSITORIES IN DOCKER CONTAINER"
+
+    echo "storedFileFolder:                 ${storedFileFolder}"
+    echo "virusScanHoldingFolder:           ${virusScanHoldingFolder}"
+    echo "virusScanQuarantinedFolder:       ${virusScanQuarantinedFolder}"
+    echo "virusScanScannedFolder:           ${virusScanScannedFolder}"
+    echo
+
+    echo "=> Deleting any uploaded files..."
+    docker exec ifs_data_1 rm -rf ${storedFileFolder}
+
+    echo "=> Deleting any holding for scan files..."
+    docker exec ifs_data_1 rm -rf ${virusScanHoldingFolder}
+
+    echo "=> Deleting any quarantined files..."
+    docker exec ifs_data_1 rm -rf ${virusScanQuarantinedFolder}
+
+    echo "=> Deleting any scanned files..."
+    docker exec ifs_data_1 rm -rf ${virusScanScannedFolder}
+
+    section "=> ADDING TEST FILES INTO DOCKER CONTAINER"
+
+    echo "=> Making the quarantined directory..."
     docker exec ifs_data_1 mkdir -p ${virusScanQuarantinedFolder}
-    echo "***********Adding pretend quarantined file ***************"
-    docker exec ifs_data_1 cp /tmp/ifs-local/8 ${virusScanQuarantinedFolder}/8
+    echo "=> Adding pretend quarantined file..."
+    docker cp ${uploadFileDir}/8 ifs_data_1:${virusScanQuarantinedFolder}/8
 }
 
-function resetLDAP() {
-    cd ../setup-files/scripts/docker-native
+function resetDB() {
+    section "=> RESETTING DATABASE STATE"
+
+    cd ${dataServiceCodeDir}
+    ./gradlew -Pprofile=docker flywayClean flywayMigrate
+
+    echo "=> Syncing Shib users..."
+    # Sync the users in Shibboleth whilst we're doing this
+    cd ${dockerDir}
     ./syncShib.sh
 }
 
-function resetDB(){
-  cd ${dataServiceCodeDir}
-  ./gradlew -Pprofile=docker-native flywayClean flywayMigrate
-  resetLDAP
-}
-
 function buildAndDeploy() {
-    echo "********BUILD AND DEPLOY THE APPLICATION********"
+    section "=> BUILDING AND DEPLOYING APPLICATION"
+
+    if [[ ${noDeploy} -eq 1 ]]
+    then
+        coloredEcho "=> No Deploy flag used. Skipping build and deploy..." yellow
+        return
+    fi
+
     cd ${dataServiceCodeDir}
+
     ## Before we start the build we need to have an webtest test build environment
-    echo "********SWAPPING IN THE WEBTEST BUILD PROPERTIES********"
-    sed 's/ext\.ifsFlywayLocations.*/ext\.ifsFlywayLocations="db\/migration,db\/setup,db\/webtest"/' docker-native-build.gradle > webtest.gradle.tmp
-    mv docker-native-build.gradle docker-native-build.gradle.tmp
-    mv webtest.gradle.tmp docker-native-build.gradle
-    ./gradlew -Pprofile=docker-native clean deployToTomcat
+    echo "=> Swapping in the webtest build properties..."
+
+    sed 's/ext\.ifsFlywayLocations.*/ext\.ifsFlywayLocations="db\/migration,db\/setup,db\/webtest"/' docker-build.gradle > webtest.gradle.tmp
+    mv docker-build.gradle docker-build.gradle.tmp
+    mv webtest.gradle.tmp docker-build.gradle
+
+    echo "=> Starting build and deploy script..."
+
+    cd ${dockerDir}
+    ./deploy.sh all
+    wait
+    sleep 5
+
     ## Replace the webtest build environment with the one we had before.
-    echo "********SWAPPING BACK THE ORIGINAL BUILD PROPERTIES********"
-    mv docker-native-build.gradle.tmp docker-native-build.gradle
+    echo "=> Swapping back original build properties..."
+    cd ${dataServiceCodeDir}
+    mv docker-build.gradle.tmp docker-build.gradle
 
-    cd ${webServiceCodeDir}
-    ./gradlew -Pprofile=docker-native clean deployToTomcat
-
-    echo "**********WAIT FOR SUCCESSFUL DEPLOYMENT OF THE DATASERVICE**********"
-    docker-compose -p ifs logs -ft --tail 10 data | while read logLine
-    do
-      [[ "${logLine}" == *"Deployment of web application archive"* ]] && pkill -P $$ tail
-    done
-    echo "**********WAIT FOR SUCCESSFUL DEPLOYMENT OF THE WEBSERVICE**********"
-    docker-compose -p ifs logs -ft --tail 10 web | while read logLine
-    do
-      [[ "${logLine}" == *"Deployment of web application archive"* ]] && pkill -P $$ tail
-    done
-
+#    cd ${webServiceCodeDir}
+#    echo "**********WAIT FOR SUCCESSFUL DEPLOYMENT OF THE DATASERVICE**********"
+#    docker-compose -p ifs logs -ft --tail 10 data | while read logLine
+#    do
+#      [[ "${logLine}" == *"Deployment of web application archive"* ]] && pkill -P $$ tail
+#    done
+#    echo "**********WAIT FOR SUCCESSFUL DEPLOYMENT OF THE WEBSERVICE**********"
+#    docker-compose -p ifs logs -ft --tail 10 web | while read logLine
+#    do
+#      [[ "${logLine}" == *"Deployment of web application archive"* ]] && pkill -P $$ tail
+#    done
 }
 
 function startSeleniumGrid() {
-    cd ../robot-tests
-    cd ${testDirectory}
+    section "=> STARTING SELENIUM GRID"
+
     cd ${scriptDir}
 
-    if [[ $parallel -eq 1 ]]
+    if [[ ${parallel} -eq 1 ]]
     then
       declare -i suiteCount=$(find ${testDirectory}/* -maxdepth 0 -type d | wc -l)
     else
       declare -i suiteCount=1
     fi
-    if [[ $suiteCount -eq 0 ]]
+    if [[ ${suiteCount} -eq 0 ]]
     then
       suiteCount=1
     fi
-    echo ${suiteCount}
+
+    echo "=> Suite count: ${suiteCount}"
+
     docker-compose -p robot up -d --force-recreate --build
     docker-compose -p robot scale chrome=${suiteCount}
+
     unset suiteCount
-    if [[ $quickTest -eq 1 ]]
+    if [[ ${quickTest} -eq 1 ]]
     then
-      echo "waiting 5 seconds for the grid to be properly started"
+      echo "=> Waiting 5 seconds for the grid to be properly started"
       sleep 5
     fi
   }
 
 function stopSeleniumGrid() {
-    cd ../robot-tests
-    cd ${testDirectory}
+    section "=> STOPPING SELENIUM GRID"
+
     cd ${scriptDir}
     docker-compose -p robot down -v --remove-orphans
 }
 
 function startPybot() {
-    echo "********** starting pybot for ${1} **************"
+    section "=> STARTING PYBOT FOR ${1}"
+
     targetDir=`basename ${1}`
+
     if [[ "$happyPath" ]]
       then
         local includeHappyPath='--include HappyPath'
       else
         local includeHappyPath=''
     fi
-    if [[ $emails -eq 1 ]]
+    if [[ ${emails} -eq 1 ]]
       then
         local emailsString='--exclude Email'
       else
         local emailsString=''
     fi
-    if [[ $rerunFailed -eq 1 ]]; then
+    if [[ ${rerunFailed} -eq 1 ]]; then
       local rerunString='--rerunfailed target/${targetDir}/output.xml --output rerun.xml'
     else
       local rerunString=''
     fi
 
-    pybot --outputdir target/${targetDir} ${rerunString} --pythonpath IFS_acceptance_tests/libs -v docker:1 -v SERVER_BASE:$webBase -v PROTOCOL:'https://' -v POSTCODE_LOOKUP_IMPLEMENTED:${postcodeLookupImplemented} -v UPLOAD_FOLDER:${uploadFileDir} -v DOWNLOAD_FOLDER:download_files -v BROWSER=chrome -v REMOTE_URL:'http://ifs-local-dev:4444/wd/hub' ${includeHappyPath} --exclude Failing --exclude Pending --exclude FailingForLocal --exclude PendingForLocal ${emailsString} --name ${targetDir} ${1} &
+    pybot --outputdir target/${targetDir} ${rerunString} --pythonpath IFS_acceptance_tests/libs \
+    -v docker:1 \
+    -v SERVER_BASE:${webBase} \
+    -v PROTOCOL:'https://' \
+    -v POSTCODE_LOOKUP_IMPLEMENTED:${postcodeLookupImplemented} \
+    -v UPLOAD_FOLDER:${uploadFileDir} \
+    -v DOWNLOAD_FOLDER:download_files \
+    -v BROWSER=chrome \
+    -v REMOTE_URL:'http://ifs-local-dev:4444/wd/hub' \
+    $includeHappyPath \
+    --exclude Failing --exclude Pending --exclude FailingForLocal --exclude PendingForLocal ${emailsString} --name ${targetDir} ${1} &
 }
 
 function runTests() {
-    echo "**********RUN THE WEB TESTS**********"
+    section "=> RUNNING THE WEB TESTS"
+
     cd ${scriptDir}
 
-    if [[ $parallel -eq 1 ]]
+    if [[ ${parallel} -eq 1 ]]
     then
       for D in `find ${testDirectory}/* -maxdepth 0 -type d`
       do
@@ -163,7 +211,7 @@ function runTests() {
         wait $job
     done
 
-    if [[ $parallel -eq 1 ]]
+    if [[ ${parallel} -eq 1 ]]
     then
       results=`find target/* -regex ".*/output\.xml"`
       rebot -d target ${results}
@@ -171,45 +219,65 @@ function runTests() {
 }
 
 function clearOldReports() {
-  echo "**********REMOVING OLD REPORTS**********"
+  section "=> REMOVING OLD REPORTS"
   rm -rf target
   mkdir target
 }
 
-cd "$(dirname "$0")"
-echo "********GETTING ALL THE VARIABLES********"
-scriptDir=`pwd`
-echo "scriptDir:        ${scriptDir}"
-uploadFileDir="${scriptDir}/upload_files"
-cd ../ifs-data-service
-dataServiceCodeDir=`pwd`
-echo "dataServiceCodeDir:${dataServiceCodeDir}"
+# ====================================
+# The actual start point of our script
+# ====================================
 
+section "=> GETTING SCRIPT VARIABLES"
+
+#cd "$(dirname "$0")"
+scriptDir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+cd ${scriptDir}
+cd ..
+
+rootDir=`pwd`
+dockerDir="${rootDir}/setup-files/scripts/docker-native"
+
+dataServiceCodeDir="${rootDir}/ifs-data-service"
+webServiceCodeDir="${rootDir}/ifs-web-service"
+webBase="ifs-local-dev"
+
+uploadFileDir="${scriptDir}/upload_files"
 baseFileStorage="/tmp/uploads"
-echo "${baseFileStorage}"
 storedFileFolder="${baseFileStorage}/ifs/"
 virusScanHoldingFolder="${baseFileStorage}/virus-scan-holding/"
 virusScanQuarantinedFolder="${baseFileStorage}/virus-scan-quarantined"
 virusScanScannedFolder="${baseFileStorage}/virus-scan-scanned"
 
-echo "virusScanQuarantinedFolder:		${virusScanQuarantinedFolder}"
-echo "We are about to delete the above directories, make sure that they are right ones!"
-postcodeLookupKey=`sed '/^\#/d' docker-native-build.gradle | grep 'ext.postcodeLookupKey'  | cut -d "=" -f2 | sed 's/"//g'`
-echo "Postcode Lookup: 		${postcodeLookupKey}"
-if [ "$postcodeLookupKey" = '' ]
+postcodeLookupKey=`sed '/^\#/d' ${dataServiceCodeDir}/docker-build.gradle | grep 'ext.postcodeLookupKey'  | cut -d "=" -f2 | sed 's/"//g'`
+
+echo "scriptDir:                    ${scriptDir}"
+echo "rootDir:                      ${rootDir}"
+echo "dockerDir:                    ${dockerDir}"
+echo "dataServiceCodeDir:           ${dataServiceCodeDir}"
+echo "webServiceCodeDir:            ${webServiceCodeDir}"
+echo "webBase:                      ${webBase}"
+echo "uploadFileDir:                ${uploadFileDir}"
+echo
+echo "baseFileStorage:              ${baseFileStorage}"
+echo "virusScanHoldingFolder:       ${virusScanHoldingFolder}"
+echo "virusScanQuarantinedFolder:   ${virusScanQuarantinedFolder}"
+echo "virusScanScannedFolder:       ${virusScanScannedFolder}"
+
+echo
+coloredEcho "=> We are about to delete the above storage directories in Docker, make sure that they are right ones!" yellow
+echo
+
+if [ "${postcodeLookupKey}" = '' ]
 then
-    echo "Postcode lookup not implemented"
+    coloredEcho "=> Postcode lookup not implemented" blue
     postcodeLookupImplemented='NO'
 else
-    echo "Postcode lookup implemented. The tests will expect proper data from the SuT."
+    echo "postcodeLookupKey:        ${postcodeLookupKey}"
+    coloredEcho "=> Postcode lookup implemented. The tests will expect proper data from the SuT." blue
     postcodeLookUpImplemented='YES'
 fi
-cd ../ifs-web-service
-webServiceCodeDir=`pwd`
-echo "webServiceCodeDir: ${webServiceCodeDir}"
-webBase="ifs-local-dev"
-echo "webBase:           ${webBase}"
-
 
 unset opt
 unset testScrub
@@ -219,48 +287,52 @@ emails=0
 rerunFailed=0
 parallel=0
 stopGrid=0
+noDeploy=0
 
 testDirectory='IFS_acceptance_tests/tests'
-while getopts ":p :h :q :t :e :r :c :d:" opt ; do
-    case $opt in
+while getopts ":p :q :h :t :e :r :d :c :n:" opt ; do
+    case ${opt} in
         p)
-          parallel=1
+            parallel=1
         ;;
         q)
-          quickTest=1
+            quickTest=1
         ;;
         h)
-          happyPath=1
+            happyPath=1
         ;;
         t)
-          testScrub=1
+            testScrub=1
         ;;
         e)
-          emails=1
+            emails=1
         ;;
         r)
-		      rerunFailed=1
-    		;;
-    		d)
-           testDirectory="$OPTARG"
-           parallel=0
+		    rerunFailed=1
+    	;;
+    	d)
+            testDirectory="$OPTARG"
+            parallel=0
         ;;
         c)
-          stopGrid=1
+            stopGrid=1
+        ;;
+        n)
+            noDeploy=1
         ;;
         \?)
-           coloredEcho "Invalid option: -$OPTARG" red >&2
-           exit 1
+            coloredEcho "=> Invalid option: -$OPTARG" red >&2
+            exit 1
         ;;
         :)
-          case $OPTARG in
-         	  d)
-             coloredEcho "Option -$OPTARG requires the location of the robottest files relative to $scriptDir." red >&2
-            ;;
-            *)
-             coloredEcho "Option -$OPTARG requires an argument." red >&2
-            ;;
-          esac
+            case $OPTARG in
+           	    d)
+                 coloredEcho "=> Option -$OPTARG requires the location of the robottest files relative to ${scriptDir}." red >&2
+                ;;
+                *)
+                 coloredEcho "=> Option -$OPTARG requires an argument." red >&2
+                ;;
+            esac
         exit 1
         ;;
     esac
@@ -268,37 +340,44 @@ done
 
 startSeleniumGrid
 
-
-if [[ $rerunFailed -eq 0 ]]
+if [[ ${rerunFailed} -eq 0 ]]
 then
-clearOldReports
+    clearOldReports
 fi
 
-if [[ $quickTest -eq 1 ]]
+if [[ ${quickTest} -eq 1 ]]
 then
-  echo "using quickTest:   TRUE" >&2
-  runTests
-elif [[ "$testScrub" ]]
+    coloredEcho "=> Using quickTest: TRUE" blue
+
+    runTests
+elif [[ ${testScrub} ]]
 then
-  echo "using testScrub mode: this will do all the dirty work but omit the tests" >&2
-  resetDB
-  addTestFiles
+    coloredEcho "=> Using testScrub mode: this will do all the dirty work but omit the tests" blue
+
+    startContainers
+    resetDB
+    addTestFiles
+    buildAndDeploy
 else
-  echo "using quickTest:   FALSE" >&2
-  resetDB
-  addTestFiles
-  runTests
+    coloredEcho "=> Using quickTest: FALSE" blue
+
+    startContainers
+    resetDB
+    addTestFiles
+    buildAndDeploy
+    runTests
 fi
 
-if [[ $stopGrid -eq 1 ]]
+if [[ ${stopGrid} -eq 1 ]]
 then
-  stopSeleniumGrid
+    stopSeleniumGrid
 fi
+
 if [[ $(which google-chrome) ]]
 then
-  google-chrome target/${targetDir}/log.html &
+    google-chrome target/${targetDir}/log.html &
 else
-  wd=$(pwd)
-  logs="target/${targetDir}/log.html"
-  open "file://${wd}/${logs}"
+    wd=$(pwd)
+    logs="target/${targetDir}/log.html"
+    open "file://${wd}/${logs}"
 fi
