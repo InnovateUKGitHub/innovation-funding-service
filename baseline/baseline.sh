7d87daf1a1d47d7ccccbd7f11fde287b712b7a02
richard <richard@hiveit.co.uk>
2016-11-03 17:39:30 +0000

    INFUND-5942 more correction of scripts
    Former-commit-id: 794aef0a876cc8ce8a2acd75bace2b09c2442e56

--- baseline/baseline.sh
+++ baseline/baseline.sh
@@ -3,6 +3,7 @@
 ./replace_old_scripts_with_new.sh
 ./generate_schema_version_statement_for_uat_and_live.sh
 ## TODO test that the new scripts will work on a copy of LIVE
+./git_revert_flyway_scripts.sh
 
 
 
