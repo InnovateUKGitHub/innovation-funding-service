851c2b9b2e62cd1928b56a6afa8616181da946e9
IKENNA IBEANU <ikena.ibeanu@gmail.com>
2016-11-04 13:40:55 +0000

    Status activity is used
    Former-commit-id: 223fb01641429fe1ca869eec3f7b76c785687f92

--- ifs-data-service/src/main/java/com/worth/ifs/project/constant/ProjectActivityStates.java
+++ ifs-data-service/src/main/java/com/worth/ifs/project/constant/ProjectActivityStates.java
@@ -4,6 +4,7 @@ package com.worth.ifs.project.constant;
  * Enum used to represent states for project activities.  Used in resource objects for showing team status.
  */
 public enum  ProjectActivityStates {
+    NOT_REQUIRED,
     NOT_STARTED,
     ACTION_REQUIRED,
     PENDING,
