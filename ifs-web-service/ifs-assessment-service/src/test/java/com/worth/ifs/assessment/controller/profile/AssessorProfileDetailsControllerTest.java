beddbeda8a68c0e1a28ff0b53bcac0d35552db3d
Nicholas Tsim <nick@hiveit.co.uk>
2016-11-04 11:22:40 +0000

    INFUND-5943 - Adds additional validation error checking on submitDetails_partialRequest
    Former-commit-id: e6eb23bc2922ebd2f4458f1b5a2eae1800b26407

--- ifs-web-service/ifs-assessment-service/src/test/java/com/worth/ifs/assessment/controller/profile/AssessorProfileDetailsControllerTest.java
+++ ifs-web-service/ifs-assessment-service/src/test/java/com/worth/ifs/assessment/controller/profile/AssessorProfileDetailsControllerTest.java
@@ -242,6 +242,12 @@ public class AssessorProfileDetailsControllerTest extends BaseControllerMockMVCT
 
         assertEquals(2, bindingResult.getFieldErrorCount("firstName"));
         assertEquals(2, bindingResult.getFieldErrorCount("lastName"));
+
+        assertEquals("Your first name should have at least {2} characters", bindingResult.getFieldErrors("firstName").get(0).getDefaultMessage());
+        assertEquals("Please enter a first name", bindingResult.getFieldErrors("firstName").get(1).getDefaultMessage());
+
+        assertEquals("Your last name should have at least {2} characters", bindingResult.getFieldErrors("lastName").get(0).getDefaultMessage());
+        assertEquals("Please enter a last name", bindingResult.getFieldErrors("lastName").get(1).getDefaultMessage());
     }
 
     @Test
