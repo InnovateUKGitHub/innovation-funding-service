668ac74c361f4eec851c6cf71001271981262082
richard <richard@hiveit.co.uk>
2016-11-07 15:27:12 +0000

    INFUND-5942 fixing database name
    Former-commit-id: ce9cca78786c2ab468b08e986c914e1478aac2f6

--- baseline/generate_setup_scripts.sh
+++ baseline/generate_setup_scripts.sh
@@ -2,12 +2,12 @@ echo "**This script requires / does the following:"
 echo "**A local database with username: root and password: password"
 echo "**An export from the live database called ifs_current_live.sql"
 echo "Creating the database if it does not exist"
-echo "CREATE SCHEMA IF NOT EXISTS ifs_current_migrated" | mysql -uroot -ppassword 
+echo "CREATE SCHEMA IF NOT EXISTS ifs_current_live_migrated" | mysql -uroot -ppassword 
 echo "**Clearing the ifs_current_live_migrated database"
 cd ../ifs-data-service
 ./gradlew flywayClean -Dflyway.schemas=ifs_current_live_migrated
 echo "**Importing the live database into the ifs_current_live_migrated"
-mysql -uroot -ppassword ifs_current_live < ../baseline/ifs_current_live.sql
+mysql -uroot -ppassword ifs_current_live_migrated < ../baseline/ifs_current_live.sql
 echo "**Applying the latest patches to the live database"
 ./gradlew flywayMigrate -Dflyway.schemas=ifs_current_live_migrated -Dflyway.locations="db/migration,db/setup"
 echo "**Exporting the minimum data from the live"
@@ -16,7 +16,7 @@ cd ../baseline
 ## TODO remove spend_profile when we find a table we actually need
 mysqldump -uroot -ppassword ifs_current_live_migrated \
 			    spend_profile \
-                            --ignore-table=ifs_current_live.schema_version \
+                            --ignore-table=ifs_current_live_migrated.schema_version \
                             --no-create-info --extended-insert=false > "generated/db/setup/V0_1_4__Minimum_data.sql"
 
 
