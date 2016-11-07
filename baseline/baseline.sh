86af54db8de78d191036c27c022be162210809e2
richard <richard@hiveit.co.uk>
2016-11-07 18:02:49 +0000

    INFUND-5942 more scripting
    Former-commit-id: 198571a137baf4d8b1626e3e0fa748762c111593

--- baseline/baseline.sh
+++ baseline/baseline.sh
@@ -4,7 +4,7 @@
 ./replace_old_scripts_with_new.sh
 ./generate_schema_version_statement_for_uat_and_live.sh
 ./dry_run_live.sh
-./new_patchs_not_on_live.sh
+./new_patches_not_on_live.sh
 ./git_revert_flyway_scripts.sh
 
 
