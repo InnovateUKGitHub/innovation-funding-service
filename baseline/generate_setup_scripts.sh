45e920d78d831e9a18aa6553a759bc95784d8926
Duncan Watson <duncan@hiveit.co.uk>
2016-11-14 22:00:35 +0000

    INFUND-5942 - additional setup data
    Former-commit-id: 12d6c3709c053a6e37e85752cfdc25cf9a3b6521

--- baseline/generate_setup_scripts.sh
+++ baseline/generate_setup_scripts.sh
@@ -16,11 +16,14 @@ echo "**TODO Knowing what needs to be here will require trial and error and we w
 cd ../baseline
 ## TODO remove spend_profile when we find a table we actually need
 mysqldump -uroot -ppassword ifs_current_live_migrated \
+                            activity_state \
 			    competition_template \
                             question_template \
+                            question_template_form_input_template \
                             section_template \
                             form_input_template \
                             form_input_template_form_validator \
+                            finance_row_meta_field \
                             --ignore-table=ifs_current_live_migrated.schema_version \
                             --no-create-info --extended-insert=false > "generated/db/setup/V0_1_4__Minimum_data.sql"
 
