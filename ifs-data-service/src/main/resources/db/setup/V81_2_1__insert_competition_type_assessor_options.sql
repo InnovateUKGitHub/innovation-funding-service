294dbb30c618e04d214a7fd83c2ce2dafa04cfcd
skistapur <sainath.kistapur@wtg.co.uk>
2016-11-16 09:31:03 +0000

    INFUND-5641: Tidy up.
    Former-commit-id: ef89d4f5f6c5c3c6f46c1810ee3df9ba718280a3

--- ifs-data-service/src/main/resources/db/setup/V81_2_1__insert_competition_type_assessor_options.sql
+++ ifs-data-service/src/main/resources/db/setup/V81_2_1__insert_competition_type_assessor_options.sql
@@ -1,50 +1,22 @@
-
 -- Programme
 INSERT INTO `competition_type_assessor_option` (`competition_type_id`, `assessor_option_name`, `assessor_option_value`,`default_option`)
-VALUES ((SELECT id from competition_type where name = 'Programme'),'1', '1', false);
-
-INSERT INTO `competition_type_assessor_option` (`competition_type_id`, `assessor_option_name`, `assessor_option_value`,`default_option`)
-VALUES ((SELECT id from competition_type where name = 'Programme'),'3', '3', false);
-
-INSERT INTO `competition_type_assessor_option` (`competition_type_id`, `assessor_option_name`, `assessor_option_value`,`default_option`)
-VALUES ((SELECT id from competition_type where name = 'Programme'),'5', '5', true);
-
+VALUES
+((SELECT id from competition_type where name = 'Programme'),'1', '1', false),
+((SELECT id from competition_type where name = 'Programme'),'3', '3', false),
+((SELECT id from competition_type where name = 'Programme'),'5', '5', true),
 -- Additive Manufacturing
-INSERT INTO `competition_type_assessor_option` (`competition_type_id`, `assessor_option_name`, `assessor_option_value`,`default_option`)
-VALUES ((SELECT id from competition_type where name = 'Additive Manufacturing'),'1', '1', false);
-
-INSERT INTO `competition_type_assessor_option` (`competition_type_id`, `assessor_option_name`, `assessor_option_value`,`default_option`)
-VALUES ((SELECT id from competition_type where name = 'Additive Manufacturing'),'3', '3', false);
-
-INSERT INTO `competition_type_assessor_option` (`competition_type_id`, `assessor_option_name`, `assessor_option_value`,`default_option`)
-VALUES ((SELECT id from competition_type where name = 'Additive Manufacturing'),'5', '5', true);
-
+((SELECT id from competition_type where name = 'Additive Manufacturing'),'1', '1', false),
+((SELECT id from competition_type where name = 'Additive Manufacturing'),'3', '3', false),
+((SELECT id from competition_type where name = 'Additive Manufacturing'),'5', '5', true),
 -- SBRI
-INSERT INTO `competition_type_assessor_option` (`competition_type_id`, `assessor_option_name`, `assessor_option_value`,`default_option`)
-VALUES ((SELECT id from competition_type where name = 'SBRI'),'1', '1', false);
-
-INSERT INTO `competition_type_assessor_option` (`competition_type_id`, `assessor_option_name`, `assessor_option_value`,`default_option`)
-VALUES ((SELECT id from competition_type where name = 'SBRI'),'3', '3', false);
-
-INSERT INTO `competition_type_assessor_option` (`competition_type_id`, `assessor_option_name`, `assessor_option_value`,`default_option`)
-VALUES ((SELECT id from competition_type where name = 'SBRI'),'5', '5', true);
-
+((SELECT id from competition_type where name = 'SBRI'),'1', '1', false),
+((SELECT id from competition_type where name = 'SBRI'),'3', '3', false),
+((SELECT id from competition_type where name = 'SBRI'),'5', '5', true),
 -- Special
-INSERT INTO `competition_type_assessor_option` (`competition_type_id`, `assessor_option_name`, `assessor_option_value`,`default_option`)
-VALUES ((SELECT id from competition_type where name = 'Special'),'1', '1', false);
-
-INSERT INTO `competition_type_assessor_option` (`competition_type_id`, `assessor_option_name`, `assessor_option_value`,`default_option`)
-VALUES ((SELECT id from competition_type where name = 'Special'),'3', '3', false);
-
-INSERT INTO `competition_type_assessor_option` (`competition_type_id`, `assessor_option_name`, `assessor_option_value`,`default_option`)
-VALUES ((SELECT id from competition_type where name = 'Special'),'5', '5', true);
-
+((SELECT id from competition_type where name = 'Special'),'1', '1', false),
+((SELECT id from competition_type where name = 'Special'),'3', '3', false),
+((SELECT id from competition_type where name = 'Special'),'5', '5', true),
 -- Sector
-INSERT INTO `competition_type_assessor_option` (`competition_type_id`, `assessor_option_name`, `assessor_option_value`,`default_option`)
-VALUES ((SELECT id from competition_type where name = 'Sector'),'1', '1', false);
-
-INSERT INTO `competition_type_assessor_option` (`competition_type_id`, `assessor_option_name`, `assessor_option_value`,`default_option`)
-VALUES ((SELECT id from competition_type where name = 'Sector'),'3', '3', false);
-
-INSERT INTO `competition_type_assessor_option` (`competition_type_id`, `assessor_option_name`, `assessor_option_value`,`default_option`)
-VALUES ((SELECT id from competition_type where name = 'Sector'),'5', '5', true);
+((SELECT id from competition_type where name = 'Sector'),'1', '1', false),
+((SELECT id from competition_type where name = 'Sector'),'3', '3', false),
+((SELECT id from competition_type where name = 'Sector'),'5', '5', true);
