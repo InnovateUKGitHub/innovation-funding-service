b1830906e441b887b1506de302e1ce2a582bbb84
eamonnharrison <eamonn.harrison.ipl@gmail.com>
2016-11-17 09:59:38 +0000

    INFUND-5001 - Add competition description, start date, and end date to upcoming competition view model.
    Former-commit-id: 73f10e93a9f0a5d974b4540a18f5801fd93eaccd

--- ifs-web-service/ifs-assessment-service/src/main/java/com/worth/ifs/assessment/model/UpcomingCompetitionModelPopulator.java
+++ ifs-web-service/ifs-assessment-service/src/main/java/com/worth/ifs/assessment/model/UpcomingCompetitionModelPopulator.java
@@ -17,6 +17,6 @@ public class UpcomingCompetitionModelPopulator {
 
     public UpcomingCompetitionViewModel populateModel(Long competitionId) {
         CompetitionResource competition = competitionsRestService.getCompetitionById(competitionId).getSuccessObjectOrThrowException();
-        return new UpcomingCompetitionViewModel(competition.getName());
+        return new UpcomingCompetitionViewModel(competition.getName(), competition.getDescription(), competition.getStartDate(), competition.getEndDate());
     }
 }
--- ifs-web-service/ifs-assessment-service/src/main/java/com/worth/ifs/assessment/viewmodel/UpcomingCompetitionViewModel.java
+++ ifs-web-service/ifs-assessment-service/src/main/java/com/worth/ifs/assessment/viewmodel/UpcomingCompetitionViewModel.java
@@ -3,15 +3,23 @@ package com.worth.ifs.assessment.viewmodel;
 import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
 
+import java.time.LocalDateTime;
+
 /**
  * ViewModel of an UpcomingCompetition.
  */
 public class UpcomingCompetitionViewModel {
 
     private String competitionName;
+    private String competitionDescription;
+    private LocalDateTime assessmentPeriodDateFrom;
+    private LocalDateTime assessmentPeriodDateTo;
 
-    public UpcomingCompetitionViewModel(String competitionName) {
+    public UpcomingCompetitionViewModel(String competitionName, String competitionDescription, LocalDateTime assessmentPeriodDateFrom, LocalDateTime assessmentPeriodDateTo) {
         this.competitionName = competitionName;
+        this.competitionDescription = competitionDescription;
+        this.assessmentPeriodDateFrom = assessmentPeriodDateFrom;
+        this.assessmentPeriodDateTo = assessmentPeriodDateTo;
     }
 
     public String getCompetitionName() {
@@ -20,6 +28,30 @@ public class UpcomingCompetitionViewModel {
 
     public void setCompetitionName(String competitionName) {
         this.competitionName = competitionName;
+    }
+
+    public String getCompetitionDescription() {
+        return competitionDescription;
+    }
+
+    public void setCompetitionDescription(String competitionDescription) {
+        this.competitionDescription = competitionDescription;
+    }
+
+    public LocalDateTime getAssessmentPeriodDateFrom() {
+        return assessmentPeriodDateFrom;
+    }
+
+    public void setAssessmentPeriodDateFrom(LocalDateTime assessmentPeriodDateFrom) {
+        this.assessmentPeriodDateFrom = assessmentPeriodDateFrom;
+    }
+
+    public LocalDateTime getAssessmentPeriodDateTo() {
+        return assessmentPeriodDateTo;
+    }
+
+    public void setAssessmentPeriodDateTo(LocalDateTime assessmentPeriodDateTo) {
+        this.assessmentPeriodDateTo = assessmentPeriodDateTo;
     }
 
     @Override
@@ -36,6 +68,9 @@ public class UpcomingCompetitionViewModel {
 
         return new EqualsBuilder()
                 .append(competitionName, that.competitionName)
+                .append(competitionDescription, that.competitionDescription)
+                .append(assessmentPeriodDateFrom, assessmentPeriodDateFrom)
+                .append(assessmentPeriodDateTo, assessmentPeriodDateTo)
                 .isEquals();
     }
 
@@ -43,6 +78,9 @@ public class UpcomingCompetitionViewModel {
     public int hashCode() {
         return new HashCodeBuilder(17, 37)
                 .append(competitionName)
+                .append(competitionDescription)
+                .append(assessmentPeriodDateFrom)
+                .append(assessmentPeriodDateTo)
                 .toHashCode();
     }
 }
