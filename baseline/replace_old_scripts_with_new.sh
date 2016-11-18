194ca82a906fd7351aad4a7a930ce02be4c2d3e1
Duncan Watson <duncan@hiveit.co.uk>
2016-11-18 11:38:58 +0000

    INFUND-5942 - removing baseline work from this branch
    Former-commit-id: ec813132f16f43666512a9bc05aa7762dd72639b

--- baseline/replace_old_scripts_with_new.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "Copy the read me files to the generated directories"
-cp ../ifs-data-service/src/main/resources/db/migration/SCRIPTS_THAT_WILL_BE_RUN_ON_ALL_ENVIRONMENTS_INCLUDING_PRODUCTION generated/db/migration/SCRIPTS_THAT_WILL_BE_RUN_ON_ALL_ENVIRONMENTS_INCLUDING_PRODUCTION
-cp ../ifs-data-service/src/main/resources/db/setup/SETUP_THAT_WILL_BE_RUN_ON_PRODUCTION generated/db/setup/SETUP_THAT_WILL_BE_RUN_ON_PRODUCTION
-cp ../ifs-data-service/src/main/resources/db/webtest/SETUP_THAT_WILL_BE_RUN_FOR_ACCEPTANCE_TESTS generated/db/webtest/SETUP_THAT_WILL_BE_RUN_FOR_ACCEPTANCE_TESTS
-echo "Deleting the old scripts and directories"
-rm -rf ../ifs-data-service/src/main/resources/db
-echo "Moving the generated files into the code base"
-rsync -r --exclude=.gitignore generated/db ../ifs-data-service/src/main/resources/ 
