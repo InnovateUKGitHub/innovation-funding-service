29fc0f06c4b5ea886b41d663399f3a676e4abb55
Nuno Alexandre <nalexandre@worth.systems>
2016-11-18 13:33:48 +0000

    Moving AssessorCountOptionResourceBuilder from data/test to commons/test
    Former-commit-id: 4a16e2765d16c55d6c7720b873d3d7cc0d474a6c

--- ifs-commons/src/test/java/com/worth/ifs/competition/builder/AssessorCountOptionResourceBuilder.java
+++ ifs-commons/src/test/java/com/worth/ifs/competition/builder/AssessorCountOptionResourceBuilder.java
@@ -6,8 +6,8 @@ import com.worth.ifs.competition.resource.AssessorCountOptionResource;
 import java.util.List;
 import java.util.function.BiConsumer;
 
-import static com.worth.ifs.BuilderAmendFunctions.setField;
-import static com.worth.ifs.BuilderAmendFunctions.uniqueIds;
+import static com.worth.ifs.base.amend.BaseBuilderAmendFunctions.setField;
+import static com.worth.ifs.base.amend.BaseBuilderAmendFunctions.uniqueIds;
 import static java.util.Collections.emptyList;
 
 public class AssessorCountOptionResourceBuilder extends BaseBuilder<AssessorCountOptionResource, AssessorCountOptionResourceBuilder> {
@@ -31,22 +31,22 @@ public class AssessorCountOptionResourceBuilder extends BaseBuilder<AssessorCoun
     }
 
     public AssessorCountOptionResourceBuilder withId(Long... ids) {
-        return withArray((id, object) -> setField("id", id, object), ids);
+        return withArray((id, object) ->setField("id", id, object), ids);
     }
 
     public AssessorCountOptionResourceBuilder withAssessorOptionName(String... optionNames) {
-        return withArray((streamName, object) -> setField("optionName", streamName, object), optionNames);
+        return withArray((streamName, object) ->setField("optionName", streamName, object), optionNames);
     }
 
     public AssessorCountOptionResourceBuilder withCompetitionType(Long... competitionType) {
-        return withArray((streamName, object) -> setField("competitionType", streamName, object), competitionType);
+        return withArray((streamName, object) ->setField("competitionType", streamName, object), competitionType);
     }
 
     public AssessorCountOptionResourceBuilder withAssessorOptionValue(Integer... optionValues) {
-        return withArray((streamName, object) -> setField("optionValue", streamName, object), optionValues);
+        return withArray((streamName, object) ->setField("optionValue", streamName, object), optionValues);
     }
 
     public AssessorCountOptionResourceBuilder withDefaultOption(Boolean... defaultOptions) {
-        return withArray((streamName, object) -> setField("defaultOption", streamName, object), defaultOptions);
+        return withArray((streamName, object) ->setField("defaultOption", streamName, object), defaultOptions);
     }
 }
