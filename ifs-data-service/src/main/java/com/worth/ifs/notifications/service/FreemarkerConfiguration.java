8aa2b21d73c28ef146bbceb4d6b8a76072306b70
Adam Birse <adambirseipl@gmail.com>
2016-11-18 11:10:54 +0000

    INFUND-6222 Free marker unit tests fixed.
    Former-commit-id: a24507490f77eb1c9f4b0693075f39fc3ce84931

--- ifs-data-service/src/main/java/com/worth/ifs/notifications/service/FreemarkerConfiguration.java
+++ ifs-data-service/src/main/java/com/worth/ifs/notifications/service/FreemarkerConfiguration.java
@@ -1,7 +1,9 @@
 package com.worth.ifs.notifications.service;
 
 import java.io.IOException;
+import java.io.File;
 import java.net.URISyntaxException;
+import java.net.URL;
 
 import com.worth.ifs.Application;
 
@@ -9,9 +11,7 @@ import org.springframework.context.annotation.Bean;
 
 import freemarker.template.Configuration;
 import freemarker.template.TemplateExceptionHandler;
-/**
- *
- */
+
 @org.springframework.context.annotation.Configuration
 public class FreemarkerConfiguration {
 
@@ -22,11 +22,9 @@ public class FreemarkerConfiguration {
         // version (here 2.3.22) do you want to apply the fixes that are not 100%
         // backward-compatible. See the Configuration JavaDoc for details.
         Configuration cfg = new Configuration(Configuration.VERSION_2_3_23);
-
         // Specify the source where the template files come from. Here I set a
         // plain directory for it, but non-file-system sources are possible too:
-
-        cfg.setClassForTemplateLoading(Application.class, "notifications/templates");
+        cfg.setDirectoryForTemplateLoading(new File(Thread.currentThread().getContextClassLoader().getResource("ftltemplates").toURI()));
         // Set the preferred charset template files are stored in. UTF-8 is
         // a good choice in most applications:
         cfg.setDefaultEncoding("UTF-8");
