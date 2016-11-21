1c75c8d7776d52fdd52b978b0c805c2eb7570952
Luke Harper <lukeharperipl@gmail.com>
2016-11-21 10:29:10 +0000

    INFUND-5630 Adding missing unit tests.
    Former-commit-id: dedd21ea8207b2541644902ed70978878882cbb3

--- ifs-data-service/src/test/java/com/worth/ifs/competition/security/CompetitionSetupQuestionServiceSecurityTest.java
+++ ifs-data-service/src/test/java/com/worth/ifs/competition/security/CompetitionSetupQuestionServiceSecurityTest.java
@@ -4,9 +4,20 @@ import com.worth.ifs.*;
 import com.worth.ifs.commons.service.*;
 import com.worth.ifs.competition.resource.*;
 import com.worth.ifs.competition.transactional.*;
+import com.worth.ifs.user.resource.*;
 import org.junit.Test;
+import org.springframework.security.access.AccessDeniedException;
+
+import java.util.List;
 
 import static com.worth.ifs.competition.builder.CompetitionSetupQuestionResourceBuilder.*;
+import static com.worth.ifs.user.builder.RoleResourceBuilder.*;
+import static com.worth.ifs.user.builder.UserResourceBuilder.*;
+import static com.worth.ifs.user.resource.UserRoleType.*;
+import static freemarker.template.utility.Collections12.singletonList;
+import static java.util.Arrays.asList;
+import static java.util.stream.Collectors.toList;
+import static org.junit.Assert.*;
 
 /**
  * Testing the permission rules applied to the secured methods in OrganisationService.  This set of tests tests for the
@@ -15,25 +26,83 @@ import static com.worth.ifs.competition.builder.CompetitionSetupQuestionResource
  */
 public class CompetitionSetupQuestionServiceSecurityTest extends BaseServiceSecurityTest<CompetitionSetupQuestionService> {
 
+    private static final long QUESTION_ID = 1L;
+
     @Override
     protected Class<? extends CompetitionSetupQuestionService> getClassUnderTest() {
         return TestCompetitionService.class;
     }
 
-
     @Test
-    public void testGetByQuestionId() {
-        setLoggedInUser(null);
-
-        assertAccessDenied(() -> classUnderTest.getByQuestionId(1L), () -> {
-        });
+    public void testGetByQuestionIdDeniedIfGlobalCompAdminRole() {
+        RoleResource compAdminRole = newRoleResource().withType(COMP_ADMIN).build();
+        setLoggedInUser(newUserResource().withRolesGlobal(singletonList(compAdminRole)).build());
+        classUnderTest.getByQuestionId(QUESTION_ID);
     }
 
     @Test
-    public void testSave() {
-        setLoggedInUser(null);
+    public void testGetByQuestionIdDeniedIfNoGlobalRolesAtAll() {
+        try {
+            classUnderTest.getByQuestionId(QUESTION_ID);
+            fail("Should not have been able to create project from application without the global Comp Admin role");
+        } catch (AccessDeniedException e) {
+            // expected behaviour
+        }
+    }
 
-        assertAccessDenied(() -> classUnderTest.save(newCompetitionSetupQuestionResource().build()), () -> {
+    @Test
+    public void testGetQuestionIdDeniedIfNotCorrectGlobalRoles() {
+
+        List<UserRoleType> nonCompAdminRoles = asList(UserRoleType.values()).stream().filter(type -> type != COMP_ADMIN && type != PROJECT_FINANCE)
+                .collect(toList());
+
+        nonCompAdminRoles.forEach(role -> {
+
+            setLoggedInUser(
+                    newUserResource().withRolesGlobal(singletonList(newRoleResource().withType(role).build())).build());
+            try {
+                classUnderTest.getByQuestionId(QUESTION_ID);
+                fail("Should not have been able to create project from application without the global Comp Admin role");
+            } catch (AccessDeniedException e) {
+                // expected behaviour
+            }
+        });
+    }
+
+
+    @Test
+    public void testSaveAllowedIfGlobalCompAdminRole() {
+        RoleResource compAdminRole = newRoleResource().withType(COMP_ADMIN).build();
+        setLoggedInUser(newUserResource().withRolesGlobal(singletonList(compAdminRole)).build());
+        classUnderTest.save(newCompetitionSetupQuestionResource().build());
+    }
+
+    @Test
+    public void testSaveAllowedIfNoGlobalRolesAtAll() {
+        try {
+            classUnderTest.save(newCompetitionSetupQuestionResource().build());
+            fail("Should not have been able to create project from application without the global Comp Admin role");
+        } catch (AccessDeniedException e) {
+            // expected behaviour
+        }
+    }
+
+    @Test
+    public void testSaveDeniedIfNotCorrectGlobalRoles() {
+
+        List<UserRoleType> nonCompAdminRoles = asList(UserRoleType.values()).stream().filter(type -> type != COMP_ADMIN && type != PROJECT_FINANCE)
+                .collect(toList());
+
+        nonCompAdminRoles.forEach(role -> {
+
+            setLoggedInUser(
+                    newUserResource().withRolesGlobal(singletonList(newRoleResource().withType(role).build())).build());
+            try {
+                classUnderTest.save(newCompetitionSetupQuestionResource().build());
+                fail("Should not have been able to create project from application without the global Comp Admin role");
+            } catch (AccessDeniedException e) {
+                // expected behaviour
+            }
         });
     }
 
