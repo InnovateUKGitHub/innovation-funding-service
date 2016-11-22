07f4deb19487fb7e62e3bf555f4920d0f9ebf4f0
Adam Birse <adambirseipl@gmail.com>
2016-11-22 09:51:41 +0000

    INFUND-6285 Freemarker tests fixed, unit tests now passing on both deployments.
    Former-commit-id: 37a8484126a5e78135d3b506be56d62d2df009ed

--- ifs-data-service/src/main/java/com/worth/ifs/notifications/service/FreemarkerConfiguration.java
+++ ifs-data-service/src/main/java/com/worth/ifs/notifications/service/FreemarkerConfiguration.java
@@ -1,9 +1,7 @@
 package com.worth.ifs.notifications.service;
 
 import java.io.IOException;
-import java.io.File;
 import java.net.URISyntaxException;
-import java.net.URL;
 
 import com.worth.ifs.Application;
 
@@ -22,9 +20,8 @@ public class FreemarkerConfiguration {
         // version (here 2.3.22) do you want to apply the fixes that are not 100%
         // backward-compatible. See the Configuration JavaDoc for details.
         Configuration cfg = new Configuration(Configuration.VERSION_2_3_23);
-        // Specify the source where the template files come from. Here I set a
-        // plain directory for it, but non-file-system sources are possible too:
-        cfg.setDirectoryForTemplateLoading(new File(Thread.currentThread().getContextClassLoader().getResource("ftltemplates").toURI()));
+        // Specify the source where the template files come from.
+        cfg.setClassForTemplateLoading(Application.class, "notifications/templates");
         // Set the preferred charset template files are stored in. UTF-8 is
         // a good choice in most applications:
         cfg.setDefaultEncoding("UTF-8");
