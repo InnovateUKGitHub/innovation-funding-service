54bbe7ed5586fa1ad69977f849a5a3a034330415
Ann Stainforth <ann.stainforth.ipl@gmail.com>
2016-11-22 17:13:23 +0000

    Revert "Revert "INFUND-5633: Fix failing unit tests and add new competition field to documentation""
    This reverts commit 3fe8945b0584bd55d45c607359b379a6f7b08495 [formerly 2e3eb8909bd437b8417dfffb92ba42da40c32aef].
    
    
    Former-commit-id: 80431cd0166db072b0469b8967dca002c1fdc3aa

--- ifs-data-service/src/test/java/com/worth/ifs/documentation/CompetitionResourceDocs.java
+++ ifs-data-service/src/test/java/com/worth/ifs/documentation/CompetitionResourceDocs.java
@@ -48,7 +48,8 @@ public class CompetitionResourceDocs {
             fieldWithPath("innovateBudget").description("the innovate budget entered during competition setup"),
             fieldWithPath("funders").description("the funders for this competition"),
             fieldWithPath("fullApplicationFinance").description("are the full finance forms required for applications"),
-            fieldWithPath("includeGrowthTable").description("should applications include a full project growth table")
+            fieldWithPath("includeGrowthTable").description("should applications include a full project growth table"),
+            fieldWithPath("useResubmissionQuestion").description("should applications include the default resubmission question")
     };
 
     public static final CompetitionResourceBuilder competitionResourceBuilder = newCompetitionResource()
