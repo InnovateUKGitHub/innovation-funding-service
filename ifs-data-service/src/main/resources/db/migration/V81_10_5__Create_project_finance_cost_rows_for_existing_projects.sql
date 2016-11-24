56d2e72808039a456856911b76daab89f12663a9
Duncan Watson <duncan@hiveit.co.uk>
2016-11-24 10:47:01 +0000

    INFUND-6054 - added patch to populate finance meta values for existing projects
    Former-commit-id: 81200d35574793c7c17e6ae56286fed28567410a

--- ifs-data-service/src/main/resources/db/migration/V81_10_5__Create_project_finance_cost_rows_for_existing_projects.sql
+++ ifs-data-service/src/main/resources/db/migration/V81_10_5__Create_project_finance_cost_rows_for_existing_projects.sql
@@ -5,9 +5,9 @@ JOIN application_finance af ON af.id = fr.target_id
 JOIN application a ON a.id = af.application_id
 JOIN project p ON p.application_id = a.id
 JOIN project_finance pf ON pf.project_id = p.id AND pf.organisation_id = af.organisation_id
-WHERE fr.row_type = 'ApplicationFinanceRow'
-AND NOT EXISTS(SELECT 1 FROM finance_row where target_id = pf.id);
+WHERE fr.row_type = 'ApplicationFinanceRow';
 
 INSERT INTO finance_row_meta_value (finance_row_id, finance_row_meta_field_id, value)
 SELECT pfr.id, afrmv.finance_row_meta_field_id, afrmv.value
-FROM 
+FROM finance_row_meta_value afrmv
+JOIN finance_row pfr ON pfr.application_row_id = afrmv.finance_row_id;
