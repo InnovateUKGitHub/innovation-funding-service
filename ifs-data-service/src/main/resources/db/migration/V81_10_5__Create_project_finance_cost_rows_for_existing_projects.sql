0b6a0f0994b283dcfd5f62a296189c0748036fb8
Duncan Watson <duncan@hiveit.co.uk>
2016-11-24 09:06:01 +0000

    INFUND-6054 - added patch to fill in finance rows for projects based upon original application rows
    Former-commit-id: 96e29e7076725fc992a14ee08c1c6f0a9f36e40a

---
+++ ifs-data-service/src/main/resources/db/migration/V81_10_5__Create_project_finance_cost_rows_for_existing_projects.sql
@@ -0,0 +1,9 @@
+INSERT INTO finance_row (cost, description, item, quantity, name, target_id, question_id, application_row_id, row_type)
+SELECT fr.cost, fr.description, fr.item, fr.quantity, fr.name, pf.id, fr.question_id, fr.id, 'ProjectFinanceRow'
+FROM finance_row fr
+JOIN application_finance af ON af.id = fr.target_id
+JOIN application a ON a.id = af.application_id
+JOIN project p ON p.application_id = a.id
+JOIN project_finance pf ON pf.project_id = p.id AND pf.organisation_id = af.organisation_id
+WHERE fr.row_type = 'ApplicationFinanceRow'
+AND NOT EXISTS(SELECT 1 FROM finance_row where target_id = pf.id);
