79adc894b380573f4847f7618822e8a431be5b2f
Tom Baldwin <tbaldwin@worth.systems>
2016-11-24 09:35:15 +0000

    INFUND-5739 - Changes CompetitionInAssessmentGuard to use competition.getCompetitionStatus() instead of testing the milestones directly.
    Former-commit-id: de5c4d545236c95a0bda6a7abccae9f8be192196

--- ifs-data-service/src/main/java/com/worth/ifs/assessment/workflow/guards/CompetitionInAssessmentGuard.java
+++ ifs-data-service/src/main/java/com/worth/ifs/assessment/workflow/guards/CompetitionInAssessmentGuard.java
@@ -4,7 +4,7 @@ import com.worth.ifs.assessment.domain.Assessment;
 import com.worth.ifs.assessment.resource.AssessmentOutcomes;
 import com.worth.ifs.assessment.resource.AssessmentStates;
 import com.worth.ifs.competition.domain.Competition;
-import com.worth.ifs.competition.resource.MilestoneType;
+import com.worth.ifs.competition.resource.CompetitionStatus;
 import org.springframework.statemachine.StateContext;
 import org.springframework.statemachine.guard.Guard;
 import org.springframework.stereotype.Component;
@@ -21,7 +21,6 @@ public class CompetitionInAssessmentGuard implements Guard<AssessmentStates, Ass
         Assessment assessment = (Assessment) context.getMessageHeader("assessment");
         Competition competition = assessment.getTarget().getCompetition();
 
-        return competition.isMilestoneReached(MilestoneType.ASSESSORS_NOTIFIED) &&
-                !competition.isMilestoneReached(MilestoneType.ASSESSMENT_CLOSED);
+        return competition.getCompetitionStatus() == CompetitionStatus.IN_ASSESSMENT;
     }
 }
