7aa3f24d8c506e0cf24356fe312d46b035bed71e
Reinaud van Rumpt <rvanrumpt@worth.systems>
2016-11-29 16:57:14 +0100

    INFUND-6402 Only changing DB values no programmatic check
    Former-commit-id: 6fdb6904534c72da22e3d3538c0f62fa0f3594a7

--- ifs-web-service/ifs-competition-mgt-service/src/main/java/com/worth/ifs/competitionsetup/service/modelpopulator/application/ApplicationQuestionModelPopulator.java
+++ ifs-web-service/ifs-competition-mgt-service/src/main/java/com/worth/ifs/competitionsetup/service/modelpopulator/application/ApplicationQuestionModelPopulator.java
@@ -1,13 +1,8 @@
 package com.worth.ifs.competitionsetup.service.modelpopulator.application;
 
-import com.worth.ifs.application.resource.QuestionResource;
-import com.worth.ifs.application.resource.SectionResource;
-import com.worth.ifs.application.service.QuestionService;
-import com.worth.ifs.application.service.SectionService;
 import com.worth.ifs.competition.resource.CompetitionResource;
 import com.worth.ifs.competition.resource.CompetitionSetupSubsection;
 import com.worth.ifs.competitionsetup.service.modelpopulator.CompetitionSetupSubsectionModelPopulator;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
 
@@ -24,31 +19,8 @@ public class ApplicationQuestionModelPopulator implements CompetitionSetupSubsec
 		return CompetitionSetupSubsection.QUESTIONS;
 	}
 
-	@Autowired
-    public QuestionService questionService;
-
-    @Autowired
-    public SectionService sectionService;
-
-    private static final String PROJECT_DETAILS_KEY = "Project details";
-
 	@Override
 	public void populateModel(Model model, CompetitionResource competitionResource, Optional<Long> objectId) {
-        objectId.ifPresent(objectIdLong ->
-                model.addAttribute("usingAppendix", checkUsingAppendix(questionService.getById(objectIdLong))));
-	}
 
-	private SectionResource getSectionToCheck(Long sectionId) {
-        SectionResource sectionResource = sectionService.getById(sectionId);
-        if(null != sectionResource.getParentSection()) {
-            return getSectionToCheck(sectionResource.getParentSection());
-        }
-        return sectionResource;
     }
-
-	private Boolean checkUsingAppendix(QuestionResource questionResource) {
-        SectionResource sectionToCheck = getSectionToCheck(questionResource.getSection());
-
-    	return !sectionToCheck.getName().equals(PROJECT_DETAILS_KEY);
-	}
 }
