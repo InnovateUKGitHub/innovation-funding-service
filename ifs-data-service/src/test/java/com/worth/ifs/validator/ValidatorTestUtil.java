a142f8b2f5a15bf5af6b1a8586f2b7bf5eadd39d
xiaonan.zhang <xiaonan.zhang.innovateuk@gmail.com>
2016-11-29 15:46:40 +0000

    INFUND-6146/INFUND-6407: tidy up
    Former-commit-id: 2ce340273176306937f50bfc1e681b930a8fdf91

--- ifs-data-service/src/test/java/com/worth/ifs/validator/ValidatorTestUtil.java
+++ ifs-data-service/src/test/java/com/worth/ifs/validator/ValidatorTestUtil.java
@@ -2,13 +2,14 @@ package com.worth.ifs.validator;
 
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.DataBinder;
+
 import com.worth.ifs.finance.resource.cost.FinanceRowItem;
 import com.worth.ifs.form.domain.FormInputResponse;
 
 public final class ValidatorTestUtil {
 
-	private ValidatorTestUtil(){}
-	
+    private ValidatorTestUtil(){}
+
     public static BindingResult getBindingResult(FormInputResponse formInputResponse) {
         return new DataBinder(formInputResponse).getBindingResult();
     }
