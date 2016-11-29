7cc65fbe441e6874d8714586e55768eeac9e6fdf
Reinaud van Rumpt <rvanrumpt@worth.systems>
2016-11-29 11:00:45 +0100

    INFUND-6402 Check the parent section for showing appendix or not
    Former-commit-id: 9dae6cb9edc0aa0d8934ca6e8c86d9d09b927779

--- ifs-web-service/ifs-competition-mgt-service/src/main/java/com/worth/ifs/competitionsetup/service/modelpopulator/application/ApplicationQuestionModelPopulator.java
+++ ifs-web-service/ifs-competition-mgt-service/src/main/java/com/worth/ifs/competitionsetup/service/modelpopulator/application/ApplicationQuestionModelPopulator.java
@@ -1,8 +1,13 @@
 package com.worth.ifs.competitionsetup.service.modelpopulator.application;
 
+import com.worth.ifs.application.resource.QuestionResource;
+import com.worth.ifs.application.resource.SectionResource;
+import com.worth.ifs.application.service.QuestionService;
+import com.worth.ifs.application.service.SectionService;
 import com.worth.ifs.competition.resource.CompetitionResource;
 import com.worth.ifs.competition.resource.CompetitionSetupSubsection;
 import com.worth.ifs.competitionsetup.service.modelpopulator.CompetitionSetupSubsectionModelPopulator;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
 
@@ -19,7 +24,29 @@ public class ApplicationQuestionModelPopulator implements CompetitionSetupSubsec
 		return CompetitionSetupSubsection.QUESTIONS;
 	}
 
+	@Autowired
+    public QuestionService questionService;
+
+    @Autowired
+    public SectionService sectionService;
+
 	@Override
 	public void populateModel(Model model, CompetitionResource competitionResource, Optional<Long> objectId) {
+        objectId.ifPresent(objectIdLong ->
+                model.addAttribute("usingAppendix", checkUsingAppendix(questionService.getById(objectIdLong))));
+	}
+
+	private SectionResource getSectionToCheck(Long sectionId) {
+        SectionResource sectionResource = sectionService.getById(sectionId);
+        if(null != sectionResource.getParentSection()) {
+            return getSectionToCheck(sectionResource.getParentSection());
+        }
+        return sectionResource;
+    }
+
+	private Boolean checkUsingAppendix(QuestionResource questionResource) {
+        SectionResource sectionToCheck = getSectionToCheck(questionResource.getSection());
+
+    	return !sectionToCheck.getName().equals("Project details");
 	}
 }
