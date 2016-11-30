fe787c26feef249767dcd1bb4316c2f1393b78fd
IKENNA IBEANU <ikena.ibeanu@gmail.com>
2016-11-30 17:22:17 +0000

    Added test for generate method access
    Former-commit-id: 09f2e37ded1366acdd08af7ffafa84817752a89b

--- ifs-data-service/src/test/java/com/worth/ifs/project/security/ProjectGrantOfferServiceSecurityTest.java
+++ ifs-data-service/src/test/java/com/worth/ifs/project/security/ProjectGrantOfferServiceSecurityTest.java
@@ -7,16 +7,26 @@ import com.worth.ifs.file.resource.FileEntryResource;
 import com.worth.ifs.file.service.FileAndContents;
 import com.worth.ifs.project.resource.ProjectResource;
 import com.worth.ifs.project.transactional.ProjectGrantOfferService;
+import com.worth.ifs.user.resource.UserRoleType;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.springframework.security.access.AccessDeniedException;
 
 import java.io.InputStream;
+import java.util.List;
 import java.util.function.Supplier;
 
+import static com.worth.ifs.file.builder.FileEntryResourceBuilder.newFileEntryResource;
 import static com.worth.ifs.project.builder.ProjectResourceBuilder.newProjectResource;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.verifyNoMoreInteractions;
-import static org.mockito.Mockito.when;
+import static com.worth.ifs.user.builder.RoleResourceBuilder.newRoleResource;
+import static com.worth.ifs.user.builder.UserResourceBuilder.newUserResource;
+import static com.worth.ifs.user.resource.UserRoleType.COMP_ADMIN;
+import static com.worth.ifs.user.resource.UserRoleType.PROJECT_FINANCE;
+import static java.util.Arrays.asList;
+import static java.util.Collections.singletonList;
+import static java.util.stream.Collectors.toList;
+import static org.mockito.Mockito.*;
 
 public class ProjectGrantOfferServiceSecurityTest extends BaseServiceSecurityTest<ProjectGrantOfferService> {
 
@@ -158,6 +168,29 @@ public class ProjectGrantOfferServiceSecurityTest extends BaseServiceSecurityTes
         assertAccessDenied(() -> classUnderTest.submitGrantOfferLetter(projectId), () -> {
             verify(projectGrantOfferPermissionRules).projectManagerSubmitGrantOfferLetter(projectId, getLoggedInUser());
             verifyNoMoreInteractions(projectGrantOfferPermissionRules);
+        });
+    }
+
+    @Test
+    public void testGenerateGrantOfferLetterDeniedIfNotCorrectGlobalRoles() {
+
+        final Long projectId = 1L;
+
+        FileEntryResource fileEntryResource = newFileEntryResource().build();
+
+        List<UserRoleType> nonCompAdminRoles = asList(UserRoleType.values()).stream().filter(type -> type != COMP_ADMIN && type != PROJECT_FINANCE)
+                .collect(toList());
+
+        nonCompAdminRoles.forEach(role -> {
+
+            setLoggedInUser(
+                    newUserResource().withRolesGlobal(singletonList(newRoleResource().withType(role).build())).build());
+            try {
+                classUnderTest.generateGrantOfferLetter(projectId, fileEntryResource);
+                Assert.fail("Should not have been able to generate GOL without the global Comp Admin role");
+            } catch (AccessDeniedException e) {
+                // expected behaviour
+            }
         });
     }
 
