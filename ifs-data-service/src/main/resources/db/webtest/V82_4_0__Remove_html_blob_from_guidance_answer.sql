318934cbc6146e14a468eb01d4347d61ae845e76
eamonnharrison <eamonn.harrison.ipl@gmail.com>
2016-12-05 14:00:36 +0000

    INFUND-6281 - Updated guidance answers.
    Former-commit-id: 8a672e40cee41bc706abcaf8ccc842b9c4e85d93

--- ifs-data-service/src/main/resources/db/webtest/V82_4_0__Remove_html_blob_from_guidance_answer.sql
+++ ifs-data-service/src/main/resources/db/webtest/V82_4_0__Remove_html_blob_from_guidance_answer.sql
@@ -1,2 +1,15 @@
-UPDATE form_input SET guidance_answer = 'Your answer should be based upon the following:' WHERE description = 'Feedback' AND guidance_answer LIKE '%answer%';
-UPDATE form_input SET guidance_answer = 'Your score should be based upon the following:' WHERE description = 'Feedback' AND guidance_answer LIKE '%score%';
+-- Update scope guidance titles
+UPDATE form_input
+    SET guidance_answer = 'Your answer should be based upon the following:'
+    WHERE form_input_type_id =
+        (SELECT id from form_input_type WHERE name = 'TEXTAREA')
+    AND scope = 'ASSESSMENT'
+    AND guidance_answer LIKE '%answer%';
+
+-- Update other guidance titles
+UPDATE form_input
+    SET guidance_answer = 'Your score should be based upon the following:'
+    WHERE form_input_type_id =
+        (SELECT id from form_input_type WHERE name = 'TEXTAREA')
+    AND scope = 'ASSESSMENT'
+    AND guidance_answer LIKE '%score%';
