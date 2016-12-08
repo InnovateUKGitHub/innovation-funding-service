32283ee6b7ff3e9fdd2f48b19ac3c02dd65686f2
Luke Harper <lukeharperipl@gmail.com>
2016-12-08 13:43:10 +0000

    INFUND-6468 Removing unused repository method
    Former-commit-id: f79dd0897d7143c3a238f62c81c821588bcb7882

--- ifs-data-service/src/main/java/com/worth/ifs/application/repository/GuidanceRowRepository.java
+++ ifs-data-service/src/main/java/com/worth/ifs/application/repository/GuidanceRowRepository.java
@@ -13,6 +13,4 @@ import java.util.List;
 public interface GuidanceRowRepository extends CrudRepository<GuidanceRow, Long> {
 
     List<GuidanceRow> findByFormInputQuestionCompetitionId(Long competitionId);
-    void deleteByFormInputId(Long formInput);
-
 }
