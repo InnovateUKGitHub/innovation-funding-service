79f559a9add458ab6d2a919f698ed8d9b15fafbf
Ewan Cormack <ewan@hiveit.co.uk>
2016-12-08 14:09:02 +0000

    Tidying up download script
    Former-commit-id: 285c5da985f6545ec0c80a5fec3461f431aeb7c8

--- robot-tests/download.py
+++ robot-tests/download.py
@@ -18,44 +18,23 @@ def shell(command):
 def getBaseUrl(downloadUrl):
   o = urlparse(downloadUrl)
   baseUrl = o.scheme + "://" + o.netloc
-  #print "*******************************************************************"
-  #print baseUrl
-  #print "*******************************************************************"
   return baseUrl
 
 # Go to the base url. Follow any redirects. Find the final auth base url. Find the relative url to post credentials to
 def getBaseAuthUrlAndPortUrl(baseUrl):
   curlCommand = "curl -L --insecure -w marker%{url_effective}marker " + baseUrl
-  #print "*******************************************************************"
-  #print curlCommand
-  #print "*******************************************************************"
   loginHtml = shell(curlCommand)
-  #print "*******************************************************************"
-  #print loginHtml
-  #print "*******************************************************************"
   loginPostUrl = re.findall('<form action="(.*)" method="post">', loginHtml, re.MULTILINE)[0]
-  #print "*******************************************************************"
-  #print loginPostUrl
-  #print "*******************************************************************"
   redirectUrl = re.findall("marker(.*)marker", loginHtml, re.MULTILINE)[0]
   o = urlparse(redirectUrl)
   baseAuthUrl = o.scheme + "://" + o.netloc
-  #print "*******************************************************************"
-  #print baseAuthUrl
-  #print "*******************************************************************"
   return loginPostUrl, baseAuthUrl
 
 # Post the credentials to the base auth url and get SAML credentials which are needed to continue the authentication process
 def postCredentialsAndGetShibbolethParameters(user, password, baseAuthUrl, loginPostUrl):
   # Note that python will put quotes around the individual parameters automatically. In particular the authUrl, and with out these the curl command would not work.
   curlCommand = "curl --insecure -L --data j_username=" + urllib.quote(user) + "&j_password=" + urllib.quote(password) + "&_eventId_proceed= " + baseAuthUrl + loginPostUrl
-  #print "*******************************************************************"
-  #print curlCommand
-  #print "*******************************************************************"
   postLoginPage = shell(curlCommand)
-  #print "*******************************************************************"
-  #print postLoginPage
-  #print "*******************************************************************"
   sAMLResponse = re.findall('name="SAMLResponse" value="(.*)"', postLoginPage, re.MULTILINE)[0]
   relayState = re.findall('RelayState" value="(.*?)"', postLoginPage, re.MULTILINE)[0]
   return sAMLResponse, relayState
@@ -64,13 +43,7 @@ def postCredentialsAndGetShibbolethParameters(user, password, baseAuthUrl, login
 def postShibbolethParametersForSession(sAMLResponse, relayState, baseUrl):
   h = HTMLParser()
   curlCommand = "curl --insecure -L -c - --data SAMLRequest=" + urllib.quote(h.unescape(sAMLResponse)) + "&RelayState=" + urllib.quote(h.unescape(relayState)) + " " + baseUrl + "/Shibboleth.sso/SAML2/POST"
-  #print "*******************************************************************"
-  #print curlCommand
-  #print "*******************************************************************"
   exchange = shell(curlCommand)
-  #print "*******************************************************************"
-  #print exchange
-  #print "*******************************************************************"
   session = re.findall('(_shibsession_([^\s-]*))\s*(.*)', exchange, re.MULTILINE)
   shibCookieName = session[0][0]
   shibCookieValue = session[0][2]
@@ -85,6 +58,7 @@ def downloadFile(shibCookieName, shibCookieValue, downloadUrl, downloadFileLocat
 # Example use case:
 # ./download.py john.doe@innovateuk.test Passw0rd https://ifs-local-dev/management/competition/1/download /tmp/file.xlsx
 # Note that repeated use in quick succession on various non developer environments will cause failure due to - most likely - to automatic lockup.
+
 def main():
   user = sys.argv[1] # e.g. john.doe@innovateuk.test
   password = sys.argv[2] # e.g. Passw0rd
