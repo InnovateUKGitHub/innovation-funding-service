b9a42c845bbc83bb33a0da785bc0fd3baf9c7887
IKENNA IBEANU <ikena.ibeanu@gmail.com>
2016-12-12 19:25:46 +0000

    Removed state from builder
    Former-commit-id: 40716ca1fd19db48500b5d445762e14ac36f5712

--- ifs-resources/src/test/java/com/worth/ifs/project/resource/SpendProfileTableResourceBuilder.java
+++ ifs-resources/src/test/java/com/worth/ifs/project/resource/SpendProfileTableResourceBuilder.java
@@ -11,30 +11,20 @@ import java.util.Map;
 import java.util.function.BiConsumer;
 
 import static com.worth.ifs.base.amend.BaseBuilderAmendFunctions.setField;
+import static com.worth.ifs.base.amend.BaseBuilderAmendFunctions.uniqueIds;
+import static java.util.Collections.emptyList;
 
 /**
  *
  **/
 public final class SpendProfileTableResourceBuilder extends BaseBuilder<SpendProfileTableResource, SpendProfileTableResourceBuilder> {
-    private Boolean markedAsComplete;
-    /*
-         * Dynamically holds the months for the duration of the project
-         */
-    private List<LocalDateResource> months;
-    /*
-         * Holds the cost per category for each month, the first entry in the list representing the first month and so on.
-         */
-    private Map<Long, List<BigDecimal>> monthlyCostsPerCategoryMap;
-    private Map<Long, BigDecimal> eligibleCostPerCategoryMap;
-    private ValidationMessages validationMessages;
-    private Map<Long, CostCategoryResource> costCategoryResourceMap;
-    private Map<String, List<Map<Long, List<BigDecimal>>>> costCategoryGroupMap;
 
-    private SpendProfileTableResourceBuilder() {
+    private SpendProfileTableResourceBuilder(List<BiConsumer<Integer, SpendProfileTableResource>> multiActions) {
+        super(multiActions);
     }
 
     public static SpendProfileTableResourceBuilder newSpendProfileTableResource() {
-        return new SpendProfileTableResourceBuilder();
+        return new SpendProfileTableResourceBuilder(emptyList()).with(uniqueIds());
     }
 
 
@@ -66,21 +56,9 @@ public final class SpendProfileTableResourceBuilder extends BaseBuilder<SpendPro
         return withArray((costCategoryGroupMap, spendProfileResource) -> setField("costCategoryGroupMap", costCategoryGroupMap, spendProfileResource), costCategoryGroupMaps);
     }
 
-    public SpendProfileTableResource build() {
-        SpendProfileTableResource spendProfileTableResource = new SpendProfileTableResource();
-        spendProfileTableResource.setMarkedAsComplete(markedAsComplete);
-        spendProfileTableResource.setMonths(months);
-        spendProfileTableResource.setMonthlyCostsPerCategoryMap(monthlyCostsPerCategoryMap);
-        spendProfileTableResource.setEligibleCostPerCategoryMap(eligibleCostPerCategoryMap);
-        spendProfileTableResource.setValidationMessages(validationMessages);
-        spendProfileTableResource.setCostCategoryResourceMap(costCategoryResourceMap);
-        spendProfileTableResource.setCostCategoryGroupMap(costCategoryGroupMap);
-        return spendProfileTableResource;
-    }
-
     @Override
     protected SpendProfileTableResourceBuilder createNewBuilderWithActions(List<BiConsumer<Integer, SpendProfileTableResource>> actions) {
-        return new SpendProfileTableResourceBuilder();
+        return new SpendProfileTableResourceBuilder(actions);
     }
 
     @Override
