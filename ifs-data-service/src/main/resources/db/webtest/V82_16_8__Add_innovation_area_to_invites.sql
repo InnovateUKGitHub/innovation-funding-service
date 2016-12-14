3e9aafe8e94f66261d96e1cff3719ed975eacb4e
Tom Baldwin <tbaldwin@worth.systems>
2016-12-14 20:07:35 +0000

    INFUND-6900 - Removes Add innovation area webtest patch as this is included in the baseline.
    Former-commit-id: 1a05832d85a2a4b8d0365e0ccc18c82015634458

--- ifs-data-service/src/main/resources/db/webtest/V82_16_8__Add_innovation_area_to_invites.sql
+++
@@ -1,7 +0,0 @@
-# tom this should now be in the tes data
-
-# -- gives an innovation area to each competition invite for non-existing users
-# UPDATE invite i
-#   SET i.innovation_category_id = (SELECT id FROM category WHERE name='Earth Observation' AND type='INNOVATION_AREA')
-# WHERE
-#   type='COMPETITION' AND NOT EXISTS (SELECT 1 FROM user u WHERE u.email=i.email);
