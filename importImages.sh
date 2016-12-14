aa0585c836eabbb11f288f2029db27ce55e4f7b7
Laszlo Fogas <laszlo@falconsocial.com>
2016-12-14 15:30:47 +0000

    Scripts to upload images to local Openshift VM - to be deleted
    Former-commit-id: dd5bd5807385c1f3712d20b5277e512ad3a127fa

---
+++ importImages.sh
@@ -0,0 +1,8 @@
+oc import-image worth/project-setup-service --confirm
+oc import-image worth/project-setup-management-service --confirm
+oc import-image worth/competition-management-service --confirm
+oc import-image worth/assessment-service --confirm
+oc import-image worth/application-service --confirm
+oc import-image worth/data-service --confirm
+
+oc get is
