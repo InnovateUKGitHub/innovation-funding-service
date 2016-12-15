c7e06e9a3cfd0027dbaf3c36c2df75e49453f293
xiaonan.zhang <xiaonan.zhang.innovateuk@gmail.com>
2016-12-15 14:49:44 +0000

    INFUND-4852: rework after found overlapping with INFUND-6377
    Former-commit-id: 0a35fd59e8cc73e6f36ae0546a8c6fe4f21474ec

--- ifs-web-service/ifs-project-setup-service/src/main/java/org/innovateuk/ifs/project/grantofferletter/controller/ProjectGrantOfferLetterController.java
+++ ifs-web-service/ifs-project-setup-service/src/main/java/org/innovateuk/ifs/project/grantofferletter/controller/ProjectGrantOfferLetterController.java
@@ -169,8 +169,10 @@ public class ProjectGrantOfferLetterController {
                 grantOfferFileDetails.isPresent() ? grantOfferFileDetails.map(FileDetailsViewModel::new).orElse(null) : null,
                 signedGrantOfferLetterFile.isPresent() ? signedGrantOfferLetterFile.map(FileDetailsViewModel::new).orElse(null) : null,
                 additionalContractFile.isPresent() ? additionalContractFile.map(FileDetailsViewModel::new).orElse(null) : null,
-                project.getOfferSubmittedDate(), project.isOfferRejected() != null && project.isOfferRejected(),
-                grantOfferLetterApproved, isProjectManager, isGrantOfferLetterSent);
+                project.getOfferSubmittedDate(),
+                grantOfferLetterApproved,
+                isProjectManager,
+		isGrantOfferLetterSent);
     }
 
     private String performActionOrBindErrorsToField(Long projectId, ValidationHandler validationHandler, Model model, UserResource loggedInUser, String fieldName, ProjectGrantOfferLetterForm form, Supplier<FailingOrSucceedingResult<?, ?>> actionFn) {
