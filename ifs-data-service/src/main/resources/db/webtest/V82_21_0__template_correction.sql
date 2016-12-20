6faf985bfd0e991b76c94f8cb19da055bbfc12ba
Luke Harper <lukeharperipl@gmail.com>
2016-12-20 13:57:46 +0000

    INFUND-6983 querying without ids.
    Former-commit-id: 3508753f11110f4d84de40513dc42deb976a3792

--- ifs-data-service/src/main/resources/db/webtest/V82_21_0__template_correction.sql
+++ ifs-data-service/src/main/resources/db/webtest/V82_21_0__template_correction.sql
@@ -1,7 +1,10 @@
 
 -- Update questions in templates in which the description has a wrongly escaped &
-UPDATE question SET description='<h2 class=\"heading-medium\">Funding rules for this competition</h2><p>We will fund projects between &pound;500,000 and &pound;1.5 million. We may consider projects with costs outside of this range. We expect projects to last between 12 and 36 months.</p><p>Innovate UK\'s aim is to optimise the level of funding businesses receive. We also recognise the importance of research organisations\' contribution to R&amp;D projects. Therefore we require the following levels of participation:</p><ul class=\"list-bullet\"> <li>at least 70% of the total eligible project costs are incurred by commercial organisations and</li> <li>a maximum of 30% of total eligible project costs are available to research participants. Where there is more than one research participant, this maximum will be shared between them.</li></ul>'
-WHERE (id=334 or id=369);
+UPDATE question q
+    INNER JOIN competition c ON q.competition_id=c.id
+    SET q.description='<h2 class=\"heading-medium\">Funding rules for this competition</h2><p>We will fund projects between &pound;500,000 and &pound;1.5 million. We may consider projects with costs outside of this range. We expect projects to last between 12 and 36 months.</p><p>Innovate UK\'s aim is to optimise the level of funding businesses receive. We also recognise the importance of research organisations\' contribution to R&amp;D projects. Therefore we require the following levels of participation:</p><ul class=\"list-bullet\"> <li>at least 70% of the total eligible project costs are incurred by commercial organisations and</li> <li>a maximum of 30% of total eligible project costs are available to research participants. Where there is more than one research participant, this maximum will be shared between them.</li></ul>'
+WHERE c.template=1
+    AND q.description LIKE '<h2 class=\"heading-medium\">Funding rules%';
 
 -- Update all template questions where the description, short_name or name is the same as the live competition.
 -- Update the fields to match the live competition.
