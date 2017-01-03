1906d8a12e9ca203829a3495d2f8c457339c6756
Simon Shakespeare <si@hiveit.co.uk>
2017-01-03 14:12:54 +0000

    INFUND-6554 - Add more unit tests for UsersRolesServiceImpl
    Former-commit-id: 58494889a5a31ac4fadd9b0967416e8567f6f1b0

--- ifs-data-service/src/test/java/org/innovateuk/ifs/user/transactional/UsersRolesServiceImplTest.java
+++ ifs-data-service/src/test/java/org/innovateuk/ifs/user/transactional/UsersRolesServiceImplTest.java
@@ -4,11 +4,16 @@ import org.innovateuk.ifs.BaseUnitTestMocksTest;
 import org.innovateuk.ifs.commons.service.ServiceResult;
 import org.innovateuk.ifs.user.domain.ProcessRole;
 import org.innovateuk.ifs.user.resource.ProcessRoleResource;
+import org.innovateuk.ifs.user.resource.UserRoleType;
 import org.junit.Test;
 import org.mockito.InjectMocks;
 
+import java.util.List;
+
+import static java.util.Arrays.asList;
 import static org.innovateuk.ifs.user.builder.ProcessRoleBuilder.newProcessRole;
 import static org.innovateuk.ifs.user.builder.ProcessRoleResourceBuilder.newProcessRoleResource;
+import static org.innovateuk.ifs.util.CollectionFunctions.zip;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.*;
@@ -33,5 +38,114 @@ public class UsersRolesServiceImplTest extends BaseUnitTestMocksTest {
         assertEquals(processRoleResource, result.getSuccessObject());
 
         verify(processRoleRepositoryMock, only()).findOne(1L);
+    }
+
+    @Test
+    public void getProcessRolesByIds() {
+
+        List<ProcessRole> processRoles = newProcessRole().build(2);
+        List<ProcessRoleResource> processRoleResources = newProcessRoleResource().build(2);
+        List<Long> processRoleIds = asList(new Long[]{1L, 2L});
+
+        when(processRoleRepositoryMock.findAll(processRoleIds)).thenReturn(processRoles);
+
+        zip(processRoles, processRoleResources, (pr, prr) -> when(processRoleMapperMock.mapToResource(same(pr))).thenReturn(prr));
+
+        ServiceResult<List<ProcessRoleResource>> result = usersRolesService.getProcessRolesByIds(new Long[]{1L, 2L});
+
+        assertTrue(result.isSuccess());
+        assertEquals(processRoleResources, result.getSuccessObject());
+
+        verify(processRoleRepositoryMock, only()).findAll(processRoleIds);
+    }
+
+    @Test
+    public void getProcessRolesByApplicationId() {
+
+        List<ProcessRole> processRoles = newProcessRole().build(2);
+        List<ProcessRoleResource> processRoleResources = newProcessRoleResource().build(2);
+
+        when(processRoleRepositoryMock.findByApplicationId(1L)).thenReturn(processRoles);
+
+        zip(processRoles, processRoleResources, (pr, prr) -> when(processRoleMapperMock.mapToResource(same(pr))).thenReturn(prr));
+
+        ServiceResult<List<ProcessRoleResource>> result = usersRolesService.getProcessRolesByApplicationId(1L);
+
+        assertTrue(result.isSuccess());
+        assertEquals(processRoleResources, result.getSuccessObject());
+
+        verify(processRoleRepositoryMock, only()).findByApplicationId(1L);
+    }
+
+    @Test
+    public void getProcessRoleByUserIdAndApplicationId() {
+
+        ProcessRole processRole = newProcessRole().build();
+        ProcessRoleResource processRoleResource = newProcessRoleResource().build();
+
+        when(processRoleRepositoryMock.findByUserIdAndApplicationId(1L, 1L)).thenReturn(processRole);
+
+        when(processRoleMapperMock.mapToResource(same(processRole))).thenReturn(processRoleResource);
+
+        ServiceResult<ProcessRoleResource> result = usersRolesService.getProcessRoleByUserIdAndApplicationId(1L, 1L);
+
+        assertTrue(result.isSuccess());
+        assertEquals(processRoleResource, result.getSuccessObject());
+
+        verify(processRoleRepositoryMock, only()).findByUserIdAndApplicationId(1L, 1L);
+    }
+
+    @Test
+    public void getProcessRolesByUserId() {
+
+        List<ProcessRole> processRoles = newProcessRole().build(2);
+        List<ProcessRoleResource> processRoleResources = newProcessRoleResource().build(2);
+
+        when(processRoleRepositoryMock.findByUserId(1L)).thenReturn(processRoles);
+
+        zip(processRoles, processRoleResources, (pr, prr) -> when(processRoleMapperMock.mapToResource(same(pr))).thenReturn(prr));
+
+        ServiceResult<List<ProcessRoleResource>> result = usersRolesService.getProcessRolesByUserId(1L);
+
+        assertTrue(result.isSuccess());
+        assertEquals(processRoleResources, result.getSuccessObject());
+
+        verify(processRoleRepositoryMock, only()).findByUserId(1L);
+    }
+
+    @Test
+    public void getAssignableProcessRolesByApplicationIdForLeadApplicant() {
+
+        List<ProcessRole> processRoles = newProcessRole().withRole(UserRoleType.LEADAPPLICANT).build(2);
+        List<ProcessRoleResource> processRoleResources = newProcessRoleResource().build(2);
+
+        when(processRoleRepositoryMock.findByApplicationId(1L)).thenReturn(processRoles);
+
+        zip(processRoles, processRoleResources, (pr, prr) -> when(processRoleMapperMock.mapToResource(same(pr))).thenReturn(prr));
+
+        ServiceResult<List<ProcessRoleResource>> result = usersRolesService.getAssignableProcessRolesByApplicationId(1L);
+
+        assertTrue(result.isSuccess());
+        assertEquals(processRoleResources.size(), result.getSuccessObject().size());
+
+        verify(processRoleRepositoryMock, only()).findByApplicationId(1L);
+    }
+
+    @Test
+    public void getAssignableProcessRolesByApplicationIdForCollaborator() {
+
+        List<ProcessRole> processRoles = newProcessRole().withRole(UserRoleType.COLLABORATOR).build(2);
+        List<ProcessRoleResource> processRoleResources = newProcessRoleResource().build(2);
+
+        when(processRoleRepositoryMock.findByApplicationId(1L)).thenReturn(processRoles);
+
+        zip(processRoles, processRoleResources, (pr, prr) -> when(processRoleMapperMock.mapToResource(same(pr))).thenReturn(prr));
+
+        ServiceResult<List<ProcessRoleResource>> result = usersRolesService.getAssignableProcessRolesByApplicationId(1L);
+
+        assertTrue(result.isSuccess());
+        assertEquals(processRoleResources.size(), result.getSuccessObject().size());
+
+        verify(processRoleRepositoryMock, only()).findByApplicationId(1L);
     }
 }
