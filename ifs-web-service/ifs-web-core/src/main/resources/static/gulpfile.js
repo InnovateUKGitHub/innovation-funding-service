887d1a0afba228f723357708806dd342abc4736e
Brent de Kok <brent@worth.systems>
2017-01-03 13:54:37 +0100

    INFUND-6621 altering gulp scripts to copy images and fonts to our folder
    Former-commit-id: c1168a09fc31233b5db58d9033b288862e390267

--- ifs-web-service/ifs-web-core/src/main/resources/static/gulpfile.js
+++ ifs-web-service/ifs-web-core/src/main/resources/static/gulpfile.js
@@ -7,10 +7,10 @@ var sass = require('gulp-sass')
 var sassLint = require('gulp-sass-lint')
 var replace = require('gulp-replace')
 var filesExist = require('files-exist')
+var compass = require('compass-importer')
 
 // Path variables
 var nodeModulesPath = __dirname + '/../../../../../node_modules/'
-
 var govukNodeModules = [
   nodeModulesPath + 'govuk_frontend_toolkit/**/*',
   nodeModulesPath + 'govuk_template_jinja/**/*',
@@ -27,9 +27,21 @@ var vendorJsFiles = [
   '!js/vendor/wysiwyg-editor/hallo-src/*.js'
 ]
 
-gulp.task('copy-gds', function () {
-  return gulp.src(filesExist(govukNodeModules, {checkGlobs: true}),{base: nodeModulesPath}).pipe(gulp.dest('sass/vendor/'))
+gulp.task('copy-govuk',['copy-npm-govuk','copy-fonts-govuk','copy-images-govuk'])
+
+gulp.task('copy-npm-govuk',function(){
+  return gulp.src(filesExist(govukNodeModules, {checkGlobs: true}), {base: nodeModulesPath}).pipe(gulp.dest('sass/vendor/'))
 })
+
+//copy over the font from the template to our sourced controlled folder
+gulp.task('copy-fonts-govuk',function(){
+  return gulp.src(filesExist('sass/vendor/govuk_template_jinja/assets/stylesheets/fonts/*')).pipe(gulp.dest('css/fonts'))
+})
+//copy over the images from the template  to our sourced controlled folder
+gulp.task('copy-images-govuk',function(){
+  return gulp.src(['sass/vendor/govuk_template_jinja/assets/images/**/**','sass/vendor/govuk_frontend_toolkit/images/**/**']).pipe(gulp.dest('images'))
+})
+
 
 gulp.task('default', ['js', 'css'])
 
@@ -63,13 +75,13 @@ gulp.task('vendor', function () {
   .pipe(gulp.dest('js/dest'))
 })
 
-gulp.task('css', ['copy-gds'], function () {
+gulp.task('css', ['copy-govuk'], function () {
   return gulp.src('./sass/**/*.scss')
     .pipe(sassLint({
       files: {
         ignore: [
           '**/prototype.scss',
-          '**/{prototype,vendor}/**/*.scss'
+          '**/prototype/**/*.scss'
         ]
       },
       config: '.sass-lint.yml'
@@ -80,6 +92,7 @@ gulp.task('css', ['copy-gds'], function () {
       'sass/vendor/govuk_template_jinja/assets/stylesheets',
       'sass/vendor/govuk-elements-sass/public/sass'
     ],
+      importer: compass,
       outputStyle: 'compressed'
     }).on('error', sass.logError))
     .pipe(replace('url(images/', 'url(/images/'))
