110ad37c02696681a7c131940732e0e86ee198ec
Luke Harper <lukeharperipl@gmail.com>
2017-01-04 11:53:42 +0000

    INFUND-6390 Fixed 500 on validation error.
    Former-commit-id: 7b081189a93eda22939537583d2553d23949d308

--- ifs-web-service/ifs-application-service/src/main/java/org/innovateuk/ifs/application/ApplicationFormController.java
+++ ifs-web-service/ifs-application-service/src/main/java/org/innovateuk/ifs/application/ApplicationFormController.java
@@ -225,6 +225,12 @@ public class ApplicationFormController {
         List<SectionResource> allSections = sectionService.getAllByCompetitionId(application.getCompetition());
         SectionResource section = simpleFilter(allSections, s -> sectionId.equals(s.getId())).get(0);
 
+        populateSection(form, model, application, section, user, bindingResult, allSections, applicationId, request);
+
+        return APPLICATION_FORM;
+    }
+
+    private void populateSection(ApplicationForm form, Model model, ApplicationResource application, SectionResource section, UserResource user, BindingResult bindingResult, List<SectionResource> allSections, Long applicationId, HttpServletRequest request) {
         if(SectionType.GENERAL.equals(section.getType())
                 || SectionType.OVERVIEW_FINANCES.equals(section.getType())) {
             openSectionModel.populateModel(form, model, application, section, user, bindingResult, allSections);
@@ -232,10 +238,7 @@ public class ApplicationFormController {
             openFinanceSectionModel.populateModel(form, model, application, section, user, bindingResult, allSections);
         }
 
-
         applicationNavigationPopulator.addAppropriateBackURLToModel(applicationId, request, model);
-
-        return APPLICATION_FORM;
     }
 
 
@@ -645,15 +648,16 @@ public class ApplicationFormController {
         UserResource user = userAuthenticationService.getAuthenticatedUser(request);
         ApplicationResource application = applicationService.getById(applicationId);
         CompetitionResource competition = competitionService.getById(application.getCompetition());
+        List<SectionResource> allSections = sectionService.getAllByCompetitionId(application.getCompetition());
         SectionResource section = sectionService.getById(sectionId);
 
         if (section.getType() == SectionType.FUNDING_FINANCES &&
                 !validFinanceTermsForMarkAsComplete(request, form, bindingResult, section, application, competition, user, model)) {
-            applicationNavigationPopulator.addAppropriateBackURLToModel(applicationId, request, model);
+            populateSection(form, model, application, section, user, bindingResult, allSections, applicationId, request);
             return APPLICATION_FORM;
         } else if (section.getType() == SectionType.PROJECT_COST_FINANCES &&
                 !validStateAidForMarkAsComplete(request, form, bindingResult, section, application, competition, user, model)) {
-            applicationNavigationPopulator.addAppropriateBackURLToModel(applicationId, request, model);
+            populateSection(form, model, application, section, user, bindingResult, allSections, applicationId, request);
             return APPLICATION_FORM;
         }
 
@@ -671,8 +675,7 @@ public class ApplicationFormController {
 
         if(saveApplicationErrors.hasErrors()){
             validationHandler.addAnyErrors(saveApplicationErrors);
-            setReturnToApplicationFormData(section, application, competition, user, model, form, applicationId);
-            applicationNavigationPopulator.addAppropriateBackURLToModel(applicationId, request, model);
+            populateSection(form, model, application, section, user, bindingResult, allSections, applicationId, request);
             return APPLICATION_FORM;
         } else {
             return getRedirectUrl(request, applicationId);
