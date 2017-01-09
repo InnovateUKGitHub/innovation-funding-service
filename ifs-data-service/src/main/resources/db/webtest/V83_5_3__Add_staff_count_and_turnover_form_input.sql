ee14abbb24c20b5da2de005b3ae306a8f129436c
richard <richard@hiveit.co.uk>
2017-01-09 10:47:53 +0000

    INFUND-6393 adding the new questions to all of the competitions in the webtest data
    Former-commit-id: 8e313f5c51f62002607c46d091b054d7e80279f2

---
+++ ifs-data-service/src/main/resources/db/webtest/V83_5_3__Add_staff_count_and_turnover_form_input.sql
@@ -0,0 +1,15 @@
+-- Add a form input under the organisation size question for all competitions and templates. Default is inactive.
+-- Staff turnover. Get the form input type and then do an insert for every organisation size question. Priority is 1 as there is already exists a form input.
+SET @staff_turnover_type_id = (SELECT id from form_input_type where name =  'STAFF_TURNOVER');
+INSERT INTO form_input (word_count, form_input_type_id, competition_id, included_in_application_summary, description, guidance_title, guidance_answer, priority, question_id, scope, active)
+     SELECT null, @staff_turnover_type_id, q.competition_id, false, "Turnover (£)", "Your turnover from the last financial year.", "", 1, q.id AS `question_id`, "APPLICATION", false
+     FROM question AS q
+     WHERE `name` = 'Organisation Size';
+-- Staff count. Get the form input type and then do an insert for every organisation size question. Priority is 2 as there are now two form inputs.
+SET @staff_count_type_id = (SELECT id from form_input_type where name =  'STAFF_COUNT');
+INSERT INTO form_input (word_count, form_input_type_id, competition_id, included_in_application_summary, description, guidance_title, guidance_answer, priority, question_id, scope, active)
+     SELECT null, @staff_count_type_id, q.competition_id, false, "Turnover (£)", "Your turnover from the last financial year.", "", 2, q.id AS `question_id`, "APPLICATION", false
+     FROM question AS q
+     WHERE `name` = 'Organisation Size';
+
+
