418edd0512c54d33a5dd7d853bd33ae96d95fffe
Ben Outram <ben@hiveit.co.uk>
2017-01-10 17:28:16 +0000

    INFUND-7046 Adding partner organisations.
    Former-commit-id: 2ae027f7db02c8ab0ae286835c2d8fd151b3bcc8

--- ifs-data-service/src/main/java/org/innovateuk/ifs/application/transactional/ApplicationAssessmentSummaryServiceImpl.java
+++ ifs-data-service/src/main/java/org/innovateuk/ifs/application/transactional/ApplicationAssessmentSummaryServiceImpl.java
@@ -1,10 +1,17 @@
 package org.innovateuk.ifs.application.transactional;
 
+import org.innovateuk.ifs.application.domain.Application;
 import org.innovateuk.ifs.application.resource.ApplicationAssessmentSummaryResource;
 import org.innovateuk.ifs.commons.service.ServiceResult;
 import org.innovateuk.ifs.competition.domain.Competition;
 import org.innovateuk.ifs.transactional.BaseTransactionalService;
+import org.innovateuk.ifs.user.domain.Organisation;
+import org.innovateuk.ifs.user.domain.ProcessRole;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
 
 /**
  * Service for retrieving {@link ApplicationAssessmentSummaryResource}'s.
@@ -14,15 +21,25 @@ public class ApplicationAssessmentSummaryServiceImpl extends BaseTransactionalSe
 
     @Override
     public ServiceResult<ApplicationAssessmentSummaryResource> getApplicationAssessmentSummary(Long applicationId) {
-        return getApplication(applicationId).andOnSuccessReturn(application -> {
+        return getApplication(applicationId).andOnSuccessReturn((Application application) -> {
+
             Competition competition = application.getCompetition();
 
             ApplicationAssessmentSummaryResource applicationAssessmentSummaryResource = new ApplicationAssessmentSummaryResource(application.getId(),
                     application.getName(),
                     competition.getId(),
                     competition.getName(),
-                    null);
+                    getPartnerOrganisationNames(application));
             return applicationAssessmentSummaryResource;
         });
+    }
+
+    private List<String> getPartnerOrganisationNames(Application application) {
+        return application.getProcessRoles().stream()
+                .filter(processRole ->
+                        processRole.isLeadApplicant() || processRole.isCollaborator())
+                .map(ProcessRole::getOrganisation)
+                .map(Organisation::getName)
+                .collect(toList());
     }
 }
--- ifs-data-service/src/test/java/org/innovateuk/ifs/application/transactional/ApplicationAssessmentSummaryServiceImplTest.java
+++ ifs-data-service/src/test/java/org/innovateuk/ifs/application/transactional/ApplicationAssessmentSummaryServiceImplTest.java
@@ -3,12 +3,18 @@ package org.innovateuk.ifs.application.transactional;
 import org.innovateuk.ifs.BaseServiceUnitTest;
 import org.innovateuk.ifs.application.domain.Application;
 import org.innovateuk.ifs.application.resource.ApplicationAssessmentSummaryResource;
+import org.innovateuk.ifs.user.domain.Organisation;
+import org.innovateuk.ifs.user.domain.ProcessRole;
 import org.junit.Test;
 import org.mockito.InOrder;
 
+import static java.util.Arrays.asList;
 import static org.innovateuk.ifs.application.builder.ApplicationAssessmentSummaryResourceBuilder.newApplicationAssessmentSummaryResource;
 import static org.innovateuk.ifs.application.builder.ApplicationBuilder.newApplication;
 import static org.innovateuk.ifs.competition.builder.CompetitionBuilder.newCompetition;
+import static org.innovateuk.ifs.user.builder.OrganisationBuilder.newOrganisation;
+import static org.innovateuk.ifs.user.builder.ProcessRoleBuilder.newProcessRole;
+import static org.innovateuk.ifs.user.resource.UserRoleType.*;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.inOrder;
 import static org.mockito.Mockito.when;
@@ -27,6 +33,12 @@ public class ApplicationAssessmentSummaryServiceImplTest extends BaseServiceUnit
                 .withCompetition(newCompetition()
                         .withName("Connected digital additive manufacturing")
                         .build())
+                .withProcessRoles(newProcessRole()
+                        .withRole(COLLABORATOR, LEADAPPLICANT, COMP_ADMIN)
+                        .withOrganisation(buildOrganisationWithName("Acme Ltd."),
+                                buildOrganisationWithName("IO systems"),
+                                buildOrganisationWithName("Liquid Dynamics"))
+                        .buildArray(3, ProcessRole.class))
                 .build();
 
         ApplicationAssessmentSummaryResource expected = newApplicationAssessmentSummaryResource()
@@ -34,6 +46,7 @@ public class ApplicationAssessmentSummaryServiceImplTest extends BaseServiceUnit
                 .withName(application.getName())
                 .withCompetitionId(application.getCompetition().getId())
                 .withCompetitionName(application.getCompetition().getName())
+                .withPartnerOrganisations(asList("Acme Ltd.", "IO systems"))
                 .build();
 
         when(applicationRepositoryMock.findOne(application.getId())).thenReturn(application);
@@ -45,5 +58,11 @@ public class ApplicationAssessmentSummaryServiceImplTest extends BaseServiceUnit
         InOrder inOrder = inOrder(applicationRepositoryMock);
         inOrder.verify(applicationRepositoryMock).findOne(application.getId());
         inOrder.verifyNoMoreInteractions();
+    }
+
+    private Organisation buildOrganisationWithName(String name) {
+        return newOrganisation()
+                .withName(name)
+                .build();
     }
 }
