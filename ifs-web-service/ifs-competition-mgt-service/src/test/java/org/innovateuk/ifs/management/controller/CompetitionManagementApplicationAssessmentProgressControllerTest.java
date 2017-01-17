b5e0a20138c6ed95e090a53c535114a3dc8f458e
glenn <gauld@worth.systems>
2017-01-17 14:12:05 +0000

    Merge branch 'feature/INFUND-7230-as-a-member-of-the-competitions' of https://devops.innovateuk.org/code-repository/scm/ifs/innovation-funding-service into feature/INFUND-7233-as-a-member-of-the-competitions
    # Conflicts:
    #	ifs-web-service/ifs-competition-mgt-service/src/test/java/org/innovateuk/ifs/management/controller/CompetitionManagementApplicationAssessmentProgressControllerTest.java
    
    
    Former-commit-id: 10e9412055724d53299bb32512f35ebe3b898577

--- ifs-web-service/ifs-competition-mgt-service/src/test/java/org/innovateuk/ifs/management/controller/CompetitionManagementApplicationAssessmentProgressControllerTest.java
+++ ifs-web-service/ifs-competition-mgt-service/src/test/java/org/innovateuk/ifs/management/controller/CompetitionManagementApplicationAssessmentProgressControllerTest.java
@@ -209,7 +209,111 @@ public class CompetitionManagementApplicationAssessmentProgressControllerTest ex
                 .withPartnerOrganisations(asList("Acme Ltd.", "IO Systems"))
                 .build();
 
+        List<ApplicationAssessorResource> assigned = newApplicationAssessorResource()
+                .withFirstName("William", "Richard", "Rachel", "Samantha", "Valerie", "Gareth")
+                .withLastName("Adamson", "Bown", "Carr", "Peacock", "Lloyd", "Morris")
+                .withBusinessType(BUSINESS, ACADEMIC, BUSINESS, ACADEMIC, BUSINESS, ACADEMIC)
+                .withInnovationAreas(newInnovationAreaResource()
+                                .withName("Infrastructure systems", "Earth Observation")
+                                .build(2),
+                        newInnovationAreaResource()
+                                .withName("Internet of Things", "Open")
+                                .build(2),
+                        newInnovationAreaResource()
+                                .withName("Creative Economy", "Bioscience")
+                                .build(2),
+                        newInnovationAreaResource()
+                                .withName("Enhanced Food Quality", "Cyber Security")
+                                .build(2),
+                        newInnovationAreaResource()
+                                .withName("User Experience", "Resource efficiency")
+                                .build(2),
+                        newInnovationAreaResource()
+                                .withName("Technical feasibility", "Medicines Technology")
+                                .build(2)
+                )
+                .withMostRecentAssessmentState(CREATED, PENDING, ACCEPTED, OPEN, READY_TO_SUBMIT, SUBMITTED)
+                .withTotalApplicationsCount(6, 4, 5, 7, 6, 3)
+                .withAssignedCount(6, 3, 1, 5, 2, 1)
+                .withSubmittedCount(4, 1, 0, 2, 1, 0)
+                .build(6);
+
+        List<ApplicationAssessorResource> rejected = newApplicationAssessorResource()
+                .withFirstName("Angela", "Anne", "David")
+                .withLastName("Casey", "Chadwick", "Cherrie")
+                .withBusinessType(ACADEMIC, BUSINESS, ACADEMIC)
+                .withInnovationAreas(newInnovationAreaResource()
+                                .withName("Infrastructure systems", "Earth Observation")
+                                .build(2),
+                        newInnovationAreaResource()
+                                .withName("Internet of Things", "Open")
+                                .build(2),
+                        newInnovationAreaResource()
+                                .withName("Creative Economy", "Bioscience")
+                                .build(2))
+                .withRejectReason("Conflict of interest", "Not available", "Not my area of expertise")
+                .withRejectComment("Member of board of directors", "I do like reviewing the applications to your competitions but please do not assign so many to me.", "No prior experience")
+                .withMostRecentAssessmentState(REJECTED)
+                .withTotalApplicationsCount(6, 7, 1)
+                .withAssignedCount(6, 4, 1)
+                .withSubmittedCount(2, 3, 0)
+                .build(3);
+
+        List<ApplicationAssessorResource> withdrawn = newApplicationAssessorResource()
+                .withFirstName("Paul", "Graeme", "Lawrence")
+                .withLastName("Cousins", "Crawford", "Currie")
+                .withBusinessType(BUSINESS, ACADEMIC, BUSINESS)
+                .withInnovationAreas(newInnovationAreaResource()
+                                .withName("Data", "Cyber Security")
+                                .build(2),
+                        newInnovationAreaResource()
+                                .withName("User Experience", "Precision Medicine")
+                                .build(2),
+                        newInnovationAreaResource()
+                                .withName("Advanced Materials", "Nuclear")
+                                .build(2))
+                .withMostRecentAssessmentState(WITHDRAWN)
+                .withTotalApplicationsCount(24, 2, 5)
+                .withAssignedCount(6, 1, 3)
+                .withSubmittedCount(2, 0, 3)
+                .build(3);
+
+        List<ApplicationAssessorResource> available = newApplicationAssessorResource()
+                .withFirstName("Christopher", "Jayne", "Narinder")
+                .withLastName("Dockerty", "Gill", "Goddard")
+                .withBusinessType(ACADEMIC, BUSINESS, ACADEMIC)
+                .withInnovationAreas(newInnovationAreaResource()
+                                .withName("Experimental development", "Infrastructure")
+                                .build(2),
+                        newInnovationAreaResource()
+                                .withName("Electronics, Sensors and photonics", "Agri Productivity")
+                                .build(2),
+                        newInnovationAreaResource()
+                                .withName("Manufacturing Readiness", "Offshore Renewable Energy")
+                                .build(2))
+                .withSkillAreas("Solar Power, Genetics, Recycling", "Human computer interaction, Wearables, IoT", "Electronic/photonic components")
+                .withAvailable(true)
+                .withTotalApplicationsCount(9, 4, 3)
+                .withAssignedCount(5, 1, 1)
+                .withSubmittedCount(2, 1, 0)
+                .build(3);
+
         when(applicationAssessmentSummaryRestService.getApplicationAssessmentSummary(applicationId)).thenReturn(restSuccess(applicationAssessmentSummaryResource));
+        when(applicationAssessmentSummaryRestService.getAssessors(applicationId)).thenReturn(restSuccess(combineLists(assigned, rejected, withdrawn, available)));
+
+        List<ApplicationAssessmentProgressAssignedRowViewModel> expectedAssignedRows = asList(
+                new ApplicationAssessmentProgressAssignedRowViewModel("William Adamson", 6, 6, BUSINESS,
+                        asList("Infrastructure systems", "Earth Observation"), false, false, false, false),
+                new ApplicationAssessmentProgressAssignedRowViewModel("Richard Bown", 4, 3, ACADEMIC,
+                        asList("Internet of Things", "Open"), true, false, false, false),
+                new ApplicationAssessmentProgressAssignedRowViewModel("Rachel Carr", 5, 1, BUSINESS,
+                        asList("Creative Economy", "Bioscience"), true, true, false, false),
+                new ApplicationAssessmentProgressAssignedRowViewModel("Samantha Peacock", 7, 5, ACADEMIC,
+                        asList("Enhanced Food Quality", "Cyber Security"), true, true, true, false),
+                new ApplicationAssessmentProgressAssignedRowViewModel("Valerie Lloyd", 6, 2, BUSINESS,
+                        asList("User Experience", "Resource efficiency"), true, true, true, false),
+                new ApplicationAssessmentProgressAssignedRowViewModel("Gareth Morris", 3, 1, ACADEMIC,
+                        asList("Technical feasibility", "Medicines Technology"), true, true, true, true));
 
         ApplicationAssessmentProgressViewModel expectedModel = new ApplicationAssessmentProgressViewModel(
                 applicationId,
@@ -219,6 +323,7 @@ public class CompetitionManagementApplicationAssessmentProgressControllerTest ex
                 asList("Acme Ltd.", "IO Systems"),
                 expectedAssignedRows
         );
+
 
         List<ApplicationAvailableAssessorsRowViewModel> assessors = asList(
                 new ApplicationAvailableAssessorsRowViewModel("John Smith", "John's skills", 10, 4, 3),
