0831de75047dda4a229d240f4870ffee8be0f5a0
Bronnyl Dias <bdias@worth.systems>
2017-01-18 16:40:40 +0000

    INFUND-4839-Save-Eligibility-RAG
    1) Redoing the changes after merging the latest code from Dan.
    
    Change-Id: I5a7e6c030b03fcdfca50e5cfcb66d28c267676d8
    
    Former-commit-id: 4db7498feebe61b374061685823ccdc78f845186

--- ifs-web-service/ifs-project-setup-mgt-service/src/main/java/org/innovateuk/ifs/project/eligibility/viewmodel/FinanceChecksEligibilityViewModel.java
+++ ifs-web-service/ifs-project-setup-mgt-service/src/main/java/org/innovateuk/ifs/project/eligibility/viewmodel/FinanceChecksEligibilityViewModel.java
@@ -1,7 +1,10 @@
 package org.innovateuk.ifs.project.eligibility.viewmodel;
 
 
+import org.innovateuk.ifs.project.finance.resource.EligibilityStatus;
 import org.innovateuk.ifs.project.finance.resource.FinanceCheckEligibilityResource;
+
+import java.time.LocalDate;
 
 /**
  * View model backing the internal Finance Team members view of the Finance Check Eligibility page
@@ -14,13 +17,44 @@ public class FinanceChecksEligibilityViewModel {
     private String applicationId;
     private Long projectId;
 
-    public FinanceChecksEligibilityViewModel(FinanceCheckEligibilityResource eligibilityOverview, String organisationName, String projectName, String applicationId, boolean leadPartnerOrganisation, Long projectId) {
+    private boolean eligibilityApproved;
+    private EligibilityStatus eligibilityStatus;
+    private String approverFirstName;
+    private String approverLastName;
+    private LocalDate approvalDate;
+
+    public FinanceChecksEligibilityViewModel(FinanceCheckEligibilityResource eligibilityOverview, String organisationName, String projectName,
+                                             String applicationId, boolean leadPartnerOrganisation, Long projectId,
+                                             boolean eligibilityApproved, EligibilityStatus eligibilityStatus, String approverFirstName,
+                                             String approverLastName, LocalDate approvalDate) {
         this.eligibilityOverview = eligibilityOverview;
         this.organisationName = organisationName;
         this.projectName = projectName;
         this.applicationId = applicationId;
         this.leadPartnerOrganisation = leadPartnerOrganisation;
         this.projectId = projectId;
+
+        this.eligibilityApproved = eligibilityApproved;
+        this.eligibilityStatus = eligibilityStatus;
+        this.approverFirstName = approverFirstName;
+        this.approverLastName = approverLastName;
+        this.approvalDate = approvalDate;
+    }
+
+    public boolean isReadOnly() {
+        return eligibilityApproved;
+    }
+
+    private boolean isApproved() {
+        return eligibilityApproved;
+    }
+
+    public boolean isShowSaveAndContinueButton() {
+        return !isApproved();
+    }
+
+    public boolean isShowBackToFinanceCheckButton() {
+        return isApproved();
     }
 
     public FinanceCheckEligibilityResource getEligibilityOverview() {
@@ -69,5 +103,45 @@ public class FinanceChecksEligibilityViewModel {
 
     public void setProjectId(Long projectId) {
         this.projectId = projectId;
+    }
+
+    public boolean isEligibilityApproved() {
+        return eligibilityApproved;
+    }
+
+    public void setEligibilityApproved(boolean eligibilityApproved) {
+        this.eligibilityApproved = eligibilityApproved;
+    }
+
+    public EligibilityStatus getEligibilityStatus() {
+        return eligibilityStatus;
+    }
+
+    public void setEligibilityStatus(EligibilityStatus eligibilityStatus) {
+        this.eligibilityStatus = eligibilityStatus;
+    }
+
+    public String getApproverFirstName() {
+        return approverFirstName;
+    }
+
+    public void setApproverFirstName(String approverFirstName) {
+        this.approverFirstName = approverFirstName;
+    }
+
+    public String getApproverLastName() {
+        return approverLastName;
+    }
+
+    public void setApproverLastName(String approverLastName) {
+        this.approverLastName = approverLastName;
+    }
+
+    public LocalDate getApprovalDate() {
+        return approvalDate;
+    }
+
+    public void setApprovalDate(LocalDate approvalDate) {
+        this.approvalDate = approvalDate;
     }
 }
