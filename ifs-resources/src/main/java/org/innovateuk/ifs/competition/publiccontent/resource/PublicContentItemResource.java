6c0ef37dffb902a289e941eb4fc4d579f67967cb
Wouter de Meijer <wdemeijer@worth.systems>
2017-01-26 16:48:00 +0100

    INFUND-6923 - Initial version for competition list page resource and web/data service interface
    Former-commit-id: 87499535f5e862c66e443601ce648fe7ca43f708

---
+++ ifs-resources/src/main/java/org/innovateuk/ifs/competition/publiccontent/resource/PublicContentItemResource.java
@@ -0,0 +1,11 @@
+package org.innovateuk.ifs.competition.publiccontent.resource;
+
+
+import java.time.LocalDateTime;
+
+public class PublicContentItemResource {
+    PublicContentResource publicContentResource;
+    String competitionTitle;
+    LocalDateTime competitionOpenDate;
+    LocalDateTime competitionCloseDate;
+}
--- ifs-rest-api-service/src/main/java/org/innovateuk/ifs/publiccontent/service/PublicContentRestService.java
+++ ifs-rest-api-service/src/main/java/org/innovateuk/ifs/publiccontent/service/PublicContentRestService.java
@@ -1,7 +1,11 @@
 package org.innovateuk.ifs.publiccontent.service;
 
 import org.innovateuk.ifs.commons.rest.RestResult;
+import org.innovateuk.ifs.competition.publiccontent.resource.PublicContentItemPageResource;
 import org.innovateuk.ifs.competition.publiccontent.resource.PublicContentResource;
+
+import java.util.List;
+import java.util.Optional;
 
 /**
  * Created by luke.harper on 25/01/2017.
@@ -11,4 +15,11 @@ public interface PublicContentRestService {
     RestResult<PublicContentResource> getByCompetitionId(final Long id);
 
     RestResult<Void> publishByCompetitionId(Long competitionId);
+
+    RestResult<PublicContentItemPageResource> getByFilterValues(Optional<Long> innovationAreaId,
+                                                                Optional<List<String>> keywords,
+                                                                Optional<Long> pageNumber,
+                                                                Optional<Long> pageSize);
+
+    RestResult<PublicContentItemPageResource> getByItemCompetitionId(final Long id);
 }
