6ce8ca41e996040c53892db49bf4e2c907455fb3
Jack Scott <jack.scott.ipl@gmail.com>
2017-01-27 11:24:24 +0000

    Added new page controller and html for page - needs tests added, current template issue
    Former-commit-id: f7f83eb5e593b0fa5dbe3eb0ce5432c6855a6d39

--- ifs-web-service/ifs-project-setup-service/src/main/java/org/innovateuk/ifs/project/financechecks/controller/FinanceChecksController.java
+++ ifs-web-service/ifs-project-setup-service/src/main/java/org/innovateuk/ifs/project/financechecks/controller/FinanceChecksController.java
@@ -5,24 +5,17 @@ import org.innovateuk.ifs.project.ProjectService;
 import org.innovateuk.ifs.project.financechecks.viewmodel.ProjectFinanceChecksViewModel;
 import org.innovateuk.ifs.project.resource.ProjectPartnerStatusResource;
 import org.innovateuk.ifs.project.resource.ProjectResource;
-import org.innovateuk.ifs.project.resource.ProjectTeamStatusResource;
-import org.innovateuk.ifs.project.resource.ProjectUserResource;
 import org.innovateuk.ifs.user.resource.OrganisationResource;
-import org.innovateuk.ifs.user.resource.UserResource;
-import org.innovateuk.ifs.user.resource.UserRoleType;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.List;
 import java.util.Optional;
 
 import static org.innovateuk.ifs.project.constant.ProjectActivityStates.COMPLETE;
-import static org.innovateuk.ifs.user.resource.UserRoleType.PROJECT_FINANCE;
-import static org.innovateuk.ifs.util.CollectionFunctions.simpleFindFirst;
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 /**
@@ -40,18 +33,18 @@ public class FinanceChecksController {
     @Autowired
     OrganisationService organisationService;
 
+    @PreAuthorize("hasPermission(#projectId, 'ACCESS_FINANCE_CHECKS_SECTION')")
     @RequestMapping(method = GET)
     public String viewFinanceChecks(Model model,
                                                @PathVariable("projectId") final Long projectId,
-                                               @PathVariable("organisationId") final Long organisationId,
-                                               @ModelAttribute("loggedInUser")UserResource loggedInUser) {
+                                               @PathVariable("organisationId") final Long organisationId) {
 
-        model.addAttribute("model", buildFinanceChecksLandingPage(projectId, organisationId, loggedInUser));
+        model.addAttribute("model", buildFinanceChecksLandingPage(projectId, organisationId));
 
         return "project/finance-checks";
     }
 
-    private ProjectFinanceChecksViewModel buildFinanceChecksLandingPage(final Long projectId, final Long organisationId, final UserResource loggedInUser) {
+    private ProjectFinanceChecksViewModel buildFinanceChecksLandingPage(final Long projectId, final Long organisationId) {
         ProjectResource projectResource = projectService.getById(projectId);
         OrganisationResource organisationResource = organisationService.getOrganisationById(organisationId);
         boolean approved = isApproved(projectId, organisationId);
@@ -60,7 +53,7 @@ public class FinanceChecksController {
     }
 
     private boolean isApproved(final Long projectId, final Long organisationId) {
-        ProjectTeamStatusResource teamStatus = projectService.getProjectTeamStatus(projectId, Optional.empty());
-        return COMPLETE.equals(teamStatus.getPartnerStatusForOrganisation(organisationId).get().getFinanceChecksStatus());
+        Optional<ProjectPartnerStatusResource> organisationStatus = projectService.getProjectTeamStatus(projectId, Optional.empty()).getPartnerStatusForOrganisation(organisationId);
+        return COMPLETE.equals(organisationStatus.get().getFinanceChecksStatus());
     }
 }
