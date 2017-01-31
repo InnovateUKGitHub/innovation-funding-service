e7b7e840df0318677f2e9db9b770da7c3c63c37f
Peter Moreton <pctmoreton@gmail.com>
2017-01-31 15:23:08 +0000

    INFUND-7560 Added more unit tests
    Former-commit-id: 6db9b37134a3cc78c9eb199fb508c2539b36f210

--- ifs-web-service/ifs-competition-mgt-service/src/test/java/org/innovateuk/ifs/management/controller/CompetitionManagementCompetitionControllerTest.java
+++ ifs-web-service/ifs-competition-mgt-service/src/test/java/org/innovateuk/ifs/management/controller/CompetitionManagementCompetitionControllerTest.java
@@ -2,10 +2,9 @@ package org.innovateuk.ifs.management.controller;
 
 import org.innovateuk.ifs.BaseControllerMockMVCTest;
 import org.innovateuk.ifs.assessment.resource.AssessmentStates;
-import org.innovateuk.ifs.competition.resource.CompetitionResource;
-import org.innovateuk.ifs.competition.resource.MilestoneResource;
-import org.innovateuk.ifs.competition.resource.MilestoneType;
+import org.innovateuk.ifs.competition.resource.*;
 import org.innovateuk.ifs.management.model.CompetitionInFlightModelPopulator;
+import org.innovateuk.ifs.management.viewmodel.CompetitionInFlightStatsViewModel;
 import org.innovateuk.ifs.management.viewmodel.CompetitionInFlightViewModel;
 import org.innovateuk.ifs.management.viewmodel.MilestonesRowViewModel;
 import org.junit.Test;
@@ -22,11 +21,14 @@ import java.util.Comparator;
 import java.util.List;
 
 import static org.innovateuk.ifs.commons.rest.RestResult.restSuccess;
+import static org.innovateuk.ifs.competition.builder.CompetitionClosedKeyStatisticsResourceBuilder.newCompetitionClosedKeyStatisticsResource;
 import static org.innovateuk.ifs.competition.builder.CompetitionFunderResourceBuilder.newCompetitionFunderResource;
+import static org.innovateuk.ifs.competition.builder.CompetitionInAssessmentKeyStatisticsResourceBuilder.newCompetitionInAssessmentKeyStatisticsResource;
+import static org.innovateuk.ifs.competition.builder.CompetitionOpenKeyStatisticsResourceBuilder.newCompetitionOpenKeyStatisticsResource;
+import static org.innovateuk.ifs.competition.builder.CompetitionReadyToOpenKeyStatisticsResourceBuilder.newCompetitionReadyToOpenKeyStatisticsResource;
 import static org.innovateuk.ifs.competition.builder.CompetitionResourceBuilder.newCompetitionResource;
 import static org.innovateuk.ifs.competition.builder.MilestoneResourceBuilder.newMilestoneResource;
-import static org.innovateuk.ifs.competition.resource.CompetitionStatus.CLOSED;
-import static org.innovateuk.ifs.competition.resource.CompetitionStatus.IN_ASSESSMENT;
+import static org.innovateuk.ifs.competition.resource.CompetitionStatus.*;
 import static org.innovateuk.ifs.util.CollectionFunctions.asLinkedSet;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
@@ -39,6 +41,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @TestPropertySource(locations = "classpath:application.properties")
 public class CompetitionManagementCompetitionControllerTest extends BaseControllerMockMVCTest<CompetitionManagementCompetitionController> {
 
+    private static final long competitionId = 1L;
+
     @Spy
     @InjectMocks
     private CompetitionInFlightModelPopulator competitionInFlightModelPopulator;
@@ -49,10 +53,65 @@ public class CompetitionManagementCompetitionControllerTest extends BaseControll
     }
 
     @Test
-    public void competition_inFlight() throws Exception {
+    public void competition_readyToOpen() throws Exception {
+        CompetitionReadyToOpenKeyStatisticsResource keyStatisticsResource = newCompetitionReadyToOpenKeyStatisticsResource()
+                .withAssessorsInvited(1L)
+                .withAssessorsAccepted(2L)
+                .build();
+        when(competitionKeyStatisticsRestServiceMock.getReadyToOpenKeyStatisticsByCompetition(competitionId)).thenReturn(restSuccess(keyStatisticsResource));
+        competition_inFlight(READY_TO_OPEN);
+        verify(competitionKeyStatisticsRestServiceMock,only()).getReadyToOpenKeyStatisticsByCompetition(competitionId);
+    }
+
+    @Test
+    public void competition_open() throws Exception {
+        CompetitionOpenKeyStatisticsResource keyStatisticsResource = newCompetitionOpenKeyStatisticsResource()
+                .withAssessorsInvited(1L)
+                .withAssessorsAccepted(2L)
+                .withApplicationsPerAssessor(3L)
+                .withApplicationsStarted(4L)
+                .withApplicationsPastHalf(5L)
+                .withApplicationsSubmitted(6L)
+                .build();
+        when(competitionKeyStatisticsRestServiceMock.getOpenKeyStatisticsByCompetition(competitionId)).thenReturn(restSuccess(keyStatisticsResource));
+        competition_inFlight(OPEN);
+        verify(competitionKeyStatisticsRestServiceMock,only()).getOpenKeyStatisticsByCompetition(competitionId);
+    }
+
+    @Test
+    public void competition_closed() throws Exception {
+        CompetitionClosedKeyStatisticsResource keyStatisticsResource = newCompetitionClosedKeyStatisticsResource()
+                .withApplicationsRequiringAssessors(1L)
+                .withAssignmentCount(2L)
+                .withAssessorsWithoutApplications(3L)
+                .withAssessorsInvited(4L)
+                .withAssessorsAccepted(5L)
+                .withApplicationsPerAssessor(6L)
+                .build();
+        when(competitionKeyStatisticsRestServiceMock.getClosedKeyStatisticsByCompetition(competitionId)).thenReturn(restSuccess(keyStatisticsResource));
+        competition_inFlight(CLOSED);
+        verify(competitionKeyStatisticsRestServiceMock,only()).getClosedKeyStatisticsByCompetition(competitionId);
+    }
+
+    @Test
+    public void competition_inAssessment() throws Exception {
+        CompetitionInAssessmentKeyStatisticsResource keyStatisticsResource = newCompetitionInAssessmentKeyStatisticsResource()
+                .withAssignmentCount(1L)
+                .withAssignmentsWaiting(2L)
+                .withAssignmentsAccepted(3L)
+                .withAssessmentsStarted(4L)
+                .withAssessmentsSubmitted(5L)
+                .build();
+        when(competitionKeyStatisticsRestServiceMock.getInAssessmentKeyStatisticsByCompetition(competitionId)).thenReturn(restSuccess(keyStatisticsResource));
+        competition_inFlight(IN_ASSESSMENT);
+        verify(competitionKeyStatisticsRestServiceMock,only()).getInAssessmentKeyStatisticsByCompetition(competitionId);
+    }
+
+    private void competition_inFlight(CompetitionStatus status) throws Exception {
 
         CompetitionResource competition = newCompetitionResource()
-                .withCompetitionStatus(IN_ASSESSMENT)
+                .withId(competitionId)
+                .withCompetitionStatus(status)
                 .withCompetitionTypeName("Programme")
                 .withInnovationSectorName("Materials and manufacturing")
                 .withInnovationAreaNames(asLinkedSet("Earth Observation", "Transport Systems"))
@@ -85,7 +144,7 @@ public class CompetitionManagementCompetitionControllerTest extends BaseControll
 
         assertEquals(competition.getId(), model.getCompetitionId());
         assertEquals("Technology inspired", model.getCompetitionName());
-        assertEquals(IN_ASSESSMENT, model.getCompetitionStatus());
+        assertEquals(status, model.getCompetitionStatus());
         assertEquals("Programme", model.getCompetitionType());
         assertEquals("Materials and manufacturing", model.getInnovationSector());
         assertEquals("Earth Observation, Transport Systems", model.getInnovationArea());
@@ -93,9 +152,25 @@ public class CompetitionManagementCompetitionControllerTest extends BaseControll
         assertEquals("Ian Cooper", model.getLead());
         assertTrue(new BigInteger("2000000").compareTo(model.getFunding()) == 0);
         assertMilestones(milestoneResources, model.getMilestones());
+        assertKeyStatistics(model.getKeyStatistics(),status);
 
         verify(competitionService, only()).getById(competition.getId());
         verify(assessmentRestService, only()).countByStateAndCompetition(AssessmentStates.CREATED, competition.getId());
+    }
+
+    private void assertKeyStatistics(CompetitionInFlightStatsViewModel model, CompetitionStatus state) {
+        switch (state) {
+            case OPEN:
+            case CLOSED:
+                assertEquals(6L, (long) model.getStatSix());
+            case IN_ASSESSMENT:
+                assertEquals(5L, (long) model.getStatFive());
+                assertEquals(4L, (long) model.getStatFour());
+                assertEquals(3L, (long) model.getStatThree());
+            case READY_TO_OPEN:
+                assertEquals(2L, model.getStatTwo());
+                assertEquals(1L, model.getStatOne());
+        }
     }
 
     @Test
