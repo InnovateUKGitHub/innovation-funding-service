2183e7d9294b3727af13d170f2ea936ae1efe077
Nuno Alexadre <nalexandre@worth.systems>
2017-02-01 17:46:00 +0100

    Adding sql scripts to create tables that support the notes and queries feature (running into issues)
    Former-commit-id: d0329986e0b945a70369810f6a1e5cf01260a025

--- ifs-data-service/src/main/java/org/innovateuk/ifs/threads/domain/Post.java
+++ ifs-data-service/src/main/java/org/innovateuk/ifs/threads/domain/Post.java
@@ -5,6 +5,7 @@ import org.innovateuk.ifs.user.domain.User;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
 public class Post {
@@ -13,6 +14,7 @@ public class Post {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private User author;
+    @NotNull
     private String body;
     private List<PostAttachment> attachments;
 
---
+++ ifs-data-service/src/main/resources/db/migration/V86_3_1__Create_Notes_And_Queries_tables.sql
@@ -0,0 +1,29 @@
+CREATE TABLE `thread` (
+  `id` BIGINT(20) NOT NULL,
+  `class_pk` BIGINT(20) NOT NULL,
+  `class_name` VARCHAR(255) NOT NULL,
+  `thread_type` VARCHAR(45) NOT NULL,
+  `title` VARCHAR(255) NOT NULL,
+  `created_on` DATETIME NOT NULL,
+  `section_type` VARCHAR(45) NULL,
+  PRIMARY KEY (`id`));
+
+
+CREATE TABLE `post` (
+  `id` BIGINT(20) NOT NULL,
+  `thread_id` BIGINT(20) NOT NULL,
+  `author_id` BIGINT(20) NOT NULL,
+  `body` TEXT NOT NULL,
+  `created_on` DATETIME NOT NULL,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `post_thread_fk` FOREIGN KEY (`thread_id`) REFERENCES `ifs`.`thread` (`id`),
+  CONSTRAINT `post_author_fk` FOREIGN KEY (`author_id`) REFERENCES `ifs`.`user` (`id`));
+
+
+CREATE TABLE `ifs`.`post_attachment` (
+  `id` BIGINT(20) NOT NULL,
+  `post_id` BIGINT(20) NOT NULL,
+  `file_entry_id` BIGINT(20) NOT NULL,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `post_attachment_post_fk` FOREIGN KEY (`post_id`) REFERENCES `ifs`.`post` (`id`),
+  CONSTRAINT `post_attachment_file_entry_fk` FOREIGN KEY (`file_entry_id`) REFERENCES `ifs`.`file_entry` (`id`));
