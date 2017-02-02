183f9dd3d054789d1aa885ecadd0a65600715b40
Simon Shakespeare <si@hiveit.co.uk>
2017-02-02 16:33:52 +0000

    INFUND-6895 - New unit tests for FundingLevelHandler
    Former-commit-id: f4c4aa6f9ba4e6323f61903e3189236c5d82e1cb

--- ifs-web-service/ifs-web-core/src/test/java/org/innovateuk/ifs/application/finance/view/FundingLevelResetHandlerTest.java
+++ ifs-web-service/ifs-web-core/src/test/java/org/innovateuk/ifs/application/finance/view/FundingLevelResetHandlerTest.java
@@ -1,8 +1,18 @@
 package org.innovateuk.ifs.application.finance.view;
 
+import org.innovateuk.ifs.application.builder.QuestionResourceBuilder;
+import org.innovateuk.ifs.application.builder.SectionResourceBuilder;
+import org.innovateuk.ifs.application.finance.service.FinanceRowService;
+import org.innovateuk.ifs.application.finance.service.FinanceService;
+import org.innovateuk.ifs.application.resource.QuestionResource;
+import org.innovateuk.ifs.application.resource.SectionResource;
+import org.innovateuk.ifs.application.resource.SectionType;
+import org.innovateuk.ifs.application.service.QuestionService;
 import org.innovateuk.ifs.application.service.SectionService;
+import org.innovateuk.ifs.commons.rest.RestResult;
 import org.innovateuk.ifs.finance.builder.ApplicationFinanceResourceBuilder;
 import org.innovateuk.ifs.finance.resource.ApplicationFinanceResource;
+import org.innovateuk.ifs.form.resource.FormInputType;
 import org.innovateuk.ifs.user.builder.ProcessRoleResourceBuilder;
 import org.innovateuk.ifs.user.builder.UserResourceBuilder;
 import org.innovateuk.ifs.user.resource.ProcessRoleResource;
@@ -10,12 +20,15 @@ import org.innovateuk.ifs.user.resource.UserResource;
 import org.innovateuk.ifs.user.service.ProcessRoleService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InOrder;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import java.util.List;
 
+import static org.mockito.Mockito.calls;
 import static org.mockito.Mockito.when;
 
 /**
@@ -33,22 +46,63 @@ public class FundingLevelResetHandlerTest {
     @Mock
     private SectionService sectionService;
 
+    @Mock
+    private QuestionService questionService;
+
+    @Mock
+    private FinanceRowService financeRowService;
+
+    @Mock
+    private FinanceService financeService;
+
     @Test
     public void tetsResetFundingAndMarkAsIncomplete() {
 
-        ApplicationFinanceResource applicationFinanceResource = ApplicationFinanceResourceBuilder.newApplicationFinanceResource().withApplication(1L).build();
-        UserResource user = UserResourceBuilder.newUserResource().withId(2L).build();
-        List<ProcessRoleResource> processRoles = ProcessRoleResourceBuilder.newProcessRoleResource().withApplication(1L).withUser(user).build(2);
+        Long applicationId = 1L;
+        Long competitionId = 3L;
+        Long userId = 2L;
+
+        ApplicationFinanceResource applicationFinanceResource = ApplicationFinanceResourceBuilder.newApplicationFinanceResource().withApplication(applicationId).build();
+        UserResource user = UserResourceBuilder.newUserResource().withId(userId).build();
+        List<ProcessRoleResource> processRoles = ProcessRoleResourceBuilder.newProcessRoleResource().withApplication(applicationId).withUser(user).build(1);
+        List<SectionResource> sectionResources = SectionResourceBuilder.newSectionResource().build(1);
+        QuestionResource questionResource = QuestionResourceBuilder.newQuestionResource().build();
 
         when(processRoleService.getByApplicationId(1L)).thenReturn(processRoles);
+        when(sectionService.getSectionsForCompetitionByType(competitionId, SectionType.FUNDING_FINANCES)).thenReturn(sectionResources);
+        when(questionService.getQuestionByCompetitionIdAndFormInputType(competitionId, FormInputType.FINANCE)).thenReturn(RestResult.restSuccess(questionResource));
 
-        target.resetFundingAndMarkAsIncomplete(applicationFinanceResource, 1L, 2L);
+        target.resetFundingAndMarkAsIncomplete(applicationFinanceResource, competitionId, userId);
+
+        Mockito.inOrder(sectionService).verify(sectionService, calls(1)).markAsInComplete(sectionResources.get(0).getId(),
+                applicationId, processRoles.get(0).getId());
     }
 
     @Test
     public void resetFundingLevelAndMarkAsIncompleteForAllCollaborators() {
 
-        target.resetFundingLevelAndMarkAsIncompleteForAllCollaborators(1l, 1L);
-    }
+        Long applicationId = 1L;
+        Long competitionId = 3L;
+        Long userId = 2L;
 
+        UserResource user = UserResourceBuilder.newUserResource().withId(userId).build();
+        List<ProcessRoleResource> processRoles = ProcessRoleResourceBuilder.newProcessRoleResource().withApplication(applicationId).withUser(user).build(2);
+        List<SectionResource> sectionResources = SectionResourceBuilder.newSectionResource().build(1);
+        QuestionResource questionResource = QuestionResourceBuilder.newQuestionResource().build();
+        List<ApplicationFinanceResource> applicationFinanceResources = ApplicationFinanceResourceBuilder.newApplicationFinanceResource().build(3);
+
+        when(processRoleService.getByApplicationId(1L)).thenReturn(processRoles);
+        when(sectionService.getSectionsForCompetitionByType(competitionId, SectionType.FUNDING_FINANCES)).thenReturn(sectionResources);
+        when(questionService.getQuestionByCompetitionIdAndFormInputType(competitionId, FormInputType.FINANCE)).thenReturn(RestResult.restSuccess(questionResource));
+        when(financeService.getApplicationFinanceDetails(applicationId)).thenReturn(applicationFinanceResources);
+
+        target.resetFundingLevelAndMarkAsIncompleteForAllCollaborators(competitionId, applicationId);
+
+        InOrder inOrder = Mockito.inOrder(sectionService, financeService);
+
+        inOrder.verify(sectionService, calls(1)).markAsInComplete(sectionResources.get(0).getId(),applicationId, processRoles.get(0).getId());
+        inOrder.verify(sectionService, calls(1)).markAsInComplete(sectionResources.get(0).getId(),applicationId, processRoles.get(1).getId());
+
+        inOrder.verify(financeService, calls(1)).getApplicationFinanceDetails(applicationId);
+    }
 }
