441d73eb365a4e983235e0c6d0b595312498b755
Nuno Alexadre <nalexandre@worth.systems>
2017-02-02 17:34:02 +0100

    Adding ProjectFinanceNotesController
    Former-commit-id: 736e2fcd0a0524c3be85cc8bda1f743accedb865

---
+++ ifs-data-service/src/main/java/org/innovateuk/ifs/project/finance/controller/ProjectFinanceNotesController.java
@@ -0,0 +1,40 @@
+package org.innovateuk.ifs.project.finance.controller;
+
+import org.innovateuk.ifs.commons.rest.RestResult;
+import org.innovateuk.ifs.finance.domain.ProjectFinance;
+import org.innovateuk.ifs.threads.domain.Post;
+import org.innovateuk.ifs.threads.domain.Note;
+import org.innovateuk.ifs.threads.service.ThreadService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
+
+@RestController
+@RequestMapping("/project/finance/notes")
+public class ProjectFinanceNotesController {
+    @Autowired
+    ThreadService<Note, ProjectFinance> service;
+
+    @RequestMapping(value = "/{projectFinanceId}", method = GET)
+    public RestResult<List<Note>> notes(@PathVariable("projectFinanceId") final Long projectFinanceId) {
+        return service.findAll(projectFinanceId).toGetResponse();
+    }
+
+    @RequestMapping(value = "", method = POST)
+    public RestResult<Void> createNote(@RequestBody Note note ) {
+        return service.create(note).toPostCreateResponse();
+    }
+
+    @RequestMapping(value = "/{noteId}/post", method = POST)
+    public RestResult<Void> addPost(@PathVariable("noteId") Long noteId, @RequestBody Post post ) {
+        return service.addPost(post, noteId).toPostCreateResponse();
+    }
+
+}
