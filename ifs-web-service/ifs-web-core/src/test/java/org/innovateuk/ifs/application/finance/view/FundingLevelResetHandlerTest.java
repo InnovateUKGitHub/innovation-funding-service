584456de357d25f69eb39b71c8f1907cabc7e936
Simon Shakespeare <si@hiveit.co.uk>
2017-02-02 17:10:35 +0000

    INFUND-6895 - New unit tests for FundingLevelHandler
    Former-commit-id: 68b6a443fa6ccd9d457e9c323105d5bc4f7dfd0e

--- ifs-web-service/ifs-web-core/src/test/java/org/innovateuk/ifs/application/finance/view/FundingLevelResetHandlerTest.java
+++ ifs-web-service/ifs-web-core/src/test/java/org/innovateuk/ifs/application/finance/view/FundingLevelResetHandlerTest.java
@@ -12,9 +12,12 @@ import org.innovateuk.ifs.application.service.SectionService;
 import org.innovateuk.ifs.commons.rest.RestResult;
 import org.innovateuk.ifs.finance.builder.ApplicationFinanceResourceBuilder;
 import org.innovateuk.ifs.finance.resource.ApplicationFinanceResource;
+import org.innovateuk.ifs.finance.resource.category.GrantClaimCategory;
+import org.innovateuk.ifs.finance.resource.cost.FinanceRowItem;
 import org.innovateuk.ifs.form.resource.FormInputType;
 import org.innovateuk.ifs.user.builder.ProcessRoleResourceBuilder;
 import org.innovateuk.ifs.user.builder.UserResourceBuilder;
+import org.innovateuk.ifs.user.resource.OrganisationTypeEnum;
 import org.innovateuk.ifs.user.resource.ProcessRoleResource;
 import org.innovateuk.ifs.user.resource.UserResource;
 import org.innovateuk.ifs.user.service.ProcessRoleService;
@@ -28,6 +31,9 @@ import org.mockito.runners.MockitoJUnitRunner;
 
 import java.util.List;
 
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Matchers.isA;
 import static org.mockito.Mockito.calls;
 import static org.mockito.Mockito.when;
 
@@ -62,7 +68,8 @@ public class FundingLevelResetHandlerTest {
         Long competitionId = 3L;
         Long userId = 2L;
 
-        ApplicationFinanceResource applicationFinanceResource = ApplicationFinanceResourceBuilder.newApplicationFinanceResource().withApplication(applicationId).build();
+        ApplicationFinanceResource applicationFinanceResource = ApplicationFinanceResourceBuilder.newApplicationFinanceResource()
+                .withApplication(applicationId).withGrantClaimPercentage(20).build();
         UserResource user = UserResourceBuilder.newUserResource().withId(userId).build();
         List<ProcessRoleResource> processRoles = ProcessRoleResourceBuilder.newProcessRoleResource().withApplication(applicationId).withUser(user).build(1);
         List<SectionResource> sectionResources = SectionResourceBuilder.newSectionResource().build(1);
@@ -74,8 +81,12 @@ public class FundingLevelResetHandlerTest {
 
         target.resetFundingAndMarkAsIncomplete(applicationFinanceResource, competitionId, userId);
 
-        Mockito.inOrder(sectionService).verify(sectionService, calls(1)).markAsInComplete(sectionResources.get(0).getId(),
+        InOrder inOrder = Mockito.inOrder(sectionService, financeRowService);
+
+        inOrder.verify(sectionService).markAsInComplete(sectionResources.get(0).getId(),
                 applicationId, processRoles.get(0).getId());
+        inOrder.verify(financeRowService).add(eq(applicationFinanceResource.getId()),
+                eq(questionResource.getId()), isA(FinanceRowItem.class));
     }
 
     @Test
@@ -89,7 +100,8 @@ public class FundingLevelResetHandlerTest {
         List<ProcessRoleResource> processRoles = ProcessRoleResourceBuilder.newProcessRoleResource().withApplication(applicationId).withUser(user).build(2);
         List<SectionResource> sectionResources = SectionResourceBuilder.newSectionResource().build(1);
         QuestionResource questionResource = QuestionResourceBuilder.newQuestionResource().build();
-        List<ApplicationFinanceResource> applicationFinanceResources = ApplicationFinanceResourceBuilder.newApplicationFinanceResource().build(3);
+        List<ApplicationFinanceResource> applicationFinanceResources = ApplicationFinanceResourceBuilder.newApplicationFinanceResource()
+        .withApplication(applicationId).withGrantClaimPercentage(20).build(3);
 
         when(processRoleService.getByApplicationId(1L)).thenReturn(processRoles);
         when(sectionService.getSectionsForCompetitionByType(competitionId, SectionType.FUNDING_FINANCES)).thenReturn(sectionResources);
@@ -98,11 +110,13 @@ public class FundingLevelResetHandlerTest {
 
         target.resetFundingLevelAndMarkAsIncompleteForAllCollaborators(competitionId, applicationId);
 
-        InOrder inOrder = Mockito.inOrder(sectionService, financeService);
+        InOrder inOrder = Mockito.inOrder(sectionService, financeService, financeRowService);
 
-        inOrder.verify(sectionService, calls(1)).markAsInComplete(sectionResources.get(0).getId(),applicationId, processRoles.get(0).getId());
-        inOrder.verify(sectionService, calls(1)).markAsInComplete(sectionResources.get(0).getId(),applicationId, processRoles.get(1).getId());
+        inOrder.verify(sectionService).markAsInComplete(sectionResources.get(0).getId(),applicationId, processRoles.get(0).getId());
+        inOrder.verify(sectionService).markAsInComplete(sectionResources.get(0).getId(),applicationId, processRoles.get(1).getId());
 
         inOrder.verify(financeService, calls(1)).getApplicationFinanceDetails(applicationId);
+        inOrder.verify(financeRowService, calls(3)).add(isA(Long.class),
+                eq(questionResource.getId()), isA(FinanceRowItem.class));
     }
 }
