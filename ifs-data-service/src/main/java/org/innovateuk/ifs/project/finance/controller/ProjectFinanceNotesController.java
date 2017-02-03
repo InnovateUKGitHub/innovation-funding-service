28f106ea681f07141188167ff2ea6db2ca12ecce
Nuno Alexadre <nalexandre@worth.systems>
2017-02-03 12:27:59 +0100

    Abstracting Notes and Queries Controller into CommonThreadController.
    Former-commit-id: 1cb08b374eb2780cb4559370ed6ba5ba12101cc7

--- ifs-data-service/src/main/java/org/innovateuk/ifs/project/finance/controller/ProjectFinanceNotesController.java
+++ ifs-data-service/src/main/java/org/innovateuk/ifs/project/finance/controller/ProjectFinanceNotesController.java
@@ -1,42 +1,18 @@
 package org.innovateuk.ifs.project.finance.controller;
 
-import org.innovateuk.ifs.commons.rest.RestResult;
-import org.innovateuk.ifs.finance.domain.ProjectFinance;
-import org.innovateuk.ifs.threads.domain.Post;
-import org.innovateuk.ifs.threads.domain.Note;
-import org.innovateuk.ifs.threads.service.ThreadService;
+import org.innovateuk.ifs.project.finance.service.ProjectFinanceNotesService;
 import org.innovateuk.threads.resource.NoteResource;
 import org.innovateuk.threads.resource.PostResource;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
-import static org.springframework.web.bind.annotation.RequestMethod.GET;
-import static org.springframework.web.bind.annotation.RequestMethod.POST;
-
 @RestController
 @RequestMapping("/project/finance/notes")
-public class ProjectFinanceNotesController {
+public class ProjectFinanceNotesController extends CommonThreadController<NoteResource, PostResource> {
+
     @Autowired
-    ThreadService<Note, ProjectFinance> service;
-
-    @RequestMapping(value = "/{projectFinanceId}", method = GET)
-    public RestResult<List<NoteResource>> notes(@PathVariable("projectFinanceId") final Long projectFinanceId) {
-        return service.findAll(projectFinanceId).toGetResponse();
+    public ProjectFinanceNotesController(ProjectFinanceNotesService service) {
+        super(service);
     }
-
-    @RequestMapping(value = "", method = POST)
-    public RestResult<Void> createNote(@RequestBody NoteResource note ) {
-        return service.create(note).toPostCreateResponse();
-    }
-
-    @RequestMapping(value = "/{noteId}/post", method = POST)
-    public RestResult<Void> addPost(@PathVariable("noteId") Long noteId, @RequestBody PostResource post ) {
-        return service.addPost(post, noteId).toPostCreateResponse();
-    }
-
 }
--- ifs-data-service/src/main/java/org/innovateuk/ifs/project/finance/controller/ProjectFinanceQueriesController.java
+++ ifs-data-service/src/main/java/org/innovateuk/ifs/project/finance/controller/ProjectFinanceQueriesController.java
@@ -1,45 +1,19 @@
 package org.innovateuk.ifs.project.finance.controller;
 
-import org.innovateuk.ifs.commons.rest.RestResult;
-import org.innovateuk.ifs.finance.domain.ProjectFinance;
 import org.innovateuk.ifs.project.finance.service.ProjectFinanceQueriesService;
-import org.innovateuk.ifs.project.resource.SpendProfileTableResource;
-import org.innovateuk.ifs.threads.domain.Post;
-import org.innovateuk.ifs.threads.domain.Query;
-import org.innovateuk.ifs.threads.service.ThreadService;
 import org.innovateuk.threads.resource.PostResource;
 import org.innovateuk.threads.resource.QueryResource;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
-import static org.springframework.web.bind.annotation.RequestMethod.POST;
-import static org.springframework.web.bind.annotation.RequestMethod.GET;
-
 
 @RestController
-@RequestMapping("/project/finance/{projectFinanceId}/queries")
-public class ProjectFinanceQueriesController {
+@RequestMapping("/project/finance/queries")
+public class ProjectFinanceQueriesController extends CommonThreadController<QueryResource, PostResource> {
 
     @Autowired
-    ProjectFinanceQueriesService service;
-
-    @RequestMapping(value = "", method = GET)
-    public RestResult<List<QueryResource>> queries(@PathVariable("projectFinanceId") final Long projectFinanceId) {
-        return service.findAll(projectFinanceId).toGetResponse();
-    }
-
-    @RequestMapping(value = "", method = POST)
-    public RestResult<Void> createQuery(@RequestBody QueryResource query ) {
-        return service.create(query).toPostCreateResponse();
-    }
-
-    @RequestMapping(value = "/{queryId}/post", method = POST)
-    public RestResult<Void> addPost(@PathVariable("queryId") Long queryId, @RequestBody PostResource post ) {
-        return service.addPost(post, queryId).toPostCreateResponse();
+    public ProjectFinanceQueriesController(ProjectFinanceQueriesService service) {
+        super(service);
     }
 }
