e3427f458a120bbf7639efd02dd32e1e2d7b881f
David Soff <dsoff@worth.systems>
2017-02-03 14:14:52 +0100

    change get test results to always use the correct project
    Former-commit-id: 080ea2e3cad99e64f0e0ea8408c20b9ab44f5210

--- setup-files/scripts/openshift/os-test-results.sh
+++ setup-files/scripts/openshift/os-test-results.sh
@@ -3,9 +3,10 @@ set -e
 
 PROJECT=$1
 
-echo "Getting test results from the $PROJECT Openshift environment"
+echo "Getting test results from the ${PROJECT} Openshift environment"
 
 function getResults() {
+    oc project ${PROJECT} >/dev/null
     oc rsync $(oc get pods | grep robot-framework | awk '{ print $1 }'):/robot-tests/target/ robot-tests/target/
 }
 
