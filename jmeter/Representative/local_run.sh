f36e0aee9f7663b3b6eec429085dd7faaec35dbb
Duncan Watson <duncan@hiveit.co.uk>
2017-02-03 09:55:18 +0000

    INFUND-7700 - updated JMeter scripts
    Former-commit-id: 7dedb475bbc24022b23271ad083fd18fb7e78efb

--- jmeter/Representative/local_run.sh
+++ jmeter/Representative/local_run.sh
@@ -1,1 +1,1 @@
-jmeter -Jinvite_user=1 -Japplicant_users=1 -Jregistration_users=1 -plocal.properties -tload_test_representative.jmx
+jmeter -Jinvite_user=1 -Japplicant_users=10 -Jregistration_users=1 -plocal.properties -tload_test_representative.jmx
