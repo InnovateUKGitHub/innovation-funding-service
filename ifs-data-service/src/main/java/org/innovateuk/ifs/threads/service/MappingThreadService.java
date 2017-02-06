3027fd7fccad9c5ad46c5967aa4e358f09a56f11
Nuno Alexadre <nalexandre@worth.systems>
2017-02-06 09:47:31 +0100

    Removing the ‘final’ keyword from the MappingThreadService in order to allow sub classes to override them.
    Former-commit-id: 28c38b9b29e00af72dc745adf70d082fe7c97a12

--- ifs-data-service/src/main/java/org/innovateuk/ifs/threads/service/MappingThreadService.java
+++ ifs-data-service/src/main/java/org/innovateuk/ifs/threads/service/MappingThreadService.java
@@ -24,20 +24,20 @@ public class MappingThreadService<D extends Thread, R, M extends BaseMapper<D, R
         this.postMapper = postMapper;
     }
 
-    public final ServiceResult<List<R>> findAll(Long projectFinanceId) {
+    public ServiceResult<List<R>> findAll(Long projectFinanceId) {
         return service.findAll(projectFinanceId)
                 .andOnSuccessReturn(queries -> simpleMap(queries, queryMapper::mapToResource));
     }
 
-    public final ServiceResult<R> findOne(Long contextClassPk) {
+    public ServiceResult<R> findOne(Long contextClassPk) {
         return service.findOne(contextClassPk).andOnSuccessReturn(queryMapper::mapToResource);
     }
 
-    public final ServiceResult<Void> create(R query) {
+    public ServiceResult<Void> create(R query) {
         return service.create(queryMapper.mapToDomain(query));
     }
 
-    public final ServiceResult<Void> addPost(PostResource post, Long threadId) {
+    public ServiceResult<Void> addPost(PostResource post, Long threadId) {
         return service.addPost(postMapper.mapToDomain(post), threadId);
     }
 }
