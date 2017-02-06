23de09ac81854555d49ead5c4b5bdf86eb2f241c
Ann Stainforth <ann.stainforth.ipl@gmail.com>
2017-02-06 07:40:56 +0000

    INFUND-6916: Unit testing for Summary page (public content)
    Former-commit-id: 2981acc1c6ba88f08f8328e8f16ab59dcf78b3ab

--- ifs-web-service/ifs-competition-mgt-service/src/main/java/org/innovateuk/ifs/publiccontent/formpopulator/SummaryFormPopulator.java
+++ ifs-web-service/ifs-competition-mgt-service/src/main/java/org/innovateuk/ifs/publiccontent/formpopulator/SummaryFormPopulator.java
@@ -1,11 +1,11 @@
 package org.innovateuk.ifs.publiccontent.formpopulator;
 
 
-import org.innovateuk.ifs.competition.publiccontent.resource.FundingType;
 import org.innovateuk.ifs.competition.publiccontent.resource.PublicContentResource;
 import org.innovateuk.ifs.publiccontent.form.SummaryForm;
 import org.springframework.stereotype.Service;
 
+import static java.util.Optional.ofNullable;
 
 @Service
 public class SummaryFormPopulator extends AbstractPublicContentFormPopulator<SummaryForm> implements PublicContentFormPopulator<SummaryForm> {
@@ -19,13 +19,9 @@ public class SummaryFormPopulator extends AbstractPublicContentFormPopulator<Sum
     protected void populateSection(SummaryForm form, PublicContentResource publicContentResource) {
         form.setDescription(publicContentResource.getSummary());
 
-        //TODO: I think this can be improved...
-        FundingType type = publicContentResource.getFundingType();
-        if (type != null) {
-            form.setFundingType(publicContentResource.getFundingType().getDisplayName());
-        } else {
-            form.setFundingType("");
-        }
+        ofNullable(publicContentResource.getFundingType())
+                .ifPresent(type ->
+                        form.setFundingType(type.getDisplayName()));
 
         form.setProjectSize(publicContentResource.getProjectSize());
     }
