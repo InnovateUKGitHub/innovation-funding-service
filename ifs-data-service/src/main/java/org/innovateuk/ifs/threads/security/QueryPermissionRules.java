6661150c53e0c221b22abd1843c95a8a4842abd4
Nuno Alexadre <nalexandre@worth.systems>
2017-02-06 17:31:36 +0100

    Now checking if the external user adding a post to a query is the Finance Contact of the organisation of the project finance of the target Query.
    Former-commit-id: d13da78ce0897d2ced5794ce7707f2bb7e67104c

--- ifs-data-service/src/main/java/org/innovateuk/ifs/threads/security/QueryPermissionRules.java
+++ ifs-data-service/src/main/java/org/innovateuk/ifs/threads/security/QueryPermissionRules.java
@@ -3,17 +3,25 @@ package org.innovateuk.ifs.threads.security;
 import org.innovateuk.ifs.alert.resource.AlertResource;
 import org.innovateuk.ifs.commons.security.PermissionRule;
 import org.innovateuk.ifs.commons.security.PermissionRules;
+import org.innovateuk.ifs.finance.domain.ProjectFinance;
+import org.innovateuk.ifs.finance.repository.ProjectFinanceRepository;
 import org.innovateuk.ifs.security.BasePermissionRules;
+import org.innovateuk.ifs.user.repository.ProcessRoleRepository;
 import org.innovateuk.ifs.user.resource.UserResource;
 import org.innovateuk.threads.resource.QueryResource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import static org.innovateuk.ifs.security.SecurityRuleUtil.isFinancialContact;
+import java.util.Optional;
+
+import static java.util.Optional.of;
 import static org.innovateuk.ifs.security.SecurityRuleUtil.isProjectFinanceUser;
 
 @Component
 @PermissionRules
 public class QueryPermissionRules extends BasePermissionRules {
+    @Autowired
+    protected ProjectFinanceRepository projectFinanceRepository;
 
     @PermissionRule(value = "CREATE", description = "Only Internal Users can create Queries")
     public boolean onlyInternalUsersCanCreateQueries(final QueryResource query, final UserResource user) {
@@ -21,18 +29,27 @@ public class QueryPermissionRules extends BasePermissionRules {
     }
 
     @PermissionRule(value = "VIEW", description = "Only Internal of Project Finance Users can view Queries")
-    public boolean onlyInternalUsersCanViewQueries(final QueryResource query, final UserResource user) {
-        return isProjectFinanceUser(user) || isFinancialContact(user) && query.g;
+    public boolean onlyInternalUsersOfFinanceContactCanViewTheirQueries(final QueryResource query, final UserResource user) {
+        return isProjectFinanceUser(user) || isFinanceContact(user, query.contextClassPk);
     }
 
     @PermissionRule(value = "ADD_POST", description = "Internal users or Project Finance users can add posts to a query,"
             + " but first post has to come from the Internal user.")
     public boolean onlyInternalOrProjectFinanceUsersCanAddPosts(final QueryResource query, final UserResource user) {
-        return query.posts.isEmpty() ? isProjectFinanceUser(user) : isProjectFinanceUser(user) || isFinancialContact(user);
+        return query.posts.isEmpty() ? isProjectFinanceUser(user) : isProjectFinanceUser(user) || isFinanceContact(user, query.contextClassPk);
     }
 
     @PermissionRule(value = "DELETE", description = "Only Internal Users can delete a Query")
     public boolean onlyInternalUsersCanDeleteQueries(final QueryResource query, final UserResource user) {
         return isProjectFinanceUser(user);
+    }
+
+    private boolean isFinanceContact(UserResource user, Long projectFinance) {
+        return findProjectFinance(projectFinance)
+                .map(pf-> pf.getOrganisation().isFinanceContact(user.getId())).orElse(false);
+    }
+
+    private Optional<ProjectFinance> findProjectFinance(Long id) {
+        return of(projectFinanceRepository.findOne(id));
     }
 }
