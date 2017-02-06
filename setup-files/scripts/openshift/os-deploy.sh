aef189f4a0b703dfb10c155e4f69a48c82144653
Laszlo Fogas <laszlo@falconsocial.com>
2017-02-06 19:41:01 +0100

    Adding docker login to remote script
    Former-commit-id: 0cbd7024c6d2dc857275556bf26528f0161c5738

--- setup-files/scripts/openshift/os-deploy.sh
+++ setup-files/scripts/openshift/os-deploy.sh
@@ -53,6 +53,8 @@ function useContainerRegistry() {
     docker tag innovateuk/shib-init:1.0-SNAPSHOT \
         ${REGISTRY}/innovateuk/shib-init:1.0-$PROJECT
 
+    docker login -u AWS -p AQECAHhNDzWVnYGylK3tUalHANpmrnQza5w4W4cHYMU82tbNIwAAAyEwggMdBgkqhkiG9w0BBwagggMOMIIDCgIBADCCAwMGCSqGSIb3DQEHATAeBglghkgBZQMEAS4wEQQMT/AtphZRMmPLmrRbAgEQgIIC1CL6K5dZmQDHJ7vXaT4FCaJIftrqmL/DfIfZPTHmaAgqqvm7TP4LsM5Hm2dpPq4UnTTLco/UjYlg9DkgU4vor1NfSeyXGGqzYUu+L3u7QdLJVAIAhKoxlLoogxiR7qlSS9v5SayKOERn331jA5fIVWt/F60jkMn3DjrNUMq2zbcHXPlnPyCFfIGuAKqzCcIaZMiq7hl1EcVqiLAq3NcqjPNKg7P3+fnA49xnB5+KU/xnf8rhNepyuixa6TnCL003a0FkgPEuRa0qtfjC/ViCCOiWyWOa0sw3DhfD4M3N8ExcSn4rPByGhjZmf3BG5ZtujvdIwRx3T8X56M4MdAntlUD1+USERbz5emjDKuLnuXirfktJzhXp07Ix2jO016+fJF4ToIof/mE12jFmV5rTcuT6Pix7mSK0mC7eai+aPE4lAM1we1uSmEAbzqVpZuPmJiB4YfAeJQ8JYe2qWFpn+YiqnlhP16EiKzoVMeLRBL9IbUYteeiOh8azxCBCD1t22PQD3Ks53Bhq9UWCMZ7YpNgJWHiGP+c56t5atMzgz1lX6pe9OUpTBkFc7261V9/SV+5M1xFNKp5MtnxXhfWpewzNDABlmeQUBQT2xToGY0g2F+eYuuFE2KkDQWtz1n9qtFGpf3AzhK+EW36fEQIhh+aDY1h8FE2vzhacJg8Nr2usIe8p5/sUZznaa72Bx1dOeYiXtVuCTJ5xfROdTIVCXDWrbgygGCUv+HM0jUSoQZWXaZ6txA0+lXxFFbjUVQx39bw88IOihxiTdmv1iW/DTlMBe8wW7xF86CEDVghMsZOMstvxQMyKMup6Yc1QDxGI68ZOu2WcZlEZD+Spa19OTgwLAsuroOae3xQuE456GvWq2whcoyHqotxwaLqT5eTbFH9dOMgaX9o+0iAqBSmFykoT2pG3GYtZ/NhMRZKUsXFFUrjuZn51x10wsJTvl8JDKvuvUxU= -e none https://721685138178.dkr.ecr.eu-west-2.amazonaws.com
+
     docker push ${REGISTRY}/innovateuk/data-service:1.0-$PROJECT
     docker push ${REGISTRY}/innovateuk/project-setup-service:1.0-$PROJECT
     docker push ${REGISTRY}/innovateuk/project-setup-management-service:1.0-$PROJECT
