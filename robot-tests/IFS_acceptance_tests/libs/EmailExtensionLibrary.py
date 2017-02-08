6c4d9b95aca3a905262150d3aabf766ab83ea22e
Duncan Watson <duncan@hiveit.co.uk>
2017-02-08 17:30:23 +0000

    INFUND-8109 - removed temporary logging
    Former-commit-id: b2262dbc03ddbeb553936c6e0ed26f884b9786a9

--- robot-tests/IFS_acceptance_tests/libs/EmailExtensionLibrary.py
+++ robot-tests/IFS_acceptance_tests/libs/EmailExtensionLibrary.py
@@ -1,18 +1,16 @@
-from logging import warn
+from logging import info
 from robot.libraries.BuiltIn import BuiltIn
 imap = BuiltIn().get_library_instance('ImapLibrary')
 
 def remove_all_emails(**kwargs):
-  warn("clearing mailbox %s" % kwargs['user'])
+
   imap.open_mailbox(**kwargs)
 
   try:
-    index = imap.wait_for_email(**kwargs)
-    warn("index is %s" % index)
+    imap.wait_for_email(**kwargs)
     imap.delete_all_emails()
-    warn("mail deleted")
 
   except:
-    warn("Couldn't find any mail to delete")
+    info("Couldn't find any mail to delete")
 
   imap.close_mailbox()
