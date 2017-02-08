b47348a6e3bf9c7fbd5bd8f8ecb78b150ef05190
Nuno Alexadre <nalexandre@worth.systems>
2017-02-08 11:51:49 +0100

    Correcting PostAttachmentRestServiceImpl.download method: was not passing the fileId into the url of the request.
    Former-commit-id: 9bb0772b8e6ec9506da6642f5ba4192af0f23d3d

--- ifs-rest-api-service/src/main/java/org/innovateuk/ifs/upload/service/PostAttachmentRestServiceImpl.java
+++ ifs-rest-api-service/src/main/java/org/innovateuk/ifs/upload/service/PostAttachmentRestServiceImpl.java
@@ -30,6 +30,6 @@ public class PostAttachmentRestServiceImpl extends BaseRestService implements Po
 
     @Override
     public RestResult<Optional<ByteArrayResource>> download(Long fileId) {
-        return getWithRestResult(baseURL+"/download/fileId", ByteArrayResource.class).toOptionalIfNotFound();
+        return getWithRestResult(baseURL+"/download/" + fileId, ByteArrayResource.class).toOptionalIfNotFound();
     }
 }
