097a37913a4a16be1cf50b417fcabeac14409584
Nuno Alexadre <nalexandre@worth.systems>
2017-02-10 00:32:55 +0100

    Moving DownloadService only method to AttachmentController
    Former-commit-id: 0f15520092f6c399a5f637bf203496d1ec1f31c0

--- ifs-data-service/src/main/java/org/innovateuk/ifs/threads/attachments/controller/ProjectFinancePostAttachmentsController.java
+++ ifs-data-service/src/main/java/org/innovateuk/ifs/threads/attachments/controller/ProjectFinancePostAttachmentsController.java
@@ -6,13 +6,9 @@ import org.innovateuk.ifs.file.resource.FileEntryResource;
 import org.innovateuk.ifs.file.transactional.FileEntryService;
 import org.innovateuk.ifs.file.transactional.FileHttpHeadersValidator;
 import org.innovateuk.ifs.file.transactional.FileService;
-import org.innovateuk.ifs.threads.attachments.DownloadService;
-import org.innovateuk.ifs.threads.attachments.controller.AttachmentController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PostFilter;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
