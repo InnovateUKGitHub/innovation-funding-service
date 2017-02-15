e377bd3135b324da6df257ded6a9fd4b9cf419c4
Nuno Alexadre <nalexandre@worth.systems>
2017-02-15 17:21:57 +0100

    Adding ProjectFinanceQueriesControllerDocumentation - there is an inconsistency with the create test.
    Former-commit-id: 2a63430454b420b4786d8247b7b12228af98d4b2

---
+++ ifs-data-service/src/test/java/org/innovateuk/ifs/documentation/QueryFieldsDocs.java
@@ -0,0 +1,16 @@
+package org.innovateuk.ifs.documentation;
+
+import org.springframework.restdocs.payload.FieldDescriptor;
+
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+
+public class QueryFieldsDocs {
+    public static final FieldDescriptor[] queryResourceFields = {
+            fieldWithPath("id").description("Query unique id if already created"),
+            fieldWithPath("contextClassPk").description("The primary key / id of the class that holds the context of this Query"),
+            fieldWithPath("posts").description("The list of posts of this Query"),
+            fieldWithPath("section").description("The Finance Check section to which this Query belongs to."),
+            fieldWithPath("title").description("The title of this Query."),
+            fieldWithPath("awaitingResponse").description("Informs if this Query is awaiting response."),
+            fieldWithPath("createdOn").description("The datetime this Query was created,")};
+}
---
+++ ifs-data-service/src/test/java/org/innovateuk/ifs/thread/documentation/ProjectFinanceQueriesControllerDocumentation.java
@@ -0,0 +1,96 @@
+package org.innovateuk.ifs.thread.documentation;
+
+import org.innovateuk.ifs.BaseControllerMockMVCTest;
+import org.innovateuk.ifs.project.finance.controller.ProjectFinanceQueriesController;
+import org.innovateuk.threads.resource.PostResource;
+import org.innovateuk.threads.resource.QueryResource;
+import org.junit.Test;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static java.util.Arrays.asList;
+import static org.innovateuk.ifs.commons.service.ServiceResult.serviceSuccess;
+import static org.innovateuk.ifs.documentation.QueryFieldsDocs.queryResourceFields;
+import static org.innovateuk.ifs.util.JsonMappingUtil.toJson;
+import static org.mockito.Mockito.when;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+public class ProjectFinanceQueriesControllerDocumentation extends BaseControllerMockMVCTest<ProjectFinanceQueriesController> {
+
+    @Test
+    public void testCreate() throws Exception {
+        final QueryResource query = new QueryResource(35L, 22L, null, null, null, false, null);
+        when(projectFinanceQueriesService.create(query)).thenReturn(serviceSuccess(query.id));
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/project/finance/queries")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(query)))
+                .andExpect(content().string(objectMapper.writeValueAsString(35L)))
+                .andExpect(status().isCreated())
+                .andDo(document("queries/{method-name}", requestFields(queryResourceFields)));
+    }
+
+    @Test
+    public void testFindOne() throws Exception {
+        final Long queryId = 22L;
+        QueryResource query = new QueryResource(queryId, null, null, null, null, false, null);
+        when(projectFinanceQueriesService.findOne(queryId)).thenReturn(serviceSuccess(query));
+
+        mockMvc.perform(get("/project/finance/queries/{queryId}", queryId))
+                .andExpect(status().isOk())
+                .andDo(document("queries/{method-name}",
+                        pathParameters(parameterWithName("queryId").description("Id of the Query to be fetched")),
+                        responseFields(queryResourceFields))
+                );
+    }
+
+    @Test
+    public void testFindAll() throws Exception {
+        final Long contextId = 22L;
+        QueryResource query = new QueryResource(3L, null, null, null, null, false, null);
+        when(projectFinanceQueriesService.findAll(contextId)).thenReturn(serviceSuccess(asList(query)));
+
+        mockMvc.perform(get("/project/finance/queries/all/{projectFinanceId}", contextId))
+                .andExpect(status().isOk())
+                .andExpect(content().json(toJson(asList(query))))
+                .andDo(document("queries/{method-name}",
+                        responseFields(fieldWithPath("[]").description("List of Queries the authenticated user has access to")),
+                        pathParameters(parameterWithName("projectFinanceId").description("The id of the project finance under which the expected queries live."))));
+    }
+
+
+    @Test
+    public void testAddPost() throws Exception {
+        Long queryId = 22L;
+        PostResource post = new PostResource(33L, null, null, null, null);
+        when(projectFinanceQueriesService.addPost(post, queryId)).thenReturn(serviceSuccess());
+
+        mockMvc.perform(post("/project/finance/queries/{queryId}/post", queryId)
+                .contentType(APPLICATION_JSON)
+                .content(toJson(post)))
+                .andExpect(status().isCreated())
+                .andDo(document("queries/{method-name}",
+                        pathParameters(parameterWithName("queryId").description("Id of the Query to which the Post is to be added to."))));
+    }
+
+    @Override
+    public void setupMockMvc() {
+        controller = new ProjectFinanceQueriesController(projectFinanceQueriesService);
+        super.setupMockMvc();
+    }
+
+    @Override
+    protected ProjectFinanceQueriesController supplyControllerUnderTest() {
+        return null;
+    }
+
+
+}
