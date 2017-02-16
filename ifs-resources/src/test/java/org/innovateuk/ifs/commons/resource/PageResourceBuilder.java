7abab764eb106845fffc89dbcfef4e2faa31279e
Nicholas Tsim <nick@hiveit.co.uk>
2017-02-16 15:55:57 +0000

    INFUND-8308 - Adds common base PageResourceBuilder
    Former-commit-id: 57b84d1ad4c78807889774374bda4e0c4a3d4869

---
+++ ifs-resources/src/test/java/org/innovateuk/ifs/commons/resource/PageResourceBuilder.java
@@ -0,0 +1,33 @@
+package org.innovateuk.ifs.commons.resource;
+
+import org.innovateuk.ifs.BaseBuilder;
+
+import java.util.List;
+import java.util.function.BiConsumer;
+
+public abstract class PageResourceBuilder<T extends PageResource, S extends PageResourceBuilder> extends BaseBuilder<T, S> {
+
+    protected PageResourceBuilder(List<BiConsumer<Integer, T>> newMultiActions) {
+        super(newMultiActions);
+    }
+
+    public S withTotalElements(Long... totalElements) {
+        return withArraySetFieldByReflection("totalElements", totalElements);
+    }
+
+    public S withTotalPages(Integer... totalPages) {
+        return withArraySetFieldByReflection("totalPages", totalPages);
+    }
+
+    public S withContent(List<T>... content) {
+        return withArraySetFieldByReflection("content", content);
+    }
+
+    public S withNumber(Integer... number) {
+        return withArraySetFieldByReflection("number", number);
+    }
+
+    public S withSize(Integer... size) {
+        return withArraySetFieldByReflection("size", size);
+    }
+}
