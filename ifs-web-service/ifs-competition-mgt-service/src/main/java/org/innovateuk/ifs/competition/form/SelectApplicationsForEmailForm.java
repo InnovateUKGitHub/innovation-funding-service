cebb4ebb0226268415fabc8c6d85a968277ebf7a
richard <richard@hiveit.co.uk>
2017-02-21 12:07:28 +0000

    INFUND-7377 chnages to accept the posting of ids
    Former-commit-id: 0d13a2d146460ed51d8c01916c57db1e4533f055

---
+++ ifs-web-service/ifs-competition-mgt-service/src/main/java/org/innovateuk/ifs/competition/form/SelectApplicationsForEmailForm.java
@@ -0,0 +1,16 @@
+package org.innovateuk.ifs.competition.form;
+
+import java.util.List;
+
+public class SelectApplicationsForEmailForm {
+
+    private List<String> ids;
+
+    public List<String> getIds() {
+        return ids;
+    }
+
+    public void setIds(List<String> ids) {
+        this.ids = ids;
+    }
+}
--- ifs-web-service/ifs-competition-mgt-service/src/main/java/org/innovateuk/ifs/management/controller/CompetitionManagementManageFundingApplicationsController.java
+++ ifs-web-service/ifs-competition-mgt-service/src/main/java/org/innovateuk/ifs/management/controller/CompetitionManagementManageFundingApplicationsController.java
@@ -1,21 +1,19 @@
 package org.innovateuk.ifs.management.controller;
 
 import org.innovateuk.ifs.competition.form.ManageFundingApplicationsQueryForm;
+import org.innovateuk.ifs.competition.form.SelectApplicationsForEmailForm;
+import org.innovateuk.ifs.controller.ValidationHandler;
 import org.innovateuk.ifs.management.model.ManageFundingApplicationsModelPopulator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.function.Supplier;
 
-import static org.innovateuk.ifs.controller.ValidationHandler.newBindingResultHandler;
 
 
 @Controller
@@ -30,17 +28,46 @@ public class CompetitionManagementManageFundingApplicationsController {
     @GetMapping
     public String applications(Model model,
                                @PathVariable("competitionId") Long competitionId,
-                               @ModelAttribute @Valid ManageFundingApplicationsQueryForm queryForm,
-                               BindingResult bindingResult) {
-
-        Supplier<String> failureView = () -> "redirect:/competition/" + competitionId + "/funding";
-
-        return newBindingResultHandler(bindingResult).failNowOrSucceedWith(failureView, () -> {
-                    model.addAttribute("model", manageFundingApplicationsModelPopulator.populate(queryForm, competitionId));
+                               @ModelAttribute @Valid ManageFundingApplicationsQueryForm query,
+                               BindingResult bindingResult,
+                               ValidationHandler validationHandler) {
+        return validationHandler.failNowOrSucceedWith(queryFailureView(competitionId), () -> {
+                    model.addAttribute("model", manageFundingApplicationsModelPopulator.populate(query, competitionId));
+                    model.addAttribute("form", new SelectApplicationsForEmailForm());
                     return "comp-mgt-manage-funding-applications";
                 }
         );
 
     }
 
+    @PostMapping
+    public String selectApplications(Model model,
+                                     @PathVariable("competitionId") Long competitionId,
+                                     @ModelAttribute @Valid ManageFundingApplicationsQueryForm query,
+                                     BindingResult queryFormBindingResult,
+                                     ValidationHandler queryFormValidationHandler,
+                                     @ModelAttribute @Valid SelectApplicationsForEmailForm ids,
+                                     BindingResult idsBindingResult,
+                                     ValidationHandler idsValidationHandler){
+        return queryFormValidationHandler.failNowOrSucceedWith(queryFailureView(competitionId), () ->
+            idsValidationHandler.failNowOrSucceedWith(idsFailureView(competitionId, query, model), () ->
+                composeEmailRedirect(competitionId, ids)
+            )
+        );
+    }
+
+    private String composeEmailRedirect(long competitionId, SelectApplicationsForEmailForm ids){
+        return "redirect:/competition/" + competitionId + "/funding/send?application_ids=";
+    }
+
+    private Supplier<String> queryFailureView(long competitionId) {
+        return  () -> "redirect:/competition/" + competitionId + "/funding";
+    }
+
+    private Supplier<String> idsFailureView(long competitionId, ManageFundingApplicationsQueryForm query, Model model) {
+        return () -> {
+            model.addAttribute("model", manageFundingApplicationsModelPopulator.populate(query, competitionId));
+            return "comp-mgt-manage-funding-applications";
+        };
+    }
 }
