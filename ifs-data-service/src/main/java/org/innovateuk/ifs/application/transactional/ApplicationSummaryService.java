4d8b30985ae270ce121ab18577693fb770e521d3
Peter Moreton <pctmoreton@gmail.com>
2017-02-22 16:06:55 +0000

    INFUN-8010 Review comments
    Former-commit-id: d698b3ad8686b31e93a8abed5896dd268faffbd7

--- ifs-data-service/src/main/java/org/innovateuk/ifs/application/transactional/ApplicationSummaryService.java
+++ ifs-data-service/src/main/java/org/innovateuk/ifs/application/transactional/ApplicationSummaryService.java
@@ -9,17 +9,31 @@ public interface ApplicationSummaryService {
 
 	@PreAuthorize("hasAnyAuthority('comp_admin' , 'project_finance')")
 	@SecuredBySpring(value = "READ", description = "Comp Admins can see all Application Summaries across the whole system", securedType = ApplicationSummaryPageResource.class)
-	ServiceResult<ApplicationSummaryPageResource> getApplicationSummariesByCompetitionId(Long competitionId, String sortBy, int pageIndex, int pageSize, String filter);
+	ServiceResult<ApplicationSummaryPageResource> getApplicationSummariesByCompetitionId(Long competitionId,
+																						 String sortBy,
+																						 int pageIndex,
+																						 int pageSize,
+																						 String filter);
 
 	@PreAuthorize("hasAnyAuthority('comp_admin' , 'project_finance')")
 	@SecuredBySpring(value = "READ", description = "Comp Admins can see all submitted Application Summaries across the whole system", securedType = ApplicationSummaryPageResource.class)
-	ServiceResult<ApplicationSummaryPageResource> getSubmittedApplicationSummariesByCompetitionId(Long competitionId, String sortBy, int pageIndex, int pageSize, String filter);
+	ServiceResult<ApplicationSummaryPageResource> getSubmittedApplicationSummariesByCompetitionId(Long competitionId,
+																								  String sortBy,
+																								  int pageIndex,
+																								  int pageSize,
+																								  String filter);
 
 	@PreAuthorize("hasAnyAuthority('comp_admin' , 'project_finance')")
     @SecuredBySpring(value = "READ", description = "Comp Admins can see all not-yet submitted Application Summaries across the whole system", securedType = ApplicationSummaryPageResource.class)
-	ServiceResult<ApplicationSummaryPageResource> getNotSubmittedApplicationSummariesByCompetitionId(Long competitionId, String sortBy, int pageIndex, int pageSize);
+	ServiceResult<ApplicationSummaryPageResource> getNotSubmittedApplicationSummariesByCompetitionId(Long competitionId,
+																									 String sortBy,
+																									 int pageIndex,
+																									 int pageSize);
 
 	@PreAuthorize("hasAnyAuthority('comp_admin' , 'project_finance')")
     @SecuredBySpring(value = "READ", description = "Comp Admins can see all Application Summaries requiring feedback across the whole system", securedType = ApplicationSummaryPageResource.class)
-	ServiceResult<ApplicationSummaryPageResource> getFeedbackRequiredApplicationSummariesByCompetitionId(Long competitionId, String sortBy, int pageIndex, int pageSize);
+	ServiceResult<ApplicationSummaryPageResource> getFeedbackRequiredApplicationSummariesByCompetitionId(Long competitionId,
+																										 String sortBy,
+																										 int pageIndex,
+																										 int pageSize);
 }
