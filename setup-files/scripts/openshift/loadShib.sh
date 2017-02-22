822431b8fb7d5971ded3c8a2e929e1d32c0a267e
Laszlo Fogas <laszlo@falconsocial.com>
2017-02-22 16:08:35 +0100

    Shib sftp to registry, Shib remote to local registry
    Former-commit-id: 4edf57f60847a662f40f8d0e59444ac416876214

--- setup-files/scripts/openshift/loadShib.sh
+++ setup-files/scripts/openshift/loadShib.sh
@@ -1,53 +1,31 @@
 #!/bin/bash
 
+REGISTRY=docker-registry-default.apps.prod.ifs-test-clusters.com
+
 LDAP_VERSION=0.2.1
 IDP_VERSION=0.3.2
 SP_VERSION=0.3.2
 
-
-BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd $BASEDIR
-if [ -z shibImages ]; then
-    mkdir shibImages
-fi
-cd shibImages
-
+docker login -p $(oc whoami -t) -e unused -u unused ${REGISTRY}
 
 if [ -z $(docker images -q innovateuk/ifs-shib-sp:${SP_VERSION}) ]
 then
-    SP_FILE=innovateuk-ifs-shib-sp-${SP_VERSION}.tar.gz
-    if [ ! -f ${SP_FILE} ]; then
-        echo "Downloading innovateuk/ifs-shib-sp:${SP_VERSION}"
-        sftp iuk-ifs@incoming.g2g3digital.net:/incoming/${SP_FILE}
-    fi
-    docker load < ${SP_FILE}
-else
-    echo "Image innovateuk/ifs-shib-sp:${SP_VERSION} already EXISTS."
+    docker pull docker-registry-default.apps.prod.ifs-test-clusters.com/innovateuk/ifs-shib-sp:${SP_VERSION}
 fi
 
 if [ -z $(docker images -q innovateuk/ifs-shib-idp:${IDP_VERSION}) ]
 then
-    IDP_FILE=innovateuk-ifs-shib-idp-${IDP_VERSION}.tar.gz
-    if [ ! -f ${IDP_FILE} ]; then
-        echo "Downloading innovateuk/ifs-shib-idp:${IDP_VERSION}"
-        sftp iuk-ifs@incoming.g2g3digital.net:/incoming/${IDP_FILE}
-    fi
-    docker load < ${IDP_FILE}
-else
-    echo "Image innovateuk/ifs-shib-idp:${IDP_VERSION} already EXISTS."
+    docker pull docker-registry-default.apps.prod.ifs-test-clusters.com/innovateuk/ifs-shib-idp:${IDP_VERSION}
 fi
 
 if [ -z $(docker images -q innovateuk/ifs-ldap:${LDAP_VERSION}) ]
 then
-    LDAP_FILE=innovateuk-ifs-ldap-${LDAP_VERSION}.tar.gz
-    if [ ! -f ${LDAP_FILE} ]; then
-        echo "Downloading innovateuk/ifs-ldap:${LDAP_VERSION}"
-        sftp iuk-ifs@incoming.g2g3digital.net:/incoming/${LDAP_FILE}
-    fi
-    docker load < ${LDAP_FILE}
-else
-    echo "Image innovateuk/ifs-ldap:${LDAP_VERSION} already EXISTS."
+    docker pull docker-registry-default.apps.prod.ifs-test-clusters.com/innovateuk/ifs-ldap:${LDAP_VERSION}
 fi
+
+docker tag ${REGISTRY}/innovateuk/ifs-shib-idp:${IDP_VERSION} innovateuk/ifs-shib-idp:${IDP_VERSION}
+docker tag ${REGISTRY}/innovateuk/ifs-ldap:${LDAP_VERSION} innovateuk/ifs-ldap:${LDAP_VERSION}
+docker tag ${REGISTRY}/innovateuk/ifs-shib-idp:${SP_VERSION} innovateuk/ifs-shib-sp:${SP_VERSION}
 
 
 echo "SUCCESS"
--- setup-files/scripts/openshift/shibImagesToRegistry.sh
+++ setup-files/scripts/openshift/shibImagesToRegistry.sh
@@ -1,14 +1,62 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-# first sftp images from https://devops.innovateuk.org/documentation/pages/viewpage.action?pageId=8455893
-# and docker load them
+LDAP_VERSION=0.2.1
+IDP_VERSION=0.3.2
+SP_VERSION=0.3.2
 
 REGISTRY=docker-registry-default.apps.prod.ifs-test-clusters.com
 
-docker tag innovateuk/ifs-shib-idp:0.2.0 ${REGISTRY}/innovateuk/ifs-shib-idp:0.2.0
-docker tag innovateuk/ifs-ldap:0.2.1 ${REGISTRY}/innovateuk/ifs-ldap:0.2.1
-docker tag innovateuk/ifs-shib-idp:0.2.0 ${REGISTRY}/innovateuk/ifs-shib-sp:0.2.0
+BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $BASEDIR
+if [ -z shibImages ]; then
+    mkdir shibImages
+fi
+cd shibImages
 
-docker push ${REGISTRY}/innovateuk/ifs-shib-idp:0.2.0
-docker push ${REGISTRY}/innovateuk/ifs-ldap:0.2.1
-docker push ${REGISTRY}/innovateuk/ifs-shib-sp:0.2.0
+
+if [ -z $(docker images -q innovateuk/ifs-shib-sp:${SP_VERSION}) ]
+then
+    SP_FILE=innovateuk-ifs-shib-sp-${SP_VERSION}.tar.gz
+    if [ ! -f ${SP_FILE} ]; then
+        echo "Downloading innovateuk/ifs-shib-sp:${SP_VERSION}"
+        sftp iuk-ifs@incoming.g2g3digital.net:/incoming/${SP_FILE}
+    fi
+    docker load < ${SP_FILE}
+else
+    echo "Image innovateuk/ifs-shib-sp:${SP_VERSION} already EXISTS."
+fi
+
+if [ -z $(docker images -q innovateuk/ifs-shib-idp:${IDP_VERSION}) ]
+then
+    IDP_FILE=innovateuk-ifs-shib-idp-${IDP_VERSION}.tar.gz
+    if [ ! -f ${IDP_FILE} ]; then
+        echo "Downloading innovateuk/ifs-shib-idp:${IDP_VERSION}"
+        sftp iuk-ifs@incoming.g2g3digital.net:/incoming/${IDP_FILE}
+    fi
+    docker load < ${IDP_FILE}
+else
+    echo "Image innovateuk/ifs-shib-idp:${IDP_VERSION} already EXISTS."
+fi
+
+if [ -z $(docker images -q innovateuk/ifs-ldap:${LDAP_VERSION}) ]
+then
+    LDAP_FILE=innovateuk-ifs-ldap-${LDAP_VERSION}.tar.gz
+    if [ ! -f ${LDAP_FILE} ]; then
+        echo "Downloading innovateuk/ifs-ldap:${LDAP_VERSION}"
+        sftp iuk-ifs@incoming.g2g3digital.net:/incoming/${LDAP_FILE}
+    fi
+    docker load < ${LDAP_FILE}
+else
+    echo "Image innovateuk/ifs-ldap:${LDAP_VERSION} already EXISTS."
+fi
+
+docker tag innovateuk/ifs-shib-idp:${IDP_VERSION} ${REGISTRY}/innovateuk/ifs-shib-idp:${IDP_VERSION}
+docker tag innovateuk/ifs-ldap:${LDAP_VERSION} ${REGISTRY}/innovateuk/ifs-ldap:${LDAP_VERSION}
+docker tag innovateuk/ifs-shib-idp:${SP_VERSION} ${REGISTRY}/innovateuk/ifs-shib-sp:${SP_VERSION}
+
+docker push ${REGISTRY}/innovateuk/ifs-shib-idp:${IDP_VERSION}
+docker push ${REGISTRY}/innovateuk/ifs-ldap:${LDAP_VERSION}
+docker push ${REGISTRY}/innovateuk/ifs-shib-sp:${SP_VERSION}
+
+
+echo "SUCCESS"
