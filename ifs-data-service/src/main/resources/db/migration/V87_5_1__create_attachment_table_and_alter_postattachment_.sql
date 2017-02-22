fc1b707ab44bd2ca017e0ff0edc9337bed9da3ed
Nuno Alexadre <nalexandre@worth.systems>
2017-02-22 21:23:06 +0100

    Changing columns names in the sql script creating the attachment table - this solves a hibernate exception
    Former-commit-id: 0fbd67de95f17d81cb78d59050b98750e0e520f3

--- ifs-data-service/src/main/resources/db/migration/V87_5_1__create_attachment_table_and_alter_postattachment_.sql
+++ ifs-data-service/src/main/resources/db/migration/V87_5_1__create_attachment_table_and_alter_postattachment_.sql
@@ -1,14 +1,14 @@
 
 CREATE TABLE `attachment` (
   `id` BIGINT(20) NOT NULL AUTO_INCREMENT,
-  `uploader` BIGINT(20) NOT NULL,
-  `file_entry` BIGINT(20) NOT NULL,
+  `uploader_id` BIGINT(20) NOT NULL,
+  `file_entry_id` BIGINT(20) NOT NULL,
   `created_on` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
-  UNIQUE INDEX `file_entry_UNIQUE` (`file_entry` ASC),
-  INDEX `attachment_uploader_fk_idx` (`uploader` ASC),
-  CONSTRAINT `attachment_uploader_fk` FOREIGN KEY (`uploader`) REFERENCES `user` (`id`),
-  CONSTRAINT `attachment_fileEntry_fk` FOREIGN KEY (`file_entry`) REFERENCES `file_entry` (`id`));
+  UNIQUE INDEX `file_entry_UNIQUE` (`file_entry_id` ASC),
+  INDEX `attachment_uploader_fk_idx` (`uploader_id` ASC),
+  CONSTRAINT `attachment_uploader_fk` FOREIGN KEY (`uploader_id`) REFERENCES `user` (`id`),
+  CONSTRAINT `attachment_fileEntry_fk` FOREIGN KEY (`file_entry_id`) REFERENCES `file_entry` (`id`));
 
 
 ALTER TABLE `post_attachment` DROP FOREIGN KEY `post_attachment_file_entry_fk`;
