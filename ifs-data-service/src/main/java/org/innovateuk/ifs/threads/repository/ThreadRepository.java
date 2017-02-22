0894c115b5b4902abd56e9f41bb869f20df105da
Nuno Alexadre <nalexandre@worth.systems>
2017-02-22 14:45:10 +0100

    Added method to ThreadRepository to find the Thread that holds an attachment with a given ID.
    Former-commit-id: 1e844fb09b9f47afe8e35bf2f2aba21015c40ba4

--- ifs-data-service/src/main/java/org/innovateuk/ifs/threads/repository/ThreadRepository.java
+++ ifs-data-service/src/main/java/org/innovateuk/ifs/threads/repository/ThreadRepository.java
@@ -1,10 +1,18 @@
 package org.innovateuk.ifs.threads.repository;
 
 import org.innovateuk.ifs.threads.domain.Thread;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ThreadRepository<T extends Thread> extends PagingAndSortingRepository<T, Long> {
+    static final String FIND_ONE_THAT_HOLDS_ATTACHMENT = "SELECT from Thread t, Post p, Attachment a " +
+            "WHERE a.id = :attachmentId AND a.post_id = p.id AND p.thread_id = t.id LIMIT 1";
     List<T> findAllByClassPkAndClassName(Long classPk, String className);
+
+    @Query(FIND_ONE_THAT_HOLDS_ATTACHMENT)
+    Optional<T> findOneThatHoldsAttachment(@Param("attachmentId") Long attachmentId);
 }
