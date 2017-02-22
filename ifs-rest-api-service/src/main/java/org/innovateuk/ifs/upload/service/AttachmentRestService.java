653bca5fd11e4517331fb542dd997920bc9f5e90
Nuno Alexadre <nalexandre@worth.systems>
2017-02-22 14:43:03 +0100

    Renaming PostAttachmentRestService interface to just AttachmentRestService
    Former-commit-id: 81ebfc393f7e88b571734bb1e64055ecbcbdb3c6

--- ifs-rest-api-service/src/main/java/org/innovateuk/ifs/upload/service/AttachmentRestService.java
+++ ifs-rest-api-service/src/main/java/org/innovateuk/ifs/upload/service/AttachmentRestService.java
@@ -2,18 +2,18 @@ package org.innovateuk.ifs.upload.service;
 
 import org.innovateuk.ifs.commons.rest.RestResult;
 import org.innovateuk.ifs.file.resource.FileEntryResource;
+import org.innovateuk.threads.attachment.resource.AttachmentResource;
 import org.springframework.core.io.ByteArrayResource;
 
 import java.util.Optional;
 
-public interface PostAttachmentRestService {
-    RestResult<FileEntryResource> find(Long fileId);
+public interface AttachmentRestService {
+    RestResult<AttachmentResource> find(Long fileId);
 
-    RestResult<FileEntryResource> upload(String contentType, long contentLength,
-                                             String originalFilename, byte[] bytes);
+    RestResult<AttachmentResource> upload(String contentType, long contentLength,
+                                          String originalFilename, byte[] bytes);
 
     RestResult<Void> delete(Long id);
 
     RestResult<Optional<ByteArrayResource>> download(Long fileId);
-
 }
