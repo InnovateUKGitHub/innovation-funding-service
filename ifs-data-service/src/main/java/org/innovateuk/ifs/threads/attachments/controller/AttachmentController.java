91697885b8fdbfb28154a917fc101787a85a8d72
Nuno Alexadre <nalexandre@worth.systems>
2017-02-22 11:07:33 +0100

    adjusting ProjectFinancePostAttachmentController and AttachmentController to the new setup using an AttachmentsService
    Former-commit-id: 90c4a958c457d86975054e09c48397bf52bf4e50

--- ifs-data-service/src/main/java/org/innovateuk/ifs/threads/attachments/controller/AttachmentController.java
+++ ifs-data-service/src/main/java/org/innovateuk/ifs/threads/attachments/controller/AttachmentController.java
@@ -11,6 +11,7 @@ import org.innovateuk.ifs.file.transactional.FileHttpHeadersValidator;
 import org.innovateuk.ifs.file.transactional.FileService;
 import org.innovateuk.ifs.threads.attachments.domain.Attachment;
 import org.innovateuk.ifs.threads.attachments.service.AttachmentsService;
+import org.innovateuk.threads.attachment.resource.AttachmentResource;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -23,21 +24,21 @@ import static org.innovateuk.ifs.file.controller.FileControllerUtils.handleFileD
 import static org.innovateuk.ifs.file.controller.FileControllerUtils.handleFileUpload;
 import static org.springframework.web.bind.annotation.RequestMethod.*;
 
-public abstract class AttachmentController {
+public abstract class AttachmentController<R> {
 
-    AttachmentsService<Attachment> service;
+    AttachmentsService<R> service;
 
-    public AttachmentController(AttachmentsService<Attachment> service) {
+    public AttachmentController(AttachmentsService<R> service) {
         this.service = service;
     }
 
     @RequestMapping(value = "/{attachmentId}", method = GET, produces = "application/json")
-    public RestResult<Attachment> find(@PathVariable("attachmentId") Long attachmentId) {
+    public RestResult<R> find(@PathVariable("attachmentId") Long attachmentId) {
         return service.findOne(attachmentId).toGetResponse();
     }
 
     @RequestMapping(value = "/upload", method = POST, produces = "application/json")
-    public RestResult<Attachment> uploadFile(
+    public RestResult<R> uploadFile(
             @RequestHeader(value = "Content-Type", required = false) String contentType,
             @RequestHeader(value = "Content-Length", required = false) String contentLength,
             @RequestParam(value = "filename", required = false) String originalFilename,
