2072e3ca190814249bcaf10780df7c9341749825
Luke Harper <lukeharperipl@gmail.com>
2017-02-23 09:07:36 +0000

    INFUND-8278 removing duplicates.
    Former-commit-id: 9d3ee9e772cff2731dc634ead72b82fdd74c6df9

--- ifs-data-service/src/main/resources/db/migration/V87_6_0__Unique_question_status_constraint.sql
+++ ifs-data-service/src/main/resources/db/migration/V87_6_0__Unique_question_status_constraint.sql
@@ -1,1 +1,1 @@
-ALTER TABLE `question_status` add CONSTRAINT unique_marked_question_status UNIQUE (marked_as_complete_by_id, application_id, question_id);
+ALTER IGNORE TABLE `question_status` ADD UNIQUE KEY question_status_complete_unique(application_id, marked_as_complete_by_id, question_id);
