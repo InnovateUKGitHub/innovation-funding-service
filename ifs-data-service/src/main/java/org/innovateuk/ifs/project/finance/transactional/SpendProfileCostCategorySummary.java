07534e3af2fbf016a7719452a3683e87c689c8d4
Bronnyl Dias <bdias@worth.systems>
2017-02-24 15:14:55 +0000

    INFUND-6148-Negative_Numbers-SP-Generation
    1) Revised the Spend Profile generation logic.
    
    Change-Id: I604bacfffc2fd421a92ab28d3cfee1d5a66181be
    
    Former-commit-id: d99227a4e25c3c2bbc9323e5107c3771793105ad

--- ifs-data-service/src/main/java/org/innovateuk/ifs/project/finance/transactional/SpendProfileCostCategorySummary.java
+++ ifs-data-service/src/main/java/org/innovateuk/ifs/project/finance/transactional/SpendProfileCostCategorySummary.java
@@ -21,11 +21,13 @@ class SpendProfileCostCategorySummary {
         this.total = total;
 
         BigDecimal durationInMonths = BigDecimal.valueOf(projectDurationInMonths);
-        BigDecimal monthlyCost = total.divide(durationInMonths, 0, HALF_EVEN);
-        BigDecimal remainder = total.subtract(monthlyCost.multiply(durationInMonths)).setScale(0, HALF_EVEN);
+        BigDecimal remainder = total.remainder(durationInMonths);
 
-        this.firstMonthSpend = monthlyCost.add(remainder);
-        this.otherMonthsSpend = monthlyCost;
+        BigDecimal perfectlyDivisibleTotal = total.subtract(remainder);
+        BigDecimal costPerMonth = perfectlyDivisibleTotal.divide(durationInMonths, 0, HALF_EVEN);
+
+        this.firstMonthSpend = costPerMonth.add(remainder);
+        this.otherMonthsSpend = costPerMonth;
     }
 
     public CostCategory getCategory() {
@@ -42,5 +44,37 @@ class SpendProfileCostCategorySummary {
 
     public BigDecimal getOtherMonthsSpend() {
         return otherMonthsSpend;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Test one");
+
+        //class vars
+        BigDecimal firstMonthSpend;
+        BigDecimal otherMonthsSpend;
+
+        //params
+        BigDecimal total = new BigDecimal(32);
+        long projectDurationInMonths = 36;
+
+/*        BigDecimal durationInMonths = BigDecimal.valueOf(projectDurationInMonths);
+        BigDecimal monthlyCost = total.divide(durationInMonths, 0, HALF_EVEN);
+        BigDecimal remainder = total.subtract(monthlyCost.multiply(durationInMonths)).setScale(0, HALF_EVEN);
+
+        firstMonthSpend = monthlyCost.add(remainder);
+        otherMonthsSpend = monthlyCost;*/
+
+
+        BigDecimal durationInMonths = BigDecimal.valueOf(projectDurationInMonths);
+        BigDecimal remainder = total.remainder(durationInMonths);
+
+        BigDecimal perfectlyDivisibleTotal = total.subtract(remainder);
+        BigDecimal costPerMonth = perfectlyDivisibleTotal.divide(durationInMonths, 0, HALF_EVEN);
+
+        firstMonthSpend = costPerMonth.add(remainder);
+        otherMonthsSpend = costPerMonth;
+
+        System.out.println("First Month spend: " + firstMonthSpend.toString());
+        System.out.println("Other Month spend: " + otherMonthsSpend.toString());
     }
 }
