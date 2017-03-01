3d544566f93783cec810a774a838a6c66013fca7
Tom Baldwin <tbaldwin@worth.systems>
2017-03-01 16:58:39 +0000

    INFUND-7861 - Adds toggle for the new feedback summary view.
    Former-commit-id: 51e19e9807369dcf9e58b3402f46522bfaef9856

--- ifs-web-service/ifs-application-service/src/main/java/org/innovateuk/ifs/application/ApplicationController.java
+++ ifs-web-service/ifs-application-service/src/main/java/org/innovateuk/ifs/application/ApplicationController.java
@@ -60,6 +60,9 @@ import static org.springframework.web.bind.annotation.RequestMethod.GET;
 public class ApplicationController {
     private static final Log LOG = LogFactory.getLog(ApplicationController.class);
 
+    // TODO implement feature toggle https://devops.innovateuk.org/issue-tracking/browse/INFUND-8579
+    private static final boolean APPLICATION_FEADBACK_SUMMARY_FEATURE_ENABLED = false;
+
     public static final String ASSIGN_QUESTION_PARAM = "assign_question";
     public static final String MARK_AS_COMPLETE = "mark_as_complete";
 
@@ -185,7 +188,7 @@ public class ApplicationController {
             model.addAttribute("assessorFeedback", assessorFeedbackViewModel);
         }
 
-        if (competition.getCompetitionStatus().isFeedbackReleased()) {
+        if (APPLICATION_FEADBACK_SUMMARY_FEATURE_ENABLED && competition.getCompetitionStatus().isFeedbackReleased()) {
             model.addAttribute("scores", assessorFormInputResponseRestService.getApplicationAssessmentAggregate(applicationId).getSuccessObjectOrThrowException());
             return "application-feedback-summary";
         }
