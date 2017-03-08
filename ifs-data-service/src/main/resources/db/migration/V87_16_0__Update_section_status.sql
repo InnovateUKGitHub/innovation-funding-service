0544be7c434875cba562a16c055fc8891d021930
richard <richard@hiveit.co.uk>
2017-03-08 16:05:22 +0000

    INFUND-8446 addressing pull request comment
    Former-commit-id: 98d7e9ea33f521afc0cd87a20bdbe119859471aa

--- ifs-data-service/src/main/resources/db/migration/V87_16_0__Update_section_status.sql
+++ ifs-data-service/src/main/resources/db/migration/V87_16_0__Update_section_status.sql
@@ -1,3 +1,6 @@
+-- The competition set up status is considered complete if the public content has been published.
+-- This patch retrospectively adds a competition set up status entry for competitions which have published public content
+-- and avoids duplicates.
 INSERT INTO competition_setup_status (competition_id, `section`, status)
 SELECT c.id, 'CONTENT', 1
   FROM competition AS c
