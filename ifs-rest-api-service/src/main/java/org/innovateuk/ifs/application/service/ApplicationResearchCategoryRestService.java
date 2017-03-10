a33f6d74276333fec262d9524ac7d11edf203a6a
Simon Shakespeare <si@hiveit.co.uk>
2017-03-10 17:15:24 +0000

    INFUND-8260 - Removed unused methods
    Former-commit-id: 1a39dfd02336687f9daff926fe6a7acd9fdd5239

--- ifs-rest-api-service/src/main/java/org/innovateuk/ifs/application/service/ApplicationResearchCategoryRestService.java
+++ ifs-rest-api-service/src/main/java/org/innovateuk/ifs/application/service/ApplicationResearchCategoryRestService.java
@@ -1,15 +1,11 @@
 package org.innovateuk.ifs.application.service;
 
 import org.innovateuk.ifs.application.resource.ApplicationResource;
-import org.innovateuk.ifs.category.resource.ResearchCategoryResource;
 import org.innovateuk.ifs.commons.rest.RestResult;
-
-import java.util.List;
 
 /**
  * Interface providing the actions for changing an application research category / retrieving available research categoriess.
  */
 public interface ApplicationResearchCategoryRestService {
     RestResult<ApplicationResource> saveApplicationResearchCategoryChoice(Long applicationId, Long researchCategoryId);
-    RestResult<List<ResearchCategoryResource>> getAvailableResearchCategoriesForApplication(Long applicationId);
 }
--- ifs-rest-api-service/src/main/java/org/innovateuk/ifs/application/service/ApplicationResearchCategoryRestServiceImpl.java
+++ ifs-rest-api-service/src/main/java/org/innovateuk/ifs/application/service/ApplicationResearchCategoryRestServiceImpl.java
@@ -1,13 +1,9 @@
 package org.innovateuk.ifs.application.service;
 
 import org.innovateuk.ifs.application.resource.ApplicationResource;
-import org.innovateuk.ifs.category.resource.ResearchCategoryResource;
 import org.innovateuk.ifs.commons.rest.RestResult;
 import org.innovateuk.ifs.commons.service.BaseRestService;
-import org.innovateuk.ifs.commons.service.ParameterizedTypeReferences;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 /**
  * Service implements methods for setting an Applications research category and retrieving available choices.
@@ -19,10 +15,5 @@ public class ApplicationResearchCategoryRestServiceImpl extends BaseRestService 
     @Override
     public RestResult<ApplicationResource> saveApplicationResearchCategoryChoice(Long applicationId, Long researchCategoryId) {
         return postWithRestResult(applicationResearchCategoryRestURL + "researchCategory/" + applicationId, researchCategoryId, ApplicationResource.class);
-    }
-
-    @Override
-    public RestResult<List<ResearchCategoryResource>> getAvailableResearchCategoriesForApplication(Long applicationId) {
-        return getWithRestResult(applicationResearchCategoryRestURL + "availableResearchCategories/" + applicationId, ParameterizedTypeReferences.researchCategoryResourceListType());
     }
 }
