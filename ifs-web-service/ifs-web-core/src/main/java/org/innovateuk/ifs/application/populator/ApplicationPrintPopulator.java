15609a231dd2cee2c1fc0586dc208b5ebe3bee9a
Simon Shakespeare <si@hiveit.co.uk>
2017-03-10 14:12:53 +0000

    INFUND-8260 - Application research category now has separate template
    Former-commit-id: 066558696a7630bc4bcea44a47e38a41583cb888

--- ifs-web-service/ifs-web-core/src/main/java/org/innovateuk/ifs/application/populator/ApplicationPrintPopulator.java
+++ ifs-web-service/ifs-web-core/src/main/java/org/innovateuk/ifs/application/populator/ApplicationPrintPopulator.java
@@ -72,9 +72,6 @@ public class ApplicationPrintPopulator {
         applicationModelPopulator.addUserDetails(model, application, user.getId());
         applicationModelPopulator.addApplicationInputs(application, model);
         applicationSectionAndQuestionModelPopulator.addMappedSectionsDetails(model, application, competition, Optional.empty(), userOrganisation);
-
-        //TODO
-        //applicationModelPopulator.addResearchCategoryName(application, model);
         applicationFinanceOverviewModelManager.addFinanceDetails(model, competition.getId(), applicationId, userOrganisation.map(OrganisationResource::getId));
 
         return "application/print";
