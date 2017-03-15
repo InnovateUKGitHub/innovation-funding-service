d291ac1487fc3638b6499680419a2ff76fca5b49
Nuno Alexadre <nalexandre@worth.systems>
2017-03-15 16:54:49 +0100

    Cleaning code @ CompetitionProjectsStatusController
    Former-commit-id: 246d63b1a272507e12680ae768bf2d64c579a886

--- ifs-web-service/ifs-project-setup-mgt-service/src/main/java/org/innovateuk/ifs/project/status/controller/CompetitionProjectsStatusController.java
+++ ifs-web-service/ifs-project-setup-mgt-service/src/main/java/org/innovateuk/ifs/project/status/controller/CompetitionProjectsStatusController.java
@@ -6,14 +6,17 @@ import org.innovateuk.ifs.commons.security.SecuredBySpring;
 import org.innovateuk.ifs.project.sections.ProjectSetupSectionInternalUser;
 import org.innovateuk.ifs.project.status.ProjectStatusService;
 import org.innovateuk.ifs.project.status.resource.CompetitionProjectsStatusResource;
+import org.innovateuk.ifs.project.status.resource.ProjectStatusResource;
 import org.innovateuk.ifs.project.status.viewmodel.CompetitionProjectStatusViewModel;
 import org.innovateuk.ifs.user.resource.UserResource;
 import org.innovateuk.ifs.user.resource.UserRoleType;
+import org.innovateuk.ifs.util.CollectionFunctions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,7 +25,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
@@ -36,71 +39,56 @@ public class CompetitionProjectsStatusController {
     @Autowired
     private BankDetailsService bankDetailsService;
 
-    @RequestMapping(method = GET)
+    @GetMapping
     @PreAuthorize("hasAnyAuthority('project_finance', 'comp_admin')")
-    public String viewCompetitionStatus(
-            Model model,
-            @ModelAttribute("loggedInUser") UserResource loggedInUser,
-            @PathVariable Long competitionId) {
-
-        CompetitionProjectStatusViewModel viewModel = populateCompetitionProjectStatusViewModel(competitionId, loggedInUser);
-
-        model.addAttribute("model", viewModel);
+    public String viewCompetitionStatus(Model model, @ModelAttribute("loggedInUser") UserResource loggedInUser,
+                                        @PathVariable Long competitionId) {
+        model.addAttribute("model", populateCompetitionProjectStatusViewModel(competitionId, loggedInUser));
         return "project/competition-status";
     }
 
     @PreAuthorize("hasAuthority('project_finance')")
     @SecuredBySpring(value = "EXPORT_BANK_DETAILS", description = "Project finance users should be able export bank details")
     @RequestMapping(value = "/bank-details/export", method = GET)
-    public void exportBankDetails(
-            Model model,
-            @ModelAttribute("loggedInUser") UserResource loggedInUser,
-            @PathVariable Long competitionId,
-            HttpServletResponse response) throws IOException {
+    public void exportBankDetails(Model model, @ModelAttribute("loggedInUser") UserResource loggedInUser,
+                                  @PathVariable Long competitionId, HttpServletResponse response) throws IOException {
 
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd_HH-mm");
         String filename = String.format("Bank_details_%s_%s.csv", competitionId, LocalDateTime.now().format(formatter));
         response.setContentType("text/csv");
         response.setHeader("Content-Transfer-Encoding", "binary");
-        response.setHeader("Content-Disposition", "attachment;filename="+filename);
+        response.setHeader("Content-Disposition", "attachment;filename=" + filename);
         final ByteArrayResource resource = bankDetailsService.downloadByCompetition(competitionId);
         IOUtils.copy(resource.getInputStream(), response.getOutputStream());
         response.flushBuffer();
     }
 
     private CompetitionProjectStatusViewModel populateCompetitionProjectStatusViewModel(Long competitionId, UserResource userResource) {
-        CompetitionProjectsStatusResource competitionProjectsStatusResource = projectStatusService.getCompetitionStatus(competitionId);
-
-        Map<Long, ProjectStatusPermission> projectStatusPermissionMap = new HashMap<>();
-
+        CompetitionProjectsStatusResource competitionProjectsStatus = projectStatusService.getCompetitionStatus(competitionId);
+        Map<Long, ProjectStatusPermission> projectStatusPermissionMap
+                = projectStatusPermissions(userResource, competitionProjectsStatus.getProjectStatusResources());
         boolean canExportBankDetails = userResource.hasRole(UserRoleType.PROJECT_FINANCE);
+        return new CompetitionProjectStatusViewModel(competitionProjectsStatus, canExportBankDetails, projectStatusPermissionMap);
+    }
 
-        if(null == competitionProjectsStatusResource.getProjectStatusResources()) {
-            return new CompetitionProjectStatusViewModel(
-                    competitionProjectsStatusResource, canExportBankDetails, projectStatusPermissionMap
-            );
-        }
+    private Map<Long, ProjectStatusPermission> projectStatusPermissions(UserResource user, List<ProjectStatusResource> projectStatuses) {
+        return CollectionFunctions.simpleToLinkedMap(projectStatuses,
+                ProjectStatusResource::getApplicationNumber,
+                projectStatus -> projectStatusPermission(new ProjectSetupSectionInternalUser(projectStatus), user));
+    }
 
-        competitionProjectsStatusResource.getProjectStatusResources().forEach(projectStatusResource -> {
-            ProjectSetupSectionInternalUser internalUser = new ProjectSetupSectionInternalUser(projectStatusResource);
-
-            ProjectStatusPermission projectStatusPermission = new ProjectStatusPermission(
-                    internalUser.canAccessCompaniesHouseSection(userResource).isAccessibleOrNotRequired(),
-                    internalUser.canAccessProjectDetailsSection(userResource).isAccessibleOrNotRequired(),
-                    internalUser.canAccessMonitoringOfficerSection(userResource).isAccessibleOrNotRequired(),
-                    internalUser.canAccessBankDetailsSection(userResource).isAccessibleOrNotRequired(),
-                    internalUser.canAccessFinanceChecksSection(userResource).isAccessibleOrNotRequired(),
-                    internalUser.canAccessSpendProfileSection(userResource).isAccessibleOrNotRequired(),
-                    internalUser.canAccessOtherDocumentsSection(userResource).isAccessibleOrNotRequired(),
-                    internalUser.canAccessGrantOfferLetterSection(userResource).isAccessibleOrNotRequired(),
-                    internalUser.canAccessGrantOfferLetterSendSection(userResource).isAccessibleOrNotRequired(),
-                    internalUser.grantOfferLetterActivityStatus(userResource));
-
-            projectStatusPermissionMap.put(projectStatusResource.getApplicationNumber(), projectStatusPermission);
-        });
-
-        return new CompetitionProjectStatusViewModel(
-                competitionProjectsStatusResource, canExportBankDetails, projectStatusPermissionMap
-        );
+    private ProjectStatusPermission projectStatusPermission(ProjectSetupSectionInternalUser internalUser,
+                                                            UserResource userResource) {
+        return new ProjectStatusPermission(
+                internalUser.canAccessCompaniesHouseSection(userResource).isAccessibleOrNotRequired(),
+                internalUser.canAccessProjectDetailsSection(userResource).isAccessibleOrNotRequired(),
+                internalUser.canAccessMonitoringOfficerSection(userResource).isAccessibleOrNotRequired(),
+                internalUser.canAccessBankDetailsSection(userResource).isAccessibleOrNotRequired(),
+                internalUser.canAccessFinanceChecksSection(userResource).isAccessibleOrNotRequired(),
+                internalUser.canAccessSpendProfileSection(userResource).isAccessibleOrNotRequired(),
+                internalUser.canAccessOtherDocumentsSection(userResource).isAccessibleOrNotRequired(),
+                internalUser.canAccessGrantOfferLetterSection(userResource).isAccessibleOrNotRequired(),
+                internalUser.canAccessGrantOfferLetterSendSection(userResource).isAccessibleOrNotRequired(),
+                internalUser.grantOfferLetterActivityStatus(userResource));
     }
 }
