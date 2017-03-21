65ab20d241313d41446de4357045d93ba7e058f1
EThomaidou <ethomaidou@worth.systems>
2017-03-21 15:42:32 +0100

    Applications fetching
    Former-commit-id: 38df700f296ec9f951f2c9fe69ade3718e4cad00

--- robot-tests/IFS_acceptance_tests/libs/Initialisation.py
+++ robot-tests/IFS_acceptance_tests/libs/Initialisation.py
@@ -16,14 +16,23 @@ db = MySQLdb.connect(**config)
 # prepare a cursor object using cursor() method
 cursor = db.cursor()
 
-# execute SQL query using execute() method.
+# execute SQL query using execute() method, to fetch the Competitions
 cursor.execute("""SELECT `id`,`name` FROM competition""")
 
-# Fetch a single row using fetchone() method.
+# Fetch all competition records
 competition_ids = {}
 for comp in cursor.fetchall():
     competition_ids[comp[1]] = int(comp[0])
     #print(competition_ids)
+
+# execute SQL query using execute() method, to fetch the Applications
+cursor.execute("""SELECT `id`,`name` FROM application""")
+
+# Fetch all application records
+application_ids = {}
+for app in cursor.fetchall():
+    application_ids[app[1]] = int(app[0])
+    print(application_ids)
 
 # disconnect from server
 db.close()
