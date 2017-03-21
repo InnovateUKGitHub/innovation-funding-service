caccfa30800298f6f5c94e795707b82037ef36ed
Nuno Alexadre <nunombalexandre@gmail.com>
2017-03-21 09:56:03 +0100

    Solving compiling issue
    Former-commit-id: dadf8513c65f4e6335024386b26f3767998a44a2

--- ifs-web-service/ifs-project-setup-service/src/main/java/org/innovateuk/ifs/project/spendprofile/viewmodel/TotalProjectSpendProfileTableViewModel.java
+++ ifs-web-service/ifs-project-setup-service/src/main/java/org/innovateuk/ifs/project/spendprofile/viewmodel/TotalProjectSpendProfileTableViewModel.java
@@ -4,6 +4,7 @@ import org.innovateuk.ifs.commons.rest.LocalDateResource;
 import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
 import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.innovateuk.ifs.user.resource.OrganisationResource;
 
 import java.math.BigDecimal;
 import java.util.List;
