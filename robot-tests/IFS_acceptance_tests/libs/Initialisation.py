4a80e41abae2aa0758713c96e99748fb44ab46fb
Eftychia Thomaidou <ethomaidou@worth.systems>
2017-03-21 17:37:03 +0100

    include Sysint Mysql variables
    Former-commit-id: d28ce1a835be88223f5c54a3b21b88dd2f885e87

--- robot-tests/IFS_acceptance_tests/libs/Initialisation.py
+++ robot-tests/IFS_acceptance_tests/libs/Initialisation.py
@@ -1,14 +1,28 @@
 #!/usr/bin/python
 
 import MySQLdb
+import os
+config = ''
 
-config = {
-    'user': 'root',
-    'passwd': 'password',
-    'host': 'ifs-database',
-    'db': 'ifs',
-    'port': 3306,
-}
+try:
+    os.environ['bamboo.ifs_mysql_user_name']
+    config = {
+        'user': os.environ['bamboo.ifs_mysql_user_name'],
+        'passwd': os.environ['bamboo.ifs_mysql_password'],
+        'host': os.environ['bamboo.ifs_mysql_hostname'],
+        'db': os.environ['bamboo.ifs_mysql_db_name'],
+        'port': 3306,
+    }
+except KeyError:
+    print("Run Mysql Locally")
+    config = {
+        'user': 'root',
+        'passwd': 'password',
+        'host': 'ifs-database',
+        'db': 'ifs',
+        'port': 3306,
+    }
+
 
 # Open database connection
 db = MySQLdb.connect(**config)
