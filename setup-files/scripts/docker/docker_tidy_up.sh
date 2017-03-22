0eab4f16d0a16e436c1f7ec777d0884ff617ce2c
Adam Birse <adambirseipl@gmail.com>
2017-03-22 14:57:15 +0000

    Upgraded version number
    Former-commit-id: 300d0f81f2e2ed54207eede0361787c932bd8f4c

---
+++ setup-files/scripts/docker/docker_tidy_up.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+VERSION=$1
+
+# Remove docker images
+docker rmi $(docker images | grep application-service | grep -v $VERSION)
+docker rmi $(docker images | grep assessment-service | grep -v $VERSION)
+docker rmi $(docker images | grep competition-management-service | grep -v $VERSION)
+docker rmi $(docker images | grep project-setup-service | grep -v $VERSION)
+docker rmi $(docker images | grep project-setup-management-service | grep -v $VERSION)
+
+docker rmi $(docker images | grep innovateuk/data-service | grep -v $VERSION)
+
+
+# Note if you are using docker toolbox this is not going to work.  Please consider upgrading.
+docker image prune -f
+docker container prune -f
+docker volume prune -f
