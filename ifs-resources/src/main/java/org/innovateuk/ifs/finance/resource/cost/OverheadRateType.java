3acf48f8e618c1690ae28ee20da32adefcd5a5e3
Ravish Bhagdev <rav@hiveit.co.uk>
2017-03-28 15:27:32 +0100

    INFUND-8986 overhead costs changes template fragment added.
    Former-commit-id: 134a5f2bfff5452f05a13efb693629cd8bac53d9

--- ifs-resources/src/main/java/org/innovateuk/ifs/finance/resource/cost/OverheadRateType.java
+++ ifs-resources/src/main/java/org/innovateuk/ifs/finance/resource/cost/OverheadRateType.java
@@ -1,10 +1,10 @@
 package org.innovateuk.ifs.finance.resource.cost;
 
 public enum OverheadRateType{
-    NONE(null, null),
-    DEFAULT_PERCENTAGE(20, null),
-    TOTAL(null, "total"),
-    CUSTOM_RATE(null, "customRate");
+    NONE(null, "None"),
+    DEFAULT_PERCENTAGE(20, "Default %"),
+    TOTAL(null, "Custom Amount"),
+    CUSTOM_RATE(null, "customRate"); // NOTE: Now that competiton 1 is gone, there is no need to keep custom rate. TODO: Check with Rogier and remove if safe : INFUND-7322
 
     private final Integer rate;
     private final String name;
