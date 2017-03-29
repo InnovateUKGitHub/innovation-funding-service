276ca00a24bfc4f3fb72891c14133ec502ecbc1b
Ben Outram <ben@hiveit.co.uk>
2017-03-29 14:00:01 +0100

    INFUND-7873 Errors saving competition sections were being bound to a field called "currentSection". Binding them to fields where possible, otherwise as global errors.
    Former-commit-id: ec2117f51a5dc7cf79b4045e6e4fca8908635b14

--- ifs-web-service/ifs-competition-mgt-service/src/main/java/org/innovateuk/ifs/competitionsetup/form/CompetitionSetupForm.java
+++ ifs-web-service/ifs-competition-mgt-service/src/main/java/org/innovateuk/ifs/competitionsetup/form/CompetitionSetupForm.java
@@ -1,11 +1,20 @@
 package org.innovateuk.ifs.competitionsetup.form;
 
+import org.innovateuk.ifs.controller.BindingResultTarget;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+
+import java.util.List;
+
 /**
  * Generic form class to pass and save section data.
  */
-public abstract class CompetitionSetupForm {
+public abstract class CompetitionSetupForm implements BindingResultTarget {
 
     private boolean markAsCompleteAction = true;
+
+    private BindingResult bindingResult;
+    private List<ObjectError> objectErrors;
 
     public void setMarkAsCompleteAction(boolean markAsCompleteAction) {
         this.markAsCompleteAction = markAsCompleteAction;
@@ -15,4 +24,19 @@ public abstract class CompetitionSetupForm {
         return markAsCompleteAction;
     }
 
+    public BindingResult getBindingResult() {
+        return bindingResult;
+    }
+
+    public void setBindingResult(BindingResult bindingResult) {
+        this.bindingResult = bindingResult;
+    }
+
+    public List<ObjectError> getObjectErrors() {
+        return objectErrors;
+    }
+
+    public void setObjectErrors(List<ObjectError> objectErrors) {
+        this.objectErrors = objectErrors;
+    }
 }
