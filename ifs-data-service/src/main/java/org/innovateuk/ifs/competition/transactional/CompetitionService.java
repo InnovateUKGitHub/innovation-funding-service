657f79412231b508c3047923c64368fcdec14505
Peter Moreton <pctmoreton@gmail.com>
2017-03-29 14:45:58 +0100

    INFUND-8854 Updated documatation test
    Former-commit-id: 067a1f07ce4c12c62aa2c21b6ff57c8dea7fc9eb

--- ifs-data-service/src/main/java/org/innovateuk/ifs/competition/transactional/CompetitionService.java
+++ ifs-data-service/src/main/java/org/innovateuk/ifs/competition/transactional/CompetitionService.java
@@ -55,6 +55,6 @@ public interface CompetitionService {
     ServiceResult<Void> releaseFeedback(long competitionId);
 
     @PreAuthorize("hasAnyAuthority('comp_admin', 'project_finance')")
-    @SecuredBySpring(value = "MANAGE_INFORM", description = "Comp Admins can manage whether the inform state is changed to")
+    @SecuredBySpring(value = "MANAGE_INFORM", description = "Comp Admins can manage the transition from Panel to Inform")
     ServiceResult<Void> manageInformState(long competitionId);
 }
