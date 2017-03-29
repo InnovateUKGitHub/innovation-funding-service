ce2312c19e218685cbcf89666551685c566d3902
Ravish Bhagdev <rav@hiveit.co.uk>
2017-03-29 09:56:55 +0100

    Adding lables to overhead rate types
    Former-commit-id: 4c3b8b4299d5198a7ae2d240250e3eecb6645885

--- ifs-resources/src/main/java/org/innovateuk/ifs/finance/resource/cost/OverheadRateType.java
+++ ifs-resources/src/main/java/org/innovateuk/ifs/finance/resource/cost/OverheadRateType.java
@@ -1,17 +1,19 @@
 package org.innovateuk.ifs.finance.resource.cost;
 
 public enum OverheadRateType{
-    NONE(null, "None"),
-    DEFAULT_PERCENTAGE(20, "Default %"),
-    CUSTOM_AMOUNT(null, "Custom Amount"),
-    CUSTOM_RATE(null, "customRate"); // NOTE: Now that competiton 1 is gone, there is no need to keep custom rate. TODO: Check with Rogier and remove if safe : INFUND-7322
+    NONE(null, "none", "None"),
+    DEFAULT_PERCENTAGE(20, "default", "Default %"),
+    CUSTOM_AMOUNT(null, "custom_amount", "Custom Amount"),
+    CUSTOM_RATE(null, "customRate", "Custom Rate"); // NOTE: Now that competiton 1 is gone, there is no need to keep custom rate. TODO: Check with Rogier and remove if safe : INFUND-7322
 
     private final Integer rate;
     private final String name;
+    private final String label;
 
-    OverheadRateType(Integer rate, String name) {
+    OverheadRateType(Integer rate, String name, String label) {
         this.rate = rate;
         this.name = name;
+        this.label = label;
     }
 
     public Integer getRate() {
@@ -19,5 +21,9 @@ public enum OverheadRateType{
     }
     public String getName() {
         return name;
+    }
+
+    public String getLabel() {
+        return label;
     }
 }
