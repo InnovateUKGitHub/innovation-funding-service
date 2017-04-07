4a89e08dfa34eedd998ace0b5b2ebe7d3cad64b7
Luke Harper <lukeharperipl@gmail.com>
2017-04-07 12:13:38 +0100

    INFUND-9010 ready for review.

--- ifs-web-service/ifs-web-core/src/main/java/org/innovateuk/ifs/application/finance/view/jes/JESFinanceModelManager.java
+++ ifs-web-service/ifs-web-core/src/main/java/org/innovateuk/ifs/application/finance/view/jes/JESFinanceModelManager.java
@@ -1,19 +1,25 @@
 package org.innovateuk.ifs.application.finance.view.jes;
 
+import com.google.common.collect.ImmutableMap;
 import org.apache.commons.lang3.NotImplementedException;
 import org.innovateuk.ifs.application.finance.form.AcademicFinance;
 import org.innovateuk.ifs.application.finance.model.AcademicFinanceFormField;
+import org.innovateuk.ifs.application.finance.service.FinanceRowService;
 import org.innovateuk.ifs.application.finance.service.FinanceService;
 import org.innovateuk.ifs.application.finance.view.FinanceModelManager;
 import org.innovateuk.ifs.application.finance.viewmodel.AcademicFinanceViewModel;
 import org.innovateuk.ifs.application.form.Form;
+import org.innovateuk.ifs.application.resource.ApplicationResource;
 import org.innovateuk.ifs.application.resource.QuestionResource;
+import org.innovateuk.ifs.application.service.ApplicationService;
 import org.innovateuk.ifs.application.service.OrganisationService;
+import org.innovateuk.ifs.application.service.QuestionService;
 import org.innovateuk.ifs.finance.resource.ApplicationFinanceResource;
 import org.innovateuk.ifs.finance.resource.category.FinanceRowCostCategory;
 import org.innovateuk.ifs.finance.resource.cost.AcademicCost;
 import org.innovateuk.ifs.finance.resource.cost.FinanceRowItem;
 import org.innovateuk.ifs.finance.resource.cost.FinanceRowType;
+import org.innovateuk.ifs.form.resource.FormInputType;
 import org.innovateuk.ifs.user.resource.OrganisationResource;
 import org.innovateuk.ifs.user.resource.ProcessRoleResource;
 import org.innovateuk.ifs.user.service.ProcessRoleService;
@@ -25,6 +31,9 @@ import java.math.BigDecimal;
 import java.util.List;
 import java.util.Map;
 
+import static java.util.Arrays.asList;
+
+
 @Component
 public class JESFinanceModelManager implements FinanceModelManager {
 
@@ -32,10 +41,29 @@ public class JESFinanceModelManager implements FinanceModelManager {
     ProcessRoleService processRoleService;
 
     @Autowired
+    private FinanceRowService financeRowService;
+
+    @Autowired
     OrganisationService organisationService;
 
     @Autowired
     FinanceService financeService;
+
+    @Autowired
+    QuestionService questionService;
+
+    @Autowired
+    ApplicationService applicationService;
+
+    private static Map<FormInputType, List<String>> TYPE_TO_COSTS = new ImmutableMap.Builder<FormInputType, List<String>>()
+            .put(FormInputType.YOUR_FINANCE, asList("tsb_reference"))
+            .put(FormInputType.LABOUR, asList("incurred_staff", "incurred_other_costs", "exceptions_staff"))
+            .put(FormInputType.TRAVEL, asList("incurred_travel_subsistence"))
+            .put(FormInputType.MATERIALS, asList("incurred_other_costs"))
+            .put(FormInputType.OTHER_COSTS, asList("allocated_estates_costs", "allocated_other_costs", "exceptions_other_costs"))
+            .put(FormInputType.OVERHEADS, asList("indirect_costs"))
+            .build();
+
 
     @Override
     public void addOrganisationFinanceDetails(Model model, Long applicationId, List<QuestionResource> costsQuestions, Long userId, Form form, Long organisationId) {
@@ -69,6 +97,7 @@ public class JESFinanceModelManager implements FinanceModelManager {
     public AcademicFinanceViewModel getFinanceViewModel(Long applicationId, List<QuestionResource> costsQuestions, Long userId, Form form, Long organisationId) {
         AcademicFinanceViewModel financeViewModel = new AcademicFinanceViewModel();
 
+        initFinanceRows(applicationId, userId);
         ApplicationFinanceResource applicationFinanceResource = getOrganisationFinances(applicationId, userId);
 
         if (applicationFinanceResource != null) {
@@ -95,6 +124,25 @@ public class JESFinanceModelManager implements FinanceModelManager {
         financeViewModel.setFinanceView("academic-finance");
 
         return financeViewModel;
+    }
+
+    private void initFinanceRows(Long applicationId, Long userId) {
+        ApplicationFinanceResource applicationFinanceResource = getOrganisationFinances(applicationId, userId);
+        Map<FinanceRowType, FinanceRowCostCategory> organisationFinanceDetails = applicationFinanceResource.getFinanceOrganisationDetails();
+        organisationFinanceDetails.forEach((financeRowType, financeRowCostCategory) -> {
+            if (financeRowCostCategory.getCosts().isEmpty() && !FinanceRowType.ACADEMIC.equals(financeRowType)) {
+                ApplicationResource applicationResource = applicationService.getById(applicationFinanceResource.getApplication());
+                QuestionResource questionResource = questionService.getQuestionByCompetitionIdAndFormInputType(applicationResource.getCompetition(), financeRowType.getFormInputType()).getSuccessObject();
+                List<String> costNames = TYPE_TO_COSTS.get(financeRowType.getFormInputType());
+                if (costNames != null) {
+                    costNames.forEach(costName -> {
+                        FinanceRowItem financeRowItem = new AcademicCost(null, costName, BigDecimal.ZERO, null);
+                        financeRowService.add(applicationFinanceResource.getId(), questionResource.getId(), financeRowItem);
+                    });
+                }
+            }
+        });
+
     }
 
     @Override
@@ -132,6 +180,7 @@ public class JESFinanceModelManager implements FinanceModelManager {
             totalValue = total.toPlainString();
         }
 
+
         switch (key) {
             case "tsb_reference":
                 academicFinance.setTsbReference(new AcademicFinanceFormField(cost.getId(), cost.getItem(), BigDecimal.ZERO));
@@ -165,4 +214,4 @@ public class JESFinanceModelManager implements FinanceModelManager {
                 break;
         }
     }
- }
+}
