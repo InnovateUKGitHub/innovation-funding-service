b1d659d7c67ce81756dc5a76c789d15f15c87b14
Ann Stainforth <ann.stainforth.ipl@gmail.com>
2017-04-10 11:49:25 +0100

    INFUND-9146: Update appendix guidance content

---
+++ ifs-data-service/src/main/resources/db/migration/V90_6_0__Update_appendix_guidance_content.sql
@@ -0,0 +1,21 @@
+-- Sector competition questions, appendix guidance
+
+-- Approach and innovation
+UPDATE form_input
+SET guidance_answer='<p>You can submit up to 2 pages in pdf format no larger than 1MB. This can provide graphics, diagrams or an image to explain your innovation.</p>'
+WHERE guidance_answer='<p>you may submit in pdf format up to 2 pages to provide graphics/diagrams/an image to explain the technology/product/service innovation.</p>';
+
+-- Team and resources
+UPDATE form_input
+SET guidance_answer='<p>You can submit up to 4 pages in PDF format no larger than 1MB. This can describe the skills and experience of the main people working on the project.</p>'
+WHERE guidance_answer='<p>you may submit in pdf format up to 4 pages to describe the skills and experience of the main people who will be working on the project.</p>';
+
+-- Project management
+UPDATE form_input
+SET guidance_answer='<p>You can submit a project plan or Gantt chart of up to 2 pages in PDF format no larger than 1MB.</p>'
+WHERE guidance_answer='<p>you can submit a project plan/Gantt chart up to 2 A4 pages in length in pdf format</p>';
+
+-- Risks
+UPDATE form_input
+SET guidance_answer='<p>You can submit a risk register of up to 2 pages in PDF format no larger than 1MB.</p>'
+WHERE guidance_answer='<p>you can submit a risk register of up to 2 A4 pages in length in pdf format</p>';
