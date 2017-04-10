b084d781d2fe51b726b454a53b86f3ae3539bb66
Luke Harper <lukeharperipl@gmail.com>
2017-04-10 15:15:11 +0100

    INFUND-8347 fixing unit test.

--- ifs-data-service/src/test/java/org/innovateuk/ifs/application/controller/QuestionControllerIntegrationTest.java
+++ ifs-data-service/src/test/java/org/innovateuk/ifs/application/controller/QuestionControllerIntegrationTest.java
@@ -258,7 +258,7 @@ public class QuestionControllerIntegrationTest extends BaseControllerIntegration
 
         List<QuestionResource> questions = questionService.getQuestionsByAssessmentId(assessmentId).getSuccessObject();
         // Since the assessment is for an application of competition 1, expect all of the questions of this competition that are visible for assessment
-        assertEquals(asList(9L, 11L, 12L, 13L, 1L, 2L, 3L, 4L, 5L, 6L, 7L, 8L, 15L, 16L, 20L, 40L, 38L, 35L, 42L, 41L, 36L),
+        assertEquals(asList(9L, 11L, 12L, 13L, 1L, 2L, 3L, 4L, 5L, 6L, 7L, 8L, 15L, 16L, 20L, 42L, 40L, 38L, 35L, 41L, 36L),
                 simpleMap(questions, QuestionResource::getId));
     }
 }
