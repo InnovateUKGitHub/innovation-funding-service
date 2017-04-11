53d1953dce7092a94c0ff32d5b5f819e6cc3adb9
Luke Harper <lukeharperipl@gmail.com>
2017-04-11 14:43:23 +0100

    INFUND-8347 adding guidance title instead of hardcoded link.

---
+++ ifs-data-service/src/main/resources/db/migration/V90_7_2__Update_JES_form_input_guidance.sql
@@ -0,0 +1,5 @@
+UPDATE `form_input`
+SET
+guidance_title='<a target="_blank" href="https://www.gov.uk/government/publications/innovate-uk-completing-your-application-project-costs-guidance/guidance-for-academics-applying-via-the-je-s-system">How do I create my Je-S output?</a>',
+guidance_answer=NULL
+WHERE form_input_type_id=20;
