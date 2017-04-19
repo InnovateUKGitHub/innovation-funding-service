ef3f159519010048db2a521064f820751f726b92
Bronnyl Dias <bdias@worth.systems>
2017-04-19 17:43:01 +0100

    INFUND-9189-Package-Refactoring-MO
    Change-Id: I8964130fa990e0045ebb111786a83d3c3926c6c7

--- ifs-data-service/src/main/java/org/innovateuk/ifs/project/monitoringofficer/controller/ProjectMonitoringOfficerController.java
+++ ifs-data-service/src/main/java/org/innovateuk/ifs/project/monitoringofficer/controller/ProjectMonitoringOfficerController.java
@@ -1,15 +1,10 @@
 package org.innovateuk.ifs.project.monitoringofficer.controller;
 
 import org.innovateuk.ifs.commons.rest.RestResult;
-import org.innovateuk.ifs.commons.security.UserAuthenticationService;
 import org.innovateuk.ifs.commons.service.ServiceResult;
-import org.innovateuk.ifs.file.transactional.FileHttpHeadersValidator;
+import org.innovateuk.ifs.project.monitoringofficer.transactional.ProjectMonitoringOfficerService;
 import org.innovateuk.ifs.project.resource.MonitoringOfficerResource;
-import org.innovateuk.ifs.project.transactional.ProjectGrantOfferService;
-import org.innovateuk.ifs.project.transactional.ProjectService;
-import org.innovateuk.ifs.project.transactional.ProjectStatusService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -27,35 +22,22 @@ import javax.validation.Valid;
 public class ProjectMonitoringOfficerController {
 
     @Autowired
-    private ProjectService projectService;
-
-    @Autowired
-    private ProjectStatusService projectStatusService;
-
-    @Autowired
-    private ProjectGrantOfferService projectGrantOfferService;
-
-    @Autowired
-    @Qualifier("projectSetupOtherDocumentsFileValidator")
-    private FileHttpHeadersValidator fileValidator;
-
-    @Autowired
-    private UserAuthenticationService userAuthenticationService;
+    private ProjectMonitoringOfficerService projectMonitoringOfficerService;
 
     @GetMapping("/{projectId}/monitoring-officer")
     public RestResult<MonitoringOfficerResource> getMonitoringOfficer(@PathVariable("projectId") final Long projectId) {
-        return projectService.getMonitoringOfficer(projectId).toGetResponse();
+        return projectMonitoringOfficerService.getMonitoringOfficer(projectId).toGetResponse();
     }
 
     @PutMapping("/{projectId}/monitoring-officer")
     public RestResult<Void> saveMonitoringOfficer(@PathVariable("projectId") final Long projectId,
                                                   @RequestBody @Valid final MonitoringOfficerResource monitoringOfficerResource) {
         final boolean[] sendNotification = new boolean[1];
-        ServiceResult<Boolean> result = projectService.saveMonitoringOfficer(projectId, monitoringOfficerResource)
+        ServiceResult<Boolean> result = projectMonitoringOfficerService.saveMonitoringOfficer(projectId, monitoringOfficerResource)
                 .andOnSuccessReturn(r -> r.isMonitoringOfficerSaved() ? (sendNotification[0] = true) : (sendNotification[0] = false));
 
         if (sendNotification[0]) {
-            return projectService.notifyStakeholdersOfMonitoringOfficerChange(monitoringOfficerResource).toPutResponse();
+            return projectMonitoringOfficerService.notifyStakeholdersOfMonitoringOfficerChange(monitoringOfficerResource).toPutResponse();
         }
 
         return result.toPutResponse();
