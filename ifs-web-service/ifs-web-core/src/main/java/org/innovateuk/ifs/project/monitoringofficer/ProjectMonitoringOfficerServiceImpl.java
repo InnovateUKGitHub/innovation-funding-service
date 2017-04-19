d9957c59cd8139e63f9fb5f2a45c54dddd50f6ba
Bronnyl Dias <bdias@worth.systems>
2017-04-19 15:01:14 +0100

    INFUND-9189-Package-Refactoring-MO
    Change-Id: I7fbaa32c6701322290ea06863452b94c1b7456ff

---
+++ ifs-web-service/ifs-web-core/src/main/java/org/innovateuk/ifs/project/monitoringofficer/ProjectMonitoringOfficerServiceImpl.java
@@ -0,0 +1,30 @@
+package org.innovateuk.ifs.project.monitoringofficer;
+
+import org.innovateuk.ifs.commons.service.ServiceResult;
+import org.innovateuk.ifs.project.monitoringofficer.service.ProjectMonitoringOfficerRestService;
+import org.innovateuk.ifs.project.resource.MonitoringOfficerResource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+/**
+ * A service for dealing with Monitoring Officer ProjectResources via the appropriate Rest services
+ */
+@Service
+public class ProjectMonitoringOfficerServiceImpl implements ProjectMonitoringOfficerService {
+
+    @Autowired
+    private ProjectMonitoringOfficerRestService projectMonitoringOfficerRestService;
+
+    @Override
+    public ServiceResult<Void> updateMonitoringOfficer(Long projectId, String firstName, String lastName, String emailAddress, String phoneNumber) {
+        return projectMonitoringOfficerRestService.updateMonitoringOfficer(projectId, firstName, lastName, emailAddress, phoneNumber).toServiceResult();
+    }
+
+    @Override
+    public Optional<MonitoringOfficerResource> getMonitoringOfficerForProject(Long projectId) {
+        return projectMonitoringOfficerRestService.getMonitoringOfficerForProject(projectId).toOptionalIfNotFound().
+                getSuccessObjectOrThrowException();
+    }
+}
