a9c002998c332fa617f7529c6089bc927bfbd255
Bronnyl Dias <bdias@worth.systems>
2017-04-20 15:05:43 +0100

    INFUND-9189-Package-Refactoring-MO
    Change-Id: I9f14081c34f20062c4f94d25a9137bc8b375bc81

--- ifs-data-service/src/test/java/org/innovateuk/ifs/project/monitoringofficer/transactional/ProjectMonitoringOfficerServiceImplTest.java
+++ ifs-data-service/src/test/java/org/innovateuk/ifs/project/monitoringofficer/transactional/ProjectMonitoringOfficerServiceImplTest.java
@@ -1,246 +1,33 @@
 package org.innovateuk.ifs.project.monitoringofficer.transactional;
 
-import org.apache.commons.lang3.tuple.Pair;
-import org.hamcrest.Matchers;
 import org.innovateuk.ifs.BaseServiceUnitTest;
-import org.innovateuk.ifs.address.domain.Address;
-import org.innovateuk.ifs.address.domain.AddressType;
-import org.innovateuk.ifs.address.resource.AddressResource;
-import org.innovateuk.ifs.application.domain.Application;
-import org.innovateuk.ifs.commons.error.CommonErrors;
 import org.innovateuk.ifs.commons.error.CommonFailureKeys;
-import org.innovateuk.ifs.commons.error.Error;
 import org.innovateuk.ifs.commons.service.ServiceResult;
-import org.innovateuk.ifs.file.domain.FileEntry;
-import org.innovateuk.ifs.file.resource.FileEntryResource;
-import org.innovateuk.ifs.finance.domain.ApplicationFinance;
-import org.innovateuk.ifs.finance.resource.ApplicationFinanceResource;
-import org.innovateuk.ifs.invite.domain.ProjectInvite;
-import org.innovateuk.ifs.invite.domain.ProjectParticipantRole;
-import org.innovateuk.ifs.invite.resource.InviteProjectResource;
-import org.innovateuk.ifs.notifications.resource.ExternalUserNotificationTarget;
-import org.innovateuk.ifs.notifications.resource.NotificationTarget;
-import org.innovateuk.ifs.organisation.domain.OrganisationAddress;
-import org.innovateuk.ifs.project.bankdetails.domain.BankDetails;
 import org.innovateuk.ifs.project.builder.MonitoringOfficerBuilder;
-import org.innovateuk.ifs.project.builder.ProjectBuilder;
 import org.innovateuk.ifs.project.monitoringofficer.domain.MonitoringOfficer;
-import org.innovateuk.ifs.project.domain.PartnerOrganisation;
 import org.innovateuk.ifs.project.domain.Project;
-import org.innovateuk.ifs.project.domain.ProjectUser;
-import org.innovateuk.ifs.project.finance.resource.EligibilityState;
-import org.innovateuk.ifs.project.finance.resource.ViabilityState;
-import org.innovateuk.ifs.project.financecheck.domain.CostCategoryType;
-import org.innovateuk.ifs.project.financecheck.domain.SpendProfile;
-import org.innovateuk.ifs.project.financecheck.transactional.CostCategoryTypeStrategy;
-import org.innovateuk.ifs.project.gol.resource.GOLState;
-import org.innovateuk.ifs.project.monitoringofficer.transactional.ProjectMonitoringOfficerService;
-import org.innovateuk.ifs.project.monitoringofficer.transactional.ProjectMonitoringOfficerServiceImpl;
-import org.innovateuk.ifs.project.resource.ApprovalType;
 import org.innovateuk.ifs.project.monitoringofficer.resource.MonitoringOfficerResource;
-import org.innovateuk.ifs.project.resource.ProjectLeadStatusResource;
-import org.innovateuk.ifs.project.resource.ProjectOrganisationCompositeId;
-import org.innovateuk.ifs.project.resource.ProjectPartnerStatusResource;
-import org.innovateuk.ifs.project.resource.ProjectResource;
-import org.innovateuk.ifs.project.resource.ProjectState;
-import org.innovateuk.ifs.project.resource.ProjectTeamStatusResource;
-import org.innovateuk.ifs.project.resource.ProjectUserResource;
-import org.innovateuk.ifs.project.transactional.EmailService;
-import org.innovateuk.ifs.project.transactional.FinanceChecksGenerator;
-import org.innovateuk.ifs.project.transactional.ProjectService;
-import org.innovateuk.ifs.project.transactional.ProjectServiceImpl;
-import org.innovateuk.ifs.user.builder.UserBuilder;
-import org.innovateuk.ifs.user.domain.Organisation;
-import org.innovateuk.ifs.user.domain.OrganisationType;
-import org.innovateuk.ifs.user.domain.ProcessRole;
-import org.innovateuk.ifs.user.domain.Role;
-import org.innovateuk.ifs.user.domain.User;
-import org.innovateuk.ifs.user.resource.OrganisationTypeEnum;
-import org.innovateuk.ifs.user.resource.UserRoleType;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Mock;
 import org.springframework.test.util.ReflectionTestUtils;
 
-import java.io.File;
-import java.io.InputStream;
-import java.time.LocalDate;
-import java.time.ZonedDateTime;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Calendar;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 import java.util.Optional;
-import java.util.function.Consumer;
-import java.util.function.Supplier;
-
-import static java.util.Arrays.asList;
-import static java.util.Collections.emptyList;
-import static java.util.Collections.emptyMap;
-import static java.util.Collections.singletonList;
-import static org.innovateuk.ifs.LambdaMatcher.createLambdaMatcher;
-import static org.innovateuk.ifs.address.builder.AddressBuilder.newAddress;
-import static org.innovateuk.ifs.address.builder.AddressResourceBuilder.newAddressResource;
-import static org.innovateuk.ifs.address.builder.AddressTypeBuilder.newAddressType;
-import static org.innovateuk.ifs.address.resource.OrganisationAddressType.OPERATING;
-import static org.innovateuk.ifs.address.resource.OrganisationAddressType.PROJECT;
-import static org.innovateuk.ifs.address.resource.OrganisationAddressType.REGISTERED;
-import static org.innovateuk.ifs.application.builder.ApplicationBuilder.newApplication;
-import static org.innovateuk.ifs.commons.error.CommonErrors.badRequestError;
-import static org.innovateuk.ifs.commons.error.CommonErrors.notFoundError;
-import static org.innovateuk.ifs.commons.error.CommonFailureKeys.NOTIFICATIONS_UNABLE_TO_SEND_MULTIPLE;
-import static org.innovateuk.ifs.commons.error.CommonFailureKeys.PROJECT_SETUP_ALREADY_COMPLETE;
-import static org.innovateuk.ifs.commons.error.CommonFailureKeys.PROJECT_SETUP_DATE_MUST_BE_IN_THE_FUTURE;
-import static org.innovateuk.ifs.commons.error.CommonFailureKeys.PROJECT_SETUP_DATE_MUST_START_ON_FIRST_DAY_OF_MONTH;
-import static org.innovateuk.ifs.commons.error.CommonFailureKeys.PROJECT_SETUP_FINANCE_CONTACT_MUST_BE_A_PARTNER_ON_THE_PROJECT_FOR_THE_ORGANISATION;
-import static org.innovateuk.ifs.commons.error.CommonFailureKeys.PROJECT_SETUP_FINANCE_CONTACT_MUST_BE_A_USER_ON_THE_PROJECT_FOR_THE_ORGANISATION;
 import static org.innovateuk.ifs.commons.error.CommonFailureKeys.PROJECT_SETUP_MONITORING_OFFICER_CANNOT_BE_ASSIGNED_UNTIL_PROJECT_DETAILS_SUBMITTED;
-import static org.innovateuk.ifs.commons.error.CommonFailureKeys.PROJECT_SETUP_PROJECT_DETAILS_CANNOT_BE_SUBMITTED_IF_INCOMPLETE;
-import static org.innovateuk.ifs.commons.error.CommonFailureKeys.PROJECT_SETUP_PROJECT_DETAILS_CANNOT_BE_UPDATED_IF_ALREADY_SUBMITTED;
 import static org.innovateuk.ifs.commons.error.CommonFailureKeys.PROJECT_SETUP_PROJECT_ID_IN_URL_MUST_MATCH_PROJECT_ID_IN_MONITORING_OFFICER_RESOURCE;
-import static org.innovateuk.ifs.commons.error.CommonFailureKeys.PROJECT_SETUP_PROJECT_MANAGER_MUST_BE_LEAD_PARTNER;
-import static org.innovateuk.ifs.commons.service.ServiceResult.serviceFailure;
-import static org.innovateuk.ifs.commons.service.ServiceResult.serviceSuccess;
-import static org.innovateuk.ifs.file.builder.FileEntryBuilder.newFileEntry;
-import static org.innovateuk.ifs.file.builder.FileEntryResourceBuilder.newFileEntryResource;
-import static org.innovateuk.ifs.finance.builder.ApplicationFinanceBuilder.newApplicationFinance;
-import static org.innovateuk.ifs.finance.builder.ApplicationFinanceResourceBuilder.newApplicationFinanceResource;
-import static org.innovateuk.ifs.invite.builder.ProjectInviteBuilder.newInvite;
-import static org.innovateuk.ifs.invite.builder.ProjectInviteResourceBuilder.newInviteProjectResource;
-import static org.innovateuk.ifs.invite.domain.ProjectParticipantRole.PROJECT_FINANCE_CONTACT;
-import static org.innovateuk.ifs.invite.domain.ProjectParticipantRole.PROJECT_MANAGER;
-import static org.innovateuk.ifs.invite.domain.ProjectParticipantRole.PROJECT_PARTNER;
-import static org.innovateuk.ifs.notifications.resource.NotificationMedium.EMAIL;
-import static org.innovateuk.ifs.organisation.builder.OrganisationAddressBuilder.newOrganisationAddress;
-import static org.innovateuk.ifs.project.bankdetails.builder.BankDetailsBuilder.newBankDetails;
-import static org.innovateuk.ifs.project.builder.CostCategoryBuilder.newCostCategory;
-import static org.innovateuk.ifs.project.builder.CostCategoryGroupBuilder.newCostCategoryGroup;
-import static org.innovateuk.ifs.project.builder.CostCategoryTypeBuilder.newCostCategoryType;
 import static org.innovateuk.ifs.project.builder.MonitoringOfficerResourceBuilder.newMonitoringOfficerResource;
-import static org.innovateuk.ifs.project.builder.PartnerOrganisationBuilder.newPartnerOrganisation;
 import static org.innovateuk.ifs.project.builder.ProjectBuilder.newProject;
-import static org.innovateuk.ifs.project.builder.ProjectLeadStatusResourceBuilder.newProjectLeadStatusResource;
-import static org.innovateuk.ifs.project.builder.ProjectPartnerStatusResourceBuilder.newProjectPartnerStatusResource;
-import static org.innovateuk.ifs.project.builder.ProjectResourceBuilder.newProjectResource;
-import static org.innovateuk.ifs.project.builder.ProjectTeamStatusResourceBuilder.newProjectTeamStatusResource;
-import static org.innovateuk.ifs.project.builder.ProjectUserBuilder.newProjectUser;
-import static org.innovateuk.ifs.project.builder.ProjectUserResourceBuilder.newProjectUserResource;
-import static org.innovateuk.ifs.project.builder.SpendProfileBuilder.newSpendProfile;
-import static org.innovateuk.ifs.project.constant.ProjectActivityStates.ACTION_REQUIRED;
-import static org.innovateuk.ifs.project.constant.ProjectActivityStates.COMPLETE;
-import static org.innovateuk.ifs.project.constant.ProjectActivityStates.NOT_REQUIRED;
-import static org.innovateuk.ifs.project.constant.ProjectActivityStates.NOT_STARTED;
-import static org.innovateuk.ifs.project.constant.ProjectActivityStates.PENDING;
-import static org.innovateuk.ifs.user.builder.OrganisationBuilder.newOrganisation;
-import static org.innovateuk.ifs.user.builder.OrganisationTypeBuilder.newOrganisationType;
-import static org.innovateuk.ifs.user.builder.ProcessRoleBuilder.newProcessRole;
-import static org.innovateuk.ifs.user.builder.RoleBuilder.newRole;
-import static org.innovateuk.ifs.user.builder.UserBuilder.newUser;
-import static org.innovateuk.ifs.user.builder.UserResourceBuilder.newUserResource;
-import static org.innovateuk.ifs.user.resource.UserRoleType.FINANCE_CONTACT;
-import static org.innovateuk.ifs.user.resource.UserRoleType.LEADAPPLICANT;
-import static org.innovateuk.ifs.user.resource.UserRoleType.PARTNER;
-import static org.innovateuk.ifs.util.CollectionFunctions.simpleFilter;
-import static org.innovateuk.ifs.util.CollectionFunctions.simpleMap;
-import static org.innovateuk.ifs.util.MapFunctions.asMap;
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
-import static org.mockito.Matchers.eq;
-import static org.mockito.Mockito.any;
-import static org.mockito.Mockito.anyLong;
-import static org.mockito.Mockito.atLeast;
-import static org.mockito.Mockito.isA;
-import static org.mockito.Mockito.never;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.verifyZeroInteractions;
 import static org.mockito.Mockito.when;
 
 public class ProjectMonitoringOfficerServiceImplTest extends BaseServiceUnitTest<ProjectMonitoringOfficerService> {
 
-    @Mock
-    private CostCategoryTypeStrategy costCategoryTypeStrategyMock;
-
-    @Mock
-    private FinanceChecksGenerator financeChecksGeneratorMock;
-
-    @Mock
-    private EmailService projectEmailService;
-
-    private Long projectId = 123L;
-    private Long applicationId = 456L;
-    private Long userId = 7L;
-    private Long otherUserId = 8L;
-
-    private Application application;
-    private Organisation organisation;
-    private Role leadApplicantRole;
-    private Role projectManagerRole;
-    private Role partnerRole;
-    private User user;
-    private User u;
-    private ProcessRole leadApplicantProcessRole;
-    private ProjectUser leadPartnerProjectUser;
-    private List<PartnerOrganisation> po;
-    private List<ProjectUserResource> puResource;
-    private List<ProjectUser> pu;
-    private Organisation o;
-    private Project project;
-    private Project p;
     private MonitoringOfficerResource monitoringOfficerResource;
-    private BankDetails bankDetails;
-    private SpendProfile spendProfile;
-
     private static final String webBaseUrl = "https://ifs-local-dev/dashboard";
 
     @Before
-    //TODO - XXX - Refactor Setup and above variables
     public void setUp() {
-
-        organisation = newOrganisation().
-                withOrganisationType(OrganisationTypeEnum.BUSINESS).
-                build();
-
-        leadApplicantRole = newRole(LEADAPPLICANT).build();
-        projectManagerRole = newRole(UserRoleType.PROJECT_MANAGER).build();
-
-        user = newUser().
-                withId(userId).
-                build();
-
-        leadApplicantProcessRole = newProcessRole().
-                withOrganisationId(organisation.getId()).
-                withRole(leadApplicantRole).
-                withUser(user).
-                build();
-
-        leadPartnerProjectUser = newProjectUser().
-                withOrganisation(organisation).
-                withRole(PROJECT_PARTNER).
-                withUser(user).
-                build();
-
-        application = newApplication().
-                withId(applicationId).
-                withProcessRoles(leadApplicantProcessRole).
-                withName("My Application").
-                withDurationInMonths(5L).
-                withStartDate(LocalDate.of(2017, 3, 2)).
-                build();
-
-        project = newProject().
-                withId(projectId).
-                withApplication(application).
-                withProjectUsers(singletonList(leadPartnerProjectUser)).
-                build();
-
         monitoringOfficerResource = newMonitoringOfficerResource()
                 .withProject(1L)
                 .withFirstName("abc")
@@ -248,57 +35,6 @@ public class ProjectMonitoringOfficerServiceImplTest extends BaseServiceUnitTest
                 .withEmail("abc.xyz@gmail.com")
                 .withPhoneNumber("078323455")
                 .build();
-
-        OrganisationType businessOrganisationType = newOrganisationType().withOrganisationType(OrganisationTypeEnum.BUSINESS).build();
-        o = organisation;
-        o.setOrganisationType(businessOrganisationType);
-
-        partnerRole = newRole().
-                withType(FINANCE_CONTACT).
-                build();
-
-        po = newPartnerOrganisation().
-                withOrganisation(o).
-                withLeadOrganisation(Boolean.TRUE).
-                build(1);
-
-        u = newUser().
-                withEmailAddress("a@b.com").
-                withFirstName("A").
-                withLastName("B").
-                build();
-
-        pu = newProjectUser().
-                withRole(PROJECT_FINANCE_CONTACT).
-                withUser(u).
-                withOrganisation(o).
-                withInvite(newInvite().
-                        build()).
-                build(1);
-
-        p = newProject().
-                withProjectUsers(pu).
-                withApplication(application).
-                withPartnerOrganisations(po).
-                withDateSubmitted(ZonedDateTime.now()).
-                withOtherDocumentsApproved(ApprovalType.APPROVED).
-                withSpendProfileSubmittedDate(ZonedDateTime.now()).
-                build();
-
-        puResource = newProjectUserResource().
-                withProject(p.getId()).
-                withOrganisation(o.getId()).
-                withRole(partnerRole.getId()).
-                withRoleName(PROJECT_PARTNER.getName()).
-                build(1);
-
-        bankDetails = newBankDetails().withOrganisation(o).withApproval(Boolean.TRUE).build();
-        spendProfile = newSpendProfile().withOrganisation(o).withGeneratedDate(Calendar.getInstance()).withMarkedComplete(Boolean.TRUE).withApproval(ApprovalType.EMPTY).build();
-
-        when(applicationRepositoryMock.findOne(applicationId)).thenReturn(application);
-        when(projectRepositoryMock.findOne(projectId)).thenReturn(project);
-        when(organisationRepositoryMock.findOne(organisation.getId())).thenReturn(organisation);
-        when(loggedInUserSupplierMock.get()).thenReturn(newUser().build());
     }
 
     @Test
