23971cf686331298f4244189c1a9a7736b11386c
Ben Outram <ben@hiveit.co.uk>
2017-04-20 19:24:51 +0100

    INFUND-9231 Responding to PR comment.

--- ifs-web-service/ifs-web-core/src/main/java/org/innovateuk/ifs/form/service/FormInputResponseServiceImpl.java
+++ ifs-web-service/ifs-web-core/src/main/java/org/innovateuk/ifs/form/service/FormInputResponseServiceImpl.java
@@ -1,7 +1,6 @@
 package org.innovateuk.ifs.form.service;
 
 import org.innovateuk.ifs.form.resource.FormInputResponseResource;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -13,7 +12,6 @@ import static org.innovateuk.ifs.util.CollectionFunctions.simpleToMap;
  * This class contains methods to retrieve and store {@link FormInputResponseResource} related data,
  * through the RestService {@link FormInputResponseRestService}.
  */
-// TODO DW - INFUND-1555 - handle rest results
 @Service
 public class FormInputResponseServiceImpl implements FormInputResponseService {
 
