495af956e8c1c8b0ea96f4e4ff7d23845b32ae3f
richard <richard@hiveit.co.uk>
2017-04-24 11:26:56 +0100

    INFUND-5751 more email tests

--- ifs-data-service/src/test/java/org/innovateuk/ifs/notifications/service/FreemarkerNotificationTemplateRendererIntegrationTest.java
+++ ifs-data-service/src/test/java/org/innovateuk/ifs/notifications/service/FreemarkerNotificationTemplateRendererIntegrationTest.java
@@ -51,6 +51,20 @@ public class FreemarkerNotificationTemplateRendererIntegrationTest extends BaseI
     }
 
     @Test
+    public void testApplicationSubmittedEmail() throws URISyntaxException, IOException {
+
+        Map<String, Object> templateArguments = asMap(
+                "applicationName", "My Application",
+                "applicationId", "1",
+                "competitionName", "Competition 123",
+                "webBaseUrl", "http://webbaseurl.com"
+        );
+
+        assertRenderedEmailTemplateContainsExpectedLines("application_submitted_text_html.html", templateArguments);
+        assertRenderedEmailTemplateContainsExpectedLines("application_submitted_text_plain.txt", templateArguments);
+    }
+
+    @Test
     public void testFundedApplicationEmail() throws URISyntaxException, IOException {
 
         Map<String, Object> templateArguments = asMap(
