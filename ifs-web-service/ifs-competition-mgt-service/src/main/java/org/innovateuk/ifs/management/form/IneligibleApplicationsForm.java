8762efaae9221d344291f8dde26b8fe419ac07d0
Peter Moreton <pctmoreton@gmail.com>
2017-04-24 11:59:49 +0100

    INFUND-7373 Review comments.

--- ifs-web-service/ifs-competition-mgt-service/src/main/java/org/innovateuk/ifs/management/form/IneligibleApplicationsForm.java
+++ ifs-web-service/ifs-competition-mgt-service/src/main/java/org/innovateuk/ifs/management/form/IneligibleApplicationsForm.java
@@ -5,8 +5,8 @@ import java.util.Optional;
 import static java.util.Optional.empty;
 
 public class IneligibleApplicationsForm {
-    String filterSearch = "";
-    Optional<Boolean> filterInform = empty();
+    private String filterSearch = "";
+    private Optional<Boolean> filterInform = empty();
 
     public String getFilterSearch() {
         return filterSearch;
