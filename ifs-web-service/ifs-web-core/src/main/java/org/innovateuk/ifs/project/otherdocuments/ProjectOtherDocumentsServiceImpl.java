1ef0ac20f6243f819a5f809c52d9dc184a24a718
Bronnyl Dias <bdias@worth.systems>
2017-04-24 15:12:25 +0100

    INFUND-9193-Other-Documents-Refactor
    Change-Id: I423571a50565b9b3b9e9dd0dad86cf15b6ea7b56

--- ifs-web-service/ifs-web-core/src/main/java/org/innovateuk/ifs/project/otherdocuments/ProjectOtherDocumentsServiceImpl.java
+++ ifs-web-service/ifs-web-core/src/main/java/org/innovateuk/ifs/project/otherdocuments/ProjectOtherDocumentsServiceImpl.java
@@ -1,11 +1,8 @@
 package org.innovateuk.ifs.project.otherdocuments;
 
-import org.innovateuk.ifs.application.service.ApplicationService;
 import org.innovateuk.ifs.commons.service.ServiceResult;
 import org.innovateuk.ifs.file.resource.FileEntryResource;
-import org.innovateuk.ifs.invite.service.ProjectInviteRestService;
-import org.innovateuk.ifs.project.service.ProjectRestService;
-import org.innovateuk.ifs.user.service.OrganisationRestService;
+import org.innovateuk.ifs.project.otherdocuments.service.ProjectOtherDocumentsRestService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.stereotype.Service;
@@ -19,69 +16,60 @@ import java.util.Optional;
 public class ProjectOtherDocumentsServiceImpl implements ProjectOtherDocumentsService {
 
     @Autowired
-    private ProjectRestService projectRestService;
-
-    @Autowired
-    private ProjectInviteRestService projectInviteRestService;
-
-    @Autowired
-    private ApplicationService applicationService;
-
-    @Autowired
-    private OrganisationRestService organisationRestService;
+    private ProjectOtherDocumentsRestService projectOtherDocumentsRestService;
 
     @Override
     public Optional<ByteArrayResource> getCollaborationAgreementFile(Long projectId) {
-        return projectRestService.getCollaborationAgreementFile(projectId).getSuccessObjectOrThrowException();
+        return projectOtherDocumentsRestService.getCollaborationAgreementFile(projectId).getSuccessObjectOrThrowException();
     }
 
     @Override
     public Optional<FileEntryResource> getCollaborationAgreementFileDetails(Long projectId) {
-        return projectRestService.getCollaborationAgreementFileDetails(projectId).getSuccessObjectOrThrowException();
+        return projectOtherDocumentsRestService.getCollaborationAgreementFileDetails(projectId).getSuccessObjectOrThrowException();
     }
 
     @Override
     public ServiceResult<FileEntryResource> addCollaborationAgreementDocument(Long projectId, String contentType, long fileSize, String originalFilename, byte[] bytes) {
-        return projectRestService.addCollaborationAgreementDocument(projectId, contentType, fileSize, originalFilename, bytes).toServiceResult();
+        return projectOtherDocumentsRestService.addCollaborationAgreementDocument(projectId, contentType, fileSize, originalFilename, bytes).toServiceResult();
     }
 
     @Override
     public ServiceResult<Void> removeCollaborationAgreementDocument(Long projectId) {
-        return projectRestService.removeCollaborationAgreementDocument(projectId).toServiceResult();
+        return projectOtherDocumentsRestService.removeCollaborationAgreementDocument(projectId).toServiceResult();
     }
 
     @Override
     public Optional<ByteArrayResource> getExploitationPlanFile(Long projectId) {
-        return projectRestService.getExploitationPlanFile(projectId).getSuccessObjectOrThrowException();
+        return projectOtherDocumentsRestService.getExploitationPlanFile(projectId).getSuccessObjectOrThrowException();
     }
 
     @Override
     public Optional<FileEntryResource> getExploitationPlanFileDetails(Long projectId) {
-        return projectRestService.getExploitationPlanFileDetails(projectId).getSuccessObjectOrThrowException();
+        return projectOtherDocumentsRestService.getExploitationPlanFileDetails(projectId).getSuccessObjectOrThrowException();
     }
 
     @Override
     public ServiceResult<FileEntryResource> addExploitationPlanDocument(Long projectId, String contentType, long fileSize, String originalFilename, byte[] bytes) {
-        return projectRestService.addExploitationPlanDocument(projectId, contentType, fileSize, originalFilename, bytes).toServiceResult();
+        return projectOtherDocumentsRestService.addExploitationPlanDocument(projectId, contentType, fileSize, originalFilename, bytes).toServiceResult();
     }
 
     @Override
     public ServiceResult<Void> removeExploitationPlanDocument(Long projectId) {
-        return projectRestService.removeExploitationPlanDocument(projectId).toServiceResult();
+        return projectOtherDocumentsRestService.removeExploitationPlanDocument(projectId).toServiceResult();
     }
 
     @Override
     public ServiceResult<Void> acceptOrRejectOtherDocuments(Long projectId, Boolean approved) {
-        return projectRestService.acceptOrRejectOtherDocuments(projectId, approved).toServiceResult();
+        return projectOtherDocumentsRestService.acceptOrRejectOtherDocuments(projectId, approved).toServiceResult();
     }
 
     @Override
     public Boolean isOtherDocumentSubmitAllowed(Long projectId) {
-        return projectRestService.isOtherDocumentsSubmitAllowed(projectId).getSuccessObjectOrThrowException();
+        return projectOtherDocumentsRestService.isOtherDocumentsSubmitAllowed(projectId).getSuccessObjectOrThrowException();
     }
 
     @Override
     public ServiceResult<Void> setPartnerDocumentsSubmitted(Long projectId) {
-        return projectRestService.setPartnerDocumentsSubmitted(projectId).toServiceResult();
+        return projectOtherDocumentsRestService.setPartnerDocumentsSubmitted(projectId).toServiceResult();
     }
 }
