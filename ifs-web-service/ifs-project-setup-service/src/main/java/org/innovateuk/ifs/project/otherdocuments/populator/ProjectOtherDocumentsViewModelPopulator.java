1536c4f013ec2c3167803ac2ace5a50abbd2a0fa
Bronnyl Dias <bdias@worth.systems>
2017-04-24 14:23:09 +0100

    INFUND-9193-Other-Documents-Refactor
    Change-Id: Ib0c6a335662671366eb1e0ff96c8a49b41b1da2e

--- ifs-web-service/ifs-project-setup-service/src/main/java/org/innovateuk/ifs/project/otherdocuments/populator/ProjectOtherDocumentsViewModelPopulator.java
+++ ifs-web-service/ifs-project-setup-service/src/main/java/org/innovateuk/ifs/project/otherdocuments/populator/ProjectOtherDocumentsViewModelPopulator.java
@@ -3,6 +3,7 @@ package org.innovateuk.ifs.project.otherdocuments.populator;
 import org.innovateuk.ifs.file.controller.viewmodel.FileDetailsViewModel;
 import org.innovateuk.ifs.file.resource.FileEntryResource;
 import org.innovateuk.ifs.project.ProjectService;
+import org.innovateuk.ifs.project.otherdocuments.ProjectOtherDocumentsService;
 import org.innovateuk.ifs.project.otherdocuments.viewmodel.ProjectOtherDocumentsViewModel;
 import org.innovateuk.ifs.project.resource.ApprovalType;
 import org.innovateuk.ifs.project.resource.ProjectResource;
@@ -10,7 +11,6 @@ import org.innovateuk.ifs.user.resource.OrganisationResource;
 import org.innovateuk.ifs.user.resource.UserResource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
@@ -21,19 +21,22 @@ import static org.innovateuk.ifs.util.CollectionFunctions.simpleMap;
 @Component
 public class ProjectOtherDocumentsViewModelPopulator {
 
-      @Autowired
-      private ProjectService projectService;
+    @Autowired
+    private ProjectService projectService;
+
+    @Autowired
+    private ProjectOtherDocumentsService projectOtherDocumentsService;
 
       public ProjectOtherDocumentsViewModel populate(Long projectId, UserResource loggedInUser) {
 
         ProjectResource project = projectService.getById(projectId);
-        Optional<FileEntryResource> collaborationAgreement = projectService.getCollaborationAgreementFileDetails(projectId);
-        Optional<FileEntryResource> exploitationPlan = projectService.getExploitationPlanFileDetails(projectId);
+        Optional<FileEntryResource> collaborationAgreement = projectOtherDocumentsService.getCollaborationAgreementFileDetails(projectId);
+        Optional<FileEntryResource> exploitationPlan = projectOtherDocumentsService.getExploitationPlanFileDetails(projectId);
         List<OrganisationResource> partnerOrganisations = projectService.getPartnerOrganisationsForProject(projectId);
 
         List<String> partnerOrganisationNames = simpleMap(partnerOrganisations, OrganisationResource::getName);
         boolean isProjectManager = projectService.isProjectManager(loggedInUser.getId(), projectId);
-        boolean isSubmitAllowed = projectService.isOtherDocumentSubmitAllowed(projectId);
+        boolean isSubmitAllowed = projectOtherDocumentsService.isOtherDocumentSubmitAllowed(projectId);
         List<String> rejectionReasons = emptyList();
 
         boolean otherDocumentsSubmitted = project.getDocumentsSubmittedDate() != null;
