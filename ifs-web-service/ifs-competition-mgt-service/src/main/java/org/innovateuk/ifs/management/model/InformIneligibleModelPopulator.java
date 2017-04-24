5e98ab0efa0155ebc7694c12eef21304f804f5b6
Peter Moreton <pctmoreton@gmail.com>
2017-04-24 15:18:37 +0100

    INFUND-7374 Resolved lead applicant issue

--- ifs-web-service/ifs-competition-mgt-service/src/main/java/org/innovateuk/ifs/management/model/InformIneligibleModelPopulator.java
+++ ifs-web-service/ifs-competition-mgt-service/src/main/java/org/innovateuk/ifs/management/model/InformIneligibleModelPopulator.java
@@ -1,10 +1,16 @@
 package org.innovateuk.ifs.management.model;
 
+import org.innovateuk.ifs.application.UserApplicationRole;
 import org.innovateuk.ifs.application.resource.ApplicationResource;
 import org.innovateuk.ifs.application.service.ApplicationService;
 import org.innovateuk.ifs.management.viewmodel.InformIneligibleViewModel;
+import org.innovateuk.ifs.user.resource.ProcessRoleResource;
+import org.innovateuk.ifs.user.service.ProcessRoleService;
+import org.innovateuk.ifs.user.service.UserRestService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 /**
  * Populator for {@link InformIneligibleViewModel}
@@ -13,15 +19,22 @@ import org.springframework.stereotype.Component;
 public class InformIneligibleModelPopulator {
 
     @Autowired
-    private ApplicationService applicationService;
+    private ProcessRoleService processRoleService;
 
     public InformIneligibleViewModel populateModel(ApplicationResource applicationResource) {
+
+        List<ProcessRoleResource> processRoles = processRoleService.findProcessRolesByApplicationId(applicationResource.getId());
+        String leadApplilcant = processRoles.stream()
+                .filter(pr -> pr.getRoleName().equals(UserApplicationRole.LEAD_APPLICANT.getRoleName()))
+                .map(ProcessRoleResource::getUserName)
+                .findFirst().orElse("");
+
 
         return new InformIneligibleViewModel(
                 applicationResource.getCompetition(),
                 applicationResource.getId(),
                 applicationResource.getCompetitionName(),
                 applicationResource.getName(),
-                "Temp name");
+                leadApplilcant);
     }
 }
