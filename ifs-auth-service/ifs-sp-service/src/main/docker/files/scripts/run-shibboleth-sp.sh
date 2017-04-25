21447ae8b1fe7d5ff4a0df1accc8f4e1d33215c0
Cyril Fait <cyril@orangebus.co.uk>
2017-04-25 12:08:20 +0100

    variable substitution needs to happen at runtime for xml files

--- ifs-auth-service/ifs-sp-service/src/main/docker/files/scripts/run-shibboleth-sp.sh
+++ ifs-auth-service/ifs-sp-service/src/main/docker/files/scripts/run-shibboleth-sp.sh
@@ -2,6 +2,11 @@
 
 # container run script
 
+# substitutions at runtime, we can't use environment variables in XML files
+sed -i "s#\/\/ifs.local-dev#\/\/$SP_DOMAIN#g" /etc/shibboleth/* /var/www/html/Logout/index.html && \
+sed -i "s/idp:8443/$IDP_PORT/g" /etc/shibboleth/metadata.xml && \
+sed -i "s#\/\/idp#\/\/$IDP_DOMAIN#g" /etc/shibboleth/shibboleth2.xml /etc/shibboleth/metadata.xml /var/www/html/Logout/index.html
+
 # Remove any lingering pid files
 for p in /var/run/apache2/apache2.pid /var/run/shibboleth/shibd.pid
 do
