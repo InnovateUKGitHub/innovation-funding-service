440eab3ab1823cb95ac23e9575c97f341c549a7c
Cyril Fait <cyril@orangebus.co.uk>
2017-04-26 14:56:48 +0100

    ifs-auth-service: gradle script to prepare secrets for both IDP and SP

--- ifs-auth-service/ifs-sp-service/src/main/docker/files/scripts/run-shibboleth-sp.sh
+++ ifs-auth-service/ifs-sp-service/src/main/docker/files/scripts/run-shibboleth-sp.sh
@@ -7,6 +7,13 @@ sed -i "s#\/\/ifs.local-dev#\/\/$SP_DOMAIN#g" /etc/shibboleth/* /var/www/html/Lo
 sed -i "s/idp:8443/$IDP_PORT/g" /etc/shibboleth/metadata.xml
 proxy_certificate=$(sed '/^-----/d' "/etc/shibboleth/$PROXY_CERTIFICATE" | sed '{:q;N;s/\n/\\n/g;t q}')
 sed -i "s#\${PROXY_CERTIFICATE}#$proxy_certificate#g" /etc/shibboleth/metadata.xml && \
+
+idp_signing_certificate=$(sed '/^-----/d' "/etc/shibboleth/$IDP_SIGNING_CERTIFICATE" | sed '{:q;N;s/\n/\\n/g;t q}')
+sed -i "s#\${IDP_SIGNING_CERTIFICATE}#$idp_signing_certificate#g" /etc/shibboleth/metadata.xml && \
+
+idp_encryption_certificate=$(sed '/^-----/d' "/etc/shibboleth/$IDP_ENCRYPTION_CERTIFICATE" | sed '{:q;N;s/\n/\\n/g;t q}')
+sed -i "s#\${IDP_ENCRYPTION_CERTIFICATE}#$idp_encryption_certificate#g" /etc/shibboleth/metadata.xml && \
+
 sed -i "s#\/\/idp#\/\/$IDP_DOMAIN#g" /etc/shibboleth/shibboleth2.xml /etc/shibboleth/metadata.xml /var/www/html/Logout/index.html
 
 # Remove any lingering pid files
