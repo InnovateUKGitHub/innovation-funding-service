f8370858db8675e5162aad605aad05a852406893
Ann Stainforth <ann.stainforth.ipl@gmail.com>
2017-04-27 16:25:54 +0100

    INFUND-9352: Update academic grant percentage of all competitions

--- ifs-data-service/src/main/resources/db/setup/V91_6_0__Acceptance_fix_academic_grant_percentage.sql
+++ ifs-data-service/src/main/resources/db/setup/V91_6_0__Acceptance_fix_academic_grant_percentage.sql
@@ -1,7 +1,4 @@
--- Update the value of the academic grant percentage for both the template competitions
-SET @sector_template_name = "Template for the Sector competition type";
-SET @programme_template_name = "Template for the Programme competition type";
-
+-- Update the value of the academic grant percentage for all ifs competitions
 UPDATE competition
 SET academic_grant_percentage=100
-WHERE name=@sector_template_name OR name=@programme_template_name AND non_ifs=0;
+WHERE non_ifs=0;
