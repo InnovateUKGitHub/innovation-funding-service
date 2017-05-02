446bb22f73cdc3687d3304db86b53750de5eede9
Ravish Bhagdev <rav@hiveit.co.uk>
2017-05-02 16:36:09 +0100

    INFUND-9404 Disallow creation of application unless competition is open

---
+++ ifs-data-service/src/main/java/org/innovateuk/ifs/competition/security/CompetitionLookupStrategy.java
@@ -0,0 +1,35 @@
+package org.innovateuk.ifs.competition.security;
+
+import org.innovateuk.ifs.commons.security.PermissionEntityLookupStrategies;
+import org.innovateuk.ifs.commons.security.PermissionEntityLookupStrategy;
+import org.innovateuk.ifs.competition.domain.Competition;
+import org.innovateuk.ifs.competition.mapper.CompetitionMapper;
+import org.innovateuk.ifs.competition.repository.CompetitionRepository;
+import org.innovateuk.ifs.competition.resource.CompetitionResource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+ * Class for converting between
+ */
+
+@Component
+@PermissionEntityLookupStrategies
+public class CompetitionLookupStrategy {
+
+    @Autowired
+    private CompetitionRepository competitionRepository;
+
+    @Autowired
+    private CompetitionMapper competitionMapper;
+
+    @PermissionEntityLookupStrategy
+    public Competition getCompetition(Long competitionId) {
+        return competitionRepository.findOne(competitionId);
+    }
+
+    @PermissionEntityLookupStrategy
+    public CompetitionResource getCompetititionResource(Long competitionId) {
+        return competitionMapper.mapToResource(competitionRepository.findOne(competitionId));
+    }
+}
