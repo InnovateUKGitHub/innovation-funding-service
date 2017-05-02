2a813ac9de0f418987b4d17e4591b7db1eeac48c
Nicholas Tsim <nick@hiveit.co.uk>
2017-05-02 16:06:48 +0100

    INFUND-9381 - Adds th to allowed tags

--- ifs-web-service/ifs-web-core/src/main/java/org/innovateuk/ifs/config/EnhancedUtextProcessor.java
+++ ifs-web-service/ifs-web-core/src/main/java/org/innovateuk/ifs/config/EnhancedUtextProcessor.java
@@ -27,7 +27,7 @@ class EnhancedUtextProcessor extends AbstractAttributeTagProcessor {
             // Tags and attributes should be in alphabetical order for ease of lookup
             .addTags(
                     "a", "caption", "h1", "h2", "h3", "div", "em", "li", "ol", "p", "span",
-                    "strong", "table", "thead", "tbody", "tfoot", "tr",  "td", "ul"
+                    "strong", "table", "tbody", "tfoot", "thead", "td", "th", "tr", "ul"
             )
             .addAttributes("a", "href", "target", "title")
             .addAttributes("div", "class", "id", "aria-hidden")
