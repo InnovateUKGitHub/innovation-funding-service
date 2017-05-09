7df31fbd17617eaf3bfb624643b4c086ecbeba87
Ravish Bhagdev <rav@hiveit.co.uk>
2017-05-09 01:08:58 +0100

    INFUND-9484 adding unit tests for project finance handler

--- ifs-data-service/src/test/java/org/innovateuk/ifs/finance/handler/ProjectFinanceHandlerImplTest.java
+++ ifs-data-service/src/test/java/org/innovateuk/ifs/finance/handler/ProjectFinanceHandlerImplTest.java
@@ -1,10 +1,16 @@
 package org.innovateuk.ifs.finance.handler;
 
 import org.innovateuk.ifs.BaseUnitTestMocksTest;
+import org.innovateuk.ifs.application.domain.Application;
 import org.innovateuk.ifs.commons.service.ServiceResult;
+import org.innovateuk.ifs.competition.domain.Competition;
 import org.innovateuk.ifs.finance.domain.ProjectFinance;
 import org.innovateuk.ifs.finance.resource.ProjectFinanceResource;
 import org.innovateuk.ifs.finance.resource.ProjectFinanceResourceId;
+import org.innovateuk.ifs.finance.resource.category.DefaultCostCategory;
+import org.innovateuk.ifs.finance.resource.category.FinanceRowCostCategory;
+import org.innovateuk.ifs.finance.resource.cost.FinanceRowType;
+import org.innovateuk.ifs.project.domain.Project;
 import org.innovateuk.ifs.user.domain.Organisation;
 import org.innovateuk.ifs.user.resource.OrganisationTypeEnum;
 import org.junit.Before;
@@ -12,11 +18,21 @@ import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static freemarker.template.utility.Collections12.singletonList;
+import static org.innovateuk.ifs.application.builder.ApplicationBuilder.newApplication;
+import static org.innovateuk.ifs.competition.builder.CompetitionBuilder.newCompetition;
 import static org.innovateuk.ifs.finance.builder.ProjectFinanceResourceBuilder.newProjectFinanceResource;
 import static org.innovateuk.ifs.finance.domain.builder.ProjectFinanceBuilder.newProjectFinance;
+import static org.innovateuk.ifs.project.builder.ProjectBuilder.newProject;
 import static org.innovateuk.ifs.user.builder.OrganisationBuilder.newOrganisation;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyLong;
 import static org.mockito.Mockito.when;
 
@@ -25,33 +41,59 @@ import static org.mockito.Mockito.when;
  */
 public class ProjectFinanceHandlerImplTest extends BaseUnitTestMocksTest {
     @InjectMocks
-    ProjectFinanceHandler handler = new ProjectFinanceHandlerImpl();
+    private ProjectFinanceHandler handler = new ProjectFinanceHandlerImpl();
 
     @Mock
     private OrganisationFinanceDefaultHandler organisationFinanceDefaultHandlerMock;
 
+    private Long projectId = 1L;
+
+    private Long organisationId = 2L;
+
+    //private Long projectFinanceId = 3L;
+
+    private ProjectFinanceResourceId projectFinanceResourceId;
+
+    private ProjectFinanceResource projectFinanceResource;
+
     @Before
     public void setUp() throws Exception {
-
-    }
-
-    @Test
-    public void test(){
-        Long projectId = 1L;
-        Long organisationId = 2L;
-
         Organisation organisation = newOrganisation().withId(organisationId).withOrganisationType(OrganisationTypeEnum.BUSINESS).build();
-        ProjectFinanceResourceId projectFinanceResourceId = new ProjectFinanceResourceId(projectId, organisationId);
-        ProjectFinance projectFinance = newProjectFinance().withOrganisation(organisation).build();
-        ProjectFinanceResource projectFinanceResource = newProjectFinanceResource().withOrganisation(organisationId).build();
+        Competition competition = newCompetition().build();
+        Application application = newApplication().withCompetition(competition).build();
+        Project project = newProject().withId(projectId).withApplication(application).build();
+        projectFinanceResourceId = new ProjectFinanceResourceId(projectId, organisationId);
+        ProjectFinance projectFinance = newProjectFinance().withOrganisation(organisation).withProject(project).build();
+        projectFinanceResource = newProjectFinanceResource().withId(projectFinance.getId()).withOrganisation(organisationId).build();
+        Map<FinanceRowType, FinanceRowCostCategory> costs = new HashMap<>();
+        costs.put(FinanceRowType.MATERIALS, new DefaultCostCategory());
 
         when(projectFinanceRepositoryMock.findByProjectIdAndOrganisationId(projectFinanceResourceId.getProjectId(), projectFinanceResourceId.getOrganisationId())).thenReturn(projectFinance);
         when(projectFinanceMapperMock.mapToResource(projectFinance)).thenReturn(projectFinanceResource);
         when(organisationRepositoryMock.findOne(organisationId)).thenReturn(organisation);
         when(organisationFinanceDelegateMock.getOrganisationFinanceHandler(anyLong())).thenReturn(organisationFinanceDefaultHandlerMock);
+        when(projectFinanceRepositoryMock.findByProjectId(projectId)).thenReturn(singletonList(projectFinance));
+        when(organisationFinanceDelegateMock.getOrganisationFinanceHandler(any(Long.class))).thenReturn(organisationFinanceDefaultHandlerMock);
+        when(organisationFinanceDefaultHandlerMock.getProjectOrganisationFinances(projectFinance.getId())).thenReturn(costs);
+        when(organisationFinanceDefaultHandlerMock.getProjectOrganisationFinanceTotals(projectFinance.getId(), competition)).thenReturn(costs);
+    }
 
+    @Test
+    public void testGetResearchParticipationPercentageFromProject(){
+        BigDecimal result = handler.getResearchParticipationPercentageFromProject(projectId);
+        assertTrue(result != null);
+    }
+
+    @Test
+    public void testGetProjectOrganisationFinances(){
         ServiceResult<ProjectFinanceResource> result = handler.getProjectOrganisationFinances(projectFinanceResourceId);
         assertTrue(result.isSuccess());
         assertEquals(result.getSuccessObject(), projectFinanceResource);
+    }
+
+    @Test
+    public void testGetFinanceChecksTotals(){
+        List<ProjectFinanceResource> result = handler.getFinanceChecksTotals(projectId);
+        assertTrue(result.size() > 0);
     }
 }
