c5e3faaef11529ba0859a0a5fcda4d8e2445715f
Bronnyl Dias <bdias@worth.systems>
2017-05-19 15:01:46 +0100

    IFS-222-Project-Details-Refactor
    Change-Id: I19162449daa3eaac515dd5660f9d7981b1b21efe

--- ifs-data-service/src/main/java/org/innovateuk/ifs/project/controller/ProjectController.java
+++ ifs-data-service/src/main/java/org/innovateuk/ifs/project/controller/ProjectController.java
@@ -51,65 +51,16 @@ public class ProjectController {
         return projectService.findAll().toGetResponse();
     }
 
-   /* @PostMapping(value="/{id}/project-manager/{projectManagerId}")
-    public RestResult<Void> setProjectManager(@PathVariable("id") final Long id, @PathVariable("projectManagerId") final Long projectManagerId) {
-        return projectService.setProjectManager(id, projectManagerId).toPostResponse();
-    }
-
-    @PostMapping("/{projectId}/startdate")
-    public RestResult<Void> updateProjectStartDate(@PathVariable("projectId") final Long projectId,
-                                                   @RequestParam("projectStartDate") @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate projectStartDate) {
-        return projectService.updateProjectStartDate(projectId, projectStartDate).toPostResponse();
-    }
-
-    @PostMapping("/{projectId}/address")
-    public RestResult<Void> updateProjectAddress(@PathVariable("projectId") final Long projectId,
-                                                 @RequestParam("leadOrganisationId") final Long leadOrganisationId,
-                                                 @RequestParam("addressType") final String addressType,
-                                                 @RequestBody AddressResource addressResource) {
-        return projectService.updateProjectAddress(leadOrganisationId, projectId, OrganisationAddressType.valueOf(addressType), addressResource).toPostResponse();
-    }*/
-
     @GetMapping(value = "/user/{userId}")
     public RestResult<List<ProjectResource>> findByUserId(@PathVariable("userId") final Long userId) {
         return projectService.findByUserId(userId).toGetResponse();
     }
-
-    /*@PostMapping("/{projectId}/organisation/{organisation}/finance-contact")
-    public RestResult<Void> updateFinanceContact(@PathVariable("projectId") final Long projectId,
-                                                 @PathVariable("organisation") final Long organisationId,
-                                                 @RequestParam("financeContact") Long financeContactUserId) {
-        ProjectOrganisationCompositeId composite = new ProjectOrganisationCompositeId(projectId, organisationId);
-        return projectService.updateFinanceContact(composite, financeContactUserId).toPostResponse();
-    }
-
-    @PostMapping("/{projectId}/invite-finance-contact")
-    public RestResult<Void> inviteFinanceContact(@PathVariable("projectId") final Long projectId,
-                                                 @RequestBody @Valid final InviteProjectResource inviteResource) {
-        return projectService.inviteFinanceContact(projectId, inviteResource).toPostResponse();
-    }
-
-    @PostMapping("/{projectId}/invite-project-manager")
-    public RestResult<Void> inviteProjectManager(@PathVariable("projectId") final Long projectId,
-                                                 @RequestBody @Valid final InviteProjectResource inviteResource) {
-        return projectService.inviteProjectManager(projectId, inviteResource).toPostResponse();
-    }*/
 
     @GetMapping("/{projectId}/project-users")
     public RestResult<List<ProjectUserResource>> getProjectUsers(@PathVariable("projectId") final Long projectId) {
         return projectService.getProjectUsers(projectId).toGetResponse();
     }
 
-    @PostMapping("/{projectId}/setApplicationDetailsSubmitted")
-    public RestResult<Void> setApplicationDetailsSubmitted(@PathVariable("projectId") final Long projectId){
-        return projectService.submitProjectDetails(projectId, ZonedDateTime.now()).toPostResponse();
-    }
-
-    /*@GetMapping("/{projectId}/isSubmitAllowed")
-    public RestResult<Boolean> isSubmitAllowed(@PathVariable("projectId") final Long projectId){
-        return projectService.isSubmitAllowed(projectId).toGetResponse();
-    }
-*/
     @GetMapping("/{projectId}/getOrganisationByUser/{userId}")
     public RestResult<OrganisationResource> getOrganisationByProjectAndUser(@PathVariable("projectId") final Long projectId,
                                                                             @PathVariable("userId") final Long userId){
--- ifs-data-service/src/main/java/org/innovateuk/ifs/project/projectdetails/controller/ProjectDetailsController.java
+++ ifs-data-service/src/main/java/org/innovateuk/ifs/project/projectdetails/controller/ProjectDetailsController.java
@@ -4,6 +4,7 @@ import org.innovateuk.ifs.address.resource.AddressResource;
 import org.innovateuk.ifs.address.resource.OrganisationAddressType;
 import org.innovateuk.ifs.commons.rest.RestResult;
 import org.innovateuk.ifs.invite.resource.InviteProjectResource;
+import org.innovateuk.ifs.project.projectdetails.transactional.ProjectDetailsService;
 import org.innovateuk.ifs.project.resource.ProjectOrganisationCompositeId;
 import org.innovateuk.ifs.project.resource.ProjectResource;
 import org.innovateuk.ifs.project.resource.ProjectTeamStatusResource;
@@ -37,35 +38,20 @@ import static java.util.Optional.ofNullable;
 public class ProjectDetailsController {
 
     @Autowired
-    private ProjectService projectService;
+    private ProjectDetailsService projectDetailsService;
 
     @Autowired
     private ProjectStatusService projectStatusService;
 
-    /*@GetMapping("/{id}")
-    public RestResult<ProjectResource> getProjectById(@PathVariable("id") final Long id) {
-        return projectService.getProjectById(id).toGetResponse();
-    }
-
-    @GetMapping("/application/{application}")
-    public RestResult<ProjectResource> getByApplicationId(@PathVariable("application") final Long application) {
-        return projectService.getByApplicationId(application).toGetResponse();
-    }
-
-    @GetMapping("/")
-    public RestResult<List<ProjectResource>> findAll() {
-        return projectService.findAll().toGetResponse();
-    }*/
-
     @PostMapping(value="/{id}/project-manager/{projectManagerId}")
     public RestResult<Void> setProjectManager(@PathVariable("id") final Long id, @PathVariable("projectManagerId") final Long projectManagerId) {
-        return projectService.setProjectManager(id, projectManagerId).toPostResponse();
+        return projectDetailsService.setProjectManager(id, projectManagerId).toPostResponse();
     }
 
     @PostMapping("/{projectId}/startdate")
     public RestResult<Void> updateProjectStartDate(@PathVariable("projectId") final Long projectId,
                                                    @RequestParam("projectStartDate") @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate projectStartDate) {
-        return projectService.updateProjectStartDate(projectId, projectStartDate).toPostResponse();
+        return projectDetailsService.updateProjectStartDate(projectId, projectStartDate).toPostResponse();
     }
 
     @PostMapping("/{projectId}/address")
@@ -73,75 +59,36 @@ public class ProjectDetailsController {
                                                  @RequestParam("leadOrganisationId") final Long leadOrganisationId,
                                                  @RequestParam("addressType") final String addressType,
                                                  @RequestBody AddressResource addressResource) {
-        return projectService.updateProjectAddress(leadOrganisationId, projectId, OrganisationAddressType.valueOf(addressType), addressResource).toPostResponse();
+        return projectDetailsService.updateProjectAddress(leadOrganisationId, projectId, OrganisationAddressType.valueOf(addressType), addressResource).toPostResponse();
     }
-
-/*    @GetMapping(value = "/user/{userId}")
-    public RestResult<List<ProjectResource>> findByUserId(@PathVariable("userId") final Long userId) {
-        return projectService.findByUserId(userId).toGetResponse();
-    }*/
 
     @PostMapping("/{projectId}/organisation/{organisation}/finance-contact")
     public RestResult<Void> updateFinanceContact(@PathVariable("projectId") final Long projectId,
                                                  @PathVariable("organisation") final Long organisationId,
                                                  @RequestParam("financeContact") Long financeContactUserId) {
         ProjectOrganisationCompositeId composite = new ProjectOrganisationCompositeId(projectId, organisationId);
-        return projectService.updateFinanceContact(composite, financeContactUserId).toPostResponse();
+        return projectDetailsService.updateFinanceContact(composite, financeContactUserId).toPostResponse();
     }
 
     @PostMapping("/{projectId}/invite-finance-contact")
     public RestResult<Void> inviteFinanceContact(@PathVariable("projectId") final Long projectId,
                                                  @RequestBody @Valid final InviteProjectResource inviteResource) {
-        return projectService.inviteFinanceContact(projectId, inviteResource).toPostResponse();
+        return projectDetailsService.inviteFinanceContact(projectId, inviteResource).toPostResponse();
     }
 
     @PostMapping("/{projectId}/invite-project-manager")
     public RestResult<Void> inviteProjectManager(@PathVariable("projectId") final Long projectId,
                                                  @RequestBody @Valid final InviteProjectResource inviteResource) {
-        return projectService.inviteProjectManager(projectId, inviteResource).toPostResponse();
-    }
-
-    /*@GetMapping("/{projectId}/project-users")
-    public RestResult<List<ProjectUserResource>> getProjectUsers(@PathVariable("projectId") final Long projectId) {
-        return projectService.getProjectUsers(projectId).toGetResponse();
+        return projectDetailsService.inviteProjectManager(projectId, inviteResource).toPostResponse();
     }
 
     @PostMapping("/{projectId}/setApplicationDetailsSubmitted")
     public RestResult<Void> setApplicationDetailsSubmitted(@PathVariable("projectId") final Long projectId){
-        return projectService.submitProjectDetails(projectId, ZonedDateTime.now()).toPostResponse();
+        return projectDetailsService.submitProjectDetails(projectId, ZonedDateTime.now()).toPostResponse();
     }
-*/
+
     @GetMapping("/{projectId}/isSubmitAllowed")
     public RestResult<Boolean> isSubmitAllowed(@PathVariable("projectId") final Long projectId){
-        return projectService.isSubmitAllowed(projectId).toGetResponse();
+        return projectDetailsService.isSubmitAllowed(projectId).toGetResponse();
     }
-
-    /*@GetMapping("/{projectId}/getOrganisationByUser/{userId}")
-    public RestResult<OrganisationResource> getOrganisationByProjectAndUser(@PathVariable("projectId") final Long projectId,
-                                                                            @PathVariable("userId") final Long userId){
-        return projectService.getOrganisationByProjectAndUser(projectId, userId).toGetResponse();
-    }
-
-    @PostMapping("/{projectId}/partners")
-    public RestResult<Void> addPartner(@PathVariable(value = "projectId")Long projectId,
-                                       @RequestParam(value = "userId", required = true) Long userId,
-                                       @RequestParam(value = "organisationId", required = true) Long organisationId) {
-        return projectService.addPartner(projectId, userId, organisationId).toPostResponse();
-    }
-
-    @GetMapping("/{projectId}/team-status")
-    public RestResult<ProjectTeamStatusResource> getTeamStatus(@PathVariable(value = "projectId") Long projectId,
-                                                               @RequestParam(value = "filterByUserId", required = false) Long filterByUserId) {
-        return projectService.getProjectTeamStatus(projectId, ofNullable(filterByUserId)).toGetResponse();
-    }*/
-
-   /* @GetMapping("/{projectId}/project-manager")
-    public RestResult<ProjectUserResource> getProjectManager(@PathVariable(value = "projectId") Long projectId) {
-        return projectService.getProjectManager(projectId).toGetResponse();
-    }
-
-    @GetMapping("/{projectId}/status")
-    public RestResult<ProjectStatusResource> getStatus(@PathVariable(value = "projectId") Long projectId) {
-        return projectStatusService.getProjectStatusByProjectId(projectId).toGetResponse();
-    }*/
 }
