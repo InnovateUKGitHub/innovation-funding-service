60f2d4f0e44b804f54cec1ca4abf073a99ecbd66
Bronnyl Dias <bdias@worth.systems>
2017-05-19 20:39:31 +0100

    IFS-222-Project-Details-Refactor
    Change-Id: Ic0eca8f86c1a3fcfe93d5876df9b24691611a6cb

---
+++ ifs-rest-api-service/src/main/java/org/innovateuk/ifs/project/projectdetails/ProjectDetailsRestServiceImpl.java
@@ -0,0 +1,54 @@
+package org.innovateuk.ifs.project.projectdetails;
+
+import org.innovateuk.ifs.address.resource.AddressResource;
+import org.innovateuk.ifs.address.resource.OrganisationAddressType;
+import org.innovateuk.ifs.commons.rest.RestResult;
+import org.innovateuk.ifs.commons.service.BaseRestService;
+import org.innovateuk.ifs.invite.resource.InviteProjectResource;
+import org.innovateuk.ifs.project.resource.ProjectOrganisationCompositeId;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+
+@Service
+public class ProjectDetailsRestServiceImpl extends BaseRestService implements ProjectDetailsRestService {
+
+    private String projectRestURL = "/project";
+
+    @Override
+    public RestResult<Void> updateProjectManager(Long projectId, Long projectManagerUserId) {
+        return postWithRestResult(projectRestURL + "/" + projectId + "/project-manager/" + projectManagerUserId, Void.class);
+    }
+    @Override
+    public RestResult<Void> updateProjectStartDate(Long projectId, LocalDate projectStartDate) {
+        return postWithRestResult(projectRestURL + "/" + projectId + "/startdate?projectStartDate=" + projectStartDate, Void.class);
+    }
+
+    @Override
+    public RestResult<Void> updateProjectAddress(long leadOrganisationId, long projectId, OrganisationAddressType addressType, AddressResource address) {
+        return postWithRestResult(projectRestURL + "/" + projectId + "/address?addressType=" + addressType.name() + "&leadOrganisationId=" + leadOrganisationId, address, Void.class);
+    }
+
+    @Override
+    public RestResult<Void> updateFinanceContact(ProjectOrganisationCompositeId composite, Long financeContactUserId) {
+        return postWithRestResult(projectRestURL + "/" + composite.getProjectId() + "/organisation/" + composite.getOrganisationId() + "/finance-contact?financeContact=" + financeContactUserId, Void.class);
+    }
+
+    @Override
+    public RestResult<Void> setApplicationDetailsSubmitted(Long projectId) {
+        return postWithRestResult(projectRestURL + "/" + projectId + "/setApplicationDetailsSubmitted", Void.class);
+    }
+
+    @Override
+    public RestResult<Boolean> isSubmitAllowed(Long projectId) {
+        return getWithRestResult(projectRestURL + "/" + projectId + "/isSubmitAllowed", Boolean.class);
+    }
+
+    public RestResult<Void> inviteFinanceContact(Long projectId, InviteProjectResource inviteResource) {
+        return postWithRestResult(projectRestURL + "/" + projectId + "/invite-finance-contact", inviteResource, Void.class);
+    }
+
+    @Override public RestResult<Void> inviteProjectManager(final Long projectId, final InviteProjectResource inviteResource) {
+        return postWithRestResult(projectRestURL + "/" + projectId + "/invite-project-manager", inviteResource, Void.class);
+    }
+}
