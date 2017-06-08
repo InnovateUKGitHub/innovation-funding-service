a016b74f5dbf91beda1a8197cc328d926f5edd89
Matthew Bentley <mbentley@worth.systems>
2017-06-08 16:38:16 +0100

    IFS-262 - Javascript copy code added

--- ifs-web-service/ifs-web-core/src/main/resources/static/js/ifs_modules/copyLink.js
+++ ifs-web-service/ifs-web-core/src/main/resources/static/js/ifs_modules/copyLink.js
@@ -7,14 +7,36 @@ IFS.core.copyLink = (function () {
     },
     init: function () {
       s = this.settings
-      jQuery('body').on('click', s.copyLinkEl, function (e) {
-        IFS.core.wordCount.copyLinkToClipboard()
-      })
+      if ((window.clipboardData && window.clipboardData.setData) || (document.queryCommandSupported && document.queryCommandSupported('copy'))) {
+        jQuery(s.copyLinkEl).attr({
+          'aria-hidden': null
+        }).show()
+        jQuery('body').on('click', s.copyLinkEl, function (e) {
+          e.preventDefault()
+          var text = jQuery(this).prev().text()
+          IFS.core.copyLink.copyToClipboard(text)
+        })
+      }
     },
-    copyLinkToClipboard: function (textarea) {
+    copyToClipboard: function (text) {
       s = this.settings
-      var url = jQuery(s.copyLinkEl).pre().text()
 
+      if (window.clipboardData && window.clipboardData.setData) {
+        // IE specific code path to prevent textarea being shown while dialog is visible.
+        return window.clipboardData.setData('Text', text)
+      } else if (document.queryCommandSupported && document.queryCommandSupported('copy')) {
+        var textArea = jQuery('<textarea></textarea>').addClass('copy-link-textarea').val(text)
+        jQuery('body').append(textArea)
+        textArea.select()
+        try {
+          return document.execCommand('copy')  // Security exception may be thrown by some browsers.
+        } catch (err) {
+          console.warn('Copy to clipboard failed.', err)
+          return false
+        } finally {
+          jQuery('.copy-link-textarea').remove()
+        }
+      }
     }
   }
 })()
