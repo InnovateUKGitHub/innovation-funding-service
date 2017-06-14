2d04973a1f5a8652c86865b6d5a6494f15bcd5c0
Tom Baldwin <tom@polymorphism.co.uk>
2017-06-14 13:32:55 +0100

    IFS-79 - Fixes guidance text and guidance row priorities.

--- ifs-data-service/src/main/resources/db/migration/V96_6_0__Change_competition_scope_guidance.sql
+++ ifs-data-service/src/main/resources/db/migration/V96_6_0__Change_competition_scope_guidance.sql
@@ -1,7 +1,7 @@
 -- Update all scope guidance (including template competitions)
 
 UPDATE form_input SET
-  guidance_answer='You should still assess this application even if you answer in the negative. Your answer should be based upon the following:'
+  guidance_answer='You should still assess this application even if you think that it is not in scope. Your answer should be based upon the following:'
 WHERE
   description='Feedback' AND
   guidance_title='Guidance for assessing scope' AND
@@ -9,7 +9,7 @@ WHERE
 
 UPDATE guidance_row SET
   subject='No',
-  priority=1
+  priority=0
 WHERE
   subject='NO' AND
   form_input_id IN (SELECT id FROM form_input WHERE
@@ -19,7 +19,7 @@ WHERE
 
 UPDATE guidance_row SET
   subject='Yes',
-  priority=0
+  priority=1
 WHERE
   subject='YES' AND
   form_input_id IN (SELECT id FROM form_input WHERE
