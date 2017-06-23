1f49dea68833ec7526f98b4ee374c07f875d591e
Adam Birse <adambirseipl@gmail.com>
2017-06-23 15:01:18 +0100

    Refactor services, security and permission rules into data-commons

--- ifs-data-layer/ifs-data-service/src/main/java/org/innovateuk/ifs/security/BasePermissionRules.java
+++ ifs-data-layer/ifs-data-service/src/main/java/org/innovateuk/ifs/security/BasePermissionRules.java
@@ -1,36 +1,20 @@
 package org.innovateuk.ifs.security;
 
-import java.util.List;
-
 import org.innovateuk.ifs.assessment.repository.AssessmentRepository;
-import org.innovateuk.ifs.project.domain.Project;
 import org.innovateuk.ifs.project.domain.ProjectUser;
 import org.innovateuk.ifs.project.repository.ProjectRepository;
 import org.innovateuk.ifs.project.repository.ProjectUserRepository;
-import org.innovateuk.ifs.user.domain.Organisation;
-import org.innovateuk.ifs.user.domain.ProcessRole;
-import org.innovateuk.ifs.user.domain.Role;
 import org.innovateuk.ifs.user.repository.OrganisationRepository;
-import org.innovateuk.ifs.user.repository.ProcessRoleRepository;
-import org.innovateuk.ifs.user.repository.RoleRepository;
-import org.innovateuk.ifs.user.repository.UserRepository;
-import org.innovateuk.ifs.user.resource.UserResource;
-
 import org.springframework.beans.factory.annotation.Autowired;
 
-import static org.innovateuk.ifs.invite.domain.ProjectParticipantRole.PROJECT_FINANCE_CONTACT;
-import static org.innovateuk.ifs.invite.domain.ProjectParticipantRole.PROJECT_MANAGER;
-import static org.innovateuk.ifs.invite.domain.ProjectParticipantRole.PROJECT_PARTNER;
-import static org.innovateuk.ifs.security.SecurityRuleUtil.checkProcessRole;
-import static org.innovateuk.ifs.user.resource.UserRoleType.*;
+import java.util.List;
+
+import static org.innovateuk.ifs.invite.domain.ProjectParticipantRole.*;
 
 /**
  * Base class to contain useful shorthand methods for the Permission rule subclasses
  */
-public abstract class BasePermissionRules {
-
-    @Autowired
-    protected ProcessRoleRepository processRoleRepository;
+public abstract class BasePermissionRules extends RootPermissionRules {
 
     @Autowired
     protected ProjectUserRepository projectUserRepository;
@@ -39,32 +23,10 @@ public abstract class BasePermissionRules {
     protected ProjectRepository projectRepository;
 
     @Autowired
-    protected RoleRepository roleRepository;
-
-    @Autowired
     protected OrganisationRepository organisationRepository;
 
     @Autowired
-    protected UserRepository userRepository;
-
-    @Autowired
     protected AssessmentRepository assessmentRepository;
-
-    protected boolean isMemberOfProjectTeam(long applicationId, UserResource user) {
-        return isLeadApplicant(applicationId, user) || isCollaborator(applicationId, user);
-    }
-
-    protected boolean isCollaborator(long applicationId, UserResource user) {
-        return checkProcessRole(user, applicationId, COLLABORATOR, processRoleRepository);
-    }
-
-    protected boolean isLeadApplicant(long applicationId, UserResource user) {
-        return checkProcessRole(user, applicationId, LEADAPPLICANT, processRoleRepository);
-    }
-
-    protected boolean isAssessor(long applicationId, UserResource user) {
-        return checkProcessRole(user, applicationId, ASSESSOR, processRoleRepository);
-    }
 
     protected boolean isPartner(long projectId, long userId) {
         List<ProjectUser> partnerProjectUser = projectUserRepository.findByProjectIdAndUserIdAndRole(projectId, userId, PROJECT_PARTNER);
@@ -79,17 +41,6 @@ public abstract class BasePermissionRules {
 
     protected boolean partnerBelongsToOrganisation(long projectId, long userId, long organisationId){
         ProjectUser partnerProjectUser = projectUserRepository.findOneByProjectIdAndUserIdAndOrganisationIdAndRole(projectId, userId, organisationId, PROJECT_PARTNER);
-        return partnerProjectUser != null;
-    }
-
-    protected boolean isLeadPartner(long projectId, long userId) {
-
-        Project project = projectRepository.findOne(projectId);
-        Role leadApplicantRole = roleRepository.findOneByName(LEADAPPLICANT.getName());
-        ProcessRole leadApplicantProcessRole = processRoleRepository.findOneByApplicationIdAndRoleId(project.getApplication().getId(), leadApplicantRole.getId());
-        Organisation leadOrganisation = organisationRepository.findOne(leadApplicantProcessRole.getOrganisationId());
-
-        ProjectUser partnerProjectUser = projectUserRepository.findOneByProjectIdAndUserIdAndOrganisationIdAndRole(projectId, userId, leadOrganisation.getId(), PROJECT_PARTNER);
         return partnerProjectUser != null;
     }
 
--- ifs-data-layer/ifs-data-service/src/main/java/org/innovateuk/ifs/transactional/BaseTransactionalService.java
+++ ifs-data-layer/ifs-data-service/src/main/java/org/innovateuk/ifs/transactional/BaseTransactionalService.java
@@ -13,23 +13,11 @@ import org.innovateuk.ifs.competition.repository.CompetitionRepository;
 import org.innovateuk.ifs.project.domain.Project;
 import org.innovateuk.ifs.project.repository.ProjectRepository;
 import org.innovateuk.ifs.user.domain.Organisation;
-import org.innovateuk.ifs.user.domain.ProcessRole;
-import org.innovateuk.ifs.user.domain.Role;
-import org.innovateuk.ifs.user.domain.User;
 import org.innovateuk.ifs.user.repository.OrganisationRepository;
-import org.innovateuk.ifs.user.repository.ProcessRoleRepository;
-import org.innovateuk.ifs.user.repository.RoleRepository;
-import org.innovateuk.ifs.user.repository.UserRepository;
-import org.innovateuk.ifs.user.resource.UserResource;
-import org.innovateuk.ifs.user.resource.UserRoleType;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
 import java.util.function.Supplier;
 
-import static org.innovateuk.ifs.commons.error.CommonErrors.forbiddenError;
 import static org.innovateuk.ifs.commons.error.CommonErrors.notFoundError;
 import static org.innovateuk.ifs.commons.error.CommonFailureKeys.COMPETITION_NOT_OPEN;
 import static org.innovateuk.ifs.commons.service.ServiceResult.serviceFailure;
@@ -41,17 +29,7 @@ import static org.innovateuk.ifs.util.EntityLookupCallbacks.find;
  * This class represents the base class for transactional services.  Method calls within this service will have
  * transaction boundaries provided to allow for safe atomic operations and persistence cascading.
  */
-@Transactional(readOnly = true)
-public abstract class BaseTransactionalService  {
-
-    @Autowired
-    protected ProcessRoleRepository processRoleRepository;
-
-    @Autowired
-    protected UserRepository userRepository;
-
-    @Autowired
-    protected RoleRepository roleRepository;
+public abstract class BaseTransactionalService extends RootTransactionalService {
 
     @Autowired
     protected CompetitionRepository competitionRepository;
@@ -74,17 +52,6 @@ public abstract class BaseTransactionalService  {
     @Autowired
     private QuestionRepository questionRepository;
 
-    protected Supplier<ServiceResult<ProcessRole>> processRole(Long processRoleId) {
-        return () -> getProcessRole(processRoleId);
-    }
-
-    protected ServiceResult<ProcessRole> getProcessRole(Long processRoleId) {
-        return find(processRoleRepository.findOne(processRoleId), notFoundError(ProcessRole.class, processRoleId));
-    }
-
-    protected ServiceResult<List<ProcessRole>> getProcessRoles(Long applicationId, UserRoleType roleType) {
-        return getRole(roleType).andOnSuccess(role -> find(processRoleRepository.findByApplicationIdAndRoleId(applicationId, role.getId()), notFoundError(ProcessRole.class, applicationId, role.getId())));
-    }
 
     protected Supplier<ServiceResult<Section>> section(final Long id) {
         return () -> getSection(id);
@@ -120,16 +87,9 @@ public abstract class BaseTransactionalService  {
     protected ServiceResult<Application> getApplication(final Long id) {
         return find(applicationRepository.findOne(id), notFoundError(Application.class, id));
     }
+
     protected ServiceResult<Section> getSection(final Long id) {
         return find(sectionRepository.findOne(id), notFoundError(Section.class, id));
-    }
-
-    protected Supplier<ServiceResult<User>> user(final Long id) {
-        return () -> getUser(id);
-    }
-
-    protected ServiceResult<User> getUser(final Long id) {
-        return find(userRepository.findOne(id), notFoundError(User.class, id));
     }
 
     protected Supplier<ServiceResult<Competition>> competition(final Long id) {
@@ -140,38 +100,12 @@ public abstract class BaseTransactionalService  {
         return find(competitionRepository.findOne(id), notFoundError(Competition.class, id));
     }
 
-    protected Supplier<ServiceResult<Role>> role(UserRoleType roleType) {
-        return () -> getRole(roleType);
-    }
-
-    protected Supplier<ServiceResult<Role>> role(String roleName) {
-        return () -> getRole(roleName);
-    }
-
-    protected ServiceResult<Role> getRole(UserRoleType roleType) {
-        return getRole(roleType.getName());
-    }
-
-    protected ServiceResult<Role> getRole(String roleName) {
-        return find(roleRepository.findOneByName(roleName), notFoundError(Role.class, roleName));
-    }
-
     protected Supplier<ServiceResult<Organisation>> organisation(Long id) {
         return () -> getOrganisation(id);
     }
 
     protected ServiceResult<Organisation> getOrganisation(Long id) {
         return find(organisationRepository.findOne(id), notFoundError(Organisation.class, id));
-    }
-
-    protected ServiceResult<User> getCurrentlyLoggedInUser() {
-        UserResource currentUser = (UserResource) SecurityContextHolder.getContext().getAuthentication().getDetails();
-
-        if (currentUser == null) {
-            return serviceFailure(forbiddenError());
-        }
-
-        return getUser(currentUser.getId());
     }
 
     protected Supplier<ServiceResult<Question>> question(Long questionId) {
