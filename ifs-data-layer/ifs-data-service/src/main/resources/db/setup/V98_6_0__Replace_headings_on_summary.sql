bb3e30a9bfa84fa9e6561fb826fbdc902ae2a75d
Reinaud van Rumpt <rvanrumpt@worth.systems>
2017-07-04 14:52:19 +0200

    IFS-630 Adding comments to the SQL scripts

--- ifs-data-layer/ifs-data-service/src/main/resources/db/setup/V98_6_0__Replace_headings_on_summary.sql
+++ ifs-data-layer/ifs-data-service/src/main/resources/db/setup/V98_6_0__Replace_headings_on_summary.sql
@@ -1,7 +1,9 @@
+/* Remove the headings from the competitions */
 UPDATE question q
 SET q.description = REPLACE (q.description, '<h2 class="heading-medium">Funding rules for this competition</h2>', '')
 WHERE q.section_id IN (SELECT id FROM section WHERE section_type = 'OVERVIEW_FINANCES');
 
+/* Update the competition templates, remove the description */
 UPDATE question q
 SET q.description = ''
 WHERE q.name IS NULL
