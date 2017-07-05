e07e5de1e8481bd6d4e1759290000cc43633dceb
Tom Baldwin <tom@polymorphism.co.uk>
2017-07-05 16:39:10 +0100

    IFS-319 - Moves getAssessorCountSummaryByCompetition() to ApplicationStatisticsRepository and adds unit tests. Fixes getAssessorCountSummaryByCompetition() to use the correct states. Removes unused AssessorStatistics and related classes.

--- ifs-data-layer/ifs-data-service/src/main/java/org/innovateuk/ifs/application/repository/ApplicationStatisticsRepository.java
+++ ifs-data-layer/ifs-data-service/src/main/java/org/innovateuk/ifs/application/repository/ApplicationStatisticsRepository.java
@@ -1,6 +1,7 @@
 package org.innovateuk.ifs.application.repository;
 
 import org.innovateuk.ifs.application.domain.ApplicationStatistics;
+import org.innovateuk.ifs.application.resource.AssessorCountSummaryResource;
 import org.innovateuk.ifs.workflow.resource.State;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -29,4 +30,37 @@ public interface ApplicationStatisticsRepository extends PagingAndSortingReposit
                                                                                            @Param("states") Collection<State> applicationStates,
                                                                                            @Param("filter") String filter,
                                                                                            Pageable pageable);
+
+
+    // TODO pass in the states as enum sets from the service, rather than hardcoding strings
+    String REJECTED_AND_SUBMITTED_STATES_STRING = "(org.innovateuk.ifs.workflow.resource.State.REJECTED,org.innovateuk.ifs.workflow.resource.State.WITHDRAWN, org.innovateuk.ifs.workflow.resource.State.SUBMITTED)";
+    String NOT_ACCEPTED_OR_SUBMITTED_STATES_STRING = "(org.innovateuk.ifs.workflow.resource.State.PENDING,org.innovateuk.ifs.workflow.resource.State.REJECTED," +
+            "org.innovateuk.ifs.workflow.resource.State.WITHDRAWN,org.innovateuk.ifs.workflow.resource.State.CREATED,org.innovateuk.ifs.workflow.resource.State.SUBMITTED)";
+    String SUBMITTED_STATES_STRING = "(org.innovateuk.ifs.workflow.resource.State.SUBMITTED)";
+
+    @Query("SELECT NEW org.innovateuk.ifs.application.resource.AssessorCountSummaryResource(" +
+            "  user.id, " +
+            "  concat(user.firstName, ' ', user.lastName), " +
+            "  profile.skillsAreas, " +
+            "  sum(case when activityState.state NOT IN " + REJECTED_AND_SUBMITTED_STATES_STRING + " THEN 1 ELSE 0 END), " + // total assigned
+            "  sum(case when competitionParticipant.competition.id = :compId AND activityState.state NOT IN " + REJECTED_AND_SUBMITTED_STATES_STRING + " THEN 1 ELSE 0 END), " + // assigned
+            "  sum(case when competitionParticipant.competition.id = :compId AND activityState.state NOT IN " + NOT_ACCEPTED_OR_SUBMITTED_STATES_STRING + " THEN 1 ELSE 0 END), " + // accepted
+            "  sum(case when competitionParticipant.competition.id = :compId AND activityState.state     IN " + SUBMITTED_STATES_STRING    + " THEN 1 ELSE 0 END)  " +  // submitted
+            ") " +
+            "FROM User user " +
+            "JOIN CompetitionParticipant competitionParticipant ON competitionParticipant.user = user " +
+            "JOIN Profile profile ON profile.id = user.profileId " +
+            "LEFT JOIN Application application ON application.competition = competitionParticipant.competition  " + // AND application.applicationProcess.activityState.state IN :submittedStates " +
+            "LEFT JOIN ProcessRole processRole ON processRole.user = user AND processRole.applicationId = application.id " +
+            "LEFT JOIN Assessment assessment ON assessment.participant = processRole.id AND assessment.target = application " +
+            "LEFT JOIN ActivityState activityState ON assessment.activityState = activityState.id " +
+            "WHERE " +
+            "  competitionParticipant.status = org.innovateuk.ifs.invite.domain.ParticipantStatus.ACCEPTED " +
+            // TODO constraint to assessors only
+            "GROUP BY user, profile " + // TODO group by user should be enough
+            "HAVING sum(case when competitionParticipant.competition.id = :compId THEN 1 ELSE 0 END) > 0")
+    Page<AssessorCountSummaryResource> getAssessorCountSummaryByCompetition(@Param("compId") long competitionId,
+//                                                                            @Param("submittedStates") Collection<State> submittedStates, // do we need to filter ineligible?
+                                                                            Pageable pageable);
+
 }
