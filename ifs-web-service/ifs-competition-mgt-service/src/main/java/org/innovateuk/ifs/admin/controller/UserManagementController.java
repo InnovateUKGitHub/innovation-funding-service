3f4c7f16e086bcd0020d3a09bfc15ae9f042326c
Bronnyl Dias <bdias@worth.systems>
2017-07-06 15:08:46 +0100

    IFS-18-Edit-Internal-User-Profile
    Intial code.
    
    Change-Id: I0b5ebcd4ef77c0da2d4e1d1f465cbc2c28f7c4c0

--- ifs-web-service/ifs-competition-mgt-service/src/main/java/org/innovateuk/ifs/admin/controller/UserManagementController.java
+++ ifs-web-service/ifs-competition-mgt-service/src/main/java/org/innovateuk/ifs/admin/controller/UserManagementController.java
@@ -1,21 +1,29 @@
 package org.innovateuk.ifs.admin.controller;
 
+import org.innovateuk.ifs.admin.form.InviteUserForm;
 import org.innovateuk.ifs.admin.viewmodel.EditUserViewModel;
 import org.innovateuk.ifs.admin.viewmodel.UserListViewModel;
+import org.innovateuk.ifs.controller.ValidationHandler;
 import org.innovateuk.ifs.management.viewmodel.PaginationViewModel;
 import org.innovateuk.ifs.profile.service.ProfileRestService;
+import org.innovateuk.ifs.user.resource.UserResource;
 import org.innovateuk.ifs.user.service.UserRestService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
 import java.util.Objects;
+import java.util.function.Supplier;
 
 /**
  * This controller will handle all requests that are related to management of users by IFS Administrators.
@@ -28,6 +36,8 @@ public class UserManagementController {
     private static final String DEFAULT_PAGE_NUMBER = "0";
 
     private static final String DEFAULT_PAGE_SIZE = "40";
+
+    private static final String FORM_ATTR_NAME = "form";
 
     @Autowired
     private UserRestService userRestService;
@@ -74,4 +84,34 @@ public class UserManagementController {
                     return "admin/user";
                 })).getSuccessObjectOrThrowException();
     }
+
+    @GetMapping("/user/{userId}/edit")
+    public String viewEditUser(Model model,
+                             HttpServletRequest request,
+                             UserResource loggedInUser) {
+
+        return viewEditUser(model);
+    }
+
+    private String viewEditUser(Model model) {
+        InviteUserForm form = new InviteUserForm();
+        model.addAttribute(FORM_ATTR_NAME, form);
+
+        return "admin/edit-user";
+
+    }
+
+    @PostMapping("/user/{userId}/edit")
+    public String updateUser(@PathVariable Long userId,
+                             Model model,
+                             HttpServletRequest request,
+                             @Valid @ModelAttribute(FORM_ATTR_NAME) InviteUserForm form,
+                             @SuppressWarnings("unused") BindingResult bindingResult, ValidationHandler validationHandler,
+                             UserResource loggedInUser) {
+
+        Supplier<String> failureView = () -> "admin/user/" + userId + "/edit";
+
+        return "redirect:/admin/users/active";
+    }
+
 }
