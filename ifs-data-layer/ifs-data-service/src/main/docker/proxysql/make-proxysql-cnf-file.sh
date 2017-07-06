93a73afa7c79d9bf9d518a985e6f570aab6e9db9
Duncan Watson <duncan@hiveit.co.uk>
2017-07-06 11:02:51 +0100

    Cleaning up scripts

--- ifs-data-layer/ifs-data-service/src/main/docker/proxysql/make-proxysql-cnf-file.sh
+++ ifs-data-layer/ifs-data-service/src/main/docker/proxysql/make-proxysql-cnf-file.sh
@@ -2,46 +2,75 @@
 
 set -e
 
-sed -i "s#<<DB_USER>>#$DB_USER#g;s#<<DB_PASS>>#$DB_PASS#g;s#<<DB_NAME>>#$DB_NAME#g;s#<<DB_HOST>>#$DB_HOST#g;s#<<DB_PORT>>#$DB_PORT#g" /etc/proxysql.cnf
+# A function to generate a set of query_rules for proxysql to rewrite data as it is being selected by mysqldump.
+# This takes rules defined in files in the /dump/rewrites folder and builds a set of proxysql configuration to apply
+# those rewrites.  These rules will be written to /dump/query_rules
+function generate_query_rules_for_proxysql() {
 
-files=(/dump/rewrites/*)
+    files=(/dump/rewrites/*)
 
-for i in "${files[@]}"
-do
-    table_name=$(echo $i | sed "s/.*\///" | sed "s/.*\///" | sed "s/\..*//")
+    # for each file within the /dump/rewrites folder
+    for i in "${files[@]}"; do
 
-    field_array=( $(cut -d '|' -f1 $i) )
-	mask_array=( $(cut -d '|' -f2 $i) )
+        # the table name is the name of the file e.g. "user"
+        table_name=$(echo $i | sed "s/.*\///" | sed "s/.*\///" | sed "s/\..*//")
 
-    full_select_statement_query="SELECT CONCAT('SELECT SQL_NO_CACHE ', ( SELECT GROUP_CONCAT(t.col) FROM (SELECT COLUMN_NAME as col FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = '$table_name' AND TABLE_SCHEMA = '$DB_NAME') t WHERE t.col IS NOT NULL) , ' FROM $table_name' );"
+        # the column_array is an array of the column names that are rewrite candidates for this table e.g. "first_name"
+        column_array=( $(cut -d '|' -f1 $i) )
 
-    full_select_statement_result=$(mysql -h$DB_HOST -u$DB_USER -p$DB_PASS -P$DB_PORT $DB_NAME -N -s -e "$full_select_statement_query")
+        # the column_rewrite_array is an array of the rewrite rules against each corresponding column name in the column_array array e.g. "CONCAT(first_name, 'XXX')"
+        column_rewrite_array=( $(cut -d '|' -f2 $i) )
 
-    replacement_pattern=$full_select_statement_result
+        # a query to find the base select statement that we wish mysqldump to issue against this table when running a dump of its data
+        full_select_statement_query="SELECT CONCAT('SELECT SQL_NO_CACHE ', \
+            (SELECT GROUP_CONCAT(t.col) FROM \
+               (SELECT COLUMN_NAME as col FROM INFORMATION_SCHEMA.COLUMNS \
+                WHERE TABLE_NAME = '$table_name' \
+                AND TABLE_SCHEMA = '$DB_NAME') t \
+             WHERE t.col IS NOT NULL) , ' FROM $table_name' );"
 
-    for j in "${!field_array[@]}"
-    do
-        replacement_pattern=$( echo $replacement_pattern | sed s/${field_array[j]}/${mask_array[j]}/ )
+        # the base select statement that we wish mysqldump to issue against this table when running a dump of its data
+        full_select_statement_result=$(mysql -h$DB_HOST -u$DB_USER -p$DB_PASS -P$DB_PORT $DB_NAME -N -s -e "$full_select_statement_query")
+
+        # now we replace every column name that we wish to replace with its replacement i.e. replace every entry from column_array (e.g. "user") with its rewrite from column_rewrite_array (e.g. "CONCAT(first_name, 'XXX')")
+        replacement_pattern=$full_select_statement_result
+        for j in "${!column_array[@]}"; do
+            replacement_pattern=$( echo $replacement_pattern | sed s/${column_array[j]}/${column_rewrite_array[j]}/ )
+        done
+
+        # and finally output this table's rewrite rule to /dump/query_rules
+        if [ "$((j))" -gt "1" ]; then
+           echo '    ,' >> /dump/query_rules
+        fi
+
+        rule_id=$j
+        echo '    {' >> /dump/query_rules
+        echo "        rule_id=$rule_id" >> /dump/query_rules
+        echo '        active=1' >> /dump/query_rules
+        match_pattern="^SELECT /\*!40001 SQL_NO_CACHE \*/ \* FROM \`$table_name\`"
+        echo "        match_pattern=\"$match_pattern\"" >> /dump/query_rules
+        echo "        replace_pattern=\"$replacement_pattern\"" >> /dump/query_rules
+        echo '        destination_hostgroup=0' >> /dump/query_rules
+        echo '        apply=1' >> /dump/query_rules
+        echo '    }' >> /dump/query_rules
+
     done
+}
 
-    if [ "$((j))" -gt "1" ]; then
-       echo '    ,' >> /dump/query_rules
-    fi
+# a function to replace the <<QUERY_RULES>> token in proxysql.cnf with the rewrite rules stored within /dump/query_rules
+function inject_query_rules_into_proxysql_cnf() {
+    sed -i "/<<QUERY_RULES>>/{
+        s/<<QUERY_RULES>>//g
+        r /dump/query_rules
+    }" /etc/proxysql.cnf
+}
 
-    rule_id=$j
-    echo '    {' >> /dump/query_rules
-    echo "        rule_id=$rule_id" >> /dump/query_rules
-    echo '        active=1' >> /dump/query_rules
-    match_pattern="^SELECT /\*!40001 SQL_NO_CACHE \*/ \* FROM \`$table_name\`"
-    echo "        match_pattern=\"$match_pattern\"" >> /dump/query_rules
-    echo "        replace_pattern=\"$replacement_pattern\"" >> /dump/query_rules
-    echo '        destination_hostgroup=0' >> /dump/query_rules
-    echo '        apply=1' >> /dump/query_rules
-    echo '    }' >> /dump/query_rules
+# a function to replace database configuration replacement tokens in proxysql.cnf with real values
+function inject_db_configuration_into_proxysql_cnf() {
+    sed -i "s#<<DB_USER>>#$DB_USER#g;s#<<DB_PASS>>#$DB_PASS#g;s#<<DB_NAME>>#$DB_NAME#g;s#<<DB_HOST>>#$DB_HOST#g;s#<<DB_PORT>>#$DB_PORT#g" /etc/proxysql.cnf
+}
 
-done
-
-sed -i "/<<QUERY_RULES>>/{
-    s/<<QUERY_RULES>>//g
-    r /dump/query_rules
-}" /etc/proxysql.cnf
+# the entrypoint into this script
+generate_query_rules_for_proxysql
+inject_query_rules_into_proxysql_cnf
+inject_db_configuration_into_proxysql_cnf
--- setup-files/scripts/openshift/init-local-cluster.sh
+++ setup-files/scripts/openshift/init-local-cluster.sh
@@ -1,5 +1,12 @@
+#!/bin/bash
+
+# (Re)start the local OpenShift cluster
 oc cluster down
 oc cluster up
+
+# give additional rights to the "developer" user
 oc login -u system:admin
 oc policy add-role-to-user admin developer -n default
+
+# and login as the developer user
 oc login -u=developer -p=developer
