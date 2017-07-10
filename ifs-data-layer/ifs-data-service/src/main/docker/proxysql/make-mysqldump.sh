3af240212248b8f2e5ff07541f78cb942223daff
Duncan Watson <duncan@hiveit.co.uk>
2017-07-10 17:02:41 +0100

    IFS-335 - yet more data for set

--- ifs-data-layer/ifs-data-service/src/main/docker/proxysql/make-mysqldump.sh
+++ ifs-data-layer/ifs-data-service/src/main/docker/proxysql/make-mysqldump.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
+# TODO DW - password-protect the gz file
+
 mysqldump -u$DB_USER -p$DB_PASS -h127.0.0.1 -P6033 $DB_NAME --skip-extended-insert | gzip -c > /dump/anonymised-dump.sql.gz
