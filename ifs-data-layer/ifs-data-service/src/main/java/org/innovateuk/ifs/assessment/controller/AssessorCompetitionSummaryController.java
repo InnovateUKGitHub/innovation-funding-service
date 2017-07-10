4285f5d77354826045082d88c8dc36d6ef2dae2e
Nicholas Tsim <nick@hiveit.co.uk>
2017-07-10 17:36:51 +0100

    IFS-156 - Add `RestController` annotation to `AssessorCompetitionSummaryController`

--- ifs-data-layer/ifs-data-service/src/main/java/org/innovateuk/ifs/assessment/controller/AssessorCompetitionSummaryController.java
+++ ifs-data-layer/ifs-data-service/src/main/java/org/innovateuk/ifs/assessment/controller/AssessorCompetitionSummaryController.java
@@ -4,16 +4,16 @@ import org.innovateuk.ifs.assessment.resource.AssessorCompetitionSummaryResource
 import org.innovateuk.ifs.assessment.transactional.AssessorCompetitionSummaryService;
 import org.innovateuk.ifs.commons.rest.RestResult;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Exposes endpoints for retrieving assessor summary resources
  * for specific competitions.
  */
-@Controller
+@RestController
 @RequestMapping("/assessor/{assessorId}/competition/{competitionId}")
 public class AssessorCompetitionSummaryController {
 
