df7d365b862d01483fce60940de1c341a6e82a4e
Tom Baldwin <tom@polymorphism.co.uk>
2017-07-11 15:35:10 +0100

    IFS-319 - Fixes typos in Application/AssessourCountSummaryServices security description

--- ifs-data-layer/ifs-data-service/src/main/java/org/innovateuk/ifs/application/transactional/AssessorCountSummaryService.java
+++ ifs-data-layer/ifs-data-service/src/main/java/org/innovateuk/ifs/application/transactional/AssessorCountSummaryService.java
@@ -6,15 +6,13 @@ import org.innovateuk.ifs.commons.security.SecuredBySpring;
 import org.innovateuk.ifs.commons.service.ServiceResult;
 import org.springframework.security.access.prepost.PreAuthorize;
 
-import java.util.Optional;
-
 /**
  * Service for retrieving statistics about assessor assessments
  */
 public interface AssessorCountSummaryService {
 
     @PreAuthorize("hasAnyAuthority('comp_admin', 'project_finance')")
-    @SecuredBySpring(value = "READ", description = "Comp Admins can see all Assessor Summary counts accros the whole system", securedType = AssessorCountSummaryResource.class)
+    @SecuredBySpring(value = "READ", description = "Comp Admins can see all Assessor Summary counts across the whole system", securedType = AssessorCountSummaryResource.class)
     ServiceResult<AssessorCountSummaryPageResource> getAssessorCountSummariesByCompetitionId(long competitionId, int pageIndex, int pageSize);
 }
 
