d8ce50afea34f576fddd2422a23419caa2377101
Brent de Kok <brent@worth.systems>
2017-07-14 15:24:46 +0200

    IFS-941 fix bug when multiple fields in one form group.

--- ifs-web-service/ifs-web-core/src/main/resources/static/js/ifs_modules/formValidation.js
+++ ifs-web-service/ifs-web-core/src/main/resources/static/js/ifs_modules/formValidation.js
@@ -646,9 +646,13 @@ IFS.core.formValidation = (function () {
         // server side remove in form group
         formGroup.find('.error-message:not([data-errorfield]):contains("' + message + '")').first().remove()
 
-        // if this was the last error we remove the error styling
+        // if this was the last error we remove the error styling on the form group
         if (formGroup.find('[data-errorfield],.error-message:not([data-errorfield])').length === 0) {
           formGroup.removeClass('form-group-error')
+        }
+        // in certain situations a form group can have multiple fields so we check if this is the last error of the field by checking the errorfield name.
+        // Also if there er no server side errors anymore we set the field valid.
+        if (formGroup.find('[data-errorfield="' + name + '"],.error-message:not([data-errorfield])').length === 0) {
           field.removeClass('form-control-error')
           if (s.html5validationMode) {
             // looping through the name attribute for setting checkboxes and radios valid
