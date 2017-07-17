8fc774955f763f43f48c5edf92746a2808f9e860
Nicholas Tsim <nick@hiveit.co.uk>
2017-07-17 15:18:42 +0100

    IFS-156 - Add `state` property to assignment row view model and associated template

--- ifs-web-service/ifs-competition-mgt-service/src/main/java/org/innovateuk/ifs/management/model/AssessorAssessmentProgressModelPopulator.java
+++ ifs-web-service/ifs-competition-mgt-service/src/main/java/org/innovateuk/ifs/management/model/AssessorAssessmentProgressModelPopulator.java
@@ -35,7 +35,8 @@ public class AssessorAssessmentProgressModelPopulator {
                         assignedAssessment.getApplicationId(),
                         assignedAssessment.getApplicationName(),
                         assignedAssessment.getLeadOrganisation(),
-                        assignedAssessment.getTotalAssessors()
+                        assignedAssessment.getTotalAssessors(),
+                        assignedAssessment.getState()
                 )
         );
 
--- ifs-web-service/ifs-competition-mgt-service/src/main/java/org/innovateuk/ifs/management/viewmodel/AssessorAssessmentProgressAssignedRowViewModel.java
+++ ifs-web-service/ifs-competition-mgt-service/src/main/java/org/innovateuk/ifs/management/viewmodel/AssessorAssessmentProgressAssignedRowViewModel.java
@@ -1,20 +1,25 @@
 package org.innovateuk.ifs.management.viewmodel;
 
+import org.innovateuk.ifs.assessment.resource.AssessmentStates;
+
 public class AssessorAssessmentProgressAssignedRowViewModel {
 
     private final long applicationId;
     private final String applicationName;
     private final String leadOrganisation;
     private final int totalAssessors;
+    private AssessmentStates state;
 
     public AssessorAssessmentProgressAssignedRowViewModel(long applicationId,
                                                           String applicationName,
                                                           String leadOrganisation,
-                                                          int totalAssessors) {
+                                                          int totalAssessors,
+                                                          AssessmentStates state) {
         this.applicationId = applicationId;
         this.applicationName = applicationName;
         this.leadOrganisation = leadOrganisation;
         this.totalAssessors = totalAssessors;
+        this.state = state;
     }
 
     public long getApplicationId() {
@@ -31,5 +36,9 @@ public class AssessorAssessmentProgressAssignedRowViewModel {
 
     public int getTotalAssessors() {
         return totalAssessors;
+    }
+
+    public AssessmentStates getState() {
+        return state;
     }
 }
