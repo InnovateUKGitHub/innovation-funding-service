cbd7009e957d093dff0fd09a582c5772aeb20aad
Reinaud van Rumpt <rvanrumpt@worth.systems>
2017-07-18 10:26:18 +0100

    IFS-747 Changing webtest fix

--- ifs-data-layer/ifs-data-service/src/main/resources/db/webtest/V98_9_3__Set_competition_template_ids.sql
+++ ifs-data-layer/ifs-data-service/src/main/resources/db/webtest/V98_9_3__Set_competition_template_ids.sql
@@ -1,2 +1,3 @@
 update competition_type set template_competition_id = 2 where name = 'Programme';
 update competition_type set template_competition_id = 3 where name = 'Sector';
+update competition_type set template_competition_id = 4 where name = 'Generic';
