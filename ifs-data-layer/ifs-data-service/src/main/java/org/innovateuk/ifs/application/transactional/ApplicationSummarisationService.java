1fd836264c4c878297e382947a1898904f903fa6
Ravish Bhagdev <rav@hiveit.co.uk>
2017-07-21 15:18:07 +0100

    IFS-1028 Allow support user to see application summary totals

--- ifs-data-layer/ifs-data-service/src/main/java/org/innovateuk/ifs/application/transactional/ApplicationSummarisationService.java
+++ ifs-data-layer/ifs-data-service/src/main/java/org/innovateuk/ifs/application/transactional/ApplicationSummarisationService.java
@@ -10,10 +10,10 @@ import java.math.BigDecimal;
 public interface ApplicationSummarisationService {
 
 	@SecuredBySpring(value = "READ", description = "Only those with either comp admin or project finance roles can read total project costs for an application")
-	@PreAuthorize("hasAnyAuthority('comp_admin' , 'project_finance', 'innovation_lead')")
+	@PreAuthorize("hasAnyAuthority('comp_admin' , 'project_finance', 'support', 'innovation_lead')")
 	ServiceResult<BigDecimal> getTotalProjectCost(Application application);
 
 	@SecuredBySpring(value = "READ", description = "Only those with either comp admin or project finance roles can read funding sought for an application")
-	@PreAuthorize("hasAnyAuthority('comp_admin' , 'project_finance', 'innovation_lead')")
+	@PreAuthorize("hasAnyAuthority('comp_admin' , 'project_finance', 'support', 'innovation_lead')")
 	ServiceResult<BigDecimal> getFundingSought(Application application);
 }
