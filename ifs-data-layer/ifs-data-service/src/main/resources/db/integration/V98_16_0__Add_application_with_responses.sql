52df39f2a781f378747802357d3b635a332fd11b
Reinaud van Rumpt <rvanrumpt@worth.systems>
2017-07-28 11:37:32 +0200

    IFS-1064 Improving unit tests

--- ifs-data-layer/ifs-data-service/src/main/resources/db/integration/V98_16_0__Add_application_with_responses.sql
+++ ifs-data-layer/ifs-data-service/src/main/resources/db/integration/V98_16_0__Add_application_with_responses.sql
@@ -1,2 +1,34 @@
-UPDATE milestone SET date = '2036-03-15 09:00:00' WHERE type = 'NOTIFICATIONS' AND competition_id = 1;
-UPDATE milestone SET date = NULL WHERE type = 'LINE_DRAW' AND competition_id = 1;
+SET @existingApplicationId =1;
+SET @newApplicationId =7;
+
+-- Insert application (same as application.id 1)
+INSERT INTO application(id, duration_in_months, name, start_date, competition, completion)
+SELECT @newApplicationId, duration_in_months, name, start_date, competition, completion
+FROM application
+where id = @existingApplicationId;
+
+-- Insert add application begin state (CREATED)
+INSERT INTO process(last_modified, process_type, target_id, activity_state_id, version)
+SELECT last_modified, process_type, @newApplicationId, activity_state_id, version
+FROM process
+where target_id = @existingApplicationId
+and process_type = "ApplicationProcess";
+
+-- Insert process_roles (same as application.id 1)
+INSERT INTO process_role(application_id, organisation_id, role_id, user_id)
+SELECT @newApplicationId, organisation_id, role_id, user_id
+FROM process_role
+where application_id = @existingApplicationId;
+
+-- Insert question statuses to this application
+INSERT INTO question_status(assigned_date, marked_as_complete, application_id, marked_as_complete_by_id, question_id)
+SELECT NOW(), 1, @newApplicationId, pr.id, q.id
+FROM process_role pr LEFT JOIN question q on q.competition_id = 1
+WHERE pr.application_id =@newApplicationId;
+
+-- Insert form input responses to this application
+INSERT INTO form_input_response(update_date, value, form_input_id, updated_by_id, application_id)
+SELECT NOW(), "1", fi.id, pr.id, @newApplicationId
+FROM process_role pr LEFT JOIN form_input fi on fi.competition_id = 1
+WHERE pr.application_id =@newApplicationId;
+
--- ifs-data-layer/ifs-data-service/src/test/java/org/innovateuk/ifs/affiliation/controller/AffiliationControllerIntegrationTest.java
+++ ifs-data-layer/ifs-data-service/src/test/java/org/innovateuk/ifs/affiliation/controller/AffiliationControllerIntegrationTest.java
@@ -3,11 +3,13 @@ package org.innovateuk.ifs.affiliation.controller;
 import org.innovateuk.ifs.BaseControllerIntegrationTest;
 import org.innovateuk.ifs.commons.rest.RestResult;
 import org.innovateuk.ifs.user.domain.User;
+import org.innovateuk.ifs.user.mapper.UserMapper;
 import org.innovateuk.ifs.user.repository.UserRepository;
 import org.innovateuk.ifs.user.resource.AffiliationResource;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static java.lang.Boolean.TRUE;
@@ -28,11 +30,14 @@ public class AffiliationControllerIntegrationTest extends BaseControllerIntegrat
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private UserMapper userMapper;
+
     @Test
     public void testGetUserAffiliations() throws Exception {
         loginPaulPlum();
 
-        User user = userRepository.findOne(getPaulPlum().getId());
+        User user = userMapper.mapToDomain(getPaulPlum());
         Long userId = user.getId();
 
         // Save some existing Affiliations
