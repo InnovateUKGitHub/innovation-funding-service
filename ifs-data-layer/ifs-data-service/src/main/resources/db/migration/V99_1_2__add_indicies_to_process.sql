cb6d1d3108660c60f1a08ea0ab0fdcb9f6bebaab
Tom Baldwin <tom@polymorphism.co.uk>
2017-08-02 14:40:27 +0100

    IFS-1119 - Renames process indexes.

--- ifs-data-layer/ifs-data-service/src/main/resources/db/migration/V99_1_2__add_indicies_to_process.sql
+++ ifs-data-layer/ifs-data-service/src/main/resources/db/migration/V99_1_2__add_indicies_to_process.sql
@@ -1,2 +1,2 @@
-CREATE INDEX process_type_ixd ON process(process_type);
-CREATE INDEX participant_id_ix ON process(participant_id);
+CREATE INDEX process_process_type_idx ON process(process_type);
+CREATE INDEX process_participant_id_idx ON process(participant_id);
