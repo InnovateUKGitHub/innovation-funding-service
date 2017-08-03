c6b71676031e1708f18b47a43e5a16da92900344
Ravish Bhagdev <rav@hiveit.co.uk>
2017-08-03 10:01:00 +0100

    Index on fk removed

--- ifs-data-layer/ifs-data-service/src/main/resources/db/migration/V99_1_4__add_application_fk_to_project.sql
+++ ifs-data-layer/ifs-data-service/src/main/resources/db/migration/V99_1_4__add_application_fk_to_project.sql
@@ -1,7 +1,4 @@
-ALTER TABLE project 
-ADD INDEX project_application_fk_idx (application_id ASC);
-
-ALTER TABLE project 
+ALTER TABLE project
 ADD CONSTRAINT project_application_fk
   FOREIGN KEY (application_id)
   REFERENCES application (id)
