1d1dd2f703753de25ee379050d6f247e7ff9d2f4
Reinaud van Rumpt <rvanrumpt@worth.systems>
2017-08-08 11:46:50 +0200

    IFS-1015 WIP Checking if the user is the eligible to be the lead applicant

--- ifs-web-service/ifs-application-service/src/main/java/org/innovateuk/ifs/application/creation/controller/ApplicationCreationAuthenticatedController.java
+++ ifs-web-service/ifs-application-service/src/main/java/org/innovateuk/ifs/application/creation/controller/ApplicationCreationAuthenticatedController.java
@@ -1,17 +1,22 @@
 package org.innovateuk.ifs.application.creation.controller;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.innovateuk.ifs.application.resource.ApplicationResource;
 import org.innovateuk.ifs.application.service.ApplicationService;
+import org.innovateuk.ifs.application.service.CompetitionService;
+import org.innovateuk.ifs.application.service.OrganisationService;
 import org.innovateuk.ifs.commons.error.exception.ObjectNotFoundException;
+import org.innovateuk.ifs.competition.resource.CompetitionResource;
+import org.innovateuk.ifs.user.resource.OrganisationResource;
 import org.innovateuk.ifs.user.resource.UserResource;
 import org.innovateuk.ifs.user.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
@@ -30,18 +35,29 @@ public class ApplicationCreationAuthenticatedController {
     public static final String COMPETITION_ID = "competitionId";
     public static final String RADIO_TRUE = "true";
     public static final String RADIO_FALSE = "false";
-    private static final Log LOG = LogFactory.getLog(ApplicationCreationAuthenticatedController.class);
     public static final String FORM_RADIO_NAME = "create-application";
+
     @Autowired
     protected ApplicationService applicationService;
+
+    @Autowired
+    protected CompetitionService competitionService;
+
+    @Autowired
+    protected OrganisationService organisationService;
+
     @Autowired
     protected UserService userService;
 
     @GetMapping("/{competitionId}")
     public String view(Model model,
                        @PathVariable(COMPETITION_ID) Long competitionId,
-                       UserResource user,
-                       HttpServletRequest request) {
+                       UserResource user) {
+
+        if(!isAllowedToLeadApplication(user.getId(), competitionId)) {
+            //TODO: if not redirect
+            return "redirect:/not-allowed";
+        }
 
         Boolean userHasApplication = userService.userHasApplicationForCompetition(user.getId(), competitionId);
         if (Boolean.TRUE.equals(userHasApplication)) {
@@ -52,11 +68,16 @@ public class ApplicationCreationAuthenticatedController {
         }
     }
 
-    @PostMapping("/{competitionId}")
+    @PostMapping(value = "/{competitionId}", params = {FORM_RADIO_NAME})
     public String post(Model model,
                        @PathVariable(COMPETITION_ID) Long competitionId,
                        UserResource user,
                        HttpServletRequest request) {
+        if(!isAllowedToLeadApplication(user.getId(), competitionId)) {
+            //TODO: if not redirect
+            return "redirect:/not-allowed";
+        }
+
         String createNewApplication = request.getParameter(FORM_RADIO_NAME);
 
         if (RADIO_TRUE.equals(createNewApplication)) {
@@ -82,5 +103,12 @@ public class ApplicationCreationAuthenticatedController {
             args.add(user.getId());
             throw new ObjectNotFoundException("Could not create a new application", args);
         }
+    }
+
+    private boolean isAllowedToLeadApplication(Long userId, Long competitionId) {
+        OrganisationResource organisation = organisationService.getOrganisationForUser(userId);
+        CompetitionResource competition = competitionService.getById(competitionId);
+
+        return competition.getLeadApplicantTypes().contains(organisation.getOrganisationType());
     }
 }
