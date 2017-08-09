3ce01984979e8767c4d7fbd0b6950a55ce30d0e5
glenn <gauld@worth.systems>
2017-08-09 10:25:33 +0100

    IFS-1079 Initial commit.

--- ifs-web-service/ifs-competition-mgt-service/src/main/java/org/innovateuk/ifs/management/controller/CompetitionManagementAssessmentsAssessorProgressController.java
+++ ifs-web-service/ifs-competition-mgt-service/src/main/java/org/innovateuk/ifs/management/controller/CompetitionManagementAssessmentsAssessorProgressController.java
@@ -1,19 +1,20 @@
 package org.innovateuk.ifs.management.controller;
 
+import org.innovateuk.ifs.assessment.service.AssessmentRestService;
+import org.innovateuk.ifs.competition.resource.AvailableAssessorsSortFieldType;
 import org.innovateuk.ifs.management.model.AssessorAssessmentProgressModelPopulator;
 import org.innovateuk.ifs.management.service.CompetitionManagementApplicationServiceImpl.ApplicationOverviewOrigin;
 import org.innovateuk.ifs.management.service.CompetitionManagementApplicationServiceImpl;
+import org.innovateuk.ifs.management.viewmodel.ApplicationAssessmentProgressRemoveViewModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.MultiValueMap;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
+import static java.lang.String.format;
 import static org.innovateuk.ifs.util.BackLinkUtil.buildOriginQueryString;
 
 @Controller
@@ -22,6 +23,9 @@ public class CompetitionManagementAssessmentsAssessorProgressController {
 
     @Autowired
     private AssessorAssessmentProgressModelPopulator assessorAssessmentProgressModelPopulator;
+
+    @Autowired
+    private AssessmentRestService assessmentRestService;
 
     @GetMapping("/{assessorId}")
     public String assessorProgress(@PathVariable("competitionId") long competitionId,
@@ -38,4 +42,31 @@ public class CompetitionManagementAssessmentsAssessorProgressController {
 
         return "competition/assessor-progress";
     }
+
+    @PostMapping("/{assessorId}/withdraw/{assessmentId}")
+    public String withdrawAssessment(@PathVariable("competitionId") long competitionId,
+                                     @PathVariable("assessorId") long assessorId,
+                                     @PathVariable("assessmentId") long assessmentId,
+                                     @RequestParam(value = "sortField", defaultValue = "") String sortField) {
+        assessmentRestService.withdrawAssessment(assessmentId).getSuccessObjectOrThrowException();
+        return format("redirect:/assessment/competition/%s/assessors/%s?sortField=%s", competitionId, assessorId, sortField);
+    }
+
+    @GetMapping(value = "/{assessorId}/withdraw/{assessmentId}/confirm")
+    public String withdrawAssessmentConfirm(
+            Model model,
+            @PathVariable("competitionId") Long competitionId,
+            @PathVariable("assessorId") long assessorId,
+            @PathVariable("applicationId") Long applicationId,
+            @PathVariable("assessmentId") Long assessmentId,
+            @RequestParam(value = "sortField", defaultValue = "TITLE") String sortField) {
+        model.addAttribute("model", new ApplicationAssessmentProgressRemoveViewModel(
+                competitionId,
+                applicationId,
+                assessmentId,
+                AvailableAssessorsSortFieldType.valueOf(sortField)
+        ));
+        return "competition/application-progress-remove-confirm";
+    }
+
 }
