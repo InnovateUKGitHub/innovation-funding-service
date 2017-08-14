4ba28b2b153625186f5730472c67d8b931cc5b71
Wouter de Meijer <wdemeijer@worth.systems>
2017-08-14 15:19:45 +0200

    IFS-1013 - Fixed ApplicationCreationControllerTest

--- ifs-web-service/ifs-application-service/src/test/java/org/innovateuk/ifs/application/creation/controller/ApplicationCreationControllerTest.java
+++ ifs-web-service/ifs-application-service/src/test/java/org/innovateuk/ifs/application/creation/controller/ApplicationCreationControllerTest.java
@@ -4,10 +4,12 @@ import org.innovateuk.ifs.BaseControllerMockMVCTest;
 import org.innovateuk.ifs.application.resource.ApplicationResource;
 import org.innovateuk.ifs.competition.publiccontent.resource.PublicContentItemResource;
 import org.innovateuk.ifs.organisation.resource.OrganisationSearchResult;
+import org.innovateuk.ifs.registration.service.RegistrationCookieService;
 import org.innovateuk.ifs.user.resource.OrganisationResource;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MvcResult;
@@ -17,15 +19,19 @@ import java.time.ZonedDateTime;
 import static org.innovateuk.ifs.application.builder.ApplicationResourceBuilder.newApplicationResource;
 import static org.innovateuk.ifs.publiccontent.builder.PublicContentItemResourceBuilder.newPublicContentItemResource;
 import static org.innovateuk.ifs.user.builder.OrganisationResourceBuilder.newOrganisationResource;
-import static org.junit.Assert.assertEquals;
-import static org.mockito.Matchers.*;
-import static org.mockito.Mockito.when;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyLong;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(MockitoJUnitRunner.class)
 @TestPropertySource(locations = "classpath:application.properties")
 public class ApplicationCreationControllerTest extends BaseControllerMockMVCTest<ApplicationCreationController> {
+
+    @Mock
+    private RegistrationCookieService registrationCookieService;
 
     @Override
     protected ApplicationCreationController supplyControllerUnderTest() {
@@ -63,11 +69,9 @@ public class ApplicationCreationControllerTest extends BaseControllerMockMVCTest
         MvcResult result = mockMvc.perform(get("/application/create/check-eligibility/{competitionId}", competitionId))
                 .andExpect(status().is2xxSuccessful())
                 .andExpect(view().name("create-application/check-eligibility"))
-                .andExpect(cookie().exists("competitionId"))
-                .andExpect(cookie().value("invite_hash", ""))
                 .andReturn();
 
-        assertEquals(String.valueOf(competitionId), getDecryptedCookieValue(result.getResponse().getCookies(), "competitionId"));
+        verify(registrationCookieService, times(1)).saveToCompetitionIdCookie(eq(competitionId), any());
     }
 
 
