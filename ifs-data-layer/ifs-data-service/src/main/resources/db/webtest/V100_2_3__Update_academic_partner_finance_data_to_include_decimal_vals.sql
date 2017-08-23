c817012c5a5d5f9726865703c6ff0fa9aeb9310c
Daniel Smith <danatinnov8@gmail.com>
2017-08-23 07:05:33 +0100

    IFS-637 IFS-1078 - remove email tables

---
+++ ifs-data-layer/ifs-data-service/src/main/resources/db/webtest/V100_2_3__Update_academic_partner_finance_data_to_include_decimal_vals.sql
@@ -0,0 +1,7 @@
+#
+# See my comments on INFUND-7348 for details about why this was done.
+# https://devops.innovateuk.org/issue-tracking/browse/INFUND-7348?focusedCommentId=21871&page=com.atlassian.jira.plugin.system.issuetabpanels:comment-tabpanel#comment-21871
+#
+update cost set value = 11.11 where id = 128;
+update cost set value = 247.66 where id = 174;
+update finance_row set cost = 11.99 where id = 490;
