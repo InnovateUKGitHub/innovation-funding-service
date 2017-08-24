b0ae354b030d3a7d1abebeb50e1bd38db617c2fd
Ravish Bhagdev <rav@hiveit.co.uk>
2017-08-24 12:06:32 +0100

    IFS-191 permissions fixed.

--- ifs-data-layer/ifs-data-service/src/main/java/org/innovateuk/ifs/application/security/CompetitionSummaryPermissionRules.java
+++ ifs-data-layer/ifs-data-service/src/main/java/org/innovateuk/ifs/application/security/CompetitionSummaryPermissionRules.java
@@ -2,6 +2,7 @@ package org.innovateuk.ifs.application.security;
 
 import org.innovateuk.ifs.commons.security.PermissionRule;
 import org.innovateuk.ifs.commons.security.PermissionRules;
+import org.innovateuk.ifs.competition.resource.CompetitionResource;
 import org.innovateuk.ifs.security.BasePermissionRules;
 import org.innovateuk.ifs.user.resource.UserResource;
 import org.springframework.stereotype.Component;
@@ -13,12 +14,12 @@ import static org.innovateuk.ifs.security.SecurityRuleUtil.isInternal;
 @PermissionRules
 public class CompetitionSummaryPermissionRules extends BasePermissionRules {
     @PermissionRule(value = "VIEW_COMPETITION_SUMMARY", description = "Innovation lead users can view competition summary of competitions assigned to them.")
-    public boolean innovationLeadsCanViewCompetitionSummaryOnAssginedComps(Long competitionId, UserResource user) {
-        return userIsInnovationLeadOnCompetition(competitionId, user.getId());
+    public boolean innovationLeadsCanViewCompetitionSummaryOnAssginedComps(CompetitionResource competition, UserResource user) {
+        return userIsInnovationLeadOnCompetition(competition.getId(), user.getId());
     }
 
     @PermissionRule(value = "VIEW_COMPETITION_SUMMARY", description = "Internal users (except innovation leads) can view competition summary of any competition.")
-    public boolean allInternalUsersCanViewCompetitionSummaryOtherThanInnovationLeads(Long competitionId, UserResource user) {
+    public boolean allInternalUsersCanViewCompetitionSummaryOtherThanInnovationLeads(CompetitionResource competition, UserResource user) {
         return isInternal(user) && !isInnovationLead(user);
     }
 }
--- ifs-data-layer/ifs-data-service/src/main/java/org/innovateuk/ifs/application/transactional/CompetitionSummaryService.java
+++ ifs-data-layer/ifs-data-service/src/main/java/org/innovateuk/ifs/application/transactional/CompetitionSummaryService.java
@@ -5,6 +5,6 @@ import org.innovateuk.ifs.commons.service.ServiceResult;
 import org.springframework.security.access.prepost.PreAuthorize;
 
 public interface CompetitionSummaryService {
-	@PreAuthorize("hasPermission(#id, 'VIEW_COMPETITION_SUMMARY')")
-	ServiceResult<CompetitionSummaryResource> getCompetitionSummaryByCompetitionId(Long id);
+	@PreAuthorize("hasPermission(#competitionId, 'org.innovateuk.ifs.competition.resource.CompetitionResource','VIEW_COMPETITION_SUMMARY')")
+	ServiceResult<CompetitionSummaryResource> getCompetitionSummaryByCompetitionId(final Long competitionId);
 }
