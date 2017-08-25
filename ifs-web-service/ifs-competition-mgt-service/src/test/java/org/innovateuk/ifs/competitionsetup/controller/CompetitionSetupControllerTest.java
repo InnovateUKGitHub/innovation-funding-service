1347932a76800cb513333cd7042f5582869df0dc
Reinaud van Rumpt <rvanrumpt@worth.systems>
2017-08-25 11:54:50 +0200

    IFS-769 Update unit test with new content

--- ifs-web-service/ifs-competition-mgt-service/src/test/java/org/innovateuk/ifs/competitionsetup/controller/CompetitionSetupControllerTest.java
+++ ifs-web-service/ifs-competition-mgt-service/src/test/java/org/innovateuk/ifs/competitionsetup/controller/CompetitionSetupControllerTest.java
@@ -268,7 +268,7 @@ public class CompetitionSetupControllerTest extends BaseControllerMockMVCTest<Co
         assertEquals(0, bindingResult.getGlobalErrorCount());
         assertEquals(10, bindingResult.getFieldErrorCount());
         assertTrue(bindingResult.hasFieldErrors("executiveUserId"));
-        assertEquals("Please select a Portfolio manager.", bindingResult.getFieldError("executiveUserId").getDefaultMessage());
+        assertEquals("Please select a Portfolio Manager.", bindingResult.getFieldError("executiveUserId").getDefaultMessage());
         assertTrue(bindingResult.hasFieldErrors("title"));
         assertEquals("Please enter a title.", bindingResult.getFieldError("title").getDefaultMessage());
         assertTrue(bindingResult.hasFieldErrors("innovationLeadUserId"));
