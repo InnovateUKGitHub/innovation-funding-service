0870b44b9650169f3d33b186a6688a45a27ae780
Reinaud van Rumpt <rvanrumpt@worth.systems>
2017-08-25 14:47:31 +0200

    IFS-713 Remove the unused getmappings

--- ifs-web-service/ifs-front-door-service/src/test/java/org/innovateuk/ifs/competition/controller/CompetitionControllerTest.java
+++ ifs-web-service/ifs-front-door-service/src/test/java/org/innovateuk/ifs/competition/controller/CompetitionControllerTest.java
@@ -3,12 +3,10 @@ package org.innovateuk.ifs.competition.controller;
 import org.innovateuk.ifs.BaseControllerMockMVCTest;
 import org.innovateuk.ifs.competition.populator.CompetitionOverviewPopulator;
 import org.innovateuk.ifs.competition.publiccontent.resource.PublicContentItemResource;
-import org.innovateuk.ifs.competition.resource.CompetitionResource;
 import org.innovateuk.ifs.competition.viewmodel.CompetitionOverviewViewModel;
 import org.junit.Test;
 import org.mockito.Mock;
 
-import static org.innovateuk.ifs.competition.builder.CompetitionResourceBuilder.newCompetitionResource;
 import static org.innovateuk.ifs.publiccontent.builder.PublicContentItemResourceBuilder.newPublicContentItemResource;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -55,48 +53,5 @@ public class CompetitionControllerTest extends BaseControllerMockMVCTest<Competi
                 .andExpect(status().isOk())
                 .andExpect(model().attribute("model", viewModel))
                 .andExpect(view().name("competition/overview"));
-    }
-
-    @Test
-    public void beforeYouApply() throws Exception {
-        final CompetitionResource competitionResource = newCompetitionResource().build();
-        when(competitionService.getPublishedById(competitionResource.getId())).thenReturn(competitionResource);
-
-        mockMvc.perform(get("/competition/{id}/info/before-you-apply", competitionResource.getId()))
-                .andExpect(status().isOk())
-                .andExpect(model().attribute("currentCompetition", competitionResource))
-                .andExpect(view().name("competition/info/before-you-apply"));
-    }
-
-    @Test
-    public void eligibility() throws Exception {
-        final CompetitionResource competitionResource = newCompetitionResource().build();
-        when(competitionService.getPublishedById(competitionResource.getId())).thenReturn(competitionResource);
-
-        mockMvc.perform(get("/competition/{id}/info/eligibility", competitionResource.getId()))
-                .andExpect(status().isOk())
-                .andExpect(model().attribute("currentCompetition", competitionResource))
-                .andExpect(view().name("competition/info/eligibility"));
-    }
-
-    @Test
-    public void termsAndConditions() throws Exception {
-        final CompetitionResource competitionResource = newCompetitionResource().build();
-        when(competitionService.getPublishedById(competitionResource.getId())).thenReturn(competitionResource);
-
-        mockMvc.perform(get("/competition/{id}/info/terms-and-conditions", competitionResource.getId()))
-                .andExpect(status().isOk())
-                .andExpect(view().name("competition/info/terms-and-conditions"));
-    }
-
-    @Test
-    public void whatWeAskYou() throws Exception {
-        final CompetitionResource competitionResource = newCompetitionResource().build();
-        when(competitionService.getPublishedById(competitionResource.getId())).thenReturn(competitionResource);
-
-        mockMvc.perform(get("/competition/{id}/info/what-we-ask-you", competitionResource.getId()))
-                .andExpect(status().isOk())
-                .andExpect(model().attribute("currentCompetition", competitionResource))
-                .andExpect(view().name("competition/info/what-we-ask-you"));
     }
 }
