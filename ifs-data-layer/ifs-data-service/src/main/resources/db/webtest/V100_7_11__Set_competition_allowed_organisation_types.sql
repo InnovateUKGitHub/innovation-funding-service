4a712b5e57f93433d8d91ec66cdb77c1acb5f3ee
Wouter de Meijer <wdemeijer@worth.systems>
2017-09-05 11:01:20 +0200

    IFS-1324 IFS-1325 - Added research type to comp 16 for testing purposes, and fixed an acc test failing because it was matching organisations

--- ifs-data-layer/ifs-data-service/src/main/resources/db/webtest/V100_7_11__Set_competition_allowed_organisation_types.sql
+++ ifs-data-layer/ifs-data-service/src/main/resources/db/webtest/V100_7_11__Set_competition_allowed_organisation_types.sql
@@ -1,2 +1,5 @@
+-- Changes in this file are temporary and will be replaced by generated data as part of IFS-1293
 INSERT INTO `lead_applicant_type` (`competition_id`, `organisation_type_id`) VALUES ('12', '3');
 UPDATE `lead_applicant_type` SET `organisation_type_id`='3' WHERE `organisation_type_id`='1' and `competition_id`='16';
+
+INSERT INTO `lead_applicant_type` (`competition_id`, `organisation_type_id`) VALUES ('16', '2');
