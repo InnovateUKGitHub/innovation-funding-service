7009aaa4dcefe4a80a2438c9dbe956b65beae3df
David Soff <dsoff@worth.systems>
2017-09-07 15:19:29 +0200

    initial generation of the assessor data

--- jmeter/Representative/regenerate_test_data.sh
+++ jmeter/Representative/regenerate_test_data.sh
@@ -7,11 +7,7 @@ cd ../..
 ./gradlew initDB
 cd -
 
-## regenerate users data
-mysql ifs -hifs-database -uroot -ppassword --skip-column-names < regenerate_user_data.sql > user_data.csv 2> /dev/null
-
-## regenerate invites data
-mysql ifs -hifs-database -uroot -ppassword --skip-column-names < regenerate_invites_data.sql > invite_data.csv 2> /dev/null
+./regenerate_test_data_and_apply_to_existing_db.sh
 
 ## and finally, clean up the database again
 cd ../..
--- jmeter/Representative/regenerate_test_data_and_apply_to_existing_db.sh
+++ jmeter/Representative/regenerate_test_data_and_apply_to_existing_db.sh
@@ -2,8 +2,23 @@
 
 set -e
 
-## regenerate users data
-mysql ifs -hifs-database -uroot -ppassword --skip-column-names < regenerate_user_data.sql > user_data.csv 2> /dev/null
+declare -i count
+count=1
+file="files-to-generate.csv"
+lines=`cat ${file} | wc -l | sed -e 's/^[ \t]*//'`
 
-## regenerate invites data
-mysql ifs -hifs-database -uroot -ppassword --skip-column-names < regenerate_invites_data.sql > invite_data.csv 2> /dev/null
+mysql ifs -hifs-database -uroot -ppassword --skip-column-names < add-indexes.sql 2> /dev/null
+
+cat ${file} | while read line
+do
+        text=`echo $line | cut -d'"' -f2`     # get the first name
+        sql=`echo $line | cut -d'"' -f4`     # get the first name
+        csv=`echo $line | cut -d'"' -f6`     # get the first name
+
+        echo "${count} / ${lines} - ${text}"
+        mysql ifs -hifs-database -uroot -ppassword --skip-column-names < ${sql} > ${csv} 2> /dev/null
+        echo "------------------"
+        count=$count+1
+done
+
+mysql ifs -hifs-database -uroot -ppassword --skip-column-names < remove-indexes.sql 2> /dev/null
