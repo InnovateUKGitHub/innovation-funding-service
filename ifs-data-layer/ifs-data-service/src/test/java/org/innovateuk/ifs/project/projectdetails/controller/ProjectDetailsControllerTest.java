32100cc3f2b3b18d03cab6baf33f9edb9d6fb79f
Bronnyl Dias <bdias@worth.systems>
2017-09-21 15:16:45 +0100

    IFS-1580-Remove-MAC-Proj-Deets
    Data Controller junits
    
    Change-Id: Id02bf7a38aed267d4a6c998313d73ffec6db5fc8

--- ifs-data-layer/ifs-data-service/src/test/java/org/innovateuk/ifs/project/projectdetails/controller/ProjectDetailsControllerTest.java
+++ ifs-data-layer/ifs-data-service/src/test/java/org/innovateuk/ifs/project/projectdetails/controller/ProjectDetailsControllerTest.java
@@ -6,15 +6,11 @@ import org.innovateuk.ifs.address.resource.OrganisationAddressType;
 import org.innovateuk.ifs.project.resource.ProjectOrganisationCompositeId;
 import org.junit.Test;
 
-import java.time.ZonedDateTime;
-
 import static org.innovateuk.ifs.address.builder.AddressResourceBuilder.newAddressResource;
 import static org.innovateuk.ifs.commons.service.ServiceResult.serviceSuccess;
 import static org.innovateuk.ifs.util.JsonMappingUtil.toJson;
-import static org.mockito.Matchers.isA;
 import static org.mockito.Mockito.*;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
-import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -62,33 +58,4 @@ public class ProjectDetailsControllerTest extends BaseControllerMockMVCTest<Proj
 
         verify(projectDetailsServiceMock).updateProjectAddress(123L, 456L, OrganisationAddressType.REGISTERED, addressResource);
     }
-
-/*    @Test
-    public void isSubmitAllowed() throws Exception {
-        when(projectDetailsServiceMock.isSubmitAllowed(123L)).thenReturn(serviceSuccess(true));
-
-        mockMvc.perform(get("/project/{projectId}/isSubmitAllowed", 123L))
-                .andExpect(status().isOk())
-                .andExpect(content().string("true"))
-                .andReturn();
-    }
-
-    @Test
-    public void isSubmitAllowedFalse() throws Exception {
-        when(projectDetailsServiceMock.isSubmitAllowed(123L)).thenReturn(serviceSuccess(false));
-
-        mockMvc.perform(get("/project/{projectId}/isSubmitAllowed", 123L))
-                .andExpect(status().isOk())
-                .andExpect(content().string("false"))
-                .andReturn();
-    }
-
-    @Test
-    public void setApplicationDetailsSubmitted() throws Exception {
-        when(projectDetailsServiceMock.submitProjectDetails(isA(Long.class), isA(ZonedDateTime.class))).thenReturn(serviceSuccess());
-
-        mockMvc.perform(post("/project/{projectId}/setApplicationDetailsSubmitted", 123L))
-                .andExpect(status().isOk())
-                .andReturn();
-    }*/
 }
