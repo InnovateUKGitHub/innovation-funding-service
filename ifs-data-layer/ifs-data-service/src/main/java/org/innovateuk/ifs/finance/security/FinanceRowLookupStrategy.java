465b72f24149cc09bd2b83df0fdb5de2cf4f0e1c
Ravish Bhagdev <rav@hiveit.co.uk>
2017-09-26 09:58:18 +0100

    IFS-1724 overheads lookup strategy fixed to use project finance rows

--- ifs-data-layer/ifs-data-service/src/main/java/org/innovateuk/ifs/finance/security/FinanceRowLookupStrategy.java
+++ ifs-data-layer/ifs-data-service/src/main/java/org/innovateuk/ifs/finance/security/FinanceRowLookupStrategy.java
@@ -3,8 +3,8 @@ package org.innovateuk.ifs.finance.security;
 import org.innovateuk.ifs.commons.security.PermissionEntityLookupStrategies;
 import org.innovateuk.ifs.commons.security.PermissionEntityLookupStrategy;
 import org.innovateuk.ifs.finance.domain.FinanceRow;
-import org.innovateuk.ifs.finance.repository.ApplicationFinanceRepository;
 import org.innovateuk.ifs.finance.repository.ApplicationFinanceRowRepository;
+import org.innovateuk.ifs.finance.repository.ProjectFinanceRowRepository;
 import org.innovateuk.ifs.finance.resource.cost.FinanceRowItem;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -16,13 +16,16 @@ import org.springframework.stereotype.Component;
 @PermissionEntityLookupStrategies
 public class FinanceRowLookupStrategy {
     @Autowired
-    private ApplicationFinanceRepository applicationFinanceRepository;
+    private ApplicationFinanceRowRepository applicationFinanceRowRepository;
 
     @Autowired
-    private ApplicationFinanceRowRepository financeRowRepository;
+    private ProjectFinanceRowRepository projectFinanceRowRepository;
 
     @PermissionEntityLookupStrategy
     public FinanceRow getFinanceRow(final Long costId) {
-        return financeRowRepository.findOne(costId);
+        FinanceRow financeRow = applicationFinanceRowRepository.findById(costId);
+        if(financeRow == null)
+            financeRow = projectFinanceRowRepository.findOne(costId);
+        return financeRow;
     }
 }
