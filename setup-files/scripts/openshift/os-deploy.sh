a1fa986d92d0785464ac2d368edbcae739dfa4e3
Duncan Watson <duncan@hiveit.co.uk>
2017-10-02 12:22:32 +0100

    IFS-1789 - removed inconsistency between deploy and upgrade scripts wrt how they handle deploy of service provider pod

--- setup-files/scripts/openshift/os-deploy.sh
+++ setup-files/scripts/openshift/os-deploy.sh
@@ -27,7 +27,7 @@ function deploy() {
         oc create -f os-files-tmp/gluster/11-gluster-endpoints.yml ${SVC_ACCOUNT_CLAUSE}
         oc create -f os-files-tmp/gluster/named-envs/12-${TARGET}-file-upload-claim.yml ${SVC_ACCOUNT_CLAUSE}
         oc create -f os-files-tmp/ ${SVC_ACCOUNT_CLAUSE}
-        oc create -f os-files-tmp/shib/5-shib.yml ${SVC_ACCOUNT_CLAUSE}
+        oc create -f os-files-tmp/shib/named-envs/5-${TARGET}-shib.yml ${SVC_ACCOUNT_CLAUSE}
         oc create -f os-files-tmp/shib/named-envs/56-idp.yml ${SVC_ACCOUNT_CLAUSE}
     else
         oc create -f os-files-tmp/mail/ ${SVC_ACCOUNT_CLAUSE}
@@ -39,8 +39,6 @@ function deploy() {
     fi
 
 }
-
-
 
 function shibInit() {
      oc rsh ${SVC_ACCOUNT_CLAUSE} $(oc get pods  ${SVC_ACCOUNT_CLAUSE} | awk '/ldap/ { print $1 }') /usr/local/bin/ldap-sync-from-ifs-db.sh ifs-database
