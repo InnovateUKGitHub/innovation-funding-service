6ce3dfd73ba8aec0ea3ede132c4911faddeeeba9
Duncan Watson <duncan@hiveit.co.uk>
2017-10-04 11:18:51 +0100

    IFS-1789 - deduped the non-named and named versions of 5-shib.yml and updated the Bamboo named environment upgrade jobs accordingly with new environment variables

--- setup-files/scripts/openshift/os-deploy.sh
+++ setup-files/scripts/openshift/os-deploy.sh
@@ -22,13 +22,12 @@ echo "Deploying the $PROJECT Openshift project"
 
 function deploy() {
 
-    oc create -f os-files-tmp/shib/5-shib.yml ${SVC_ACCOUNT_CLAUSE}
-
     if $(isNamedEnvironment ${TARGET}); then
         oc create -f os-files-tmp/gluster/10-gluster-svc.yml ${SVC_ACCOUNT_CLAUSE}
         oc create -f os-files-tmp/gluster/11-gluster-endpoints.yml ${SVC_ACCOUNT_CLAUSE}
         oc create -f os-files-tmp/gluster/named-envs/12-${TARGET}-file-upload-claim.yml ${SVC_ACCOUNT_CLAUSE}
         oc create -f os-files-tmp/ ${SVC_ACCOUNT_CLAUSE}
+        oc create -f os-files-tmp/shib/5-shib.yml ${SVC_ACCOUNT_CLAUSE}
         oc create -f os-files-tmp/shib/named-envs/56-idp.yml ${SVC_ACCOUNT_CLAUSE}
     else
         oc create -f os-files-tmp/shib/55-ldap.yml ${SVC_ACCOUNT_CLAUSE}
@@ -37,6 +36,7 @@ function deploy() {
         oc create -f os-files-tmp/gluster/ ${SVC_ACCOUNT_CLAUSE}
         oc create -f os-files-tmp/spring-admin/ ${SVC_ACCOUNT_CLAUSE}
         oc create -f os-files-tmp/ ${SVC_ACCOUNT_CLAUSE}
+        oc create -f os-files-tmp/shib/5-shib.yml ${SVC_ACCOUNT_CLAUSE}
         oc create -f os-files-tmp/shib/56-idp.yml ${SVC_ACCOUNT_CLAUSE}
     fi
 
