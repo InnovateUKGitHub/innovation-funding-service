914c0ec5369654c7d11e52acaa8add68d6a7fc02
richard <richard@hiveit.co.uk>
2017-10-12 16:02:30 +0100

    stub fires up now and serves requests

--- ifs-data-layer/ifs-sil-stub/src/main/java/org/innovateuk/ifs/sil/crm/controller/CrmEndpointController.java
+++ ifs-data-layer/ifs-sil-stub/src/main/java/org/innovateuk/ifs/sil/crm/controller/CrmEndpointController.java
@@ -2,16 +2,16 @@ package org.innovateuk.ifs.sil.crm.controller;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.innovateuk.ifs.commons.error.Error;
 import org.innovateuk.ifs.commons.rest.RestResult;
+import org.innovateuk.ifs.sil.crm.resource.SilAddress;
 import org.innovateuk.ifs.sil.crm.resource.SilContact;
 import org.innovateuk.ifs.util.JsonMappingUtil;
 import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import static org.innovateuk.ifs.commons.rest.RestResult.restFailure;
 import static org.innovateuk.ifs.commons.rest.RestResult.restSuccess;
 
 /**
@@ -28,5 +28,4 @@ public class CrmEndpointController {
         LOG.info("Stubbing out SIL CRM update contact endpoint: " + JsonMappingUtil.toJson(contact));
         return restSuccess(HttpStatus.ACCEPTED);
     }
-
 }
