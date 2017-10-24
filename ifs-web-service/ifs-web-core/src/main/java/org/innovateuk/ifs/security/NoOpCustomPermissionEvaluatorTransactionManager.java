27c9d7fd5e6637119fab0a55d529704661b6caa0
richard <richard@hiveit.co.uk>
2017-10-24 12:10:10 +0100

    more refactor of name

--- ifs-web-service/ifs-web-core/src/main/java/org/innovateuk/ifs/security/NoOpCustomPermissionEvaluatorTransactionManager.java
+++ ifs-web-service/ifs-web-core/src/main/java/org/innovateuk/ifs/security/NoOpCustomPermissionEvaluatorTransactionManager.java
@@ -1,13 +1,13 @@
 package org.innovateuk.ifs.security;
 
 import org.innovateuk.ifs.commons.security.evaluator.CustomPermissionEvaluatorTransactionManager;
-import org.innovateuk.ifs.security.evaluator.RootCustomPermissionEvaluator;
+import org.innovateuk.ifs.security.evaluator.CustomPermissionEvaluator;
 import org.springframework.stereotype.Component;
 
 import java.util.function.Supplier;
 
 /**
- * A no-op transaction manager for use in the web layer.  To make the {@link RootCustomPermissionEvaluator}
+ * A no-op transaction manager for use in the web layer.  To make the {@link CustomPermissionEvaluator}
  * portable between the data and web layer, this no-op component takes the place of the transactional component as used in the data layer
  */
 @Component
---
+++ ifs-web-service/ifs-web-core/src/main/java/org/innovateuk/ifs/security/evaluator/CustomPermissionEvaluator.java
@@ -0,0 +1,11 @@
+package org.innovateuk.ifs.security.evaluator;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * The {@link Component} annotation is added here where it is needed.
+ * See {@link CustomPermissionEvaluator} for more information.
+ */
+@Component
+public class CustomPermissionEvaluator extends CustomPermissionEvaluator {
+}
