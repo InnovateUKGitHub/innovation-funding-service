350257f5d3c268bdb1a18d2dc6814f8108eea3bc
richard <richard@hiveit.co.uk>
2017-10-24 14:09:58 +0100

    comment

--- ifs-data-layer/data-service-commons/src/main/java/org/innovateuk/ifs/security/ServiceResultAwareMethodSecurityExpressionHandlerConfig.java
+++ ifs-data-layer/data-service-commons/src/main/java/org/innovateuk/ifs/security/ServiceResultAwareMethodSecurityExpressionHandlerConfig.java
@@ -6,6 +6,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.access.expression.method.DefaultMethodSecurityExpressionHandler;
 
 
+/**
+ * Spring configuration class to override the default {@link org.springframework.security.access.PermissionEvaluator}
+ * with the custom {@link CustomPermissionEvaluator} in our subclass of {@link DefaultMethodSecurityExpressionHandler}
+ * the {@link DefaultMethodSecurityExpressionHandler}.
+ */
 @Configuration
 public class ServiceResultAwareMethodSecurityExpressionHandlerConfig {
 
