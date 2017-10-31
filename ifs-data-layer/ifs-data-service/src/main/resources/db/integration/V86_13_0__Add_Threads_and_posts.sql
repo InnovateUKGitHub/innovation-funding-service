dd50b76f4defd2496d2cca9c225cc599bb83a71a
richard <richard@hiveit.co.uk>
2017-10-31 10:26:33 +0000

    removing failing insert statements in the integration test data

--- ifs-data-layer/ifs-data-service/src/main/resources/db/integration/V86_13_0__Add_Threads_and_posts.sql
+++ ifs-data-layer/ifs-data-service/src/main/resources/db/integration/V86_13_0__Add_Threads_and_posts.sql
@@ -1,7 +1,5 @@
 INSERT IGNORE INTO thread (id, class_pk, class_name, thread_type, title, created_on, section) VALUES (1, 6, 'org.innovateuk.ifs.finance.domain.ProjectFinance', 'QUERY', 'Query1', '2017-02-08 09:00:00', 'ELIGIBILITY');
 INSERT IGNORE INTO post (id, thread_id, author_id, body, created_on) VALUES (1, 1, 18, 'What do we want?', '2017-02-08 09:01:00');
-INSERT IGNORE INTO post_attachment (post_id, file_entry_id) VALUES(1,1);
-INSERT IGNORE INTO post (id, thread_id, author_id, body, created_on) VALUES (2, 1, 55, 'IFS', '2017-02-08 09:02:00');
 
 INSERT IGNORE INTO thread (id, class_pk, class_name, thread_type, title, created_on, section) VALUES (2,6, 'org.innovateuk.ifs.finance.domain.ProjectFinance', 'QUERY', 'Query2', '2017-02-08 09:05:00', 'ELIGIBILITY');
 INSERT IGNORE INTO post (id, thread_id, author_id, body, created_on) VALUES (3, 2, 18, 'Why do we want?', '2017-02-08 09:06:00');
