67417a2e06c0bc6563799989bc4d3709cd0d4ae8
Ewan Cormack <ewan@hiveit.co.uk>
2017-11-02 14:13:28 +0000

    IFS-1387: Tidy up validator

--- ifs-data-layer/ifs-data-service/src/main/java/org/innovateuk/ifs/validator/AssessorScopeValidator.java
+++ ifs-data-layer/ifs-data-service/src/main/java/org/innovateuk/ifs/validator/AssessorScopeValidator.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 
 import static org.innovateuk.ifs.commons.rest.ValidationMessages.rejectValue;
+import static org.innovateuk.ifs.form.resource.FormInputType.ASSESSOR_APPLICATION_IN_SCOPE;
 
 /**
  * This class validates the FormInputResponse, it checks that if the response is for scope then that field is not empty
@@ -14,14 +15,16 @@ import static org.innovateuk.ifs.commons.rest.ValidationMessages.rejectValue;
  */
 @Component
 public class AssessorScopeValidator extends BaseValidator {
-    private static final Log LOG = LogFactory.getLog(AssessorScopeValidator.class);
 
     @Override
     public void validate(Object target, Errors errors) {
         FormInputResponse response = (FormInputResponse) target;
 
-        if (response.getValue().equals("none")) {
-            rejectValue(errors, "value", "validation.assessor.scope.invalidScope", response.getFormInput().getId());
+        if (ASSESSOR_APPLICATION_IN_SCOPE == response.getFormInput().getType()) {
+            String value = response.getValue();
+            if (!value.equals("true") && !value.equals("false")) {
+                rejectValue(errors, "value", "validation.assessor.scope.invalidScope", response.getFormInput().getId());
+            }
         }
     }
 }
