59d83f36f6431d046fd324d37d607d1055280411
Reinaud van Rumpt <rvanrumpt@worth.systems>
2017-11-07 13:08:35 +0100

    IFS-1678 Formatting of the Validator file

--- ifs-data-layer/ifs-data-service/src/main/java/org/innovateuk/ifs/validator/AcademicValidator.java
+++ ifs-data-layer/ifs-data-service/src/main/java/org/innovateuk/ifs/validator/AcademicValidator.java
@@ -1,8 +1,6 @@
 package org.innovateuk.ifs.validator;
 
 import org.apache.commons.lang3.StringUtils;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.innovateuk.ifs.finance.resource.cost.AcademicCost;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
@@ -15,18 +13,18 @@ import static org.innovateuk.ifs.commons.rest.ValidationMessages.rejectValue;
  */
 @Component
 public class AcademicValidator implements Validator {
+    
     @Override
     public boolean supports(Class<?> clazz) {
         return AcademicCost.class.equals(clazz);
     }
-    private static final Log LOG = LogFactory.getLog(AcademicValidator.class);
 
     @Override
     public void validate(Object target, Errors errors) {
         AcademicCost response = (AcademicCost) target;
 
-        if("tsb_reference".equals(response.getName()) && StringUtils.isBlank(response.getItem())){
-            rejectValue(errors, "item","validation.field.must.not.be.blank");
+        if ("tsb_reference".equals(response.getName()) && StringUtils.isBlank(response.getItem())) {
+            rejectValue(errors, "item", "validation.field.must.not.be.blank");
         }
     }
 }
