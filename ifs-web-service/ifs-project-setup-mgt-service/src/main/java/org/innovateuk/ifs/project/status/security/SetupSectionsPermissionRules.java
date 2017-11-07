1f77450eab6398503167db7d92648d1ef21ea0f3
Ravish Bhagdev <rav@hiveit.co.uk>
2017-11-07 12:50:09 +0000

    IFS-1307 fixed type in permission descriotion

--- ifs-web-service/ifs-project-setup-mgt-service/src/main/java/org/innovateuk/ifs/project/status/security/SetupSectionsPermissionRules.java
+++ ifs-web-service/ifs-project-setup-mgt-service/src/main/java/org/innovateuk/ifs/project/status/security/SetupSectionsPermissionRules.java
@@ -101,7 +101,7 @@ public class SetupSectionsPermissionRules {
         return doSectionCheck(projectId, user, SetupSectionInternalUser::canAccessGrantOfferLetterSendSection, SecurityRuleUtil::isInternalAdmin);
     }
 
-    @PermissionRule(value = "ACCESS_GRANT_OFFER_LETTER_SEND_SECTION", description = "An internal user can access the Grant Offer Letter send " +
+    @PermissionRule(value = "ACCESS_GRANT_OFFER_LETTER_SEND_SECTION", description = "A support user can access the Grant Offer Letter send " +
             "section when the lead partner submits the documents")
     public boolean supportCanAccessGrantOfferLetterSendSection(Long projectId, UserResource user) {
         return doSectionCheck(projectId, user, SetupSectionInternalUser::canAccessGrantOfferLetterSendSection, SecurityRuleUtil::isSupport);
