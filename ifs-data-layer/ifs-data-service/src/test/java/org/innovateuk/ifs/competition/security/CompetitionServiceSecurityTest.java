4e8563d1f9de7b1cb77ccead55b5aa0f473292aa
Bronnyl Dias <bdias@worth.systems>
2017-11-09 11:59:25 +0000

    IFS-2016-PF-SpendProfile-Action
    Security Related junits.
    
    Change-Id: I8c96d10825f0e4cc6064845dabfa9d8a39d9475d

--- ifs-data-layer/ifs-data-service/src/test/java/org/innovateuk/ifs/competition/security/CompetitionServiceSecurityTest.java
+++ ifs-data-layer/ifs-data-service/src/test/java/org/innovateuk/ifs/competition/security/CompetitionServiceSecurityTest.java
@@ -237,6 +237,16 @@ public class CompetitionServiceSecurityTest extends BaseServiceSecurityTest<Comp
         testOnlyAUserWithOneOfTheGlobalRolesCan(() -> classUnderTest.findAllOpenQueries(1L), PROJECT_FINANCE);
     }
 
+    @Test
+    public void getPendingSpendProfiles() {
+        testOnlyAUserWithOneOfTheGlobalRolesCan(() -> classUnderTest.getPendingSpendProfiles(1L), PROJECT_FINANCE);
+    }
+
+    @Test
+    public void countPendingSpendProfiles() {
+        testOnlyAUserWithOneOfTheGlobalRolesCan(() -> classUnderTest.countPendingSpendProfiles(1L), PROJECT_FINANCE);
+    }
+
     private void runAsRole(UserRoleType roleType, Runnable serviceCall) {
         setLoggedInUser(
                 newUserResource()
@@ -359,6 +369,16 @@ public class CompetitionServiceSecurityTest extends BaseServiceSecurityTest<Comp
         @Override
         public ServiceResult<Long>countAllOpenQueries(Long competitionId) {
             return serviceSuccess(0L);
+        }
+
+        @Override
+        public ServiceResult<List<CompetitionPendingSpendProfilesResource>> getPendingSpendProfiles(Long competitionId) {
+            return null;
+        }
+
+        @Override
+        public ServiceResult<Integer> countPendingSpendProfiles(Long competitionId) {
+            return null;
         }
     }
 }
