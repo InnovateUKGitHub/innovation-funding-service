28c0b007878ad9768a7aff31ea4a0befa839b949
richard <richard@hiveit.co.uk>
2017-11-14 09:42:37 +0000

    pull comment fix

--- ifs-commons/src/main/java/org/innovateuk/ifs/util/StreamFunctions.java
+++ ifs-commons/src/main/java/org/innovateuk/ifs/util/StreamFunctions.java
@@ -2,8 +2,6 @@ package org.innovateuk.ifs.util;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Function;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static java.util.Collections.addAll;
