63e8fce4fea00fe991750e87fadfd32552a9ffb2
Tom Baldwin <tom@polymorphism.co.uk>
2017-11-14 13:02:07 +0000

    IFS-1833 - Adds unit test for GoogleAnalyticsDataLayerRestServiceImpl.

---
+++ ifs-rest-api-service/src/test/java/org/innovateuk/ifs/analytics/service/GoogleAnalyticsDataLayerRestServiceImplTest.java
@@ -0,0 +1,81 @@
+package org.innovateuk.ifs.analytics.service;
+
+import org.innovateuk.ifs.BaseRestServiceUnitTest;
+import org.junit.Test;
+
+import static java.lang.String.format;
+import static org.junit.Assert.assertEquals;
+
+public class GoogleAnalyticsDataLayerRestServiceImplTest extends BaseRestServiceUnitTest<GoogleAnalyticsDataLayerRestServiceImpl> {
+
+    private static final String restUrl = "/analytics";
+
+    @Override
+    protected GoogleAnalyticsDataLayerRestServiceImpl registerRestServiceUnderTest() {
+        return new GoogleAnalyticsDataLayerRestServiceImpl();
+    }
+
+    @Test
+    public void getCompetitionNameForApplication() {
+        long applicationId = 5L;
+        String expected = "competition name";
+
+        setupGetWithRestResultExpectations(
+                format("%s/%s/%d/competition-name", restUrl, "application", applicationId),
+                String.class,
+                expected
+        );
+
+        String actual = service.getCompetitionNameForApplication(applicationId).getSuccessObject();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getCompetitionName() {
+        long competitionId = 7L;
+        String expected = "competition name";
+
+        setupGetWithRestResultExpectations(
+                format("%s/%s/%d/competition-name", restUrl, "competition", competitionId),
+                String.class,
+                expected
+        );
+
+        String actual = service.getCompetitionName(competitionId).getSuccessObject();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getCompetitionNameForProject() {
+        long projectId = 11L;
+        String expected = "competition name";
+
+        setupGetWithRestResultExpectations(
+                format("%s/%s/%d/competition-name", restUrl, "project", projectId),
+                String.class,
+                expected
+        );
+
+        String actual = service.getCompetitionNameForProject(projectId).getSuccessObject();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getCompetitionNameForAssessment() {
+        long assessmentId = 13L;
+        String expected = "competition name";
+
+        setupGetWithRestResultExpectations(
+                format("%s/%s/%d/competition-name", restUrl, "assessment", assessmentId),
+                String.class,
+                expected
+        );
+
+        String actual = service.getCompetitionNameForAssessment(assessmentId).getSuccessObject();
+
+        assertEquals(expected, actual);
+    }
+}
