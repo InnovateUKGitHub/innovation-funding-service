8029bd5c69dcd57018ac77afa9889da1ad1b5de1
Tom Baldwin <tom@polymorphism.co.uk>
2017-11-14 08:24:31 +0000

    IFS-1833 - Adds unit test for GoogleAnalyticsDataLayerInterceptor.

--- ifs-web-service/ifs-web-core/src/main/java/org/innovateuk/ifs/analytics/GoogleAnalyticsDataLayerInterceptor.java
+++ ifs-web-service/ifs-web-core/src/main/java/org/innovateuk/ifs/analytics/GoogleAnalyticsDataLayerInterceptor.java
@@ -1,10 +1,7 @@
 package org.innovateuk.ifs.analytics;
 
 import org.innovateuk.ifs.analytics.service.GoogleAnalyticsDataLayerRestService;
-import org.innovateuk.ifs.application.service.ApplicationRestService;
-import org.innovateuk.ifs.assessment.service.AssessmentRestService;
-import org.innovateuk.ifs.competition.service.CompetitionRestService;
-import org.innovateuk.ifs.project.service.ProjectRestService;
+import org.innovateuk.ifs.commons.rest.RestResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.servlet.HandlerMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -13,11 +10,13 @@ import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.Map;
+import java.util.function.Function;
 
 import static java.lang.Long.parseLong;
+import static org.innovateuk.ifs.util.JsonMappingUtil.fromJson;
 
 /**
- * Interceptor to add Google Analytics data to the Model.
+ * Interceptor to add Google Analytics data layer to the Model.
  */
 public class GoogleAnalyticsDataLayerInterceptor extends HandlerInterceptorAdapter {
     static final String ANALYTICS_DATA_LAYER_NAME = "dataLayer";
@@ -37,20 +36,25 @@ public class GoogleAnalyticsDataLayerInterceptor extends HandlerInterceptorAdapt
 
             if (pathVariables.containsKey("competitionId")) {
                 final long competitionId = parseLong((String) pathVariables.get("competitionId"));
-                dl.setCompName(googleAnalyticsDataLayerRestService.getCompetitionName(competitionId).getSuccessObjectOrThrowException());
+                dl.setCompName(fromJson(googleAnalyticsDataLayerRestService.getCompetitionName(competitionId).getSuccessObjectOrThrowException(), String.class));
             }
             else if (pathVariables.containsKey("projectId")) {
                 final long projectId = parseLong((String) pathVariables.get("projectId"));
-                dl.setCompName(googleAnalyticsDataLayerRestService.getCompetitionNameForProject(projectId).getSuccessObjectOrThrowException());
+                dl.setCompName(fromJson(googleAnalyticsDataLayerRestService.getCompetitionNameForProject(projectId).getSuccessObjectOrThrowException(), String.class));
             }
             else if (pathVariables.containsKey("applicationId")) {
                 final long applicationId = parseLong((String) pathVariables.get("applicationId"));
-                dl.setCompName(googleAnalyticsDataLayerRestService.getCompetitionNameForApplication(applicationId).getSuccessObjectOrThrowException());
+                dl.setCompName(fromJson(googleAnalyticsDataLayerRestService.getCompetitionNameForApplication(applicationId).getSuccessObjectOrThrowException(), String.class));
             }
             else if (pathVariables.containsKey("assessmentId")) {
                 final long assessmentId = parseLong((String) pathVariables.get("assessmentId"));
-                dl.setCompName(googleAnalyticsDataLayerRestService.getCompetitionNameForAssessment(assessmentId).getSuccessObjectOrThrowException());
+                dl.setCompName(fromJson(googleAnalyticsDataLayerRestService.getCompetitionNameForAssessment(assessmentId).getSuccessObjectOrThrowException(), String.class));
             }
         }
+    }
+
+    private static void foo(GoogleTagManagerDataLayer dl, Function<Long, RestResult<String>> f, final Map pathVariables, String pathVariable) {
+        final long id = parseLong((String) pathVariables.get(pathVariable));
+        dl.setCompName(fromJson(f.apply(id).getSuccessObjectOrThrowException(), String.class));
     }
 }
---
+++ ifs-web-service/ifs-web-core/src/test/java/org/innovateuk/ifs/analytics/GoogleAnalyticsDataLayerInterceptorTest.java
@@ -0,0 +1,133 @@
+package org.innovateuk.ifs.analytics;
+
+import org.innovateuk.ifs.BaseUnitTestMocksTest;
+import org.innovateuk.ifs.analytics.service.GoogleAnalyticsDataLayerRestService;
+import org.innovateuk.ifs.commons.rest.RestResult;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import static java.util.Collections.emptyMap;
+import static java.util.Collections.singletonMap;
+import static org.innovateuk.ifs.util.JsonMappingUtil.toJson;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
+import static org.springframework.web.servlet.HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE;
+
+public class GoogleAnalyticsDataLayerInterceptorTest extends BaseUnitTestMocksTest {
+
+    @Mock
+    private GoogleAnalyticsDataLayerRestService googleAnalyticsDataLayerRestServiceMock;
+
+    @InjectMocks
+    private GoogleAnalyticsDataLayerInterceptor googleAnalyticsDataLayerInterceptor;
+
+    @Mock
+    private HttpServletRequest httpServletRequestMock;
+
+    @Mock
+    private HttpServletResponse httpServletResponseMock;
+
+    private ModelAndView mav;
+
+    @Override
+    @Before
+    public void setUp() {
+        super.setUp();
+        mav = new ModelAndView();
+    }
+
+    @Test
+    public void postHandle() throws Exception {
+        final String expectedCompName = "competition name";
+        final long expectedCompetitionId = 7L;
+
+        when(googleAnalyticsDataLayerRestServiceMock.getCompetitionName(expectedCompetitionId)).thenReturn(RestResult.restSuccess(toJson(expectedCompName)));
+
+        when(httpServletRequestMock.getAttribute(URI_TEMPLATE_VARIABLES_ATTRIBUTE)).thenReturn(singletonMap("competitionId", Long.toString(expectedCompetitionId)));
+
+        googleAnalyticsDataLayerInterceptor.postHandle(httpServletRequestMock, httpServletResponseMock, null, mav);
+
+        GoogleTagManagerDataLayer expectedDataLayer = new GoogleTagManagerDataLayer();
+        expectedDataLayer.setCompName(expectedCompName);
+
+        assertEquals(expectedDataLayer, mav.getModel().get("dataLayer"));
+
+        verify(googleAnalyticsDataLayerRestServiceMock, only()).getCompetitionName(expectedCompetitionId);
+    }
+
+    @Test
+    public void postHandle_applicationId() throws Exception {
+        final String expectedCompName = "competition name";
+        final long expectedApplicationId = 7L;
+
+        when(googleAnalyticsDataLayerRestServiceMock.getCompetitionNameForApplication(expectedApplicationId)).thenReturn(RestResult.restSuccess(toJson(expectedCompName)));
+
+        when(httpServletRequestMock.getAttribute(URI_TEMPLATE_VARIABLES_ATTRIBUTE)).thenReturn(singletonMap("applicationId", Long.toString(expectedApplicationId)));
+
+        googleAnalyticsDataLayerInterceptor.postHandle(httpServletRequestMock, httpServletResponseMock, null, mav);
+
+        GoogleTagManagerDataLayer expectedDataLayer = new GoogleTagManagerDataLayer();
+        expectedDataLayer.setCompName(expectedCompName);
+
+        assertEquals(expectedDataLayer, mav.getModel().get("dataLayer"));
+
+        verify(googleAnalyticsDataLayerRestServiceMock, only()).getCompetitionNameForApplication(expectedApplicationId);
+    }
+
+    @Test
+    public void postHandle_projectId() throws Exception {
+        final String expectedCompName = "competition name";
+        final long expectedProjectId = 7L;
+
+        when(googleAnalyticsDataLayerRestServiceMock.getCompetitionNameForProject(expectedProjectId)).thenReturn(RestResult.restSuccess(toJson(expectedCompName)));
+
+        when(httpServletRequestMock.getAttribute(URI_TEMPLATE_VARIABLES_ATTRIBUTE)).thenReturn(singletonMap("projectId", Long.toString(expectedProjectId)));
+
+        googleAnalyticsDataLayerInterceptor.postHandle(httpServletRequestMock, httpServletResponseMock, null, mav);
+
+        GoogleTagManagerDataLayer expectedDataLayer = new GoogleTagManagerDataLayer();
+        expectedDataLayer.setCompName(expectedCompName);
+
+        assertEquals(expectedDataLayer, mav.getModel().get("dataLayer"));
+
+        verify(googleAnalyticsDataLayerRestServiceMock, only()).getCompetitionNameForProject(expectedProjectId);
+    }
+
+    @Test
+    public void postHandle_assessmentId() throws Exception {
+        final String expectedCompName = "competition name";
+        final long expectedAssessmentId = 7L;
+
+        when(googleAnalyticsDataLayerRestServiceMock.getCompetitionNameForAssessment(expectedAssessmentId)).thenReturn(RestResult.restSuccess(toJson(expectedCompName)));
+
+        when(httpServletRequestMock.getAttribute(URI_TEMPLATE_VARIABLES_ATTRIBUTE)).thenReturn(singletonMap("assessmentId", Long.toString(expectedAssessmentId)));
+
+        googleAnalyticsDataLayerInterceptor.postHandle(httpServletRequestMock, httpServletResponseMock, null, mav);
+
+        GoogleTagManagerDataLayer expectedDataLayer = new GoogleTagManagerDataLayer();
+        expectedDataLayer.setCompName(expectedCompName);
+
+        assertEquals(expectedDataLayer, mav.getModel().get("dataLayer"));
+
+        verify(googleAnalyticsDataLayerRestServiceMock, only()).getCompetitionNameForAssessment(expectedAssessmentId);
+    }
+
+    @Test
+    public void postHandle_noParam() throws Exception {
+        when(httpServletRequestMock.getAttribute(URI_TEMPLATE_VARIABLES_ATTRIBUTE)).thenReturn(emptyMap());
+
+        googleAnalyticsDataLayerInterceptor.postHandle(httpServletRequestMock, httpServletResponseMock, null, mav);
+
+        GoogleTagManagerDataLayer expectedDataLayer = new GoogleTagManagerDataLayer();
+
+        assertEquals(expectedDataLayer, mav.getModel().get("dataLayer"));
+
+        verify(googleAnalyticsDataLayerRestServiceMock, never());
+    }
+}
