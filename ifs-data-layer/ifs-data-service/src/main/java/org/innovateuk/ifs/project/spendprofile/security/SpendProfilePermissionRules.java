5b342e30d56609eca88d951a3f98fbc222b54e01
Ravish Bhagdev <rav@hiveit.co.uk>
2017-11-15 15:59:42 +0000

    IFS-1308 Adding web layer unit tests , commit 1

--- ifs-data-layer/ifs-data-service/src/main/java/org/innovateuk/ifs/project/spendprofile/security/SpendProfilePermissionRules.java
+++ ifs-data-layer/ifs-data-service/src/main/java/org/innovateuk/ifs/project/spendprofile/security/SpendProfilePermissionRules.java
@@ -113,7 +113,7 @@ public class SpendProfilePermissionRules extends BasePermissionRules {
         return isLeadPartner(projectOrganisationCompositeId.getProjectId(), user.getId());
     }
 
-    @PermissionRule(value = "COMPLETE_SPEND_PROFILE_REVIEW", description = "Only a Project Manager can complete the projects spend profiles review")
+    @PermissionRule(value = "COMPLETE_SPEND_PROFILE_REVIEW", description = "Only a Project Manager can complete the project's spend profiles review")
     public boolean projectManagerCanCompleteSpendProfile(Long projectId, UserResource user) {
         return isProjectManager(projectId, user.getId());
     }
