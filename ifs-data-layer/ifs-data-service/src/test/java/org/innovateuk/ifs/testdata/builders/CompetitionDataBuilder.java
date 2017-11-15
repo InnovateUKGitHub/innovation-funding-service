6168d230adfecdeb2895a939b4afa1a9e67302d8
Wouter de Meijer <wdemeijer@worth.systems>
2017-11-15 17:22:40 +0100

    IFS-1117 - Fixed competition data builder adding registration date to all competitions

--- ifs-data-layer/ifs-data-service/src/test/java/org/innovateuk/ifs/testdata/builders/CompetitionDataBuilder.java
+++ ifs-data-layer/ifs-data-service/src/test/java/org/innovateuk/ifs/testdata/builders/CompetitionDataBuilder.java
@@ -246,7 +246,9 @@ public class CompetitionDataBuilder extends BaseDataBuilder<CompetitionData, Com
 
     public CompetitionDataBuilder withNewMilestones() {
         return asCompAdmin(data ->
-            Stream.of(MilestoneType.presetValues()).forEach(type ->
+            Stream.of(MilestoneType.presetValues())
+                    .filter(m -> !m.isOnlyNonIfs())
+                    .forEach(type ->
                 milestoneService.getMilestoneByTypeAndCompetitionId(type, data.getCompetition().getId())
                         .handleSuccessOrFailure(
                                 failure -> milestoneService.create(type, data.getCompetition().getId()).getSuccessObjectOrThrowException(),
