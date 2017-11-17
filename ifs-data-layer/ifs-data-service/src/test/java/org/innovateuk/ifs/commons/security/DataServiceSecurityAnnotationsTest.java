acf7446988f3dc5335ebe976ebccf2f8c5c816f1
richard <richard@hiveit.co.uk>
2017-11-17 16:48:29 +0000

    tidy

--- ifs-data-layer/ifs-data-service/src/test/java/org/innovateuk/ifs/commons/security/DataServiceSecurityAnnotationsTest.java
+++ ifs-data-layer/ifs-data-service/src/test/java/org/innovateuk/ifs/commons/security/DataServiceSecurityAnnotationsTest.java
@@ -1,51 +1,17 @@
 package org.innovateuk.ifs.commons.security;
 
-import org.innovateuk.ifs.commons.AbstractServiceSecurityAnnotationsTest;
-import org.innovateuk.ifs.security.StatelessAuthenticationFilter;
-import org.innovateuk.ifs.security.UidAuthenticationService;
-import org.springframework.security.access.prepost.PostAuthorize;
-import org.springframework.security.access.prepost.PostFilter;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.access.prepost.PreFilter;
-import org.springframework.stereotype.Service;
+import org.innovateuk.ifs.security.AbstractDataServiceSecurityAnnotationsTest;
 
-import java.lang.annotation.Annotation;
 import java.util.List;
 
-import static java.util.Arrays.asList;
 import static org.assertj.core.util.Lists.emptyList;
 
-public class DataServiceSecurityAnnotationsTest extends AbstractServiceSecurityAnnotationsTest {
-
-    // TODO push down to a sub class?
-    @Override
-    protected List<Class<?>> excludedClasses() {
-        return asList(
-                UidAuthenticationService.class,
-                StatelessAuthenticationFilter.class
-        );
-    }
-
+public class DataServiceSecurityAnnotationsTest extends AbstractDataServiceSecurityAnnotationsTest {
 
     @Override
-    protected List<Class<? extends Annotation>> classLevelSecurityAnnotations() {
+    protected List<Class<?>> additionalExcludedClasses() {
         return emptyList();
     }
 
-    @Override
-    protected List<Class<? extends Annotation>> annotationsOnClassesToSecure() {
-        return asList(Service.class);
-    }
-
-    @Override
-    protected List<Class<? extends Annotation>> methodLevelSecurityAnnotations() {
-        return asList(
-                PreAuthorize.class,
-                PreFilter.class,
-                PostAuthorize.class,
-                PostFilter.class,
-                NotSecured.class
-        );
-    }
 
 }
