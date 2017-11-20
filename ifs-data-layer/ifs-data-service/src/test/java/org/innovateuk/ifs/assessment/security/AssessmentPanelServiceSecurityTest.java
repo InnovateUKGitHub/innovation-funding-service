1862ae8c234d901dcb876fe8ef10539ab4e45968
Ewan Cormack <ewan@hiveit.co.uk>
2017-11-20 13:43:10 +0000

    IFS-2039: Add security to data tier service and test to cover security

---
+++ ifs-data-layer/ifs-data-service/src/test/java/org/innovateuk/ifs/assessment/security/AssessmentPanelServiceSecurityTest.java
@@ -0,0 +1,44 @@
+package org.innovateuk.ifs.assessment.security;
+
+import org.innovateuk.ifs.BaseServiceSecurityTest;
+import org.innovateuk.ifs.assessment.transactional.AssessmentPanelService;
+import org.innovateuk.ifs.commons.service.ServiceResult;
+import org.junit.Test;
+
+import static org.innovateuk.ifs.user.resource.UserRoleType.COMP_ADMIN;
+import static org.innovateuk.ifs.user.resource.UserRoleType.PROJECT_FINANCE;
+
+public class AssessmentPanelServiceSecurityTest extends BaseServiceSecurityTest<AssessmentPanelService> {
+
+    private long applicationId = 1L;
+
+    @Override
+    protected Class<? extends AssessmentPanelService> getClassUnderTest() {
+        return TestAssessmentPanelService.class;
+    }
+
+    @Test
+    public void assignApplicationToPanel() throws Exception {
+
+        testOnlyAUserWithOneOfTheGlobalRolesCan(() -> classUnderTest.assignApplicationToPanel(applicationId), COMP_ADMIN, PROJECT_FINANCE);
+    }
+
+    @Test
+    public void unAssignApplicationFromPanel() throws Exception {
+
+        testOnlyAUserWithOneOfTheGlobalRolesCan(() -> classUnderTest.unAssignApplicationFromPanel(applicationId), COMP_ADMIN, PROJECT_FINANCE);
+    }
+
+    public static class TestAssessmentPanelService implements AssessmentPanelService {
+
+        @Override
+        public ServiceResult<Void> assignApplicationToPanel(long applicationId) {
+            return null;
+        }
+
+        @Override
+        public ServiceResult<Void> unAssignApplicationFromPanel(long applicationId) {
+            return null;
+        }
+    }
+}
