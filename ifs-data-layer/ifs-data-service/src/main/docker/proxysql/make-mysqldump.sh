831f4897f5646ca9ec8c2dc27d5f41c75ab12d1b
richard <richard@hiveit.co.uk>
2017-11-23 14:03:01 +0000

    having an extended insert dump as well

--- ifs-data-layer/ifs-data-service/src/main/docker/proxysql/make-mysqldump.sh
+++ ifs-data-layer/ifs-data-service/src/main/docker/proxysql/make-mysqldump.sh
@@ -2,6 +2,7 @@
 
 # take the data dump
 mysqldump -u$DB_USER -p$DB_PASS -h127.0.0.1 -P6033 $DB_NAME --skip-extended-insert > /dump/anonymised-dump.sql
+mysqldump -u$DB_USER -p$DB_PASS -h127.0.0.1 -P6033 $DB_NAME > /dump/anonymised-dump-extended-insert.sql
 
 # encrypt the dump file before transit and finally remove the unprotected file
 gpg --yes --batch --passphrase=$DB_PASS -c /dump/anonymised-dump.sql
