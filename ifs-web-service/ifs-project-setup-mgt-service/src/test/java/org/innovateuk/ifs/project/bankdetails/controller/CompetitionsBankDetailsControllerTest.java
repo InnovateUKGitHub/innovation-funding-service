f88a1024db02c00eb9ff84bf76e1cb79e790cd93
Bronnyl Dias <bdias@worth.systems>
2017-11-29 18:21:51 +0000

    IFS-2015-PF-BankDetails-Action
    Web Layer junits.
    
    Change-Id: Ib6565ec0d948ebaf6cfb233a5f9007bd2a5c7db5

---
+++ ifs-web-service/ifs-project-setup-mgt-service/src/test/java/org/innovateuk/ifs/project/bankdetails/controller/CompetitionsBankDetailsControllerTest.java
@@ -0,0 +1,41 @@
+package org.innovateuk.ifs.project.bankdetails.controller;
+
+import org.innovateuk.ifs.BaseControllerMockMVCTest;
+import org.innovateuk.ifs.competition.resource.BankDetailsReviewResource;
+import org.innovateuk.ifs.project.bankdetails.viewmodel.CompetitionPendingBankDetailsApprovalsViewModel;
+import org.junit.Test;
+import org.springframework.test.web.servlet.MvcResult;
+
+import java.util.List;
+
+import static java.util.Collections.singletonList;
+import static org.innovateuk.ifs.commons.rest.RestResult.restSuccess;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+public class CompetitionsBankDetailsControllerTest extends BaseControllerMockMVCTest<CompetitionsBankDetailsController> {
+
+    @Override
+    protected CompetitionsBankDetailsController supplyControllerUnderTest() {
+        return new CompetitionsBankDetailsController();
+    }
+
+    @Test
+    public void viewPendingBankDetailsApprovals() throws Exception {
+
+        List<BankDetailsReviewResource> pendingBankDetails = singletonList(new BankDetailsReviewResource(1L, 11L, "Comp1", 12L, "project1", 22L, "Org1"));
+        when(bankDetailsRestService.getPendingBankDetailsApprovals()).thenReturn(restSuccess(pendingBankDetails));
+
+        MvcResult result = mockMvc.perform(get("/competitions/status/pending-bank-details-approvals")).
+                andExpect(view().name("project/competition-pending-bank-details")).
+                andExpect(status().isOk()).
+                andReturn();
+
+        CompetitionPendingBankDetailsApprovalsViewModel model = (CompetitionPendingBankDetailsApprovalsViewModel) result.getModelAndView().getModel().get("model");
+
+        assertEquals(model, new CompetitionPendingBankDetailsApprovalsViewModel(pendingBankDetails));
+    }
+}
