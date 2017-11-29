e22f450f01a48e5f1cf47505eadf9b242f2abd35
Bronnyl Dias <bdias@worth.systems>
2017-11-29 16:52:50 +0000

    IFS-2015-PF-BankDetails-Action
    Refactoring.
    
    Change-Id: I2c308241fd6ff7b34c61b594afa8ab40c33ec904

--- ifs-data-layer/ifs-data-service/src/main/java/org/innovateuk/ifs/project/bankdetails/repository/BankDetailsRepository.java
+++ ifs-data-layer/ifs-data-service/src/main/java/org/innovateuk/ifs/project/bankdetails/repository/BankDetailsRepository.java
@@ -1,12 +1,25 @@
 package org.innovateuk.ifs.project.bankdetails.repository;
 
+import org.innovateuk.ifs.competition.resource.BankDetailsReviewResource;
 import org.innovateuk.ifs.project.bankdetails.domain.BankDetails;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import java.util.List;
 
 public interface BankDetailsRepository extends PagingAndSortingRepository<BankDetails, Long> {
+
+    String PENDING_BANK_DETAILS_APPROVALS_QUERY = " SELECT NEW org.innovateuk.ifs.competition.resource.BankDetailsReviewResource("
+            + " p.application.id, c.id, c.name, p.id, p.name, bd.organisation.id, bd.organisation.name)"
+            + " FROM Competition c, Project p, BankDetails bd"
+            + " WHERE p.application.competition.id = c.id"
+            + " AND bd.project.id = p.id"
+            + " AND bd.manualApproval = FALSE";
+
     BankDetails findByProjectIdAndOrganisationId(Long projectId, Long organisationId);
     List<BankDetails> findByProjectId(Long projectId);
     List<BankDetails> findByProjectApplicationCompetitionId(Long competitionId);
+
+    @Query(PENDING_BANK_DETAILS_APPROVALS_QUERY)
+    List<BankDetailsReviewResource> getPendingBankDetailsApprovals();
 }
--- ifs-data-layer/ifs-data-service/src/main/java/org/innovateuk/ifs/project/bankdetails/transactional/BankDetailsService.java
+++ ifs-data-layer/ifs-data-service/src/main/java/org/innovateuk/ifs/project/bankdetails/transactional/BankDetailsService.java
@@ -1,5 +1,6 @@
 package org.innovateuk.ifs.project.bankdetails.transactional;
 
+import org.innovateuk.ifs.competition.resource.BankDetailsReviewResource;
 import org.innovateuk.ifs.project.bankdetails.resource.BankDetailsResource;
 import org.innovateuk.ifs.project.bankdetails.resource.ProjectBankDetailsStatusSummary;
 import org.innovateuk.ifs.commons.security.SecuredBySpring;
@@ -7,6 +8,8 @@ import org.innovateuk.ifs.commons.service.ServiceResult;
 import org.springframework.security.access.method.P;
 import org.springframework.security.access.prepost.PostAuthorize;
 import org.springframework.security.access.prepost.PreAuthorize;
+
+import java.util.List;
 
 public interface BankDetailsService {
     @PostAuthorize("hasPermission(returnObject, 'READ')")
@@ -24,4 +27,8 @@ public interface BankDetailsService {
     @PreAuthorize("hasAuthority('project_finance')")
     @SecuredBySpring(value = "READ", description = "Project Finance users can see bank details status summary for all partners", securedType = ProjectBankDetailsStatusSummary.class)
     ServiceResult<ProjectBankDetailsStatusSummary> getProjectBankDetailsStatusSummary(final Long projectId);
+
+    @PreAuthorize("hasAnyAuthority('project_finance')")
+    @SecuredBySpring(value = "GET_PENDING_BANK_DETAILS_APPROVALS", description = "Project finance users can get organisations for which Bank Details approval is pending")
+    ServiceResult<List<BankDetailsReviewResource>> getPendingBankDetailsApprovals();
 }
