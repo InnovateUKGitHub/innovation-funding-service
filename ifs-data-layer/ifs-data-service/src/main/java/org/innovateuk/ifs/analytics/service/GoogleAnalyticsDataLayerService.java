126b93ad82490080667a54223246c5aa65f3b26c
Tom Baldwin <tom@polymorphism.co.uk>
2017-11-30 12:31:09 +0000

    IFS-2113 - Fixes security descriptions in GoogleAnalyticsDataLayerService.

--- ifs-data-layer/ifs-data-service/src/main/java/org/innovateuk/ifs/analytics/service/GoogleAnalyticsDataLayerService.java
+++ ifs-data-layer/ifs-data-service/src/main/java/org/innovateuk/ifs/analytics/service/GoogleAnalyticsDataLayerService.java
@@ -6,18 +6,19 @@ import org.springframework.security.access.prepost.PreAuthorize;
 
 public interface GoogleAnalyticsDataLayerService {
 
-    @SecuredBySpring(value = "READ", description = "Only those with either comp admin or project finance roles can read the status related to setup")
+    @SecuredBySpring(value = "READ", description = "Any authenticated user can get any competition name by application ID")
     @PreAuthorize("isAuthenticated()")
     ServiceResult<String> getCompetitionNameByApplicationId(long applicationId);
 
+    @SecuredBySpring(value = "READ", description = "Any anonymous or authenticated user can get any competition name by project ID")
     @PreAuthorize("permitAll")
     ServiceResult<String> getCompetitionName(long competitionId);
 
-    @SecuredBySpring(value = "READ", description = "Only those with either comp admin or project finance roles can read the status related to setup")
+    @SecuredBySpring(value = "READ", description = "Any authenticated user can get any competition name by project ID")
     @PreAuthorize("isAuthenticated()")
     ServiceResult<String> getCompetitionNameByProjectId(long projectId);
 
-    @SecuredBySpring(value = "READ", description = "Only those with either comp admin or project finance roles can read the status related to setup")
+    @SecuredBySpring(value = "READ", description = "Any authenticated user can get any competition name by assessment ID")
     @PreAuthorize("isAuthenticated()")
     ServiceResult<String> getCompetitionNameByAssessmentId(long assessmentId);
 }
