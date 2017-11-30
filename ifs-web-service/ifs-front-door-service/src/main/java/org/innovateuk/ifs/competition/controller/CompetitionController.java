509d9e424ae68d9f0af7418cacecc6a467664dea
Ewan Cormack <ewan@hiveit.co.uk>
2017-11-30 10:35:00 +0000

    IFS-2284: Address review comments

--- ifs-web-service/ifs-front-door-service/src/main/java/org/innovateuk/ifs/competition/controller/CompetitionController.java
+++ ifs-web-service/ifs-front-door-service/src/main/java/org/innovateuk/ifs/competition/controller/CompetitionController.java
@@ -52,8 +52,8 @@ public class CompetitionController {
     @GetMapping("info/terms-and-conditions")
     public String termsAndConditions(@PathVariable("competitionId") final long competitionId) {
         CompetitionResource compResource = competitionService.getById(competitionId);
-
-        return "APC".equals(compResource.getCompetitionTypeName()) ?
+        final Long APC_COMPETITION_TYPE_ID = 7L;
+        return APC_COMPETITION_TYPE_ID.equals(compResource.getCompetitionType()) ?
                 "competition/info/apc-terms-and-conditions" : "competition/info/default-terms-and-conditions";
     }
 }
