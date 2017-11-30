135d4fd0d15c30f43f11b19cb77f2c49e4d7bc4c
Ravish Bhagdev <rav@hiveit.co.uk>
2017-11-30 21:51:59 +0000

    IFS-2285 Adding patches to update new field for pre-existing competitions.

--- ifs-data-layer/ifs-data-service/src/main/resources/db/migration/V105_6_0__Insert_column_to_store_allowed_file_types_to_form_input.sql
+++
@@ -1,5 +0,0 @@
-/**
-Column to be used to store file types as org.springframework.http.MediaType comma separated string.
-Can be NULL for form inputs that are not of type FILE
-**/
-ALTER TABLE `ifs`.`form_input` ADD COLUMN `allowed_file_types` LONGTEXT NULL DEFAULT NULL AFTER `active`;
