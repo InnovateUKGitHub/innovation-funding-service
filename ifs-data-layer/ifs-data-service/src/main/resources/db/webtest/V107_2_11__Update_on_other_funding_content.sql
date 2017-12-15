88e9daa9585169c0a8651a2f507ad53392bb2e74
Grace Williams <grace.williams@innovateuk.gov.uk>
2017-12-15 09:45:46 +0000

    IFS-2317: Added new script to update the content on Other funding question

---
+++ ifs-data-layer/ifs-data-service/src/main/resources/db/webtest/V107_2_11__Update_on_other_funding_content.sql
@@ -0,0 +1,3 @@
+update question
+set description='Tell us if you have received any other public sector funding for this project. This information is important as other public sector support counts as part of the funding you can receive for your project.'
+where name='Other funding';
