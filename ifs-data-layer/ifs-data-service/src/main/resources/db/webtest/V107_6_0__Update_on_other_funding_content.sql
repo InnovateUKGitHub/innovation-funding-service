70601a46f1ceb03b5b791e7a63b6cf7355e0674d
Grace Williams <grace.williams@innovateuk.gov.uk>
2017-12-18 12:12:04 +0000

    IFS-2317: Capitalised mysql keywords

--- ifs-data-layer/ifs-data-service/src/main/resources/db/webtest/V107_6_0__Update_on_other_funding_content.sql
+++ ifs-data-layer/ifs-data-service/src/main/resources/db/webtest/V107_6_0__Update_on_other_funding_content.sql
@@ -1,4 +1,4 @@
 --Update the first line of the content on Other funding question
-update question
-set description='Tell us if you have received any other public sector funding for this project. This information is important as other public sector support counts as part of the funding you can receive for your project.'
-where description like '%or received any other public sector funding for this project. This information is important as other public sector%' and question_type <> 'GENERAL';
+UPDATE question
+SET description='Tell us if you have received any other public sector funding for this project. This information is important as other public sector support counts as part of the funding you can receive for your project.'
+WHERE description LIKE '%or received any other public sector funding for this project. This information is important as other public sector%' AND question_type <> 'GENERAL';
