ffa1bcd5ff4a7e78c8721d8b170f9426da5988ac
glenn <gauld@worth.systems>
2017-12-18 16:52:44 +0000

    IFS-2376 Update to show application finance total correctly

--- ifs-web-service/ifs-web-core/src/main/java/org/innovateuk/ifs/application/finance/view/ApplicationFinanceOverviewModelManager.java
+++ ifs-web-service/ifs-web-core/src/main/java/org/innovateuk/ifs/application/finance/view/ApplicationFinanceOverviewModelManager.java
@@ -18,6 +18,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.ui.Model;
 
+import java.math.BigDecimal;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -60,7 +61,7 @@ public class ApplicationFinanceOverviewModelManager implements FinanceOverviewMo
         Map<Long, BaseFinanceResource> organisationFinances = organisationFinanceOverview.getFinancesByOrganisation();
         model.addAttribute("organisationFinances", organisationFinances);
         model.addAttribute("academicFileEntries", organisationFinanceOverview.getAcademicOrganisationFileEntries());
-        model.addAttribute("totalFundingSought", organisationFinanceOverview.getTotalFundingSought());
+        model.addAttribute("totalFundingSought", organisationFinanceOverview.getTotalFundingSought() );
         model.addAttribute("totalContribution", organisationFinanceOverview.getTotalContribution());
         model.addAttribute("totalOtherFunding", organisationFinanceOverview.getTotalOtherFunding());
         model.addAttribute("researchParticipationPercentage", applicationFinanceRestService.getResearchParticipationPercentage(applicationId).getSuccessObjectOrThrowException());
@@ -118,12 +119,16 @@ public class ApplicationFinanceOverviewModelManager implements FinanceOverviewMo
         Map<Long, BaseFinanceResource> organisationFinances = organisationFinanceOverview.getFinancesByOrganisation();
         viewModel.setOrganisationFinances(organisationFinances);
         viewModel.setAcademicFileEntries(organisationFinanceOverview.getAcademicOrganisationFileEntries());
-        viewModel.setTotalFundingSought(organisationFinanceOverview.getTotalFundingSought());
+        viewModel.setTotalFundingSought(getDisplayValue(organisationFinanceOverview.getTotalFundingSought()));
         viewModel.setTotalContribution(organisationFinanceOverview.getTotalContribution());
         viewModel.setTotalOtherFunding(organisationFinanceOverview.getTotalOtherFunding());
         viewModel.setResearchParticipationPercentage(applicationFinanceRestService.getResearchParticipationPercentage(applicationId).getSuccessObjectOrThrowException());
 
         return viewModel;
+    }
+
+    private BigDecimal getDisplayValue(BigDecimal value) {
+        return value != null ? value : BigDecimal.ZERO;
     }
 
     private void addFinanceSections(Long competitionId, BaseFinanceOverviewViewModel viewModel) {
