ecbcbd2065a26ff4b4a8d7058414622a944fc6a1
Wouter de Meijer <wdemeijer@worth.systems>
2017-12-20 17:20:01 +0100

    IFS-2493 - Reworked how ifs-data-layer gradle tasks structure to allow database initialization for both finance and data services

---
+++ ifs-data-layer/ifs-finance-data-service/src/main/resources/db/migration/V1_0_0__Initial_totals_table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE `cost_total` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `finance_id` bigint(20) NOT NULL,
+  `type` varchar(255) COLLATE utf8_bin NOT NULL,
+  `name` varchar(255) COLLATE utf8_bin NOT NULL,
+  `total` bigint(20) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY (`finance_id`,`type`,`name`)
+)
