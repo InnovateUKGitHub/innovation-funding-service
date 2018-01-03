553bbdb92b40ae51ef89fb8a3be18d6a50ef1470
Adam Birse <abirse@worth.systems>
2018-01-03 11:33:37 +0000

    non root working for data-service

--- setup-files/scripts/openshift/os-deploy.sh
+++ setup-files/scripts/openshift/os-deploy.sh
@@ -26,25 +26,22 @@ function deploy() {
         oc create -f $(getBuildLocation)/gluster/10-gluster-svc.yml ${SVC_ACCOUNT_CLAUSE}
         oc create -f $(getBuildLocation)/gluster/11-gluster-endpoints.yml ${SVC_ACCOUNT_CLAUSE}
         oc create -f $(getBuildLocation)/gluster/named-envs/12-${TARGET}-file-upload-claim.yml ${SVC_ACCOUNT_CLAUSE}
-#    else
-#        oc create -f $(getBuildLocation)/shib/55-ldap.yml ${SVC_ACCOUNT_CLAUSE}
-#        oc create -f $(getBuildLocation)/mail/ ${SVC_ACCOUNT_CLAUSE}
-#        oc create -f $(getBuildLocation)/mysql/ ${SVC_ACCOUNT_CLAUSE}
-#        oc create -f $(getBuildLocation)/gluster/ ${SVC_ACCOUNT_CLAUSE}
-#        oc create -f $(getBuildLocation)/spring-admin/ ${SVC_ACCOUNT_CLAUSE}
+    else
+        oc create -f $(getBuildLocation)/shib/55-ldap.yml ${SVC_ACCOUNT_CLAUSE}
+        oc create -f $(getBuildLocation)/mail/ ${SVC_ACCOUNT_CLAUSE}
+        oc create -f $(getBuildLocation)/mysql/ ${SVC_ACCOUNT_CLAUSE}
+        oc create -f $(getBuildLocation)/gluster/ ${SVC_ACCOUNT_CLAUSE}
+        oc create -f $(getBuildLocation)/spring-admin/ ${SVC_ACCOUNT_CLAUSE}
     fi
 
     # The SIL stub is required in all environments, in one form or another, except for production
-#    if ! $(isProductionEnvironment ${TARGET}); then
-#        oc create -f $(getBuildLocation)/sil-stub/ ${SVC_ACCOUNT_CLAUSE}
-#    fi
+    if ! $(isProductionEnvironment ${TARGET}); then
+        oc create -f $(getBuildLocation)/sil-stub/ ${SVC_ACCOUNT_CLAUSE}
+    fi
 
-    oc create -f $(getBuildLocation)/4-application-service.yml ${SVC_ACCOUNT_CLAUSE}
-    oc create -f $(getBuildLocation)/31-data-service.yml ${SVC_ACCOUNT_CLAUSE}
-
-#    oc create -f $(getBuildLocation)/ ${SVC_ACCOUNT_CLAUSE}
-#    oc create -f $(getBuildLocation)/shib/5-shib.yml ${SVC_ACCOUNT_CLAUSE}
-#    oc create -f $(getBuildLocation)/shib/56-idp.yml ${SVC_ACCOUNT_CLAUSE}
+    oc create -f $(getBuildLocation)/ ${SVC_ACCOUNT_CLAUSE}
+    oc create -f $(getBuildLocation)/shib/5-shib.yml ${SVC_ACCOUNT_CLAUSE}
+    oc create -f $(getBuildLocation)/shib/56-idp.yml ${SVC_ACCOUNT_CLAUSE}
 }
 
 function shibInit() {
