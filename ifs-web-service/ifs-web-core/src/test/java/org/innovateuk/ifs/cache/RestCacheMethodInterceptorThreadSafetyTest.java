fd551c9b45ccc24f235b2d5b4875d7f5c2cf1d22
Duncan Watson <duncan@hiveit.co.uk>
2018-01-05 11:42:55 +0000

    Added tests for ensuring that reads from the rest cache block writes until complete

--- ifs-web-service/ifs-web-core/src/test/java/org/innovateuk/ifs/cache/RestCacheMethodInterceptorThreadSafetyTest.java
+++ ifs-web-service/ifs-web-core/src/test/java/org/innovateuk/ifs/cache/RestCacheMethodInterceptorThreadSafetyTest.java
@@ -3,6 +3,7 @@ package org.innovateuk.ifs.cache;
 import com.google.common.cache.Cache;
 import org.aopalliance.intercept.MethodInvocation;
 import org.apache.commons.lang3.tuple.Pair;
+import org.apache.commons.lang3.tuple.Triple;
 import org.innovateuk.ifs.BaseUnitTestMocksTest;
 import org.junit.Test;
 import org.mockito.InjectMocks;
@@ -19,10 +20,14 @@ import java.util.concurrent.locks.ReadWriteLock;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.hamcrest.Matchers.hasSize;
 import static org.innovateuk.ifs.async.ReadWriteLockTestHelper.isReadLocked;
 import static org.innovateuk.ifs.async.ReadWriteLockTestHelper.isWriteLocked;
 import static org.innovateuk.ifs.util.CollectionFunctions.simpleMap;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 import static org.mockito.Matchers.eq;
 import static org.mockito.Matchers.isA;
 import static org.mockito.Mockito.when;
@@ -133,5 +138,99 @@ public class RestCacheMethodInterceptorThreadSafetyTest extends BaseUnitTestMock
         // {@link RestCacheMethodInterceptor#get}
         List<Boolean> successfulOperationLockStates = simpleMap(successfulCalls, Pair::getRight);
         assertTrue(successfulOperationLockStates.stream().allMatch(state -> state));
+    }
+
+    /**
+     * This test asserts that an operation holding the read lock in the {@link RestCacheMethodInterceptor#get} method
+     * will block a second operation from being able to write its results to the cache.
+     */
+    @Test
+    public void testReadOperationBlocksWriteOperations() throws Throwable {
+
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
+
+        CountDownLatch readOperationLatch = new CountDownLatch(1);
+        CountDownLatch writeOperationLatch = new CountDownLatch(1);
+        CountDownLatch completeTestLatch = new CountDownLatch(1);
+
+        ReadWriteLock lockFromInterceptor =
+                (ReadWriteLock) ReflectionTestUtils.getField(interceptor, "lock");
+
+        when(methodInvocationMock.getArguments()).thenReturn(new Object[] {"id", 123});
+
+        when(uidSupplierMock.get()).thenReturn("http-request-1");
+
+        Queue<Triple<String, Boolean, Boolean>> successfulCalls = new ConcurrentLinkedQueue<>();
+
+        ThreadLocal<String> namedCallsThreadLocal = new ThreadLocal<>();
+
+        when(cacheMock.get(eq("http-request-1"), isA(Callable.class))).thenAnswer(invocation -> {
+
+            String operationName = namedCallsThreadLocal.get();
+
+            successfulCalls.add(Triple.of(operationName, isReadLocked(lockFromInterceptor), isWriteLocked(lockFromInterceptor)));
+            writeOperationLatch.countDown();
+            readOperationLatch.await();
+
+            return new HashMap<>();
+        });
+
+        Future<Object> readOperationFuture = executorService.submit(() -> {
+            try {
+                namedCallsThreadLocal.set("Read Operation");
+                return interceptor.invoke(methodInvocationMock);
+            } catch (Throwable e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+        Future<Object> writeOperationFuture = executorService.submit(() -> {
+            try {
+                writeOperationLatch.await();
+                namedCallsThreadLocal.set("Write Operation");
+                Object result = interceptor.invoke(methodInvocationMock);
+                completeTestLatch.countDown();
+                return result;
+            } catch (Throwable e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+        try {
+            writeOperationFuture.get(50, TimeUnit.MILLISECONDS);
+            fail("This write operation should have timed out because it was blocked by the read lock");
+        } catch (TimeoutException e) {
+            // expected behaviour - the write operation was successfully blocked from writing to the cache.  Now allow
+            // the readOperation to complete (which in turn allows the writeOperation to proceed) and wait on the
+            // completeTestLatch until both the read and the write are fully finished.
+            assertTrue(isReadLocked(lockFromInterceptor));
+            assertFalse(isWriteLocked(lockFromInterceptor));
+            readOperationLatch.countDown();
+            completeTestLatch.await();
+        }
+
+        // assert that all operations were successful (both reads and both writes of the cache results)
+        List<String> successfulOperationNames = simpleMap(successfulCalls, Triple::getLeft);
+        assertThat(successfulOperationNames, hasSize(4));
+
+        // we know the order of the first writes because it is "Read Operation" that initially acquires and holds the
+        // read lock
+        List<String> successfulInitialReads = successfulOperationNames.subList(0, 2);
+        assertThat(successfulInitialReads, contains("Read Operation", "Write Operation"));
+
+        // and we expect both operations to eventually write their results to the cache, but we can't guarantee the
+        // order of this as they are both freed up at the same time by "Read Operation" unblocking
+        List<String> successfulResultWrites = successfulOperationNames.subList(2, 4);
+        assertThat(successfulResultWrites, containsInAnyOrder("Read Operation", "Write Operation"));
+
+        // Assert that the read lock was on for both operations reading from the cache, but not on when the
+        // operations then successfully wrote their results to the cache
+        //
+        // In turn, assert that the write lock was not on during the 2 operations' reads from the cache, but was on
+        // during the operations' writing their results to the cache
+        List<Boolean> successfulOperationReadLockStates = simpleMap(successfulCalls, Triple::getMiddle);
+        List<Boolean> successfulOperationWriteLockStates = simpleMap(successfulCalls, Triple::getRight);
+        assertThat(successfulOperationReadLockStates, contains(true, true, false, false));
+        assertThat(successfulOperationWriteLockStates, contains(false, false, true, true));
     }
 }
