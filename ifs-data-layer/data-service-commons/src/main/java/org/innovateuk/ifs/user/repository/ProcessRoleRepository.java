d09ae6c7148b8b3143bb8bb5aa2bab7c513dd78a
glenn <gauld@worth.systems>
2018-01-08 15:06:11 +0000

    IFS-1138 Updated method in process role repository

--- ifs-data-layer/data-service-commons/src/main/java/org/innovateuk/ifs/user/repository/ProcessRoleRepository.java
+++ ifs-data-layer/data-service-commons/src/main/java/org/innovateuk/ifs/user/repository/ProcessRoleRepository.java
@@ -24,5 +24,5 @@ public interface ProcessRoleRepository extends PagingAndSortingRepository<Proces
     List<ProcessRole> findByApplicationIdAndOrganisationId(Long applicationId, Long organisationId);
     ProcessRole findByUserIdAndApplicationId(Long userId, Long applicationId);
     ProcessRole findByUserIdAndRoleIdAndApplicationIdAndOrganisationId(Long userId, Long roleId, Long applicationId, Long organisationId);
-    Boolean existsByUserIdAndApplicationId(Long userId, Long applicationId);
+    boolean existsByUserIdAndApplicationId(long userId, long applicationId);
 }
