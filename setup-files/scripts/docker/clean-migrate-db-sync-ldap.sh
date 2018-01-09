7b7e97b96cb009fa29ce2383670fdfb16554a071
Ravish Bhagdev <rav@hiveit.co.uk>
2018-01-09 15:50:19 +0000

    Tidying up test messages

--- setup-files/scripts/docker/clean-migrate-db-sync-ldap.sh
+++ setup-files/scripts/docker/clean-migrate-db-sync-ldap.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 # R Popple: Depending on environment variables clean migrate the database and sync it with ldap.
-
-echo "*******TEST*********n\n\n\n"
-cat /flyway/flyway.conf
-
 if ! [ $ONLY_SYNC_LDAP = "true" ] ; then
 . /clean-migrate-db.sh
 fi
