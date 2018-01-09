17be9d54480cb1a97134c1e4b899ab0f952497d6
Nicholas Tsim <nicholastsim@gmail.com>
2018-01-09 15:24:28 +0000

    IFS-2335 - Update setup patch to set `full_application_finance = null` and `max_research_ratio = 0` for existing competitions
    Also corrects the patch version names.

--- ifs-data-layer/ifs-data-service/src/main/resources/db/setup/V109_3_1__Update_EOI_full_application_finance_to_null.sql
+++
@@ -1,3 +0,0 @@
--- IFS-2335 - EOI competitions should have a nulled `full_application_finance` column to represent having 'No Finances'.
-
-UPDATE competition SET full_application_finance = null WHERE name = 'Template for the Expression of interest competition type';
