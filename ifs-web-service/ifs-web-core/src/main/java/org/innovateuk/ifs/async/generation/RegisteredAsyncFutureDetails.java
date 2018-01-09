8b3dface74d3cf0c55e1195522e2615bec430bc5
Duncan Watson <duncan@hiveit.co.uk>
2018-01-09 08:56:02 +0000

    Removing wildcard as per SonarQube recommendations

--- ifs-web-service/ifs-web-core/src/main/java/org/innovateuk/ifs/async/generation/RegisteredAsyncFutureDetails.java
+++ ifs-web-service/ifs-web-core/src/main/java/org/innovateuk/ifs/async/generation/RegisteredAsyncFutureDetails.java
@@ -11,19 +11,19 @@ import static org.innovateuk.ifs.util.CollectionFunctions.combineLists;
 /**
  * A holder of useful information about a Future and how it was created
  */
-public class RegisteredAsyncFutureDetails {
+public class RegisteredAsyncFutureDetails<T> {
 
-    private CompletableFuture<?> future;
+    private CompletableFuture<T> future;
     private String futureName;
     private AsyncFutureDetails parentFutureDetails;
 
-    public RegisteredAsyncFutureDetails(CompletableFuture<?> future, String futureName, AsyncFutureDetails asyncFutureDetails) {
+    public RegisteredAsyncFutureDetails(CompletableFuture<T> future, String futureName, AsyncFutureDetails asyncFutureDetails) {
         this.future = future;
         this.futureName = futureName;
         this.parentFutureDetails = asyncFutureDetails;
     }
 
-    public CompletableFuture<?> getFuture() {
+    public CompletableFuture<T> getFuture() {
         return future;
     }
 
@@ -41,21 +41,29 @@ public class RegisteredAsyncFutureDetails {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
+        if (this == o) {
+            return true;
+        }
 
-        if (o == null || getClass() != o.getClass()) return false;
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
 
-        RegisteredAsyncFutureDetails that = (RegisteredAsyncFutureDetails) o;
+        RegisteredAsyncFutureDetails<?> that = (RegisteredAsyncFutureDetails<?>) o;
 
         return new EqualsBuilder()
+                .append(future, that.future)
                 .append(futureName, that.futureName)
+                .append(parentFutureDetails, that.parentFutureDetails)
                 .isEquals();
     }
 
     @Override
     public int hashCode() {
         return new HashCodeBuilder(17, 37)
+                .append(future)
                 .append(futureName)
+                .append(parentFutureDetails)
                 .toHashCode();
     }
 }
