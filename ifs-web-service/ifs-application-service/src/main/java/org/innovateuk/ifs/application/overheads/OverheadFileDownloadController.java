fba0767e30508ca58baf9826f0cc66d3d76b7933
Adam Mapstone <adam.mapstone@innovateuk.gov.uk>
2018-01-09 11:21:30 +0000

    SonarQube - InterruptedException removed and unused imports removed.

--- ifs-web-service/ifs-application-service/src/main/java/org/innovateuk/ifs/application/overheads/OverheadFileDownloadController.java
+++ ifs-web-service/ifs-application-service/src/main/java/org/innovateuk/ifs/application/overheads/OverheadFileDownloadController.java
@@ -32,7 +32,7 @@ public class OverheadFileDownloadController {
     @GetMapping("/{overheadId}")
     public @ResponseBody
     ResponseEntity<ByteArrayResource> downloadQuestionFile(
-            @PathVariable("overheadId") final Long overheadId) throws ExecutionException, InterruptedException {
+            @PathVariable("overheadId") final Long overheadId) throws ExecutionException {
 
         final ByteArrayResource resource = overheadFileRestService.getOverheadFile(overheadId).getSuccessObjectOrThrowException();
         final FileEntryResource fileEntryResource = overheadFileRestService.getOverheadFileDetails(overheadId).getSuccessObjectOrThrowException();
