e0db1d56c2bdbc93fd1ea4706a15f861d4b3dc73
glenn <gauld@worth.systems>
2018-01-12 15:01:21 +0000

    IFS-29 Initial updates to controller

--- ifs-web-service/ifs-assessment-service/src/main/java/org/innovateuk/ifs/assessment/review/controller/AssessmentReviewApplicationSummaryController.java
+++ ifs-web-service/ifs-assessment-service/src/main/java/org/innovateuk/ifs/assessment/review/controller/AssessmentReviewApplicationSummaryController.java
@@ -1,7 +1,15 @@
 package org.innovateuk.ifs.assessment.review.controller;
 
+import org.innovateuk.ifs.application.resource.ApplicationResource;
+import org.innovateuk.ifs.application.service.ApplicationService;
+import org.innovateuk.ifs.application.service.CompetitionService;
+import org.innovateuk.ifs.application.service.SectionService;
 import org.innovateuk.ifs.assessment.review.populator.AssessmentReviewApplicationSummaryModelPopulator;
 import org.innovateuk.ifs.commons.security.SecuredBySpring;
+import org.innovateuk.ifs.competition.resource.CompetitionResource;
+import org.innovateuk.ifs.form.resource.FormInputResponseResource;
+import org.innovateuk.ifs.form.service.FormInputResponseRestService;
+import org.innovateuk.ifs.form.service.FormInputResponseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
@@ -9,6 +17,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 /**
  * Controller to manage display of applications in panel review
@@ -22,10 +32,44 @@ public class AssessmentReviewApplicationSummaryController {
     @Autowired
     private AssessmentReviewApplicationSummaryModelPopulator assessmentReviewApplicationSummaryModelPopulator;
 
-    @GetMapping("application/{applicationId}")
+    @Autowired
+    private FormInputResponseService formInputResponseService;
+
+    @Autowired
+    private FormInputResponseRestService formInputResponseRestService;
+
+    @Autowired
+    private SectionService sectionService;
+
+    @Autowired
+    private ApplicationService applicationService;
+
+    @Autowired
+    private CompetitionService competitionService;
+
+
+
+    @GetMapping("/application/{applicationId}")
     public String viewApplication(@PathVariable("applicationId") long applicationId,
                                  Model model) {
-        model.addAttribute("model", assessmentReviewApplicationSummaryModelPopulator.populateModel(applicationId));
+        //model.addAttribute("model", assessmentReviewApplicationSummaryModelPopulator.populateModel(applicationId));
+
+        List<FormInputResponseResource> responses = formInputResponseRestService.getResponsesByApplicationId(applicationId).getSuccessObjectOrThrowException();
+        model.addAttribute("incompletedSections", sectionService.getInCompleted(applicationId));
+        model.addAttribute("responses", formInputResponseService.mapFormInputResponsesToFormInput(responses));
+
+        ApplicationResource application = applicationService.getById(applicationId);
+        CompetitionResource competition = competitionService.getById(application.getCompetition());
+       // List<ProcessRoleResource> userApplicationRoles = processRoleService.findProcessRolesByApplicationId(application.getId());
+
+      //  addApplicationAndSectionsInternalWithOrgDetails(application, competition, user, model, form, userApplicationRoles, Optional.of(Boolean.FALSE));
+     //   ProcessRoleResource userApplicationRole = userRestService.findProcessRole(user.getId(), applicationId).getSuccessObjectOrThrowException();
+
+     //   applicationModelPopulator.addOrganisationAndUserFinanceDetails(competition.getId(), applicationId, user, model, form, userApplicationRole.getOrganisationId());
+
+        model.addAttribute("applicationReadyForSubmit", applicationService.isApplicationReadyForSubmit(application.getId()));
+
+
         return "assessor-panel-application-overview";
     }
 }
