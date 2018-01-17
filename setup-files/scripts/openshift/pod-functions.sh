6fbd285521037810af0c882cf5449bfe777ec4b4
richard <richard@hiveit.co.uk>
2018-01-17 14:46:56 +0000

    rationalising scripts

--- setup-files/scripts/openshift/pod-functions.sh
+++ setup-files/scripts/openshift/pod-functions.sh
@@ -1,23 +1,7 @@
 #!/bin/bash
+# Series of functions to help spin up individual pods
 
-# This function only waits until the pod reports a Running status.
-# This does not necessarily indicate that any processes in the pod are at a point to use.
-# If this is required it may be necessary to wait for output in the pods log.
-function waitForPodToStart() {
-    POD_NAME=$1
-    SVC_ACCOUNT_CLAUSE=$2
-    if [ -z "${POD_NAME}" ]; then echo "POD_NAME variable not set"; exit -1; fi
-    if [ -z "${SVC_ACCOUNT_CLAUSE}" ]; then echo "SVC_ACCOUNT_CLAUSE variable not set"; exit -1; fi
-
-    until oc get pods ${POD_NAME} ${SVC_ACCOUNT_CLAUSE} | grep "Running" &> /dev/null;
-    do
-      echo "Wait for pod ${POD_NAME} to start running"
-      sleep 2
-    done
-    echo "Pod ${POD_NAME} running"
-}
-
-# This function will first delete any pre-existing pods before starting a new one.
+# Function to delete any pre-existing pods before starting a new one.
 function startupPod() {
     POD_CONFIG_PATH=$1
     SVC_ACCOUNT_CLAUSE=$2
@@ -34,7 +18,42 @@ function startupPod() {
     echo "Pod started using configuration file ${POD_CONFIG_PATH}"
 }
 
-# This function will shutdown then delete a pod.
+# Function to wait until the pod reports a Running status.
+# This does not necessarily indicate that any processes in the pod are at a point to use.
+# If this is required it may be necessary to wait for output in the pods log. See waitForPodLogs.
+function waitForPodToStart() {
+    POD_NAME=$1
+    SVC_ACCOUNT_CLAUSE=$2
+    if [ -z "${POD_NAME}" ]; then echo "POD_NAME variable not set"; exit -1; fi
+    if [ -z "${SVC_ACCOUNT_CLAUSE}" ]; then echo "SVC_ACCOUNT_CLAUSE variable not set"; exit -1; fi
+
+    until oc get pods ${POD_NAME} ${SVC_ACCOUNT_CLAUSE} | grep "Running" &> /dev/null;
+    do
+      echo "Wait for pod ${POD_NAME} to start running"
+      sleep 2
+    done
+    echo "Pod ${POD_NAME} running"
+}
+
+# Function to wait until the provided text is seen in pod logs.
+function waitForPodLogs() {
+    POD_NAME=$1
+    POD_LOG_TEXT=$2
+    SVC_ACCOUNT_CLAUSE=$3
+    if [ -z "${POD_NAME}" ]; then echo "POD_NAME variable not set"; exit -1; fi
+    if [ -z "${POD_LOG_TEXT}" ]; then echo "POD_LOG_TEXT variable not set"; exit -1; fi
+    if [ -z "${SVC_ACCOUNT_CLAUSE}" ]; then echo "SVC_ACCOUNT_CLAUSE variable not set"; exit -1; fi
+
+    echo "Waiting until the text ${POD_LOG_TEXT} is seen in the pod ${POD_NAME} logs"
+    until oc logs ${POD_NAME} ${SVC_ACCOUNT_CLAUSE} | grep "${POD_LOG_TEXT}" &> /dev/null;
+    do
+      echo "Waiting to see text ${POD_LOG_TEXT} in pod ${POD_NAME} logs"
+      sleep 2
+    done
+    echo "Text ${POD_LOG_TEXT} seen in the pod ${POD_NAME} logs"
+}
+
+# Function to shutdown then delete a pod
 function deletePod() {
     POD_NAME=$1
     POD_CONFIG_PATH=$2
