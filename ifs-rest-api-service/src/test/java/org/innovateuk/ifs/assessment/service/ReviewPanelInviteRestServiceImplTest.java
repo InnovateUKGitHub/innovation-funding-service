8a988049e0a782b9185de54c8128994a7249b2a0
Tom Baldwin <tom@polymorphism.co.uk>
2018-02-09 12:06:40 +0000

    IFS-2727 - Assign applications tab for interview panel. Renaming of panel classes.

---
+++ ifs-rest-api-service/src/test/java/org/innovateuk/ifs/assessment/service/ReviewPanelInviteRestServiceImplTest.java
@@ -0,0 +1,259 @@
+package org.innovateuk.ifs.assessment.service;
+
+import org.innovateuk.ifs.BaseRestServiceUnitTest;
+import org.innovateuk.ifs.commons.rest.RestResult;
+import org.innovateuk.ifs.commons.service.ParameterizedTypeReferences;
+import org.innovateuk.ifs.invite.resource.*;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static java.lang.Boolean.TRUE;
+import static java.lang.String.format;
+import static java.util.Arrays.asList;
+import static java.util.Collections.singletonList;
+import static org.innovateuk.ifs.assessment.builder.AssessmentReviewPanelInviteResourceBuilder.newAssessmentReviewPanelInviteResource;
+import static org.innovateuk.ifs.invite.builder.AssessmentReviewPanelParticipantResourceBuilder.newAssessmentReviewPanelParticipantResource;
+import static org.innovateuk.ifs.invite.builder.AssessorCreatedInvitePageResourceBuilder.newAssessorCreatedInvitePageResource;
+import static org.innovateuk.ifs.invite.builder.AssessorCreatedInviteResourceBuilder.newAssessorCreatedInviteResource;
+import static org.innovateuk.ifs.invite.builder.AssessorInviteOverviewPageResourceBuilder.newAssessorInviteOverviewPageResource;
+import static org.innovateuk.ifs.invite.builder.AssessorInviteSendResourceBuilder.newAssessorInviteSendResource;
+import static org.innovateuk.ifs.invite.builder.AssessorInvitesToSendResourceBuilder.newAssessorInvitesToSendResource;
+import static org.innovateuk.ifs.invite.builder.AvailableAssessorPageResourceBuilder.newAvailableAssessorPageResource;
+import static org.innovateuk.ifs.invite.builder.AvailableAssessorResourceBuilder.newAvailableAssessorResource;
+import static org.innovateuk.ifs.invite.builder.ExistingUserStagedInviteListResourceBuilder.newExistingUserStagedInviteListResource;
+import static org.innovateuk.ifs.invite.builder.ExistingUserStagedInviteResourceBuilder.newExistingUserStagedInviteResource;
+import static org.innovateuk.ifs.invite.resource.CompetitionParticipantRoleResource.PANEL_ASSESSOR;
+import static org.innovateuk.ifs.invite.resource.ParticipantStatusResource.ACCEPTED;
+import static org.innovateuk.ifs.invite.resource.ParticipantStatusResource.PENDING;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+import static org.springframework.http.HttpStatus.OK;
+
+public class ReviewPanelInviteRestServiceImplTest extends BaseRestServiceUnitTest<ReviewPanelInviteRestServiceImpl> {
+
+    private static final String restUrl = "/assessmentpanelinvite";
+
+    @Override
+    protected ReviewPanelInviteRestServiceImpl registerRestServiceUnderTest() {
+        ReviewPanelInviteRestServiceImpl assessmentPanelInviteRestService = new ReviewPanelInviteRestServiceImpl();
+        return assessmentPanelInviteRestService;
+    }
+
+    @Test
+    public void getAvailableAssessors() throws Exception {
+        long competitionId = 1L;
+        int page = 1;
+
+        List<AvailableAssessorResource> assessorItems = newAvailableAssessorResource().build(2);
+
+        AvailableAssessorPageResource expected = newAvailableAssessorPageResource()
+                .withContent(assessorItems)
+                .build();
+
+        setupGetWithRestResultExpectations(
+                format("%s/%s/%s?page=1", restUrl, "getAvailableAssessors", competitionId),
+                AvailableAssessorPageResource.class,
+                expected
+        );
+
+        AvailableAssessorPageResource actual = service.getAvailableAssessors(competitionId, page).getSuccessObjectOrThrowException();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getAvailableAssessorIds() throws Exception {
+        long competitionId = 1L;
+
+        List<Long> assessorItems = asList(1L, 2L);
+
+        setupGetWithRestResultExpectations(
+                format("%s/%s/%s", restUrl, "getAvailableAssessorIds", competitionId),
+                ParameterizedTypeReferences.longsListType(),
+                assessorItems
+        );
+
+        List<Long> actual = service.getAvailableAssessorIds(competitionId).getSuccessObjectOrThrowException();
+        assertEquals(assessorItems, actual);
+    }
+
+    @Test
+    public void getCreatedInvites() throws Exception {
+        long competitionId = 1L;
+        int page = 1;
+        AssessorCreatedInvitePageResource expected = newAssessorCreatedInvitePageResource()
+                .withContent(newAssessorCreatedInviteResource().build(2))
+                .build();
+
+        setupGetWithRestResultExpectations(format("%s/%s/%s?page=1", restUrl, "getCreatedInvites", competitionId), AssessorCreatedInvitePageResource.class, expected);
+
+        AssessorCreatedInvitePageResource actual = service.getCreatedInvites(competitionId, page).getSuccessObjectOrThrowException();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void inviteUsers() throws Exception {
+        long competitionId = 1L;
+
+        ExistingUserStagedInviteListResource existingUserStagedInviteListResource = newExistingUserStagedInviteListResource()
+                .withInvites(
+                        newExistingUserStagedInviteResource()
+                                .withUserId(1L, 2L)
+                                .withCompetitionId(competitionId)
+                                .build(2)
+                )
+                .build();
+
+        setupPostWithRestResultExpectations(format("%s/%s", restUrl, "inviteUsers"), existingUserStagedInviteListResource, OK);
+
+        RestResult<Void> restResult = service.inviteUsers(existingUserStagedInviteListResource);
+        assertTrue(restResult.isSuccess());
+    }
+
+    @Test
+    public void sendAllInvites() {
+        long competitionId = 1L;
+        AssessorInviteSendResource assessorInviteSendResource = newAssessorInviteSendResource()
+                .withSubject("subject")
+                .withContent("content")
+                .build();
+
+        setupPostWithRestResultExpectations(format("%s/%s/%s", restUrl, "sendAllInvites", competitionId), assessorInviteSendResource, OK);
+        assertTrue(service.sendAllInvites(competitionId, assessorInviteSendResource).isSuccess());
+        setupPostWithRestResultVerifications(format("%s/%s/%s", restUrl, "sendAllInvites", competitionId), Void.class, assessorInviteSendResource);
+    }
+
+    @Test
+    public void getAllInvitesToResend() throws Exception {
+        long competitionId = 1L;
+        List<Long> inviteIds = asList(1L, 2L);
+
+        AssessorInvitesToSendResource expected = newAssessorInvitesToSendResource()
+                .withRecipients(asList("James", "John"))
+                .build();
+
+        setupGetWithRestResultExpectations(format("%s/%s/%s%s", restUrl, "getAllInvitesToResend", competitionId, "?inviteIds=1,2"), AssessorInvitesToSendResource.class, expected);
+        AssessorInvitesToSendResource actual = service.getAllInvitesToResend(competitionId, inviteIds).getSuccessObjectOrThrowException();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getAllInvitesToSend() throws Exception {
+        long competitionId = 1L;
+
+        AssessorInvitesToSendResource expected = newAssessorInvitesToSendResource()
+                .withRecipients(asList("James", "John"))
+                .build();
+
+        setupGetWithRestResultExpectations(format("%s/%s/%s", restUrl, "getAllInvitesToSend", competitionId), AssessorInvitesToSendResource.class, expected);
+        AssessorInvitesToSendResource actual = service.getAllInvitesToSend(competitionId).getSuccessObjectOrThrowException();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void resendInvites() {
+        List<Long> inviteIds = asList(1L, 2L);
+        AssessorInviteSendResource assessorInviteSendResource = newAssessorInviteSendResource()
+                .withSubject("subject")
+                .withContent("content")
+                .build();
+
+        setupPostWithRestResultExpectations(format("%s/%s%s", restUrl, "resendInvites", "?inviteIds=1,2"), assessorInviteSendResource, OK);
+
+        assertTrue(service.resendInvites(inviteIds, assessorInviteSendResource).isSuccess());
+    }
+
+    @Test
+    public void getInvitationOverview() throws Exception {
+        long competitionId = 1L;
+        int page = 1;
+
+        AssessorInviteOverviewPageResource expected = newAssessorInviteOverviewPageResource().build();
+
+        String expectedUrl = format("%s/%s/%s?page=1&statuses=ACCEPTED,PENDING", restUrl, "getInvitationOverview", competitionId);
+
+        setupGetWithRestResultExpectations(expectedUrl, AssessorInviteOverviewPageResource.class, expected);
+
+        AssessorInviteOverviewPageResource actual = service.getInvitationOverview(competitionId, page, Arrays.asList(ACCEPTED, PENDING))
+                .getSuccessObjectOrThrowException();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getAllInvitesByUser() throws Exception {
+        long userId = 1L;
+        AssessmentReviewPanelParticipantResource assessmentReviewPanelParticipantResource = newAssessmentReviewPanelParticipantResource()
+                .withUser(userId)
+                .withCompetitionParticipantRole(PANEL_ASSESSOR)
+                .withCompetitionName("Competition Name")
+                .build();
+        List<AssessmentReviewPanelParticipantResource> expected = singletonList(assessmentReviewPanelParticipantResource);
+
+        setupGetWithRestResultExpectations(format("%s/%s/%s", restUrl, "getAllInvitesByUser", userId), ParameterizedTypeReferences.assessmentPanelParticipantResourceListType(), expected, OK);
+
+        List<AssessmentReviewPanelParticipantResource> actual = service.getAllInvitesByUser(userId).getSuccessObjectOrThrowException();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void openInvite() {
+        AssessmentReviewPanelInviteResource expected = newAssessmentReviewPanelInviteResource().build();
+        expected.setCompetitionName("my competition");
+        setupPostWithRestResultAnonymousExpectations(format("%s/%s/%s", restUrl, "openInvite", "hash"), AssessmentReviewPanelInviteResource.class, null, expected, OK);
+        AssessmentReviewPanelInviteResource actual = service.openInvite("hash").getSuccessObjectOrThrowException();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void openInvite_hashNotExists() {
+        setupPostWithRestResultAnonymousExpectations(format("%s/%s/%s", restUrl, "openInvite", "hashNotExists"), AssessmentReviewPanelInviteResource.class, null, null, NOT_FOUND);
+        RestResult<AssessmentReviewPanelInviteResource> restResult = service.openInvite("hashNotExists");
+        assertTrue(restResult.isFailure());
+    }
+
+    @Test
+    public void acceptInvite() {
+        setupPostWithRestResultExpectations(format("%s/%s/%s", restUrl, "acceptInvite", "hash"), OK);
+        RestResult<Void> restResult = service.acceptInvite("hash");
+        assertTrue(restResult.isSuccess());
+    }
+
+    @Test
+    public void rejectInvite() {
+        setupPostWithRestResultAnonymousExpectations(format("%s/%s/%s", restUrl, "rejectInvite", "hash"), Void.class, null, null, OK);
+
+        RestResult<Void> restResult = service.rejectInvite("hash");
+        assertTrue(restResult.isSuccess());
+    }
+
+    @Test
+    public void checkExistingUser() {
+        setupGetWithRestResultAnonymousExpectations(format("%s/%s/%s", restUrl, "checkExistingUser", "hash"), Boolean.class, TRUE);
+        assertTrue(service.checkExistingUser("hash").getSuccessObjectOrThrowException());
+    }
+
+    @Test
+    public void deleteInvite() {
+        String email = "firstname.lastname@example.com";
+        long competitionId = 1L;
+
+        setupDeleteWithRestResultExpectations(format("%s/%s?competitionId=%s&email=%s", restUrl, "deleteInvite", competitionId, email));
+
+        RestResult<Void> resultResult = service.deleteInvite(email, competitionId);
+        assertTrue(resultResult.isSuccess());
+    }
+
+    @Test
+    public void deleteAllInvites() throws Exception {
+        long competitionId = 1L;
+
+        setupDeleteWithRestResultExpectations(format("%s/%s?competitionId=%s", restUrl, "deleteAllInvites", competitionId));
+
+        RestResult<Void> resultResult = service.deleteAllInvites(competitionId);
+        assertTrue(resultResult.isSuccess());
+    }
+
+}
