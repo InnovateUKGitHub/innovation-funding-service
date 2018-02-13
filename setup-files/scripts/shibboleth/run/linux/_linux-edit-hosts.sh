4d220fe3f0f708d23fc9d280f1f891d20e64bc5a
Adam Birse <abirse@worth.systems>
2018-02-13 09:53:33 +0000

    Removed unused files

--- setup-files/scripts/shibboleth/run/linux/_linux-edit-hosts.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-cp /etc/hosts /tmp/hostsbackup
-ip_address=`docker inspect --format '{{ .NetworkSettings.IPAddress }}' ifs-local-dev`
-cat /etc/hosts | grep -v 'ifs-local-dev' | grep -v 'iuk-auth-localdev' > /tmp/temphosts
-echo "$ip_address  iuk-auth-localdev" >> /tmp/temphosts
-echo "$ip_address  ifs-local-dev" >> /tmp/temphosts
-sudo cp /tmp/temphosts /etc/hosts
