27697da37ebff64c8221dd14446d6e41cca008e1
Wouter de Meijer <wdemeijer@worth.systems>
2018-02-22 10:02:36 +0100

    IFS-2565 - Fixed documentation and renamed zzdupdated field on resource

--- ifs-resources/src/main/java/org/innovateuk/ifs/competition/resource/CompetitionSetupQuestionResource.java
+++ ifs-resources/src/main/java/org/innovateuk/ifs/competition/resource/CompetitionSetupQuestionResource.java
@@ -23,7 +23,7 @@ import static org.innovateuk.ifs.file.resource.FileTypeCategory.SPREADSHEET;
 @FieldRequiredIf(required = "fileUploadGuidance", argument = "appendix", predicate = true, message = "{validation.field.must.not.be.blank}")
 public class CompetitionSetupQuestionResource {
     //@ZeroDownTime(reference = "IFS-2565", description = "Indicator on resource whether updated or not. To be removed during contraction.")
-    private boolean isZDDUpdated;
+    private boolean ZDDUpdated;
 
     private Long questionId;
 
@@ -257,11 +257,11 @@ public class CompetitionSetupQuestionResource {
 
 
     public boolean isZDDUpdated() {
-        return isZDDUpdated;
+        return ZDDUpdated;
     }
 
     public void setZDDUpdated(boolean ZDDUpdated) {
-        isZDDUpdated = ZDDUpdated;
+        this.ZDDUpdated = ZDDUpdated;
     }
 
     @Override
