e4005cfbc35d485121b0c417f2bee93ea4fb63dc
Nicholas Tsim <nick@hiveit.co.uk>
2018-02-27 11:12:06 +0000

    IFS-2875 - Tidy and document InviteService

--- ifs-data-layer/ifs-data-service/src/main/java/org/innovateuk/ifs/invite/transactional/InviteService.java
+++ ifs-data-layer/ifs-data-service/src/main/java/org/innovateuk/ifs/invite/transactional/InviteService.java
@@ -18,6 +18,12 @@ import java.util.function.Supplier;
 import static org.innovateuk.ifs.commons.error.CommonErrors.notFoundError;
 import static org.innovateuk.ifs.util.EntityLookupCallbacks.find;
 
+/**
+ * Base service class providing common methods for
+ * working in an {@link Invite} subclass' domain.
+ *
+ * @param <T> subclass of {@link Invite}
+ */
 @Transactional(readOnly = true)
 public abstract class InviteService<T extends Invite> {
 
@@ -47,9 +53,13 @@ public abstract class InviteService<T extends Invite> {
     }
 
     @PreAuthorize("hasAuthority('system_registrar')")
-    @SecuredBySpring(value = "CHECK_EXISTING_USER_ON_HASH",
-            description = "The System Registration user can check for the presence of a User on an invite or the presence of a User with the invited e-mail address",
-            additionalComments = "The hash should be unguessable so the only way to successfully call this method would be to have been given the hash in the first place")
+    @SecuredBySpring(
+            value = "CHECK_EXISTING_USER_ON_HASH",
+            description = "The System Registration user can check for the presence of a User on an invite or the " +
+                    "presence of a User with the invited e-mail address",
+            additionalComments = "The hash should be unguessable so the only way to successfully call this method " +
+                    "would be to have been given the hash in the first place"
+    )
     public ServiceResult<Boolean> checkExistingUser(@P("hash") String hash) {
         return getByHash(hash).andOnSuccessReturn(invite -> {
             if (invite.getUser() != null) {
