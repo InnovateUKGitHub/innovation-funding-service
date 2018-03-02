a45eda7ad100ef7baefbccc9a3347a428c799c18
Duncan Watson <duncan@hiveit.co.uk>
2018-03-02 10:45:15 +0000

    IFS-1875 - made generate-test-data.sh non-interactive and a little less time-consuming.  Also by default it will now override the current patch with the new one if no version number is specified

--- setup-files/scripts/generate-test-data.sh
+++ setup-files/scripts/generate-test-data.sh
@@ -4,6 +4,9 @@ set -e
 
 cd "$(dirname "$0")"
 
+project_root_dir="`pwd`/../.."
+webtest_patches_dir="${project_root_dir}/ifs-data-layer/ifs-data-service/src/main/resources/db/webtest"
+
 reset_db () {
     mysql -uroot -ppassword -hifs-database -e "drop database if exists ifs";
     mysql -uroot -ppassword -hifs-database -e "create database ifs";
@@ -11,11 +14,18 @@ reset_db () {
     mysql -uroot -ppassword -hifs-database -e "create database ifs_test";
 }
 
+get_current_patch_level () {
+
+    # extract the current version of the webtest data
+    echo "`find ${webtest_patches_dir} -name '*__Base_webtest_data.sql' | sed 's/.*\(V.*\)_[0-9]*__.*/\1/g'`"
+}
+
 do_baseline () {
+
     generate_test_class="ifs-data-layer/ifs-data-service/src/test/java/org/innovateuk/ifs/testdata/GenerateTestData.java"
 
     # navigate to project root
-    cd ../../
+    cd ${project_root_dir}
 
     # clean database
     reset_db
@@ -26,58 +36,42 @@ do_baseline () {
     # run generator test class
     IFS_GENERATE_TEST_DATA_EXECUTION=SINGLE_THREADED IFS_GENERATE_TEST_DATA_COMPETITION_FILTER=ALL_COMPETITIONS ./gradlew -PtestGroups=generatetestdata :ifs-data-layer:ifs-data-service:cleanTest :ifs-data-layer:ifs-data-service:test --tests org.innovateuk.ifs.testdata.GenerateTestData -x asciidoctor
 
-    cd ifs-data-layer/ifs-data-service/src/main/resources/db/webtest/
-
     # extract the current version of the webtest data
-    oldversion="`find . -name '*__Base_webtest_data.sql' | sed 's/.*\(V.*\)_[0-9]*__.*/\1/g'`_"
+    current_version="`get_current_patch_level`_"
 
-    for i in ${oldversion}*; do mv $i ${i/${oldversion}/tmp_${newversion}}; done
-    rm -f ${newversion}*.sql
-    for i in tmp_${newversion}*; do mv $i ${i/tmp_${newversion}/${newversion}}; done
+    cd ${webtest_patches_dir}
+    for i in ${current_version}*; do mv $i ${i/${current_version}/tmp_${new_version}}; done
+    rm -f ${new_version}*.sql
+    for i in tmp_${new_version}*; do mv $i ${i/tmp_${new_version}/${new_version}}; done
 
-    cd ../../../../../../../
+    cd ${project_root_dir}/setup-files/scripts
 
     # create baseline dump
-    setup-files/scripts/create-baseline-dump.sh ${newversion}
+    ./create-baseline-dump.sh ${new_version}
+
+    cd ${project_root_dir}
 
     reset_db
-
-    #verify correct build
-    ./gradlew clean buildDocker initDB flywayClean flywayMigrate composeUp syncShib -x test
 
     cat << EOF
 * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
 *                                                                       *
 *           You have successfully run a webtest baseline.               *
-*       please verify the changes by running a full acceptance suite    *
+*       Please verify the changes by running a full acceptance suite    *
 *                                                                       *
 * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
 EOF
 
 }
 
-newversion=${1?please specify the new baseline version(e.g. V100_12)}
-newversion="${newversion}_"
+if [[ -n "$1" ]]; then
+    new_version_or_current=$1
+else
+    new_version_or_current=`get_current_patch_level`
+fi
 
+new_version="${new_version_or_current}_"
 
-cat << EOF
-* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
-*                                                                       *
-*           You are about to run a webtest baseline.                    *
-*       This will take a while so make sure you are not in a rush       *
-*                                                                       *
-* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
-EOF
+echo "Creating new baseline patch - current version is `get_current_patch_level` and new version will be ${new_version_or_current}"
 
-while true; do
-    read -p "Do you want to start the baseline? (y/N)" yn
-    case $yn in
-        [Yy]* ) do_baseline $1 $2; break;;
-        [Nn]* ) exit;;
-        * ) exit;;
-    esac
-done
-
-
-
-
+do_baseline
