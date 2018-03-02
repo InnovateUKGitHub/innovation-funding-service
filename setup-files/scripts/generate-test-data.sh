5b0921c6d9698eb23a8a9d9bad533f27a82536e8
Duncan Watson <duncan@hiveit.co.uk>
2018-03-02 10:50:11 +0000

    Revert "IFS-1875 - made generate-test-data.sh non-interactive and a little less time-consuming.  Also by default it will now override the current patch with the new one if no version number is specified"
    This reverts commit a45eda7ad100ef7baefbccc9a3347a428c799c18.

--- setup-files/scripts/generate-test-data.sh
+++ setup-files/scripts/generate-test-data.sh
@@ -4,9 +4,6 @@ set -e
 
 cd "$(dirname "$0")"
 
-project_root_dir="`pwd`/../.."
-webtest_patches_dir="${project_root_dir}/ifs-data-layer/ifs-data-service/src/main/resources/db/webtest"
-
 reset_db () {
     mysql -uroot -ppassword -hifs-database -e "drop database if exists ifs";
     mysql -uroot -ppassword -hifs-database -e "create database ifs";
@@ -14,18 +11,11 @@ reset_db () {
     mysql -uroot -ppassword -hifs-database -e "create database ifs_test";
 }
 
-get_current_patch_level () {
-
-    # extract the current version of the webtest data
-    echo "`find ${webtest_patches_dir} -name '*__Base_webtest_data.sql' | sed 's/.*\(V.*\)_[0-9]*__.*/\1/g'`"
-}
-
 do_baseline () {
-
     generate_test_class="ifs-data-layer/ifs-data-service/src/test/java/org/innovateuk/ifs/testdata/GenerateTestData.java"
 
     # navigate to project root
-    cd ${project_root_dir}
+    cd ../../
 
     # clean database
     reset_db
@@ -36,42 +26,58 @@ do_baseline () {
     # run generator test class
     IFS_GENERATE_TEST_DATA_EXECUTION=SINGLE_THREADED IFS_GENERATE_TEST_DATA_COMPETITION_FILTER=ALL_COMPETITIONS ./gradlew -PtestGroups=generatetestdata :ifs-data-layer:ifs-data-service:cleanTest :ifs-data-layer:ifs-data-service:test --tests org.innovateuk.ifs.testdata.GenerateTestData -x asciidoctor
 
+    cd ifs-data-layer/ifs-data-service/src/main/resources/db/webtest/
+
     # extract the current version of the webtest data
-    current_version="`get_current_patch_level`_"
+    oldversion="`find . -name '*__Base_webtest_data.sql' | sed 's/.*\(V.*\)_[0-9]*__.*/\1/g'`_"
 
-    cd ${webtest_patches_dir}
-    for i in ${current_version}*; do mv $i ${i/${current_version}/tmp_${new_version}}; done
-    rm -f ${new_version}*.sql
-    for i in tmp_${new_version}*; do mv $i ${i/tmp_${new_version}/${new_version}}; done
+    for i in ${oldversion}*; do mv $i ${i/${oldversion}/tmp_${newversion}}; done
+    rm -f ${newversion}*.sql
+    for i in tmp_${newversion}*; do mv $i ${i/tmp_${newversion}/${newversion}}; done
 
-    cd ${project_root_dir}/setup-files/scripts
+    cd ../../../../../../../
 
     # create baseline dump
-    ./create-baseline-dump.sh ${new_version}
-
-    cd ${project_root_dir}
+    setup-files/scripts/create-baseline-dump.sh ${newversion}
 
     reset_db
+
+    #verify correct build
+    ./gradlew clean buildDocker initDB flywayClean flywayMigrate composeUp syncShib -x test
 
     cat << EOF
 * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
 *                                                                       *
 *           You have successfully run a webtest baseline.               *
-*       Please verify the changes by running a full acceptance suite    *
+*       please verify the changes by running a full acceptance suite    *
 *                                                                       *
 * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
 EOF
 
 }
 
-if [[ -n "$1" ]]; then
-    new_version_or_current=$1
-else
-    new_version_or_current=`get_current_patch_level`
-fi
+newversion=${1?please specify the new baseline version(e.g. V100_12)}
+newversion="${newversion}_"
 
-new_version="${new_version_or_current}_"
 
-echo "Creating new baseline patch - current version is `get_current_patch_level` and new version will be ${new_version_or_current}"
+cat << EOF
+* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+*                                                                       *
+*           You are about to run a webtest baseline.                    *
+*       This will take a while so make sure you are not in a rush       *
+*                                                                       *
+* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+EOF
 
-do_baseline
+while true; do
+    read -p "Do you want to start the baseline? (y/N)" yn
+    case $yn in
+        [Yy]* ) do_baseline $1 $2; break;;
+        [Nn]* ) exit;;
+        * ) exit;;
+    esac
+done
+
+
+
+
