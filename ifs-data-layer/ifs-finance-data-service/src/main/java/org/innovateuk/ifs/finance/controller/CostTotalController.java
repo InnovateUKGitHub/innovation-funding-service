ee861ece960066819dfbd31e3320260f38a5111e
Nicholas Tsim <nick@hiveit.co.uk>
2018-03-06 16:03:35 +0000

    IFS-2394 - Add `/cost-totals` endpoint for bulk add and rename singular variant to `/cost-total`

--- ifs-data-layer/ifs-finance-data-service/src/main/java/org/innovateuk/ifs/finance/controller/CostTotalController.java
+++ ifs-data-layer/ifs-finance-data-service/src/main/java/org/innovateuk/ifs/finance/controller/CostTotalController.java
@@ -9,10 +9,11 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.constraints.NotNull;
+import java.util.List;
 
 /**
  * Controller concerned with handling {@link org.innovateuk.ifs.finance.domain.CostTotal}s.
- *
+ * <p>
  * Typically these will be received as {@link FinanceCostTotalResource}s
  */
 @RestController
@@ -25,8 +26,13 @@ public class CostTotalController {
         this.costTotalService = costTotalService;
     }
 
-    @PostMapping("/cost-totals")
+    @PostMapping("/cost-total")
     public RestResult<Void> addCostTotal(@NotNull @RequestBody FinanceCostTotalResource financeCostTotalResource) {
         return costTotalService.saveCostTotal(financeCostTotalResource).toPostCreateResponse();
+    }
+
+    @PostMapping("/cost-totals")
+    public RestResult<Void> addCostTotals(@NotNull @RequestBody List<FinanceCostTotalResource> financeCostTotalResources) {
+        return costTotalService.saveCostTotals(financeCostTotalResources).toPostCreateResponse();
     }
 }
--- ifs-data-layer/ifs-finance-data-service/src/test/java/org/innovateuk/ifs/finance/controller/CostTotalControllerTest.java
+++ ifs-data-layer/ifs-finance-data-service/src/test/java/org/innovateuk/ifs/finance/controller/CostTotalControllerTest.java
@@ -10,11 +10,13 @@ import org.mockito.Mock;
 import org.springframework.http.MediaType;
 
 import java.math.BigDecimal;
+import java.util.List;
 import java.util.function.Consumer;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.innovateuk.ifs.LambdaMatcher.createLambdaMatcher;
 import static org.innovateuk.ifs.commons.service.ServiceResult.serviceSuccess;
+import static org.innovateuk.ifs.finance.builder.sync.FinanceCostTotalResourceBuilder.newFinanceCostTotalResource;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -46,12 +48,37 @@ public class CostTotalControllerTest extends MockMvcTest<CostTotalController> {
         when(costTotalService.saveCostTotal(createLambdaMatcher(matchesExpectedResource))).thenReturn(serviceSuccess());
 
         mockMvc.perform(
-                post("/cost-totals")
+                post("/cost-total")
                         .content(json(financeCostTotalResource))
                         .contentType(MediaType.APPLICATION_JSON)
         )
                 .andExpect(status().isCreated());
 
         verify(costTotalService).saveCostTotal(createLambdaMatcher(matchesExpectedResource));
+    }
+
+    @Test
+    public void addCostTotals() throws Exception {
+        List<FinanceCostTotalResource> financeCostTotalResources = newFinanceCostTotalResource()
+                .withFinanceType(FinanceType.APPLICATION)
+                .withFinanceRowType(FinanceRowType.LABOUR, FinanceRowType.MATERIALS)
+                .withFinanceId(1L, 2L)
+                .withTotal(BigDecimal.valueOf(1000L), BigDecimal.valueOf(2000L))
+                .build(2);
+
+        Consumer<List<FinanceCostTotalResource>> matchesExpectedResources = (resources) -> {
+            assertThat(resources).usingFieldByFieldElementComparator().containsAll(financeCostTotalResources);
+        };
+
+        when(costTotalService.saveCostTotals(createLambdaMatcher(matchesExpectedResources))).thenReturn(serviceSuccess());
+
+        mockMvc.perform(
+                post("/cost-totals")
+                        .content(json(financeCostTotalResources))
+                        .contentType(MediaType.APPLICATION_JSON)
+        )
+                .andExpect(status().isCreated());
+
+        verify(costTotalService).saveCostTotals(createLambdaMatcher(matchesExpectedResources));
     }
 }
