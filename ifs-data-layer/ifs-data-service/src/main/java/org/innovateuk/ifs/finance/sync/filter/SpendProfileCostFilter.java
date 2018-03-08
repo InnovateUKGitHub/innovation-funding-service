c145516e04f8f510dc59bd058dd748c9117aa604
Nicholas Tsim <nick@hiveit.co.uk>
2018-03-08 13:03:33 +0000

    IFS-2394 - Simplify SpendProfileCostFilter further

--- ifs-data-layer/ifs-data-service/src/main/java/org/innovateuk/ifs/finance/sync/filter/SpendProfileCostFilter.java
+++ ifs-data-layer/ifs-data-service/src/main/java/org/innovateuk/ifs/finance/sync/filter/SpendProfileCostFilter.java
@@ -1,6 +1,5 @@
 package org.innovateuk.ifs.finance.sync.filter;
 
-import org.innovateuk.ifs.finance.resource.cost.FinanceRowType;
 import org.innovateuk.ifs.finance.resource.sync.FinanceCostTotalResource;
 import org.springframework.stereotype.Component;
 
@@ -11,17 +10,13 @@ import static org.innovateuk.ifs.util.CollectionFunctions.simpleFilter;
 /**
  * Filters a list of {@link FinanceCostTotalResource}s and return only the ones tagged as "Spend Profile".
  */
-
 @Component
 public class SpendProfileCostFilter {
+
     public List<FinanceCostTotalResource> filterBySpendProfile(List<FinanceCostTotalResource> financeCostTotalResources) {
         return simpleFilter(
                 financeCostTotalResources,
-                financeResource -> isSpendProfile(financeResource.getFinanceRowType())
+                financeResource -> financeResource.getFinanceRowType().isIncludedInSpendProfile()
         );
-    }
-
-    private boolean isSpendProfile(FinanceRowType rowType) {
-        return rowType.isIncludedInSpendProfile();
     }
 }
