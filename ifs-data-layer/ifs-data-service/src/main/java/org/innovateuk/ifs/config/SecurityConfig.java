f0ce156c401df8441e20a29d13dacc9cc8eb6dfb
Nicholas Tsim <nick@hiveit.co.uk>
2018-03-08 11:20:46 +0000

    IFS-2394 - Move data-service SecurityConfig out of data-service-commons
    It is currently problematic to have this kind of config inside of the
    commons module as it gets propagated down to the finance-data-service due
    to it's `@Configuration` annotation. This means it makes incorrect assertions
    about how the finance-data-service should be configured.
    
    This highlights problems with how we've currently structured our project
    files. This should be assessed further in some other tech debt ticket.

--- ifs-data-layer/ifs-data-service/src/main/java/org/innovateuk/ifs/config/SecurityConfig.java
+++ ifs-data-layer/ifs-data-service/src/main/java/org/innovateuk/ifs/config/SecurityConfig.java
@@ -1,5 +1,6 @@
-package org.innovateuk.ifs.security;
+package org.innovateuk.ifs.config;
 
+import org.innovateuk.ifs.security.StatelessAuthenticationFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
