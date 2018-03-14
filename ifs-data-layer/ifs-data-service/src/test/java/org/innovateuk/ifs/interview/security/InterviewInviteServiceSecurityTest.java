df0be06308d29165f148070097a690dbfa22a50e
Kieran Hester <>
2018-03-14 15:11:07 +0000

    IFS-3054 fix merge conflict

--- ifs-data-layer/ifs-data-service/src/test/java/org/innovateuk/ifs/interview/security/InterviewInviteServiceSecurityTest.java
+++ ifs-data-layer/ifs-data-service/src/test/java/org/innovateuk/ifs/interview/security/InterviewInviteServiceSecurityTest.java
@@ -23,7 +23,6 @@ import static org.innovateuk.ifs.commons.service.ServiceResult.serviceSuccess;
 import static org.innovateuk.ifs.interview.builder.InterviewParticipantResourceBuilder.newInterviewParticipantResource;
 import static org.innovateuk.ifs.invite.builder.AssessorInviteSendResourceBuilder.newAssessorInviteSendResource;
 import static org.innovateuk.ifs.invite.builder.ExistingUserStagedInviteResourceBuilder.newExistingUserStagedInviteResource;
-import static org.innovateuk.ifs.review.builder.ReviewParticipantResourceBuilder.newReviewParticipantResource;
 import static org.innovateuk.ifs.user.builder.RoleResourceBuilder.newRoleResource;
 import static org.innovateuk.ifs.user.builder.UserResourceBuilder.newUserResource;
 import static org.innovateuk.ifs.user.resource.UserRoleType.*;
