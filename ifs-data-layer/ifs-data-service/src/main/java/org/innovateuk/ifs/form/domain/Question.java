478acfed4d2038c6f48230dc7f1db3f194e23e0a
Luke Harper <luke.harper@nationaltrust.org.uk>
2018-03-14 09:06:33 +0000

    IFS-2981 adding further description to @ZeroDowntime

--- ifs-data-layer/ifs-data-service/src/main/java/org/innovateuk/ifs/form/domain/Question.java
+++ ifs-data-layer/ifs-data-service/src/main/java/org/innovateuk/ifs/form/domain/Question.java
@@ -15,7 +15,10 @@ import java.util.List;
  */
 @Entity
 public class Question {
-    @ZeroDowntime(reference = "IFS-2981", description = "Remove use of PREVIOUS_PACKAGE_NAME only need to check Question.class")
+    @ZeroDowntime(reference = "IFS-2981",
+            description = "Remove use of PREVIOUS_PACKAGE_NAME only need to check Question.class " +
+                    "NOTE: The script V113_1_0__migrate_old_question_package_name.sql will need to be ran again " +
+                    "to cleanup any statuses that were created with old package name while deploying.")
     public static final String PREVIOUS_PACKAGE_NAME = "org.innovate.ifs.application.domain";
     private static final String SCOPE = "Scope";
 
