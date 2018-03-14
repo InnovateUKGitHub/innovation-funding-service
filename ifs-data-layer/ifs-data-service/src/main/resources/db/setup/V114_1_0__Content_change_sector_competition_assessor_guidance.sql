46fef511b9a8cfdbe3b3c85de73d98b5084ffc23
Fleur Oudenampsen <fleuroudenampsen@gmail.com>
2018-03-14 14:57:32 +0000

    Changed patch version number and added a new update for production database

--- ifs-data-layer/ifs-data-service/src/main/resources/db/setup/V114_1_0__Content_change_sector_competition_assessor_guidance.sql
+++ ifs-data-layer/ifs-data-service/src/main/resources/db/setup/V114_1_0__Content_change_sector_competition_assessor_guidance.sql
@@ -1,3 +1,7 @@
 UPDATE guidance_row
 SET justification = 'The project costs are entirely appropriate and represent excellent value for money compared to alternative approaches outlined (including doing nothing). The partners have a clear idea of how they will finance their contribution. The balance of costs and grants between partners, and use of subcontractors is justified and reasonable for the proposed project.'
 WHERE justification = 'The project costs are entirely appropriate and represent excellent value for money compared to alternative approaches outlined (including doing nothing)..  The partners have a clear idea of how they will finance their contribution.  The balance of costs and grants between partners, and use of subcontractors is justified and reasonable for the proposed project.';
+
+UPDATE guidance_row
+SET justification = 'Target customers are identified along with the value proposition to them. The routes to market and how profit, productivity and growth will increase is identified and evidenced. The exploitation and/or dissemination of the main project outputs is outlined.'
+WHERE justification = 'Target customers are identified along with the value proposition to them. The routes to market and how profit, productivity and growth will increaseis identified and evidenced. . The exploitation and/or dissemination of the main project outputs is outlined.';
