4681004ae134691b669ff07159e886b0f2de9137
Ben Outram <ben@hiveit.co.uk>
2018-03-20 15:04:26 +0000

    IFS-3059 Renaming test class.

--- ifs-data-layer/ifs-data-service/src/test/java/org/innovateuk/ifs/finance/totals/service/AsyncRestCostTotalEndpointTest.java
+++ ifs-data-layer/ifs-data-service/src/test/java/org/innovateuk/ifs/finance/totals/service/AsyncRestCostTotalEndpointTest.java
@@ -20,7 +20,7 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
-public class AsyncResCostTotalEndpointTest {
+public class AsyncRestCostTotalEndpointTest {
 
     @Mock
     private AbstractRestTemplateAdaptor restTemplateAdaptorMock;
