91367b3861d18e18b320df50d1c27c500287dc9b
Bronnyl Dias <bdias@worth.systems>
2018-03-23 12:21:31 +0000

    IFS-2920-Proj-Location-Per-Partner
    Change-Id: Ic46d8316eb2fd42b36dc9431baa7d79eb1839969

--- ifs-web-service/ifs-project-setup-service/src/main/java/org/innovateuk/ifs/project/status/security/SetupSectionsPermissionRules.java
+++ ifs-web-service/ifs-project-setup-service/src/main/java/org/innovateuk/ifs/project/status/security/SetupSectionsPermissionRules.java
@@ -60,6 +60,12 @@ public class SetupSectionsPermissionRules {
         return doSectionCheck(projectCompositeId.id(), user, SetupSectionAccessibilityHelper::canAccessFinanceContactPage);
     }
 
+    @PermissionRule(value = "ACCESS_PARTNER_PROJECT_LOCATION_PAGE", description = "A partner can access the Finance Contact " +
+            "page when their Companies House data is complete or not required, and the Grant Offer Letter has not yet been generated")
+    public boolean partnerCanAccessProjectLocationPage(ProjectCompositeId projectCompositeId, UserResource user) {
+        return doSectionCheck(projectCompositeId.id(), user, SetupSectionAccessibilityHelper::canAccessPartnerProjectLocationPage);
+    }
+
     @PermissionRule(value = "ACCESS_PROJECT_MANAGER_PAGE", description = "A lead can access the Project Manager " +
             "page when their Companies House data is complete or not required, and the Grant Offer Letter has not yet been generated")
     public boolean leadCanAccessProjectManagerPage(ProjectCompositeId projectCompositeId, UserResource user) {
