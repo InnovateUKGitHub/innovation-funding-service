5404daa59497564afbc899098861701b99fe822b
Bronnyl Dias <bdias@worth.systems>
2018-03-23 12:18:11 +0000

    IFS-2920-Proj-Location-Per-Partner
    Internal user project details page changes.
    ViewModel and view changes.
    
    Change-Id: I7fe84a580d83911dd203e86a7f0eda78a4e069c5

--- ifs-web-service/ifs-project-setup-mgt-service/src/main/java/org/innovateuk/ifs/project/projectdetails/viewmodel/ProjectDetailsViewModel.java
+++ ifs-web-service/ifs-project-setup-mgt-service/src/main/java/org/innovateuk/ifs/project/projectdetails/viewmodel/ProjectDetailsViewModel.java
@@ -1,9 +1,11 @@
 package org.innovateuk.ifs.project.projectdetails.viewmodel;
 
+import org.innovateuk.ifs.project.resource.PartnerOrganisationResource;
 import org.innovateuk.ifs.project.resource.ProjectResource;
 import org.innovateuk.ifs.project.resource.ProjectUserResource;
 import org.innovateuk.ifs.user.resource.OrganisationResource;
 
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -16,15 +18,21 @@ public class ProjectDetailsViewModel {
     private String leadOrganisation;
     private ProjectUserResource projectManager;
     private Map<OrganisationResource, ProjectUserResource> organisationFinanceContactMap;
+    private boolean locationPerPartnerRequired;
+    private List<PartnerOrganisationResource> partnerOrganisations;
 
     public ProjectDetailsViewModel(ProjectResource project, Long competitionId, String leadOrganisation,
                                    ProjectUserResource projectManager,
-                                   Map<OrganisationResource, ProjectUserResource> organisationFinanceContactMap) {
+                                   Map<OrganisationResource, ProjectUserResource> organisationFinanceContactMap,
+                                   boolean locationPerPartnerRequired,
+                                   List<PartnerOrganisationResource> partnerOrganisations) {
         this.project = project;
         this.competitionId = competitionId;
         this.leadOrganisation = leadOrganisation;
         this.projectManager = projectManager;
         this.organisationFinanceContactMap = organisationFinanceContactMap;
+        this.locationPerPartnerRequired = locationPerPartnerRequired;
+        this.partnerOrganisations = partnerOrganisations;
     }
 
     public ProjectResource getProject() {
@@ -45,5 +53,17 @@ public class ProjectDetailsViewModel {
 
     public Map<OrganisationResource, ProjectUserResource> getOrganisationFinanceContactMap() {
         return organisationFinanceContactMap;
+    }
+
+    public boolean isLocationPerPartnerRequired() {
+        return locationPerPartnerRequired;
+    }
+
+    public String getPostCodeForPartnerOrganisation(Long organisationId) {
+        return partnerOrganisations.stream()
+                .filter(partnerOrganisation ->  partnerOrganisation.getOrganisation().equals(organisationId))
+                .findFirst()
+                .map(PartnerOrganisationResource::getPostCode)
+                .orElse(null);
     }
 }
