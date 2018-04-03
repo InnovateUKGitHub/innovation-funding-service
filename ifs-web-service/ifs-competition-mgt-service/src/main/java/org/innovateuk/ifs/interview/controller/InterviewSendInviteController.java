9d0e351bc4b2b87f15cdc7ec6b850849bf9d4038
Luke Harper <lharper@worth.systems>
2018-04-03 11:24:53 +0100

    IFS-3154 adding resend ability.

--- ifs-web-service/ifs-competition-mgt-service/src/main/java/org/innovateuk/ifs/interview/controller/InterviewSendInviteController.java
+++ ifs-web-service/ifs-competition-mgt-service/src/main/java/org/innovateuk/ifs/interview/controller/InterviewSendInviteController.java
@@ -2,12 +2,14 @@ package org.innovateuk.ifs.interview.controller;
 
 import org.innovateuk.ifs.commons.rest.RestResult;
 import org.innovateuk.ifs.commons.security.SecuredBySpring;
+import org.innovateuk.ifs.commons.service.ServiceResult;
 import org.innovateuk.ifs.controller.ValidationHandler;
 import org.innovateuk.ifs.interview.form.InterviewOverviewSelectionForm;
 import org.innovateuk.ifs.interview.service.InterviewInviteRestService;
 import org.innovateuk.ifs.invite.resource.AssessorInviteSendResource;
 import org.innovateuk.ifs.invite.resource.AssessorInvitesToSendResource;
 import org.innovateuk.ifs.management.controller.CompetitionManagementCookieController;
+import org.innovateuk.ifs.management.form.ResendInviteForm;
 import org.innovateuk.ifs.management.form.SendInviteForm;
 import org.innovateuk.ifs.management.viewmodel.SendInvitesViewModel;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,13 +18,19 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 import java.util.function.Supplier;
 
 import static java.lang.String.format;
 import static org.innovateuk.ifs.commons.rest.RestFailure.error;
+import static org.innovateuk.ifs.controller.ErrorToObjectErrorConverterFactory.asGlobalErrors;
+import static org.innovateuk.ifs.controller.ErrorToObjectErrorConverterFactory.fieldErrorsToFieldErrors;
 import static org.innovateuk.ifs.util.CollectionFunctions.removeDuplicates;
+import static org.innovateuk.ifs.util.MapFunctions.asMap;
 
 /**
  * This controller will handle all Competition Management requests related to sending interview panel invites to assessors
@@ -33,7 +41,7 @@ import static org.innovateuk.ifs.util.CollectionFunctions.removeDuplicates;
 @PreAuthorize("hasAnyAuthority('comp_admin','project_finance')")
 public class InterviewSendInviteController extends CompetitionManagementCookieController<InterviewOverviewSelectionForm> {
 
-    private static final String SELECTION_FORM = "assessmentInterviewPanelOverviewSelectionForm";
+    private static final String SELECTION_FORM = "interviewOverviewSelectionForm";
 
     @Autowired
     private InterviewInviteRestService interviewInviteRestService;
@@ -90,8 +98,98 @@ public class InterviewSendInviteController extends CompetitionManagementCookieCo
         });
     }
 
+    @GetMapping("/reviewResend")
+    public String getInvitesToResendFailureView(Model model,
+                                                @PathVariable("competitionId") long competitionId,
+                                                @ModelAttribute(name = "form", binding = false) ResendInviteForm inviteform,
+                                                BindingResult bindingResult,
+                                                ValidationHandler validationHandler) {
+        if(inviteform.getInviteIds() == null || inviteform.getInviteIds().isEmpty()){
+            return redirectToOverview(competitionId, 0);
+        }
+
+        AssessorInvitesToSendResource invites = interviewInviteRestService.getAllInvitesToResend(
+                competitionId,
+                inviteform.getInviteIds()).getSuccess();
+        model.addAttribute("model", new SendInvitesViewModel(
+                invites.getCompetitionId(),
+                invites.getCompetitionName(),
+                invites.getRecipients(),
+                invites.getContent()
+        ));
+        populateResendInviteFormWithExistingValues(inviteform, invites);
+        return "assessors/interview-resend-invites";
+    }
+
+    @PostMapping("/reviewResend")
+    public String getInvitesToResend(Model model,
+                                     @PathVariable("competitionId") long competitionId,
+                                     @RequestParam(defaultValue = "0") int page,
+                                     @ModelAttribute(SELECTION_FORM) InterviewOverviewSelectionForm selectionForm,
+                                     @ModelAttribute(name = "form", binding = false) ResendInviteForm inviteform,
+                                     ValidationHandler validationHandler,
+                                     BindingResult bindingResult,
+                                     HttpServletRequest request) {
+
+        InterviewOverviewSelectionForm submittedSelectionForm = getSelectionFormFromCookie(request, competitionId)
+                .filter(form -> !form.getSelectedInviteIds().isEmpty())
+                .orElse(selectionForm);
+        Supplier<String> failureView = () -> redirectToOverview(competitionId, page);
+
+        return validationHandler.failNowOrSucceedWith(failureView, () -> {
+            AssessorInvitesToSendResource invites = interviewInviteRestService.getAllInvitesToResend(
+                    competitionId,
+                    submittedSelectionForm.getSelectedInviteIds()).getSuccess();
+            model.addAttribute("model", new SendInvitesViewModel(
+                    invites.getCompetitionId(),
+                    invites.getCompetitionName(),
+                    invites.getRecipients(),
+                    invites.getContent()
+            ));
+            inviteform.setInviteIds(submittedSelectionForm.getSelectedInviteIds());
+            populateResendInviteFormWithExistingValues(inviteform, invites);
+            return "assessors/interview-resend-invites";
+        });
+    }
+
+    @PostMapping("/resend")
+    public String resendInvites (Model model,
+                                 @PathVariable("competitionId") long competitionId,
+                                 @ModelAttribute("form") @Valid ResendInviteForm form,
+                                 BindingResult bindingResult,
+                                 ValidationHandler validationHandler,
+                                 HttpServletResponse response){
+
+        Supplier<String> failureView = () -> getInvitesToResendFailureView(model, competitionId, form, bindingResult, validationHandler);
+
+        return validationHandler.failNowOrSucceedWith(failureView, () -> {
+            ServiceResult<Void> resendResult = interviewInviteRestService.resendInvites(form.getInviteIds(),
+                    new AssessorInviteSendResource(form.getSubject(), form.getContent()))
+                    .toServiceResult();
+            return validationHandler.addAnyErrors(resendResult, fieldErrorsToFieldErrors(), asGlobalErrors())
+                    .failNowOrSucceedWith(failureView, () -> {
+                        removeCookie(response, competitionId);
+                        return redirectToOverview(competitionId, 0);
+                    });
+        });
+    }
+
+    private String redirectToOverview(long competitionId, int page) {
+        UriComponentsBuilder builder = UriComponentsBuilder
+                .fromPath("/assessment/interview/competition/{competitionId}/assessors/pending-and-declined")
+                .queryParam("page", page);
+
+        return "redirect:" + builder.buildAndExpand(asMap("competitionId", competitionId))
+                .toUriString();
+    }
+
     private void populateGroupInviteFormWithExistingValues(SendInviteForm form, AssessorInvitesToSendResource assessorInviteToSendResource) {
         form.setSubject(format("Invitation to Innovate UK interview panel for '%s'", assessorInviteToSendResource.getCompetitionName()));
+    }
+
+
+    private void populateResendInviteFormWithExistingValues(ResendInviteForm form, AssessorInvitesToSendResource assessorInviteToSendResource) {
+        form.setSubject(format("Invitation to assessment panel for '%s'", assessorInviteToSendResource.getCompetitionName()));
     }
 
     private String redirectToInterviewPanelFindTab(long competitionId) {
