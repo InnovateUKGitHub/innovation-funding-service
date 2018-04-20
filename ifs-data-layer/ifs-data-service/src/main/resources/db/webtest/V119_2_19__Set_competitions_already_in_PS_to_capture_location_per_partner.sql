c1a482bc77b2be56a4703fb846bcd8e9f7180374
Duncan Watson <duncan@hiveit.co.uk>
2018-04-20 16:09:40 +0100

    IFS-1875 - added particular competitions as opting into capturing project location

--- ifs-data-layer/ifs-data-service/src/main/resources/db/webtest/V119_2_19__Set_competitions_already_in_PS_to_capture_location_per_partner.sql
+++ ifs-data-layer/ifs-data-service/src/main/resources/db/webtest/V119_2_19__Set_competitions_already_in_PS_to_capture_location_per_partner.sql
@@ -1,2 +1,3 @@
 --IFS-2920 - Set competitions already in Project Setup to capture location per partner
-UPDATE competition SET location_per_partner = TRUE WHERE id = 14;
+UPDATE competition SET location_per_partner = FALSE;
+UPDATE competition SET location_per_partner = TRUE WHERE name IN ('Internet of Things', 'New designs for a circular economy');
