d70e7a1c5ba992cbc0b7b7e9741ea9b1787c2191
Jonathan Lee <jlee@worth.systems>
2018-04-23 13:30:40 +0100

    IFS-3350 gradle task for starting db if it is startable state

--- buildSrc/src/main/groovy/org/innovateuk/ifs/tasks/StartDbContainerIfNotStartedTask.groovy
+++ buildSrc/src/main/groovy/org/innovateuk/ifs/tasks/StartDbContainerIfNotStartedTask.groovy
@@ -2,8 +2,8 @@ package org.innovateuk.ifs.tasks
 
 import com.bmuschko.gradle.docker.tasks.AbstractDockerRemoteApiTask
 import org.gradle.api.tasks.Internal
-
-
+import org.gradle.api.GradleScriptException
+import com.github.dockerjava.api.exception.NotFoundException
 
 class StartDbContainerIfNotStartedTask extends AbstractDockerRemoteApiTask {
 
@@ -11,32 +11,29 @@ class StartDbContainerIfNotStartedTask extends AbstractDockerRemoteApiTask {
     String containerId
 
     void runRemoteCommand(dockerClient) {
-        println "container id is $containerId"
         def inspectCommand = dockerClient.inspectContainerCmd(containerId)
 
         def inspectResponse
         try {
             inspectResponse = inspectCommand.exec()
-        } catch (e) {
-            println "container id $containerId is not found"
-            return
+        } catch (NotFoundException e) {
+            throw new GradleScriptException("container [$containerId] is not found", e)
         }
 
-        println "container id $containerId is found in status $inspectResponse.state.status"
+        def status = inspectResponse.state.status
 
-        def state = response.status.status
+        if("running" == status || "restarting" == status) {
+            logger.info("container [$containerId] is $status - nothing to do")
+        } else if("created" == status || "paused" == status || "exited" == status || "dead" == status) {
 
-        if("running" == state) {
-            println "container id $containerId is running - nothing to do"
-        } else if("created" == state || "paused" == state || "exited" == state || "dead" == state) {
-
-            println "container id $containerId is $state - starting container"
+            logger.info("container [$containerId] is $status - starting container")
 
             def startCommand = dockerClient.startContainerCmd(containerId)
             startCommand.exec()
+        } else if("removing" == status){
+            throw new GradleScriptException("container [$containerId] is in unrecoverable state - $status", null)
         } else {
-            println "container id $containerId is $state - unable to handle this"
-
+            throw new GradleScriptException("container [$containerId] is in unrecognised state - $status", null)
         }
     }
 }
