50177031cc9626cb9c886b661cb876277888681f
Bronnyl Dias <bdias@worth.systems>
2018-04-25 17:38:29 +0100

    IFS-3417-Refactor-ProjectSetUp-DataLayer
    Change-Id: Icf82800500b3c2fa5ff144eb6442a2a0430789fd

--- ifs-data-layer/ifs-data-service/src/test/java/org/innovateuk/ifs/project/core/security/PartnerOrganisationServiceSecurityTest.java
+++ ifs-data-layer/ifs-data-service/src/test/java/org/innovateuk/ifs/project/core/security/PartnerOrganisationServiceSecurityTest.java
@@ -1,4 +1,4 @@
-package org.innovateuk.ifs.project.security;
+package org.innovateuk.ifs.project.core.security;
 
 import org.innovateuk.ifs.BaseServiceSecurityTest;
 import org.innovateuk.ifs.commons.service.ServiceResult;
