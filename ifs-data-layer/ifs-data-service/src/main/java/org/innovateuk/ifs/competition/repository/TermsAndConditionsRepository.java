d7efbaa2fbc739d2e9052793ebb199b17d471832
Fleur Oudenampsen <fleuroudenampsen@gmail.com>
2018-04-30 11:49:59 +0100

    IFS-3086: only use the latest version

--- ifs-data-layer/ifs-data-service/src/main/java/org/innovateuk/ifs/competition/repository/TermsAndConditionsRepository.java
+++ ifs-data-layer/ifs-data-service/src/main/java/org/innovateuk/ifs/competition/repository/TermsAndConditionsRepository.java
@@ -1,6 +1,7 @@
 package org.innovateuk.ifs.competition.repository;
 
 import org.innovateuk.ifs.competition.domain.TermsAndConditions;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
@@ -12,7 +13,12 @@ public interface TermsAndConditionsRepository extends CrudRepository<TermsAndCon
 
     public static final String DEFAULT_TEMPLATE_NAME = "default-terms-and-conditions";
 
+    String FIND_LATEST_VERSION = "SELECT t1 " +
+            "FROM TermsAndConditions t1 " +
+            "WHERE version=(SELECT MAX(version) FROM TermsAndConditions t2 WHERE t1.name=t2.name)";
+
     TermsAndConditions findOneByTemplate(String templateName);
 
-    List<TermsAndConditions> findAll();
+    @Query(FIND_LATEST_VERSION)
+    List<TermsAndConditions> findLatestVersions();
 }
