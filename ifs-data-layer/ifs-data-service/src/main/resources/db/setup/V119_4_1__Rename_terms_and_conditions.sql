1b7315a1c601c9e275ed958eefe44528c30091c8
Fleur Oudenampsen <fleuroudenampsen@gmail.com>
2018-05-01 15:41:54 +0100

    IFS-3086: feedback from content

---
+++ ifs-data-layer/ifs-data-service/src/main/resources/db/setup/V119_4_1__Rename_terms_and_conditions.sql
@@ -0,0 +1,19 @@
+UPDATE terms_and_conditions
+SET name = 'Innovate UK'
+WHERE name = 'Default Terms and Conditions';
+
+UPDATE terms_and_conditions
+SET name = 'Aerospace Technology Institute (ATI)'
+WHERE name = 'ATI Terms and Conditions';
+
+UPDATE terms_and_conditions
+SET name = 'Advanced Propulsion Centre (APC)'
+WHERE name = 'APC Terms and Conditions';
+
+UPDATE terms_and_conditions
+SET name = 'Advanced Propulsion Centre (APC)'
+WHERE name = 'APC Terms and Conditions v2';
+
+UPDATE terms_and_conditions
+SET name = 'The Prince''s Trust'
+WHERE name = 'The Prince''s Trust Terms and Conditions';
