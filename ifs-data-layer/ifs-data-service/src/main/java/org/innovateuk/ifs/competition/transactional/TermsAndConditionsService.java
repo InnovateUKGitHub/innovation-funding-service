04d865ae23497cee4565ad5125ae3a3c6ea59bc8
Ben Outram <ben@hiveit.co.uk>
2018-05-02 12:29:36 +0100

    IFS-3093 TermsAndConditionsService#getLatestSiteTermsAndConditions does not need securing.

--- ifs-data-layer/ifs-data-service/src/main/java/org/innovateuk/ifs/competition/transactional/TermsAndConditionsService.java
+++ ifs-data-layer/ifs-data-service/src/main/java/org/innovateuk/ifs/competition/transactional/TermsAndConditionsService.java
@@ -1,7 +1,6 @@
 package org.innovateuk.ifs.competition.transactional;
 
 import org.innovateuk.ifs.commons.security.NotSecured;
-import org.innovateuk.ifs.commons.security.SecuredBySpring;
 import org.innovateuk.ifs.commons.service.ServiceResult;
 import org.innovateuk.ifs.competition.resource.SiteTermsAndConditionsResource;
 
@@ -10,8 +9,8 @@ import org.innovateuk.ifs.competition.resource.SiteTermsAndConditionsResource;
  */
 public interface TermsAndConditionsService {
 
-    @SecuredBySpring(value = "TODO", description = "TODO")
-    @NotSecured(value = "TODO", mustBeSecuredByOtherServices = false)
+    @NotSecured(value = "Any user can see the latest site terms and conditions",
+            mustBeSecuredByOtherServices = false)
     ServiceResult<SiteTermsAndConditionsResource> getLatestSiteTermsAndConditions();
 
 }
