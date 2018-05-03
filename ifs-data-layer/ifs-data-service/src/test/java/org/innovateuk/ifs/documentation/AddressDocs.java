5e1d0094ca90f5ce97ba45a93b5651c8f13949fa
Bronnyl Dias <bdias@worth.systems>
2018-05-03 13:49:40 +0100

    IFS-3020-Cyclic-Dep-Address-Organisation
    ASCII Docs
    
    Change-Id: Ibce76f09a0ea0a6c113000e3d1405f56803f4f7a

--- ifs-data-layer/ifs-data-service/src/test/java/org/innovateuk/ifs/documentation/AddressDocs.java
+++ ifs-data-layer/ifs-data-service/src/test/java/org/innovateuk/ifs/documentation/AddressDocs.java
@@ -3,7 +3,6 @@ package org.innovateuk.ifs.documentation;
 import org.innovateuk.ifs.address.builder.AddressResourceBuilder;
 import org.springframework.restdocs.payload.FieldDescriptor;
 
-import static java.util.Arrays.asList;
 import static org.innovateuk.ifs.address.builder.AddressResourceBuilder.newAddressResource;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 
@@ -15,8 +14,7 @@ public class AddressDocs {
             fieldWithPath("addressLine3").description("third addressLine"),
             fieldWithPath("town").description("fourth addressLine"),
             fieldWithPath("county").description("county where requested address is located"),
-            fieldWithPath("postcode").description("postcode of the requested address"),
-            fieldWithPath("organisations").description("Ids of the organisation addresses")
+            fieldWithPath("postcode").description("postcode of the requested address")
     };
 
     public static final AddressResourceBuilder addressResourceBuilder = newAddressResource()
@@ -27,7 +25,4 @@ public class AddressDocs {
             .withTown("regular town")
             .withCounty("regular county")
             .withPostcode("PD65OQ");
-    //TODO - FIx this later
-            //.withOrganisationList(asList(1L, 2L, 3L));
-
 }
