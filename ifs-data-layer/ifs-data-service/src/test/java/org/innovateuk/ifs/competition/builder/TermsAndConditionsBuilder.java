f0fb5f9b896b392679975c912fb52063e67e6bcd
Fleur Oudenampsen <fleuroudenampsen@gmail.com>
2018-05-04 10:31:44 +0100

    IFS-3086: feedback pull request, new test classes added

---
+++ ifs-data-layer/ifs-data-service/src/test/java/org/innovateuk/ifs/competition/builder/TermsAndConditionsBuilder.java
@@ -0,0 +1,37 @@
+package org.innovateuk.ifs.competition.builder;
+
+import org.innovateuk.ifs.BaseBuilder;
+import org.innovateuk.ifs.competition.domain.TermsAndConditions;
+import org.innovateuk.ifs.competition.resource.MilestoneType;
+
+import java.util.List;
+import java.util.function.BiConsumer;
+
+import static org.assertj.core.util.Lists.emptyList;
+import static org.innovateuk.ifs.base.amend.BaseBuilderAmendFunctions.createDefault;
+import static org.innovateuk.ifs.base.amend.BaseBuilderAmendFunctions.uniqueIds;
+
+public class TermsAndConditionsBuilder extends BaseBuilder<TermsAndConditions, TermsAndConditionsBuilder>  {
+
+    private TermsAndConditionsBuilder(List<BiConsumer<Integer, TermsAndConditions>> newMultiActions) {
+        super(newMultiActions);
+    }
+
+    public static TermsAndConditionsBuilder newTermsAndConditions() {
+        return new TermsAndConditionsBuilder(emptyList()).with(uniqueIds());
+    }
+
+    public TermsAndConditionsBuilder withName(String... names) {
+        return withArraySetFieldByReflection("name", names);
+    }
+
+    @Override
+    protected TermsAndConditionsBuilder createNewBuilderWithActions(List<BiConsumer<Integer, TermsAndConditions>> actions) {
+        return new TermsAndConditionsBuilder(actions);
+    }
+
+    @Override
+    protected TermsAndConditions createInitial() {
+        return createDefault(TermsAndConditions.class);
+    }
+}
--- ifs-data-layer/ifs-data-service/src/test/java/org/innovateuk/ifs/competition/transactional/TermsAndConditionsServiceImplTest.java
+++ ifs-data-layer/ifs-data-service/src/test/java/org/innovateuk/ifs/competition/transactional/TermsAndConditionsServiceImplTest.java
@@ -1,13 +1,34 @@
 package org.innovateuk.ifs.competition.transactional;
 
 import org.innovateuk.ifs.BaseServiceUnitTest;
+import org.innovateuk.ifs.commons.service.ServiceResult;
+import org.innovateuk.ifs.competition.domain.TermsAndConditions;
+import org.innovateuk.ifs.competition.mapper.TermsAndConditionsMapper;
+import org.innovateuk.ifs.competition.repository.TermsAndConditionsRepository;
+import org.innovateuk.ifs.competition.resource.TermsAndConditionsResource;
 import org.junit.Test;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
+import java.util.List;
+
+import static org.innovateuk.ifs.competition.builder.TermsAndConditionsBuilder.newTermsAndConditions;
+import static org.innovateuk.ifs.competition.builder.TermsAndConditionsResourceBuilder.newTermsAndConditionsResource;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.when;
 
 public class TermsAndConditionsServiceImplTest extends BaseServiceUnitTest<TermsAndConditionsServiceImpl> {
 
     @InjectMocks
     private TermsAndConditionsServiceImpl service;
+
+    @Mock
+    private TermsAndConditionsRepository termsAndConditionsRepository;
+
+    @Mock
+    private TermsAndConditionsMapper termsAndConditionsMapper;
 
     @Override
     protected TermsAndConditionsServiceImpl supplyServiceUnderTest() {
@@ -16,27 +37,38 @@ public class TermsAndConditionsServiceImplTest extends BaseServiceUnitTest<Terms
 
     @Test
     public void test_getTemplateById() {
+        String name = "Innovate UK";
+        TermsAndConditions termsAndConditions = newTermsAndConditions().withName(name).build();
+        TermsAndConditionsResource termsAndConditionsResource = newTermsAndConditionsResource().withName(name).build();
 
-    }
+        when(termsAndConditionsRepository.findOne(termsAndConditions.getId())).thenReturn(termsAndConditions);
+        when(termsAndConditionsMapper.mapToResource(termsAndConditions)).thenReturn(termsAndConditionsResource);
 
-    @Test
-    public void test_getTemplateByNonExistingId() {
-
+        ServiceResult<TermsAndConditionsResource> result = service.getById(termsAndConditions.getId());
+        assertTrue(result.isSuccess());
+        assertNotNull(result);
+        assertEquals(name, result.getSuccess().getName());
     }
 
     @Test
     public void test_getTemplateByNull() {
-
+        ServiceResult<TermsAndConditionsResource> result = service.getById(null);
+        assertTrue(result.isFailure());
+        assertNotNull(result);
     }
 
     @Test
     public void test_getLatestVersionsForAllTermsAndConditions() {
+        List<TermsAndConditions> termsAndConditionsList = newTermsAndConditions().build(3);
+        List<TermsAndConditionsResource> termsAndConditionsResourceList = newTermsAndConditionsResource().build(3);
+
+        when(termsAndConditionsRepository.findLatestVersions()).thenReturn(termsAndConditionsList);
+        when(termsAndConditionsMapper.mapToResource(termsAndConditionsList)).thenReturn(termsAndConditionsResourceList);
+
+        ServiceResult<List<TermsAndConditionsResource>> result = service.getLatestVersionsForAllTermsAndConditions();
+        assertTrue(result.isSuccess());
+        assertNotNull(result);
+        assertEquals(3, result.getSuccess().size());
 
     }
-
-    @Test
-    public void test_getLatestVersionsForAllTermsAndConditionsWithoutResult() {
-
-    }
-
 }
