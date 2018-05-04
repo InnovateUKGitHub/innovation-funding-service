7e8329cc26e1c36748c8282f5e092c5d008f177d
Duncan Watson <duncan@hiveit.co.uk>
2018-05-04 11:43:02 +0100

    IFS-3414 - cleanup before raising PR

--- ifs-commons/src/test/java/org/innovateuk/ifs/ControllerTest.java
+++ ifs-commons/src/test/java/org/innovateuk/ifs/ControllerTest.java
@@ -1,7 +1,10 @@
 package org.innovateuk.ifs;
 
 /**
- * TODO DW - document this class
+ * An annotation to mark a test as a Controller test.
+ *
+ * This is primarily useful for flagging up tests that help produce the Asciidoc API documentation, and we are
+ * therefore able to target only these tests to run when producing API documentation.
  */
 public @interface ControllerTest {
 }
