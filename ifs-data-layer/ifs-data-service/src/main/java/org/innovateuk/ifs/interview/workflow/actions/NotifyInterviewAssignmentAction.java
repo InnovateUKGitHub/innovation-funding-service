a6233d184c570d9bd1cef2ac4dda27266d4c869a
Luke Harper <lharper@worth.systems>
2018-05-08 16:28:59 +0100

    IFS-3253 addressing review comments.

--- ifs-data-layer/ifs-data-service/src/main/java/org/innovateuk/ifs/interview/workflow/actions/NotifyInterviewAssignmentAction.java
+++ ifs-data-layer/ifs-data-service/src/main/java/org/innovateuk/ifs/interview/workflow/actions/NotifyInterviewAssignmentAction.java
@@ -4,13 +4,12 @@ import org.innovateuk.ifs.interview.domain.InterviewAssignment;
 import org.innovateuk.ifs.interview.domain.InterviewAssignmentMessageOutcome;
 import org.innovateuk.ifs.interview.resource.InterviewAssignmentEvent;
 import org.innovateuk.ifs.interview.resource.InterviewAssignmentState;
-import org.innovateuk.ifs.interview.workflow.configuration.InterviewAssignmentWorkflow;
 import org.springframework.statemachine.StateContext;
 import org.springframework.stereotype.Component;
 
 /**
  * The {@code NotifyInterviewAssignmentAction} is used when an interview assignment is made to an application for an interview panel.
- * For more info see {@link InterviewAssignmentWorkflow}
+ * For more info see {@link org.innovateuk.ifs.interview.workflow.configuration.InterviewAssignmentWorkflow}
  */
 @Component
 public class NotifyInterviewAssignmentAction extends BaseInterviewAssignmentAction {
