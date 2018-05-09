3e467a187c8fe966fff9f91c06c53009504c95ac
Adam Mapstone <adam.mapstone@innovateuk.gov.uk>
2018-05-09 14:11:10 +0100

    IFS-3471 - manage endpoint controller and respective test removed.

--- ifs-web-service/ifs-competition-mgt-service/src/main/java/org/innovateuk/ifs/management/controller/CompetitionManagementApplicationsController.java
+++ ifs-web-service/ifs-competition-mgt-service/src/main/java/org/innovateuk/ifs/management/controller/CompetitionManagementApplicationsController.java
@@ -137,16 +137,6 @@ public class CompetitionManagementApplicationsController {
         return "competition/previous-applications";
     }
 
-    @SecuredBySpring(value = "READ", description = "Comp Admins, Project Finance users, Support users and IFS Admins can navigate between different lists of applications to a competition")
-    @PreAuthorize("hasAnyAuthority('comp_admin', 'project_finance', 'support', 'ifs_administrator', 'innovation_lead')")
-    @GetMapping("/manage")
-    public String manageApplications(Model model,
-                                     @PathVariable("competitionId") long competitionId) {
-
-        model.addAttribute("model", navigateApplicationsModelPopulator.populateModel(competitionId));
-        return "competition/navigate-applications";
-    }
-
     @SecuredBySpring(value = "UPDATE", description = "Only the IFS admin is able to mark an application as successful after funding decisions have been made")
     @PreAuthorize("hasAuthority('ifs_administrator')")
     @PostMapping("/mark-successful/application/{applicationId}")
