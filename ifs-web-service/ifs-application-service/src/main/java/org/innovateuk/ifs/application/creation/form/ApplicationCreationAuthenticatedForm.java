09551333dd995e7504318e47fa3b20e777490a03
Grace Williams <grace.williams@innovateuk.gov.uk>
2018-05-15 10:56:50 +0100

    IFS-1963: ApplicationCreationAunthenticatedForm added to ifs

---
+++ ifs-web-service/ifs-application-service/src/main/java/org/innovateuk/ifs/application/creation/form/ApplicationCreationAuthenticatedForm.java
@@ -0,0 +1,45 @@
+package org.innovateuk.ifs.application.creation.form;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.hibernate.validator.constraints.NotBlank;
+import org.innovateuk.ifs.controller.BaseBindingResultTarget;
+
+import javax.validation.constraints.NotNull;
+
+/**
+ * Bean serves as a container for form parameters.
+ */
+
+public class ApplicationCreationAuthenticatedForm extends BaseBindingResultTarget {
+
+    @NotNull(message = "validation.field.confirm.new.application")
+    private Boolean createNewApplication;
+
+    public Boolean getCreateNewApplication() {
+        return createNewApplication;
+    }
+
+    public void setCreateNewApplication(Boolean createNewApplication) {
+        this.createNewApplication = createNewApplication;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        ApplicationCreationAuthenticatedForm that = (ApplicationCreationAuthenticatedForm) o;
+
+        return new EqualsBuilder()
+                .append(createNewApplication, that.createNewApplication)
+                .isEquals();
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder(17, 37)
+                .append(createNewApplication)
+                .toHashCode();
+    }
+}
