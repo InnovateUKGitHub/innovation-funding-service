e4599b0d147998b017f535eb22e7a20ff8c99e0a
Grace Williams <grace.williams@innovateuk.gov.uk>
2018-05-17 13:09:36 +0100

    IFS-1963: Comment added on form file.

--- ifs-web-service/ifs-application-service/src/main/java/org/innovateuk/ifs/application/creation/form/ApplicationCreationAuthenticatedForm.java
+++ ifs-web-service/ifs-application-service/src/main/java/org/innovateuk/ifs/application/creation/form/ApplicationCreationAuthenticatedForm.java
@@ -3,6 +3,10 @@ package org.innovateuk.ifs.application.creation.form;
 import org.innovateuk.ifs.controller.BaseBindingResultTarget;
 import javax.validation.constraints.NotNull;
 
+/**
+ * Form field model for the screen asking whether the user wishes to continue with an existing application.
+ */
+
 public class ApplicationCreationAuthenticatedForm extends BaseBindingResultTarget {
 
     @NotNull(message = "{validation.field.confirm.new.application}")
