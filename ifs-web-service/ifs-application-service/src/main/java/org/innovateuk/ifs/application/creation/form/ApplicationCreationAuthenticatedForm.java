0274a886f0f59d4d02075ddc2c1f3d1f5353b45b
Grace Williams <grace.williams@innovateuk.gov.uk>
2018-05-17 10:32:00 +0100

    IFS-1963: Form file updated

--- ifs-web-service/ifs-application-service/src/main/java/org/innovateuk/ifs/application/creation/form/ApplicationCreationAuthenticatedForm.java
+++ ifs-web-service/ifs-application-service/src/main/java/org/innovateuk/ifs/application/creation/form/ApplicationCreationAuthenticatedForm.java
@@ -3,10 +3,6 @@ package org.innovateuk.ifs.application.creation.form;
 import org.innovateuk.ifs.controller.BaseBindingResultTarget;
 import javax.validation.constraints.NotNull;
 
-/**
- * Bean serves as a container for form parameters.
- */
-
 public class ApplicationCreationAuthenticatedForm extends BaseBindingResultTarget {
 
     @NotNull(message = "{validation.field.confirm.new.application}")
