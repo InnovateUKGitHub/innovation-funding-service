d41100615392379d061f469029dcc4dc3eff0e87
Ewan Cormack <ewan@hiveit.co.uk>
2018-05-21 18:31:39 +0100

    IFS-3554: Implement javascript validation for all uppercase passwords on registration and password change screens

--- ifs-web-service/ifs-web-core/src/main/resources/static/js/ifs_modules/formValidation.js
+++ ifs-web-service/ifs-web-core/src/main/resources/static/js/ifs_modules/formValidation.js
@@ -153,11 +153,12 @@ IFS.core.formValidation = (function () {
     },
     checkPasswordPolicy: function (field, errorStyles) {
       var hasUppercase = IFS.core.formValidation.checkFieldContainsUppercase(field)
+      var hasLowercase = IFS.core.formValidation.checkFieldContainsLowercase(field)
       var hasNumber = IFS.core.formValidation.checkFieldContainsNumber(field)
       var isMinlength = IFS.core.formValidation.checkMinLength(field)
       var isFilledOut = IFS.core.formValidation.checkRequired(field)
       var formGroup = field.closest('.form-group')
-      var confirmsToPasswordPolicy = hasUppercase && hasNumber && isMinlength && isFilledOut
+      var confirmsToPasswordPolicy = hasUppercase && hasLowercase && hasNumber && isMinlength && isFilledOut
       if (errorStyles) {
         if (confirmsToPasswordPolicy) {
           formGroup.removeClass('form-group-error')
@@ -188,6 +189,26 @@ IFS.core.formValidation = (function () {
       IFS.core.formValidation.setStatus(field, uppercaseDataAttribute, hasUppercase)
 
       if (hasUppercase) {
+        IFS.core.formValidation.setValid(field, errorMessage, displayValidationMessages)
+        return true
+      } else {
+        IFS.core.formValidation.setInvalid(field, errorMessage, displayValidationMessages)
+        return false
+      }
+    },
+    checkFieldContainsLowercase: function (field) {
+      var fieldVal = field.val()
+      var lowercaseDataAttribute = 'containsLowercase'
+
+      var displayValidationMessages = IFS.core.formValidation.getMessageDisplaySetting(field, lowercaseDataAttribute)
+      var errorMessage = IFS.core.formValidation.getErrorMessage(field, lowercaseDataAttribute)
+
+      var lowercase = /(?=\S*?[a-z])/
+      var hasLowercase = lowercase.test(fieldVal) !== false
+
+      IFS.core.formValidation.setStatus(field, lowercaseDataAttribute, hasLowercase)
+
+      if (hasLowercase) {
         IFS.core.formValidation.setValid(field, errorMessage, displayValidationMessages)
         return true
       } else {
