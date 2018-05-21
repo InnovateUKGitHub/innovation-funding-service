6d4cbbf68f7afbf238b8f60f408b3d505eb417d6
Bronnyl Dias <bdias@worth.systems>
2018-05-21 10:54:40 +0100

    IFS-3564-ZDD-Phase2Of3-Rename-post_code_to_postcode-partner_organisation
    Clean up
    
    Change-Id: I490f93fdc2721cd3c5ffebc3249bacd9cf7bbd51

--- ifs-data-layer/ifs-data-service/src/main/java/org/innovateuk/ifs/project/core/domain/PartnerOrganisation.java
+++ ifs-data-layer/ifs-data-service/src/main/java/org/innovateuk/ifs/project/core/domain/PartnerOrganisation.java
@@ -1,6 +1,5 @@
 package org.innovateuk.ifs.project.core.domain;
 
-import org.innovateuk.ifs.commons.ZeroDowntime;
 import org.innovateuk.ifs.user.domain.ProcessActivity;
 import org.innovateuk.ifs.user.domain.Organisation;
 
@@ -27,12 +26,6 @@ public class PartnerOrganisation implements ProcessActivity {
 
     private boolean leadOrganisation;
 
-/*    @ZeroDowntime(reference = "IFS-3470", description = "This will be removed in subsequent release")
-    @Column(name = "post_code")
-    private String postCode;*/
-
-    //@ZeroDowntime(reference = "IFS-3470", description = "Remove the Column annotation. Its just added to satisfy Hibernate at the moment")
-    //@Column(name = "postcode")
     private String postcode;
 
     public PartnerOrganisation() {
@@ -61,15 +54,11 @@ public class PartnerOrganisation implements ProcessActivity {
         return leadOrganisation;
     }
 
-    //@ZeroDowntime(reference = "IFS-3470", description = "Currently this will read the old column.")
     public String getPostcode() {
-        //return postCode; // returns the old column postCode (not postcode)
         return postcode;
     }
 
-    //@ZeroDowntime(reference = "IFS-3470", description = "Currently this will write to both old and new.")
     public void setPostcode(String postcode) {
-        //this.postCode = postcode; // Writes to old
-        this.postcode = postcode; // Writes to new
+        this.postcode = postcode;
     }
 }
