63f42dac35968d15dadf440f513fe03cfe296c2b
Luke Harper <lharper@worth.systems>
2018-05-22 10:46:15 +0100

    IFS-3542 fixing unit tests.

--- ifs-web-service/ifs-application-service/src/test/java/org/innovateuk/ifs/application/forms/viewmodel/InterviewFeedbackViewModelTest.java
+++ ifs-web-service/ifs-application-service/src/test/java/org/innovateuk/ifs/application/forms/viewmodel/InterviewFeedbackViewModelTest.java
@@ -10,22 +10,24 @@ public class InterviewFeedbackViewModelTest {
 
     @Test
     public void testConstruct() {
-        InterviewFeedbackViewModel leadWithResponse = new InterviewFeedbackViewModel("response", "feedback", true);
+        InterviewFeedbackViewModel leadWithResponse = new InterviewFeedbackViewModel("response", "feedback", true, true);
 
         assertThat(leadWithResponse.getBannerText(), is(equalTo(InterviewFeedbackViewModel.LEAD_WITH_RESPONSE_BANNER)));
         assertThat(leadWithResponse.hasResponse(), is(true));
         assertThat(leadWithResponse.hasFeedback(), is(true));
+        assertThat(leadWithResponse.displayResponseSection(), is(true));
 
-        InterviewFeedbackViewModel leadWithoutResponse = new InterviewFeedbackViewModel(null, "feedback", true);
+        InterviewFeedbackViewModel leadWithoutResponse = new InterviewFeedbackViewModel(null, "feedback", true, true);
 
         assertThat(leadWithoutResponse.getBannerText(), is(equalTo(InterviewFeedbackViewModel.LEAD_WITHOUT_RESPONSE_BANNER)));
         assertThat(leadWithoutResponse.hasResponse(), is(false));
+        assertThat(leadWithoutResponse.displayResponseSection(), is(false));
 
-        InterviewFeedbackViewModel collabWithResponse = new InterviewFeedbackViewModel("response", "feedback", false);
+        InterviewFeedbackViewModel collabWithResponse = new InterviewFeedbackViewModel("response", "feedback", false, false);
 
         assertThat(collabWithResponse.getBannerText(), is(equalTo(InterviewFeedbackViewModel.COLLAB_WITH_RESPONSE_BANNER)));
 
-        InterviewFeedbackViewModel collabWithoutResponse = new InterviewFeedbackViewModel(null, "feedback", false);
+        InterviewFeedbackViewModel collabWithoutResponse = new InterviewFeedbackViewModel(null, "feedback", false, false);
 
         assertThat(collabWithoutResponse.getBannerText(), is(equalTo(InterviewFeedbackViewModel.COLLAB_WITHOUT_RESPONSE_BANNER)));
     }
