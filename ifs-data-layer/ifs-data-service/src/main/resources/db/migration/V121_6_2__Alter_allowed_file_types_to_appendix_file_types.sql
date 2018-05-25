503e42d5dd4eac294f7e20e377bb4912e4b5ce3f
Fleur Oudenampsen <fleuroudenampsen@gmail.com>
2018-05-25 15:52:49 +0100

    IFS-2959: disable removal of db field and make insertions unique

--- ifs-data-layer/ifs-data-service/src/main/resources/db/migration/V121_6_2__Alter_allowed_file_types_to_appendix_file_types.sql
+++ ifs-data-layer/ifs-data-service/src/main/resources/db/migration/V121_6_2__Alter_allowed_file_types_to_appendix_file_types.sql
@@ -1,12 +1,12 @@
 --IFS-2959 Convert allowed_file_types values to rows in the appendix_file_types table
 
-INSERT INTO appendix_file_types(form_input_id, type)
+INSERT IGNORE INTO appendix_file_types(form_input_id, type)
     SELECT f.id, 'PDF'
   FROM form_input f
   WHERE allowed_file_types LIKE '%PDF%';
 
 
-INSERT INTO appendix_file_types(form_input_id, type)
+INSERT IGNORE INTO appendix_file_types(form_input_id, type)
     SELECT f.id, 'SPREADSHEET'
   FROM form_input f
   WHERE allowed_file_types LIKE '%SPREADSHEET%';
--- ifs-data-layer/ifs-data-service/src/main/resources/db/migration/V121_6_3__Remove_column_allowed_file_types.sql
+++ ifs-data-layer/ifs-data-service/src/main/resources/db/migration/V121_6_3__Remove_column_allowed_file_types.sql
@@ -1,4 +1,4 @@
 --IFS-2959 Remove unnecessary column allowed_file_types
 
-ALTER TABLE form_input DROP COLUMN allowed_file_types;
-
+-- Run this script in ZDD clean up.
+-- ALTER TABLE form_input DROP COLUMN allowed_file_types;
