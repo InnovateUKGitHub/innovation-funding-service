df5f699eedc1891f686ac26406ca1cd308b25c59
Adam Birse <abirse@worth.systems>
2018-06-01 11:25:26 +0100

    Reinstated PrioritySortingTest

--- ifs-commons/src/test/java/org/innovateuk/ifs/util/PrioritySortingTest.java
+++ ifs-commons/src/test/java/org/innovateuk/ifs/util/PrioritySortingTest.java
@@ -1,7 +1,7 @@
 package org.innovateuk.ifs.util;
 
-//import static org.innovateuk.ifs.user.builder.OrganisationResourceBuilder.*;
-//import org.innovateuk.ifs.user.resource.OrganisationResource;
+//import static org.innovateuk.ifs.user.builder.TestResourceBuilder.*;
+//import org.innovateuk.ifs.user.resource.TestResource;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -11,6 +11,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class PrioritySortingTest {
 
@@ -27,26 +28,32 @@ public class PrioritySortingTest {
         assertEquals(list, Arrays.asList("exceptionElement", "A", "C"));
     }
 
-//    @Test
-//    public void sortingListWorksAsExpectedWithFunction() {
-//        List<OrganisationResource> orgs = OrganisationResourceBuilder.newOrganisationResource().withName("C", "A", "exceptionElement").build(3);
-//        List<OrganisationResource> list = new PrioritySorting<>(orgs, OrganisationResourceBuilder.newOrganisationResource()
-//                .withName("exceptionElement").build(), OrganisationResource::getName).unwrap();
-//        assertTrue(list.size() == 3);
-//        Assert.assertEquals(CollectionFunctions.simpleMap(list, OrganisationResource::getName),
-//                CollectionFunctions.simpleMap(OrganisationResourceBuilder.newOrganisationResource().withName("exceptionElement", "A", "C")
-//                        .build(3), OrganisationResource::getName));
-//    }
-//
-//    @Test
-//    public void sortingListWithoutPriorityStillSortsElementsByField() {
-//        List<OrganisationResource> orgs = OrganisationResourceBuilder.newOrganisationResource().withName("C", "A", "exceptionElement").build(3);
-//        List<OrganisationResource> list = new PrioritySorting<>(orgs, null, OrganisationResource::getName).unwrap();
-//        assertTrue(list.size() == 3);
-//        Assert.assertEquals(CollectionFunctions.simpleMap(list, OrganisationResource::getName),
-//                CollectionFunctions.simpleMap(OrganisationResourceBuilder.newOrganisationResource().withName("A", "C", "exceptionElement")
-//                        .build(3), OrganisationResource::getName));
-//    }
+    @Test
+    public void sortingListWorksAsExpectedWithFunction() {
+        List<TestResource> orgs = createResources("C", "A", "exceptionElement");
+        List<TestResource> list = new PrioritySorting<>(orgs, createResource("exceptionElement"), TestResource::getName).unwrap();
+        assertTrue(list.size() == 3);
+        Assert.assertEquals(CollectionFunctions.simpleMap(list, TestResource::getName),
+                CollectionFunctions.simpleMap(createResources("exceptionElement", "A", "C"), TestResource::getName));
+    }
+
+    @Test
+    public void sortingListWithoutPriorityStillSortsElementsByField() {
+        List<TestResource> orgs = createResources("C", "A", "exceptionElement");
+        List<TestResource> list = new PrioritySorting<>(orgs, null, TestResource::getName).unwrap();
+        assertTrue(list.size() == 3);
+        Assert.assertEquals(CollectionFunctions.simpleMap(list, TestResource::getName),
+                CollectionFunctions.simpleMap(createResources("A", "C", "exceptionElement"), TestResource::getName));
+    }
+    
+    private List<TestResource> createResources(String ... names) {
+        return Arrays.stream(names)
+                .map(n -> new TestResource(n))
+                .collect(Collectors.toList());
+    }
+    private TestResource createResource(String name) {  
+        return createResources(name).get(0);
+    }
 
 
 }
---
+++ ifs-commons/src/test/java/org/innovateuk/ifs/util/TestResource.java
@@ -0,0 +1,35 @@
+package org.innovateuk.ifs.util;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestResource {
+
+    private String name;
+
+    public TestResource(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        return EqualsBuilder.reflectionEquals(this, o, false);
+    }
+
+    @Override
+    public int hashCode() {
+        return HashCodeBuilder.reflectionHashCode(this);
+    }
+}
