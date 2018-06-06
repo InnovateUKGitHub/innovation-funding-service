964a4c44c7ced86c0fa38fa7b148ff66869ae548
Ben Outram <ben@hiveit.co.uk>
2018-06-06 10:40:24 +0100

    IFS-3088 Fixing test org.innovateuk.ifs.assessment.overview.controller.AssessmentOverviewControllerTest#getOverview.

--- ifs-resources/src/test/java/org/innovateuk/ifs/form/builder/SectionResourceBuilder.java
+++ ifs-resources/src/test/java/org/innovateuk/ifs/form/builder/SectionResourceBuilder.java
@@ -32,6 +32,14 @@ public class SectionResourceBuilder extends BaseBuilder<SectionResource, Section
         return withArraySetFieldByReflection("questions", questions);
     }
 
+    public SectionResourceBuilder withGeneralQuestions(List<Long>... generalQuestions) {
+        return withArraySetFieldByReflection("generalQuestions", generalQuestions);
+    }
+
+    public SectionResourceBuilder withLeadQuestions(List<Long>... leadQuestions) {
+        return withArraySetFieldByReflection("leadQuestions", leadQuestions);
+    }
+
     public SectionResourceBuilder withCompetitionAndPriority(Long competition, Integer priority) {
         return with(section -> {
             section.setCompetition(competition);
