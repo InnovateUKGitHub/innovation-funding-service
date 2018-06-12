09e730373998aa08f936f8decae18a823edbeb68
Bronnyl Dias <bdias@worth.systems>
2018-06-12 10:49:27 +0100

    IFS-3565-Filter-Internal-Project-Setup-View
    REST Layer Junits.
    
    Change-Id: I781291b95aee456432e7c27d15c37c4810c32851

--- ifs-rest-api-service/src/test/java/org/innovateuk/ifs/project/status/service/StatusRestServiceImplTest.java
+++ ifs-rest-api-service/src/test/java/org/innovateuk/ifs/project/status/service/StatusRestServiceImplTest.java
@@ -23,31 +23,35 @@ public class StatusRestServiceImplTest extends BaseRestServiceUnitTest<StatusRes
         return new StatusRestServiceImpl();
     }
 
-    //TODO - XXX - Fix this
-/*    @Test
-    public void testGetProjectById() {
+    @Test
+    public void getCompetitionStatus() {
+
+        Long competitionId = 1L;
+        String applicationSearchString = "12";
 
         CompetitionProjectsStatusResource returnedResponse = new CompetitionProjectsStatusResource();
 
-        setupGetWithRestResultExpectations(competitionURL + "/123", CompetitionProjectsStatusResource.class, returnedResponse);
+        setupGetWithRestResultExpectations(competitionURL + "/" + competitionId + "?applicationSearchString=" + applicationSearchString, CompetitionProjectsStatusResource.class, returnedResponse);
 
-        RestResult<CompetitionProjectsStatusResource> result = service.getCompetitionStatus(123L);
+        RestResult<CompetitionProjectsStatusResource> result = service.getCompetitionStatus(competitionId, applicationSearchString);
 
         assertTrue(result.isSuccess());
 
         Assert.assertEquals(returnedResponse, result.getSuccess());
-    }*/
+        setupGetWithRestResultVerifications(competitionURL + "/" + competitionId + "?applicationSearchString=" + applicationSearchString, null, CompetitionProjectsStatusResource.class);
+    }
 
     @Test
-    public void testGetStatusByProjectId() {
+    public void getStatusByProjectId() {
         ProjectStatusResource returnedResponse = new ProjectStatusResource();
         setupGetWithRestResultExpectations(projectRestURL + "/123/status", ProjectStatusResource.class, returnedResponse);
         ProjectStatusResource result = service.getProjectStatus(123L).getSuccess();
         Assert.assertEquals(returnedResponse, result);
+        setupGetWithRestResultVerifications(projectRestURL + "/123/status", null, ProjectStatusResource.class);
     }
 
     @Test
-    public void testGetProjectTeamStatus() {
+    public void getProjectTeamStatus() {
         String expectedUrl = projectRestURL + "/123/team-status";
 
         setupGetWithRestResultExpectations(expectedUrl, ProjectTeamStatusResource.class, null, OK);
@@ -55,10 +59,11 @@ public class StatusRestServiceImplTest extends BaseRestServiceUnitTest<StatusRes
         RestResult<ProjectTeamStatusResource> result = service.getProjectTeamStatus(123L, Optional.empty());
 
         assertTrue(result.isSuccess());
+        setupGetWithRestResultVerifications(projectRestURL + "/123/team-status", null, ProjectTeamStatusResource.class);
     }
 
     @Test
-    public void testGetProjectTeamStatusWithFilterByUserId() {
+    public void getProjectTeamStatusWithFilterByUserId() {
         String expectedUrl = projectRestURL + "/123/team-status?filterByUserId=456";
 
         setupGetWithRestResultExpectations(expectedUrl, ProjectTeamStatusResource.class, null, OK);
@@ -66,5 +71,6 @@ public class StatusRestServiceImplTest extends BaseRestServiceUnitTest<StatusRes
         RestResult<ProjectTeamStatusResource> result = service.getProjectTeamStatus(123L, Optional.of(456L));
 
         assertTrue(result.isSuccess());
+        setupGetWithRestResultVerifications(projectRestURL + "/123/team-status?filterByUserId=456", null, ProjectTeamStatusResource.class);
     }
 }
