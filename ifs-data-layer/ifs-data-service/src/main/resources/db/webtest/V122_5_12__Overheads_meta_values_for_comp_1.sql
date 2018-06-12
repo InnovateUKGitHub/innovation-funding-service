4c3d8dbd7872415c114d9b59c4552734b2ce5850
Duncan Watson <duncan@hiveit.co.uk>
2018-06-12 10:21:20 +0100

    IFS-3577 - upped patch level to conform to new release patch level

--- ifs-data-layer/ifs-data-service/src/main/resources/db/webtest/V122_5_12__Overheads_meta_values_for_comp_1.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO finance_row_meta_value (finance_row_id, finance_row_meta_field_id, value)
-	SELECT id, 5, 'false' from finance_row where name='overhead' and question_id=29;
--- ifs-data-layer/ifs-data-service/src/main/resources/db/webtest/V122_5_18__Update_on_other_funding_content.sql
+++
@@ -1,5 +0,0 @@
--- Script to update the first line of the content on Other funding question.
--- Have been discussed to place this to webtest folder and to be applied after the webtest data (IFS-2317)
-UPDATE question
-SET description='Tell us if you have received any other public sector funding for this project. This information is important as other public sector support counts as part of the funding you can receive for your project.'
-WHERE description LIKE '%or received any other public sector funding for this project. This information is important as other public sector%' AND question_type <> 'GENERAL';
