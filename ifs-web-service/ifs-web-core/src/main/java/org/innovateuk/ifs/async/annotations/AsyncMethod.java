16066d7ac99aa57946295e7f4c098adec5d48ee7
Tom Baldwin <tom@polymorphism.co.uk>
2018-06-13 10:38:14 +0100

    IFS-2873 - Removes cyclic dependencies from async generation

--- ifs-web-service/ifs-web-core/src/main/java/org/innovateuk/ifs/async/annotations/AsyncMethod.java
+++ ifs-web-service/ifs-web-core/src/main/java/org/innovateuk/ifs/async/annotations/AsyncMethod.java
@@ -1,10 +1,12 @@
 package org.innovateuk.ifs.async.annotations;
 
+import org.innovateuk.ifs.async.generation.AsyncFuturesGenerator;
+
 import java.lang.annotation.*;
 
 /**
  * Allows a method (and any methods that it uses) to use the Async futures mechanism via
- * {@link org.innovateuk.ifs.async.generation.AsyncFuturesGenerator}
+ * {@link AsyncFuturesGenerator}
  */
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
