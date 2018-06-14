f73f36efa74cd2172d52001356dbe733d85a474a
Bronnyl Dias <bdias@worth.systems>
2018-06-14 16:52:29 +0100

    IFS-3683-Pending-Queries-SP-BD-Withdraw-Project
    Fixing integration tests.
    
    Change-Id: Idd6a130ebdba0b672891db36843fe159a41f1cfc

---
+++ ifs-data-layer/ifs-data-service/src/main/resources/db/integration/V123_2_1__Set_project_state_in_process.sql
@@ -0,0 +1,14 @@
+insert into process (event, last_modified, process_type, target_id, activity_state_id)
+values ('project-created', NOW(), 'ProjectProcess', 5, 17);
+
+insert into process (event, last_modified, process_type, target_id, activity_state_id)
+values ('project-created', NOW(), 'ProjectProcess', 3, 17);
+
+insert into process (event, last_modified, process_type, target_id, activity_state_id)
+values ('project-created', NOW(), 'ProjectProcess', 2, 17);
+
+insert into process (event, last_modified, process_type, target_id, activity_state_id)
+values ('project-created', NOW(), 'ProjectProcess', 6, 17);
+
+insert into process (event, last_modified, process_type, target_id, activity_state_id)
+values ('project-created', NOW(), 'ProjectProcess', 7, 17);
