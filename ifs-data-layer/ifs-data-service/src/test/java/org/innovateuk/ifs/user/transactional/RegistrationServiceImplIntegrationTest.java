c86f4c6d4feb1f361539e6386d9e59164198ca50
Duncan Watson <duncan@hiveit.co.uk>
2018-06-15 12:26:24 +0100

    IFS-3648 - added test for alternative user registration

--- ifs-data-layer/ifs-data-service/src/test/java/org/innovateuk/ifs/user/transactional/RegistrationServiceImplIntegrationTest.java
+++ ifs-data-layer/ifs-data-service/src/test/java/org/innovateuk/ifs/user/transactional/RegistrationServiceImplIntegrationTest.java
@@ -1,10 +1,13 @@
 package org.innovateuk.ifs.user.transactional;
 
+import org.hibernate.Hibernate;
 import org.innovateuk.ifs.BaseAuthenticationAwareIntegrationTest;
 import org.innovateuk.ifs.authentication.service.RestIdentityProviderService;
 import org.innovateuk.ifs.commons.error.Error;
 import org.innovateuk.ifs.commons.service.AbstractRestTemplateAdaptor;
 import org.innovateuk.ifs.commons.service.ServiceResult;
+import org.innovateuk.ifs.organisation.domain.Organisation;
+import org.innovateuk.ifs.organisation.repository.OrganisationRepository;
 import org.innovateuk.ifs.registration.resource.UserRegistrationResource;
 import org.innovateuk.ifs.testdata.services.TestService;
 import org.innovateuk.ifs.user.repository.UserRepository;
@@ -17,11 +20,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.util.ReflectionTestUtils;
 
+import java.util.function.Consumer;
+
 import static java.util.Collections.emptyList;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.innovateuk.ifs.commons.error.CommonFailureKeys.GENERAL_UNEXPECTED_ERROR;
 import static org.innovateuk.ifs.registration.builder.UserRegistrationResourceBuilder.newUserRegistrationResource;
 import static org.innovateuk.ifs.user.builder.EthnicityResourceBuilder.newEthnicityResource;
+import static org.innovateuk.ifs.user.builder.UserResourceBuilder.newUserResource;
 import static org.innovateuk.ifs.util.Either.left;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.mock;
@@ -40,18 +46,15 @@ public class RegistrationServiceImplIntegrationTest extends BaseAuthenticationAw
     private UserRepository userRepository;
 
     @Autowired
+    private OrganisationRepository organisationRepository;
+
+    @Autowired
     private TestService testService;
 
     @Test
     public void registrationApiBeingUnavailableDoesntLeavePartialUserDataInDatabase() {
 
-        // swap out the real RestTemplate adaptor from the IDP service, so we can simulate a failure in communicating
-        // with the registration API
-        AbstractRestTemplateAdaptor originalRestTemplate = (AbstractRestTemplateAdaptor) ReflectionTestUtils.getField(idpService, "adaptor");
-        AbstractRestTemplateAdaptor mockRestTemplate = mock(AbstractRestTemplateAdaptor.class);
-        ReflectionTestUtils.setField(idpService, "adaptor", mockRestTemplate);
-
-        try {
+        doWithMockIdpRestTemplate(mockRestTemplate -> {
 
             Either<ResponseEntity<Object>, ResponseEntity<Object>> responseFromRegistrationApi =
                     left(new ResponseEntity<>(SERVICE_UNAVAILABLE));
@@ -78,10 +81,81 @@ public class RegistrationServiceImplIntegrationTest extends BaseAuthenticationAw
 
             // assert that no partial user data remains in the database
             assertThat(userRepository.findByEmail("thebspig@example.com")).isEmpty();
+        });
+    }
 
+    @Test
+    public void registrationApiBeingUnavailableDoesntLeavePartialOrganisationUserDataInDatabase() {
+
+        doWithMockIdpRestTemplate(mockRestTemplate -> {
+
+            Either<ResponseEntity<Object>, ResponseEntity<Object>> responseFromRegistrationApi =
+                    left(new ResponseEntity<>(SERVICE_UNAVAILABLE));
+
+            Organisation organisation = getOrganisationForTest();
+            int originalUserCount = organisation.getUsers().size();
+            int originalProcessRoleCount = organisation.getProcessRoles().size();
+
+            when(mockRestTemplate.restPostWithEntity(any(), any(), any(), any(), any())).thenReturn(responseFromRegistrationApi);
+
+            testService.doWithinTransaction(this::loginSystemRegistrationUser);
+
+            UserResource registrationInfo = newUserResource().
+                    withTitle(Title.Dr).
+                    withFirstName("Bob").
+                    withLastName("Spiggot").
+                    withEmail("thebspig@example.com").
+                    withGender(Gender.MALE).
+                    withPassword("thebspig").
+                    build();
+
+            // assert that we got a failure indicating that the Registration API was not available
+            ServiceResult<UserResource> result = registrationService.createOrganisationUser(organisation.getId(), registrationInfo);
+            assertThat(result.isFailure()).isTrue();
+            assertThat(result.getFailure().is(new Error(GENERAL_UNEXPECTED_ERROR, SERVICE_UNAVAILABLE))).isTrue();
+
+            // assert that no partial user data remains in the database
+            assertThat(userRepository.findByEmail("thebspig@example.com")).isEmpty();
+
+            Organisation freshOrganisation = getOrganisationForTest();
+
+            // assert no association data is changed for this organisation
+            int freshUserCount = freshOrganisation.getUsers().size();
+            int freshProcessRoleCount = freshOrganisation.getProcessRoles().size();
+            assertThat(freshUserCount).isEqualTo(originalUserCount);
+            assertThat(freshProcessRoleCount).isEqualTo(originalProcessRoleCount);
+        });
+    }
+
+    private Organisation getOrganisationForTest() {
+        return testService.doWithinTransaction(() -> {
+            Organisation organisation = organisationRepository.findOneByName("Empire Ltd");
+            Hibernate.initialize(organisation);
+            Hibernate.initialize(organisation.getUsers());
+            Hibernate.initialize(organisation.getProcessRoles());
+            return organisation;
+        });
+    }
+
+    /**
+     * Temporarily swaps out the IDP Service's Rest Template out for a mock one during a test run, and restores the
+     * original afterwards
+     */
+    private void doWithMockIdpRestTemplate(Consumer<AbstractRestTemplateAdaptor> testCode) {
+
+        // swap out the real RestTemplate adaptor from the IDP service, so we can simulate a failure in communicating
+        // with the registration API
+        AbstractRestTemplateAdaptor originalRestTemplate = (AbstractRestTemplateAdaptor) ReflectionTestUtils.getField(idpService, "adaptor");
+        AbstractRestTemplateAdaptor mockRestTemplate = mock(AbstractRestTemplateAdaptor.class);
+        ReflectionTestUtils.setField(idpService, "adaptor", mockRestTemplate);
+
+        try {
+
+            // run the test code, given the mock rest template
+            testCode.accept(mockRestTemplate);
         } finally {
 
-            // finally swap the original real RestTemplate adaptor back in
+            // and finally swap the original real RestTemplate adaptor back in
             ReflectionTestUtils.setField(idpService, "adaptor", originalRestTemplate);
         }
     }
