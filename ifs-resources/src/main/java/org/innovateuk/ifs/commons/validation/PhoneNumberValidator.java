2322d45d87ab572385031f70a2fbf31cd0e43eda
Adam Mapstone <adam.mapstone@innovateuk.gov.uk>
2018-06-15 11:24:55 +0100

    IFS-3365 - Unit tests added for PhoneNumberValidator

--- ifs-resources/src/main/java/org/innovateuk/ifs/commons/validation/PhoneNumberValidator.java
+++ ifs-resources/src/main/java/org/innovateuk/ifs/commons/validation/PhoneNumberValidator.java
@@ -1,5 +1,8 @@
 package org.innovateuk.ifs.commons.validation;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * This regex matches a null entry or a valid entry, as we don't want to perform a telephone check on a null value,
  * it then discounts permitted non numeric characters from the 8-20 count, checks out numerics and the following
@@ -7,5 +10,16 @@ package org.innovateuk.ifs.commons.validation;
  */
 public class PhoneNumberValidator {
 
+    Pattern pattern;
+    private Matcher matcher;
     public static final String VALID_PHONE_NUMBER_8_20_DIGITS = "^[\\\\)\\\\(\\\\+\\s-]*(?:\\d[\\\\)\\\\(\\\\+\\s-]*){8,20}$";
+
+    public PhoneNumberValidator(){
+        pattern = Pattern.compile(VALID_PHONE_NUMBER_8_20_DIGITS);
+    }
+
+    public boolean validate(final String phonenumber){
+        matcher = pattern.matcher(phonenumber);
+        return matcher.matches();
+    }
 }
