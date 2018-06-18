d3e9c601e52146b1754125455dbec4461c75ce8e
Adam Mapstone <adam.mapstone@innovateuk.gov.uk>
2018-06-18 08:33:27 +0100

    IFS-3365 - Update to comment in PhoneNumberValidator.

--- ifs-resources/src/main/java/org/innovateuk/ifs/commons/validation/PhoneNumberValidator.java
+++ ifs-resources/src/main/java/org/innovateuk/ifs/commons/validation/PhoneNumberValidator.java
@@ -5,8 +5,8 @@ import java.util.regex.Pattern;
 
 /**
  * This regex matches a null entry or a valid entry, as we don't want to perform a telephone check on a null value,
- * it then discounts permitted non numeric characters from the 8-20 count, checks out numerics and the following
- * permitted characters, and finally checks that the string is between 8-20.
+ * it then discounts permitted non numeric characters from the 8-20 count, checks out numerics and
+ * permitted characters (+()-), and finally checks that the string is between 8-20.
  */
 public class PhoneNumberValidator {
 
@@ -14,11 +14,11 @@ public class PhoneNumberValidator {
     private Matcher matcher;
     public static final String VALID_PHONE_NUMBER = "^[\\\\)\\\\(\\\\+\\s-]*(?:\\d[\\\\)\\\\(\\\\+\\s-]*){8,20}$";
 
-    public PhoneNumberValidator(){
+    public PhoneNumberValidator() {
         pattern = Pattern.compile(VALID_PHONE_NUMBER);
     }
 
-    public boolean validate(final String phonenumber){
+    public boolean validate(final String phonenumber) {
         matcher = pattern.matcher(phonenumber);
         return matcher.matches();
     }
