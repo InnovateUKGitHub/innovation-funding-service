d85b3190b7b6068ea54216fd62b49b000ff326c8
Adam Mapstone <adam.mapstone@innovateuk.gov.uk>
2018-06-18 14:52:47 +0100

    IFS-3609 - Competition Admin & Project Finances roles added to controller.

--- ifs-web-service/ifs-application-service/src/main/java/org/innovateuk/ifs/application/forms/controller/ApplicationSectionController.java
+++ ifs-web-service/ifs-application-service/src/main/java/org/innovateuk/ifs/application/forms/controller/ApplicationSectionController.java
@@ -115,9 +115,11 @@ public class ApplicationSectionController {
     }
 
     @SecuredBySpring(value = "TODO", description = "TODO")
-    @PreAuthorize("hasAnyAuthority('support', 'innovation_lead', 'ifs_administrator')")
+    @PreAuthorize("hasAnyAuthority('support', 'innovation_lead', 'ifs_administrator', 'comp_admin', 'project_finance')")
     @GetMapping(SECTION_URL + "{sectionId}/{applicantOrganisationId}")
-    public String applicationFormWithOpenSectionForApplicant(@Valid @ModelAttribute(name = MODEL_ATTRIBUTE_FORM, binding = false) ApplicationForm form, BindingResult bindingResult, Model model,
+    public String applicationFormWithOpenSectionForApplicant(@Valid @ModelAttribute(name = MODEL_ATTRIBUTE_FORM, binding = false) ApplicationForm form,
+                                                             BindingResult bindingResult,
+                                                             Model model,
                                                              @PathVariable(APPLICATION_ID) final Long applicationId,
                                                              @PathVariable("sectionId") final Long sectionId,
                                                              @PathVariable("applicantOrganisationId") final Long applicantOrganisationId,
