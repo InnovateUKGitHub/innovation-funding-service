afab587246393f2cc72613c5b3da7ca69071b8df
Kieran Hester <>
2018-06-19 11:42:18 +0100

    IFS-3603 update properties file

--- ifs-web-service/ifs-survey-service/src/main/java/org/innovateuk/ifs/survey/SurveyController.java
+++ ifs-web-service/ifs-survey-service/src/main/java/org/innovateuk/ifs/survey/SurveyController.java
@@ -1,22 +1,18 @@
 package org.innovateuk.ifs.survey;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-import org.innovateuk.ifs.commons.security.SecuredBySpring;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/**
+ * A controller for users to complete the satisfaction survey after submitting an application.
+ */
 @Controller
 @RequestMapping("/survey")
-@SecuredBySpring(value = "Controller", description = "TODO" , securedType = SurveyController.class)
 public class SurveyController {
 
-    private static final Log LOG = LogFactory.getLog(SurveyController.class);
-
-    @GetMapping
+    @GetMapping("/feedback")
     public String view() {
-        LOG.debug("Survey View");
         return "survey/survey";
     }
 }
