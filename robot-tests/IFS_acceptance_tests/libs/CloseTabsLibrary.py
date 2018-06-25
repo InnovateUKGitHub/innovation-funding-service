118d32557f42fe7dbcd7c5608b379870d408a328
Fahad <fahmed@worth.systems>
2018-06-25 13:33:28 +0100

    IFS-3689 Changed se2lib variable to browser.

--- robot-tests/IFS_acceptance_tests/libs/CloseTabsLibrary.py
+++ robot-tests/IFS_acceptance_tests/libs/CloseTabsLibrary.py
@@ -1,9 +1,9 @@
 from robot.libraries.BuiltIn import BuiltIn
 
-se2lib = BuiltIn().get_library_instance('Selenium2Library')
+browser = BuiltIn().get_library_instance('Selenium2Library')
 
 def get_webdriver_instance():
-    return se2lib._current_browser()  # This is used because switch_to needs a webdriver instance, not a Selenium2Library instance.
+    return browser._current_browser()  # This is used because switch_to needs a webdriver instance, not a Selenium2Library instance.
 
 def the_user_closes_the_last_opened_tab():
     get_webdriver_instance().switch_to.window(get_webdriver_instance().window_handles[-1])  # Switches to the last opened tab.
