666e7f9c61c792bd3dd5f42b526fecf10d79f50d
Fahad <fahmed@worth.systems>
2018-06-25 10:48:54 +0100

    IFS-3689 Removing comments.

--- robot-tests/IFS_acceptance_tests/libs/CloseTabsLibrary.py
+++ robot-tests/IFS_acceptance_tests/libs/CloseTabsLibrary.py
@@ -1,7 +1,3 @@
-#from selenium import webdriver
-# from selenium.webdriver.common.by import By
-# from selenium.webdriver.support.ui import WebDriverWait
-# from selenium.webdriver.support import expected_conditions as EC
 from robot.libraries.BuiltIn import BuiltIn
 
 se2lib = BuiltIn().get_library_instance('Selenium2Library')
