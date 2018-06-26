595ce5ee977548747230d303f6b0c968e3442e74
Fahad <fahmed@worth.systems>
2018-06-26 08:33:40 +0100

    IFS-3689 Changes to the library.

--- robot-tests/IFS_acceptance_tests/libs/CloseTabsLibrary.py
+++ robot-tests/IFS_acceptance_tests/libs/CloseTabsLibrary.py
@@ -1,11 +1,9 @@
 from robot.libraries.BuiltIn import BuiltIn
 
-browser = BuiltIn().get_library_instance('Selenium2Library')
-
-def get_webdriver_instance():
-    return browser._current_browser()  # This is used because switch_to needs a webdriver instance, not a Selenium2Library instance.
+se2lib = BuiltIn().get_library_instance('Selenium2Library')
 
 def the_user_closes_the_last_opened_tab():
-    get_webdriver_instance().switch_to.window(get_webdriver_instance().window_handles[-1])  # Switches to the last opened tab.
-    get_webdriver_instance().close()
-    get_webdriver_instance().switch_to.window(get_webdriver_instance().window_handles[0])   # Switches back to the main tab.
+    browser = se2lib._current_browser()  # This is used because switch_to needs a webdriver instance, not a Selenium2Library instance.
+    browser.switch_to.window(browser.window_handles[-1])  # Switches to the last opened tab.
+    browser.close()
+    browser.switch_to.window(browser.window_handles[0])   # Switches back to the main tab.
