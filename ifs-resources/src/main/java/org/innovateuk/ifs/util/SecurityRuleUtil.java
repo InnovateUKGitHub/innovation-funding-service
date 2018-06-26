2ed530e0979d979c5128705247b281b5110a9599
Adam Mapstone <adam.mapstone@innovateuk.gov.uk>
2018-06-26 09:54:44 +0100

    IFS-3609 - UserResource returned in place of User.

--- ifs-resources/src/main/java/org/innovateuk/ifs/util/SecurityRuleUtil.java
+++ ifs-resources/src/main/java/org/innovateuk/ifs/util/SecurityRuleUtil.java
@@ -1,6 +1,6 @@
 package org.innovateuk.ifs.util;
 
-import org.innovateuk.ifs.user.domain.User;
+import org.innovateuk.ifs.user.resource.UserResource;
 
 import static org.innovateuk.ifs.user.resource.Role.*;
 
@@ -8,42 +8,42 @@ public final class SecurityRuleUtil {
 
     private SecurityRuleUtil() {}
 
-    public static boolean isCompAdmin(User user) {
+    public static boolean isCompAdmin(UserResource user) {
         return user.hasRole(COMP_ADMIN);
     }
 
-    public static boolean isInternal(User user) {
+    public static boolean isInternal(UserResource user) {
         return user.isInternalUser();
     }
 
-    public static boolean isInternalAdmin(User user) {
+    public static boolean isInternalAdmin(UserResource user) {
         return user.hasRole(COMP_ADMIN) ||
                 user.hasRole(PROJECT_FINANCE);
     }
 
-    public static boolean isProjectFinanceUser(User user) {
+    public static boolean isProjectFinanceUser(UserResource user) {
         return user.hasRole(PROJECT_FINANCE);
     }
 
-    public static boolean isSystemMaintenanceUser(User user) {
+    public static boolean isSystemMaintenanceUser(UserResource user) {
         return user.hasRole(SYSTEM_MAINTAINER);
     }
 
-    public static boolean isSystemRegistrationUser(User user) {
+    public static boolean isSystemRegistrationUser(UserResource user) {
         return user.hasRole(SYSTEM_REGISTRATION_USER);
     }
 
-    public static boolean isAssessor(User user) {
+    public static boolean isAssessor(UserResource user) {
         return user.hasRole(ASSESSOR);
     }
 
-    public static boolean isSupport(User user) {
+    public static boolean isSupport(UserResource user) {
         return user.hasRole(SUPPORT); }
 
-    public static boolean isInnovationLead(User user) {
+    public static boolean isInnovationLead(UserResource user) {
         return user.hasRole(INNOVATION_LEAD); }
 
-    public static boolean isIFSAdmin(User user) {
+    public static boolean isIFSAdmin(UserResource user) {
         return user.hasRole(IFS_ADMINISTRATOR);
     }
 }
