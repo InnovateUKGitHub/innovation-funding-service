b28012945898b67ef638c627e4c4d8c720adf8b3
Grace Williams <grace.williams@innovateuk.gov.uk>
2018-06-28 15:50:36 +0100

    sonarcube-improvements: replaced all tabs with white spaces

--- ifs-data-layer/ifs-data-service/src/main/java/org/innovateuk/ifs/application/resource/comparators/ApplicationSummaryResourceNumberOfPartnersComparator.java
+++ ifs-data-layer/ifs-data-service/src/main/java/org/innovateuk/ifs/application/resource/comparators/ApplicationSummaryResourceNumberOfPartnersComparator.java
@@ -9,15 +9,15 @@ import java.util.Comparator;
  */
 public class ApplicationSummaryResourceNumberOfPartnersComparator extends DualFieldComparator<Integer, Long> implements Comparator<ApplicationSummaryResource> {
 
-	@Override
-	public int compare(ApplicationSummaryResource resource1, ApplicationSummaryResource resource2) {
-		
-		Integer o1Lead = resource1.getNumberOfPartners();
-		Integer o2Lead = resource2.getNumberOfPartners();
-		
-		Long o1Id = resource1.getId();
-		Long o2Id = resource2.getId();
-		
-		return compare(o1Lead, o2Lead, o1Id, o2Id);
-	}
+    @Override
+    public int compare(ApplicationSummaryResource resource1, ApplicationSummaryResource resource2) {
+
+        Integer o1Lead = resource1.getNumberOfPartners();
+        Integer o2Lead = resource2.getNumberOfPartners();
+
+        Long o1Id = resource1.getId();
+        Long o2Id = resource2.getId();
+
+        return compare(o1Lead, o2Lead, o1Id, o2Id);
+    }
 }
