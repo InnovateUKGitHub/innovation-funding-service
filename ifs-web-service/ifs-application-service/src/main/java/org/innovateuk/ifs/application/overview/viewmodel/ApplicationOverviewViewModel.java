c2b01c8f2db317479ae6fa716c61e83c0e6aa479
Fleur Oudenampsen <fleuroudenampsen@gmail.com>
2018-07-04 11:41:44 +0100

    IFS-3820 created incomplete/complete for “Finances overview”

--- ifs-web-service/ifs-application-service/src/main/java/org/innovateuk/ifs/application/overview/viewmodel/ApplicationOverviewViewModel.java
+++ ifs-web-service/ifs-application-service/src/main/java/org/innovateuk/ifs/application/overview/viewmodel/ApplicationOverviewViewModel.java
@@ -3,6 +3,7 @@ package org.innovateuk.ifs.application.overview.viewmodel;
 import org.innovateuk.ifs.application.resource.ApplicationResource;
 import org.innovateuk.ifs.category.resource.ResearchCategoryResource;
 import org.innovateuk.ifs.competition.resource.CompetitionResource;
+import org.innovateuk.ifs.form.resource.SectionResource;
 import org.innovateuk.ifs.organisation.resource.OrganisationResource;
 import org.innovateuk.ifs.project.resource.ProjectResource;
 
@@ -93,5 +94,12 @@ public class ApplicationOverviewViewModel {
 
     public ApplicationOverviewSectionViewModel getSection() {
         return section;
+    }
+
+    public boolean isComplete(SectionResource section) {
+        if (section.getId().equals(financeSectionId)) {
+            return completed.getUserFinanceSectionCompleted();
+        }
+        return completed.getSectionsMarkedAsComplete().contains(financeSectionId);
     }
 }
