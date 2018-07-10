89910fb0c400e41f4f8e8782fea8b286bcb397d7
Tom Baldwin <tom@polymorphism.co.uk>
2018-07-10 15:51:42 +0100

    IFS-3313 - moves applicationContext null checks into ProcessHistoryRepositoryProvider

--- ifs-data-layer/ifs-data-service/src/main/java/org/innovateuk/ifs/workflow/audit/ProcessHistoryEntityListener.java
+++ ifs-data-layer/ifs-data-service/src/main/java/org/innovateuk/ifs/workflow/audit/ProcessHistoryEntityListener.java
@@ -1,24 +1,18 @@
 package org.innovateuk.ifs.workflow.audit;
 
 import org.innovateuk.ifs.workflow.domain.Process;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.context.support.SpringBeanAutowiringSupport;
 
 import javax.persistence.PreUpdate;
+
+import static org.innovateuk.ifs.workflow.audit.ProcessHistoryRepositoryProvider.processHistoryRepository;
 
 /**
  * {@code EntityListener} to create new {@link ProcessHistory}s whenever a {@link Process} is updated.
  */
 public class ProcessHistoryEntityListener {
 
-    @Autowired
-    private ProcessHistoryRepository processHistoryRepository;
-
     @PreUpdate
     public void preUpdate(Process process) {
-        SpringBeanAutowiringSupport.processInjectionBasedOnCurrentContext(this);
-        if (processHistoryRepository != null) {
-            processHistoryRepository.save(new ProcessHistory(process));
-        }
+        processHistoryRepository().ifPresent(r -> r.save(new ProcessHistory(process)));
     }
 }
--- ifs-data-layer/ifs-data-service/src/main/java/org/innovateuk/ifs/workflow/audit/ProcessHistoryRepositoryProvider.java
+++ ifs-data-layer/ifs-data-service/src/main/java/org/innovateuk/ifs/workflow/audit/ProcessHistoryRepositoryProvider.java
@@ -1,28 +1,26 @@
 package org.innovateuk.ifs.workflow.audit;
 
 import org.springframework.beans.BeansException;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
 
+import java.util.Optional;
+
 /**
  * TODO class comment
  */
+@Component
 public class ProcessHistoryRepositoryProvider implements ApplicationContextAware {
-
 
     private static volatile ApplicationContext applicationContext;
 
-    public static ProcessHistoryRepository processHistoryRepository() {
-        if (ProcessHistoryRepositoryProvider.applicationContext == null) {
-            throw new IllegalStateException("applicationContext not autowired in ProcessEntityListener");
-        }
-        return ProcessHistoryRepositoryProvider.applicationContext.getBean(ProcessHistoryRepository.class);
+    public static Optional<ProcessHistoryRepository> processHistoryRepository() {
+        return Optional.ofNullable(applicationContext == null ?
+                null : ProcessHistoryRepositoryProvider.applicationContext.getBean(ProcessHistoryRepository.class));
 
     }
 
-    @Autowired
     public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
         ProcessHistoryRepositoryProvider.applicationContext = applicationContext;
     }
