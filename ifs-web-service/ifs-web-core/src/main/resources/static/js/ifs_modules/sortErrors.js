e8fddb193b3798ddf2a21847b1ba4c5a1a29d0b1
Nathan Phillips <nathanphillips@MacBook-Pro-2.local>
2018-07-17 13:56:14 +0100

    IFS-3945 updated comments, escape non standard characters before using as a selector

--- ifs-web-service/ifs-web-core/src/main/resources/static/js/ifs_modules/sortErrors.js
+++ ifs-web-service/ifs-web-core/src/main/resources/static/js/ifs_modules/sortErrors.js
@@ -1,41 +1,52 @@
+// Sorts errors to match order of fields on page
+// Error sorting order: global errors (no link) -> errors with dead links -> errors in link order
+// within the same link errors are sorted alphabetically
+// if there is no error list code exits immediately
 IFS.core.sortingErrors = (function () {
   'use strict'
 
   return {
     init: function () {
-    	IFS.core.sortingErrors.sortList();
+      IFS.core.sortingErrors.sortList()
     },
 
-	sortList: function () {
-      var ul = jQuery('.error-summary-list')[0]
-      if (!ul) {
+    sortList: function () {
+      var errorContainer = jQuery('.error-summary-list')[0]
+      if (!errorContainer) {
+        // if theres no error summary return
         return
       }
-      var collection = ul.getElementsByTagName('LI')
+      var errors = errorContainer.getElementsByTagName('LI')
 
-      var ids = []
-      // forEach error grab the field
-      for (var i = 0; i < collection.length; i++) {
-        var hash = IFS.core.sortingErrors.getHashFromElement(collection[i])
+      // for each error grab the field (the hash of the link)
+      var fieldIds = []
+      for (var i = 0; i < errors.length; i++) {
+        var hash = IFS.core.sortingErrors.getHashFromElement(errors[i])
         if (hash) {
-          ids.push(hash)
+          fieldIds.push('#' + window.CSS.escape(hash.substring(1)))
         }
       }
-      var elementsInOrder = jQuery(ids.join(','))
-      // select returns fields in order.
+      // use jquery search on all hash arguments to return elements in the order they appear on the page
+      var elementsInOrder = jQuery(fieldIds.join(','))
+
+      // convert the elements back to IDs of the pattern #fieldID
       var idsInOrder = []
       for (var j = 0; j < elementsInOrder.length; j++) {
         idsInOrder.push('#' + elementsInOrder[j].id)
       }
 
-      var listLi = jQuery('li', jQuery(ul))
-      listLi.sort(function (a, b) {
+      // use jQuery sort syntax to so sort the elements
+      var jQueryErrors = jQuery('li', jQuery(errorContainer))
+      jQueryErrors.sort(function (a, b) {
         return IFS.core.sortingErrors.compareHash(a, b, idsInOrder)
       })
-      jQuery.each(listLi, function (index, row) {
-        jQuery(ul).append(row)
+
+      // reattach sorted elements
+      jQuery.each(jQueryErrors, function (index, row) {
+        jQuery(errorContainer).append(row)
       })
     },
+    // compares to errors, if they have matching hash, returns alphabetical
     compareHash: function (a, b, idsInOrder) {
       var aIndex = IFS.core.sortingErrors.getSortOrder(IFS.core.sortingErrors.getHashFromElement(a), idsInOrder)
       var bIndex = IFS.core.sortingErrors.getSortOrder(IFS.core.sortingErrors.getHashFromElement(b), idsInOrder)
@@ -46,6 +57,7 @@ IFS.core.sortingErrors = (function () {
       return a.textContent.localeCompare(b.textContent)
     },
 
+    // returns hash of hyperlink of error or null if not found
     getHashFromElement: function (element) {
       if (jQuery(element).find('a').length) {
         return jQuery(element).find('a')[0].hash
@@ -53,6 +65,7 @@ IFS.core.sortingErrors = (function () {
       return null
     },
 
+    // returns order of hash on the page or -1 if not on page, or -2 if no hash provided.
     getSortOrder: function (hash, idsInOrder) {
       if (hash != null) {
         return idsInOrder.indexOf(hash)
