7cf464a55bc7fc05610a62c0f9fa59eca0bd2556
Bronnyl Dias <bdias@worth.systems>
2018-07-25 16:03:44 +0100

    IFS-3916 - Configurable Project Setup documents
    Change-Id: If9eff7911d22918085f740ca48cf9365bf1bc0bb

--- ifs-web-service/ifs-competition-mgt-service/src/main/java/org/innovateuk/ifs/competitionsetup/projectdocument/controller/CompetitionSetupProjectDocumentController.java
+++ ifs-web-service/ifs-competition-mgt-service/src/main/java/org/innovateuk/ifs/competitionsetup/projectdocument/controller/CompetitionSetupProjectDocumentController.java
@@ -126,4 +126,14 @@ public class CompetitionSetupProjectDocumentController {
         return new ProjectDocumentForm(resource.getId(), resource.getTitle(), resource.getGuidance(), resource.isEnabled(), resource.isPdf(), resource.isSpreadsheet());
 
     }
+
+    @DeleteMapping("/{projectDocumentId}")
+    public String deleteProjectDocument(@PathVariable(COMPETITION_ID_KEY) long competitionId,
+                                          @PathVariable("projectDocumentId") long projectDocumentId,
+                                          Model model) {
+
+        competitionSetupProjectDocumentRestService.delete(projectDocumentId);
+
+        return projectDocumentLandingPage(model, competitionId);
+    }
 }
