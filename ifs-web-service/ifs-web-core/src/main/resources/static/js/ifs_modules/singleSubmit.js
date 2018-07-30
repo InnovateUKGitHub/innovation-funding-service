377136ec00935ccc33b311f199665a7057b69635
Luke Harper <lharper@worth.systems>
2018-07-30 15:10:35 +0100

    IFS-4009 disallowing double click submit.

--- ifs-web-service/ifs-web-core/src/main/resources/static/js/ifs_modules/singleSubmit.js
+++ ifs-web-service/ifs-web-core/src/main/resources/static/js/ifs_modules/singleSubmit.js
@@ -2,15 +2,24 @@ IFS.core.singleSubmit = (function () {
   'use strict'
   return {
     init: function () {
-      jQuery(document).on('submit', '[data-single-submit]', function (e) {
+      jQuery(document).on('submit', 'form', function (e) {
         var form = jQuery(this)
         if (form.is('[data-submitting]')) {
           e.preventDefault()
         } else {
           form.attr('data-submitting', '')
-          var submitButton = form.find('button[data-content-on-submit]')
-          submitButton.attr('disabled', 'disabled')
-          submitButton.html(submitButton.attr('data-content-on-submit'))
+          setTimeout(function () {
+            IFS.core.singleSubmit.disableButton(form)
+          }, 0)
+        }
+      })
+    },
+    disableButton: function (form) {
+      var submitButton = form.find(':button')
+      submitButton.attr('disabled', 'disabled')
+      submitButton.each(function () {
+        if (jQuery(this).is('[data-content-on-submit]')) {
+          jQuery(this).html(jQuery(this).attr('data-content-on-submit'))
         }
       })
     }
