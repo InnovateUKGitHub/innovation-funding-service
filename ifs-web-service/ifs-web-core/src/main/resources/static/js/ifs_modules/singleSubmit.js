4624c730048f3761de0997e3524cbf361c8329d0
Luke Harper <lharper@worth.systems>
2018-07-31 11:07:13 +0100

    IFS-4009 handling other events stoping default.

--- ifs-web-service/ifs-web-core/src/main/resources/static/js/ifs_modules/singleSubmit.js
+++ ifs-web-service/ifs-web-core/src/main/resources/static/js/ifs_modules/singleSubmit.js
@@ -9,15 +9,20 @@ IFS.core.singleSubmit = (function () {
         } else {
           form.attr('data-submitting', '')
           setTimeout(function () {
-            IFS.core.singleSubmit.disableButton(form)
+            IFS.core.singleSubmit.disableButton(form, e)
           }, 0)
         }
       })
     },
-    disableButton: function (form) {
-      var submitButton = form.find(':button')
-      submitButton.attr('disabled', 'disabled')
-      submitButton.filter('.button:not(.button-secondary)').html('Please wait...')
+    disableButton: function (form, e) {
+      if (e.isDefaultPrevented()) {
+        // another event handler has stopped the submit event.
+        form.removeAttr('data-submitting')
+      } else {
+        var submitButton = form.find(':button')
+        submitButton.attr('disabled', 'disabled')
+        submitButton.filter('.button:not(.button-secondary)').html('Please wait...')
+      }
     }
   }
 })()
