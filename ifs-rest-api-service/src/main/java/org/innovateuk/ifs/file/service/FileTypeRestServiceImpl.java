4c08c583f68a868db7ef4087d0e2c1e687e90e2d
Bronnyl Dias <bdias@worth.systems>
2018-08-01 22:19:54 +0100

    IFS-3916 - Configurable Project Setup documents
    REST Layer Changes.
    
    Change-Id: I522c508dbbebb2b560e9a8e65f8e1e098dfab2e3

---
+++ ifs-rest-api-service/src/main/java/org/innovateuk/ifs/file/service/FileTypeRestServiceImpl.java
@@ -0,0 +1,27 @@
+package org.innovateuk.ifs.file.service;
+
+import org.innovateuk.ifs.commons.rest.RestResult;
+import org.innovateuk.ifs.commons.service.BaseRestService;
+import org.innovateuk.ifs.file.resource.FileTypeResource;
+import org.springframework.stereotype.Service;
+
+/**
+ * Implements {@link FileTypeRestService}
+ */
+@Service
+public class FileTypeRestServiceImpl extends BaseRestService implements FileTypeRestService {
+
+    private String fileTypeRestURL = "/file/file-type";
+
+    @Override
+    public RestResult<FileTypeResource> findOne(long id) {
+        return getWithRestResult(fileTypeRestURL + "/" + id, FileTypeResource.class);
+    }
+
+    @Override
+    public RestResult<FileTypeResource> findByName(String name) {
+        return getWithRestResult(fileTypeRestURL + "/findByName/" + name, FileTypeResource.class);
+    }
+}
+
+
