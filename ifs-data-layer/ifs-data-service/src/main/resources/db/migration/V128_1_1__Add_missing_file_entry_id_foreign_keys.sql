c2bec53b5245ba8c5baf2b658f094f7bc0e5e2d4
Duncan Watson <duncan@hiveit.co.uk>
2018-08-02 15:54:13 +0100

    IFS-3948 - added missing foreign keys

---
+++ ifs-data-layer/ifs-data-service/src/main/resources/db/migration/V128_1_1__Add_missing_file_entry_id_foreign_keys.sql
@@ -0,0 +1,4 @@
+ALTER TABLE project ADD CONSTRAINT `grant_offer_letter_file_entry_id_fk` FOREIGN KEY (`grant_offer_letter_file_entry_id`) REFERENCES `file_entry` (`id`);
+ALTER TABLE project ADD CONSTRAINT `additional_contract_file_entry_id_fk` FOREIGN KEY (`additional_contract_file_entry_id`) REFERENCES `file_entry` (`id`);
+ALTER TABLE project ADD CONSTRAINT `signed_grant_offer_file_entry_id_fk` FOREIGN KEY (`signed_grant_offer_file_entry_id`) REFERENCES `file_entry` (`id`);
+ALTER TABLE application_finance ADD CONSTRAINT `finance_file_entry_id_fk` FOREIGN KEY (`finance_file_entry_id`) REFERENCES `file_entry` (`id`);
