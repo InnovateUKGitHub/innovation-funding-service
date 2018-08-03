40fa3ee0d0ca64b389f8f2ddcf49fb3ea3bcb341
Jon Lee <jlee@worth.systems>
2018-08-03 16:07:39 +0100

    using silent mockito junit runner

--- ifs-web-service/ifs-competition-mgt-service/src/test/java/org/innovateuk/ifs/competitionsetup/eligibility/populator/EligibilityFormPopulatorTest.java
+++ ifs-web-service/ifs-competition-mgt-service/src/test/java/org/innovateuk/ifs/competitionsetup/eligibility/populator/EligibilityFormPopulatorTest.java
@@ -14,7 +14,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import java.util.List;
 
@@ -27,7 +27,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.when;
 
-@RunWith(MockitoJUnitRunner.class)
+@RunWith(MockitoJUnitRunner.Silent.class)
 public class EligibilityFormPopulatorTest {
 
     @Mock
