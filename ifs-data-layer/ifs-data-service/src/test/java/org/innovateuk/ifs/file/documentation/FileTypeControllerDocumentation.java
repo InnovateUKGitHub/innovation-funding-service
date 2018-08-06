37a139aca8183190174a549b51381a464487043e
Bronnyl Dias <bdias@worth.systems>
2018-08-06 16:21:55 +0100

    IFS-3916 - Configurable Project Setup documents
    File Type ASCII Docs.
    
    Change-Id: Ice7b9fce07e255d931d93f0fa95c86ab9e958ee4

---
+++ ifs-data-layer/ifs-data-service/src/test/java/org/innovateuk/ifs/file/documentation/FileTypeControllerDocumentation.java
@@ -0,0 +1,81 @@
+package org.innovateuk.ifs.file.documentation;
+
+import org.innovateuk.ifs.BaseControllerMockMVCTest;
+import org.innovateuk.ifs.file.controller.FileTypeController;
+import org.innovateuk.ifs.file.resource.FileTypeResource;
+import org.innovateuk.ifs.file.service.FileTypeService;
+import org.junit.Test;
+import org.mockito.Mock;
+
+import static org.innovateuk.ifs.commons.service.ServiceResult.serviceSuccess;
+import static org.innovateuk.ifs.documentation.FileTypeResourceDocs.fileTypeResourceFields;
+import static org.innovateuk.ifs.util.JsonMappingUtil.toJson;
+import static org.mockito.Mockito.only;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+public class FileTypeControllerDocumentation extends BaseControllerMockMVCTest<FileTypeController> {
+
+    @Mock
+    private FileTypeService fileTypeServiceMock;
+
+    @Override
+    protected FileTypeController supplyControllerUnderTest() {
+        return new FileTypeController();
+    }
+
+    @Test
+    public void findOne() throws Exception {
+
+        long fileTypeId = 1L;
+
+        FileTypeResource fileTypeResource = new FileTypeResource();
+
+        when(fileTypeServiceMock.findOne(fileTypeId)).thenReturn(serviceSuccess(fileTypeResource));
+
+        mockMvc.perform(get("/file/file-type/{fileTypeId}", fileTypeId)
+        )
+                .andExpect(status().isOk())
+                .andExpect(content().json(toJson(fileTypeResource)))
+                .andDo(document("file/file-type/{method-name}",
+                        pathParameters(
+                                parameterWithName("fileTypeId").description("Id of the file type to be retrieved")
+                        ),
+                        responseFields(fileTypeResourceFields)
+                ));
+
+        verify(fileTypeServiceMock, only()).findOne(fileTypeId);
+    }
+
+    @Test
+    public void findByName() throws Exception {
+
+        String name = "name";
+
+        FileTypeResource fileTypeResource = new FileTypeResource();
+
+        when(fileTypeServiceMock.findByName(name)).thenReturn(serviceSuccess(fileTypeResource));
+
+        mockMvc.perform(get("/file/file-type/findByName/{name}", name)
+        )
+                .andExpect(status().isOk())
+                .andExpect(content().json(toJson(fileTypeResource)))
+                .andDo(document("file/file-type/{method-name}",
+                        pathParameters(
+                                parameterWithName("name").description("The name by which the file type should be retrieved")
+                        ),
+                        responseFields(fileTypeResourceFields)
+                ));
+
+        verify(fileTypeServiceMock, only()).findByName(name);
+    }
+}
+
+
