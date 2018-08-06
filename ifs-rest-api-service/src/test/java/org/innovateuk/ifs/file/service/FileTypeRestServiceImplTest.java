ea2f19ac1b5c5c20c122f97d21570291c14a792d
Bronnyl Dias <bdias@worth.systems>
2018-08-06 16:22:19 +0100

    IFS-3916 - Configurable Project Setup documents
    REST Layer File Type Junits.
    
    Change-Id: I88c0d5cd7141c6493c5dfdff095137c768cc5815

---
+++ ifs-rest-api-service/src/test/java/org/innovateuk/ifs/file/service/FileTypeRestServiceImplTest.java
@@ -0,0 +1,50 @@
+package org.innovateuk.ifs.file.service;
+
+import org.innovateuk.ifs.BaseRestServiceUnitTest;
+import org.innovateuk.ifs.file.resource.FileTypeResource;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+public class FileTypeRestServiceImplTest extends BaseRestServiceUnitTest<FileTypeRestServiceImpl> {
+
+    private static final String fileTypeRestURL = "/file/file-type";
+
+    @Override
+    protected FileTypeRestServiceImpl registerRestServiceUnderTest() {
+        return new FileTypeRestServiceImpl();
+    }
+
+    @Test
+    public void findOne() {
+
+        long fileTypeId = 1L;
+
+        FileTypeResource responseBody = new FileTypeResource();
+
+        setupGetWithRestResultExpectations(String.format("%s/%s", fileTypeRestURL, fileTypeId), FileTypeResource.class, responseBody);
+
+        FileTypeResource response = service.findOne(fileTypeId).getSuccess();
+        assertNotNull(response);
+        assertEquals(responseBody, response);
+
+        setupGetWithRestResultVerifications(String.format("%s/%s", fileTypeRestURL, fileTypeId), null, FileTypeResource.class);
+    }
+
+    @Test
+    public void findByName() {
+
+        String name = "name";
+
+        FileTypeResource responseBody = new FileTypeResource();
+
+        setupGetWithRestResultExpectations(String.format("%s/findByName/%s", fileTypeRestURL, name), FileTypeResource.class, responseBody);
+
+        FileTypeResource response = service.findByName(name).getSuccess();
+        assertNotNull(response);
+        assertEquals(responseBody, response);
+
+        setupGetWithRestResultVerifications(String.format("%s/findByName/%s", fileTypeRestURL, name), null, FileTypeResource.class);
+    }
+}
+
