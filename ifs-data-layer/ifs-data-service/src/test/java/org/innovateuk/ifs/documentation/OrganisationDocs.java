fa74bbc8c5b3d88bfef804775cb3ece254499e62
Tom Baldwin <tom@polymorphism.co.uk>
2018-08-07 17:33:30 +0100

    IFS-3195 - Fixes OrganisationDocs

--- ifs-data-layer/ifs-data-service/src/test/java/org/innovateuk/ifs/documentation/OrganisationDocs.java
+++ ifs-data-layer/ifs-data-service/src/test/java/org/innovateuk/ifs/documentation/OrganisationDocs.java
@@ -1,5 +1,6 @@
 package org.innovateuk.ifs.documentation;
 
+import org.innovateuk.ifs.commons.ZeroDowntime;
 import org.innovateuk.ifs.organisation.builder.OrganisationResourceBuilder;
 import org.springframework.restdocs.payload.FieldDescriptor;
 
@@ -12,9 +13,11 @@ import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWit
 
 public class OrganisationDocs {
 
+    @ZeroDowntime(description = "remove companyHouseNumber", reference = "IFS-3195")
     public static final FieldDescriptor[] organisationResourceFields = {
             fieldWithPath("id").description("Id of the organisation"),
             fieldWithPath("name").description("Name of the organisation"),
+            fieldWithPath("companyHouseNumber").description("The companies house number"),
             fieldWithPath("companiesHouseNumber").description("The companies house number"),
             fieldWithPath("processRoles").description("Ids of the process roles"),
             fieldWithPath("applicationFinances").description("Ids of the applications finances"),
