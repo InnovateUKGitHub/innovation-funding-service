013cf94d8b8714c3720fcfe0e5f725540361de96
Ann Stainforth <ann.stainforth.ipl@gmail.com>
2018-08-08 09:20:18 +0100

    IFS-4110: Temporary tweak to webtest data. 'Make photonics for public' an example of a competition that had application team question but not research category question.

--- ifs-data-layer/ifs-data-service/src/main/resources/db/webtest/V126_2_21__Remove_research_category_question_from_photonics_for_all_competition.sql
+++ ifs-data-layer/ifs-data-service/src/main/resources/db/webtest/V126_2_21__Remove_research_category_question_from_photonics_for_all_competition.sql
@@ -1,7 +1,16 @@
 -- IFS-2123 - Temporarily modify test data to allow testing for backwards compatability for this feature
--- Remove the new research category question from one example competition in the web test data
+-- Remove the new research category question from one example competition in the web test data which _doesn't_ have the
+-- new application team view either
 
 SET @competition_id = (SELECT id FROM competition WHERE name="Photonics for All");
+SET @question_id = (SELECT id FROM question WHERE competition_id=@competition_id AND question_setup_type="RESEARCH_CATEGORY");
+
+DELETE from question_status WHERE question_id = @question_id;
+DELETE from question WHERE id = @question_id;
+
+-- Remove the new research category question from one example competition in the web test data
+-- For a competition which _does_ have the new application team view deployed.
+SET @competition_id = (SELECT id FROM competition WHERE name="Photonics for Public");
 SET @question_id = (SELECT id FROM question WHERE competition_id=@competition_id AND question_setup_type="RESEARCH_CATEGORY");
 
 DELETE from question_status WHERE question_id = @question_id;
