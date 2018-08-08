6f37fa793d861735c71da6bd17bcfb71cc29d67d
Ann Stainforth <ann.stainforth.ipl@gmail.com>
2018-08-08 11:42:30 +0100

    IFS-4110: Apply script to Photonics for RTO and Business. Just to help Val with manual testing.

--- ifs-data-layer/ifs-data-service/src/main/resources/db/webtest/V126_2_21__Remove_research_category_question_from_photonics_for_all_competition.sql
+++ ifs-data-layer/ifs-data-service/src/main/resources/db/webtest/V126_2_21__Remove_research_category_question_from_photonics_for_all_competition.sql
@@ -15,3 +15,11 @@ SET @question_id = (SELECT id FROM question WHERE competition_id=@competition_id
 
 DELETE from question_status WHERE question_id = @question_id;
 DELETE from question WHERE id = @question_id;
+
+-- Remove the new research category question from one example competition in the web test data
+-- For a competition which _does_ have the new application team view deployed.
+SET @competition_id = (SELECT id FROM competition WHERE name="Photonics for RTO and Business");
+SET @question_id = (SELECT id FROM question WHERE competition_id=@competition_id AND question_setup_type="RESEARCH_CATEGORY");
+
+DELETE from question_status WHERE question_id = @question_id;
+DELETE from question WHERE id = @question_id;
