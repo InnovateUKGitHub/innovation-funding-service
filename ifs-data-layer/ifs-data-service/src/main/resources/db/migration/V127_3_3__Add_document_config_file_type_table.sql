dd0a82f94812e44a87d63efea9d1b06591195624
Bronnyl Dias <bdias@worth.systems>
2018-08-09 13:15:06 +0100

    IFS-3916 - Configurable Project Setup documents
    Implemented minor comment from Rogier to get rid of the id from the join
    table.
    
    Change-Id: Ie9d9fdbfeb5ded586c2da03303febe02416c633c

--- ifs-data-layer/ifs-data-service/src/main/resources/db/migration/V127_3_3__Add_document_config_file_type_table.sql
+++ ifs-data-layer/ifs-data-service/src/main/resources/db/migration/V127_3_3__Add_document_config_file_type_table.sql
@@ -2,10 +2,9 @@
 -- Add a new table document_config_file_type
 
 CREATE TABLE `document_config_file_type` (
-	`id` bigint(20) NOT NULL AUTO_INCREMENT,
 	`document_config_id` bigint(20) NOT NULL,
 	`file_type_id` bigint(20) NOT NULL,
-	PRIMARY KEY (`id`),
+	PRIMARY KEY (`document_config_id`,`file_type_id`),
 	CONSTRAINT `document_config_fk` FOREIGN KEY (`document_config_id`) REFERENCES `document_config` (`id`),
 	CONSTRAINT `file_type_fk` FOREIGN KEY (`file_type_id`) REFERENCES `file_type` (`id`)
 );
