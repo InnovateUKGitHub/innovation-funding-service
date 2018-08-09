84748bb2d05ebc9279fd88f12c78e0eb2d4b8fb1
Bronnyl Dias <bdias@worth.systems>
2018-08-09 16:16:53 +0100

    IFS-3965-IFS-3977-MO-Creation
    1) Created MO Role
    2) Add new MOs
    3) Code changes to allow access to dashboard
    
    Change-Id: I262f51be85f73a5b96ecef00ea069bc540f147db

--- ifs-data-layer/ifs-data-service/src/main/resources/db/reference/V127_4_1__Add_new_role_monitoring_officer.sql
+++ ifs-data-layer/ifs-data-service/src/main/resources/db/reference/V127_4_1__Add_new_role_monitoring_officer.sql
@@ -1,2 +1,7 @@
 -- IFS-3965: Create a new role for Monitoring Officer
 
+INSERT INTO role
+(name, url)
+VALUES
+('monitoring_officer', 'applicant/dashboard');
+
---
+++ ifs-data-layer/ifs-data-service/src/main/resources/db/webtest/V127_4_2__Add_new_monitoring_officers.sql
@@ -0,0 +1,36 @@
+-- IFS-3977: Add new monitoring offiers.
+
+SET @ifs_web_system_user_id =
+(SELECT id FROM user WHERE email = 'ifs_web_user@innovateuk.org');
+
+SET @monitoring_officer_role_id =
+(SELECT id FROM role WHERE name = 'monitoring_officer');
+
+INSERT INTO `user`
+(email, first_name, last_name, status, uid, system_user, allow_marketing_emails, created_by, created_on, modified_by, modified_on)
+VALUES ('Orville.Gibbs@gmail.com', 'Orville', 'Gibbs', 'ACTIVE', '623d49e5-816e-4acf-b95a-5d712562e037', 0, 0, @ifs_web_system_user_id, NOW(), @ifs_web_system_user_id, NOW());
+
+INSERT INTO `user`
+(email, first_name, last_name, status, uid, system_user, allow_marketing_emails, created_by, created_on, modified_by, modified_on)
+VALUES ('Nilesh.Patti@gmail.com', 'Nilesh', 'Patti', 'ACTIVE', 'ea4bc32f-4149-444b-a781-02c6b08ae0d1', 0, 0, @ifs_web_system_user_id, NOW(), @ifs_web_system_user_id, NOW());
+
+INSERT INTO `user`
+(email, first_name, last_name, status, uid, system_user, allow_marketing_emails, created_by, created_on, modified_by, modified_on)
+VALUES ('Rupesh.Pereira@gmail.com', 'Rupesh', 'Pereira', 'ACTIVE', '4486a84a-fbef-419a-9cd0-032170031c97', 0, 0, @ifs_web_system_user_id, NOW(), @ifs_web_system_user_id, NOW());
+
+INSERT INTO user_role
+(user_id, role_id)
+VALUES
+((select id from user where email = 'Orville.Gibbs@gmail.com'), @monitoring_officer_role_id);
+
+INSERT INTO user_role
+(user_id, role_id)
+VALUES
+((select id from user where email = 'Nilesh.Patti@gmail.com'), @monitoring_officer_role_id);
+
+INSERT INTO user_role
+(user_id, role_id)
+VALUES
+((select id from user where email = 'Rupesh.Pereira@gmail.com'), @monitoring_officer_role_id);
+
+
