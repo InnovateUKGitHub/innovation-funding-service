21c74e6ff8a02a8d08024526001306adc634b004
Bronnyl Dias <bdias@worth.systems>
2018-08-10 17:42:03 +0100

    IFS-3965-IFS-3977-MO-Creation
    Added Phone numbers to the MO. Duh!
    
    Change-Id: I07621a374d85f1ad18b3d5568ef1e91f36379cc6

--- ifs-data-layer/ifs-data-service/src/main/resources/db/webtest/V127_4_2__Add_new_monitoring_officers.sql
+++ ifs-data-layer/ifs-data-service/src/main/resources/db/webtest/V127_4_2__Add_new_monitoring_officers.sql
@@ -7,16 +7,16 @@ SET @monitoring_officer_role_id =
 (SELECT id FROM role WHERE name = 'monitoring_officer');
 
 INSERT INTO `user`
-(email, first_name, last_name, status, uid, system_user, allow_marketing_emails, created_by, created_on, modified_by, modified_on)
-VALUES ('Orville.Gibbs@gmail.com', 'Orville', 'Gibbs', 'ACTIVE', '623d49e5-816e-4acf-b95a-5d712562e037', 0, 0, @ifs_web_system_user_id, NOW(), @ifs_web_system_user_id, NOW());
+(email, first_name, last_name, phone_number, status, uid, system_user, allow_marketing_emails, created_by, created_on, modified_by, modified_on)
+VALUES ('Orville.Gibbs@gmail.com', 'Orville', 'Gibbs', '449823090017', 'ACTIVE', '623d49e5-816e-4acf-b95a-5d712562e037', 0, 0, @ifs_web_system_user_id, NOW(), @ifs_web_system_user_id, NOW());
 
 INSERT INTO `user`
-(email, first_name, last_name, status, uid, system_user, allow_marketing_emails, created_by, created_on, modified_by, modified_on)
-VALUES ('Nilesh.Patti@gmail.com', 'Nilesh', 'Patti', 'ACTIVE', 'ea4bc32f-4149-444b-a781-02c6b08ae0d1', 0, 0, @ifs_web_system_user_id, NOW(), @ifs_web_system_user_id, NOW());
+(email, first_name, last_name, phone_number, status, uid, system_user, allow_marketing_emails, created_by, created_on, modified_by, modified_on)
+VALUES ('Nilesh.Patti@gmail.com', 'Nilesh', 'Patti', '449890325459', 'ACTIVE', 'ea4bc32f-4149-444b-a781-02c6b08ae0d1', 0, 0, @ifs_web_system_user_id, NOW(), @ifs_web_system_user_id, NOW());
 
 INSERT INTO `user`
-(email, first_name, last_name, status, uid, system_user, allow_marketing_emails, created_by, created_on, modified_by, modified_on)
-VALUES ('Rupesh.Pereira@gmail.com', 'Rupesh', 'Pereira', 'ACTIVE', '4486a84a-fbef-419a-9cd0-032170031c97', 0, 0, @ifs_web_system_user_id, NOW(), @ifs_web_system_user_id, NOW());
+(email, first_name, last_name, phone_number, status, uid, system_user, allow_marketing_emails, created_by, created_on, modified_by, modified_on)
+VALUES ('Rupesh.Pereira@gmail.com', 'Rupesh', 'Pereira', '448554937227', 'ACTIVE', '4486a84a-fbef-419a-9cd0-032170031c97', 0, 0, @ifs_web_system_user_id, NOW(), @ifs_web_system_user_id, NOW());
 
 INSERT INTO user_role
 (user_id, role_id)
