eb8cb2f0517948f2a41339e2a6ed4a63cb8e4836
Ben Outram <ben@hiveit.co.uk>
2018-08-13 11:50:33 +0100

    IFS-3954 ZDD Cleanup for IFS-3818

--- ifs-data-layer/ifs-data-service/src/main/resources/db/migration/V127_3_1__Prepare_to_remove_competition_type_from_gcm.sql
+++ ifs-data-layer/ifs-data-service/src/main/resources/db/migration/V127_3_1__Prepare_to_remove_competition_type_from_gcm.sql
@@ -3,4 +3,4 @@
 -- We will remove the reference and stop the column from being used so it can be removed by a later ZDD task.
 -- To allow new rows to be inserted the column will be made nullable.
 ALTER TABLE grant_claim_maximum DROP FOREIGN KEY grant_claim_maximum_competition_type_fk;
-ALTER TABLE grant_claim_maximum MODIFY grant_claim_maximum BIGINT(20) NULL;
+ALTER TABLE grant_claim_maximum MODIFY competition_type_id BIGINT(20) NULL;
