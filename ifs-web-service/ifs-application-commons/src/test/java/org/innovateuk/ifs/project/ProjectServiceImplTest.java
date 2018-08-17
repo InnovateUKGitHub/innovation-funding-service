24cb51868c7b2c29d3729f74cdc916f40775e0e8
Bronnyl Dias <bdias@worth.systems>
2018-08-17 10:20:20 +0100

    IFS-3978-MO-Dashboard-Assigned-Projects
    Junits for the above functionality
    
    Change-Id: Iecb246892dc06257f8d5d48d1a04698399ef11e8

--- ifs-web-service/ifs-application-commons/src/test/java/org/innovateuk/ifs/project/ProjectServiceImplTest.java
+++ ifs-web-service/ifs-application-commons/src/test/java/org/innovateuk/ifs/project/ProjectServiceImplTest.java
@@ -6,10 +6,13 @@ import org.innovateuk.ifs.application.resource.ApplicationResource;
 import org.innovateuk.ifs.commons.exception.ForbiddenActionException;
 import org.innovateuk.ifs.commons.exception.ObjectNotFoundException;
 import org.innovateuk.ifs.commons.service.ServiceResult;
+import org.innovateuk.ifs.organisation.builder.OrganisationResourceBuilder;
 import org.innovateuk.ifs.organisation.resource.OrganisationResource;
+import org.innovateuk.ifs.project.builder.PartnerOrganisationResourceBuilder;
 import org.innovateuk.ifs.project.resource.PartnerOrganisationResource;
 import org.innovateuk.ifs.project.resource.ProjectResource;
 import org.innovateuk.ifs.project.resource.ProjectUserResource;
+import org.innovateuk.ifs.project.service.PartnerOrganisationRestService;
 import org.innovateuk.ifs.project.service.ProjectRestService;
 import org.innovateuk.ifs.user.resource.ProcessRoleResource;
 import org.innovateuk.ifs.user.resource.Role;
@@ -21,6 +24,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 
 import java.util.Collections;
@@ -32,6 +36,7 @@ import static org.innovateuk.ifs.application.builder.ApplicationResourceBuilder.
 import static org.innovateuk.ifs.commons.BaseIntegrationTest.setLoggedInUser;
 import static org.innovateuk.ifs.commons.rest.RestResult.restSuccess;
 import static org.innovateuk.ifs.organisation.builder.OrganisationResourceBuilder.newOrganisationResource;
+import static org.innovateuk.ifs.project.builder.PartnerOrganisationResourceBuilder.*;
 import static org.innovateuk.ifs.project.builder.ProjectResourceBuilder.newProjectResource;
 import static org.innovateuk.ifs.project.builder.ProjectUserResourceBuilder.newProjectUserResource;
 import static org.innovateuk.ifs.user.builder.ProcessRoleResourceBuilder.newProcessRoleResource;
@@ -46,6 +51,9 @@ public class ProjectServiceImplTest extends BaseServiceUnitTest<ProjectService> 
 
     @Mock
     private ProjectRestService projectRestService;
+
+    @Mock
+    private PartnerOrganisationRestService partnerOrganisationRestService;
 
     @Mock
     private UserService userService;
@@ -115,6 +123,32 @@ public class ProjectServiceImplTest extends BaseServiceUnitTest<ProjectService> 
         assertEquals(result.getSuccess(), projects);
 
         verify(projectRestService).findByUserId(1L);
+    }
+
+    @Test
+    public void getPartnerOrganisationsForProject() {
+
+        Long projectId = 1L;
+        Long organisationId1 = 12L;
+        Long organisationId2 = 14L;
+
+        List<PartnerOrganisationResource> partnerOrganisationResources = newPartnerOrganisationResource()
+                .withOrganisation(organisationId1, organisationId2)
+                .build(2);
+
+        List<OrganisationResource> organisationResources = newOrganisationResource()
+                .withId(organisationId1, organisationId2)
+                .build(2);
+
+        when(partnerOrganisationRestService.getProjectPartnerOrganisations(projectId)).thenReturn(restSuccess(partnerOrganisationResources));
+        when(organisationRestService.getOrganisationById(organisationId1)).thenReturn(restSuccess(organisationResources.get(0)));
+        when(organisationRestService.getOrganisationById(organisationId2)).thenReturn(restSuccess(organisationResources.get(1)));
+
+        List<OrganisationResource> result = service.getPartnerOrganisationsForProject(projectId);
+
+        assertTrue(result.size() == 2);
+        assertEquals(organisationResources.get(0), result.get(0));
+        assertEquals(organisationResources.get(1), result.get(1));
     }
 
     @Test
