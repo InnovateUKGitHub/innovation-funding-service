9e6a55ea9a4d5754130aedc56112439dca0fb1df
Aimar Aareleid <aareleid@worth.systems>
2018-08-17 16:13:26 +0200

    test file copying

--- setup-files/scripts/openshift/os-test-remote.sh
+++ setup-files/scripts/openshift/os-test-remote.sh
@@ -17,13 +17,16 @@ echo "Deploying tests to the current oc project ($PROJECT)"
 
 function tailorToAppInstance() {
     rm -rf $(getBuildLocation)
+    echo "Starting to copy os-files to ${getBuildLocation}" 
     cp -r os-files $(getBuildLocation)
     sed -i.bak "s#innovateuk/#${INTERNAL_REGISTRY}/${PROJECT}/#g" $(getBuildLocation)/robot-tests/*.yml
     sed -i.bak "s/<<SHIB-ADDRESS>>/$PROJECT.$ROUTE_DOMAIN/g" $(getBuildLocation)/robot-tests/*.yml
 
     cp -r robot-tests robot-tests-tmp
+    echo "PING!!!!"
     sed -i.bak "s/<<SHIB-ADDRESS>>/$PROJECT.$ROUTE_DOMAIN/g" robot-tests-tmp/openshift/*.sh
     sed -i.bak "s/<<SHIB-ADDRESS>>/$PROJECT.$ROUTE_DOMAIN/g" robot-tests-tmp/os_run_tests.sh
+    echo "PONG!!!!"
     sed -i.bak "s#\[\"./os_run_tests.sh\", \"-q\"\]#[\"./os_run_tests.sh\", \"-q\", \"$ROBOT_COMMAND\"]#g" robot-tests-tmp/Dockerfile
 
 }
