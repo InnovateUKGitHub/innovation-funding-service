28cf88de89c99e6c359a132e089005a9ad58010c
Aimar Aareleid <aareleid@worth.systems>
2018-08-20 13:47:42 +0200

    fix

--- setup-files/scripts/openshift/os-test-remote.sh
+++ setup-files/scripts/openshift/os-test-remote.sh
@@ -21,7 +21,7 @@ function tailorToAppInstance() {
     mkdir -p $(getBuildLocation)
     cp -r os-files/* $(getBuildLocation)
     echo "running ls"
-    ls -la $(getBuildLocation)/os-files/robot-tests/
+    ls -la $(getBuildLocation)/robot-tests/
     echo "running ls end"
     sed -i.bak "s#innovateuk/#${INTERNAL_REGISTRY}/${PROJECT}/#g" $(getBuildLocation)/robot-tests/*.yml
     sed -i.bak "s/<<SHIB-ADDRESS>>/$PROJECT.$ROUTE_DOMAIN/g" $(getBuildLocation)/robot-tests/*.yml
