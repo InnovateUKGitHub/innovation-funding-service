574062eb647600af49df9d41e2e41e3100f3298b
Aimar Aareleid <aareleid@worth.systems>
2018-08-21 09:58:51 +0200

    fix

--- setup-files/scripts/openshift/os-test-remote.sh
+++ setup-files/scripts/openshift/os-test-remote.sh
@@ -20,9 +20,7 @@ function tailorToAppInstance() {
     echo "Starting to copy os-files to $(getBuildLocation)"
     mkdir -p $(getBuildLocation)
     cp -r os-files/* $(getBuildLocation)
-    echo "running ls"
     ls -la $(getBuildLocation)/robot-tests/
-    echo "running ls end"
     sed -i.bak "s#innovateuk/#${INTERNAL_REGISTRY}/${PROJECT}/#g" $(getBuildLocation)/robot-tests/*.yml
     sed -i.bak "s/<<SHIB-ADDRESS>>/$PROJECT.$ROUTE_DOMAIN/g" $(getBuildLocation)/robot-tests/*.yml
 
--- setup-files/scripts/openshift/os-test-results.sh
+++ setup-files/scripts/openshift/os-test-results.sh
@@ -7,7 +7,8 @@ PROJECT=$(oc project -q)
 echo "Getting test results from the current oc project (${PROJECT})"
 
 function getResults() {
-    mkdir robot-tests/test-results-${PROJECT}
+    rm -rf robot-tests/test-results-${PROJECT}
+    mkdir -p robot-tests/test-results-${PROJECT}
     oc rsync $(oc get pods | grep robot-framework | awk '{ print $1 }'):/robot-tests/target/ robot-tests/test-results-${PROJECT}
 }
 
