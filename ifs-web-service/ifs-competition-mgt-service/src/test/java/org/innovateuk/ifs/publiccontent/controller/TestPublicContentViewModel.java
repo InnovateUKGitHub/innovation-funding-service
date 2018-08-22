c6f20dfe19c2136f8db3aa209974c8bed6abc603
Ben Outram <ben@hiveit.co.uk>
2018-08-22 16:48:18 +0100

    IFS-1626 Adding class comments to explain what these are.

--- ifs-web-service/ifs-competition-mgt-service/src/test/java/org/innovateuk/ifs/publiccontent/controller/TestPublicContentViewModel.java
+++ ifs-web-service/ifs-competition-mgt-service/src/test/java/org/innovateuk/ifs/publiccontent/controller/TestPublicContentViewModel.java
@@ -3,7 +3,7 @@ package org.innovateuk.ifs.publiccontent.controller;
 import org.innovateuk.ifs.publiccontent.viewmodel.AbstractPublicContentViewModel;
 
 /**
- * Subclass of {@link AbstractPublicContentViewModel} used as type parameters when testing subclasses of
+ * Subclass of {@link AbstractPublicContentViewModel} used as a type parameter when testing subclasses of
  * {@link AbstractPublicContentSectionController} in {@link AbstractContentGroupControllerTest} and
  * {@link AbstractPublicContentSectionControllerTest}.
  */
