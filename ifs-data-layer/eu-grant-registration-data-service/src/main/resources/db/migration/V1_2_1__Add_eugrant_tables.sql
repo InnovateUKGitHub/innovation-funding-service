fe109d081f5967e94e87de4c0dbb840d6e538389
Tom Baldwin <tom@polymorphism.co.uk>
2018-08-23 17:48:21 +0100

    IFS-4171 -- Adds foreign key constraints to eu grant tables.
    - Adds eu_funding.eu_action_type_id column
    - Additional not null constraints

--- ifs-data-layer/eu-grant-registration-data-service/src/main/resources/db/migration/V1_2_1__Add_eugrant_tables.sql
+++ ifs-data-layer/eu-grant-registration-data-service/src/main/resources/db/migration/V1_2_1__Add_eugrant_tables.sql
@@ -10,16 +10,14 @@ CREATE TABLE eu_address (
   county VARCHAR(255)
 );
 
-CREATE TABLE eu_action_type (
-  id BIGINT(20) PRIMARY KEY,
-  name VARCHAR(255) NOT NULL UNIQUE,
-  priority INT(11) NOT NULL UNIQUE
-);
 CREATE TABLE eu_organisation (
   id BIGINT(20) PRIMARY KEY AUTO_INCREMENT,
   name VARCHAR(255) NOT NULL,
   company_house_number VARCHAR(255),
-  organisation_type ENUM('BUSINESS', 'RESEARCH', 'RTO', 'PUBLIC_SECTOR_OR_CHARITY') NOT NULL
+  organisation_type ENUM('BUSINESS', 'RESEARCH', 'RTO', 'PUBLIC_SECTOR_OR_CHARITY') NOT NULL,
+  eu_address_id BIGINT(20) NOT NULL,
+
+  CONSTRAINT fk_eu_address_id FOREIGN KEY (eu_address_id) REFERENCES eu_address(id)
 );
 
 CREATE TABLE eu_contact (
@@ -29,27 +27,42 @@ CREATE TABLE eu_contact (
   telephone VARCHAR(255)
 );
 
+CREATE TABLE eu_action_type (
+  id BIGINT(20) PRIMARY KEY,
+  name VARCHAR(255) NOT NULL UNIQUE,
+  priority INT(11) NOT NULL UNIQUE
+);
+
 CREATE TABLE eu_funding (
   id BIGINT(20) PRIMARY KEY AUTO_INCREMENT,
 
-  call_name VARCHAR(255),
-  call_id VARCHAR(255),
-  topic_id VARCHAR(255),
-  project_name VARCHAR(255),
+  eu_action_type_id BIGINT(20) NOT NULL,
+  call_name VARCHAR(255) NOT NULL,
+  call_id VARCHAR(255) NOT NULL,
+  topic_id VARCHAR(255) NOT NULL,
+  project_name VARCHAR(255) NOT NULL,
 
-  project_start_date DATE,
-  project_end_date DATE,
+  project_start_date DATE NOT NULL,
+  project_end_date DATE NOT NULL,
 
-  grant_agreement_number VARCHAR(255),
-  fundingContribution BIGINT(20),
+  grant_agreement_number VARCHAR(255) NOT NULL,
+  fundingContribution BIGINT(20) NOT NULL,
 
-  project_coordinator BOOLEAN DEFAULT FALSE
+  project_coordinator BOOLEAN DEFAULT FALSE NOT NULL,
+
+  CONSTRAINT fk_eu_action_type_id FOREIGN KEY (eu_action_type_id) REFERENCES eu_action_type(id)
 );
 
 ALTER TABLE eu_grant ADD COLUMN eu_organisation_id BIGINT(20);
-ALTER TABLE eu_grant ADD COLUMN eu_contact_id BIGINT(20);
-ALTER TABLE eu_grant ADD COLUMN eu_funding_id BIGINT(20);
-ALTER TABLE eu_grant ADD COLUMN submitted BOOLEAN NOT NULL DEFAULT FALSE;
-ALTER TABLE eu_grant ADD COLUMN short_code VARCHAR(12); -- UNIQUE KEY;
+ALTER TABLE eu_grant ADD CONSTRAINT fk_eu_organisation_id FOREIGN KEY (eu_organisation_id) REFERENCES eu_organisation(id);
 
--- TODO foreign keys
+ALTER TABLE eu_grant ADD COLUMN eu_contact_id BIGINT(20);
+ALTER TABLE eu_grant ADD CONSTRAINT fk_eu_contact_id FOREIGN KEY (eu_contact_id) REFERENCES eu_contact(id);
+
+ALTER TABLE eu_grant ADD COLUMN eu_funding_id BIGINT(20);
+ALTER TABLE eu_grant ADD CONSTRAINT fk_eu_funding_id FOREIGN KEY (eu_funding_id) REFERENCES eu_funding(id);
+
+ALTER TABLE eu_grant ADD COLUMN submitted BOOLEAN NOT NULL DEFAULT FALSE;
+
+ALTER TABLE eu_grant ADD COLUMN short_code VARCHAR(12);
+ALTER TABLE eu_grant ADD UNIQUE KEY u_short_code (short_code);
