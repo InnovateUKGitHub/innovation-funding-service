95490f4e7597182384cb1ed85eb351040c956303
Aimar Aareleid <aareleid@worth.systems>
2018-08-23 09:08:09 +0200

    update os_run_tests.sh to support proper xunit format

--- robot-tests/os_run_tests.sh
+++ robot-tests/os_run_tests.sh
@@ -72,7 +72,7 @@ function startPybot() {
     fi
 
 
-    pybot --outputdir target/${targetDir} ${rerunString} --pythonpath IFS_acceptance_tests/libs \
+    pybot --xunit target/output-xunit.xml --outputdir target/${targetDir} ${rerunString} --pythonpath IFS_acceptance_tests/libs \
     -v docker:1 \
     -v SERVER_BASE:${webBase} \
     -v PROTOCOL:'https://' \
