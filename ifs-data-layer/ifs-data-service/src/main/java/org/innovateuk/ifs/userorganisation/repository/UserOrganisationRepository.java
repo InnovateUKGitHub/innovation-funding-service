92e50656c86f52eec781f72c2a8c50d4313aef59
Luke Harper <lharper@worth.systems>
2018-08-24 12:09:00 +0100

    removing whitespace.

--- ifs-data-layer/ifs-data-service/src/main/java/org/innovateuk/ifs/userorganisation/repository/UserOrganisationRepository.java
+++ ifs-data-layer/ifs-data-service/src/main/java/org/innovateuk/ifs/userorganisation/repository/UserOrganisationRepository.java
@@ -7,8 +7,6 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import java.util.Set;
 
 public interface UserOrganisationRepository extends PagingAndSortingRepository<UserOrganisation, Long> {
-
-
     Set<UserOrganisation> findByUserFirstNameLikeOrUserLastNameLikeAndUserRolesInOrderByUserEmailAsc(String firstName, String lastName, Set<Role> name);
     Set<UserOrganisation> finByUserEmailLikeAndUserRolesInOrderByUserEmailAsc(String email, Set<Role> name);
     Set<UserOrganisation> findByOrganisationNameLikeAndUserRolesInOrderByUserEmailAsc(String organisationName, Set<Role> name);
