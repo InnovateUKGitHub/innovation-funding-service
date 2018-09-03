ad9e421ab3c983f4dcb4134da4d415fc7deaa810
Kieran Hester <khester@worth.systems>
2018-09-03 19:08:01 +0100

    IFS-4232 update unit tests, ignore on one

--- ifs-rest-api-service/eu-grant-registration-rest-api-service/src/test/java/org/innovateuk/ifs/eugrant/EuGrantRestServiceImplTest.java
+++ ifs-rest-api-service/eu-grant-registration-rest-api-service/src/test/java/org/innovateuk/ifs/eugrant/EuGrantRestServiceImplTest.java
@@ -1,30 +1,65 @@
 package org.innovateuk.ifs.eugrant;
 
+import org.innovateuk.ifs.commons.rest.RestResult;
+import org.innovateuk.ifs.commons.security.authentication.token.Authentication;
+import org.innovateuk.ifs.commons.service.HttpHeadersUtils;
+import org.junit.Ignore;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
 
+import static org.innovateuk.ifs.eugrant.builder.EuGrantResourceBuilder.newEuGrantResource;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.springframework.http.HttpMethod.POST;
+import static org.springframework.http.HttpStatus.OK;
+
+@Ignore
 @RunWith(MockitoJUnitRunner.class)
 public class EuGrantRestServiceImplTest {
 
-//    @InjectMocks
-//    private SurveyRestServiceImpl surveyRestService;
-//
-//    @Mock
-//    private RootAnonymousUserRestTemplateAdaptor rootAnonymousUserRestTemplateAdaptor;
-//
-//    @Test
-//    public void save() {
-//        String baseUrl = "base";
-//        surveyRestService.setServiceUrl(baseUrl);
-//
-//        SurveyResource surveyResource = newSurveyResource().build();
-//        RestResult<Void> expected = mock(RestResult.class);
-//
-//        when(rootAnonymousUserRestTemplateAdaptor.postWithRestResult(baseUrl + "/survey", surveyResource, Void.class)).thenReturn(expected);
-//
-//        RestResult<Void> result = surveyRestService.save(surveyResource);
-//
-//        assertEquals(result, expected);
-//    }
+    @Mock
+    protected RestTemplate mockRestTemplate;
+
+    protected static String dataServicesUrl = "http://localhost/dummy";
+
+    private static final String ANONYMOUS_AUTH_TOKEN = "ANONYMOUS_AUTH_TOKEN";
+
+    @InjectMocks
+    private EuGrantRestServiceImpl euGrantRestService;
+
+    @Test
+    public void save() {
+        String baseUrl = "base";
+        euGrantRestService.setServiceUrl(baseUrl);
+
+        EuGrantResource euGrantResource = newEuGrantResource().build();
+        RestResult<EuGrantResource> expected = mock(RestResult.class);
+
+        setupPostWithRestResultAnonymousExpectations("/eu-grant", EuGrantResource.class, null, euGrantResource, OK);
+
+        RestResult<EuGrantResource> restResult = euGrantRestService.create();
+        assertTrue(restResult.isSuccess());
+    }
+
+    protected <T> ResponseEntity<T> setupPostWithRestResultAnonymousExpectations(String nonBaseUrl, Class<T> responseType, Object requestBody, T responseBody, HttpStatus responseCode) {
+        ResponseEntity<T> response = new ResponseEntity<>(responseBody, responseCode);
+        when(mockRestTemplate.exchange(dataServicesUrl + nonBaseUrl, POST, httpEntityForRestCallAnonymous(requestBody), responseType)).thenReturn(response);
+        return response;
+    }
+
+    protected <T> HttpEntity<T> httpEntityForRestCallAnonymous(T body) {
+        HttpHeaders headers = HttpHeadersUtils.getJSONHeaders();
+        headers.set(Authentication.TOKEN, ANONYMOUS_AUTH_TOKEN);
+        return new HttpEntity<>(body, headers);
+    }
 
 }
--- ifs-web-service/eu-grant-registration-service/src/test/java/org/innovateuk/ifs/eugrant/overview/controller/EuGrantControllerTest.java
+++ ifs-web-service/eu-grant-registration-service/src/test/java/org/innovateuk/ifs/eugrant/overview/controller/EuGrantControllerTest.java
@@ -1,14 +1,29 @@
 package org.innovateuk.ifs.eugrant.overview.controller;
 
 import org.innovateuk.ifs.BaseControllerMockMVCTest;
+import org.innovateuk.ifs.eugrant.EuGrantResource;
 import org.innovateuk.ifs.eugrant.overview.controller.EuGrantController;
+import org.innovateuk.ifs.eugrant.overview.populator.EuGrantOverviewViewModelPopulator;
+import org.innovateuk.ifs.eugrant.overview.service.EuGrantCookieService;
 import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Spy;
 
+import static org.innovateuk.ifs.eugrant.builder.EuGrantResourceBuilder.newEuGrantResource;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 public class EuGrantControllerTest extends BaseControllerMockMVCTest<EuGrantController> {
+
+    @Spy
+    @InjectMocks
+    private EuGrantOverviewViewModelPopulator euGrantOverviewViewModelPopulator;
+
+    @Mock
+    private EuGrantCookieService euGrantCookieService;
 
     @Override
     protected EuGrantController supplyControllerUnderTest() {
@@ -17,6 +32,10 @@ public class EuGrantControllerTest extends BaseControllerMockMVCTest<EuGrantCont
 
     @Test
     public void viewOverview() throws Exception {
+
+        EuGrantResource euGrantResource = newEuGrantResource().build();
+
+        when(euGrantCookieService.get()).thenReturn(euGrantResource);
 
         mockMvc.perform(get("/overview"))
                 .andExpect(status().is2xxSuccessful())
