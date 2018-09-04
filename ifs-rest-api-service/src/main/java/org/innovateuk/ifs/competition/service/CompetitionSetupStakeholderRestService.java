628f0314300cb04d38367a926bf2c5fbcb43f3f9
Bronnyl Dias <bdias@worth.systems>
2018-09-04 13:58:18 +0100

    IFS-4190-Invite-Stakeholder
    REST Layer changes.
    
    Change-Id: I1d566090bd56dad7cd96447be9d11a10626648c9

---
+++ ifs-rest-api-service/src/main/java/org/innovateuk/ifs/competition/service/CompetitionSetupStakeholderRestService.java
@@ -0,0 +1,13 @@
+package org.innovateuk.ifs.competition.service;
+
+import org.innovateuk.ifs.commons.rest.RestResult;
+import org.innovateuk.ifs.invite.resource.InviteUserResource;
+
+/**
+ * Interface for CRUD operations on Stakeholder related data.
+ */
+public interface CompetitionSetupStakeholderRestService {
+
+    RestResult<Void> saveStakeholderInvite(InviteUserResource inviteUserResource);
+}
+
---
+++ ifs-rest-api-service/src/main/java/org/innovateuk/ifs/competition/service/CompetitionSetupStakeholderRestServiceImpl.java
@@ -0,0 +1,22 @@
+package org.innovateuk.ifs.competition.service;
+
+import org.innovateuk.ifs.commons.rest.RestResult;
+import org.innovateuk.ifs.commons.service.BaseRestService;
+import org.innovateuk.ifs.invite.resource.InviteUserResource;
+import org.springframework.stereotype.Service;
+
+/**
+ * Implements {@link CompetitionSetupStakeholderRestService}
+ */
+@Service
+public class CompetitionSetupStakeholderRestServiceImpl extends BaseRestService implements CompetitionSetupStakeholderRestService {
+
+    private String competitionSetupStakeholderRestURL = "/competition/setup/stakeholder";
+
+    @Override
+    public RestResult<Void> saveStakeholderInvite(InviteUserResource inviteUserResource) {
+        return postWithRestResult(competitionSetupStakeholderRestURL + "/save-invite", inviteUserResource, Void.class);
+    }
+}
+
+
