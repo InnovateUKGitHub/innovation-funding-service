f0f6e0b01a6c4277db3d643474565c868fce4f43
Kieran Hester <khester@worth.systems>
2018-09-04 10:06:40 +0100

    IFS-4232 tidy up

--- ifs-rest-api-service/eu-grant-registration-rest-api-service/src/test/java/org/innovateuk/ifs/eugrant/EuGrantRestServiceImplTest.java
+++ ifs-rest-api-service/eu-grant-registration-rest-api-service/src/test/java/org/innovateuk/ifs/eugrant/EuGrantRestServiceImplTest.java
@@ -1,43 +1,23 @@
 package org.innovateuk.ifs.eugrant;
 
 import org.innovateuk.ifs.commons.rest.RestResult;
-import org.innovateuk.ifs.commons.security.authentication.token.Authentication;
-import org.innovateuk.ifs.commons.service.BaseRestService;
-import org.innovateuk.ifs.commons.service.HttpHeadersUtils;
 import org.innovateuk.ifs.commons.service.RootAnonymousUserRestTemplateAdaptor;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.client.RestTemplate;
 
 import java.util.UUID;
 
 import static java.util.UUID.randomUUID;
 import static org.innovateuk.ifs.eugrant.builder.EuGrantResourceBuilder.newEuGrantResource;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
-import static org.springframework.http.HttpMethod.POST;
-import static org.springframework.http.HttpStatus.CREATED;
-import static org.springframework.http.HttpStatus.OK;
 
 @RunWith(MockitoJUnitRunner.class)
 public class EuGrantRestServiceImplTest {
-
-    @Mock
-    protected RestTemplate mockRestTemplate;
-
-    protected static String dataServicesUrl = "http://localhost/dummy";
-
-    private static final String ANONYMOUS_AUTH_TOKEN = "ANONYMOUS_AUTH_TOKEN";
 
     @InjectMocks
     private EuGrantRestServiceImpl euGrantRestService;
