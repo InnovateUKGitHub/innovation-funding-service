1c3d4c8b17d0e05a9e4dd6b14648006db7799850
Kieran Hester <khester@worth.systems>
2018-09-04 10:04:54 +0100

    IFS-4232 fix unit tests

--- ifs-rest-api-service/eu-grant-registration-rest-api-service/src/test/java/org/innovateuk/ifs/eugrant/EuGrantRestServiceImplTest.java
+++ ifs-rest-api-service/eu-grant-registration-rest-api-service/src/test/java/org/innovateuk/ifs/eugrant/EuGrantRestServiceImplTest.java
@@ -2,7 +2,9 @@ package org.innovateuk.ifs.eugrant;
 
 import org.innovateuk.ifs.commons.rest.RestResult;
 import org.innovateuk.ifs.commons.security.authentication.token.Authentication;
+import org.innovateuk.ifs.commons.service.BaseRestService;
 import org.innovateuk.ifs.commons.service.HttpHeadersUtils;
+import org.innovateuk.ifs.commons.service.RootAnonymousUserRestTemplateAdaptor;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -15,14 +17,18 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.UUID;
+
+import static java.util.UUID.randomUUID;
 import static org.innovateuk.ifs.eugrant.builder.EuGrantResourceBuilder.newEuGrantResource;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 import static org.springframework.http.HttpMethod.POST;
+import static org.springframework.http.HttpStatus.CREATED;
 import static org.springframework.http.HttpStatus.OK;
 
-@Ignore
 @RunWith(MockitoJUnitRunner.class)
 public class EuGrantRestServiceImplTest {
 
@@ -36,30 +42,61 @@ public class EuGrantRestServiceImplTest {
     @InjectMocks
     private EuGrantRestServiceImpl euGrantRestService;
 
+    @Mock
+    private RootAnonymousUserRestTemplateAdaptor anonymousRestTemplateAdaptor;
+
     @Test
-    public void save() {
+    public void create() throws Exception {
         String baseUrl = "base";
         euGrantRestService.setServiceUrl(baseUrl);
 
         EuGrantResource euGrantResource = newEuGrantResource().build();
         RestResult<EuGrantResource> expected = mock(RestResult.class);
 
-        setupPostWithRestResultAnonymousExpectations("/eu-grant", EuGrantResource.class, null, euGrantResource, OK);
+        when(anonymousRestTemplateAdaptor.postWithRestResult(baseUrl + "/eu-grant", EuGrantResource.class)).thenReturn(expected);
 
-        RestResult<EuGrantResource> restResult = euGrantRestService.create();
-        assertTrue(restResult.isSuccess());
+        RestResult<EuGrantResource> result = euGrantRestService.create();
+
+        assertEquals(result, expected);
     }
 
-    protected <T> ResponseEntity<T> setupPostWithRestResultAnonymousExpectations(String nonBaseUrl, Class<T> responseType, Object requestBody, T responseBody, HttpStatus responseCode) {
-        ResponseEntity<T> response = new ResponseEntity<>(responseBody, responseCode);
-        when(mockRestTemplate.exchange(dataServicesUrl + nonBaseUrl, POST, httpEntityForRestCallAnonymous(requestBody), responseType)).thenReturn(response);
-        return response;
+    @Test
+    public void save() throws Exception {
+
+        final UUID uuid = randomUUID();
+
+        String baseUrl = "base";
+        euGrantRestService.setServiceUrl(baseUrl);
+
+        EuGrantResource euGrantResource = newEuGrantResource().build();
+        euGrantResource.setId(uuid);
+
+        RestResult<Void> expected = mock(RestResult.class);
+
+        when(anonymousRestTemplateAdaptor.putWithRestResult(baseUrl + "/eu-grant/" + uuid.toString(), euGrantResource, Void.class)).thenReturn(expected);
+
+        RestResult<Void> result = euGrantRestService.update(euGrantResource);
+
+        assertEquals(result, expected);
     }
 
-    protected <T> HttpEntity<T> httpEntityForRestCallAnonymous(T body) {
-        HttpHeaders headers = HttpHeadersUtils.getJSONHeaders();
-        headers.set(Authentication.TOKEN, ANONYMOUS_AUTH_TOKEN);
-        return new HttpEntity<>(body, headers);
-    }
+    @Test
+    public void findById() throws Exception {
 
+        final UUID uuid = randomUUID();
+
+        String baseUrl = "base";
+        euGrantRestService.setServiceUrl(baseUrl);
+
+        EuGrantResource euGrantResource = newEuGrantResource().build();
+        euGrantResource.setId(uuid);
+
+        RestResult<EuGrantResource> expected = mock(RestResult.class);
+
+        when(anonymousRestTemplateAdaptor.getWithRestResult(baseUrl + "/eu-grant/" + uuid.toString(), EuGrantResource.class)).thenReturn(expected);
+
+        RestResult<EuGrantResource> result = euGrantRestService.findById(uuid);
+
+        assertEquals(result, expected);
+    }
 }
