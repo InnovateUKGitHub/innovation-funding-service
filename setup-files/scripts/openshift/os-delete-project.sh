9c35e09a62237cf19b971b187ae8385f636f69fc
Laszlo Fogas <laszlo@laszlo.cloud>
2018-09-07 11:51:12 +0200

    Using variable based cluster authentication

---
+++ setup-files/scripts/openshift/os-delete-project.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+
+PROJECT=$1
+TARGET=$2
+VERSION=$3
+
+. $(dirname $0)/deploy-functions.sh
+
+PROJECT=$(getProjectName $PROJECT $TARGET)
+SVC_ACCOUNT_TOKEN=$(getSvcAccountToken)
+HOST=$(getHost $TARGET)
+ROUTE_DOMAIN=$(getRouteDomain $TARGET $HOST)
+SVC_ACCOUNT_CLAUSE=$(getSvcAccountClause $TARGET $PROJECT $SVC_ACCOUNT_TOKEN)
+
+echo "Deleting $PROJECT Openshift project"
+
+oc delete ${PROJECT} ${SVC_ACCOUNT_CLAUSE}
