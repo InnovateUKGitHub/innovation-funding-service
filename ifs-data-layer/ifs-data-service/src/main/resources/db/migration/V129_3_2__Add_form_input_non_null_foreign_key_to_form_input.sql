d08613e08c59776372ad7df71dc48595b36574a0
Bronnyl Dias <bdias@worth.systems>
2018-09-07 19:04:36 +0100

    IFS-4304-ISE-Your-Org-Page-Part2
    Change-Id: Ic4f798343c30929b6efb301588ee7c35a0100ff7

--- ifs-data-layer/ifs-data-service/src/main/resources/db/migration/V129_3_2__Add_form_input_non_null_foreign_key_to_form_input.sql
+++ ifs-data-layer/ifs-data-service/src/main/resources/db/migration/V129_3_2__Add_form_input_non_null_foreign_key_to_form_input.sql
@@ -1,4 +1,3 @@
 -- IFS-3950 - adding missing constraints to the form_input_response table to improve data integrity
-
-ALTER TABLE form_input_response MODIFY form_input_id bigint(20) NOT NULL;
-ALTER TABLE form_input_response MODIFY application_id bigint(20) NOT NULL;
+--IFS-4304: There are issues with adding NOT NULL constraints to application id. As a result, the SQL statements in this file,
+--have been removed (as this has not yet been run on production)
