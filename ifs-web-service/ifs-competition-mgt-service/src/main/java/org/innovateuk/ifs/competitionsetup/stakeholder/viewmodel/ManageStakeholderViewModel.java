d1c0bca7457510dbd0a69531036dcdbea9e15d4a
Bronnyl Dias <bdias@worth.systems>
2018-09-07 11:00:22 +0100

    IFS-4190-Invite-Stakeholder
    More junits for the above.
    
    Change-Id: I1211d90515089fa498fceb59406e408f569be543

--- ifs-web-service/ifs-competition-mgt-service/src/main/java/org/innovateuk/ifs/competitionsetup/stakeholder/viewmodel/ManageStakeholderViewModel.java
+++ ifs-web-service/ifs-competition-mgt-service/src/main/java/org/innovateuk/ifs/competitionsetup/stakeholder/viewmodel/ManageStakeholderViewModel.java
@@ -1,5 +1,7 @@
 package org.innovateuk.ifs.competitionsetup.stakeholder.viewmodel;
 
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
 import org.innovateuk.ifs.competitionsetup.core.viewmodel.CompetitionSetupViewModel;
 
 public class ManageStakeholderViewModel extends CompetitionSetupViewModel {
@@ -18,6 +20,28 @@ public class ManageStakeholderViewModel extends CompetitionSetupViewModel {
 
     public String getCompetitionName() {
         return competitionName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+
+        if (o == null || getClass() != o.getClass()) return false;
+
+        ManageStakeholderViewModel that = (ManageStakeholderViewModel) o;
+
+        return new EqualsBuilder()
+                .append(competitionId, that.competitionId)
+                .append(competitionName, that.competitionName)
+                .isEquals();
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder(17, 37)
+                .append(competitionId)
+                .append(competitionName)
+                .toHashCode();
     }
 }
 
