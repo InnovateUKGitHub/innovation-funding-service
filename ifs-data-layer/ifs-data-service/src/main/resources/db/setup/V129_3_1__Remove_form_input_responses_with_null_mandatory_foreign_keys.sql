923ed379068fea3b4dfd8c2f1718e764233e971c
Bronnyl Dias <bdias@worth.systems>
2018-09-07 14:07:12 +0100

    IFS-4304-ISE-Your-Org-Page
    Change-Id: I3979b2b0a973150b2c37954ccd05f613be5e819e

--- ifs-data-layer/ifs-data-service/src/main/resources/db/setup/V129_3_1__Remove_form_input_responses_with_null_mandatory_foreign_keys.sql
+++ ifs-data-layer/ifs-data-service/src/main/resources/db/setup/V129_3_1__Remove_form_input_responses_with_null_mandatory_foreign_keys.sql
@@ -1,3 +1,5 @@
 -- IFS-3950 - removing redundant form_input_response rows before enforcing non-null foreign key constraints
+-- UPDATE: There are issues with adding NOT NULL constraints to application id in V129_3_2__Add_form_input_non_null_foreign_key_to_form_input.sql
+-- Those NOT NULL constraints have been revered in V129_5_1. As a result the below statement has been commented (as this has not yet been run on production)
 
-DELETE FROM form_input_response WHERE application_id IS NULL OR form_input_id IS NULL;
+--DELETE FROM form_input_response WHERE application_id IS NULL OR form_input_id IS NULL;
