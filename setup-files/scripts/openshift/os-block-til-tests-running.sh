a2072a4591ce97a14d7619e76d5bc63fadab6a2d
Laszlo Fogas <laszlo@laszlo.cloud>
2018-09-11 15:14:32 +0200

    Ignore deploy pod

--- setup-files/scripts/openshift/os-block-til-tests-running.sh
+++ setup-files/scripts/openshift/os-block-til-tests-running.sh
@@ -14,7 +14,7 @@ HOST=$(getHost $TARGET)
 ROUTE_DOMAIN=$(getRouteDomain $TARGET $HOST)
 SVC_ACCOUNT_CLAUSE=$(getSvcAccountClause $TARGET $PROJECT $SVC_ACCOUNT_TOKEN)
 
-PODNAME=$(oc get pods ${SVC_ACCOUNT_CLAUSE} | grep ^robot-framework | awk '{ print $1 }')
+PODNAME=$(oc get pods ${SVC_ACCOUNT_CLAUSE} | grep ^robot-framework | grep -v deploy | awk '{ print $1 }')
 
 echo "Waiting tests to be ready on $PODNAME"
 
