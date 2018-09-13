a5fb5ae6d0db7cb14f2365d1ff675e8782e67b76
Laszlo Fogas <laszlo@laszlo.cloud>
2018-09-13 07:08:42 +0200

    Removed debug lines

--- setup-files/scripts/openshift/os-test-remote.sh
+++ setup-files/scripts/openshift/os-test-remote.sh
@@ -28,7 +28,6 @@ function tailorToAppInstance() {
     cp -r robot-tests robot-tests-tmp
     sed -i.bak "s/<<SHIB-ADDRESS>>/$PROJECT.$ROUTE_DOMAIN/g" robot-tests-tmp/openshift/*.sh
     sed -i.bak "s/<<SHIB-ADDRESS>>/$PROJECT.$ROUTE_DOMAIN/g" robot-tests-tmp/os_run_tests.sh
-    echo "ROBOT COMMAND: $ROBOT_COMMAND"
     sed -i.bak "s#\[\"./os_run_tests.sh\", \"-q\"\]#[\"./os_run_tests.sh\", \"-q\", $ROBOT_COMMAND]#g" robot-tests-tmp/Dockerfile
 }
 
