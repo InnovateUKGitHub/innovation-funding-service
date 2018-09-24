6982c439b160f1f54686384c58efef14ce00a43f
rmohammed999 <rmohammed@worth.systems>
2018-09-24 09:41:13 +0100

    IFS-4199 addressed Keiran's comments...

--- ifs-data-layer/ifs-data-service/src/test/java/org/innovateuk/ifs/documentation/CompanyHouseDocs.java
+++ ifs-data-layer/ifs-data-service/src/test/java/org/innovateuk/ifs/documentation/CompanyHouseDocs.java
@@ -11,14 +11,14 @@ public class CompanyHouseDocs {
                 fieldWithPath("organisationSearchId").description("Id of the Organisation Search Result"),
                 fieldWithPath("name").description("Name of the associated Organisation"),
                 fieldWithPath("organisationAddress").description("The associated Organisation's Address"),
-                fieldWithPath("organisationAddress.id").description("The associated Organisation Address Id"),
-                fieldWithPath("organisationAddress.addressLine1").description("The associated Organisation Address AddressLine1"),
-                fieldWithPath("organisationAddress.addressLine2").description("The associated Organisations Address AddressLine2"),
-                fieldWithPath("organisationAddress.addressLine3").description("The associated Organisations Address AddressLine3"),
-                fieldWithPath("organisationAddress.town").description("The associated Organisations Address town"),
-                fieldWithPath("organisationAddress.county").description("The associated Organisations Address county"),
-                fieldWithPath("organisationAddress.postcode").description("The associated Organisations Address postcode"),
-                fieldWithPath("extraAttributes") .description("Any extra attributes of the Organisation")
+                fieldWithPath("organisationAddress.id").description("The id of the Organisation's address"),
+                fieldWithPath("organisationAddress.addressLine1").description("The first line of the Organisation's Address"),
+                fieldWithPath("organisationAddress.addressLine2").description("The second line of the Organisation's Address"),
+                fieldWithPath("organisationAddress.addressLine3").description("The third line of the Organisation's Address"),
+                fieldWithPath("organisationAddress.town").description("The town of the Organisation's Address"),
+                fieldWithPath("organisationAddress.county").description("The county of Organisation's Address"),
+                fieldWithPath("organisationAddress.postcode").description("The postcode of the Organisation's Address"),
+                fieldWithPath("extraAttributes") .description("Extra attributes of the Organisation")
         };
     }
 }
