92ec150d218ec737a9a56f3ea50033ae01612432
Ben Outram <ben@hiveit.co.uk>
2018-09-26 15:00:12 +0100

    IFS-4271 Removing unused db column grant_claim_maximum.organisation_type_id.

--- ifs-data-layer/ifs-data-service/src/main/resources/db/migration/V129_8_1__Drop_grant_claim_maximum_colum_organisation_type_id.sql
+++ ifs-data-layer/ifs-data-service/src/main/resources/db/migration/V129_8_1__Drop_grant_claim_maximum_colum_organisation_type_id.sql
@@ -1,3 +1,6 @@
 -- IFS-4271
 -- ZDD cleanup for Grant Claim Maximum
+ALTER TABLE grant_claim_maximum
+  DROP FOREIGN KEY grant_claim_maximum_organisation_type_fk;
+
 ALTER TABLE grant_claim_maximum DROP COLUMN organisation_type_id;
