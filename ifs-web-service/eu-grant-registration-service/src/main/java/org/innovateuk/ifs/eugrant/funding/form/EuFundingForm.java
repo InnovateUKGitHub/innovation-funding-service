718689db91562f6bee639a58872455836d508bf2
Grace Williams <grace.williams@innovateuk.gov.uk>
2018-10-02 11:02:53 +0100

    IFS-4432: Method added to display the month in words on Eu Funding details page

--- ifs-web-service/eu-grant-registration-service/src/main/java/org/innovateuk/ifs/eugrant/funding/form/EuFundingForm.java
+++ ifs-web-service/eu-grant-registration-service/src/main/java/org/innovateuk/ifs/eugrant/funding/form/EuFundingForm.java
@@ -164,6 +164,11 @@ public class EuFundingForm {
         return LocalDate.parse(date, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
     }
 
+    // this method is used only to display the month in words
+    public LocalDate getProjectLocalDate(int month, int year){
+
+        return LocalDate.of(year, month, 1);
+    }
 
     @Override
     public boolean equals(Object o) {
