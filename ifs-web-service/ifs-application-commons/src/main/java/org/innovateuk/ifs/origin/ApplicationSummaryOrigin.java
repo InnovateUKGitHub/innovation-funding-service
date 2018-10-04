f61266ef1ea3d9d586c1b85427474e64d9c4d67c
Kieran Hester <khester@worth.systems>
2018-10-04 07:50:23 +0100

    IFS-4244 change queryParams to originQuery

--- ifs-web-service/ifs-application-commons/src/main/java/org/innovateuk/ifs/origin/ApplicationSummaryOrigin.java
+++ ifs-web-service/ifs-application-commons/src/main/java/org/innovateuk/ifs/origin/ApplicationSummaryOrigin.java
@@ -5,14 +5,17 @@ public enum ApplicationSummaryOrigin implements BackLinkOrigin {
     SET_UP_YOUR_PROJECT("/project-setup/project/{projectId}", "Set up your project"),
     APPLICANT_DASHBOARD("/applicant/dashboard", "Dashboard"),
     ASSESSOR_INTERVIEW("/assessment/assessor/dashboard/competition/{competitionId}/interview", "Interview"),
+    MANAGE_APPLICATIONS("/assessment/competition/{competitionId}/applications", "Back"),
+    APPLICATION_PROGRESS("/assessment/competition/{competitionId}/application/{applicationId}/assessors", "Back"),
+    ASSESSOR_PROGRESS("/assessment/competition/{competitionId}/assessors/{assessorId}", "Application overview"),
     COMP_EXEC_INTERVIEW("/management/assessment/interview/competition/{competitionId}/applications/view-status", "Applications"),
     PREVIOUS_APPLICATIONS("/competition/{competitionId}/applications/previous", "Back"),
     ALL_APPLICATIONS("/competition/{competitionId}/applications/all", "All Applications"),
     APPLICATION_SUMMARY("/application/{applicationId}/summary", "Application Overview"),
-    SUBMITTED_APPLICATIONS("/competition/{competitionId}/applications/submitted", "Application Overview"),
+    SUBMITTED_APPLICATIONS("/competition/{competitionId}/applications/submitted", "Application overview"),
     PROJECT_SETUP_MANAGEMENT_STATUS("/project-setup-management/competition/{competitionId}/status", "Back"),
-    INELIGIBLE_APPLICATIONS("/competition/{competitionId}/applications/ineligible", "Application Overview"),
-    APPLICATION("/application/{applicationId}", "Application Overview");
+    INELIGIBLE_APPLICATIONS("/competition/{competitionId}/applications/ineligible", "Application overview"),
+    APPLICATION("/application/{applicationId}", "Application overview");
 
     private String originUrl;
     private String title;
