e58809d813bdb108bc7186af1649549c2a95cae1
Martin Waite <martin@hiveit.co.uk>
2018-10-05 10:14:32 +0100

    IFS-2610 Javascript updates checking errors in tables

--- ifs-web-service/ifs-application-service/src/main/resources/static/js/ifsApplicationLoader.js
+++ ifs-web-service/ifs-application-service/src/main/resources/static/js/ifsApplicationLoader.js
@@ -43,5 +43,8 @@ IFS.application.loadOrder = {
   },
   'competition-management': {
     init: function () { IFS.application.competitionManagement.init() }
+  },
+  'set-table-validation': {
+    init: function () { IFS.application.setTableValidation.init() }
   }
 }
