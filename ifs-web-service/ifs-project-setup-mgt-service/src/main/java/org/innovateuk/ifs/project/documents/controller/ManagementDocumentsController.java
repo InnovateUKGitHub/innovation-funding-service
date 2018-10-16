709e0ad92a047348f67e81574cf639cc537de21f
Bronnyl Dias <bdias@worth.systems>
2018-10-16 16:20:29 +0100

    IFS-4573-PD-11-Internal-User-Show-All-Docs
    Change-Id: I4ac5974d81ad1a606f5b16207dd5995f9ef80e47

---
+++ ifs-web-service/ifs-project-setup-mgt-service/src/main/java/org/innovateuk/ifs/project/documents/controller/ManagementDocumentsController.java
@@ -0,0 +1,32 @@
+package org.innovateuk.ifs.project.documents.controller;
+
+import org.innovateuk.ifs.project.documents.populator.DocumentsPopulator;
+import org.innovateuk.ifs.user.resource.UserResource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Controller backing the Documents page
+ */
+@Controller
+@RequestMapping("/project/{projectId}/document")
+//TODO - XXX - Rename this class back to normal
+public class ManagementDocumentsController {
+
+    @Autowired
+    DocumentsPopulator populator;
+
+    //TODO - XXX - Permissions
+    //@PreAuthorize("hasPermission(#projectId, 'org.innovateuk.ifs.project.resource.ProjectCompositeId', 'ACCESS_OTHER_DOCUMENTS_SECTION')")
+    @GetMapping("/all")
+    public String viewAllDocuments(@PathVariable("projectId") long projectId, Model model,
+                                   UserResource loggedInUser) {
+
+        model.addAttribute("model", populator.populateAllDocuments(projectId));
+        return "project/documents-all"; //TODO - XXX - Create this view please.
+    }
+}
---
+++ ifs-web-service/ifs-project-setup-mgt-service/src/main/java/org/innovateuk/ifs/project/documents/populator/DocumentsPopulator.java
@@ -0,0 +1,72 @@
+package org.innovateuk.ifs.project.documents.populator;
+
+import org.innovateuk.ifs.application.resource.ApplicationResource;
+import org.innovateuk.ifs.application.service.ApplicationService;
+import org.innovateuk.ifs.competition.resource.CompetitionResource;
+import org.innovateuk.ifs.competition.service.CompetitionRestService;
+import org.innovateuk.ifs.project.ProjectService;
+import org.innovateuk.ifs.project.document.resource.DocumentStatus;
+import org.innovateuk.ifs.project.document.resource.ProjectDocumentResource;
+import org.innovateuk.ifs.project.document.resource.ProjectDocumentStatus;
+import org.innovateuk.ifs.project.documents.viewmodel.AllDocumentsViewModel;
+import org.innovateuk.ifs.project.resource.BasicDetails;
+import org.innovateuk.ifs.project.resource.ProjectResource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.innovateuk.ifs.util.CollectionFunctions.simpleFindAny;
+
+@Component
+public class DocumentsPopulator {
+
+    @Autowired
+    private ProjectService projectService;
+
+    @Autowired
+    private ApplicationService applicationService;
+
+    @Autowired
+    private CompetitionRestService competitionRestService;
+
+    public AllDocumentsViewModel populateAllDocuments(long projectId) {
+
+        BasicDetails basicDetails = populateBasicDetails(projectId);
+        ProjectResource project = basicDetails.getProject();
+        CompetitionResource competition = basicDetails.getCompetition();
+
+        List<org.innovateuk.ifs.competition.resource.ProjectDocumentResource> configuredProjectDocuments = competition.getProjectDocuments();
+        List<ProjectDocumentResource> projectDocuments = project.getProjectDocuments();
+
+        List<ProjectDocumentStatus> documents = new ArrayList<>();
+
+        for (org.innovateuk.ifs.competition.resource.ProjectDocumentResource configuredDocument : configuredProjectDocuments) {
+            documents.add(new ProjectDocumentStatus(configuredDocument.getId(),
+                    configuredDocument.getTitle(),
+                    getProjectDocumentStatus(projectDocuments, configuredDocument.getId())));
+        }
+
+        return new AllDocumentsViewModel(projectId, project.getName(), documents);
+    }
+
+    private BasicDetails populateBasicDetails(long projectId) {
+
+        ProjectResource project = projectService.getById(projectId);
+
+        ApplicationResource application = applicationService.getById(project.getApplication());
+
+        CompetitionResource competition = competitionRestService.getCompetitionById(application.getCompetition()).getSuccess();
+
+        return new BasicDetails(project, application, competition);
+
+    }
+
+    private DocumentStatus getProjectDocumentStatus(List<ProjectDocumentResource> projectDocuments, Long documentConfigId) {
+
+        return simpleFindAny(projectDocuments, projectDocumentResource -> projectDocumentResource.getProjectDocument().getId().equals(documentConfigId))
+                .map(projectDocumentResource -> projectDocumentResource.getStatus())
+                .orElse(DocumentStatus.UNSET);
+    }
+}
