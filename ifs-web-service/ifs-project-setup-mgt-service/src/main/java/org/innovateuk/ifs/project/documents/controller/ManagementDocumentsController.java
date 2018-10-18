a5a042b6c68a43c2a6521c2eb56fc4075837fe16
Bronnyl Dias <bdias@worth.systems>
2018-10-18 16:16:50 +0100

    IFS-4599-PD-13-Internal-User-Download-Doc
    Change-Id: Ic4b321110cb6bede70139ea0b59601dac68d2014

--- ifs-web-service/ifs-project-setup-mgt-service/src/main/java/org/innovateuk/ifs/project/documents/controller/ManagementDocumentsController.java
+++ ifs-web-service/ifs-project-setup-mgt-service/src/main/java/org/innovateuk/ifs/project/documents/controller/ManagementDocumentsController.java
@@ -1,13 +1,31 @@
 package org.innovateuk.ifs.project.documents.controller;
 
+import org.innovateuk.ifs.commons.exception.ObjectNotFoundException;
+import org.innovateuk.ifs.controller.ValidationHandler;
+import org.innovateuk.ifs.file.resource.FileEntryResource;
 import org.innovateuk.ifs.project.documents.populator.DocumentsPopulator;
+import org.innovateuk.ifs.project.documents.service.DocumentsRestService;
+import org.innovateuk.ifs.project.otherdocuments.form.OtherDocumentsForm;
 import org.innovateuk.ifs.user.resource.UserResource;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.method.P;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Optional;
+
+import static java.util.Arrays.asList;
+import static java.util.Collections.singletonList;
+import static org.innovateuk.ifs.file.controller.FileDownloadControllerUtils.getFileResponseEntity;
 
 /**
  * Controller backing the Documents page
@@ -19,6 +37,9 @@ public class ManagementDocumentsController {
 
     @Autowired
     DocumentsPopulator populator;
+
+    @Autowired
+    private DocumentsRestService documentsRestService;
 
     //TODO - XXX - Permissions
     //@PreAuthorize("hasPermission(#projectId, 'org.innovateuk.ifs.project.resource.ProjectCompositeId', 'ACCESS_OTHER_DOCUMENTS_SECTION')")
@@ -47,4 +68,25 @@ public class ManagementDocumentsController {
         return "project/document";
     }
 
+    //TODO - XXX - Permissions
+    //@PreAuthorize("hasPermission(#projectId, 'org.innovateuk.ifs.project.resource.ProjectCompositeId', 'ACCESS_OTHER_DOCUMENTS_SECTION')")
+    @GetMapping("/config/{documentConfigId}/download")
+    public
+    @ResponseBody
+    ResponseEntity<ByteArrayResource> downloadDocument(@PathVariable("projectId") long projectId,
+                                                       @PathVariable("documentConfigId") long documentConfigId) {
+
+        final Optional<ByteArrayResource> fileContents = documentsRestService.getFileContents(projectId, documentConfigId).getSuccess();
+        final Optional<FileEntryResource> fileEntryDetails = documentsRestService.getFileEntryDetails(projectId, documentConfigId).getSuccess();
+        return returnFileIfFoundOrThrowNotFoundException(projectId, documentConfigId, fileContents, fileEntryDetails);
+    }
+
+    private ResponseEntity<ByteArrayResource> returnFileIfFoundOrThrowNotFoundException(long projectId, long documentConfigId,
+                                                                                        Optional<ByteArrayResource> fileContents, Optional<FileEntryResource> fileEntryDetails) {
+        if (fileContents.isPresent() && fileEntryDetails.isPresent()) {
+            return getFileResponseEntity(fileContents.get(), fileEntryDetails.get());
+        } else {
+            throw new ObjectNotFoundException("Could not find document with document config id: " + documentConfigId + " for project: " + projectId, asList(projectId, documentConfigId));
+        }
+    }
 }
