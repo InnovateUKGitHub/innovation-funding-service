bc0d1c486834084cdf270ab93539433949e94f7d
Nathan Phillips <nathanphillips@MacBook-Pro-5.local>
2018-10-19 15:08:00 +0100

    IFS-3405: added new js function to block non number input on number html fields

--- ifs-web-service/ifs-web-core/src/main/resources/static/js/ifs_modules/preventInputRegex.js
+++ ifs-web-service/ifs-web-core/src/main/resources/static/js/ifs_modules/preventInputRegex.js
@@ -9,6 +9,24 @@ IFS.core.preventInputRegex = (function () {
     init: function () {
       s = this.settings
 
+      /* we catch paste/keypress commands for number fields as the input event directly clashes with browsers
+        own inadequate attempts to prevent bad input */
+      jQuery('[data-prevent-input-non-number]').on('paste', function (event) {
+        var regex = new RegExp('[^0-9]', 'g')
+        var data = event.originalEvent.clipboardData.getData('text')
+        if (data.replace(regex, '') !== data) {
+          event.originalEvent.preventDefault()
+        }
+      })
+
+      jQuery('[data-prevent-input-non-number]').on('keypress', function (event) {
+        var regex = new RegExp('[^0-9]', 'g')
+        if (event.originalEvent.key.replace(regex, '') === '') {
+          event.originalEvent.preventDefault()
+        }
+      })
+
+      // general form that can be used with any regex
       jQuery(document).on('input', '[' + s.preventInputRegexEl + ']', function () {
         var el = jQuery(this)
         var regex = new RegExp(el.attr(s.preventInputRegexEl), 'g')
