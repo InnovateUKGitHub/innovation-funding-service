77f6437f0e6f296cb36e4572eb1c4b09c3e0d48d
Ewan Cormack <ewan@hiveit.co.uk>
2018-10-24 10:58:13 +0100

    IFS-4459: Exclude Princes Trust competitions as they never reach the project setup phase

--- ifs-data-layer/ifs-data-service/src/main/resources/db/setup/V134_1_3__Create_default_docs_for_all_competitions_not_yet_in_PS.sql
+++ ifs-data-layer/ifs-data-service/src/main/resources/db/setup/V134_1_3__Create_default_docs_for_all_competitions_not_yet_in_PS.sql
@@ -1,4 +1,5 @@
 -- IFS-4459 - Project Documents - Create default documents for all competitions which have not yet reached the Project Setup phase.
+-- Expression of interest and Prince's trust competitions will never reach PS so are excluded by default
 INSERT INTO document_config
 (competition_id, title, guidance, editable, enabled, type)
 SELECT c.id AS competition_id,
@@ -13,6 +14,7 @@ WHERE c.id NOT IN (
 	WHERE EXISTS (SELECT a.manage_funding_email_date FROM application a WHERE a.competition = c.id AND a.funding_decision = 'FUNDED' AND a.manage_funding_email_date IS NOT NULL)
 	AND c.setup_complete = TRUE AND c.template = FALSE AND c.non_ifs = FALSE
 	AND ct.name != 'Expression of interest'
+	AND ct.name != 'The Prince''s Trust'
 	);
 
 INSERT INTO document_config
@@ -29,5 +31,6 @@ WHERE c.id NOT IN (
 	WHERE EXISTS (SELECT a.manage_funding_email_date FROM application a WHERE a.competition = c.id AND a.funding_decision = 'FUNDED' AND a.manage_funding_email_date IS NOT NULL)
 	AND c.setup_complete = TRUE AND c.template = FALSE AND c.non_ifs = FALSE
 	AND ct.name != 'Expression of interest'
+	AND ct.name != 'The Prince''s Trust'
 );
 
