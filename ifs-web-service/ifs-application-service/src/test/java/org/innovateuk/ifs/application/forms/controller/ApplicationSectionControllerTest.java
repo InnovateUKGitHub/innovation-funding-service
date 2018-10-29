f8c1299d12a8e75fb9555000f2c22060e6c47e8b
Tom Baldwin <tom@polymorphism.co.uk>
2018-10-29 12:24:23 +0000

    IFS-4569 - Adds ApplicationSectionControllerTest tests for project location validation.

--- ifs-web-service/ifs-application-service/src/test/java/org/innovateuk/ifs/application/forms/controller/ApplicationSectionControllerTest.java
+++ ifs-web-service/ifs-application-service/src/test/java/org/innovateuk/ifs/application/forms/controller/ApplicationSectionControllerTest.java
@@ -361,6 +361,45 @@ public class ApplicationSectionControllerTest extends AbstractApplicationMockMVC
     }
 
     @Test
+    public void applicationFormSubmit_projectLocation() throws Exception {
+        String projectLocationPostcode = "123";
+        when(applicantRestService.getSection(any(), any(), any())).thenReturn(sectionBuilder.withSection(newSectionResource().withType(SectionType.PROJECT_LOCATION).build()).build());
+        mockMvc.perform(
+                post("/application/{applicationId}/form/section/{sectionId}", application.getId(), "1")
+                        .param("financePosition-projectLocation", projectLocationPostcode)
+                        .param(MARK_SECTION_AS_COMPLETE, String.valueOf("1"))
+        ).andExpect(status().is3xxRedirection())
+                .andExpect(MockMvcResultMatchers.redirectedUrlPattern("/application/" + application.getId() + "/form/section/**"));    }
+
+    @Test
+    public void applicationFormSubmit_projectLocationTooShort() throws Exception {
+        String projectLocationPostcode = "123456718901";
+        when(applicantRestService.getSection(any(), any(), any())).thenReturn(sectionBuilder.withSection(newSectionResource().withType(SectionType.PROJECT_LOCATION).build()).build());
+        mockMvc.perform(
+                post("/application/{applicationId}/form/section/{sectionId}", application.getId(), "1")
+                        .param("financePosition-projectLocation", projectLocationPostcode)
+                        .param(MARK_SECTION_AS_COMPLETE, String.valueOf("1"))
+        ).andExpect(status().isOk())
+                .andExpect(view().name("application-form"))
+                .andExpect(model().attributeErrorCount("form", 1));
+        verify(applicationNavigationPopulator).addAppropriateBackURLToModel(any(Long.class), any(Model.class), any(SectionResource.class), any(Optional.class), any(Optional.class), any(Boolean.class));
+    }
+
+    @Test
+    public void applicationFormSubmit_projectLocationTooLong() throws Exception {
+        String projectLocationPostcode = "12";
+        when(applicantRestService.getSection(any(), any(), any())).thenReturn(sectionBuilder.withSection(newSectionResource().withType(SectionType.PROJECT_LOCATION).build()).build());
+        mockMvc.perform(
+                post("/application/{applicationId}/form/section/{sectionId}", application.getId(), "1")
+                        .param("financePosition-projectLocation", projectLocationPostcode)
+                        .param(MARK_SECTION_AS_COMPLETE, String.valueOf("1"))
+        ).andExpect(status().isOk())
+                .andExpect(view().name("application-form"))
+                .andExpect(model().attributeErrorCount("form", 1));
+        verify(applicationNavigationPopulator).addAppropriateBackURLToModel(any(Long.class), any(Model.class), any(SectionResource.class), any(Optional.class), any(Optional.class), any(Boolean.class));
+    }
+
+    @Test
     public void applicationFormSubmit_markSectionInComplete() throws Exception {
 
         mockMvc.perform(
