625868114e0121c7b2a3bd5decd8810416f15cf1
Ian Homer <ihomer@worth.systems>
2018-10-30 10:08:55 +0000

    IFS-4491 : Internal grant service does not need to be secured

--- ifs-data-layer/ifs-data-service/src/main/java/org/innovateuk/ifs/grant/service/GrantService.java
+++ ifs-data-layer/ifs-data-service/src/main/java/org/innovateuk/ifs/grant/service/GrantService.java
@@ -1,8 +1,10 @@
 package org.innovateuk.ifs.grant.service;
 
+import org.innovateuk.ifs.commons.security.NotSecured;
 import org.innovateuk.ifs.commons.service.ServiceResult;
 
 public interface GrantService {
+    @NotSecured(value = "Anyone can send grant data", mustBeSecuredByOtherServices = false)
     ServiceResult<Void> sendProject(final Long applicationId);
 }
 
