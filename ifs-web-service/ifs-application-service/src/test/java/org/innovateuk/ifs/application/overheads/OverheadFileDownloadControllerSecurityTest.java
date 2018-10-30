8b2cf196f18921441ac7716957289c217ec569eb
Lewis Toseland <lewis@hiveit.co.uk>
2018-10-30 13:53:27 +0000

    Security tests added

--- ifs-web-service/ifs-application-service/src/test/java/org/innovateuk/ifs/application/overheads/OverheadFileDownloadControllerSecurityTest.java
+++ ifs-web-service/ifs-application-service/src/test/java/org/innovateuk/ifs/application/overheads/OverheadFileDownloadControllerSecurityTest.java
@@ -1,7 +1,24 @@
 package org.innovateuk.ifs.application.overheads;
 
 import org.innovateuk.ifs.security.BaseControllerSecurityTest;
+import org.innovateuk.ifs.user.resource.Role;
+import org.junit.Test;
 
-public abstract class OverheadFileDownloadControllerSecurityTest<ControllerType> extends BaseControllerSecurityTest<ControllerType> {
+public class OverheadFileDownloadControllerSecurityTest extends BaseControllerSecurityTest<OverheadFileDownloadController> {
 
+    @Override
+    protected Class<? extends OverheadFileDownloadController> getClassUnderTest() {
+        return OverheadFileDownloadController.class;
+    }
+
+    @Test
+    public void downloadQuestionFile() {
+        assertRolesCanPerform(() -> classUnderTest.downloadQuestionFile(1L),
+                Role.APPLICANT,
+                Role.PROJECT_FINANCE,
+                Role.COMP_ADMIN,
+                Role.SUPPORT,
+                Role.IFS_ADMINISTRATOR,
+                Role.INNOVATION_LEAD);
+    }
 }
