17a2a8d6ba8cb49a116060c391aa23647c66c879
Tom Baldwin <tom@polymorphism.co.uk>
2018-11-01 15:30:39 +0000

    IFS-4662 - Adds method to CategoryController to get all innovation areas excluding 'None'
    - Updates javascript and model populator use the new CategoryController method
      when populating the 'All' sector
    - Refactors endpoint names to use hyphens

--- ifs-data-layer/ifs-data-service/src/main/java/org/innovateuk/ifs/category/controller/CategoryController.java
+++ ifs-data-layer/ifs-data-service/src/main/java/org/innovateuk/ifs/category/controller/CategoryController.java
@@ -4,6 +4,7 @@ import org.innovateuk.ifs.category.resource.InnovationAreaResource;
 import org.innovateuk.ifs.category.resource.InnovationSectorResource;
 import org.innovateuk.ifs.category.resource.ResearchCategoryResource;
 import org.innovateuk.ifs.category.transactional.CategoryService;
+import org.innovateuk.ifs.commons.ZeroDowntime;
 import org.innovateuk.ifs.commons.rest.RestResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +13,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+
+import static org.innovateuk.ifs.util.CollectionFunctions.simpleFilter;
 
 /**
  * Controller for finding generic categories by type or parentId
@@ -22,22 +25,37 @@ public class CategoryController {
     @Autowired
     private CategoryService categoryService;
 
-    @GetMapping("/findInnovationAreas")
+    @ZeroDowntime(reference = "IFS-4662", description = "remove findInnovationAreas")
+    @GetMapping({"/findInnovationAreas", "find-innovation-areas"})
     public RestResult<List<InnovationAreaResource>> findInnovationAreas() {
         return categoryService.getInnovationAreas().toGetResponse();
     }
 
-    @GetMapping("/findInnovationSectors")
+    @GetMapping("/find-innovation-areas-excluding-none")
+    public RestResult<List<InnovationAreaResource>> findInnovationAreasExcludingNone() {
+        return categoryService.getInnovationAreas()
+                .andOnSuccessReturn(CategoryController::filterNoneInnovationArea)
+                .toGetResponse();
+    }
+
+    private static List<InnovationAreaResource> filterNoneInnovationArea(List<InnovationAreaResource> innovationAreaResources) {
+        return simpleFilter(innovationAreaResources, InnovationAreaResource::isNotNone);
+    }
+
+    @ZeroDowntime(reference = "IFS-4662", description = "remove findInnovationSectors")
+    @GetMapping({"/findInnovationSectors", "find-innovation-sectors"})
     public RestResult<List<InnovationSectorResource>> findInnovationSectors() {
         return categoryService.getInnovationSectors().toGetResponse();
     }
 
-    @GetMapping("/findResearchCategories")
+    @ZeroDowntime(reference = "IFS-4662", description = "remove findResearchCategories")
+    @GetMapping({"/findResearchCategories", "find-research-categories"})
     public RestResult<List<ResearchCategoryResource>> findResearchCategories() {
         return categoryService.getResearchCategories().toGetResponse();
     }
 
-    @GetMapping("/findByInnovationSector/{sectorId}")
+    @ZeroDowntime(reference = "IFS-4662", description = "remove findByInnovationSector")
+    @GetMapping({"/findByInnovationSector/{sectorId}", "/find-by-innovation-sector/{sectorId}"})
     public RestResult<List<InnovationAreaResource>> findInnovationAreasBySector(@PathVariable("sectorId") final long sectorId){
         return categoryService.getInnovationAreasBySector(sectorId).toGetResponse();
     }
