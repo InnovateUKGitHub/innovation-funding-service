49d15239fd4a32d22b031cf1d711cdbebc071da8
Nathan Phillips <nathanphillips@MacBook-Pro-5.local>
2018-11-06 16:39:40 +0000

    IFS-2381 disable button test

---
+++ ifs-web-service/ifs-web-core/src/main/resources/static/js/ifs_modules/disableSubmit.test.js
@@ -0,0 +1,24 @@
+describe('test disable submit', () => {
+    beforeAll(() => {
+        // import IFS module
+        IFS = {"core" : {}}
+        jQuery = require('jquery');
+        require("./disableSubmit")
+    })
+
+    test("test updateButton", () => {
+        // build mock button
+        var button = jQuery('<button id="test"></button>');
+
+        // disable button, check attributes
+        IFS.core.disableSubmitUntilChecked.updateButton(button, false);
+        expect(button.attr('aria-disabled')).toBe("true")
+        expect(button.hasClass('disabled')).toBe(true)
+
+        // enable button, check attributes
+        IFS.core.disableSubmitUntilChecked.updateButton(button, true);
+        expect(button.attr('aria-disabled')).toBe(undefined)
+        expect(button.hasClass('disabled')).toBe(false)
+
+    })
+})
