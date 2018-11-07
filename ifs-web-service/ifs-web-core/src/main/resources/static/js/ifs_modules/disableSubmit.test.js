86051c3e6f469645af5e49273549ab589b8149ed
Nathan Phillips <nathanphillips@MacBook-Pro-5.local>
2018-11-07 10:01:46 +0000

    IFS-2831 mirror elements and finance tests

--- ifs-web-service/ifs-web-core/src/main/resources/static/js/ifs_modules/disableSubmit.test.js
+++ ifs-web-service/ifs-web-core/src/main/resources/static/js/ifs_modules/disableSubmit.test.js
@@ -14,11 +14,12 @@ describe('test disable submit', () => {
         IFS.core.disableSubmitUntilChecked.updateButton(button, false);
         expect(button.attr('aria-disabled')).toBe("true")
         expect(button.hasClass('disabled')).toBe(true)
+        expect(button.prop('disabled')).toBe(true)
 
         // enable button, check attributes
         IFS.core.disableSubmitUntilChecked.updateButton(button, true);
         expect(button.attr('aria-disabled')).toBe(undefined)
         expect(button.hasClass('disabled')).toBe(false)
-
+        expect(button.prop('disabled')).toBe(false)
     })
 })
