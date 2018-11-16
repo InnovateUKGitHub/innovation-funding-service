5cedbd71baf82a2fd745fabacd9d0d4026fc0098
Ian Homer <ihomer@worth.systems>
2018-11-16 16:51:08 +0000

    IFS-4688 : Placeholder for log level

--- ifs-auth-service/ifs-idp-service/src/main/docker/scripts/idp-extras.sh
+++ ifs-auth-service/ifs-idp-service/src/main/docker/scripts/idp-extras.sh
@@ -5,6 +5,9 @@
 LOCAL_CONFIG_DIR=/etc/shibboleth/extras
 SHIBBOLETH_CONF_DIR=/etc/shibboleth
 
+# Configuration
+replacePlaceholders /opt/shibboleth-idp/conf/logback.xml IDP_LOG_LEVEL
+
 mkdir /etc/shibboleth/metadata/
 
 # Template IDP / SP entity properties
