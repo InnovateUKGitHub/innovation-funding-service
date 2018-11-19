d1edd4394fdb2e86de15035fc94e37f59ecee6f9
Nathan Phillips <nathanphillips@MacBook-Pro-5.local>
2018-11-19 11:37:23 +0000

    IFS-2831 removed tabs

--- ifs-web-service/ifs-web-core/src/main/resources/static/js/ifs_modules/wordCount.test.js
+++ ifs-web-service/ifs-web-core/src/main/resources/static/js/ifs_modules/wordCount.test.js
@@ -7,14 +7,14 @@ describe('test wordCount', () => {
     })
 
     test("test updateWordCount", () => {
-    	// a template for quickly creating different size textareas
-    	var textAreaWrapperTemplate = `<div class="word-count">
-        	<textarea data-max_words="<MAX>" data-maxwordslength="<MAX>" data-maxwordslength-errormessage="TEST1234">
-        		<TEXT>
-        	</textarea>
-        	<span class="count-label">
-            	<span class="count-down positive"></span>
-        	</span>
+        // a template for quickly creating different size textareas
+        var textAreaWrapperTemplate = `<div class="word-count">
+            <textarea data-max_words="<MAX>" data-maxwordslength="<MAX>" data-maxwordslength-errormessage="TEST1234">
+                <TEXT>
+            </textarea>
+            <span class="count-label">
+                <span class="count-down positive"></span>
+            </span>
         </div>`
 
         // max = 10, 2 words, error message = "Words remaining: 8"
@@ -22,21 +22,21 @@ describe('test wordCount', () => {
         var textarea = textAreaWrapper.find('textarea')                
         IFS.core.wordCount.updateWordCount(textarea[0])
         expect(textAreaWrapper.find('.count-down').text()).toBe("Words remaining: 8")
-		expect(textAreaWrapper.find('.count-down').hasClass("negative")).toBe(false)
+        expect(textAreaWrapper.find('.count-down').hasClass("negative")).toBe(false)
 
         // max = 7, 3 words, error message = "Words remaining: 4"
         var textAreaWrapper = jQuery(textAreaWrapperTemplate.replace("<MAX>", "7").replace("<TEXT>", "a-b test test"));
         var textarea = textAreaWrapper.find('textarea')
         IFS.core.wordCount.updateWordCount(textarea[0])
         expect(textAreaWrapper.find('.count-down').text()).toBe("Words remaining: 4")
-		expect(textAreaWrapper.find('.count-down').hasClass("negative")).toBe(false)
+        expect(textAreaWrapper.find('.count-down').hasClass("negative")).toBe(false)
 
         // max = 5, 8 words, error message = "Words remaining: -3" class becomes negative
         var textAreaWrapper = jQuery(textAreaWrapperTemplate.replace("<MAX>", "5").replace("<TEXT>", "a b c d e f g h"));
         var textarea = textAreaWrapper.find('textarea')
         IFS.core.wordCount.updateWordCount(textarea[0])
         expect(textAreaWrapper.find('.count-down').text()).toBe("Words remaining: -3")
-		expect(textAreaWrapper.find('.count-down').hasClass("negative")).toBe(true)
+        expect(textAreaWrapper.find('.count-down').hasClass("negative")).toBe(true)
 
     })
 
