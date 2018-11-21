3d4db4b6aab94eba944b4fbf7af49d759d48303b
Ian Homer <ihomer@worth.systems>
2018-11-21 16:12:26 +0000

    IFS-4421 : Correct grant service unit test

--- ifs-data-layer/ifs-data-service/src/test/java/org/innovateuk/ifs/grant/service/GrantServiceImplTest.java
+++ ifs-data-layer/ifs-data-service/src/test/java/org/innovateuk/ifs/grant/service/GrantServiceImplTest.java
@@ -2,6 +2,8 @@ package org.innovateuk.ifs.grant.service;
 
 import org.innovateuk.ifs.BaseServiceUnitTest;
 import org.innovateuk.ifs.LambdaMatcher;
+import org.innovateuk.ifs.application.domain.FormInputResponse;
+import org.innovateuk.ifs.application.repository.FormInputResponseRepository;
 import org.innovateuk.ifs.commons.service.ServiceResult;
 import org.innovateuk.ifs.project.core.domain.Project;
 import org.innovateuk.ifs.project.core.repository.ProjectRepository;
@@ -10,9 +12,12 @@ import org.innovateuk.ifs.sil.grant.service.GrantEndpoint;
 import org.junit.Test;
 import org.mockito.Mock;
 
+import java.util.ArrayList;
 import java.util.function.Predicate;
 
 import static org.hamcrest.CoreMatchers.equalTo;
+import static org.innovateuk.ifs.application.builder.ApplicationBuilder.newApplication;
+import static org.innovateuk.ifs.competition.builder.CompetitionBuilder.newCompetition;
 import static org.innovateuk.ifs.project.core.builder.ProjectBuilder.newProject;
 import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.verify;
@@ -26,6 +31,9 @@ public class GrantServiceImplTest extends BaseServiceUnitTest<GrantServiceImpl> 
     private ProjectRepository projectRepository;
 
     @Mock
+    private FormInputResponseRepository formInputResponseRepository;
+
+    @Mock
     private GrantEndpoint grantEndpoint;
 
     @Override
@@ -36,8 +44,20 @@ public class GrantServiceImplTest extends BaseServiceUnitTest<GrantServiceImpl> 
     @Test
     public void testSend() {
         long applicationId = 1L;
-        Project project = newProject().withId(applicationId).build();
+        long competitionId = 2L;
+        Project project = newProject()
+                .withId(applicationId)
+                .withDuration(12L)
+                .withApplication(
+                        newApplication()
+                                .withCompetition(
+                                        newCompetition().withId(competitionId).build())
+                                .build())
+                .build();
         when(projectRepository.findOneByApplicationId(applicationId)).thenReturn(project);
+        when(formInputResponseRepository.findByApplicationId(applicationId))
+                .thenReturn(new ArrayList<>());
+
         ServiceResult<Void> result = service.sendProject(applicationId);
         assertThat(result.isSuccess(), equalTo(true));
         verify(grantEndpoint).send(LambdaMatcher.createLambdaMatcher(matchGrant(project)));
