a6bb5b23117931addb8afcc4670cc02ef99cdb4f
rmohammed999 <rmohammed@worth.systems>
2018-11-29 11:22:35 +0000

    IFS-4288 Added feature and tests

--- ifs-web-service/ifs-competition-mgt-service/src/main/java/org/innovateuk/ifs/competitionsetup/stakeholder/controller/CompetitionSetupStakeholderController.java
+++ ifs-web-service/ifs-competition-mgt-service/src/main/java/org/innovateuk/ifs/competitionsetup/stakeholder/controller/CompetitionSetupStakeholderController.java
@@ -11,6 +11,7 @@ import org.innovateuk.ifs.competitionsetup.stakeholder.populator.ManageStakehold
 import org.innovateuk.ifs.controller.ValidationHandler;
 import org.innovateuk.ifs.invite.resource.InviteUserResource;
 import org.innovateuk.ifs.user.resource.UserResource;
+import org.innovateuk.ifs.user.service.UserRestService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
@@ -24,6 +25,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.validation.Valid;
+import java.util.List;
+import java.util.Optional;
 import java.util.function.Supplier;
 
 import static org.innovateuk.ifs.commons.error.CommonFailureKeys.STAKEHOLDER_INVITE_INVALID_EMAIL;
@@ -58,6 +61,9 @@ public class CompetitionSetupStakeholderController {
     @Autowired
     private ManageStakeholderModelPopulator manageStakeholderModelPopulator;
 
+    @Autowired
+    private UserRestService userRestService;
+
     @PreAuthorize("hasPermission(#competitionId, 'org.innovateuk.ifs.competition.resource.CompetitionCompositeId', 'MANAGE_STAKEHOLDERS')")
     @GetMapping("/{competitionId}/manage-stakeholders")
     public String manageStakeholders(@PathVariable(COMPETITION_ID_KEY) long competitionId,
@@ -89,6 +95,14 @@ public class CompetitionSetupStakeholderController {
                                     @Valid @ModelAttribute(FORM_ATTR_NAME) InviteStakeholderForm form,
                                     @SuppressWarnings("unused") BindingResult bindingResult, ValidationHandler validationHandler) {
 
+        Optional<UserResource> userResource = userRestService.findUserByEmail(form.getEmailAddress()).getOptionalSuccessObject();
+
+        String userEmail = form.getEmailAddress().toLowerCase();
+
+        if(userResource.isPresent() && !isInternal(userEmail) && !isUserAlreadyStakeholder(competitionId, userResource.get())){
+            return addStakeholder(competitionId, userResource.get().getId(), model);
+        }
+
         Supplier<String> failureView = () -> doViewManageStakeholders(competitionId, model, form, tab);
 
         form.setVisible(true);
@@ -103,20 +117,6 @@ public class CompetitionSetupStakeholderController {
                     failNowOrSucceedWith(failureView, () -> "redirect:/competition/setup/" + competitionId + "/manage-stakeholders?tab=" + tab);
 
         });
-    }
-
-    private ValidationHandler handleInviteStakeholderErrors(RestResult<Void> saveResult, ValidationHandler validationHandler) {
-        return validationHandler.addAnyErrors(saveResult, mappingErrorKeyToField(STAKEHOLDER_INVITE_INVALID_EMAIL, "emailAddress"), fieldErrorsToFieldErrors(), asGlobalErrors());
-    }
-
-    private InviteUserResource constructInviteUserResource(InviteStakeholderForm form) {
-
-        UserResource invitedUser = new UserResource();
-        invitedUser.setFirstName(form.getFirstName());
-        invitedUser.setLastName(form.getLastName());
-        invitedUser.setEmail(form.getEmailAddress());
-
-        return new InviteUserResource(invitedUser);
     }
 
     @PostMapping(value = "/{competitionId}/manage-stakeholders", params = {"addStakeholder"})
@@ -135,5 +135,28 @@ public class CompetitionSetupStakeholderController {
 
         competitionSetupStakeholderRestService.removeStakeholder(competitionId, stakeholderUserId);
         return "redirect:/competition/setup/" + competitionId + "/manage-stakeholders?tab=" + ADDED_TAB;
+    }
+
+    private boolean isUserAlreadyStakeholder(long competitionId, UserResource userResource) {
+        Optional<List<UserResource>> optionalSuccessObject = competitionSetupStakeholderRestService.findStakeholders(competitionId).getOptionalSuccessObject();
+        return optionalSuccessObject.get().contains(userResource);
+    }
+
+    private boolean isInternal(String emailAddress) {
+        return (emailAddress.contains("@innovate.ukri.org") || emailAddress.contains("@innovateuk.gov.uk") || /*for testing only*/ emailAddress.contains("@innovateuk.test"));
+    }
+
+    private ValidationHandler handleInviteStakeholderErrors(RestResult<Void> saveResult, ValidationHandler validationHandler) {
+        return validationHandler.addAnyErrors(saveResult, mappingErrorKeyToField(STAKEHOLDER_INVITE_INVALID_EMAIL, "emailAddress"), fieldErrorsToFieldErrors(), asGlobalErrors());
+    }
+
+    private InviteUserResource constructInviteUserResource(InviteStakeholderForm form) {
+
+        UserResource invitedUser = new UserResource();
+        invitedUser.setFirstName(form.getFirstName());
+        invitedUser.setLastName(form.getLastName());
+        invitedUser.setEmail(form.getEmailAddress());
+
+        return new InviteUserResource(invitedUser);
     }
 }
--- ifs-web-service/ifs-competition-mgt-service/src/test/java/org/innovateuk/ifs/competitionsetup/stakeholder/controller/CompetitionSetupStakeholderControllerTest.java
+++ ifs-web-service/ifs-competition-mgt-service/src/test/java/org/innovateuk/ifs/competitionsetup/stakeholder/controller/CompetitionSetupStakeholderControllerTest.java
@@ -9,23 +9,28 @@ import org.innovateuk.ifs.competitionsetup.core.service.CompetitionSetupService;
 import org.innovateuk.ifs.competitionsetup.stakeholder.form.InviteStakeholderForm;
 import org.innovateuk.ifs.competitionsetup.stakeholder.populator.ManageStakeholderModelPopulator;
 import org.innovateuk.ifs.competitionsetup.stakeholder.viewmodel.ManageStakeholderViewModel;
+import org.innovateuk.ifs.user.resource.UserResource;
+import org.innovateuk.ifs.user.service.UserRestService;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import static java.util.Collections.*;
+import static java.util.Collections.emptyList;
+import static java.util.Collections.singletonList;
+import static junit.framework.TestCase.assertTrue;
+import static org.innovateuk.ifs.commons.error.CommonFailureKeys.GENERAL_UNEXPECTED_ERROR;
 import static org.innovateuk.ifs.commons.error.CommonFailureKeys.STAKEHOLDER_INVITE_INVALID_EMAIL;
 import static org.innovateuk.ifs.commons.rest.RestResult.restFailure;
 import static org.innovateuk.ifs.commons.rest.RestResult.restSuccess;
+import static org.innovateuk.ifs.user.builder.UserResourceBuilder.newUserResource;
+import static org.junit.Assert.assertEquals;
 import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.*;
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 /**
  * Class for testing public functions of {@link CompetitionSetupStakeholderController}
@@ -46,8 +51,13 @@ public class CompetitionSetupStakeholderControllerTest extends BaseControllerMoc
     @Mock
     private CompetitionSetupStakeholderRestService competitionSetupStakeholderRestService;
 
+    @Mock
+    private UserRestService userRestService;
+
     @Override
-    protected CompetitionSetupStakeholderController supplyControllerUnderTest() { return new CompetitionSetupStakeholderController(); }
+    protected CompetitionSetupStakeholderController supplyControllerUnderTest() {
+        return new CompetitionSetupStakeholderController();
+    }
 
     @Override
     @Before
@@ -87,20 +97,24 @@ public class CompetitionSetupStakeholderControllerTest extends BaseControllerMoc
 
         String tab = "add";
 
-        when(competitionSetupStakeholderRestService.inviteStakeholder(any(), eq(COMPETITION_ID)))
-                .thenReturn(restFailure(STAKEHOLDER_INVITE_INVALID_EMAIL));
-
         String competitionName = "competitionName";
+
         CompetitionResource competitionResource = CompetitionResourceBuilder.newCompetitionResource()
                 .withId(COMPETITION_ID)
                 .withName(competitionName)
                 .build();
 
+        ManageStakeholderViewModel viewModel = new ManageStakeholderViewModel(COMPETITION_ID, competitionName,
+                emptyList(), emptyList(), emptyList(), tab);
+
+        when(userRestService.findUserByEmail(any())).thenReturn(restFailure(GENERAL_UNEXPECTED_ERROR));
+
+        when(competitionSetupStakeholderRestService.inviteStakeholder(any(), eq(COMPETITION_ID)))
+                .thenReturn(restFailure(STAKEHOLDER_INVITE_INVALID_EMAIL));
+
         when(competitionRestService.getCompetitionById(COMPETITION_ID))
                 .thenReturn(restSuccess(competitionResource));
 
-        ManageStakeholderViewModel viewModel = new ManageStakeholderViewModel(COMPETITION_ID, competitionName,
-                emptyList(), emptyList(), emptyList(), tab);
         when(manageStakeholderModelPopulator.populateModel(competitionResource, tab))
                 .thenReturn(viewModel);
 
@@ -116,6 +130,8 @@ public class CompetitionSetupStakeholderControllerTest extends BaseControllerMoc
     public void inviteStakeholderSuccess() throws Exception {
 
         when(competitionSetupStakeholderRestService.inviteStakeholder(any(), eq(COMPETITION_ID))).thenReturn(restSuccess());
+
+        when(userRestService.findUserByEmail(any())).thenReturn(restFailure(GENERAL_UNEXPECTED_ERROR));
 
         mockMvc.perform(MockMvcRequestBuilders.post("/competition/setup/{competitionId}/manage-stakeholders?inviteStakeholder=inviteStakeholder", COMPETITION_ID).
                 param("firstName", "First").
@@ -135,7 +151,7 @@ public class CompetitionSetupStakeholderControllerTest extends BaseControllerMoc
         when(competitionSetupStakeholderRestService.addStakeholder(COMPETITION_ID, stakeholderUserId)).thenReturn(restSuccess());
 
         mockMvc.perform(MockMvcRequestBuilders.post("/competition/setup/{competitionId}/manage-stakeholders?addStakeholder=addStakeholder&stakeholderUserId={stakeholderUserId}", COMPETITION_ID, stakeholderUserId)
-                )
+        )
                 .andExpect(status().is3xxRedirection())
                 .andExpect(view().name("redirect:/competition/setup/" + COMPETITION_ID + "/manage-stakeholders?tab=add"));
 
@@ -147,7 +163,7 @@ public class CompetitionSetupStakeholderControllerTest extends BaseControllerMoc
 
         long stakeholderUserId = 2L;
 
-        when(competitionSetupStakeholderRestService.removeStakeholder(COMPETITION_ID, stakeholderUserId )).thenReturn(restSuccess());
+        when(competitionSetupStakeholderRestService.removeStakeholder(COMPETITION_ID, stakeholderUserId)).thenReturn(restSuccess());
 
         mockMvc.perform(MockMvcRequestBuilders.post("/competition/setup/{competitionId}/manage-stakeholders?removeStakeholder=removeStakeholder&stakeholderUserId={stakeholderUserId}", COMPETITION_ID, stakeholderUserId)
         )
@@ -155,6 +171,129 @@ public class CompetitionSetupStakeholderControllerTest extends BaseControllerMoc
                 .andExpect(view().name("redirect:/competition/setup/" + COMPETITION_ID + "/manage-stakeholders?tab=added"));
 
         verify(competitionSetupStakeholderRestService).removeStakeholder(COMPETITION_ID, stakeholderUserId);
+    }
+
+    @Test
+    public void inviteExistingUserAsStakeholder() throws Exception {
+        UserResource user = newUserResource()
+                .withId(1l)
+                .withFirstName("First")
+                .withLastName("Last")
+                .withEmail("user@test.com")
+                .build();
+        when(competitionSetupStakeholderRestService.findStakeholders(COMPETITION_ID)).thenReturn(restSuccess(emptyList()));
+        when(userRestService.findUserByEmail(any())).thenReturn(restSuccess(user));
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/competition/setup/{competitionId}/manage-stakeholders?inviteStakeholder=inviteStakeholder", COMPETITION_ID).
+                param("firstName", user.getFirstName()).
+                param("lastName", user.getLastName()).
+                param("emailAddress", user.getEmail()))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/competition/setup/" + COMPETITION_ID + "/manage-stakeholders?tab=add"));
+
+        verify(competitionSetupStakeholderRestService).addStakeholder(COMPETITION_ID, user.getId());
+        verify(userRestService).findUserByEmail(user.getEmail());
+    }
+
+    @Test
+    public void inviteInternalUserAsStakeholder() throws Exception {
+
+        String tab = "add";
+
+        UserResource user = newUserResource()
+                .withId(1l)
+                .withFirstName("First")
+                .withLastName("Last")
+                .withEmail("user@innovate.ukri.org")
+                .build();
+
+        String competitionName = "competitionName";
+        CompetitionResource competitionResource = CompetitionResourceBuilder.newCompetitionResource()
+                .withId(COMPETITION_ID)
+                .withName(competitionName)
+                .build();
+
+        ManageStakeholderViewModel viewModel = new ManageStakeholderViewModel(COMPETITION_ID, competitionName,
+                emptyList(), emptyList(), emptyList(), tab);
+
+        when(userRestService.findUserByEmail(user.getEmail())).thenReturn(restSuccess(user));
+
+        when(competitionSetupStakeholderRestService.inviteStakeholder(any(), eq(COMPETITION_ID)))
+                .thenReturn(restFailure(STAKEHOLDER_INVITE_INVALID_EMAIL));
+
+        when(competitionRestService.getCompetitionById(COMPETITION_ID))
+                .thenReturn(restSuccess(competitionResource));
+
+        when(manageStakeholderModelPopulator.populateModel(competitionResource, tab))
+                .thenReturn(viewModel);
+
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.post("/competition/setup/{competitionId}/manage-stakeholders?inviteStakeholder=inviteStakeholder", COMPETITION_ID).
+                param("firstName", user.getFirstName()).
+                param("lastName", user.getLastName()).
+                param("emailAddress", user.getEmail()))
+                .andReturn();
+
+        InviteStakeholderForm form = (InviteStakeholderForm) result.getModelAndView().getModel().get("form");
+
+        assertEquals(200, result.getResponse().getStatus());
+        assertEquals("competition/setup/manage-stakeholders", result.getModelAndView().getViewName());
+        assertTrue(form.getBindingResult().hasErrors());
+
+        verify(userRestService).findUserByEmail(user.getEmail());
+        verify(competitionSetupStakeholderRestService).inviteStakeholder(any(), eq(COMPETITION_ID));
+        verify(competitionRestService).getCompetitionById(COMPETITION_ID);
+        verify(manageStakeholderModelPopulator).populateModel(competitionResource, tab);
+    }
+
+    @Test
+    public void inviteStakeholderAlreadyOnTheCompetition() throws Exception {
+        String tab = "add";
+
+        UserResource user = newUserResource()
+                .withId(1l)
+                .withFirstName("First")
+                .withLastName("Last")
+                .withEmail("user@innovate.ukri.org")
+                .build();
+
+        String competitionName = "competitionName";
+        CompetitionResource competitionResource = CompetitionResourceBuilder.newCompetitionResource()
+                .withId(COMPETITION_ID)
+                .withName(competitionName)
+                .build();
+
+        ManageStakeholderViewModel viewModel = new ManageStakeholderViewModel(COMPETITION_ID, competitionName,
+                emptyList(), emptyList(), emptyList(), tab);
+
+        when(userRestService.findUserByEmail(user.getEmail())).thenReturn(restSuccess(user));
+
+        when(competitionSetupStakeholderRestService.findStakeholders(COMPETITION_ID)).thenReturn(restSuccess(singletonList(user)));
+
+        when(competitionSetupStakeholderRestService.inviteStakeholder(any(), eq(COMPETITION_ID)))
+                .thenReturn(restFailure(STAKEHOLDER_INVITE_INVALID_EMAIL));
+
+        when(competitionRestService.getCompetitionById(COMPETITION_ID))
+                .thenReturn(restSuccess(competitionResource));
+
+        when(manageStakeholderModelPopulator.populateModel(competitionResource, tab))
+                .thenReturn(viewModel);
+
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.post("/competition/setup/{competitionId}/manage-stakeholders?inviteStakeholder=inviteStakeholder", COMPETITION_ID).
+                param("firstName", user.getFirstName()).
+                param("lastName", user.getLastName()).
+                param("emailAddress", user.getEmail()))
+                .andReturn();
+
+        InviteStakeholderForm form = (InviteStakeholderForm) result.getModelAndView().getModel().get("form");
+
+        assertEquals(200, result.getResponse().getStatus());
+        assertEquals("competition/setup/manage-stakeholders", result.getModelAndView().getViewName());
+        assertTrue(form.getBindingResult().hasErrors());
+
+        verify(userRestService).findUserByEmail(user.getEmail());
+        verify(competitionSetupStakeholderRestService).inviteStakeholder(any(), eq(COMPETITION_ID));
+        verify(competitionRestService).getCompetitionById(COMPETITION_ID);
+        verify(manageStakeholderModelPopulator).populateModel(competitionResource, tab);
     }
 }
 
