01505797740fb8869805531191c6d75512fc5289
Kieran Hester <khester@worth.systems>
2018-12-05 16:35:30 +0000

    IFS-4765 update script

--- ifs-data-layer/ifs-data-service/src/main/resources/db/migration/V135_1_1__Add_unique_constraint_process_role_table.sql
+++ ifs-data-layer/ifs-data-service/src/main/resources/db/migration/V135_1_1__Add_unique_constraint_process_role_table.sql
@@ -26,18 +26,49 @@ INSERT INTO process_roles_to_delete
 
 -- 3. Merge question_status marked_as_complete_by_id into new ids
 UPDATE question_status qs
-    JOIN process_role delete_role ON qs.marked_as_complete_by_id = delete_role.id
-    JOIN process_role join_role ON join_role.application_id=delete_role.application_id AND join_role.user_id=delete_role.user_id AND join_role.role_id=delete_role.role_id AND join_role.id != delete_role.id
-    SET qs.marked_as_complete_by_id = join_role.id
-WHERE marked_as_complete_by_id IN (SELECT * FROM process_roles_to_delete);
+       JOIN process_role delete_role
+         ON qs.marked_as_complete_by_id = delete_role.id
+       JOIN process_role join_role
+         ON join_role.application_id = delete_role.application_id
+            AND join_role.user_id = delete_role.user_id
+            AND join_role.role_id = delete_role.role_id
+            AND join_role.id != delete_role.id
+SET    qs.marked_as_complete_by_id = join_role.id
+WHERE  marked_as_complete_by_id IN (SELECT *
+                                    FROM   process_roles_to_delete);
 
+-- 4. Merge question_status assigned_by_id into new ids
+UPDATE question_status qs
+       JOIN process_role delete_role
+         ON qs.assigned_by_id = delete_role.id
+       JOIN process_role join_role
+         ON join_role.application_id = delete_role.application_id
+            AND join_role.user_id = delete_role.user_id
+            AND join_role.role_id = delete_role.role_id
+            AND join_role.id != delete_role.id
+SET    qs.assigned_by_id = join_role.id
+WHERE  assigned_by_id IN (SELECT *
+                          FROM   process_roles_to_delete);
 
--- 4. Delete duplicates
+-- 5. Merge question_status assignee_id into new ids
+UPDATE question_status qs
+       JOIN process_role delete_role
+         ON qs.assignee_id = delete_role.id
+       JOIN process_role join_role
+         ON join_role.application_id = delete_role.application_id
+            AND join_role.user_id = delete_role.user_id
+            AND join_role.role_id = delete_role.role_id
+            AND join_role.id != delete_role.id
+SET    qs.assignee_id = join_role.id
+WHERE  assignee_id IN (SELECT *
+                       FROM   process_roles_to_delete);
+
+-- 6. Delete duplicates
 DELETE FROM process_role WHERE id IN (SELECT * FROM process_roles_to_delete);
 
--- 4. Drop temporary table
+-- 7. Drop temporary table
 DROP TABLE process_roles_to_delete;
 
--- 6. Add unique constraint to process_role rows
+-- 8. Add unique constraint to process_role rows
 ALTER TABLE process_role
 ADD CONSTRAINT `UC_process_role_unique_row` UNIQUE (`application_id`, `role_id`, `user_id`, `organisation_id`);
