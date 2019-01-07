724c78eb3681474b6450d9a8073476d622acfd15
Tom Baldwin <tom@polymorphism.co.uk>
2019-01-07 20:12:18 +0000

    IFS-4998 - Removes funding type check from public_content triggers so that null values are correctly copied to competition

--- ifs-data-layer/ifs-data-service/src/main/resources/db/migration/V136_1_1__Add_funding_type_to_competition.sql
+++ ifs-data-layer/ifs-data-service/src/main/resources/db/migration/V136_1_1__Add_funding_type_to_competition.sql
@@ -3,11 +3,11 @@ ALTER TABLE competition ADD COLUMN funding_type enum('GRANT','LOAN','PROCUREMENT
 -- triggers to sync public_content.funding_type -> competition.funding_type
 CREATE TRIGGER insert_public_content_to_competition AFTER INSERT ON public_content
 FOR EACH ROW
-  UPDATE competition c SET c.funding_type = NEW.funding_type WHERE c.id = NEW.competition_id AND c.funding_type <> NEW.funding_type;
+  UPDATE competition c SET c.funding_type = NEW.funding_type WHERE c.id = NEW.competition_id;
 
 CREATE TRIGGER update_public_content_to_competition AFTER UPDATE ON public_content
 FOR EACH ROW
-  UPDATE competition c SET c.funding_type = NEW.funding_type WHERE c.id = NEW.competition_id AND c.funding_type <> NEW.funding_type;
+  UPDATE competition c SET c.funding_type = NEW.funding_type WHERE c.id = NEW.competition_id;
 
 UPDATE competition c SET funding_type = (SELECT funding_type FROM public_content pc WHERE pc.competition_id = c.id);
 
