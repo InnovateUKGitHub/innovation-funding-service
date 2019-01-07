347d2bcfc16a25049c98edfe4a077ef34bf19fd1
Tom Baldwin <tom@polymorphism.co.uk>
2019-01-07 20:33:42 +0000

    IFS-4998 - Fixes trigger test in PublicContentRepositoryIntegrationTest and adds test for null fundingTypes.

--- ifs-data-layer/ifs-data-service/src/test/java/org/innovateuk/ifs/publiccontent/repository/PublicContentRepositoryIntegrationTest.java
+++ ifs-data-layer/ifs-data-service/src/test/java/org/innovateuk/ifs/publiccontent/repository/PublicContentRepositoryIntegrationTest.java
@@ -7,13 +7,15 @@ import org.innovateuk.ifs.publiccontent.domain.PublicContent;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import static org.innovateuk.ifs.competition.builder.CompetitionBuilder.newCompetition;
-
 import static org.innovateuk.ifs.base.amend.BaseBuilderAmendFunctions.id;
+import static org.innovateuk.ifs.competition.builder.CompetitionBuilder.newCompetition;
+import static org.innovateuk.ifs.competition.publiccontent.resource.FundingType.GRANT;
+import static org.innovateuk.ifs.competition.publiccontent.resource.FundingType.PROCUREMENT;
 import static org.innovateuk.ifs.publiccontent.builder.PublicContentBuilder.newPublicContent;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 
+// TODO IFS-4982 remove along with the triggers
 public class PublicContentRepositoryIntegrationTest extends BaseRepositoryIntegrationTest<PublicContentRepository> {
 
     @Autowired
@@ -26,18 +28,16 @@ public class PublicContentRepositoryIntegrationTest extends BaseRepositoryIntegr
     private CompetitionRepository competitionRepository;
 
     @Test
-    // TODO IFS-4982 remove along with the triggers
     public void testDbUpdateTrigger() {
         loginIfsAdmin();
-        Competition competition = newCompetition().with(id(null)).build();
+        Competition competition = newCompetition().withFundingType(GRANT).with(id(null)).build();
         competitionRepository.save(competition);
         flushAndClearSession();
-
-        assertNull(competition.getFundingType());
 
         PublicContent publicContent = newPublicContent()
                 .with(id(null))
                 .withCompetitionId(competition.getId())
+                .withFundingType(PROCUREMENT)
                 .build();
         repository.save(publicContent);
         flushAndClearSession();
@@ -45,5 +45,25 @@ public class PublicContentRepositoryIntegrationTest extends BaseRepositoryIntegr
         Competition actual = competitionRepository.findById(competition.getId());
 
         assertEquals(publicContent.getFundingType(), actual.getFundingType());
+    }
+
+    @Test
+    public void testDbUpdateTrigger_null() {
+        loginIfsAdmin();
+        Competition competition = newCompetition().withFundingType(GRANT).with(id(null)).build();
+        competitionRepository.save(competition);
+        flushAndClearSession();
+
+        PublicContent publicContent = newPublicContent()
+                .with(id(null))
+                .withCompetitionId(competition.getId())
+                .withFundingType(null)
+                .build();
+        repository.save(publicContent);
+        flushAndClearSession();
+
+        Competition actual = competitionRepository.findById(competition.getId());
+
+        assertNull(actual.getFundingType());
     }
 }
