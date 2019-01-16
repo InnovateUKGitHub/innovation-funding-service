3d667bbd8a8edc6c74d7289ebe8e33a81297abe4
Kieran Hester <khester@worth.systems>
2019-01-16 16:23:40 +0000

    IFS-3638 remove file outputs

--- setup-files/scripts/docker/clean-migrate-db.sh
+++ setup-files/scripts/docker/clean-migrate-db.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 # R Popple: Clean down the database and run all the flyway migrations against it.
 echo "Cleaning and migrating the database"
-
-cat /flyway/flyway.conf
-
-echo "end of file"
 flyway clean
 flyway migrate
 flyway info
