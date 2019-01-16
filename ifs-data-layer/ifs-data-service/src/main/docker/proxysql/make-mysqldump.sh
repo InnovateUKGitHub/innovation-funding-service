9298847cbabd30e7cfd8755d695d63a25b32f71d
Kieran Hester <khester@worth.systems>
2019-01-16 20:23:36 +0000

    IFS-5038 skip network timeout

--- ifs-data-layer/ifs-data-service/src/main/docker/proxysql/make-mysqldump.sh
+++ ifs-data-layer/ifs-data-service/src/main/docker/proxysql/make-mysqldump.sh
@@ -2,7 +2,7 @@
 set -e
 
 # take the data dump
-mysqldump -u$DB_USER -p$DB_PASS -h127.0.0.1 -P6033 $DB_NAME --single-transaction --debug-info --extended-insert --ignore-table=$DB_NAME._ApplicationSetUp --ignore-table=$DB_NAME._ApplicationSetUp_CapUse --ignore-table=$DB_NAME._ApplicationSetUp_Value --ignore-table=$DB_NAME._tempIFS --ignore-table=$DB_NAME._tempIFS_AssScore --ignore-table=$DB_NAME._tempIFS_CapUse > /dump/anonymised-dump.sql
+mysqldump -u$DB_USER -p$DB_PASS -h127.0.0.1 -P6033 $DB_NAME --single-transaction --debug-info --extended-insert --skip-network-timeout --ignore-table=$DB_NAME._ApplicationSetUp --ignore-table=$DB_NAME._ApplicationSetUp_CapUse --ignore-table=$DB_NAME._ApplicationSetUp_Value --ignore-table=$DB_NAME._tempIFS --ignore-table=$DB_NAME._tempIFS_AssScore --ignore-table=$DB_NAME._tempIFS_CapUse > /dump/anonymised-dump.sql
 
 # encrypt the dump file before transit and finally remove the unprotected file
 gpg --yes --batch --passphrase=$DB_ANON_PASS -c /dump/anonymised-dump.sql
