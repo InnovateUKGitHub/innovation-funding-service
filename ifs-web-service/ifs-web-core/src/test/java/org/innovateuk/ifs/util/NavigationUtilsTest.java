7d9877c287e0ca16f07c3f0e666f5bd232a9cdbb
Grace Williams <grace.williams@innovateuk.gov.uk>
2019-01-28 13:05:35 +0000

    IFS-4959 Additional test added for NavigationUtils

--- ifs-web-service/ifs-web-core/src/test/java/org/innovateuk/ifs/util/NavigationUtilsTest.java
+++ ifs-web-service/ifs-web-core/src/test/java/org/innovateuk/ifs/util/NavigationUtilsTest.java
@@ -3,29 +3,46 @@ package org.innovateuk.ifs.util;
 import org.innovateuk.ifs.user.resource.Role;
 import org.junit.Test;
 
+import javax.servlet.http.HttpServletRequest;
+
 import static junit.framework.TestCase.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class NavigationUtilsTest {
 
     @Test
-    public void getRedirectToDashboardUrlForApplicantRole(){
+    public void getRedirectToDashboardUrlForApplicantRole() throws Exception{
 
         Role applicant = Role.APPLICANT;
         assertEquals("redirect:/applicant/dashboard", new NavigationUtils().getRedirectToDashboardUrlForRole(applicant));
     }
 
     @Test
-    public void getRedirectToDashboardUrlForAssessorRole(){
+    public void getRedirectToDashboardUrlForAssessorRole() throws Exception{
 
         Role assessor = Role.ASSESSOR;
         assertEquals("redirect:/assessment/assessor/dashboard", new NavigationUtils().getRedirectToDashboardUrlForRole(assessor));
     }
 
     @Test
-    public void getRedirectToDashboardUrlForStakeholderRole(){
+    public void getRedirectToDashboardUrlForStakeholderRole() throws Exception{
 
         Role stakeholder = Role.STAKEHOLDER;
         assertEquals("redirect:/management/dashboard", new NavigationUtils().getRedirectToDashboardUrlForRole(stakeholder));
+    }
+
+    @Test
+    public void getRedirectToLandingPageUrl() throws Exception{
+
+        HttpServletRequest request = mock(HttpServletRequest.class);
+        when(request.getScheme()).thenReturn("https");
+        when(request.getServerName()).thenReturn("site");
+        when(request.getServerPort()).thenReturn(8080);
+
+        //String.format("redirect:%s://%s:%s"
+        assertEquals("redirect:https://site:8080", new NavigationUtils().getRedirectToLandingPageUrl(request));
+
     }
 
 }
