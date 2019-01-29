0bf17fa421802125c11d1d3be1f77772b54c9bc5
Kieran Hester <khester@worth.systems>
2019-01-29 08:12:14 +0000

    IFS-3638 remove unused import

--- ifs-registration-service/src/main/java/org/innovateuk/ifs/shibboleth/api/LdapProperties.java
+++ ifs-registration-service/src/main/java/org/innovateuk/ifs/shibboleth/api/LdapProperties.java
@@ -1,7 +1,6 @@
 package org.innovateuk.ifs.shibboleth.api;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
 import javax.validation.constraints.NotNull;
