def2cea6acada7d4cd486dfc7395cfecf46a2e42
Adam Mapstone <adam.mapstone@innovateuk.gov.uk>
2019-01-30 16:41:43 +0000

    IFS-4857 - Comment removed.

--- ifs-data-layer/ifs-data-service/src/main/java/org/innovateuk/ifs/invite/repository/UserInviteRepository.java
+++ ifs-data-layer/ifs-data-service/src/main/java/org/innovateuk/ifs/invite/repository/UserInviteRepository.java
@@ -5,11 +5,6 @@ import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
-/**
- * Base repository to extend to access {@link Invite} based queries.
- *
- * This does not include any queries against the target or owner entities.
- */
 public interface UserInviteRepository extends CrudRepository<Invite, Long> {
 
     List<Invite> findByEmail(String email);
