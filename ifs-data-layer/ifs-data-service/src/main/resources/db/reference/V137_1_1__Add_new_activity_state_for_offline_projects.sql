46ea18c68c8fec5f982b3507708e492ec64b0600
Val Frost <valfrost10@gmail.com>
2019-02-11 10:02:05 +0000

    IFS-5110 Addressing review comments

--- ifs-data-layer/ifs-data-service/src/main/resources/db/reference/V137_1_1__Add_new_activity_state_for_offline_projects.sql
+++ ifs-data-layer/ifs-data-service/src/main/resources/db/reference/V137_1_1__Add_new_activity_state_for_offline_projects.sql
@@ -1,5 +1,5 @@
  -- IFS-5110 Create a state for projects to be handled and completed offline
 ALTER TABLE `activity_state` MODIFY COLUMN `state` enum('CREATED','PENDING','REJECTED','ACCEPTED','WITHDRAWN','OPEN','READY_TO_SUBMIT','SUBMITTED','VERIFIED','NOT_VERIFIED','ASSIGNED','NOT_ASSIGNED','NOT_APPLICABLE','NOT_APPLICABLE_INFORMED','CONFLICT_OF_INTEREST', 'HANDLED_OFFLINE', 'COMPLETED_OFFLINE') NOT NULL;
 
-INSERT INTO `activity_state` (activity_type, state) VALUES ('PROJECT_SETUP', 'HANDLED_OFFLINE');
-INSERT INTO `activity_state` (activity_type, state) VALUES ('PROJECT_SETUP', 'COMPLETED_OFFLINE');
+INSERT INTO `activity_state` (id, activity_type, state) VALUES (51, 'PROJECT_SETUP', 'HANDLED_OFFLINE');
+INSERT INTO `activity_state` (id, activity_type, state) VALUES (52, 'PROJECT_SETUP', 'COMPLETED_OFFLINE');
