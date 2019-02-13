59b2eb3b7043cfd963f9b1832c995e102549679a
Toby Marson <tmarson@worth.systems>
2019-02-13 13:31:43 +0000

    IFS-5188: Do not include h2020 when serializing json

--- ifs-resources/src/main/java/org/innovateuk/ifs/competition/resource/CompetitionResource.java
+++ ifs-resources/src/main/java/org/innovateuk/ifs/competition/resource/CompetitionResource.java
@@ -26,7 +26,7 @@ public class CompetitionResource {
     private static final int CLOSING_SOON_AMOUNT = 3;
     public static final DateTimeFormatter START_DATE_FORMAT = DateTimeFormatter.ofPattern("dd/MM/YYYY");
 
-    private static final String H2020_TYPE_NAME = "H2020";
+    private static final String H2020_TYPE_NAME = "Programme";
     private static final DateTimeFormatter ASSESSMENT_DATE_FORMAT = DateTimeFormatter.ofPattern("MMMM YYYY");
 
     private Long id;
@@ -138,6 +138,7 @@ public class CompetitionResource {
         return CompetitionStatus.OPEN.equals(competitionStatus);
     }
 
+    @JsonIgnore
     public boolean isH2020() {
         return H2020_TYPE_NAME.equals(competitionTypeName);
     }
