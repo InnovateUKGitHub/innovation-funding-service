9bcf49fa28e670212c63adf945032effa7f1866f
Adam Mapstone <adam.mapstone@innovateuk.gov.uk>
2019-02-14 11:01:08 +0000

    IFS-5195 - Monitoring officer conditional check and dummy organisation added.

--- ifs-data-layer/ifs-data-service/src/main/java/org/innovateuk/ifs/crm/transactional/CrmServiceImpl.java
+++ ifs-data-layer/ifs-data-service/src/main/java/org/innovateuk/ifs/crm/transactional/CrmServiceImpl.java
@@ -15,11 +15,14 @@ import org.innovateuk.ifs.user.transactional.BaseUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
 
 import static java.lang.String.format;
 import static org.innovateuk.ifs.commons.error.CommonErrors.notFoundError;
 import static org.innovateuk.ifs.commons.service.ServiceResult.serviceSuccess;
+import static org.innovateuk.ifs.user.resource.Role.MONITORING_OFFICER;
 import static org.innovateuk.ifs.util.EntityLookupCallbacks.find;
 
 @Service
@@ -39,7 +42,7 @@ public class CrmServiceImpl implements CrmService {
     public ServiceResult<Void> syncCrmContact(long userId) {
          return find(userService.getUserById(userId), notFoundError(User.class, userId)).andOnSuccess(user -> {
             if (!user.getSuccess().isInternalUser()) {
-                return organisationService.getAllByUserId(userId).andOnSuccess(organisations -> {
+                return getUserOrganisationByUserIdOrCreateOrganisationForMonitoringOfficer(user.getSuccess()).andOnSuccess(organisations -> {
                     ServiceResult<Void> result = serviceSuccess();
                     for (OrganisationResource organisation : organisations) {
                         result = result.andOnSuccess(() -> {
@@ -54,6 +57,24 @@ public class CrmServiceImpl implements CrmService {
             }
             return serviceSuccess();
          });
+    }
+
+    private ServiceResult<List<OrganisationResource>> getUserOrganisationByUserIdOrCreateOrganisationForMonitoringOfficer(UserResource user) {
+        if (user.hasRole(MONITORING_OFFICER)) {
+            OrganisationResource monitoringOfficerOrganisationResource = createMonitoringOfficerOrganisation();
+            return serviceSuccess(Collections.singletonList(monitoringOfficerOrganisationResource)); // static import
+        }
+
+        return organisationService.getAllByUserId(user.getId());
+    }
+
+    private OrganisationResource createMonitoringOfficerOrganisation() {
+        OrganisationResource organisationResource = new OrganisationResource();
+        organisationResource.setName("IFS MO Company");
+        organisationResource.setCompaniesHouseNumber("");
+        organisationResource.setId(1L);
+
+        return organisationResource;
     }
 
     private SilContact toSilContact(UserResource user, OrganisationResource organisation) {
