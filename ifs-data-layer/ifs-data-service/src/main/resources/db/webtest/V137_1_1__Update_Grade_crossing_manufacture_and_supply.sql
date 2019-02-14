404363cb5db4b70a4286203bb624f3aa63679dfd
Kieran Hester <khester@worth.systems>
2019-02-14 08:18:25 +0000

    IFS-5129 delete unwanted webtest file

--- ifs-data-layer/ifs-data-service/src/main/resources/db/webtest/V137_1_1__Update_Grade_crossing_manufacture_and_supply.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO project_user (project_id, organisation_id, user_id, project_role, participant_status_id) VALUES ('10', '44', '79', 'PROJECT_FINANCE_CONTACT', '1');
-INSERT INTO project_user (project_id, organisation_id, user_id, project_role, participant_status_id) VALUES ('10', '45', '115', 'PROJECT_FINANCE_CONTACT', '1');
-INSERT INTO project_user (project_id, organisation_id, user_id, project_role, participant_status_id) VALUES ('10', '46', '116', 'PROJECT_FINANCE_CONTACT', '1');
