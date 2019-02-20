7792e5888ec703c6d0deab103cbcf27585b2ad0b
Ewan Cormack <ewan@hiveit.co.uk>
2019-02-20 15:08:18 +0000

    IFS-5208: Add totals to model plus dummy data

--- ifs-web-service/ifs-competition-mgt-service/src/main/java/org/innovateuk/ifs/invite/viewmodel/EuInviteViewModel.java
+++ ifs-web-service/ifs-competition-mgt-service/src/main/java/org/innovateuk/ifs/invite/viewmodel/EuInviteViewModel.java
@@ -13,9 +13,18 @@ public class EuInviteViewModel {
 
     private final Pagination pagination;
 
-    public EuInviteViewModel(List<EuContactResource> contacts, Pagination pagination) {
+    private final long totalNotified;
+
+    private final long totalNonNotified;
+
+    public EuInviteViewModel(List<EuContactResource> contacts,
+                             Pagination pagination,
+                             long totalNotified,
+                             long totalNonNotified) {
         this.contacts = contacts;
         this.pagination = pagination;
+        this.totalNotified = totalNotified;
+        this.totalNonNotified = totalNonNotified;
     }
 
     public List<EuContactResource> getContacts() {
@@ -24,6 +33,14 @@ public class EuInviteViewModel {
 
     public Pagination getPagination() {
         return pagination;
+    }
+
+    public long getTotalNotified() {
+        return totalNotified;
+    }
+
+    public long getTotalNonNotified() {
+        return totalNonNotified;
     }
 
     @Override
@@ -37,6 +54,8 @@ public class EuInviteViewModel {
         return new EqualsBuilder()
                 .append(contacts, viewModel.contacts)
                 .append(pagination, viewModel.pagination)
+                .append(totalNotified, viewModel.totalNotified)
+                .append(totalNonNotified, viewModel.totalNonNotified)
                 .isEquals();
     }
 
@@ -45,6 +64,8 @@ public class EuInviteViewModel {
         return new HashCodeBuilder(17, 37)
                 .append(contacts)
                 .append(pagination)
+                .append(totalNotified)
+                .append(totalNonNotified)
                 .toHashCode();
     }
 }
