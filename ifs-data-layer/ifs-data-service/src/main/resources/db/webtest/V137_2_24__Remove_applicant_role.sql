f2739dd11a6b5f6f2162ca24e6a0e60831499cf0
Kieran Hester <khester@worth.systems>
2019-02-21 14:03:50 +0000

    IFS-5129 remove applicant role from stakeholders and monitoring officers

---
+++ ifs-data-layer/ifs-data-service/src/main/resources/db/webtest/V137_2_24__Remove_applicant_role.sql
@@ -0,0 +1,14 @@
+-- TODO Configure user builder to register stakeholders and monitoring officers and then this can be deleted
+SET @applicant_role_id =
+(SELECT id FROM role WHERE name = 'applicant');
+
+DELETE FROM user_role WHERE user_id = (select id from user where email = 'orville.Gibbs@gmail.com') AND role_id = @applicant_role_id;
+DELETE FROM user_role WHERE user_id = (select id from user where email = 'Nilesh.Patti@gmail.com') AND role_id = @applicant_role_id;
+DELETE FROM user_role WHERE user_id = (select id from user where email = 'Rupesh.Pereira@gmail.com') AND role_id = @applicant_role_id;
+DELETE FROM user_role WHERE user_id = (select id from user where email = 'Rayon.Kevin@gmail.com') AND role_id = @applicant_role_id;
+DELETE FROM user_role WHERE user_id = (SELECT id FROM user WHERE email = 'Rui.Lemos@gmail.com') AND role_id = @applicant_role_id;
+DELETE FROM user_role WHERE user_id = (SELECT id FROM user WHERE email = 'blake.wood@gmail.com') AND role_id = @applicant_role_id;
+DELETE FROM user_role WHERE user_id = (SELECT id FROM user WHERE email = 'Lance.Lemos@gmail.com') AND role_id = @applicant_role_id;
+DELETE FROM user_role WHERE user_id = (SELECT id FROM user WHERE email = 'Kingsley.Roy@gmail.com') AND role_id = @applicant_role_id;
+DELETE FROM user_role WHERE user_id = (SELECT id FROM user WHERE email = 'Sonal.Dsilva@gmail.com') AND role_id = @applicant_role_id;
+DELETE FROM user_role WHERE user_id = (SELECT id FROM user WHERE email = 'carolyn.reed@example.com') AND role_id = @applicant_role_id;
