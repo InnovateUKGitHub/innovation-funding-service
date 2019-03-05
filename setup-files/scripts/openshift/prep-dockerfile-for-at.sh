d62d0a005c6833a1f68223c4a557c917c2cc5d96
Zsolt Pazmandy <zpazmandy@worth.systems>
2019-03-05 23:42:44 +0000

    dynamic dockerfile template for at

--- setup-files/scripts/openshift/prep-dockerfile-for-at.sh
+++ setup-files/scripts/openshift/prep-dockerfile-for-at.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-cd ../../..
-
 sed -i.bak "s#VOLUME\ \/mnt/ifs_storage#VOLUME\ \/tmp#g" ifs-data-layer/docker/Dockerfile-template
 sed -i.bak "/USER\ gluster/d" ifs-data-layer/docker/Dockerfile-template
+sed -i.bak "/umask\ 0002/d"  setup-files/scripts/docker/set-umask0002-and-run-jar.sh
 
