f0e7e77b1f6203b450a43c10c83aac846ff6e052
Matthew Bentley <mbentley@worth.systems>
2019-03-20 13:17:31 +0000

    IFS-5088 Disable submit button until an item is selected

--- ifs-web-service/ifs-web-core/src/main/resources/static/js/ifs_modules/autoComplete.js
+++ ifs-web-service/ifs-web-core/src/main/resources/static/js/ifs_modules/autoComplete.js
@@ -3,19 +3,33 @@ IFS.core.autoComplete = (function () {
   var s // private alias to settings
   return {
     settings: {
+      autoCompleteWrapper: '.autocomplete__wrapper',
       autoCompleteElement: '[data-auto-complete]',
+      autoCompleteSubmitElement: '[data-auto-complete-submit]',
       autoCompletePlugin: accessibleAutocomplete // eslint-disable-line
     },
     init: function () {
       s = this.settings
       var autoCompleteElement = jQuery(s.autoCompleteElement)
+      var autoCompleteSubmitElement = jQuery(s.autoCompleteSubmitElement)
       if (autoCompleteElement.length > 0) {
+        autoCompleteElement.prepend('<option value="">Select</option>').val('')
+        autoCompleteSubmitElement.prop('disabled', true)
+        jQuery(document).on('keydown', s.autoCompleteWrapper, function (e) {
+          if (e.which !== 13 && e.which !== 32) {
+            autoCompleteSubmitElement.prop('disabled', true)
+          }
+        })
         var showAllValues = autoCompleteElement.children('option').length <= 20
         s.autoCompletePlugin.enhanceSelectElement({
           selectElement: autoCompleteElement[0],
           showAllValues: showAllValues,
           defaultValue: '',
-          confirmOnBlur: false
+          confirmOnBlur: false,
+          onConfirm: function (confirmed) {
+            autoCompleteElement.children('option:contains(' + confirmed + ')').attr('selected', 'selected')
+            autoCompleteSubmitElement.prop('disabled', false)
+          }
         })
       }
     }
