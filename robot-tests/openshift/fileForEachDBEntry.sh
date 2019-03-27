cdfb974d92c19b774814bac2531eba4bd60c2355
Zsolt Pazmandy <zpazmandy@worth.systems>
2019-03-27 11:12:09 +0000

    permissions and paths update

--- robot-tests/openshift/fileForEachDBEntry.sh
+++ robot-tests/openshift/fileForEachDBEntry.sh
@@ -5,13 +5,14 @@ max_file_entry_id=$(mysql ifs -uroot -ppassword -hifs-database -s -e 'select max
 # curl -L https://github.com/openshift/origin/releases/download/v3.11.0/openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit.tar.gz | tar xvz
 # ocpath=openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit/
 
-mkdir -p "/mnt/ifs_storage/ifs/000000000_999999999/000000_999999/000_999/.test"
+# mkdir -p "/mnt/ifs_storage/ifs/000000000_999999999/000000_999999/000_999"
+mkdir -p "/mnt/ifs_storage/ifs/100000000_999999999/000000_999999/000_999"
 
 for i in `seq 1 ${max_file_entry_id}`;
 do
   if [ "${i}" != "8" ]
   then
     # ./${ocpath}/oc ${SVC_ACCOUNT_CLAUSE} rsync --include=testing.pdf ${i} ${DATA_SERVICE_POD}:/mnt/ifs_storage/000000000_999999999/000000_999999/000_999/${i}
-    cp /robot-tests/upload_files/testing.pdf /mnt/ifs_storage/ifs/000000000_999999999/000000_999999/000_999/.test/${i}
+    cp /robot-tests/upload_files/testing.pdf /mnt/ifs_storage/ifs/100000000_999999999/000000_999999/000_999/${i}
   fi
 done
