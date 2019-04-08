d666f1c8ff1603231e64fcde01fca8aa00b99084
Kieran Hester <khester@worth.systems>
2019-04-08 10:46:57 +0100

    IFS-5587 add logging

--- ifs-data-layer/ifs-data-service/src/main/docker/proxysql/make-mysqldump.sh
+++ ifs-data-layer/ifs-data-service/src/main/docker/proxysql/make-mysqldump.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/env bash
 set -e
-
+echo "before dump"
 # take the data dump
 mysqldump -u$DB_USER -p$DB_PASS -h127.0.0.1 -P6033 $DB_NAME --single-transaction --skip-extended-insert --ignore-table=$DB_NAME._ApplicationSetUp --ignore-table=$DB_NAME._ApplicationSetUp_CapUse --ignore-table=$DB_NAME._ApplicationSetUp_Value --ignore-table=$DB_NAME._tempIFS --ignore-table=$DB_NAME._tempIFS_AssScore --ignore-table=$DB_NAME._tempIFS_CapUse --ignore-table=$DB_NAME._ProjectSetUp --ignore-table=$DB_NAME._ProjectSetUp_CapUse --ignore-table=$DB_NAME._ProjectSetUp_Value > /dump/anonymised-dump.sql
 
+echo "taken dump"
+
 # encrypt the dump file before transit and finally remove the unprotected file
 gpg --yes --batch --passphrase=$DB_ANON_PASS -c /dump/anonymised-dump.sql
+
+echo "encrypted"
+
 rm /dump/anonymised-dump.sql
+
+echo "removed"
