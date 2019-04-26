8d3138c6dd48226a6fc04e84134847508ecb263f
Ewan Cormack <ewan@hiveit.co.uk>
2019-04-26 09:43:08 +0100

    IFS-5700: Create models for organisations and rework project team model

--- ifs-web-service/ifs-project-setup-service/src/main/java/org/innovateuk/ifs/project/projectteam/populator/ProjectTeamViewModelPopulator.java
+++ ifs-web-service/ifs-project-setup-service/src/main/java/org/innovateuk/ifs/project/projectteam/populator/ProjectTeamViewModelPopulator.java
@@ -1,11 +1,10 @@
 package org.innovateuk.ifs.project.projectteam.populator;
 
-import org.innovateuk.ifs.application.resource.ApplicationResource;
-import org.innovateuk.ifs.application.service.ApplicationService;
 import org.innovateuk.ifs.competition.resource.CompetitionResource;
 import org.innovateuk.ifs.competition.service.CompetitionRestService;
 import org.innovateuk.ifs.organisation.resource.OrganisationResource;
 import org.innovateuk.ifs.project.ProjectService;
+import org.innovateuk.ifs.project.projectteam.viewmodel.ProjectOrganisationViewModel;
 import org.innovateuk.ifs.project.projectteam.viewmodel.ProjectTeamViewModel;
 import org.innovateuk.ifs.project.resource.ProjectResource;
 import org.innovateuk.ifs.project.resource.ProjectUserResource;
@@ -16,26 +15,22 @@ import org.innovateuk.ifs.project.status.security.SetupSectionAccessibilityHelpe
 import org.innovateuk.ifs.status.StatusService;
 import org.innovateuk.ifs.user.resource.UserResource;
 import org.innovateuk.ifs.user.service.OrganisationRestService;
-import org.innovateuk.ifs.util.PrioritySorting;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.*;
-import java.util.function.Supplier;
+import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
-import static org.innovateuk.ifs.user.resource.Role.PARTNER;
 import static org.innovateuk.ifs.user.resource.Role.PROJECT_MANAGER;
-import static org.innovateuk.ifs.util.CollectionFunctions.*;
+import static org.innovateuk.ifs.util.CollectionFunctions.simpleFilter;
+import static org.innovateuk.ifs.util.CollectionFunctions.simpleFindFirst;
 
 @Component
 public class ProjectTeamViewModelPopulator {
 
     @Autowired
     private ProjectService projectService;
-
-    @Autowired
-    private ApplicationService applicationService;
 
     @Autowired
     private CompetitionRestService competitionRestService;
@@ -55,60 +50,56 @@ public class ProjectTeamViewModelPopulator {
 
     public ProjectTeamViewModel populate(long projectId, UserResource loggedInUser) {
 
-
-
         ProjectResource projectResource = projectService.getById(projectId);
-        ApplicationResource applicationResource = applicationService.getById(projectResource.getApplication());
-        CompetitionResource competitionResource = competitionRestService.getCompetitionById(applicationResource.getCompetition()).getSuccess();
-        boolean partnerProjectLocationRequired = competitionResource.isLocationPerPartner();
+        CompetitionResource competitionResource = competitionRestService.getCompetitionById(projectResource.getCompetition()).getSuccess();
 
         List<ProjectUserResource> projectUsers = projectService.getProjectUsersForProject(projectResource.getId());
+        List<OrganisationResource> projectOrganisations = projectService.getPartnerOrganisationsForProject(projectId);
         OrganisationResource leadOrganisation = projectService.getLeadOrganisation(projectId);
-        List<OrganisationResource> organisations
-                = new PrioritySorting<>(getPartnerOrganisations(projectUsers), leadOrganisation, OrganisationResource::getName).unwrap();
+        ProjectUserResource loggedInProjectUser = simpleFindFirst(projectUsers,
+                                                                  u -> u.getId().equals(loggedInUser.getId())).get();
+        OrganisationResource loggedInUserOrg = simpleFindFirst(projectOrganisations,
+                                                               org -> org.getId().equals(loggedInProjectUser.getOrganisation())).get();
+
+        boolean isLead = loggedInUserOrg.equals(leadOrganisation);
+
+        ProjectOrganisationViewModel leadOrgModel = mapToProjectOrganisationViewModel(projectUsers, leadOrganisation, true);
+        ProjectOrganisationViewModel loggedInUserOrgModel = mapToProjectOrganisationViewModel(projectUsers, loggedInUserOrg, isLead);
+        List<ProjectOrganisationViewModel> partnerOrgModels = projectOrganisations
+                .stream()
+                .filter(org -> !org.getId().equals(leadOrganisation.getId()))
+                .map(org -> mapToProjectOrganisationViewModel(projectUsers, org, false))
+                .collect(Collectors.toList());
 
         ProjectTeamStatusResource teamStatus = statusService.getProjectTeamStatus(projectId, Optional.empty());
         SetupSectionAccessibilityHelper statusAccessor = new SetupSectionAccessibilityHelper(teamStatus);
         boolean spendProfileGenerated = statusAccessor.isSpendProfileGenerated();
         boolean monitoringOfficerAssigned = statusAccessor.isMonitoringOfficerAssigned();
 
-        boolean allProjectDetailsFinanceContactsAndProjectLocationsAssigned = setupStatusViewModelPopulator.checkLeadPartnerProjectDetailsProcessCompleted(teamStatus, partnerProjectLocationRequired);
-
-        return new ProjectTeamViewModel(projectResource, loggedInUser,
-                                        getUsersPartnerOrganisations(loggedInUser, projectUsers),
-                                        organisations,
-                                        partnerProjectLocationRequired ?
-                                                partnerOrganisationService.getProjectPartnerOrganisations(projectId).getSuccess()
-                                                : Collections.emptyList(),
-                                        leadOrganisation, applicationResource, projectUsers, competitionResource,
-                                        projectService.isUserLeadPartner(projectId, loggedInUser.getId()), allProjectDetailsFinanceContactsAndProjectLocationsAssigned,
-                                        getProjectManager(projectResource.getId()).orElse(null), monitoringOfficerAssigned, spendProfileGenerated, statusAccessor.isGrantOfferLetterGenerated(), false);
-    }
-
-    private List<OrganisationResource> getPartnerOrganisations(final List<ProjectUserResource> projectRoles) {
-
-        final Comparator<OrganisationResource> compareById =
-                Comparator.comparingLong(OrganisationResource::getId);
-
-        final Supplier<SortedSet<OrganisationResource>> supplier = () -> new TreeSet<>(compareById);
-
-        SortedSet<OrganisationResource> organisationSet = projectRoles.stream()
-                .filter(uar -> uar.getRole() == PARTNER.getId())
-                .map(uar -> organisationRestService.getOrganisationById(uar.getOrganisation()).getSuccess())
-                .collect(Collectors.toCollection(supplier));
-
-        return new ArrayList<>(organisationSet);
-    }
-
-    private List<Long> getUsersPartnerOrganisations(UserResource loggedInUser, List<ProjectUserResource> projectUsers) {
-        List<ProjectUserResource> partnerProjectUsers = simpleFilter(projectUsers,
-                                                                     user -> loggedInUser.getId().equals(user.getUser()) && user.getRoleName().equals(PARTNER.getName()));
-        return simpleMap(partnerProjectUsers, ProjectUserResource::getOrganisation);
+        return new ProjectTeamViewModel(
+                competitionResource.getName(),
+                projectResource.getName(),
+                projectResource.getId(),
+                partnerOrgModels,
+                loggedInUserOrgModel,
+                leadOrgModel,
+                getProjectManager(projectResource.getId()).orElse(null),
+                isLead,
+                monitoringOfficerAssigned,
+                spendProfileGenerated,
+                statusAccessor.isGrantOfferLetterGenerated(),
+                false);
     }
 
     private Optional<ProjectUserResource> getProjectManager(Long projectId) {
         List<ProjectUserResource> projectUsers = projectService.getProjectUsersForProject(projectId);
         return simpleFindFirst(projectUsers, pu -> PROJECT_MANAGER.getId() == pu.getRole());
+    }
+
+    private ProjectOrganisationViewModel mapToProjectOrganisationViewModel(List<ProjectUserResource> totalUsers, OrganisationResource organisation, boolean isLead) {
+        List<ProjectUserResource> usersForOrganisation = simpleFilter(totalUsers,
+                                                                      user -> user.getOrganisation().equals(organisation.getId()));
+        return new ProjectOrganisationViewModel(usersForOrganisation, organisation.getName(), isLead);
     }
 
 
---
+++ ifs-web-service/ifs-project-setup-service/src/main/java/org/innovateuk/ifs/project/projectteam/viewmodel/ProjectOrganisationViewModel.java
@@ -0,0 +1,61 @@
+package org.innovateuk.ifs.project.projectteam.viewmodel;
+
+import org.innovateuk.ifs.project.resource.ProjectUserResource;
+
+import java.util.List;
+
+import static org.innovateuk.ifs.util.CollectionFunctions.simpleFindFirst;
+
+/**
+ * View model backing the Project Details page for Project Setup
+ */
+public class ProjectOrganisationViewModel {
+
+    private List<ProjectUserResource> users;
+
+    private String orgName;
+
+    private boolean leadOrg;
+
+    public ProjectOrganisationViewModel(List<ProjectUserResource> users,
+                                        String orgName,
+                                        boolean leadOrg) {
+        this.users = users;
+        this.orgName = orgName;
+        this.leadOrg = leadOrg;
+    }
+
+    public ProjectUserResource getFinanceContact() {
+        return simpleFindFirst(users,
+                               ProjectUserResource::isFinanceContact).orElse(null);
+    }
+
+    public ProjectUserResource getProjectManager() {
+        return simpleFindFirst(users,
+                               ProjectUserResource::isProjectManager).orElse(null);
+    }
+
+    public List<ProjectUserResource> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<ProjectUserResource> users) {
+        this.users = users;
+    }
+
+    public String getOrgName() {
+        return orgName;
+    }
+
+    public void setOrgName(String orgName) {
+        this.orgName = orgName;
+    }
+
+    public boolean isLeadOrg() {
+        return leadOrg;
+    }
+
+    public void setLeadOrg(boolean leadOrg) {
+        this.leadOrg = leadOrg;
+    }
+}
