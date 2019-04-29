cec30ef56736b605c8da551e200a80b980686043
Tom Baldwin <tom@polymorphism.co.uk>
2019-04-29 17:24:39 +0100

    IFS-5668 - Fixes ApplicationFinanceTest

--- ifs-data-layer/ifs-data-service/src/test/java/org/innovateuk/ifs/finance/domain/ApplicationFinanceTest.java
+++ ifs-data-layer/ifs-data-service/src/test/java/org/innovateuk/ifs/finance/domain/ApplicationFinanceTest.java
@@ -2,21 +2,21 @@ package org.innovateuk.ifs.finance.domain;
 
 import org.innovateuk.ifs.application.domain.Application;
 import org.innovateuk.ifs.organisation.domain.Organisation;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class ApplicationFinanceTest {
     private ApplicationFinance applicationFinance;
-    private Long id;
     private Organisation organisation;
     private Application application;
 
     @Before
     public void setUp() {
-        id=0L;
         organisation = new Organisation("Worth Internet Systems");
         application = new Application();
+        applicationFinance = new ApplicationFinance(application, organisation);
     }
 
     @Test
@@ -27,15 +27,14 @@ public class ApplicationFinanceTest {
 
     @Test
     public void applicationFinanceShouldReturnCorrectAttributeValues() {
-        Assert.assertEquals(applicationFinance.getId(), id);
-        Assert.assertEquals(applicationFinance.getOrganisation(), organisation);
-        Assert.assertEquals(applicationFinance.getApplication(), application);
+        assertEquals(applicationFinance.getOrganisation(), organisation);
+        assertEquals(applicationFinance.getApplication(), application);
     }
 
     @Test
     public void applicationFinanceShouldReturnCorrectAttributeValuesAfterSetId() {
         Long newId = 2L;
         applicationFinance.setId(newId);
-        Assert.assertEquals(applicationFinance.getId(), newId);
+        assertEquals(applicationFinance.getId(), newId);
     }
 }
