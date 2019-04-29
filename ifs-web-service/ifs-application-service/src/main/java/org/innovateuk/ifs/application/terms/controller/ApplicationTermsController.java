5f80773600cdf3e67e391cd6d370229c9abd1bc4
Tom Baldwin <tom@polymorphism.co.uk>
2019-04-29 16:15:37 +0100

    IFS-5668 - Adds security to ApplicationTermsController

--- ifs-web-service/ifs-application-service/src/main/java/org/innovateuk/ifs/application/terms/controller/ApplicationTermsController.java
+++ ifs-web-service/ifs-application-service/src/main/java/org/innovateuk/ifs/application/terms/controller/ApplicationTermsController.java
@@ -2,8 +2,10 @@ package org.innovateuk.ifs.application.terms.controller;
 
 import org.innovateuk.ifs.application.resource.ApplicationResource;
 import org.innovateuk.ifs.application.service.ApplicationRestService;
+import org.innovateuk.ifs.commons.security.SecuredBySpring;
 import org.innovateuk.ifs.competition.resource.CompetitionResource;
 import org.innovateuk.ifs.competition.service.CompetitionRestService;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +14,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/application/{applicationId}/terms-and-conditions")
+@PreAuthorize("hasAuthority('applicant')")
+@SecuredBySpring(value="Controller",
+        description = "Only applicants are allowed to view the application terms",
+        securedType = ApplicationTermsController.class)
 public class ApplicationTermsController {
 
     private ApplicationRestService applicationRestService;
