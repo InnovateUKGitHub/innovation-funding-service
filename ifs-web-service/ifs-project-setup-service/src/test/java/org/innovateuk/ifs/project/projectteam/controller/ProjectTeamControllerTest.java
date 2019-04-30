b7412f4a8467a4983e9f9b02caa85374919768f1
Ewan Cormack <ewan@hiveit.co.uk>
2019-04-30 11:57:11 +0100

    IFS-5718: Add unit tests for remove project users endpoint

--- ifs-web-service/ifs-project-setup-service/src/test/java/org/innovateuk/ifs/project/projectteam/controller/ProjectTeamControllerTest.java
+++ ifs-web-service/ifs-project-setup-service/src/test/java/org/innovateuk/ifs/project/projectteam/controller/ProjectTeamControllerTest.java
@@ -6,20 +6,22 @@ import org.innovateuk.ifs.project.projectteam.viewmodel.ProjectOrganisationUserR
 import org.innovateuk.ifs.project.projectteam.viewmodel.ProjectOrganisationViewModel;
 import org.innovateuk.ifs.project.projectteam.viewmodel.ProjectTeamViewModel;
 import org.innovateuk.ifs.project.resource.ProjectUserResource;
+import org.innovateuk.ifs.project.service.ProjectRestService;
 import org.innovateuk.ifs.user.resource.UserResource;
 import org.junit.Test;
 import org.mockito.Mock;
 import org.springframework.test.web.servlet.MvcResult;
 
 import static java.util.Collections.singletonList;
+import static org.innovateuk.ifs.commons.rest.RestResult.restSuccess;
 import static org.innovateuk.ifs.project.builder.ProjectUserResourceBuilder.newProjectUserResource;
 import static org.innovateuk.ifs.user.builder.UserResourceBuilder.newUserResource;
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 public class ProjectTeamControllerTest extends BaseControllerMockMVCTest<ProjectTeamController> {
 
@@ -30,6 +32,9 @@ public class ProjectTeamControllerTest extends BaseControllerMockMVCTest<Project
 
     @Mock
     private ProjectTeamViewModelPopulator populator;
+
+    @Mock
+    private ProjectRestService projectRestService;
 
     @Test
     public void viewProjectTeam() throws Exception {
@@ -60,5 +65,20 @@ public class ProjectTeamControllerTest extends BaseControllerMockMVCTest<Project
         ProjectTeamViewModel actual = (ProjectTeamViewModel) result.getModelAndView().getModel().get("model");
         assertEquals(expected, actual);
 
+    }
+
+    @Test
+    public void removeUser() throws Exception {
+        UserResource loggedInUser = newUserResource().build();
+        setLoggedInUser(loggedInUser);
+        long userId = 444L;
+        long projectId = 555L;
+
+        when(projectRestService.removeUser(projectId, userId)).thenReturn(restSuccess());
+
+        mockMvc.perform(post("/project/" + projectId + "/team/remove-user/" + userId))
+                .andExpect(status().is3xxRedirection());
+
+        verify(projectRestService).removeUser(projectId, userId);
     }
 }
