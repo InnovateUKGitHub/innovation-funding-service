bd358fd681886075cf61c8ab9d3c61ae55c531ab
Tom Baldwin <tom@polymorphism.co.uk>
2019-05-07 10:19:12 +0100

    IFS-5816 - Changes priority of t&c section and renames the t&c question.

--- ifs-data-layer/ifs-data-service/src/main/resources/db/migration/V143_1_1__Migrate_application_completed_terms.sql
+++ ifs-data-layer/ifs-data-service/src/main/resources/db/migration/V143_1_1__Migrate_application_completed_terms.sql
@@ -5,7 +5,7 @@ INSERT INTO section
 (display_in_assessment_application_summary, name, priority, competition_id, question_group, section_type)
 SELECT false                  AS display_in_assessment_application_summary,
        'Terms and conditions' AS name,
-       '1'                    AS priority,
+       '11'                   AS priority,
        c.id                   AS competition_id,
        false                  AS question_group,
        'TERMS_AND_CONDITIONS' AS section_type
@@ -15,18 +15,18 @@ WHERE setup_complete;
 -- Add the terms question to the terms section
 INSERT INTO question (assign_enabled, description, mark_as_completed_enabled, multiple_statuses, name, short_name,
                       priority, question_number, competition_id, section_id, question_type, question_setup_type)
-SELECT false                  AS assign_enabled,
-       'Terms and conditions' AS description,
-       true                   AS mark_as_completed_enabled,
-       false                  AS multiple_statuses,
-       'Terms and Conditions' AS name,
-       'T&C'                  AS short_name,
-       '1'                    AS priority,
-       '1'                    AS question_number,
-       c.id                   AS competition_id,
-       s.id                   AS section_id,
-       'GENERAL'              AS question_type,
-       'TERMS_AND_CONDITIONS' AS question_setup_type
+SELECT false                        AS assign_enabled,
+       'Award terms and conditions' AS description,
+       true                         AS mark_as_completed_enabled,
+       false                        AS multiple_statuses,
+       'Award terms and conditions' AS name,
+       'T&C'                        AS short_name,
+       '1'                          AS priority,
+       '1'                          AS question_number,
+       c.id                         AS competition_id,
+       s.id                         AS section_id,
+       'GENERAL'                    AS question_type,
+       'TERMS_AND_CONDITIONS'       AS question_setup_type
 FROM competition c
          INNER JOIN section s ON s.competition_id = c.id AND s.section_type = 'TERMS_AND_CONDITIONS'
 WHERE setup_complete;
