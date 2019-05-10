428a88fedb9f2818ebf5391c38c0e80442c0ae39
Ewan Cormack <ewan@hiveit.co.uk>
2019-05-10 15:13:36 +0100

    IFS-5723: Support permissioning for pending invite removal

--- ifs-web-service/ifs-project-setup-service/src/main/java/org/innovateuk/ifs/project/projectteam/controller/ProjectTeamController.java
+++ ifs-web-service/ifs-project-setup-service/src/main/java/org/innovateuk/ifs/project/projectteam/controller/ProjectTeamController.java
@@ -83,8 +83,6 @@ public class ProjectTeamController {
         return "redirect:/project/" + projectId + "/team";
     }
 
-
-
     @PreAuthorize("hasPermission(#projectId, 'org.innovateuk.ifs.project.resource.ProjectCompositeId', 'ACCESS_PROJECT_TEAM_SECTION')")
     @PostMapping(value = "/{projectId}/team", params = "add-team-member")
     public String openAddTeamMemberForm(@ModelAttribute(value = "form") ProjectTeamForm form,
