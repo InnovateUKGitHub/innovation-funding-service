68c730f6733c9d21f88350bade4648181da518cc
Ewan Cormack <ewan@hiveit.co.uk>
2019-05-13 16:31:20 +0100

    IFS-5723: Update permissioning

--- ifs-data-layer/ifs-data-service/src/main/java/org/innovateuk/ifs/project/projectteam/transactional/ProjectTeamService.java
+++ ifs-data-layer/ifs-data-service/src/main/java/org/innovateuk/ifs/project/projectteam/transactional/ProjectTeamService.java
@@ -4,6 +4,7 @@ import org.innovateuk.ifs.commons.service.ServiceResult;
 import org.innovateuk.ifs.invite.resource.ProjectUserInviteResource;
 import org.innovateuk.ifs.project.resource.ProjectUserCompositeId;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.parameters.P;
 
 /**
  * Transactional and secure service for Project team processing work
@@ -14,7 +15,7 @@ public interface ProjectTeamService {
     ServiceResult<Void> removeUser(ProjectUserCompositeId composite);
 
     @PreAuthorize("hasPermission(#projectUserInviteResourceId, 'org.innovateuk.ifs.invite.resource.ProjectUserInviteResource', 'DELETE_PROJECT_INVITE')")
-    ServiceResult<Void> removeInvite(long projectUserInviteResourceId, long projectId);
+    ServiceResult<Void> removeInvite(@P("projectUserInviteResourceId") long projectUserInviteResourceId, long projectId);
 
     @PreAuthorize("hasPermission(#inviteResource, 'SEND_PROJECT_INVITE')")
     ServiceResult<Void> inviteTeamMember(long projectId, ProjectUserInviteResource inviteResource);
