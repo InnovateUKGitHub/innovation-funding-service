a4e879f19c2998f527a5d3534ad03b3178969ab7
Luke Harper <lharper@worth.systems>
2019-05-24 16:02:29 +0100

    IFS-5910 repo formatting.

--- ifs-data-layer/ifs-data-service/src/main/java/org/innovateuk/ifs/project/monitoring/repository/MonitoringOfficerRepository.java
+++ ifs-data-layer/ifs-data-service/src/main/java/org/innovateuk/ifs/project/monitoring/repository/MonitoringOfficerRepository.java
@@ -26,11 +26,13 @@ public interface MonitoringOfficerRepository extends PagingAndSortingRepository<
     void deleteByUserIdAndProjectId(long userId, long projectId);
 
     @Query("SELECT NEW org.innovateuk.ifs.project.monitoring.resource.MonitoringOfficerUnassignedProjectResource(" +
-            "project.id," +
-            "project.application.id," +
-            "project.name) " +
+            "   project.id," +
+            "   project.application.id," +
+            "   project.name" +
+            ") " +
             "FROM Project project " +
-            "LEFT JOIN MonitoringOfficer monitoringOfficer ON monitoringOfficer.id = project.projectMonitoringOfficer.id " +
+            "LEFT JOIN MonitoringOfficer monitoringOfficer " +
+            "   ON monitoringOfficer.id = project.projectMonitoringOfficer.id " +
             "WHERE " +
             "   monitoringOfficer.id IS NULL " +
             "   AND project.projectProcess.activityState in " + PROJECT_STATES +
@@ -38,14 +40,18 @@ public interface MonitoringOfficerRepository extends PagingAndSortingRepository<
     List<MonitoringOfficerUnassignedProjectResource> findUnassignedProject();
 
     @Query("SELECT NEW org.innovateuk.ifs.project.monitoring.resource.MonitoringOfficerAssignedProjectResource(" +
-            "project.id," +
-            "project.application.id," +
-            "project.application.competition.id," +
-            "project.name," +
-            "organisation.name) " +
+            "   project.id," +
+            "   project.application.id," +
+            "   project.application.competition.id," +
+            "   project.name," +
+            "   organisation.name" +
+            ") " +
             "FROM Project project " +
-            "JOIN ProcessRole processRole ON processRole.applicationId = project.application.id AND processRole.role = org.innovateuk.ifs.user.resource.Role.LEADAPPLICANT " +
-            "JOIN Organisation organisation ON organisation.id = processRole.organisationId " +
+            "JOIN ProcessRole processRole " +
+            "   ON processRole.applicationId = project.application.id " +
+            "   AND processRole.role = org.innovateuk.ifs.user.resource.Role.LEADAPPLICANT " +
+            "JOIN Organisation organisation " +
+            "   ON organisation.id = processRole.organisationId " +
             "WHERE " +
             "   project.projectMonitoringOfficer.user.id = :userId " +
             "   AND project.projectProcess.activityState in " + PROJECT_STATES +
