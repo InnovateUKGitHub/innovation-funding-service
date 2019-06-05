0e1f6760a4b46529849dfad2a4792e29a3d49840
Zsolt Pazmandy <zpazmandy@worth.systems>
2019-06-05 21:50:32 +0100

    chowning test files because umask wont affect them

--- robot-tests/openshift/fileForEachDBEntry.sh
+++ robot-tests/openshift/fileForEachDBEntry.sh
@@ -1,12 +1,17 @@
+set -x
 echo "***********Creating file entry for each db entry***********"
 max_file_entry_id=$(mysql ifs -uroot -ppassword -hifs-database -s -e 'select max(id) from file_entry;')
 
-mkdir -p "/mnt/ifs_storage/at_files/000000000_999999999/000000_999999/000_999"
+TEST_FILES_DIR="/mnt/ifs_storage/at_files/000000000_999999999/000000_999999/000_999"
+
+mkdir -p "${TEST_FILES_DIR}"
 
 for i in `seq 1 ${max_file_entry_id}`;
 do
   if [ "${i}" != "8" ]
   then
-    cp /robot-tests/upload_files/testing.pdf /mnt/ifs_storage/at_files/000000000_999999999/000000_999999/000_999/${i}
+    CURRENT_FILE_PATH="${TEST_FILES_DIR}/${i}"
+    cp /robot-tests/upload_files/testing.pdf $CURRENT_FILE_PATH
+    chown gluster:gluster $CURRENT_FILE_PATH
   fi
 done
