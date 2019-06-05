402cf4a33a4929a0aace0f650c96942f1b2f2a5a
Zsolt Pazmandy <zpazmandy@worth.systems>
2019-06-05 12:44:19 +0100

    rename AT test file storage dir

--- robot-tests/openshift/fileForEachDBEntry.sh
+++ robot-tests/openshift/fileForEachDBEntry.sh
@@ -1,12 +1,12 @@
 echo "***********Creating file entry for each db entry***********"
 max_file_entry_id=$(mysql ifs -uroot -ppassword -hifs-database -s -e 'select max(id) from file_entry;')
 
-mkdir -p "/mnt/ifs_storage/ifs/200000000_999999999/000000_999999/000_999"
+mkdir -p "/mnt/ifs_storage/at_files/000000000_999999999/000000_999999/000_999"
 
 for i in `seq 1 ${max_file_entry_id}`;
 do
   if [ "${i}" != "8" ]
   then
-    cp /robot-tests/upload_files/testing.pdf /mnt/ifs_storage/ifs/200000000_999999999/000000_999999/000_999/${i}
+    cp /robot-tests/upload_files/testing.pdf /mnt/ifs_storage/at_files/000000000_999999999/000000_999999/000_999/${i}
   fi
 done
