cb08b132e9982ce63ab95ade20db0e96ce7cbfb4
Ewan Cormack <ewan@hiveit.co.uk>
2019-06-06 14:47:35 +0100

    Revert "IFS-5867: Update competition type names"
    This reverts commit fa63c02aa5534c1aca5f153379ecf68d7af963df.

--- ifs-data-layer/ifs-data-service/src/main/resources/db/migration/V144_1_1__Update_APC_and_ATI_competition_names.sql
+++
@@ -1,4 +0,0 @@
--- IFS-5867 Update names of two competition types
-
-UPDATE competition_type SET name = 'BEIS: Advanced Propulsion Centre (APC) Programme' WHERE name = 'Advanced Propulsion Centre';
-UPDATE competition_type SET name = 'BEIS: Aerospace Technology Institute (ATI) Programme' WHERE name = 'Aerospace Technology Institute';
