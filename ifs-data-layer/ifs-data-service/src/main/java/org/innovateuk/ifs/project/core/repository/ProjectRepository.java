f736bf1483a3e9a45214df98e06933b492267bbb
Ewan Cormack <ewan@hiveit.co.uk>
2019-06-07 11:49:15 +0100

    IFS-5966: Show withdrawn projects on the project setup tab of the competition dashboard

--- ifs-data-layer/ifs-data-service/src/main/java/org/innovateuk/ifs/project/core/repository/ProjectRepository.java
+++ ifs-data-layer/ifs-data-service/src/main/java/org/innovateuk/ifs/project/core/repository/ProjectRepository.java
@@ -1,19 +1,16 @@
 package org.innovateuk.ifs.project.core.repository;
 
 import org.innovateuk.ifs.project.core.domain.Project;
-import org.innovateuk.ifs.project.resource.ProjectState;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 
-import java.util.Collection;
 import java.util.List;
 
 public interface ProjectRepository extends PagingAndSortingRepository<Project, Long>{
 
-    String PROJECTS_BY_APP_ID_LIKE_AND_COMP_ID_AND_NOT_IN_STATE = "SELECT DISTINCT pp.target FROM ProjectProcess pp " +
+    String PROJECTS_BY_APP_ID_LIKE_AND_COMP_ID = "SELECT DISTINCT pp.target FROM ProjectProcess pp " +
             " WHERE pp.target.application.competition.id = :compId " +
-            " AND pp.activityState NOT IN :states " +
             " AND (str(pp.target.application.id) LIKE CONCAT('%', :applicationSearchString, '%'))";
 
     @Override
@@ -23,8 +20,8 @@ public interface ProjectRepository extends PagingAndSortingRepository<Project, L
 
     int countByApplicationCompetitionId(final Long competitionId);
 
-    @Query(PROJECTS_BY_APP_ID_LIKE_AND_COMP_ID_AND_NOT_IN_STATE)
-    List<Project> searchByCompetitionIdAndApplicationIdLikeAndProjectStateNotIn(@Param("compId") long competitionId, @Param("applicationSearchString") String applicationSearchString, @Param("states") Collection<ProjectState> projectStates);
+    @Query(PROJECTS_BY_APP_ID_LIKE_AND_COMP_ID)
+    List<Project> searchByCompetitionIdAndApplicationIdLike(@Param("compId") long competitionId, @Param("applicationSearchString") String applicationSearchString);
 
 
 }
--- ifs-data-layer/ifs-data-service/src/main/java/org/innovateuk/ifs/project/status/transactional/StatusServiceImpl.java
+++ ifs-data-layer/ifs-data-service/src/main/java/org/innovateuk/ifs/project/status/transactional/StatusServiceImpl.java
@@ -59,7 +59,6 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 
 import static java.util.Arrays.asList;
-import static java.util.Collections.singleton;
 import static java.util.Comparator.comparing;
 import static org.innovateuk.ifs.commons.error.CommonFailureKeys.GENERAL_NOT_FOUND;
 import static org.innovateuk.ifs.commons.service.ServiceResult.serviceSuccess;
@@ -127,7 +126,7 @@ public class StatusServiceImpl extends AbstractProjectServiceImpl implements Sta
     @Override
     public ServiceResult<CompetitionProjectsStatusResource> getCompetitionStatus(Long competitionId, String applicationSearchString) {
         Competition competition = competitionRepository.findById(competitionId).get();
-        List<Project> projects = projectRepository.searchByCompetitionIdAndApplicationIdLikeAndProjectStateNotIn(competitionId, applicationSearchString, singleton(ProjectState.WITHDRAWN));
+        List<Project> projects = projectRepository.searchByCompetitionIdAndApplicationIdLike(competitionId, applicationSearchString);
         List<ProjectStatusResource> projectStatuses = projectStatuses(projects);
         CompetitionProjectsStatusResource competitionProjectsStatusResource
                 = new CompetitionProjectsStatusResource(competition.getId(), competition.getName(), projectStatuses);
