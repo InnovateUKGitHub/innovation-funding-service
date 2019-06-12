828140eb45f7eebff26ce9e40c72f0c4450afed3
Kieran Hester <khester@worth.systems>
2019-06-12 11:07:20 +0100

    IFS-6008 update scripts

--- setup-files/scripts/create-baseline-dump.sh
+++ setup-files/scripts/create-baseline-dump.sh
@@ -4,9 +4,11 @@ cd "$(dirname "$0")"
 
 baselineversion="${1?please specify the new baseline version (e.g. V100_11_)}"
 
+echo "database host is  ${2}"
+
 echo "creating baseline for version ${baselineversion}"
-mysql -h ifs-database ifs -uroot -ppassword -e 'delete from user_role where exists (select 1 from user u where u.id = user_id and u.system_user = 1);'
-mysql -h ifs-database ifs -uroot -ppassword -e 'SET foreign_key_checks = 0; delete from user where system_user = 1; SET foreign_key_checks = 1;'
+mysql -h ${2} ifs -uroot -ppassword -e 'delete from user_role where exists (select 1 from user u where u.id = user_id and u.system_user = 1);'
+mysql -h ${2} ifs -uroot -ppassword -e 'SET foreign_key_checks = 0; delete from user where system_user = 1; SET foreign_key_checks = 1;'
 
 
-mysqldump -h 127.0.0.1 ifs -uroot -ppassword --no-create-info --ignore-table=ifs.schema_version --ignore-table=ifs.activity_state --ignore-table=ifs.finance_row_meta_field --ignore-table=ifs.address_type --ignore-table=ifs.application_status --ignore-table=ifs.category --ignore-table=ifs.form_validator --ignore-table=ifs.form_input_type --ignore-table=ifs.organisation_type --ignore-table=ifs.participant_status --ignore-table=ifs.project_role --ignore-table=ifs.role --ignore-table=ifs.competition_type --ignore-table=ifs.academic --ignore-table=ifs.agreement --ignore-table=ifs.ethnicity --ignore-table=ifs.rejection_reason --ignore-table=ifs.assessor_count_option --ignore-table=ifs.grant_claim_maximum --ignore-table=ifs.organisation_size --ignore-table=ifs.terms_and_conditions --ignore-table=ifs.eu_action_type --ignore-table=ifs.eu_grant_transfer > ../../ifs-data-layer/ifs-data-service/src/main/resources/db/webtest/${baselineversion}8__Base_webtest_data.sql
+mysqldump -h ${2} ifs -uroot -ppassword --no-create-info --ignore-table=ifs.schema_version --ignore-table=ifs.activity_state --ignore-table=ifs.finance_row_meta_field --ignore-table=ifs.address_type --ignore-table=ifs.application_status --ignore-table=ifs.category --ignore-table=ifs.form_validator --ignore-table=ifs.form_input_type --ignore-table=ifs.organisation_type --ignore-table=ifs.participant_status --ignore-table=ifs.project_role --ignore-table=ifs.role --ignore-table=ifs.competition_type --ignore-table=ifs.academic --ignore-table=ifs.agreement --ignore-table=ifs.ethnicity --ignore-table=ifs.rejection_reason --ignore-table=ifs.assessor_count_option --ignore-table=ifs.grant_claim_maximum --ignore-table=ifs.organisation_size --ignore-table=ifs.terms_and_conditions --ignore-table=ifs.eu_action_type --ignore-table=ifs.eu_grant_transfer > ../../ifs-data-layer/ifs-data-service/src/main/resources/db/webtest/${baselineversion}8__Base_webtest_data.sql
--- setup-files/scripts/generate-test-data.sh
+++ setup-files/scripts/generate-test-data.sh
@@ -53,6 +53,9 @@ run_flyway_migrate() {
 
 do_baseline () {
 
+    : ${DATABASE_NAME:=ifs-database}
+    echo "database is $DATABASE_NAME"
+
     generate_test_class="ifs-data-layer/ifs-data-service/src/test/java/org/innovateuk/ifs/testdata/GenerateTestData.java"
 
     # clean database
@@ -77,7 +80,7 @@ do_baseline () {
     cd ${project_root_dir}/setup-files/scripts
 
     # create baseline dump
-    ./create-baseline-dump.sh ${new_version}
+    ./create-baseline-dump.sh ${new_version} $DATABASE_NAME
 
     cd ${project_root_dir}
 
