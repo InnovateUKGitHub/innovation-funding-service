0e615e511dc017facf77acbd7a73ac0a87141ad9
Ewan Cormack <ewan@hiveit.co.uk>
2019-06-13 14:49:38 +0100

    IFS-5958: Fix up more references to document view model

--- ifs-web-service/ifs-project-setup-service/src/test/java/org/innovateuk/ifs/project/documents/controller/DocumentsControllerTest.java
+++ ifs-web-service/ifs-project-setup-service/src/test/java/org/innovateuk/ifs/project/documents/controller/DocumentsControllerTest.java
@@ -100,7 +100,7 @@ public class DocumentsControllerTest extends BaseControllerMockMVCTest<Documents
 
         DocumentViewModel viewModel = new DocumentViewModel(projectId, "Project 12", applicationId,
                 documentConfigId, "Risk Register", "Guidance for Risk Register",
-                null, DocumentStatus.UNSET, "", true);
+                null, DocumentStatus.UNSET, "", true, true);
 
         when(populator.populateViewDocument(projectId, loggedInUser.getId(), documentConfigId)).thenReturn(viewModel);
 
@@ -201,7 +201,7 @@ public class DocumentsControllerTest extends BaseControllerMockMVCTest<Documents
 
         DocumentViewModel viewModel = new DocumentViewModel(projectId, "Project 12", applicationId,
                 documentConfigId, "Risk Register", "Guidance for Risk Register",
-                fileDetailsViewModel, DocumentStatus.SUBMITTED, "",true);
+                fileDetailsViewModel, DocumentStatus.SUBMITTED, "",true, true);
 
         when(populator.populateViewDocument(projectId, loggedInUser.getId(), documentConfigId)).thenReturn(viewModel);
 
@@ -251,7 +251,7 @@ public class DocumentsControllerTest extends BaseControllerMockMVCTest<Documents
 
         DocumentViewModel viewModel = new DocumentViewModel(projectId, "Project 12", applicationId,
                 documentConfigId, "Risk Register", "Guidance for Risk Register",
-                fileDetailsViewModel, DocumentStatus.UNSET, "",true);
+                fileDetailsViewModel, DocumentStatus.UNSET, "",true, true);
 
         when(populator.populateViewDocument(projectId, loggedInUser.getId(), documentConfigId)).thenReturn(viewModel);
 
