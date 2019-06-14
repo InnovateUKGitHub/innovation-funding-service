16b3590c7ee541dd99efeff8257d4c85b9a854a7
Ewan Cormack <ewan@hiveit.co.uk>
2019-06-14 15:04:11 +0100

    IFS-5958: Update bank details unit test

--- ifs-web-service/ifs-project-setup-mgt-service/src/test/java/org/innovateuk/ifs/project/bankdetails/controller/BankDetailsManagementControllerTest.java
+++ ifs-web-service/ifs-project-setup-mgt-service/src/test/java/org/innovateuk/ifs/project/bankdetails/controller/BankDetailsManagementControllerTest.java
@@ -98,7 +98,26 @@ public class BankDetailsManagementControllerTest extends BaseControllerMockMVCTe
 
         bankDetailsReviewViewModel = buildModelView(project, projectUsers.get(0), organisationResource, bankDetailsResource);
 
-        notUpdatedChangeBankDetailsViewModel = new ChangeBankDetailsViewModel(bankDetailsReviewViewModel.getProjectId(), bankDetailsReviewViewModel.getApplicationId(), bankDetailsReviewViewModel.getProjectName(), bankDetailsReviewViewModel.getFinanceContactName(), bankDetailsReviewViewModel.getFinanceContactEmail(), bankDetailsReviewViewModel.getFinanceContactPhoneNumber(), bankDetailsReviewViewModel.getOrganisationId(), bankDetailsReviewViewModel.getOrganisationName(), bankDetailsReviewViewModel.getRegistrationNumber(), bankDetailsReviewViewModel.getBankAccountNumber(), bankDetailsReviewViewModel.getSortCode(), bankDetailsReviewViewModel.getOrganisationAddress(), bankDetailsReviewViewModel.getVerified(), bankDetailsReviewViewModel.getCompanyNameScore(), bankDetailsReviewViewModel.getRegistrationNumberMatched(), bankDetailsReviewViewModel.getAddressScore(), bankDetailsReviewViewModel.getApproved(), bankDetailsReviewViewModel.getApprovedManually(), false);
+        notUpdatedChangeBankDetailsViewModel = new ChangeBankDetailsViewModel(bankDetailsReviewViewModel.getProjectId(),
+                                                                              bankDetailsReviewViewModel.getApplicationId(),
+                                                                              bankDetailsReviewViewModel.getProjectName(),
+                                                                              bankDetailsReviewViewModel.getFinanceContactName(),
+                                                                              bankDetailsReviewViewModel.getFinanceContactEmail(),
+                                                                              bankDetailsReviewViewModel.getFinanceContactPhoneNumber(),
+                                                                              bankDetailsReviewViewModel.getOrganisationId(),
+                                                                              bankDetailsReviewViewModel.getOrganisationName(),
+                                                                              bankDetailsReviewViewModel.getRegistrationNumber(),
+                                                                              bankDetailsReviewViewModel.getBankAccountNumber(),
+                                                                              bankDetailsReviewViewModel.getSortCode(),
+                                                                              bankDetailsReviewViewModel.getOrganisationAddress(),
+                                                                              bankDetailsReviewViewModel.getVerified(),
+                                                                              bankDetailsReviewViewModel.getCompanyNameScore(),
+                                                                              bankDetailsReviewViewModel.getRegistrationNumberMatched(),
+                                                                              bankDetailsReviewViewModel.getAddressScore(),
+                                                                              bankDetailsReviewViewModel.getApproved(),
+                                                                              bankDetailsReviewViewModel.getApprovedManually(),
+                                                                              bankDetailsReviewViewModel.isProjectActive(),
+                                                                              false);
     }
 
     private BankDetailsReviewViewModel buildModelView(ProjectResource project, ProjectUserResource financeContact, OrganisationResource organisation, BankDetailsResource bankDetails){
