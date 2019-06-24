697eac408c5024de44476ed80e9910f0534b59fe
Tom Baldwin <tom@polymorphism.co.uk>
2019-06-24 15:34:35 +0100

    IFS-5989 - Database change for EoI t&c descriptions.

--- ifs-data-layer/ifs-data-service/src/main/resources/db/migration/V144_1_4__Make_terms_and_conditions_lead_only_for_eoi_competitions.sql
+++ ifs-data-layer/ifs-data-service/src/main/resources/db/migration/V144_1_4__Make_terms_and_conditions_lead_only_for_eoi_competitions.sql
@@ -4,9 +4,11 @@ SET @eoi_competition_type = (SELECT id FROM competition_type ct WHERE ct.name='E
 
 -- we don't want to accept per organisation
 UPDATE question q
+INNER JOIN section s ON s.id = q.section_id
     SET q.multiple_statuses = FALSE
 WHERE
-    q.competition_id IN (
+    s.section_type = 'TERMS_AND_CONDITIONS' AND
+    s.competition_id IN (
         SELECT c.id
         FROM competition c
         WHERE
