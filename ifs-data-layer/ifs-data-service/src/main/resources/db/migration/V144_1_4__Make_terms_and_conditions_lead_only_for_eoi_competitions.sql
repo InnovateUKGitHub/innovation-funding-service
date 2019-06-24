9888cf2dbf1e52983800829917a56849474254c2
Tom Baldwin <tom@polymorphism.co.uk>
2019-06-24 11:06:04 +0100

    IFS-5989 - Updates EoI t&c changes to include the EoI competition template.

--- ifs-data-layer/ifs-data-service/src/main/resources/db/migration/V144_1_4__Make_terms_and_conditions_lead_only_for_eoi_competitions.sql
+++ ifs-data-layer/ifs-data-service/src/main/resources/db/migration/V144_1_4__Make_terms_and_conditions_lead_only_for_eoi_competitions.sql
@@ -6,4 +6,10 @@ SET @eoi_competition_type = (SELECT id FROM competition_type ct WHERE ct.name='E
 UPDATE question q
     SET q.multiple_statuses = FALSE
 WHERE
-    q.competition_id IN (SELECT c.id FROM competition c WHERE c.competition_type_id = @eoi_competition_type);
+    q.competition_id IN (
+        SELECT c.id
+        FROM competition c
+        WHERE
+            c.competition_type_id = @eoi_competition_type OR
+            c.template AND c.name = 'Template for the Expression of interest competition type'
+        );
