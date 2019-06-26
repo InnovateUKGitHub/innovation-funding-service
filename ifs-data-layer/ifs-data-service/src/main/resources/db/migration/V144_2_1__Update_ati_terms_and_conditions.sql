29a03f4b0db8743d4eb6d202f2873caedd45bed1
Tom Baldwin <tom@polymorphism.co.uk>
2019-06-26 12:03:11 +0100

    IFS-5980 - Fixes ati t&c patch to only update the template competition.

--- ifs-data-layer/ifs-data-service/src/main/resources/db/migration/V144_2_1__Update_ati_terms_and_conditions.sql
+++ ifs-data-layer/ifs-data-service/src/main/resources/db/migration/V144_2_1__Update_ati_terms_and_conditions.sql
@@ -1,15 +1,18 @@
--- IFS-5980 update ati terms to point to the new template
+-- IFS-5980 update ati terms to point to the new terms template
 
 SET @system_maintenance_user_id = (
     SELECT id
     FROM user
     WHERE email = 'ifs_system_maintenance_user@innovateuk.org');
 
-UPDATE terms_and_conditions
-SET
-    template = 'ati-terms-and-conditions-v3',
-    version = '3',
-    modified_by = @system_maintenance_user_id,
-    modified_on = now()
+SET @new_terms_id = 11;
+
+INSERT INTO terms_and_conditions (id, name, template, version, type, created_by, created_on, modified_by, modified_on)
+VALUES (@new_terms_id, 'Aerospace Technology Institute (ATI)', 'ati-terms-and-conditions-v3', 3, 'GRANT',
+        @system_maintenance_user_id, now(), @system_maintenance_user_id, now());
+
+-- update the ati template competition to point to the new t&cs
+UPDATE competition c
+SET terms_and_conditions_id = @new_terms_id
 WHERE
-    id = 8;
+      c.name = 'Template for the Aerospace Technology Institute competition type' AND c.template;
