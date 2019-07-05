a6a7dd3b598d762e6f1f7b190d684a80ad2f4e59
Luke Harper <lharper@worth.systems>
2019-07-05 07:43:43 +0100

    IFS-6062 added unit test.

---
+++ ifs-web-service/ifs-project-setup-mgt-service/src/test/java/org/innovateuk/ifs/project/activitylog/populator/ActivityLogUrlHelperTest.java
@@ -0,0 +1,42 @@
+package org.innovateuk.ifs.project.activitylog.populator;
+
+import org.innovateuk.ifs.activitylog.resource.ActivityLogResourceBuilder;
+import org.innovateuk.ifs.activitylog.resource.ActivityType;
+import org.innovateuk.ifs.project.resource.ProjectResource;
+import org.innovateuk.ifs.threads.resource.FinanceChecksSectionType;
+import org.junit.Test;
+
+import static java.util.Arrays.stream;
+import static org.innovateuk.ifs.activitylog.resource.ActivityLogResourceBuilder.newActivityLogResource;
+import static org.innovateuk.ifs.project.builder.ProjectResourceBuilder.newProjectResource;
+import static org.junit.Assert.assertNotNull;
+
+public class ActivityLogUrlHelperTest {
+
+    @Test
+    public void url() {
+        long projectId = 1L;
+        long applicationId = 2L;
+        long competitionId = 3L;
+        long queryId = 4L;
+        long documentId = 5L;
+        long organisationId = 6L;
+
+        ProjectResource project = newProjectResource()
+                .withId(projectId)
+                .withApplication(applicationId)
+                .withCompetition(competitionId)
+                .build();
+        ActivityLogResourceBuilder activity = newActivityLogResource()
+                .withQuery(queryId)
+                .withQueryType(FinanceChecksSectionType.VIABILITY)
+                .withDocumentConfig(documentId)
+                .withOrganisation(organisationId);
+
+        //Test all enum entries have a URL.
+        stream(ActivityType.values()).forEach((type) ->
+            assertNotNull(ActivityLogUrlHelper.url(activity.withActivityType(type).build(), project))
+        );
+
+    }
+}
