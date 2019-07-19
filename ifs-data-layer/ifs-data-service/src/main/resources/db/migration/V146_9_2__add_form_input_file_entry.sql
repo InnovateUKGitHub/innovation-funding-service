5267b1e7beb4227bed2b89e07ed636535440e3b7
Luke Harper <lharper@worth.systems>
2019-07-19 13:41:13 +0100

    IFS-6186 cleaning up flyway patches.

--- ifs-data-layer/ifs-data-service/src/main/resources/db/migration/V146_9_2__add_form_input_file_entry.sql
+++ ifs-data-layer/ifs-data-service/src/main/resources/db/migration/V146_9_2__add_form_input_file_entry.sql
@@ -1,30 +1,17 @@
+-- IFS-6186 Template form input type
+
+-- Add new form input field for the template document
 ALTER TABLE form_input ADD COLUMN file_entry_id BIGINT(20);
 ALTER TABLE form_input ADD CONSTRAINT fk_form_input_file_entry FOREIGN KEY (file_entry_id) REFERENCES file_entry(id);
+
+-- This column is no longer used.
 ALTER TABLE form_input DROP COLUMN allowed_file_types;
 -- check with mark b above
 
-DELETE FROM form_input_type WHERE id in (1, 3, 18);
+-- Insert new form input type for template document.
 INSERT INTO form_input_type (id, name) VALUES ('29', 'TEMPLATE_UPLOAD');
 
--- Appendix shouldn't exist for your organisation
-DELETE aft FROM appendix_file_types aft
-INNER JOIN form_input fi
-ON fi.id = aft.form_input_id
-INNER JOIN question q
-ON q.id = fi.question_id
-INNER JOIN section s
-ON q.section_id = s.id
-WHERE s.section_type = 'ORGANISATION_FINANCES'
-AND fi.form_input_type_id = 4;
-
-DELETE fi FROM form_input fi
-INNER JOIN question q
-ON q.id = fi.question_id
-INNER JOIN section s
-ON q.section_id = s.id
-WHERE s.section_type = 'ORGANISATION_FINANCES'
-AND fi.form_input_type_id = 4;
-
+-- Insert inactive template document form inputs wherever there is an appendix form input.
 INSERT INTO form_input (word_count, form_input_type_id, competition_id, included_in_application_summary, description, guidance_title, guidance_answer, priority, question_id, scope, active)
 
 SELECT NULL                 as word_count,
