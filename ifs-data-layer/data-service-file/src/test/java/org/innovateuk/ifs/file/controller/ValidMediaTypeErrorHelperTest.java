07a77be2f71c97f10e9c01dd7b41dd7d1da3cdd9
Luke Harper <lharper@worth.systems>
2019-07-26 11:46:45 +0100

    IFS-6181 adding validation.

--- ifs-data-layer/data-service-file/src/test/java/org/innovateuk/ifs/file/controller/ValidMediaTypeErrorHelperTest.java
+++ ifs-data-layer/data-service-file/src/test/java/org/innovateuk/ifs/file/controller/ValidMediaTypeErrorHelperTest.java
@@ -14,7 +14,7 @@ import static org.junit.Assert.assertEquals;
 import static org.springframework.http.HttpStatus.UNSUPPORTED_MEDIA_TYPE;
 
 public class ValidMediaTypeErrorHelperTest {
-    
+
     @Test
     public void testPdfOnlyMessage() {
         assertSpecialisedMessageProduced(
