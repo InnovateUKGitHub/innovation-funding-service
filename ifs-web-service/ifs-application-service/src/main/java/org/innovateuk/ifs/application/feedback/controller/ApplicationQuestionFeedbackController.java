393a4594f890cd4a420a2d323c0835901ef69b8f
Kieran Hester <khester@worth.systems>
2019-08-06 07:33:59 +0100

    IFS-6283 add team page

--- ifs-web-service/ifs-application-service/src/main/java/org/innovateuk/ifs/application/feedback/controller/ApplicationQuestionFeedbackController.java
+++ ifs-web-service/ifs-application-service/src/main/java/org/innovateuk/ifs/application/feedback/controller/ApplicationQuestionFeedbackController.java
@@ -1,9 +1,13 @@
 package org.innovateuk.ifs.application.feedback.controller;
 
 import org.innovateuk.ifs.application.feedback.populator.AssessorQuestionFeedbackPopulator;
+import org.innovateuk.ifs.application.forms.questions.team.form.ApplicationTeamForm;
+import org.innovateuk.ifs.application.forms.questions.team.populator.ApplicationTeamPopulator;
 import org.innovateuk.ifs.application.resource.ApplicationResource;
 import org.innovateuk.ifs.application.service.ApplicationRestService;
+import org.innovateuk.ifs.application.service.QuestionRestService;
 import org.innovateuk.ifs.commons.security.SecuredBySpring;
+import org.innovateuk.ifs.form.resource.QuestionResource;
 import org.innovateuk.ifs.interview.service.InterviewAssignmentRestService;
 import org.innovateuk.ifs.user.resource.UserResource;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +18,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+
 @Controller
 @RequestMapping("/application")
 public class ApplicationQuestionFeedbackController {
@@ -21,15 +26,23 @@ public class ApplicationQuestionFeedbackController {
     private ApplicationRestService applicationRestService;
     private InterviewAssignmentRestService interviewAssignmentRestService;
     private AssessorQuestionFeedbackPopulator assessorQuestionFeedbackPopulator;
+    private QuestionRestService questionRestService;
+    private ApplicationTeamPopulator applicationTeamPopulator;
 
     public ApplicationQuestionFeedbackController() {
     }
 
     @Autowired
-    public ApplicationQuestionFeedbackController(ApplicationRestService applicationRestService, InterviewAssignmentRestService interviewAssignmentRestService, AssessorQuestionFeedbackPopulator assessorQuestionFeedbackPopulator) {
+    public ApplicationQuestionFeedbackController(ApplicationRestService applicationRestService,
+                                                 InterviewAssignmentRestService interviewAssignmentRestService,
+                                                 AssessorQuestionFeedbackPopulator assessorQuestionFeedbackPopulator,
+                                                 QuestionRestService questionRestService,
+                                                 ApplicationTeamPopulator applicationTeamPopulator) {
         this.applicationRestService = applicationRestService;
         this.interviewAssignmentRestService = interviewAssignmentRestService;
         this.assessorQuestionFeedbackPopulator = assessorQuestionFeedbackPopulator;
+        this.questionRestService = questionRestService;
+        this.applicationTeamPopulator = applicationTeamPopulator;
     }
 
     @GetMapping(value = "/{applicationId}/question/{questionId}/feedback")
@@ -48,7 +61,16 @@ public class ApplicationQuestionFeedbackController {
             return "redirect:/application/" + applicationId + "/summary";
         }
 
-        model.addAttribute("model", assessorQuestionFeedbackPopulator.populate(applicationResource, questionId));
+        QuestionResource questionResource = questionRestService.findById(questionId).getSuccess();
+
+        switch(questionResource.getQuestionSetupType()) {
+            case APPLICATION_TEAM:
+                model.addAttribute("form", new ApplicationTeamForm());
+                model.addAttribute("model", applicationTeamPopulator.populate(applicationId, questionId, user));
+                return "application/questions/application-team";
+        }
+
+        model.addAttribute("model", assessorQuestionFeedbackPopulator.populate(applicationResource, questionId, user, model));
         return "application-assessor-feedback";
 
     }
--- ifs-web-service/ifs-application-service/src/main/java/org/innovateuk/ifs/application/feedback/populator/AssessorQuestionFeedbackPopulator.java
+++ ifs-web-service/ifs-application-service/src/main/java/org/innovateuk/ifs/application/feedback/populator/AssessorQuestionFeedbackPopulator.java
@@ -1,6 +1,9 @@
 package org.innovateuk.ifs.application.feedback.populator;
 
 import org.innovateuk.ifs.application.feedback.viewmodel.AssessQuestionFeedbackViewModel;
+import org.innovateuk.ifs.application.forms.questions.researchcategory.form.ResearchCategoryForm;
+import org.innovateuk.ifs.application.forms.questions.researchcategory.populator.ApplicationResearchCategoryFormPopulator;
+import org.innovateuk.ifs.application.forms.questions.researchcategory.populator.ApplicationResearchCategoryModelPopulator;
 import org.innovateuk.ifs.application.resource.ApplicationResource;
 import org.innovateuk.ifs.application.resource.FormInputResponseResource;
 import org.innovateuk.ifs.application.service.QuestionRestService;
@@ -9,10 +12,14 @@ import org.innovateuk.ifs.assessment.resource.AssessmentFeedbackAggregateResourc
 import org.innovateuk.ifs.assessment.service.AssessorFormInputResponseRestService;
 import org.innovateuk.ifs.form.resource.QuestionResource;
 import org.innovateuk.ifs.form.service.FormInputResponseRestService;
+import org.innovateuk.ifs.user.resource.UserResource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.ui.Model;
 
 import java.util.List;
+
+import static org.innovateuk.ifs.question.resource.QuestionSetupType.RESEARCH_CATEGORY;
 
 /**
  * Populator for the individual question assessor feedback page
@@ -30,9 +37,15 @@ public class AssessorQuestionFeedbackPopulator {
     private FeedbackNavigationPopulator feedbackNavigationPopulator;
 
     @Autowired
+    private ApplicationResearchCategoryModelPopulator applicationResearchCategoryModelPopulator;
+
+    @Autowired
+    private ApplicationResearchCategoryFormPopulator applicationResearchCategoryFormPopulator;
+
+    @Autowired
     private AssessorFormInputResponseRestService assessorFormInputResponseRestService;
 
-    public AssessQuestionFeedbackViewModel populate(ApplicationResource applicationResource, long questionId) {
+    public AssessQuestionFeedbackViewModel populate(ApplicationResource applicationResource, long questionId, UserResource user, Model model) {
 
         QuestionResource questionResource = questionRestService.findById(questionId).getSuccess();
         long applicationId = applicationResource.getId();
@@ -40,15 +53,22 @@ public class AssessorQuestionFeedbackPopulator {
         List<FormInputResponseResource> responseResource = formInputResponseRestService.getByApplicationIdAndQuestionId(
                 applicationId, questionResource.getId()).getSuccess();
 
+        if (questionResource.getQuestionSetupType() == RESEARCH_CATEGORY) {
+            model.addAttribute("researchCategoryModel", applicationResearchCategoryModelPopulator.populate(
+                    applicationResource, user.getId(), questionId));
+            model.addAttribute("form", applicationResearchCategoryFormPopulator.populate(applicationResource,
+                    new ResearchCategoryForm()));
+        }
+
         AssessmentFeedbackAggregateResource aggregateResource = assessorFormInputResponseRestService
                 .getAssessmentAggregateFeedback(applicationId, questionId)
                 .getSuccess();
         NavigationViewModel navigationViewModel = feedbackNavigationPopulator.addNavigation(questionResource, applicationId);
 
-      return new AssessQuestionFeedbackViewModel(applicationResource,
-              questionResource,
-              responseResource,
-              aggregateResource,
-              navigationViewModel);
+        return new AssessQuestionFeedbackViewModel(applicationResource,
+                questionResource,
+                responseResource,
+                aggregateResource,
+                navigationViewModel);
     }
 }
