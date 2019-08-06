78322537127dc8f265935c9c3b540eb7fd389a4e
Tom Baldwin <tom@polymorphism.co.uk>
2019-08-06 10:22:11 +0100

    IFS-6045 - Updates test for front-door CompetitionController to cover view model.

--- ifs-web-service/ifs-front-door-service/src/main/java/org/innovateuk/ifs/competition/viewmodel/CompetitionTermsViewModel.java
+++ ifs-web-service/ifs-front-door-service/src/main/java/org/innovateuk/ifs/competition/viewmodel/CompetitionTermsViewModel.java
@@ -1,5 +1,8 @@
 package org.innovateuk.ifs.competition.viewmodel;
 
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+
 public class CompetitionTermsViewModel {
 
     private final long competitionId;
@@ -10,5 +13,32 @@ public class CompetitionTermsViewModel {
 
     public long getCompetitionId() {
         return competitionId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+
+        if (o == null || getClass() != o.getClass()) return false;
+
+        CompetitionTermsViewModel that = (CompetitionTermsViewModel) o;
+
+        return new EqualsBuilder()
+                .append(competitionId, that.competitionId)
+                .isEquals();
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder(17, 37)
+                .append(competitionId)
+                .toHashCode();
+    }
+
+    @Override
+    public String toString() {
+        return "CompetitionTermsViewModel{" +
+                "competitionId=" + competitionId +
+                '}';
     }
 }
