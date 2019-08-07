c110069382047237f069a077e03175f8bff026a6
Luke Harper <lharper@worth.systems>
2019-08-07 11:59:51 +0100

    IFS-4776 excluding logic.

--- ifs-web-service/ifs-application-service/src/main/java/org/innovateuk/ifs/application/forms/sections/yourfinances/populator/YourFinancesModelPopulator.java
+++ ifs-web-service/ifs-application-service/src/main/java/org/innovateuk/ifs/application/forms/sections/yourfinances/populator/YourFinancesModelPopulator.java
@@ -8,10 +8,15 @@ import org.innovateuk.ifs.application.resource.ApplicationResource;
 import org.innovateuk.ifs.application.service.ApplicationRestService;
 import org.innovateuk.ifs.application.service.SectionRestService;
 import org.innovateuk.ifs.application.service.SectionStatusRestService;
+import org.innovateuk.ifs.commons.rest.RestResult;
 import org.innovateuk.ifs.competition.resource.CompetitionResource;
 import org.innovateuk.ifs.competition.service.CompetitionRestService;
 import org.innovateuk.ifs.form.resource.SectionResource;
+import org.innovateuk.ifs.form.resource.SectionType;
+import org.innovateuk.ifs.organisation.resource.OrganisationResource;
+import org.innovateuk.ifs.organisation.resource.OrganisationTypeEnum;
 import org.innovateuk.ifs.user.resource.UserResource;
+import org.innovateuk.ifs.user.service.OrganisationRestService;
 import org.innovateuk.ifs.user.service.UserRestService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -42,6 +47,9 @@ public class YourFinancesModelPopulator {
     private ApplicationUrlHelper applicationUrlHelper;
 
     @Autowired
+    private OrganisationRestService organisationRestService;
+
+    @Autowired
     private ApplicationFinanceSummaryViewModelPopulator applicationFinanceSummaryViewModelPopulator;
 
     public YourFinancesViewModel populate(long applicationId, long sectionId, long organisationId, UserResource user) {
@@ -49,15 +57,29 @@ public class YourFinancesModelPopulator {
         ApplicationResource application = applicationRestService.getApplicationById(applicationId).getSuccess();
         CompetitionResource competition = competitionRestService.getCompetitionById(application.getCompetition()).getSuccess();
         List<Long> completedSections = sectionStatusRestService.getCompletedSectionIds(applicationId, organisationId).getSuccess();
+        OrganisationResource organisation = organisationRestService.getOrganisationById(organisationId).getSuccess();
 
-        List<YourFinancesRowViewModel> rows = yourFinances.getChildSections().stream().map(subSectionId -> {
-            SectionResource subSection = sectionRestService.getById(subSectionId).getSuccess();
-            return new YourFinancesRowViewModel(subSection.getName(),
-                    applicationUrlHelper.getSectionUrl(subSection.getType(), subSectionId, applicationId, organisationId, application.getCompetition()).get(),
-                    completedSections.contains(subSection.getId()));
-        }).collect(toList());
+        List<YourFinancesRowViewModel> rows = yourFinances.getChildSections().stream()
+                .map(sectionRestService::getById)
+                .map(RestResult::getSuccess)
+                .filter(subSection -> !isSectionExcluded(subSection, competition, organisation))
+                .map(subSection ->
+                        new YourFinancesRowViewModel(subSection.getName(),
+                                applicationUrlHelper.getSectionUrl(subSection.getType(), subSection.getId(), applicationId, organisationId, application.getCompetition()).get(),
+                                completedSections.contains(subSection.getId()))
+                ).collect(toList());
         return new YourFinancesViewModel(applicationId, application.getName(), competition,
                 applicationFinanceSummaryViewModelPopulator.populate(applicationId, user),
                 rows);
+    }
+
+    private boolean isSectionExcluded(SectionResource section, CompetitionResource competition, OrganisationResource organisation) {
+        if (section.getType() == SectionType.ORGANISATION_FINANCES) {
+            boolean isResearchOrganisation = organisation != null && OrganisationTypeEnum.RESEARCH.getId() == organisation.getOrganisationType();
+            boolean excludeYourOrganisationSectionForResearchOrgs =
+                    Boolean.FALSE.equals(competition.getIncludeYourOrganisationSection());
+            return isResearchOrganisation && excludeYourOrganisationSectionForResearchOrgs;
+        }
+        return section.getType() == SectionType.FUNDING_FINANCES && competition.isFullyFunded();
     }
 }
