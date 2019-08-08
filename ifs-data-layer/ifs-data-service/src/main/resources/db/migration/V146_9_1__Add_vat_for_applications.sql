2be397176bafa60ec5931cbefb1fd7690ba8e29c
Kieran Hester <khester@worth.systems>
2019-08-08 10:00:39 +0100

    IFS-5098 update script

--- ifs-data-layer/ifs-data-service/src/main/resources/db/migration/V146_9_1__Add_vat_for_applications.sql
+++ ifs-data-layer/ifs-data-service/src/main/resources/db/migration/V146_9_1__Add_vat_for_applications.sql
@@ -1,5 +1,5 @@
 -- IFS-5098 add vat for procurement competitions
-
+-- Application finance
 INSERT INTO finance_row (`cost`, `description`, `item`, `quantity`, `name`, `target_id`, `row_type`, `type`)
 SELECT
 '0' AS cost,
@@ -10,4 +10,17 @@ SELECT
 target_id AS target_id,
 'ApplicationFinanceRow' AS row_type,
 'VAT' AS type
- FROM finance_row GROUP BY target_id;
+ FROM finance_row where row_type = 'ApplicationFinanceRow' GROUP BY target_id;
+
+--  Project finance
+INSERT INTO finance_row (`cost`, `description`, `item`, `quantity`, `name`, `target_id`, `row_type`, `type`)
+SELECT
+'0' AS cost,
+'VAT' AS cost,
+'false' AS cost,
+'0' AS cost,
+'vat' AS cost,
+target_id AS target_id,
+'ProjectFinanceRow' AS row_type,
+'VAT' AS type
+ FROM finance_row where row_type = 'ProjectFinanceRow' GROUP BY target_id;
