bf90d86d7fd7ab18dc0e0d0ea0c1916ecba05d41
Luke Harper <lharper@worth.systems>
2019-08-19 09:58:53 +0100

    IFS-6242 fixing compilation.

--- ifs-data-layer/ifs-data-service/src/main/java/org/innovateuk/ifs/finance/handler/item/VatHandler.java
+++ ifs-data-layer/ifs-data-service/src/main/java/org/innovateuk/ifs/finance/handler/item/VatHandler.java
@@ -5,6 +5,7 @@ import org.innovateuk.ifs.finance.domain.ApplicationFinanceRow;
 import org.innovateuk.ifs.finance.domain.FinanceRow;
 import org.innovateuk.ifs.finance.domain.ProjectFinanceRow;
 import org.innovateuk.ifs.finance.resource.cost.FinanceRowItem;
+import org.innovateuk.ifs.finance.resource.cost.FinanceRowType;
 import org.innovateuk.ifs.finance.resource.cost.Vat;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.BindingResult;
@@ -26,6 +27,11 @@ public class VatHandler extends FinanceRowHandler<Vat> {
     @Override
     public FinanceRowItem toResource(FinanceRow cost) {
         return buildRowItem(cost);
+    }
+
+    @Override
+    public FinanceRowType getFinanceRowType() {
+        return FinanceRowType.VAT;
     }
 
     private FinanceRowItem buildRowItem(FinanceRow cost){
