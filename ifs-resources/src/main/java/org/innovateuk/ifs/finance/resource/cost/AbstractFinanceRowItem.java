63d89aa12b84ba6979eeb6272d2c58d5e8e7f8ea
Luke Harper <lharper@worth.systems>
2019-08-19 10:59:02 +0100

    IFS-6242 fixing unit tests.

--- ifs-resources/src/main/java/org/innovateuk/ifs/finance/resource/cost/AbstractFinanceRowItem.java
+++ ifs-resources/src/main/java/org/innovateuk/ifs/finance/resource/cost/AbstractFinanceRowItem.java
@@ -1,8 +1,5 @@
 package org.innovateuk.ifs.finance.resource.cost;
 
-import org.apache.commons.lang3.builder.EqualsBuilder;
-import org.apache.commons.lang3.builder.HashCodeBuilder;
-
 import java.math.BigDecimal;
 
 /**
@@ -30,25 +27,5 @@ public abstract class AbstractFinanceRowItem implements FinanceRowItem {
 
     public void setTargetId(Long targetId) {
         this.targetId = targetId;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-
-        if (o == null || getClass() != o.getClass()) return false;
-
-        AbstractFinanceRowItem that = (AbstractFinanceRowItem) o;
-
-        return new EqualsBuilder()
-                .append(targetId, that.targetId)
-                .isEquals();
-    }
-
-    @Override
-    public int hashCode() {
-        return new HashCodeBuilder(17, 37)
-                .append(targetId)
-                .toHashCode();
     }
 }
