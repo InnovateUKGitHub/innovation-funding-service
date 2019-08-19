896123ff30a26eec60d7a074ef4bd649fc630e63
Luke Harper <lharper@worth.systems>
2019-08-19 15:46:12 +0100

    IFS-6242 fixing grant claim amount.

--- ifs-data-layer/ifs-data-service/src/main/resources/db/migration/V146_12_1__new_grant_claim_amount.sql
+++ ifs-data-layer/ifs-data-service/src/main/resources/db/migration/V146_12_1__new_grant_claim_amount.sql
@@ -3,6 +3,7 @@
 ALTER TABLE finance_row MODIFY COLUMN type enum (
     'LABOUR',
     'OVERHEADS',
+    'PROCUREMENT_OVERHEADS',
     'MATERIALS',
     'CAPITAL_USAGE',
     'SUBCONTRACTING_COSTS',
@@ -11,12 +12,14 @@ ALTER TABLE finance_row MODIFY COLUMN type enum (
     'FINANCE',
     'OTHER_FUNDING',
     'YOUR_FINANCE',
+    'VAT',
     'GRANT_CLAIM_AMOUNT'
 );
 
 ALTER TABLE competition_finance_row_types MODIFY COLUMN finance_row_type enum (
     'LABOUR',
     'OVERHEADS',
+    'PROCUREMENT_OVERHEADS',
     'MATERIALS',
     'CAPITAL_USAGE',
     'SUBCONTRACTING_COSTS',
@@ -25,5 +28,6 @@ ALTER TABLE competition_finance_row_types MODIFY COLUMN finance_row_type enum (
     'FINANCE',
     'OTHER_FUNDING',
     'YOUR_FINANCE',
+    'VAT',
     'GRANT_CLAIM_AMOUNT'
 ) NOT NULL;
