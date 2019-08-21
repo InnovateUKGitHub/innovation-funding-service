ac265d34c1925fe343143d13a6856257a58f5138
Luke Harper <lharper@worth.systems>
2019-08-21 11:36:57 +0100

    IFS-6208 Column ordering.

--- ifs-data-layer/ifs-data-service/src/test/java/org/innovateuk/ifs/testdata/builders/CompetitionDataBuilder.java
+++ ifs-data-layer/ifs-data-service/src/test/java/org/innovateuk/ifs/testdata/builders/CompetitionDataBuilder.java
@@ -205,7 +205,7 @@ public class CompetitionDataBuilder extends BaseDataBuilder<CompetitionData, Com
     public CompetitionDataBuilder withApplicationFormFromTemplate() {
 
         return asCompAdmin(data -> {
-
+            
             CompetitionResource competition = data.getCompetition();
 
             competitionSetupService.copyFromCompetitionTypeTemplate(competition.getId(), competition.getCompetitionType()).
