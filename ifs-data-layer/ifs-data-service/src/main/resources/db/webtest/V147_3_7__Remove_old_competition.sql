ca3d62b67c636b19893d4e7c7bf8c06a4a188afc
Kieran Hester <khester@worth.systems>
2019-09-05 18:35:55 +0100

    IFS-6307 fix patches

--- ifs-data-layer/ifs-data-service/src/main/resources/db/webtest/V147_3_7__Remove_old_competition.sql
+++ ifs-data-layer/ifs-data-service/src/main/resources/db/webtest/V147_3_7__Remove_old_competition.sql
@@ -30,8 +30,6 @@ delete from section where competition_id in (1,2,3,4,5,6,7,8,9);
 
 delete from competition_finance_row_types where competition_id in (1,2,3,4,5,6,7,8,9);
 
-delete from project_stages where competition_id in (1,2,3,4,5,6,7,8,9);
-
 delete from milestone where competition_id in (1,2,3,4,5,6,7,8,9);
 
 update competition_type set template_competition_id = NULL where name = 'Programme';
