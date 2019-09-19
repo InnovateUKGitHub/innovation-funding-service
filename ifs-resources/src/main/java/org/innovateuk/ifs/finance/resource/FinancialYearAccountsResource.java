c7c5ba8d2353956e192d18600214f43edf1e781a
Luke Harper <lharper@worth.systems>
2019-09-19 17:25:39 +0100

    IFS-6499 json.

--- ifs-resources/src/main/java/org/innovateuk/ifs/finance/resource/FinancialYearAccountsResource.java
+++ ifs-resources/src/main/java/org/innovateuk/ifs/finance/resource/FinancialYearAccountsResource.java
@@ -1,6 +1,14 @@
 package org.innovateuk.ifs.finance.resource;
 
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import org.innovateuk.ifs.finance.resource.cost.*;
 
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "type")
+@JsonSubTypes({
+        @JsonSubTypes.Type(value = EmployeesAndTurnoverResource.class, name = "EmployeesAndTurnover"),
+        @JsonSubTypes.Type(value = GrowthTableResource.class, name = "GrowthTable")
+})
 public abstract class FinancialYearAccountsResource {
     private Long employees;
 
