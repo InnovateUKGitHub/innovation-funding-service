af504c9609099a8ad90c142554869ac4d2701e1c
Kieran Hester <khester@worth.systems>
2019-09-23 15:04:04 +0100

    IFS-6309 update users

--- setup-files/scripts/docker/add-performance-users.sh
+++ setup-files/scripts/docker/add-performance-users.sh
@@ -40,3 +40,13 @@ do
     INSERT INTO process_role (application_id, organisation_id, role_id, user_id) VALUES ($application_id, '21', '1', @inserted_user_id);
     INSERT INTO user_terms_and_conditions (user_id, terms_and_conditions_id, accepted_date) VALUES (@inserted_user_id, '9', now());"
 done
+
+for (( u=2; u<=${PERFORMANCE_h2020_USERS}; u++ ))
+do
+    email="h2020.perf.applicant${u}@example.com"
+    executeMySQLCommand "INSERT INTO user (email, first_name, last_name, status, uid, system_user, allow_marketing_emails, created_by, created_on, modified_by, modified_on) VALUES ('$email', 'firstName', 'lastName', 'ACTIVE', UUID(), '0', '0', '16', now(), '16', now());
+    SET @inserted_user_id = last_insert_id();
+    INSERT INTO user_role (role_id, user_id) values (4, @inserted_user_id);
+    INSERT INTO process_role (application_id, organisation_id, role_id, user_id) VALUES ($application_id, '21', '1', @inserted_user_id);
+    INSERT INTO user_terms_and_conditions (user_id, terms_and_conditions_id, accepted_date) VALUES (@inserted_user_id, '9', now());"
+done
