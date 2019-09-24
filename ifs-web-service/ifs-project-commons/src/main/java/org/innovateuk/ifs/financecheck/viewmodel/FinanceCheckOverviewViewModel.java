2cc37ee5a74d3e64fab3314395938827d722e581
Adam Mapstone <adam.mapstone@innovateuk.gov.uk>
2019-09-24 13:27:45 +0100

    IFS-6298 - Further isLoan changes

--- ifs-web-service/ifs-project-commons/src/main/java/org/innovateuk/ifs/financecheck/viewmodel/FinanceCheckOverviewViewModel.java
+++ ifs-web-service/ifs-project-commons/src/main/java/org/innovateuk/ifs/financecheck/viewmodel/FinanceCheckOverviewViewModel.java
@@ -7,16 +7,19 @@ public class FinanceCheckOverviewViewModel {
     private final ProjectFinanceOverviewViewModel overview;
     private final FinanceCheckSummariesViewModel summaries;
     private final ProjectFinanceCostBreakdownViewModel breakdown;
+    private final boolean loanCompetition;
 
     private final long applicationId;
     private final boolean canChangeFunding;
 
-    public FinanceCheckOverviewViewModel(ProjectFinanceOverviewViewModel overview, FinanceCheckSummariesViewModel summaries, ProjectFinanceCostBreakdownViewModel breakdown, long applicationId, boolean canChangeFunding) {
+    public FinanceCheckOverviewViewModel(ProjectFinanceOverviewViewModel overview, FinanceCheckSummariesViewModel summaries, ProjectFinanceCostBreakdownViewModel breakdown, long applicationId,
+                                         boolean canChangeFunding, boolean loanCompetition) {
         this.overview = overview;
         this.summaries = summaries;
         this.breakdown = breakdown;
         this.applicationId = applicationId;
         this.canChangeFunding = canChangeFunding;
+        this.loanCompetition = loanCompetition;
     }
 
     public ProjectFinanceOverviewViewModel getOverview() {
@@ -37,5 +40,9 @@ public class FinanceCheckOverviewViewModel {
 
     public boolean isCanChangeFunding() {
         return canChangeFunding;
+    }
+
+    public boolean isLoanCompetition() {
+        return loanCompetition;
     }
 }
--- ifs-web-service/ifs-project-setup-service/src/main/java/org/innovateuk/ifs/project/financechecks/controller/ProjectFinanceChecksOverviewController.java
+++ ifs-web-service/ifs-project-setup-service/src/main/java/org/innovateuk/ifs/project/financechecks/controller/ProjectFinanceChecksOverviewController.java
@@ -74,7 +74,7 @@ public class ProjectFinanceChecksOverviewController {
         List<PartnerOrganisationResource> partnerOrgs = partnerOrganisationRestService.getProjectPartnerOrganisations(project.getId()).getSuccess();
         CompetitionResource competition = competitionRestService.getCompetitionById(project.getCompetition()).getSuccess();
         return new FinanceCheckOverviewViewModel(null, getProjectFinanceSummaries(project, partnerOrgs, competition),
-                getProjectFinanceCostBreakdown(project.getId(), partnerOrgs, competition), project.getApplication(), false);
+                getProjectFinanceCostBreakdown(project.getId(), partnerOrgs, competition), project.getApplication(), false, competition.isLoan());
     }
 
     private FinanceCheckSummariesViewModel getProjectFinanceSummaries(ProjectResource project, List<PartnerOrganisationResource> partnerOrgs, CompetitionResource competition) {
