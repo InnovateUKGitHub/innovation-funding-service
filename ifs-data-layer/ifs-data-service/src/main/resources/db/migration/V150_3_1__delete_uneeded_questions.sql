a8355ead9e5ed14967d926cf7300aeb745d2aece
Luke Harper <lharper@worth.systems>
2019-10-01 13:42:10 +0100

    Fixed

--- ifs-data-layer/ifs-data-service/src/main/resources/db/migration/V150_3_1__delete_uneeded_questions.sql
+++ ifs-data-layer/ifs-data-service/src/main/resources/db/migration/V150_3_1__delete_uneeded_questions.sql
@@ -48,7 +48,8 @@ DELETE fi FROM form_input fi
         ON q.id = fi.question_id
     INNER JOIN section s
         ON s.id = q.section_id
-    WHERE s.section_type = 'OVERVIEW_FINANCES';
+    WHERE s.section_type = 'OVERVIEW_FINANCES'
+    AND fi.form_input_type_id != 16;
 
 DELETE qs FROM question_status qs
     INNER JOIN question q
