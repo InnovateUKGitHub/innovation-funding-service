f775c03aba57c4225d33193baa605b4fb2f8e9dc
Luke Harper <lharper@worth.systems>
2019-10-09 15:13:19 +0100

    Splitting patches.

--- ifs-data-layer/ifs-data-service/src/main/resources/db/migration/V151_1_14__update_app_finance_employees.sql
+++ ifs-data-layer/ifs-data-service/src/main/resources/db/migration/V151_1_14__update_app_finance_employees.sql
@@ -1,7 +1,3 @@
 UPDATE application_finance af
-    INNER JOIN growth_table g on g.temp_application_finance_id = af.id
-SET af.growth_table_id = g.id;
-
-UPDATE application_finance af
     INNER JOIN employees_and_turnover e on e.temp_application_finance_id = af.id
 SET af.employees_and_turnover_id = e.id;
--- ifs-data-layer/ifs-data-service/src/main/resources/db/migration/V151_1_17__update_project_finance_employees.sql
+++ ifs-data-layer/ifs-data-service/src/main/resources/db/migration/V151_1_17__update_project_finance_employees.sql
@@ -1,7 +1,3 @@
 UPDATE project_finance pf
-    INNER JOIN growth_table g on g.temp_project_finance_id = pf.id
-SET pf.growth_table_id = g.id;
-
-UPDATE project_finance pf
     INNER JOIN employees_and_turnover e on e.temp_project_finance_id = pf.id
 SET pf.employees_and_turnover_id = e.id;
