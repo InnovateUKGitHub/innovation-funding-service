6795eaab9708e1727f8725b58fcf4e1250e789c4
Luke Harper <lharper@worth.systems>
2019-10-11 09:17:20 +0100

    IFS-6347 fixed.

--- ifs-web-service/ifs-project-setup-mgt-service/src/main/java/org/innovateuk/ifs/project/monitoringofficer/viewmodel/LegacyMonitoringOfficerViewModel.java
+++ ifs-web-service/ifs-project-setup-mgt-service/src/main/java/org/innovateuk/ifs/project/monitoringofficer/viewmodel/LegacyMonitoringOfficerViewModel.java
@@ -2,6 +2,7 @@ package org.innovateuk.ifs.project.monitoringofficer.viewmodel;
 
 import org.innovateuk.ifs.address.resource.AddressResource;
 import org.innovateuk.ifs.application.resource.CompetitionSummaryResource;
+import org.innovateuk.ifs.project.resource.ProjectResource;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -26,25 +27,27 @@ public class LegacyMonitoringOfficerViewModel {
     private boolean editMode;
     private boolean editable;
     private List<String> primaryAddressLines;
+    private boolean collaborativeProject;
 
-    public LegacyMonitoringOfficerViewModel(Long projectId, String projectTitle, Long applicationId, String area, AddressResource primaryAddress,
-                                            LocalDate targetProjectStartDate, String projectManagerName,
+    public LegacyMonitoringOfficerViewModel(ProjectResource project, String area, String projectManagerName,
                                             List<String> partnerOrganisationNames, String leadOrganisationName,
-                                            CompetitionSummaryResource competitionSummary, boolean existingMonitoringOfficer,
+                                            CompetitionSummaryResource competitionSummary,
                                             boolean editMode, boolean editable) {
-        this.projectId = projectId;
-        this.projectTitle = projectTitle;
-        this.applicationId = applicationId;
+        this.projectId = project.getId();
+        this.projectTitle = project.getName();
+        this.applicationId = project.getApplication();
         this.area = area;
+        AddressResource primaryAddress = project.getAddress();
         this.primaryAddressLines = primaryAddress != null ? primaryAddress.getNonEmptyLines() : emptyList();
-        this.targetProjectStartDate = targetProjectStartDate;
+        this.targetProjectStartDate = project.getTargetStartDate();
         this.projectManagerName = projectManagerName;
         this.partnerOrganisationNames = partnerOrganisationNames;
         this.leadOrganisationName = leadOrganisationName;
         this.competitionSummary = competitionSummary;
-        this.existingMonitoringOfficer = existingMonitoringOfficer;
+        this.existingMonitoringOfficer = project.getProjectMonitoringOfficer() != null;
         this.editMode = editMode;
         this.editable = editable;
+        this.collaborativeProject = project.isCollaborativeProject();
     }
 
     public Long getProjectId() {
@@ -117,5 +120,9 @@ public class LegacyMonitoringOfficerViewModel {
 
     public boolean isEditable() {
         return editable;
+    }
+
+    public boolean isCollaborativeProject() {
+        return collaborativeProject;
     }
 }
