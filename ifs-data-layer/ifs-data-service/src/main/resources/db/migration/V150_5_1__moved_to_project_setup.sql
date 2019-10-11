6e4d5ba0582e6da5955fa88a40d9a5ce7bfe3503
Luke Harper <lharper@worth.systems>
2019-10-11 09:24:59 +0100

    IFS-6284 fixed review comments.

--- ifs-data-layer/ifs-data-service/src/main/resources/db/migration/V150_5_1__moved_to_project_setup.sql
+++
@@ -1,8 +0,0 @@
-
-ALTER TABLE competition ADD COLUMN project_setup_started datetime;
-
-UPDATE competition c
-inner join application app on app.competition = c.id
-inner join project p on p.application_id=app.id
-SET c.project_setup_started = (select min(manage_funding_email_date) from application where competition = c.id)
-where p.id is not null;
