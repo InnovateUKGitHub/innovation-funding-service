9ae2dd33fc52db785e400f9cf19dff0fe0bb880d
Grace Williams <grace.williams@innovateuk.gov.uk>
2019-10-17 11:50:18 +0100

    Builder added and AssessmentCompetitionDashboard unit test

--- ifs-data-layer/ifs-data-service/src/main/java/org/innovateuk/ifs/assessment/dashboard/transactional/ApplicationAssessmentServiceImpl.java
+++ ifs-data-layer/ifs-data-service/src/main/java/org/innovateuk/ifs/assessment/dashboard/transactional/ApplicationAssessmentServiceImpl.java
@@ -36,7 +36,7 @@ public class ApplicationAssessmentServiceImpl extends RootTransactionalService i
                 .collect(toList()));
     }
 
-    public ApplicationAssessmentResource mapToResource(Assessment assessment) {
+    private ApplicationAssessmentResource mapToResource(Assessment assessment) {
         Optional<Organisation> leadOrganisation = organisationRepository.findById(assessment.getTarget().getLeadOrganisationId());
 
         return new ApplicationAssessmentResource(
---
+++ ifs-resources/src/test/java/org/innovateuk/ifs/assessment/builder/ApplicationAssessmentResourceBuilder.java
@@ -0,0 +1,59 @@
+package org.innovateuk.ifs.assessment.builder;
+
+import org.innovateuk.ifs.BaseBuilder;
+import org.innovateuk.ifs.assessment.resource.AssessmentState;
+import org.innovateuk.ifs.assessment.resource.dashboard.ApplicationAssessmentResource;
+
+import java.util.List;
+import java.util.function.BiConsumer;
+
+import static java.util.Collections.emptyList;
+
+public class ApplicationAssessmentResourceBuilder extends BaseBuilder<ApplicationAssessmentResource, ApplicationAssessmentResourceBuilder> {
+
+    public static ApplicationAssessmentResourceBuilder newApplicationAssessmentResource() {
+        return  new ApplicationAssessmentResourceBuilder(emptyList());
+    }
+
+    protected ApplicationAssessmentResourceBuilder(List<BiConsumer<Integer, ApplicationAssessmentResource>> newActions) {
+        super(newActions);
+    }
+
+    @Override
+    protected ApplicationAssessmentResourceBuilder createNewBuilderWithActions(List<BiConsumer<Integer, ApplicationAssessmentResource>> actions) {
+        return new ApplicationAssessmentResourceBuilder(actions);
+    }
+
+    @Override
+    protected ApplicationAssessmentResource createInitial() {
+        return new ApplicationAssessmentResource();
+    }
+
+    public ApplicationAssessmentResourceBuilder withApplicationId(Long... applicationIds) {
+        return withArraySetFieldByReflection("applicationId", applicationIds);
+    }
+
+    public ApplicationAssessmentResourceBuilder withAssessmentId(Long... assessmentIds) {
+        return withArraySetFieldByReflection("assessmentId", assessmentIds);
+    }
+
+    public ApplicationAssessmentResourceBuilder withCompetitionName(String... competitionNames) {
+        return withArraySetFieldByReflection("competitionName", competitionNames);
+    }
+
+    public ApplicationAssessmentResourceBuilder withLeadOrganisation(String... leadOrganisations) {
+        return withArraySetFieldByReflection("leadOrganisation", leadOrganisations);
+    }
+
+    public ApplicationAssessmentResourceBuilder withState(AssessmentState... states) {
+        return withArraySetFieldByReflection("state", states);
+    }
+
+    public ApplicationAssessmentResourceBuilder withOverallScore(Integer... overallScores) {
+        return withArraySetFieldByReflection("overallScore", overallScores);
+    }
+
+    public ApplicationAssessmentResourceBuilder withRecommended(Boolean... recommended) {
+        return withArraySetFieldByReflection("recommended", recommended);
+    }
+}
