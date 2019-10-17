86b97deb155908d1f8ca254ccb4de5030a5ee306
Calum Brunt <cbrunt@Calums-MacBook-Pro.local>
2019-10-17 17:19:42 +0100

    Remove NPE, reorder html errors and example, write unit tests

--- ifs-web-service/ifs-web-core/src/main/java/org/innovateuk/ifs/address/validation/ValidAddressFormValidator.java
+++ ifs-web-service/ifs-web-core/src/main/java/org/innovateuk/ifs/address/validation/ValidAddressFormValidator.java
@@ -1,12 +1,12 @@
 package org.innovateuk.ifs.address.validation;
 
-import org.innovateuk.ifs.address.form.AddressForm;
-import org.innovateuk.ifs.address.resource.AddressResource;
+import static com.google.common.base.Strings.isNullOrEmpty;
+
 
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
-
-import static com.google.common.base.Strings.isNullOrEmpty;
+import org.innovateuk.ifs.address.form.AddressForm;
+import org.innovateuk.ifs.address.resource.AddressResource;
 
 public class ValidAddressFormValidator implements ConstraintValidator<ValidAddressForm, AddressForm> {
 
@@ -21,29 +21,20 @@ public class ValidAddressFormValidator implements ConstraintValidator<ValidAddre
             if (value.isManualAddressEntry()) {
                 valid = isAddressValid(value.getManualAddress(), context);
             } else if (value.isPostcodeAddressEntry()) {
-                if (value.getSelectedPostcodeIndex() < 0) {
-                    context.disableDefaultConstraintViolation();
-                    context
-                        .buildConstraintViolationWithTemplate("{validation.standard.address.select.required}")
-                        .addPropertyNode("selectedPostcodeIndex").addConstraintViolation();
+                if (value.getSelectedPostcodeIndex() == null || value.getSelectedPostcodeIndex() < 0) {
+                    updateConstraintValidatorContext(context, "{validation.standard.address.select.required}", "selectedPostcodeIndex");
                     valid = false;
                 } else {
                     valid = true;
                 }
             } else {
+                updateConstraintValidatorContext(context, "{validation.standard.address.required}", "postcodeInput");
                 valid = false;
-                context.disableDefaultConstraintViolation();
-                context
-                        .buildConstraintViolationWithTemplate("{validation.standard.address.required}")
-                        .addPropertyNode("postcodeInput").addConstraintViolation();
             }
         } else if (value.getAction().equals(AddressForm.Action.SEARCH_POSTCODE) &&
-                isNullOrEmpty(value.getPostcodeInput())) {
+            isNullOrEmpty(value.getPostcodeInput())) {
+            updateConstraintValidatorContext(context, "{validation.standard.address.search.postcode.required}", "postcodeInput");
             valid = false;
-            context.disableDefaultConstraintViolation();
-            context
-                    .buildConstraintViolationWithTemplate("{validation.standard.address.search.postcode.required}")
-                    .addPropertyNode("postcodeInput").addConstraintViolation();
         } else {
             valid = true;
         }
@@ -54,32 +45,25 @@ public class ValidAddressFormValidator implements ConstraintValidator<ValidAddre
     private boolean isAddressValid(AddressResource address, ConstraintValidatorContext context) {
         boolean valid = true;
         if (isNullOrEmpty(address.getAddressLine1())) {
-            context.disableDefaultConstraintViolation();
-            context
-                    .buildConstraintViolationWithTemplate("{validation.standard.addressline1.required}")
-                    .addPropertyNode("manualAddress.addressLine1").addConstraintViolation();
+            updateConstraintValidatorContext(context, "{validation.standard.addressline1.required}", "manualAddress.addressLine1");
             valid = false;
         }
         if (isNullOrEmpty(address.getTown())) {
-            context.disableDefaultConstraintViolation();
-            context
-                    .buildConstraintViolationWithTemplate("{validation.standard.town.required}")
-                    .addPropertyNode("manualAddress.town").addConstraintViolation();
+            updateConstraintValidatorContext(context, "{validation.standard.town.required}", "manualAddress.town");
             valid = false;
         }
         if (isNullOrEmpty(address.getPostcode())) {
-            context.disableDefaultConstraintViolation();
-            context
-                    .buildConstraintViolationWithTemplate("{validation.standard.postcode.required}")
-                    .addPropertyNode("manualAddress.postcode").addConstraintViolation();
+            updateConstraintValidatorContext(context, "{validation.standard.postcode.required}", "manualAddress.postcode");
             valid = false;
         } else if (address.getPostcode().length() >= 9) {
-            context.disableDefaultConstraintViolation();
-            context
-                    .buildConstraintViolationWithTemplate("{validation.standard.postcode.length}")
-                    .addPropertyNode("manualAddress.postcode").addConstraintViolation();
+            updateConstraintValidatorContext(context, "{validation.standard.postcode.length}", "manualAddress.postcode");
             valid = false;
         }
         return valid;
+    }
+
+    private void updateConstraintValidatorContext(ConstraintValidatorContext context, String messageTemplate, String propertyNodeName) {
+        context.disableDefaultConstraintViolation();
+        context.buildConstraintViolationWithTemplate(messageTemplate).addPropertyNode(propertyNodeName).addConstraintViolation();
     }
 }
