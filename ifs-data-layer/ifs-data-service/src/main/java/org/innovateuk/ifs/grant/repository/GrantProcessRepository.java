44a4d3cc14084125295efd80410d7074a2dcc8df
Luke Harper <lharper@worth.systems>
2019-10-18 08:49:52 +0100

    Send one at a time.

--- ifs-data-layer/ifs-data-service/src/main/java/org/innovateuk/ifs/grant/repository/GrantProcessRepository.java
+++ ifs-data-layer/ifs-data-service/src/main/java/org/innovateuk/ifs/grant/repository/GrantProcessRepository.java
@@ -3,11 +3,16 @@ package org.innovateuk.ifs.grant.repository;
 import org.innovateuk.ifs.grant.domain.GrantProcess;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
+import javax.swing.text.html.Option;
 import java.util.List;
+import java.util.Optional;
 
 public interface GrantProcessRepository extends PagingAndSortingRepository<GrantProcess, Long> {
 
     GrantProcess findOneByApplicationId(long applicationId);
 
+    Optional<GrantProcess> findFirstByPendingIsTrue();
+
     List<GrantProcess> findByPendingIsTrue();
+
 }
