40f18e818d79df6b7ff4eecd91ebebe786479ba1
Luke Harper <lharper@worth.systems>
2019-10-23 09:59:02 +0100

    IFS-6468 splitting status service.

---
+++ ifs-data-layer/ifs-data-service/src/main/java/org/innovateuk/ifs/project/status/transactional/InternalUserProjectStatusService.java
@@ -0,0 +1,23 @@
+package org.innovateuk.ifs.project.status.transactional;
+
+import org.innovateuk.ifs.commons.security.NotSecured;
+import org.innovateuk.ifs.commons.service.ServiceResult;
+import org.innovateuk.ifs.project.core.domain.Project;
+import org.innovateuk.ifs.project.status.resource.ProjectStatusResource;
+import org.springframework.security.access.prepost.PreAuthorize;
+
+import java.util.List;
+
+public interface InternalUserProjectStatusService {
+    @PreAuthorize("hasPermission(#competitionId, 'org.innovateuk.ifs.competition.resource.CompetitionResource', 'VIEW_PROJECT_SETUP_COMPETITION_STATUS')")
+    ServiceResult<List<ProjectStatusResource>> getCompetitionStatus(long competitionId, String applicationSearchString);
+
+    @PreAuthorize("hasPermission(#competitionId, 'org.innovateuk.ifs.competition.resource.CompetitionResource', 'VIEW_PROJECT_SETUP_COMPETITION_STATUS')")
+    ServiceResult<List<ProjectStatusResource>> getPreviousCompetitionStatus(long competitionId);
+
+    @NotSecured(value = "This Service is only used within a secured service for performing validation checks (update of project manager and address)", mustBeSecuredByOtherServices = true)
+    ServiceResult<ProjectStatusResource> getProjectStatusByProject(Project project);
+
+    @PreAuthorize("hasPermission(#projectId, 'org.innovateuk.ifs.project.resource.ProjectResource', 'VIEW_PROJECT_STATUS')")
+    ServiceResult<ProjectStatusResource> getProjectStatusByProjectId(Long projectId);
+}
