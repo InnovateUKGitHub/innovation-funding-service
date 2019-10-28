ca30ba8523b5ce41f2d14e8b0c7da9e783e41647
Kieran Hester <khester@worth.systems>
2019-10-28 09:01:40 +0000

    IFS-6484 seperate file

--- ifs-data-layer/ifs-data-service/src/main/resources/db/migration/V152_5_1__alter_activity_log_constraint.sql
+++ ifs-data-layer/ifs-data-service/src/main/resources/db/migration/V152_5_1__alter_activity_log_constraint.sql
@@ -1,13 +1,8 @@
+-- IFS-6484 alter activity log constraints.
+
 ALTER TABLE activity_log
 DROP FOREIGN KEY fk_activity_log_thread_id;
 
 ALTER TABLE activity_log
 ADD CONSTRAINT fk_activity_log_thread_id
     FOREIGN KEY (thread_id) REFERENCES thread(id) ON DELETE SET NULL;
-
-ALTER TABLE bank_details
-DROP FOREIGN KEY bank_details_to_organisation_address_fk;
-
-ALTER TABLE bank_details
-ADD CONSTRAINT bank_details_to_organisation_address_fk
-    FOREIGN KEY (organisation_address_id) REFERENCES organisation_address (id) ON DELETE CASCADE;
