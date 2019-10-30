850debbd6f1161b0fbe86f0a74a2d89304a30137
Grace Williams <grace.williams@innovateuk.gov.uk>
2019-10-30 12:15:49 +0000

    IFS-6685: Integration test updated

--- ifs-data-layer/ifs-data-service/src/test/java/org/innovateuk/ifs/project/core/repository/PartnerOrganisationRepositoryIntegrationTest.java
+++ ifs-data-layer/ifs-data-service/src/test/java/org/innovateuk/ifs/project/core/repository/PartnerOrganisationRepositoryIntegrationTest.java
@@ -1,6 +1,7 @@
 package org.innovateuk.ifs.project.core.repository;
 
 import org.innovateuk.ifs.BaseRepositoryIntegrationTest;
+import org.innovateuk.ifs.address.domain.Address;
 import org.innovateuk.ifs.application.domain.Application;
 import org.innovateuk.ifs.application.repository.ApplicationRepository;
 import org.innovateuk.ifs.organisation.domain.Organisation;
@@ -8,15 +9,18 @@ import org.innovateuk.ifs.organisation.repository.OrganisationRepository;
 import org.innovateuk.ifs.project.core.domain.PartnerOrganisation;
 import org.innovateuk.ifs.project.core.domain.Project;
 import org.innovateuk.ifs.project.core.domain.ProjectUser;
+import org.innovateuk.ifs.project.resource.ApprovalType;
 import org.innovateuk.ifs.user.domain.User;
 import org.innovateuk.ifs.user.repository.UserRepository;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.time.ZonedDateTime;
 import java.util.List;
 
-import static java.util.Arrays.asList;
+import static java.time.LocalDate.now;
+import static org.innovateuk.ifs.project.core.builder.PartnerOrganisationBuilder.newPartnerOrganisation;
 import static org.innovateuk.ifs.project.core.builder.ProjectBuilder.newProject;
 import static org.innovateuk.ifs.project.core.builder.ProjectUserBuilder.newProjectUser;
 import static org.innovateuk.ifs.project.core.domain.ProjectParticipantRole.PROJECT_MANAGER;
@@ -40,13 +44,14 @@ public class PartnerOrganisationRepositoryIntegrationTest extends BaseRepository
     @Autowired
     private OrganisationRepository organisationRepository;
 
-    private User user;
+//    private User user;
     private User leadApplicant;
     private User collaborator;
     private Project project;
     private Organisation empire;
     private Organisation ludlow;
     List<ProjectUser> projectUsers;
+    List<PartnerOrganisation> partnerOrganisations;
 
     @Autowired
     @Override
@@ -56,44 +61,62 @@ public class PartnerOrganisationRepositoryIntegrationTest extends BaseRepository
 
     @Before
     public void setup() {
-        user = userRepository.findByEmail("lee.bowman@innovateuk.test").orElseThrow(() -> new IllegalStateException("Expected to find test user for email lee.bowman@innovateuk.test"));
-        leadApplicant = userRepository.findByEmail("steve.smith@empire.com").orElseThrow(() -> new IllegalStateException("Expected to find test user for email steve.smith@empire.com"));
-        collaborator = userRepository.findByEmail("jessica.doe@ludlow.co.uk").orElseThrow(() -> new IllegalStateException("Expected to find test user for email jessica.doe@ludlow.co.uk"));
+//        user = userRepository.findByEmail("lee.bowman@innovateuk.test").orElseThrow(() -> new IllegalStateException("Expected to find test user for email lee.bowman@innovateuk.test"));
+        leadApplicant = userRepository.findByEmail("steve.smith@empire.com").get();
+        collaborator = userRepository.findByEmail("jessica.doe@ludlow.co.uk").get();
 
         Application application = applicationRepository.findById(1L).get();
         empire = organisationRepository.findOneByName("Empire Ltd");
         ludlow = organisationRepository.findOneByName("Ludlow");
-        project = projectRepository.findById(20L).get();
-
-        ProjectUser projectManager = projectUserRepository.save(newProjectUser()
-                .withId(123L)
-                .withProject(project)
-                .withUser(leadApplicant)
-                .withRole(PROJECT_MANAGER)
-                .withOrganisation(empire)
-                .build());
-
-        ProjectUser projectPartner = projectUserRepository.save(newProjectUser()
-                .withId(144L)
-                .withProject(project)
-                .withUser(collaborator)
-                .withRole(PROJECT_PARTNER)
-                .withOrganisation(ludlow)
-                .build());
-
-        projectUsers = asList(projectManager, projectPartner);
 
         project = newProject()
-                .withId(90L)
                 .withApplication(application)
-                .withProjectUsers(projectUsers)
-                .build();
+                .withDateSubmitted(ZonedDateTime.now())
+                .withDuration(6L)
+                .withName("My test project")
+                .withAddress(new Address("2 Polaris House", "Swindon", "Wiltshire", null, null, "SN2 1EU"))
+                .withOtherDocumentsApproved(ApprovalType.APPROVED)
+                .withTargetStartDate(now()).build();
+
+        projectRepository.save(project);
+
+//        ProjectUser projectManager = projectUserRepository.save(newProjectUser()
+//                .withId(123L)
+//                .withProject(project)
+//                .withUser(leadApplicant)
+//                .withRole(PROJECT_MANAGER)
+//                .withOrganisation(empire)
+//                .build());
+//
+//        ProjectUser projectPartner = projectUserRepository.save(newProjectUser()
+//                .withId(144L)
+//                .withProject(project)
+//                .withUser(collaborator)
+//                .withRole(PROJECT_PARTNER)
+//                .withOrganisation(ludlow)
+//                .build());
+
+//        projectUsers = asList(projectManager, projectPartner);
+
+        partnerOrganisations = newPartnerOrganisation().withProject(project).withId(30L, 40L).withOrganisation(empire,ludlow).withLeadOrganisation(true,false).build(2);
+        repository.save(partnerOrganisations.get(0));
+        repository.save(partnerOrganisations.get(1));
+
     }
 
     @Test
     public void getProjectPartnerOrganisations() {
 
-        PartnerOrganisation partnerOrganisation = repository.findOneByProjectIdAndOrganisationId(project.getId(), ludlow.getId());
-        assertEquals("Ludlow", partnerOrganisation);
+        List<PartnerOrganisation> partners = repository.findByProjectId(project.getId());
+
+        assertEquals(empire, partners.get(0).getOrganisation());
+        assertEquals(ludlow, partners.get(1).getOrganisation());
+    }
+
+    @Test
+    public void deleteOneByProjectIdAndOrganisationId() {
+        repository.deleteOneByProjectIdAndOrganisationId(project.getId(), ludlow.getId());
+
+        assertEquals(1, repository.findByProjectId(project.getId()).size());
     }
 }
