87a7090a47753dbe88e0d71b84ef3f7a278a2f6e
Tom Baldwin <tom@polymorphism.co.uk>
2019-10-30 11:37:33 +0000

    IFS-6673 - Renames H2020 terms -> Horizon 2020

--- ifs-data-layer/ifs-data-service/src/main/resources/db/migration/V153_1_1__Add_h2020_competition_terms.sql
+++ ifs-data-layer/ifs-data-service/src/main/resources/db/migration/V153_1_1__Add_h2020_competition_terms.sql
@@ -2,7 +2,7 @@
 
 -- add the new terms
 INSERT INTO terms_and_conditions (id, name, template, version, type, created_by, created_on, modified_on, modified_by)
-VALUES (24, 'H2020', 'h2020-terms-and-conditions', 1, 'GRANT', 15, now(), now(), 15);
+VALUES (24, 'Horizon 2020', 'h2020-terms-and-conditions', 1, 'GRANT', 15, now(), now(), 15);
 
 SET @h2020_competition_type = (SELECT id FROM competition_type WHERE name = 'Horizon 2020');
 
