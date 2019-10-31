ce6ee481431352e543c7c136d1ba987d093f628e
Kieran Hester <khester@worth.systems>
2019-10-31 07:33:18 +0000

    IFS-6484 tidy

--- ifs-data-layer/ifs-data-service/src/main/java/org/innovateuk/ifs/organisation/domain/OrganisationAddress.java
+++ ifs-data-layer/ifs-data-service/src/main/java/org/innovateuk/ifs/organisation/domain/OrganisationAddress.java
@@ -31,7 +31,7 @@ public class OrganisationAddress {
     @JoinColumn(name = "address_type_id", referencedColumnName = "id")
     private AddressType addressType;
 
-    @OneToMany(cascade = ALL, mappedBy = "organisationAddress", orphanRemoval = true)
+    @OneToMany(mappedBy = "organisationAddress")
     private List<BankDetails> bankDetails;
 
     public OrganisationAddress(Organisation organisation, Address address, AddressType addressType) {
