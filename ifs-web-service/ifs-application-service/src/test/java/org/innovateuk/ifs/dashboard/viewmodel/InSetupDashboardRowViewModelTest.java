5934ff73bce13a38d97c18d73df6e1415ded8bde
Luke Harper <lharper@worth.systems>
2019-11-01 14:20:06 +0000

    IFS-6490 fixing unit tests.

--- ifs-web-service/ifs-application-service/src/test/java/org/innovateuk/ifs/dashboard/viewmodel/InSetupDashboardRowViewModelTest.java
+++ ifs-web-service/ifs-application-service/src/test/java/org/innovateuk/ifs/dashboard/viewmodel/InSetupDashboardRowViewModelTest.java
@@ -2,28 +2,37 @@ package org.innovateuk.ifs.dashboard.viewmodel;
 
 import org.junit.Test;
 
-import java.time.LocalDate;
-
+import static org.innovateuk.ifs.applicant.resource.dashboard.DashboardInSetupRowResource.DashboardInSetupRowResourceBuilder.aDashboardInSetupRowResource;
 import static org.junit.Assert.assertEquals;
 
 public class InSetupDashboardRowViewModelTest {
 
-    private static final LocalDate YESTERDAY = LocalDate.now().minusDays(1);
-
     @Test
-    public void testConstruct() {
-        InSetupDashboardRowViewModel viewModel = new InSetupDashboardRowViewModel("Application", 1L,
-                "Competition", 2L, "Project", YESTERDAY);
+    public void notPending() {
+        InSetupDashboardRowViewModel viewModel = new InSetupDashboardRowViewModel(aDashboardInSetupRowResource()
+                .withCompetitionTitle("Competition")
+                .withApplicationId(1L)
+                .withProjectId(2L)
+                .withProjectTitle("Project")
+                .withPendingPartner(false)
+                .build());
 
         assertEquals("/project-setup/project/2", viewModel.getLinkUrl());
         assertEquals("Project", viewModel.getTitle());
     }
 
     @Test
-    public void testNullTitle() {
-        InSetupDashboardRowViewModel viewModel = new InSetupDashboardRowViewModel("Application", 1L,
-                "Competition", 2L, "", YESTERDAY);
+    public void pending() {
+        InSetupDashboardRowViewModel viewModel = new InSetupDashboardRowViewModel(aDashboardInSetupRowResource()
+                .withCompetitionTitle("Competition")
+                .withApplicationId(1L)
+                .withProjectId(2L)
+                .withProjectTitle("Project")
+                .withPendingPartner(true)
+                .withOrganisationId(3L)
+                .build());
 
-        assertEquals("Competition", viewModel.getTitle());
+        assertEquals("/project-setup/project/2/organisation/3/pending-partner-progress", viewModel.getLinkUrl());
+        assertEquals("Project", viewModel.getTitle());
     }
 }
