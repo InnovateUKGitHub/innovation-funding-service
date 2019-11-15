5e8aec78121f4c769b2d8a87ec4dac4dc80cc158
Calum Brunt <cbrunt@Calums-MacBook-Pro.local>
2019-11-15 11:22:59 +0000

    IFS-6491 updating to match development branch

--- ifs-data-layer/ifs-data-service/src/main/java/org/innovateuk/ifs/finance/transactional/ProjectFinanceRowServiceImpl.java
+++ ifs-data-layer/ifs-data-service/src/main/java/org/innovateuk/ifs/finance/transactional/ProjectFinanceRowServiceImpl.java
@@ -7,13 +7,19 @@ import org.innovateuk.ifs.finance.domain.*;
 import org.innovateuk.ifs.finance.handler.IndustrialCostFinanceHandler;
 import org.innovateuk.ifs.finance.handler.OrganisationFinanceDelegate;
 import org.innovateuk.ifs.finance.handler.OrganisationTypeFinanceHandler;
+import org.innovateuk.ifs.finance.handler.ProjectFinanceHandler;
 import org.innovateuk.ifs.finance.handler.item.FinanceRowHandler;
 import org.innovateuk.ifs.finance.mapper.ProjectFinanceMapper;
+import org.innovateuk.ifs.finance.repository.EmployeesAndTurnoverRepository;
 import org.innovateuk.ifs.finance.repository.FinanceRowMetaFieldRepository;
 import org.innovateuk.ifs.finance.repository.FinanceRowMetaValueRepository;
+import org.innovateuk.ifs.finance.repository.GrowthTableRepository;
 import org.innovateuk.ifs.finance.repository.ProjectFinanceRepository;
 import org.innovateuk.ifs.finance.repository.ProjectFinanceRowRepository;
+import org.innovateuk.ifs.finance.resource.ProjectFinanceResource;
+import org.innovateuk.ifs.finance.resource.ProjectFinanceResourceId;
 import org.innovateuk.ifs.finance.resource.cost.FinanceRowItem;
+import org.innovateuk.ifs.finance.resource.cost.FinanceRowType;
 import org.innovateuk.ifs.transactional.BaseTransactionalService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -23,6 +29,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Supplier;
 
+import static java.lang.Boolean.TRUE;
 import static org.innovateuk.ifs.commons.error.CommonErrors.notFoundError;
 import static org.innovateuk.ifs.commons.service.ServiceResult.serviceSuccess;
 import static org.innovateuk.ifs.util.EntityLookupCallbacks.find;
@@ -44,6 +51,9 @@ public class ProjectFinanceRowServiceImpl extends BaseTransactionalService imple
     private OrganisationFinanceDelegate organisationFinanceDelegate;
 
     @Autowired
+    private ProjectFinanceHandler projectFinanceHandler;
+
+    @Autowired
     private ProjectFinanceRowRepository projectFinanceRowRepository;
 
     @Autowired
@@ -54,6 +64,12 @@ public class ProjectFinanceRowServiceImpl extends BaseTransactionalService imple
 
     @Autowired
     private IndustrialCostFinanceHandler organisationFinanceDefaultHandler;
+
+    @Autowired
+    private EmployeesAndTurnoverRepository employeesAndTurnoverRepository;
+
+    @Autowired
+    private GrowthTableRepository growthTableRepository;
 
     @Override
     public ServiceResult<FinanceRowItem> get(long costItemId) {
@@ -78,30 +94,56 @@ public class ProjectFinanceRowServiceImpl extends BaseTransactionalService imple
     @Transactional
     public ServiceResult<FinanceRowItem> update(final long id, final FinanceRowItem newCostItem) {
         return find(projectFinanceRowRepository.findById(id), notFoundError(ProjectFinanceRow.class)).
-                andOnSuccess(projectFinanceRow -> doUpdate(id, newCostItem).andOnSuccessReturn(cost -> {
-                            OrganisationTypeFinanceHandler organisationFinanceHandler = organisationFinanceDelegate.getOrganisationFinanceHandler(((ProjectFinanceRow) cost).getTarget().getProject().getApplication().getCompetition().getId(), ((ProjectFinanceRow) cost).getTarget().getOrganisation().getOrganisationType().getId());
-                            return organisationFinanceHandler.toResource(cost);
-                        })
-                );
+            andOnSuccess(projectFinanceRow -> doUpdate(id, newCostItem).andOnSuccessReturn(cost -> {
+                    OrganisationTypeFinanceHandler organisationFinanceHandler = organisationFinanceDelegate.getOrganisationFinanceHandler(((ProjectFinanceRow) cost).getTarget().getProject().getApplication().getCompetition().getId(), ((ProjectFinanceRow) cost).getTarget().getOrganisation().getOrganisationType().getId());
+                    return organisationFinanceHandler.toResource(cost);
+                })
+            );
     }
 
     @Override
     @Transactional
     public ServiceResult<Void> delete(long costId) {
         return find(projectFinanceRowRepository.findById(costId), notFoundError(ProjectFinanceRow.class)).
-                andOnSuccessReturnVoid(projectFinanceRow -> {
-                    financeRowMetaValueRepository.deleteByFinanceRowId(costId);
-                    projectFinanceRowRepository.deleteById(costId);
-                });
+            andOnSuccessReturnVoid(projectFinanceRow -> {
+                financeRowMetaValueRepository.deleteByFinanceRowId(costId);
+                projectFinanceRowRepository.deleteById(costId);
+            });
     }
 
+    @Override
+    public ServiceResult<ProjectFinanceResource> financeChecksDetails(long projectId, long organisationId) {
+        return getProjectFinanceForOrganisation(new ProjectFinanceResourceId(projectId, organisationId));
+    }
+
+    @Override
+    public ServiceResult<List<ProjectFinanceResource>> financeChecksTotals(long projectId) {
+        return find(projectFinanceHandler.getFinanceChecksTotals(projectId), notFoundError(ProjectFinance.class, projectId));
+    }
 
     @Override
     public FinanceRowHandler getCostHandler(FinanceRowItem costItem) {
         return organisationFinanceDefaultHandler.getCostHandler(costItem.getCostType());
     }
 
+    @Override
+    public ServiceResult<Void> createProjectFinance(long projectId, long organisationId) {
+        return find(project(projectId), organisation(organisationId)).andOnSuccessReturnVoid((project, organisation) -> {
+            ProjectFinance projectFinance = projectFinanceRepository.save(new ProjectFinance(project, organisation));
+            if (TRUE.equals(projectFinance.getCompetition().getIncludeProjectGrowthTable())) {
+                projectFinance.setGrowthTable(new GrowthTable());
+                growthTableRepository.save(projectFinance.getGrowthTable());
+            } else {
+                projectFinance.setEmployeesAndTurnover(new EmployeesAndTurnover());
+                employeesAndTurnoverRepository.save(projectFinance.getEmployeesAndTurnover());
+            }
+            OrganisationTypeFinanceHandler organisationFinanceHandler = organisationFinanceDelegate.getOrganisationFinanceHandler(projectFinance.getCompetition().getId(), projectFinance.getOrganisation().getOrganisationType().getId());
 
+            for (FinanceRowType costType : projectFinance.getCompetition().getFinanceRowTypes()) {
+                organisationFinanceHandler.initialiseCostType(projectFinance, costType);
+            }
+        });
+    }
 
     private Supplier<ServiceResult<ProjectFinance>> projectFinance(long projectFinanceId) {
         return () -> getProjectFinance(projectFinanceId);
@@ -111,6 +153,9 @@ public class ProjectFinanceRowServiceImpl extends BaseTransactionalService imple
         return find(projectFinanceRepository.findById(projectFinanceId), notFoundError(ProjectFinance.class, projectFinanceId));
     }
 
+    private ServiceResult<ProjectFinanceResource> getProjectFinanceForOrganisation(ProjectFinanceResourceId projectFinanceResourceId) {
+        return projectFinanceHandler.getProjectOrganisationFinances(projectFinanceResourceId);
+    }
 
     private FinanceRow addCostItem(ProjectFinance projectFinance, FinanceRowItem newCostItem) {
         OrganisationTypeFinanceHandler organisationFinanceHandler = organisationFinanceDelegate.getOrganisationFinanceHandler(projectFinance.getProject().getApplication().getCompetition().getId(), projectFinance.getOrganisation().getOrganisationType().getId());
@@ -183,11 +228,11 @@ public class ProjectFinanceRowServiceImpl extends BaseTransactionalService imple
             ProjectFinanceRow savedCost = projectFinanceRowRepository.save(updatedCost);
 
             newCost.getFinanceRowMetadata()
-                    .stream()
-                    .filter(c -> c.getValue() != null)
-                    .filter(c -> !"null".equals(c.getValue()))
-                    .peek(c -> LOG.debug("FinanceRowMetaValue: " + c.getValue()))
-                    .forEach(costValue -> updateCostValue(costValue, savedCost));
+                .stream()
+                .filter(c -> c.getValue() != null)
+                .filter(c -> !"null".equals(c.getValue()))
+                .peek(c -> LOG.debug("FinanceRowMetaValue: " + c.getValue()))
+                .forEach(costValue -> updateCostValue(costValue, savedCost));
 
             // refresh the object, since we need to reload the costvalues, on the cost object.
             return savedCost;
