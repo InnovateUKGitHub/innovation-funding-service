a19b587c7c26f479440e7a346eca8c5dcb68e57a
Kieran Hester <khester@worth.systems>
2019-11-19 10:35:31 +0000

    IFS-6783 fix innovation lead

--- ifs-data-layer/ifs-data-service/src/main/resources/db/webtest/V154_4_9__Set_competition_template_ids_and_innovation_leads.sql
+++ ifs-data-layer/ifs-data-service/src/main/resources/db/webtest/V154_4_9__Set_competition_template_ids_and_innovation_leads.sql
@@ -8,8 +8,8 @@ update competition_type set template_competition_id = 8 where name = "The Prince
 update competition_type set template_competition_id = 9 where name = 'Horizon 2020';
 
 -- Script for adding competition_user records for already set innovation leads during competition setup on existing competitions (IFS-191)
-INSERT INTO competition_user (competition_id, competition_role, user_id, participant_status_id)
-    SELECT id, 'INNOVATION_LEAD', lead_technologist_user_id, 2
+INSERT INTO competition_user (competition_id, competition_role, user_id, participant_status_id, type)
+    SELECT id, 'INNOVATION_LEAD', lead_technologist_user_id, 2, 'INNOVATION_LEAD'
 	FROM competition c
 	WHERE c.lead_technologist_user_id IS NOT NULL
     AND NOT EXISTS (SELECT * FROM competition_user cu WHERE cu.competition_role = 'INNOVATION_LEAD' AND cu.competition_id = c.id AND cu.user_id = c.lead_technologist_user_id);
