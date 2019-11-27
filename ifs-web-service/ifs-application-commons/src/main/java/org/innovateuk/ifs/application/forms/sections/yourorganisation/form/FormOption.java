56d9e93e893474c28a9d5f3fa746e72ef7519049
Calum Brunt <cbrunt@Calums-MacBook-Pro.local>
2019-11-27 10:29:21 +0000

    IFS-6491 adding unit tests

--- ifs-web-service/ifs-application-commons/src/main/java/org/innovateuk/ifs/application/forms/sections/yourorganisation/form/FormOption.java
+++ ifs-web-service/ifs-application-commons/src/main/java/org/innovateuk/ifs/application/forms/sections/yourorganisation/form/FormOption.java
@@ -1,5 +1,8 @@
 package org.innovateuk.ifs.application.forms.sections.yourorganisation.form;
 
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+
 /**
  * Represents a radio option.
  */
@@ -19,5 +22,30 @@ public class FormOption {
 
     public String getValue() {
         return value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        FormOption that = (FormOption) o;
+
+        return new EqualsBuilder()
+            .append(description, that.description)
+            .append(value, that.value)
+            .isEquals();
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder(17, 37)
+            .append(description)
+            .append(value)
+            .toHashCode();
     }
 }
