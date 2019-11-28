762bed2c23cc0365df8aa76e29d9414860f65c89
richard <richard@hiveit.co.uk>
2019-11-28 11:50:22 +0000

    IFS-6558 whitespace removal 3

--- ifs-data-layer/ifs-data-service/src/test/java/org/innovateuk/ifs/project/core/documentation/ProjectControllerDocumentation.java
+++ ifs-data-layer/ifs-data-service/src/test/java/org/innovateuk/ifs/project/core/documentation/ProjectControllerDocumentation.java
@@ -111,7 +111,7 @@ public class ProjectControllerDocumentation extends BaseControllerMockMVCTest<Pr
                                         .andWithPrefix("address.", AddressDocs.addressResourceFields)
                 ));
     }
-    
+
     @Test
     public void existsOnApplication() throws Exception {
         Long userId = 1L;
