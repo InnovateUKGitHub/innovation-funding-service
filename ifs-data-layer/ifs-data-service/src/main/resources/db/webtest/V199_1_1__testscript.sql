12180626ad4d1ce87e0442d2b5615bd198996fe0
Kieran Hester <khester@worth.systems>
2019-11-28 08:27:33 +0000

    IFS-6835 rename file

--- ifs-data-layer/ifs-data-service/src/main/resources/db/webtest/V199_1_1__testscript.sql
+++
@@ -1,7 +0,0 @@
-SET @support_role_id =
-(SELECT id FROM role WHERE name = 'support');
-
-INSERT INTO user_role
-(user_id, role_id)
-VALUES
-((select id from user where email = 'orville.Gibbs@gmail.com'), @support_role_id);
