08664c7a598ca9bc984160c02e5e14d1b402c4af
Luke Harper <lharper@worth.systems>
2019-12-17 15:07:06 +0000

    IFS-6931 fixing comp templates.

--- ifs-data-layer/ifs-data-service/src/main/resources/db/migration/V156_1_1__Update_default_terms_and_conditions.sql
+++ ifs-data-layer/ifs-data-service/src/main/resources/db/migration/V156_1_1__Update_default_terms_and_conditions.sql
@@ -5,8 +5,12 @@ SET @system_maintenance_user_id = (
     FROM user
     WHERE email = 'ifs_system_maintenance_user@innovateuk.org');
 
+SET @old_terms_id = 10;
 SET @new_terms_id = 25;
 
 INSERT INTO terms_and_conditions (id, name, template, version, type, created_by, created_on, modified_by, modified_on)
 VALUES (@new_terms_id, 'Innovate UK', 'default-terms-and-conditions-v4', 4, 'GRANT',
         @system_maintenance_user_id, now(), @system_maintenance_user_id, now());
+
+-- Update link from template competition to new version of terms and conditions so these become the default Ts&Cs
+UPDATE competition SET terms_and_conditions_id = @new_terms_id WHERE terms_and_conditions_id = @old_terms_id and template = 1;
