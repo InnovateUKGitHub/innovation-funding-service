2c7024f7d3cb5c2149ebf79e9b4fbb4cfeb37ed8
Calum Brunt <cbrunt@Calums-MacBook-Pro.local>
2019-12-19 11:36:10 +0000

    IFS-6866 move aws credentials location from home into project

--- get-secrets.sh
+++ get-secrets.sh
@@ -5,19 +5,21 @@ if test -f "$FILE"; then
 
     echo "Start copying secrets from parameter store"
 
-    touch aws/credentials
-    #chmod 777 ~/.aws/credentials
+    #mkdir aws
+    #touch aws/credentials
 
     docker image rm ssm-access-image
     docker build --tag="ssm-access-image" docker/aws-cli
 
     mkdir -p ifs-auth-service/ifs-ldap-service/src/main/docker/certs/
     echo "just created directory"
-    IFS_LDAP_ENCRYPTION_KEY=$(env AWS_PROFILE=iukorg docker run --rm -e AWS_PROFILE -v aws:/root/.aws ssm-access-image aws ssm get-parameter --name "/CI/IFS/LDAP/ENCRYPTION/KEY" --with-decryption | jq -r ".Parameter.Value")
+    IFS_LDAP_ENCRYPTION_KEY=$(env AWS_PROFILE=iukorg docker run --rm -e AWS_PROFILE -v $PWD/aws:/root/.aws ssm-access-image aws ssm get-parameter --name "/CI/IFS/LDAP/ENCRYPTION/KEY" --with-decryption | jq -r ".Parameter.Value")
     echo "just made call to aws"
     echo $IFS_LDAP_ENCRYPTION_KEY > ifs-auth-service/ifs-ldap-service/src/main/docker/certs/ldap-encryption.key
     echo "just wrote to file"
     echo "$(cat ifs-auth-service/ifs-ldap-service/src/main/docker/certs/ldap-encryption.key)"
+    : <<'END'
+
     IFS_LDAP_ENCRYPTION_CERT=$(env AWS_PROFILE=iukorg docker run --rm -it -e AWS_PROFILE -v ~/.aws:/root/.aws ssm-access-image aws ssm get-parameter --name "/CI/IFS/LDAP/ENCRYPTION/CERT" --with-decryption | jq -r ".Parameter.Value")
     echo $IFS_LDAP_ENCRYPTION_CERT > ifs-auth-service/ifs-ldap-service/src/main/docker/certs/ldap-encryption.crt
     echo "$(cat ifs-auth-service/ifs-ldap-service/src/main/docker/certs/ldap-encryption.crt)"
@@ -67,7 +69,7 @@ if test -f "$FILE"; then
     IFS_SIGNED_CACERT=$(env AWS_PROFILE=iukorg docker run --rm -e AWS_PROFILE -v ~/.aws:/root/.aws ssm-access-image aws ssm get-parameters-by-path --path "/CI/IFS/SIGNED/CACERT" --with-decryption | jq -r ".Parameters|sort_by(.Name)[].Name")
     echo $IFS_SIGNED_CACERT > ca.crt
     echo "$(cat ca.crt)"
-
+END
     echo "Finished copying secrets from parameter store"
 
 else
