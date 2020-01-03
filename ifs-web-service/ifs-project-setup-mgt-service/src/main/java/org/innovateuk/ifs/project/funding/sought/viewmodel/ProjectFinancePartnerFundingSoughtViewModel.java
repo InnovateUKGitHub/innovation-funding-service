353c2aaab02bc618ec0895788cd067f9a56965ef
Luke Harper <lharper@worth.systems>
2020-01-03 14:29:42 +0000

    IFS-6695 start

---
+++ ifs-web-service/ifs-project-setup-mgt-service/src/main/java/org/innovateuk/ifs/project/funding/sought/viewmodel/ProjectFinancePartnerFundingSoughtViewModel.java
@@ -0,0 +1,27 @@
+package org.innovateuk.ifs.project.funding.viewmodel;
+
+import java.math.BigDecimal;
+
+public class ProjectFinancePartnerFundingSoughtViewModel {
+    private final long id;
+    private final String name;
+    private final BigDecimal currentFunding;
+
+    public ProjectFinancePartnerFundingSoughtViewModel(long id, String name, BigDecimal currentFunding) {
+        this.id = id;
+        this.name = name;
+        this.currentFunding = currentFunding;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public BigDecimal getCurrentFunding() {
+        return currentFunding;
+    }
+}
