e6571b065329edc1349c5c7f40bb5e8fb993a619
Calum Brunt <cbrunt@Calums-MacBook-Pro.local>
2020-01-03 13:46:49 +0000

    IFS-6866 Add support for prod variables

--- get-secrets.sh
+++ get-secrets.sh
@@ -3,10 +3,9 @@ ENV=$1
 AWS_PROFILE=$2
 KEY_ID=$3
 KEY=$4
+echo $AWS_PROFILE
 
-#mkdir ifs-auth-service/aws
-#touch ifs-auth-service/aws/credentials
-echo "$AWS_PROFILE
+echo "[$AWS_PROFILE]
 aws_access_key_id = $KEY_ID
 aws_secret_access_key = $KEY" > ifs-auth-service/aws/credentials
 
@@ -14,13 +13,14 @@ echo "Start copying secrets from parameter store"
 
 docker image rm ssm-access-image
 docker build --tag="ssm-access-image" docker/aws-cli
+docker run -itd --rm -e AWS_PROFILE=$AWS_PROFILE -v $PWD/ifs-auth-service/aws:/root/.aws --name ssm-access-container ssm-access-image
 
 #1 = paramName
 getParameter () {
-  docker run --rm -e AWS_PROFILE=iukorg -v $PWD/ifs-auth-service/aws:/root/.aws ssm-access-image aws ssm get-parameter --name "$1" --with-decryption | jq ".Parameter.Value" | tr -d \"
+  docker exec -it ssm-access-container aws ssm get-parameter --name $1 --with-decryption | jq ".Parameter.Value" | tr -d \"
 }
 
-#1 = paramValue 2 = writePath
+#1 = paramValue, 2 = writePath
 writeParameter () {
   echo -e $1 > $2
   echo $1
@@ -36,39 +36,34 @@ appendParameter () {
 }
 
 writeParameter "$(getParameter "/CI/IFS/$ENV/LDAP/ENCRYPTION/KEY")" "ifs-auth-service/ifs-ldap-service/src/main/docker/certs/ldap-encryption.key"
-
 writeParameter "$(getParameter "/CI/IFS/$ENV/LDAP/ENCRYPTION/CERT")" "ifs-auth-service/ifs-ldap-service/src/main/docker/certs/ldap-encryption.crt"
-
 writeParameter "$(getParameter "/CI/IFS/$ENV/IDP/ENCRYPTION/KEY")" "ifs-auth-service/ifs-idp-service/src/main/docker/certs/idp-encryption.key"
-
 writeParameter "$(getParameter "/CI/IFS/$ENV/IDP/ENCRYPTION/CERT")" "ifs-auth-service/ifs-idp-service/src/main/docker/certs/idp-encryption.crt"
-
 writeParameter "$(getParameter "/CI/IFS/$ENV/IDP/PROXY/KEY")" "ifs-auth-service/ifs-idp-service/src/main/docker/certs/idp_proxy_key.pem"
-
 writeParameter "$(getParameter "/CI/IFS/$ENV/IDP/PROXY/CERT")" "ifs-auth-service/ifs-idp-service/src/main/docker/certs/idp_proxy_certificate.pem"
-
 writeParameter "$(getParameter "/CI/IFS/$ENV/IDP/PROXY/CACERT/1")" "ifs-auth-service/ifs-idp-service/src/main/docker/certs/idp_proxy_cacertificate.pem"
-appendParameter "$(getParameter "/CI/IFS/$ENV/IDP/PROXY/CACERT/2")" "ifs-auth-service/ifs-idp-service/src/main/docker/certs/idp_proxy_cacertificate.pem"
-appendParameter "$(getParameter "/CI/IFS/$ENV/IDP/PROXY/CACERT/3")" "ifs-auth-service/ifs-idp-service/src/main/docker/certs/idp_proxy_cacertificate.pem"
-
 writeParameter "$(getParameter "/CI/IFS/$ENV/IDP/SIGNING/KEY")" "ifs-auth-service/ifs-idp-service/src/main/docker/certs/idp-signing.key"
-
 writeParameter "$(getParameter "/CI/IFS/$ENV/IDP/SIGNING/CERT")" "ifs-auth-service/ifs-idp-service/src/main/docker/certs/idp-signing.crt"
-
 writeParameter "$(getParameter "/CI/IFS/$ENV/SP/PROXY/KEY")" "ifs-auth-service/ifs-sp-service/src/main/docker/certs/sp_proxy_key.pem"
-
 writeParameter "$(getParameter "/CI/IFS/$ENV/SP/PROXY/CERT")" "ifs-auth-service/ifs-sp-service/src/main/docker/certs/sp_proxy_certificate.pem"
-
 writeParameter "$(getParameter "/CI/IFS/$ENV/SP/PROXY/CACERT/1")" "ifs-auth-service/ifs-sp-service/src/main/docker/certs/sp_proxy_cacertificate.pem"
-appendParameter "$(getParameter "/CI/IFS/$ENV/SP/PROXY/CACERT/2")" "ifs-auth-service/ifs-sp-service/src/main/docker/certs/sp_proxy_cacertificate.pem"
-appendParameter "$(getParameter "/CI/IFS/$ENV/SP/PROXY/CACERT/3")" "ifs-auth-service/ifs-sp-service/src/main/docker/certs/sp_proxy_cacertificate.pem"
 
-writeParameter "$(getParameter "/CI/IFS/$ENV/SIGNED/CERT")" "server.crt"
+if [ "$ENV" == "NON-PROD" ]; then
+  echo "env is non prod"
+  appendParameter "$(getParameter "/CI/IFS/$ENV/IDP/PROXY/CACERT/2")" "ifs-auth-service/ifs-idp-service/src/main/docker/certs/idp_proxy_cacertificate.pem"
+  appendParameter "$(getParameter "/CI/IFS/$ENV/IDP/PROXY/CACERT/3")" "ifs-auth-service/ifs-idp-service/src/main/docker/certs/idp_proxy_cacertificate.pem"
 
-writeParameter "$(getParameter "/CI/IFS/$ENV/SIGNED/KEY")" "server.key"
+  appendParameter "$(getParameter "/CI/IFS/$ENV/SP/PROXY/CACERT/2")" "ifs-auth-service/ifs-sp-service/src/main/docker/certs/sp_proxy_cacertificate.pem"
+  appendParameter "$(getParameter "/CI/IFS/$ENV/SP/PROXY/CACERT/3")" "ifs-auth-service/ifs-sp-service/src/main/docker/certs/sp_proxy_cacertificate.pem"
 
-writeParameter "$(getParameter "/CI/IFS/$ENV/SIGNED/CACERT/1")" "ca.crt"
-appendParameter "$(getParameter "/CI/IFS/$ENV/SIGNED/CACERT/2")" "ca.crt"
-appendParameter "$(getParameter "/CI/IFS/$ENV/SIGNED/CACERT/3")" "ca.crt"
+  writeParameter "$(getParameter "/CI/IFS/$ENV/SIGNED/CERT")" "server.crt"
+  writeParameter "$(getParameter "/CI/IFS/$ENV/SIGNED/KEY")" "server.key"
+  writeParameter "$(getParameter "/CI/IFS/$ENV/SIGNED/CACERT/1")" "ca.crt"
+  appendParameter "$(getParameter "/CI/IFS/$ENV/SIGNED/CACERT/2")" "ca.crt"
+  appendParameter "$(getParameter "/CI/IFS/$ENV/SIGNED/CACERT/3")" "ca.crt"
+fi
+
+docker stop ssm-access-container
+docker rm ssm-access-container
 
 echo "Finished copying secrets from parameter store"
