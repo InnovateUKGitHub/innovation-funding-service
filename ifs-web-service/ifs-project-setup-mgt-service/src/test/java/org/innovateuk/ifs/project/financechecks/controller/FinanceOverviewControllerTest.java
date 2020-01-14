79eaad9e31a00b73518ed01e1826e17072275aca
Tom Baldwin <tom@polymorphism.co.uk>
2020-01-14 15:25:38 +0000

    IFS-6712 - Fixes FinanceOverviewControllerTest

--- ifs-web-service/ifs-project-setup-mgt-service/src/test/java/org/innovateuk/ifs/project/financechecks/controller/FinanceOverviewControllerTest.java
+++ ifs-web-service/ifs-project-setup-mgt-service/src/test/java/org/innovateuk/ifs/project/financechecks/controller/FinanceOverviewControllerTest.java
@@ -16,15 +16,14 @@ import org.innovateuk.ifs.project.resource.ProjectResource;
 import org.innovateuk.ifs.project.service.PartnerOrganisationRestService;
 import org.junit.Test;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.springframework.test.web.servlet.MvcResult;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
-import java.util.Collections;
 import java.util.EnumSet;
 import java.util.List;
 
+import static java.util.Collections.emptyList;
 import static org.innovateuk.ifs.commons.rest.RestResult.restSuccess;
 import static org.innovateuk.ifs.commons.service.ServiceResult.serviceSuccess;
 import static org.innovateuk.ifs.competition.builder.CompetitionResourceBuilder.newCompetitionResource;
@@ -34,10 +33,9 @@ import static org.innovateuk.ifs.project.finance.builder.FinanceCheckOverviewRes
 import static org.innovateuk.ifs.project.finance.builder.FinanceCheckPartnerStatusResourceBuilder.FinanceCheckEligibilityResourceBuilder.newFinanceCheckEligibilityResource;
 import static org.innovateuk.ifs.project.finance.builder.FinanceCheckSummaryResourceBuilder.newFinanceCheckSummaryResource;
 import static org.junit.Assert.assertEquals;
-import static org.mockito.ArgumentMatchers.isNull;
 import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.ArgumentMatchers.isNull;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
@@ -73,21 +71,21 @@ public class FinanceOverviewControllerTest extends BaseControllerMockMVCTest<Fin
         when(partnerOrganisationRestService.getProjectPartnerOrganisations(projectId)).thenReturn(restSuccess(partnerOrganisationResources));
         when(financeCheckServiceMock.getFinanceCheckOverview(projectId)).thenReturn(serviceSuccess(mockFinanceOverview()));
         when(financeCheckServiceMock.getFinanceCheckEligibilityDetails(projectId, organisationId)).thenReturn(financeCheckEligibilityResource);
-        when(projectFinanceService.getProjectFinances(projectId)).thenReturn(Collections.emptyList());
+        when(projectFinanceService.getProjectFinances(projectId)).thenReturn(emptyList());
         ProjectResource projectResource = newProjectResource().withCompetition(competition.getId()).build();
         when(projectService.getById(projectId)).thenReturn(projectResource);
         when(competitionRestService.getCompetitionById(projectResource.getCompetition())).thenReturn(restSuccess(competition));
-        when(financeCheckServiceMock.getFinanceCheckSummary(projectId)).thenReturn(serviceSuccess(newFinanceCheckSummaryResource().build()));
-        MvcResult result = mockMvc.perform(get("/project/" + projectId + "/finance-check-overview")).
+        when(financeCheckServiceMock.getFinanceCheckSummary(projectId)).thenReturn(serviceSuccess(newFinanceCheckSummaryResource().withPartnerStatusResources(emptyList()).build()));
+        MvcResult result = mockMvc.perform(get("/project/{projectId}/finance-check-overview", projectId)).
                 andExpect(view().name("project/financecheck/overview")).
                 andReturn();
     
         FinanceCheckOverviewViewModel financeCheckOverviewViewModel = (FinanceCheckOverviewViewModel) result.getModelAndView().getModel().get("model");
-        assertEquals(LocalDate.of(2016, 01, 01), financeCheckOverviewViewModel.getOverview().getProjectStartDate());
+        assertEquals(LocalDate.of(2016, 1, 1), financeCheckOverviewViewModel.getOverview().getProjectStartDate());
         assertEquals("test-project", financeCheckOverviewViewModel.getOverview().getProjectName());
 
         verify(financeCheckServiceMock).getFinanceCheckOverview(projectId);
-        verify(financeCheckServiceMock, Mockito.times(3)).getFinanceCheckEligibilityDetails(anyLong(), isNull());
+        verify(financeCheckServiceMock, times(3)).getFinanceCheckEligibilityDetails(anyLong(), isNull());
         verify(projectFinanceService).getProjectFinances(projectId);
     }
 
@@ -116,5 +114,4 @@ public class FinanceOverviewControllerTest extends BaseControllerMockMVCTest<Fin
                 withProjectStartDate(startDate).
                 withTotalProjectCost(totalProjectCost).build();
     }
-
 }
