208e726f0ead64587ebd2a8d83802ae1cdc329f2
Grace Williams <grace.williams@innovateuk.gov.uk>
2020-01-20 08:34:19 +0000

    IFS-6923: Template and fragments updated

--- ifs-web-service/ifs-project-setup-mgt-service/src/main/java/org/innovateuk/ifs/project/organisationsize/controller/ProjectOrganisationSizeController.java
+++ ifs-web-service/ifs-project-setup-mgt-service/src/main/java/org/innovateuk/ifs/project/organisationsize/controller/ProjectOrganisationSizeController.java
@@ -1,29 +1,26 @@
 package org.innovateuk.ifs.project.organisationsize.controller;
 
-import org.innovateuk.ifs.application.forms.sections.yourorganisation.form.YourOrganisationWithGrowthTableFormPopulator;
+import org.innovateuk.ifs.commons.security.SecuredBySpring;
 import org.innovateuk.ifs.finance.resource.OrganisationFinancesWithoutGrowthTableResource;
-import org.innovateuk.ifs.finance.resource.OrganisationSize;
-import org.innovateuk.ifs.finance.resource.ProjectFinanceResource;
 import org.innovateuk.ifs.organisation.resource.OrganisationResource;
-import org.innovateuk.ifs.project.finance.service.ProjectFinanceRestService;
-import org.innovateuk.ifs.project.finance.service.ProjectYourOrganisationRestService;
 import org.innovateuk.ifs.project.organisationsize.form.ProjectOrganisationSizeWithoutGrowthTableForm;
 import org.innovateuk.ifs.project.organisationsize.viewmodel.ProjectOrganisationSizeViewModel;
+
 import org.innovateuk.ifs.project.resource.ProjectResource;
-import org.innovateuk.ifs.project.service.PartnerOrganisationRestService;
 import org.innovateuk.ifs.project.service.ProjectRestService;
 import org.innovateuk.ifs.user.service.OrganisationRestService;
 import org.innovateuk.ifs.yourorganisation.service.YourOrganisationRestService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
-@RequestMapping("/project/{projectId}/organisation/{organisationId}/edit")
+@RequestMapping("/project/{projectId}/organisation/{organisationId}")
+@PreAuthorize("hasAnyAuthority('project_finance', 'comp_admin', 'support', 'innovation_lead', 'stakeholder')")
+@SecuredBySpring(value = "Controller", description = "TODO", securedType = ProjectOrganisationSizeController.class)
 public class ProjectOrganisationSizeController {
 
     @Autowired
@@ -35,8 +32,9 @@ public class ProjectOrganisationSizeController {
     @Autowired
     private YourOrganisationRestService yourOrganisationRestService;
 
-    @GetMapping
+    @GetMapping("/edit")
     public String editOrganisationSize(@ModelAttribute(value = "form", binding = false) ProjectOrganisationSizeWithoutGrowthTableForm form,
+                                       BindingResult bindingResult,
                                        @PathVariable long projectId,
                                        @PathVariable long organisationId,
                                        Model model) {
@@ -45,8 +43,27 @@ public class ProjectOrganisationSizeController {
         OrganisationFinancesWithoutGrowthTableResource finances = yourOrganisationRestService.getOrganisationFinancesWithoutGrowthTable(project.getApplication(), organisationId).getSuccess();
 
         model.addAttribute("model", new ProjectOrganisationSizeViewModel(project, organisation.getName(), finances.getOrganisationSize(), finances.getTurnover(), finances.getHeadCount()));
+        model.addAttribute("form", form);
 
         return "project/edit-organisation-size";
     }
+
+//    @PostMapping
+//    @PreAuthorize("hasAuthority('project_finance')")
+//    @SecuredBySpring(value = "VIEW_EDIT_ORGANISATION", description = "Internal users can view edit organisation size")
+//    public String editOrganisationSize(ProjectOrganisationSizeWithoutGrowthTableForm form,
+//                                       @PathVariable long projectId,
+//                                       @PathVariable long organisationId,
+//                                       UserResource loggedInUser,
+//                                       Model model) {
+//        ProjectResource project = projectRestService.getProjectById(projectId).getSuccess();
+//        OrganisationResource organisation = organisationRestService.getOrganisationById(organisationId).getSuccess();
+//        OrganisationFinancesWithoutGrowthTableResource finances = yourOrganisationRestService.getOrganisationFinancesWithoutGrowthTable(project.getApplication(), organisationId).getSuccess();
+//
+//        model.addAttribute("model", new ProjectOrganisationSizeViewModel(project, organisation.getName(), finances.getTurnover(), finances.getHeadCount()));
+//        model.addAttribute("form", form);
+//
+//        return "project/edit-organisation-size";
+//    }
 
 }
--- ifs-web-service/ifs-project-setup-mgt-service/src/main/java/org/innovateuk/ifs/project/organisationsize/viewmodel/ProjectOrganisationSizeViewModel.java
+++ ifs-web-service/ifs-project-setup-mgt-service/src/main/java/org/innovateuk/ifs/project/organisationsize/viewmodel/ProjectOrganisationSizeViewModel.java
@@ -1,5 +1,7 @@
 package org.innovateuk.ifs.project.organisationsize.viewmodel;
 
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
 import org.innovateuk.ifs.application.forms.sections.yourorganisation.form.FormOption;
 import org.innovateuk.ifs.finance.resource.OrganisationSize;
 import org.innovateuk.ifs.project.resource.ProjectResource;
@@ -15,9 +17,14 @@ public class ProjectOrganisationSizeViewModel {
     private final String organisationName;
     private final OrganisationSize organisationSize;
     private final BigDecimal turnover;
-    private final long employees;
+    private final Long employees;
+    private boolean showStateAidAgreement;
+    private boolean fundingSectionComplete;
+    private boolean h2020;
+    private boolean readOnly;
 
-    public ProjectOrganisationSizeViewModel(ProjectResource project, String organisationName, OrganisationSize organisationSize, BigDecimal turnover, long employees) {
+
+    public ProjectOrganisationSizeViewModel(ProjectResource project, String organisationName, OrganisationSize organisationSize, BigDecimal turnover, Long employees) {
         this.project = project;
         this.organisationName = organisationName;
         this.organisationSize = organisationSize;
@@ -45,7 +52,57 @@ public class ProjectOrganisationSizeViewModel {
         return turnover;
     }
 
-    public long getEmployees() {
+    public Long getEmployees() {
         return employees;
+    }
+
+    public boolean isShowStateAidAgreement() {
+        return showStateAidAgreement;
+    }
+
+    public boolean isShowOrganisationSizeAlert() {
+        return fundingSectionComplete;
+    }
+
+    public boolean isH2020() {
+        return h2020;
+    }
+
+    public boolean isReadOnly() {
+        return readOnly;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+
+        if (o == null || getClass() != o.getClass()) return false;
+
+        ProjectOrganisationSizeViewModel that = (ProjectOrganisationSizeViewModel) o;
+
+        return new EqualsBuilder()
+                .append(project, that.project)
+                .append(organisationName, that.organisationName)
+                .append(organisationSize, that.organisationSize)
+                .append(turnover, that.turnover)
+                .append(employees, that.employees)
+                .append(showStateAidAgreement, that.showStateAidAgreement)
+                .append(fundingSectionComplete, that.fundingSectionComplete)
+                .append(h2020, that.h2020)
+                .isEquals();
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder(17, 37)
+                .append(project)
+                .append(organisationName)
+                .append(organisationSize)
+                .append(turnover)
+                .append(employees)
+                .append(showStateAidAgreement)
+                .append(fundingSectionComplete)
+                .append(h2020)
+                .toHashCode();
     }
 }
