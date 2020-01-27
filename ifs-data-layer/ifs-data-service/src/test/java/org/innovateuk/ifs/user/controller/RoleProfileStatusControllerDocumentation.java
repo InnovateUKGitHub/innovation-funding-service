6d0d5a8e7875620ae6011dd85cbf57195489f67c
Kieran Hester <khester@worth.systems>
2020-01-27 22:03:49 +0000

    IFS-7052 fix test

--- ifs-data-layer/ifs-data-service/src/test/java/org/innovateuk/ifs/user/controller/RoleProfileStatusControllerDocumentation.java
+++ ifs-data-layer/ifs-data-service/src/test/java/org/innovateuk/ifs/user/controller/RoleProfileStatusControllerDocumentation.java
@@ -19,12 +19,12 @@ import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.put;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
 import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 public class RoleProfileStatusControllerDocumentation extends BaseControllerMockMVCTest<RoleProfileStatusController> {
