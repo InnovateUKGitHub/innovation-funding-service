854e3a5ab660f1fd0a87bf32544859238dcceefd
Kieran Hester <khester@worth.systems>
2020-01-28 19:48:08 +0000

    IFS-6835 fix location

--- setup-files/scripts/openshift/os-deploy-config.sh
+++ setup-files/scripts/openshift/os-deploy-config.sh
@@ -21,28 +21,28 @@ REGISTRY_TOKEN=$SVC_ACCOUNT_TOKEN
 echo "Applying the config maps for $PROJECT Openshift project"
 
 function applyConfigMaps {
-    oc apply -f $(getBuildLocation)/acc-config.yml ${SVC_ACCOUNT_CLAUSE}
-    oc apply -f $(getBuildLocation)/cache-config.yml ${SVC_ACCOUNT_CLAUSE}
-    oc apply -f $(getBuildLocation)/data-service-config.yml ${SVC_ACCOUNT_CLAUSE}
-    oc apply -f $(getBuildLocation)/db-config.yml ${SVC_ACCOUNT_CLAUSE}
-    oc apply -f $(getBuildLocation)/finance-db-config.yml ${SVC_ACCOUNT_CLAUSE}
-    oc apply -f $(getBuildLocation)/flyway-config.yml ${SVC_ACCOUNT_CLAUSE}
-    oc apply -f $(getBuildLocation)/grant-db-config.yml ${SVC_ACCOUNT_CLAUSE}
-    oc apply -f $(getBuildLocation)/ldap-config.yml ${SVC_ACCOUNT_CLAUSE}
-    oc apply -f $(getBuildLocation)/idp-config.yml ${SVC_ACCOUNT_CLAUSE}
-    oc apply -f $(getBuildLocation)/new-relic-config.yml ${SVC_ACCOUNT_CLAUSE}
-    oc apply -f $(getBuildLocation)/performance-config.yml ${SVC_ACCOUNT_CLAUSE}
-    oc apply -f $(getBuildLocation)/shibboleth-config.yml ${SVC_ACCOUNT_CLAUSE}
-    oc apply -f $(getBuildLocation)/survey-db-config.yml ${SVC_ACCOUNT_CLAUSE}
-    oc apply -f $(getBuildLocation)/web-config.yml ${SVC_ACCOUNT_CLAUSE}
+    oc apply -f $(getBuildLocation)/config-maps/acc-config.yml ${SVC_ACCOUNT_CLAUSE}
+    oc apply -f $(getBuildLocation)/config-maps/cache-config.yml ${SVC_ACCOUNT_CLAUSE}
+    oc apply -f $(getBuildLocation)/config-maps/data-service-config.yml ${SVC_ACCOUNT_CLAUSE}
+    oc apply -f $(getBuildLocation)/config-maps/db-config.yml ${SVC_ACCOUNT_CLAUSE}
+    oc apply -f $(getBuildLocation)/config-maps/finance-db-config.yml ${SVC_ACCOUNT_CLAUSE}
+    oc apply -f $(getBuildLocation)/config-maps/flyway-config.yml ${SVC_ACCOUNT_CLAUSE}
+    oc apply -f $(getBuildLocation)/config-maps/grant-db-config.yml ${SVC_ACCOUNT_CLAUSE}
+    oc apply -f $(getBuildLocation)/config-maps/ldap-config.yml ${SVC_ACCOUNT_CLAUSE}
+    oc apply -f $(getBuildLocation)/config-maps/idp-config.yml ${SVC_ACCOUNT_CLAUSE}
+    oc apply -f $(getBuildLocation)/config-maps/new-relic-config.yml ${SVC_ACCOUNT_CLAUSE}
+    oc apply -f $(getBuildLocation)/config-maps/performance-config.yml ${SVC_ACCOUNT_CLAUSE}
+    oc apply -f $(getBuildLocation)/config-maps/shibboleth-config.yml ${SVC_ACCOUNT_CLAUSE}
+    oc apply -f $(getBuildLocation)/config-maps/survey-db-config.yml ${SVC_ACCOUNT_CLAUSE}
+    oc apply -f $(getBuildLocation)/config-maps/web-config.yml ${SVC_ACCOUNT_CLAUSE}
 }
 
 function applyRoutes {
-    oc apply -f $(getBuildLocation)/idp-route.yml ${SVC_ACCOUNT_CLAUSE}
-    oc apply -f $(getBuildLocation)/shib-route.yml ${SVC_ACCOUNT_CLAUSE}
+    oc apply -f $(getBuildLocation)/routes/idp-route.yml ${SVC_ACCOUNT_CLAUSE}
+    oc apply -f $(getBuildLocation)/routes/shib-route.yml ${SVC_ACCOUNT_CLAUSE}
 
     if $(isNamedEnvironment ${TARGET}); then
-        oc apply -f $(getBuildLocation)/mail-route.yml ${SVC_ACCOUNT_CLAUSE}
+        oc apply -f $(getBuildLocation)/routes/mail-route.yml ${SVC_ACCOUNT_CLAUSE}
     fi
 }
 
