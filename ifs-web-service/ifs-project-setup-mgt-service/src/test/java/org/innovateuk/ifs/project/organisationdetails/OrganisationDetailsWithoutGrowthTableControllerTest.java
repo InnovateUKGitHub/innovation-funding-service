2c46ca7bb8659bac090ecb50f665527d88592c78
Calum Brunt <cbrunt@Calums-MacBook-Pro.local>
2020-02-03 13:28:32 +0000

    Update unit tests

--- ifs-web-service/ifs-project-setup-mgt-service/src/test/java/org/innovateuk/ifs/project/organisationdetails/OrganisationDetailsWithoutGrowthTableControllerTest.java
+++ ifs-web-service/ifs-project-setup-mgt-service/src/test/java/org/innovateuk/ifs/project/organisationdetails/OrganisationDetailsWithoutGrowthTableControllerTest.java
@@ -135,7 +135,7 @@ public class OrganisationDetailsWithoutGrowthTableControllerTest extends BaseCon
 
     @Test
     public void viewOrganisationDetailsWithoutYourOrganisationSectionAndAddressEmpty() throws Exception {
-        competition.setIncludeYourOrganisationSection(true);
+        competition.setIncludeYourOrganisationSection(false);
         competition.setIncludeJesForm(true);
         competition.setFundingType(GRANT);
         organisation.setOrganisationType(2L);
