72fe70e3cfc7586e6ab828fe867caa51a1eb3548
Luke Harper <lharper@worth.systems>
2020-02-10 11:25:17 +0000

    IFS-6923 fixed controller.

--- ifs-web-service/ifs-project-setup-mgt-service/src/main/java/org/innovateuk/ifs/project/organisationdetails/view/controller/OrganisationDetailsWithGrowthTableController.java
+++ ifs-web-service/ifs-project-setup-mgt-service/src/main/java/org/innovateuk/ifs/project/organisationdetails/view/controller/OrganisationDetailsWithGrowthTableController.java
@@ -1,33 +1,13 @@
 package org.innovateuk.ifs.project.organisationdetails.view.controller;
 
-import org.innovateuk.ifs.address.resource.AddressResource;
 import org.innovateuk.ifs.application.forms.sections.yourorganisation.form.YourOrganisationWithGrowthTableForm;
 import org.innovateuk.ifs.application.forms.sections.yourorganisation.form.YourOrganisationWithGrowthTableFormPopulator;
-import org.innovateuk.ifs.async.generation.AsyncAdaptor;
 import org.innovateuk.ifs.commons.security.SecuredBySpring;
-import org.innovateuk.ifs.competition.resource.CompetitionResource;
-import org.innovateuk.ifs.competition.service.CompetitionRestService;
-import org.innovateuk.ifs.financecheck.FinanceCheckService;
-import org.innovateuk.ifs.organisation.resource.OrganisationResource;
-import org.innovateuk.ifs.organisation.resource.OrganisationTypeEnum;
-import org.innovateuk.ifs.project.finance.resource.FinanceCheckSummaryResource;
 import org.innovateuk.ifs.project.finance.service.ProjectYourOrganisationRestService;
-import org.innovateuk.ifs.project.organisationdetails.view.viewmodel.OrganisationDetailsViewModel;
-import org.innovateuk.ifs.project.resource.ProjectResource;
-import org.innovateuk.ifs.project.service.PartnerOrganisationRestService;
-import org.innovateuk.ifs.project.service.ProjectRestService;
-import org.innovateuk.ifs.project.yourorganisation.viewmodel.ProjectYourOrganisationViewModel;
-import org.innovateuk.ifs.user.resource.UserResource;
-import org.innovateuk.ifs.user.service.OrganisationRestService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.Optional;
 
 /**
  * This controller will allow the user to view organisation details with a growth table.
@@ -37,91 +17,21 @@ import java.util.Optional;
 @SecuredBySpring(value = "Controller", description = "Internal users can view organisation details",
         securedType = OrganisationDetailsWithGrowthTableController.class)
 @PreAuthorize("hasAnyAuthority('project_finance', 'comp_admin', 'support', 'innovation_lead', 'stakeholder')")
-public class OrganisationDetailsWithGrowthTableController extends AsyncAdaptor {
-
-    @Autowired
-    private ProjectRestService projectRestService;
+public class OrganisationDetailsWithGrowthTableController extends AbstractOrganisationDetailsController<YourOrganisationWithGrowthTableForm> {
 
     @Autowired
     private ProjectYourOrganisationRestService projectYourOrganisationRestService;
 
     @Autowired
-    private OrganisationRestService organisationRestService;
-
-    @Autowired
-    private PartnerOrganisationRestService partnerOrganisationRestService;
-
-    @Autowired
     private YourOrganisationWithGrowthTableFormPopulator withGrowthTableFormPopulator;
 
-    @Autowired
-    private FinanceCheckService financeCheckService;
-
-    @Autowired
-    private CompetitionRestService competitionRestService;
-
-    @GetMapping
-    public String viewOrganisationDetails(@PathVariable long competitionId,
-                                          @PathVariable long projectId,
-                                          @PathVariable long organisationId,
-                                          Model model,
-                                          UserResource loggedInUser) {
-        ProjectResource project = projectRestService.getProjectById(projectId).getSuccess();
-        OrganisationResource organisation = organisationRestService.getOrganisationById(organisationId).getSuccess();
-
-        boolean includeYourOrganisationSection = isIncludeYourOrganisationSection(competitionId, organisation);
-
-        model.addAttribute("organisationDetails", new OrganisationDetailsViewModel(project,
-                competitionId,
-                organisation,
-                getAddress(organisation),
-                project.isCollaborativeProject()));
-
-        if (includeYourOrganisationSection) {
-            model.addAttribute("yourOrganisation", new ProjectYourOrganisationViewModel(false,
-                    false,
-                    false,
-                    projectId,
-                    project.getName(),
-                    organisationId,
-                    true,
-                    false,
-                    loggedInUser,
-                    isAllEligibilityAndViabilityInReview(projectId)));
-
-            model.addAttribute("form", getForm(projectId, organisationId));
-        }
+    @Override
+    protected String getView() {
         return "project/organisationdetails/organisation-details-with-growth-table";
     }
 
-    private boolean isIncludeYourOrganisationSection(long competitionId, OrganisationResource organisation) {
-        CompetitionResource competition = competitionRestService.getCompetitionById(competitionId).getSuccess();
-
-        return competition.applicantShouldUseJesFinances(OrganisationTypeEnum.getFromId(organisation.getOrganisationType()))
-                && !competition.getIncludeYourOrganisationSection()
-                ? false : true;
-    }
-
-    private AddressResource getAddress(OrganisationResource organisation) {
-        return organisation.getAddresses().size() > 0
-                ? organisation.getAddresses().get(0).getAddress()
-                : createNewAddress();
-    }
-
-    private AddressResource createNewAddress() {
-        return new AddressResource("", "", "", "", "", "");
-    }
-
-    private YourOrganisationWithGrowthTableForm getForm(long projectId, long organisationId) {
+    @Override
+    protected YourOrganisationWithGrowthTableForm getForm(long projectId, long organisationId) {
         return withGrowthTableFormPopulator.populate(projectYourOrganisationRestService.getOrganisationFinancesWithGrowthTable(projectId, organisationId).getSuccess());
-    }
-
-    private boolean isAllEligibilityAndViabilityInReview(long projectId) {
-        Optional<FinanceCheckSummaryResource> financeCheckSummary = financeCheckService.getFinanceCheckSummary(projectId).getOptionalSuccessObject();
-
-        if (financeCheckSummary.isPresent()) {
-            return financeCheckSummary.get().isAllEligibilityAndViabilityInReview();
-        }
-        return false;
     }
 }
--- ifs-web-service/ifs-project-setup-mgt-service/src/main/java/org/innovateuk/ifs/project/organisationdetails/view/controller/OrganisationDetailsWithoutGrowthTableController.java
+++ ifs-web-service/ifs-project-setup-mgt-service/src/main/java/org/innovateuk/ifs/project/organisationdetails/view/controller/OrganisationDetailsWithoutGrowthTableController.java
@@ -1,32 +1,13 @@
 package org.innovateuk.ifs.project.organisationdetails.view.controller;
 
-import org.innovateuk.ifs.address.resource.AddressResource;
 import org.innovateuk.ifs.application.forms.sections.yourorganisation.form.YourOrganisationWithoutGrowthTableForm;
 import org.innovateuk.ifs.application.forms.sections.yourorganisation.form.YourOrganisationWithoutGrowthTableFormPopulator;
 import org.innovateuk.ifs.commons.security.SecuredBySpring;
-import org.innovateuk.ifs.competition.resource.CompetitionResource;
-import org.innovateuk.ifs.competition.service.CompetitionRestService;
-import org.innovateuk.ifs.financecheck.FinanceCheckService;
-import org.innovateuk.ifs.organisation.resource.OrganisationResource;
-import org.innovateuk.ifs.organisation.resource.OrganisationTypeEnum;
-import org.innovateuk.ifs.project.finance.resource.FinanceCheckSummaryResource;
 import org.innovateuk.ifs.project.finance.service.ProjectYourOrganisationRestService;
-import org.innovateuk.ifs.project.organisationdetails.view.viewmodel.OrganisationDetailsViewModel;
-import org.innovateuk.ifs.project.resource.ProjectResource;
-import org.innovateuk.ifs.project.service.PartnerOrganisationRestService;
-import org.innovateuk.ifs.project.service.ProjectRestService;
-import org.innovateuk.ifs.project.yourorganisation.viewmodel.ProjectYourOrganisationViewModel;
-import org.innovateuk.ifs.user.resource.UserResource;
-import org.innovateuk.ifs.user.service.OrganisationRestService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.Optional;
 
 /**
  * This controller will allow the user to view organisation details without a growth table.
@@ -36,10 +17,7 @@ import java.util.Optional;
 @PreAuthorize("hasAnyAuthority('project_finance', 'comp_admin', 'support', 'innovation_lead', 'stakeholder')")
 @SecuredBySpring(value = "Controller", description = "Internal users can view organisation details",
         securedType = OrganisationDetailsWithoutGrowthTableController.class)
-public class OrganisationDetailsWithoutGrowthTableController {
-
-    @Autowired
-    private ProjectRestService projectRestService;
+public class OrganisationDetailsWithoutGrowthTableController extends AbstractOrganisationDetailsController<YourOrganisationWithoutGrowthTableForm> {
 
     @Autowired
     private ProjectYourOrganisationRestService projectYourOrganisationRestService;
@@ -47,80 +25,13 @@ public class OrganisationDetailsWithoutGrowthTableController {
     @Autowired
     private YourOrganisationWithoutGrowthTableFormPopulator withoutGrowthTableFormPopulator;
 
-    @Autowired
-    private CompetitionRestService competitionRestService;
-
-    @Autowired
-    private OrganisationRestService organisationRestService;
-
-    @Autowired
-    private PartnerOrganisationRestService partnerOrganisationRestService;
-
-    @Autowired
-    private FinanceCheckService financeCheckService;
-
-    @GetMapping
-    public String viewOrganisationDetails(@PathVariable long competitionId,
-                                          @PathVariable long projectId,
-                                          @PathVariable long organisationId,
-                                          Model model,
-                                          UserResource loggedInUser) {
-        ProjectResource project = projectRestService.getProjectById(projectId).getSuccess();
-        OrganisationResource organisation = organisationRestService.getOrganisationById(organisationId).getSuccess();
-
-        boolean includeYourOrganisationSection = isIncludeYourOrganisationSection(competitionId, organisation);
-
-        model.addAttribute("organisationDetails", new OrganisationDetailsViewModel(project,
-                competitionId,
-                organisation,
-                getAddress(organisation),
-                project.isCollaborativeProject()));
-
-        if (includeYourOrganisationSection) {
-            model.addAttribute("yourOrganisation", new ProjectYourOrganisationViewModel(false,
-                    false,
-                    false,
-                    projectId,
-                    project.getName(),
-                    organisationId,
-                    true,
-                    false,
-                    loggedInUser,
-                    isAllEligibilityAndViabilityInReview(projectId)));
-
-            model.addAttribute("form", getForm(projectId, organisationId));
-        }
+    @Override
+    protected String getView() {
         return "project/organisationdetails/organisation-details-without-growth-table";
     }
 
-    private boolean isIncludeYourOrganisationSection(long competitionId, OrganisationResource organisation) {
-        CompetitionResource competition = competitionRestService.getCompetitionById(competitionId).getSuccess();
-
-        return competition.applicantShouldUseJesFinances(OrganisationTypeEnum.getFromId(organisation.getOrganisationType()))
-                && !competition.getIncludeYourOrganisationSection()
-                ? false : true;
-    }
-
-    private AddressResource getAddress(OrganisationResource organisation) {
-        return organisation.getAddresses().size() > 0
-                ? organisation.getAddresses().get(0).getAddress()
-                : createNewAddress();
-    }
-
-    private AddressResource createNewAddress() {
-        return new AddressResource("", "", "", "", "", "");
-    }
-
-    private YourOrganisationWithoutGrowthTableForm getForm(long projectId, long organisationId) {
+    @Override
+    protected YourOrganisationWithoutGrowthTableForm getForm(long projectId, long organisationId) {
         return withoutGrowthTableFormPopulator.populate(projectYourOrganisationRestService.getOrganisationFinancesWithoutGrowthTable(projectId, organisationId).getSuccess());
-    }
-
-    private boolean isAllEligibilityAndViabilityInReview(long projectId) {
-        Optional<FinanceCheckSummaryResource> financeCheckSummary = financeCheckService.getFinanceCheckSummary(projectId).getOptionalSuccessObject();
-
-        if (financeCheckSummary.isPresent()) {
-            return financeCheckSummary.get().isAllEligibilityAndViabilityInReview();
-        }
-        return false;
     }
 }
