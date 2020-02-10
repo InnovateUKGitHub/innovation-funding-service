ced950c03aeacd0cb1511e9204c1e4a954285d3c
Luke Harper <lharper@worth.systems>
2020-02-10 12:36:50 +0000

    IFS-6923 fixed.

--- ifs-web-service/ifs-project-setup-mgt-service/src/main/java/org/innovateuk/ifs/project/organisationdetails/edit/controller/EditOrganisationDetailsControllerWithoutGrowthTable.java
+++ ifs-web-service/ifs-project-setup-mgt-service/src/main/java/org/innovateuk/ifs/project/organisationdetails/edit/controller/EditOrganisationDetailsControllerWithoutGrowthTable.java
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-
 @Controller
 @RequestMapping("/project/{projectId}/organisation/{organisationId}/edit/without-growth-table")
 public class EditOrganisationDetailsControllerWithoutGrowthTable extends AbstractEditOrganisationDetailsController<YourOrganisationWithoutGrowthTableForm> {
