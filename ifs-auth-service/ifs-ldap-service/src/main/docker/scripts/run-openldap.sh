23bc89372391af904ef72e86dfa08814834f765e
Kieran Hester <khester@worth.systems>
2020-02-11 17:24:29 +0000

    IFS-7094 update binds

--- ifs-auth-service/ifs-ldap-service/src/main/docker/scripts/run-openldap.sh
+++ ifs-auth-service/ifs-ldap-service/src/main/docker/scripts/run-openldap.sh
@@ -1,7 +1,3 @@
 #!/bin/sh
 
-# TODO comment
-cat /var/certs/ldap-encryption.key > /etc/ldap/ldap-encryption.key
-cat /var/certs/ldap-encryption.crt > /etc/ldap/ldap-encryption.crt
-
 exec /usr/sbin/slapd -h "ldaps://0.0.0.0:$LDAP_PORT/ ldapi:///" -F /etc/ldap/slapd.d -d 256
