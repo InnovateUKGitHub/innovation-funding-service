f984c712486abf3ae53f1c1a29e64d0f8b730cdd
Adam Mapstone <adam.mapstone@innovateuk.gov.uk>
2020-02-11 11:15:03 +0000

    IFS-7094 - Commenting out doFirst copy of idap-encryption crt for testing purposes.

--- ifs-registration-service/docker/scripts/run-registration-service.sh
+++ ifs-registration-service/docker/scripts/run-registration-service.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
 $JAVA_HOME/bin/keytool -import -trustcacerts -keystore $JAVA_HOME/jre/lib/security/cacerts -storepass changeit -noprompt -alias iuk-auth-localdev -file /var/certs/ldap-encryption.crt
 
