94b96dcf51acf5c461156385562b8789438c6dec
Adam Mapstone <adam.mapstone@innovateuk.gov.uk>
2020-02-13 15:05:41 +0000

    IFS-7094 - ldap changes

--- ifs-auth-service/ifs-ldap-service/src/main/docker/scripts/run-openldap.sh
+++ ifs-auth-service/ifs-ldap-service/src/main/docker/scripts/run-openldap.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
-
 # enable & enforce SSL, be lax about self-signed certificates \
-  echo /var/certs/ldap-encryption.key > /etc/ldap/ldap-encryption.key && \
-  echo /var/certs/ldap-encryption.crt > /etc/ldap/ldap-encryption.crt && \
-  ldapmodify -Y EXTERNAL -H ldapi:/// -f mod_ssl.ldif && \
-  sed -i 's#/etc/ssl/certs/ca-certificates.crt#/etc/ldap/ldap-encryption.crt##' /etc/ldap/ldap.conf && \
-  echo "TLS_REQCERT allow" >> /etc/ldap/ldap.conf
+  echo /var/certs/ldap-encryption.key > /etc/ldap/ldap-encryption.key
+  echo /var/certs/ldap-encryption.crt > /etc/ldap/ldap-encryption.crt
+
+  ldapmodify -Y EXTERNAL -H ldapi:/// -f /usr/local/bin/mod_ssl.ldif
 
 exec /usr/sbin/slapd -h "ldaps://0.0.0.0:$LDAP_PORT/ ldapi:///" -F /etc/ldap/slapd.d -d 256
