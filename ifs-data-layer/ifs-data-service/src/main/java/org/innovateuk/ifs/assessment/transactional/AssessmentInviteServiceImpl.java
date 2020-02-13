162d3c1047a8f04747180cc4e588b9e287f1cc55
Kieran Hester <khester@worth.systems>
2020-02-13 08:24:03 +0000

    IFS-7085 add new row when invite

--- ifs-data-layer/ifs-data-service/src/main/java/org/innovateuk/ifs/assessment/transactional/AssessmentInviteServiceImpl.java
+++ ifs-data-layer/ifs-data-service/src/main/java/org/innovateuk/ifs/assessment/transactional/AssessmentInviteServiceImpl.java
@@ -32,7 +32,10 @@ import org.innovateuk.ifs.notifications.service.NotificationTemplateRenderer;
 import org.innovateuk.ifs.profile.domain.Profile;
 import org.innovateuk.ifs.profile.repository.ProfileRepository;
 import org.innovateuk.ifs.security.LoggedInUserSupplier;
+import org.innovateuk.ifs.user.domain.RoleProfileStatus;
 import org.innovateuk.ifs.user.domain.User;
+import org.innovateuk.ifs.user.repository.RoleProfileStatusRepository;
+import org.innovateuk.ifs.user.resource.ProfileRole;
 import org.innovateuk.ifs.user.resource.Role;
 import org.innovateuk.ifs.user.resource.UserResource;
 import org.innovateuk.ifs.user.transactional.UserService;
@@ -101,6 +104,9 @@ public class AssessmentInviteServiceImpl extends InviteService<AssessmentInvite>
 
     @Autowired
     private InnovationAreaRepository innovationAreaRepository;
+
+    @Autowired
+    private RoleProfileStatusRepository roleProfileStatusRepository;
 
     @Autowired
     private AssessmentInviteMapper assessmentInviteMapper;
@@ -549,7 +555,12 @@ public class AssessmentInviteServiceImpl extends InviteService<AssessmentInvite>
 
     private void addAssessorRoleToUser(User user) {
         user.addRole(Role.ASSESSOR);
+        createAssessorRoleProfileStatus(user);
         userService.evictUserCache(user.getUid());
+    }
+
+    private void createAssessorRoleProfileStatus(User user) {
+        roleProfileStatusRepository.save(new RoleProfileStatus(user, ProfileRole.ASSESSOR));
     }
 
     @Override
