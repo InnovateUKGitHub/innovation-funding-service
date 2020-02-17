2b8a59094c45c5716bb2340a7aa042d8517e0873
Luke Harper <lharper@worth.systems>
2020-02-17 11:10:03 +0000

    IFS-7025 fixing js.

--- ifs-web-service/ifs-web-core/src/main/resources/static/js/ifs_modules/changeModal.js
+++ ifs-web-service/ifs-web-core/src/main/resources/static/js/ifs_modules/changeModal.js
@@ -1,17 +1,23 @@
-// Using radio buttons changes the model destination for a button with id = modal-button.
-// radio buttons must have modal-link = "destination" for this to work
+// Using a form to change the modal that will open on clicking a button.
+// Use the data-change-modal wrapping the form elements that will change the button. The value of this attribute
+// should be a selector for the button.
+// The form elements that can change must have data-modal-link = "destination" for this to work
 IFS.core.changeModel = (function () {
   'use strict'
+  var s // private alias to settings
+
   return {
+    settings: {
+      changeModal: 'data-change-modal',
+      modalLink: 'data-modal-link'
+    },
     init: function () {
-      jQuery(window).bind('load', function () {
-        jQuery('[change-modal]').each(function () {
-          jQuery('.govuk-radios__item').on('click', function (e) {
-            var button = jQuery('#modal-button')
-            var name = jQuery(e.target).attr('modal-link')
-            button.attr('data-js-modal', 'modal-' + name.toLowerCase())
-          })
-        })
+      s = this.settings
+      jQuery('body').on('change', '[' + s.changeModal + ']', function (e) {
+        var container = jQuery(this)
+        var button = jQuery(container.attr(s.changeModal))
+        var name = jQuery(e.target).attr(s.modalLink)
+        button.attr('data-js-modal', 'modal-' + name.toLowerCase())
       })
     }
   }
