1386216154118ce6163d607d0a1b7a6cb7642938
richard <richard@hiveit.co.uk>
2020-02-18 11:13:32 +0000

    IFS-7094 another go at getting background process running

--- ifs-auth-service/ifs-ldap-service/src/main/docker/scripts/post_start_actions.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# TODO wait on LDAP to be up[ instead of sleeping
-sleep 60
-ldapmodify -H ldapi:/// -D cn=admin,cn=config -w foobar123 -f mod_ssl.ldif
-# TODO remove the root user.
-
-
--- ifs-auth-service/ifs-ldap-service/src/main/docker/scripts/run-openldap.sh
+++ ifs-auth-service/ifs-ldap-service/src/main/docker/scripts/run-openldap.sh
@@ -1,14 +1,21 @@
 #!/bin/sh
+# Turn on bash's job control. We need this so we can start back ground process.
+# see https://docs.docker.com/config/containers/multi-service_container/ for more information.
+set -m
+
 # Copy certs from the mounted runtime directory
 cat /var/certs/ldap-encryption.key > /etc/ldap/ldap-encryption.key
 cat /var/certs/ldap-encryption.crt > /etc/ldap/ldap-encryption.crt
 
-# Start the main process in a background thread - see https://docs.docker.com/config/containers/multi-service_container/
+# Start the main process in a background thread
 /usr/sbin/slapd -h "ldaps://0.0.0.0:$LDAP_PORT/ ldapi:///" -F /etc/ldap/slapd.d -d 256 &
 
-# Enable SSL
-post_start_actions.sh
+# Do any actions we need after we'ce started LDAP.
+# TODO better solution than wait
+sleep 60
+ldapmodify -H ldapi:/// -D cn=admin,cn=config -w foobar123 -f mod_ssl.ldif
+# TODO remove root user.
 
-# Now we bring the LDAP primary process back into the foreground and leave it there
+# Now we bring the LDAP primary process back into the foreground and leave it there.
 fg %1
 
