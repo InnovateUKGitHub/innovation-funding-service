dde12ed31af2dc81116efa679464e0dfac1c0075
richard <richard@hiveit.co.uk>
2020-02-18 12:39:44 +0000

    IFS-7094 now we wait for LDAP to start and remove the admin password after all config is done

--- ifs-auth-service/ifs-ldap-service/src/main/docker/scripts/run-openldap.sh
+++ ifs-auth-service/ifs-ldap-service/src/main/docker/scripts/run-openldap.sh
@@ -8,14 +8,25 @@ cat /var/certs/ldap-encryption.key > /etc/ldap/ldap-encryption.key
 cat /var/certs/ldap-encryption.crt > /etc/ldap/ldap-encryption.crt
 
 # Start the main process in a background thread
+echo "Starting LDAP in a back ground process"
 /usr/sbin/slapd -h "ldaps://0.0.0.0:$LDAP_PORT/ ldapi:///" -F /etc/ldap/slapd.d -d 256 &
 
-# Do any actions we need after we'ce started LDAP.
-# TODO better solution than wait
-sleep 30
-ldapmodify -H ldapi:/// -D cn=admin,cn=config -w foobar123 -f mod_ssl.ldif
-# TODO remove root user.
+# Do any actions we need after we've started LDAP.
+echo "Post start up configuration"
+
+# Enable SSL
+echo "Enabling SSL"
+until ldapmodify -H ldapi:/// -D cn=admin,cn=config -w root_password_123 -f mod_ssl.ldif
+do
+    echo "Waiting for LDAP to start"
+    sleep 3
+done
+
+# Remove the root password now configuration is complete
+echo "Removing the root password now configuration is complete"
+ldapmodify -H ldapi:/// -D cn=admin,cn=config -w root_password_123 -f rootpw_cnconfig_delete.ldif
 
 # Now we bring the LDAP primary process back into the foreground and leave it there.
+echo "Bringing LDAP to the foreground"
 fg %1
 
