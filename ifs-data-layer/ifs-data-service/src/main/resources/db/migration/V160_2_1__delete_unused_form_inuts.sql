03f769f196930b0e3179b9b3dcaefead3212c5aa
Luke Harper <lharper@worth.systems>
2020-02-27 16:29:20 +0000

    IFS-6216 fixed migration.

--- ifs-data-layer/ifs-data-service/src/main/resources/db/migration/V160_2_1__delete_unused_form_inuts.sql
+++ ifs-data-layer/ifs-data-service/src/main/resources/db/migration/V160_2_1__delete_unused_form_inuts.sql
@@ -1,7 +1,8 @@
--- IFS-4551 Empty form input type deletion
+-- IFS-6216 Delete unused form inputs
 
-delete from form_input_validator where form_input_id in (select fi.id from form_input fi  WHERE fi.form_input_type_id in (5, 16, 24, 25, 26, 27, 28));
-DELETE FROM form_input WHERE form_input_type_id in (5, 16, 24, 25, 26, 27, 28);
+DELETE FROM form_input_response WHERE form_input_id IN (SELECT fi.id FROM form_input fi  WHERE fi.form_input_type_id IN (5, 16, 24, 25, 26, 27, 28));
+DELETE FROM form_input_validator WHERE form_input_id IN (SELECT fi.id FROM form_input fi  WHERE fi.form_input_type_id IN (5, 16, 24, 25, 26, 27, 28));
+DELETE FROM form_input WHERE form_input_type_id IN (5, 16, 24, 25, 26, 27, 28);
 
 -- These form input types are no longer needed.
-DELETE FROM form_input_type WHERE id in (5, 16, 24, 25, 26, 27, 28);
+DELETE FROM form_input_type WHERE id IN (5, 16, 24, 25, 26, 27, 28);
