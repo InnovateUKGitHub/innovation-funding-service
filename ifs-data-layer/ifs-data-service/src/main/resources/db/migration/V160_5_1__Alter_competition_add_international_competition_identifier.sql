50503c16fabe1889e48e18bcec48c1ca0b4bbd49
Grace Williams <grace.williams@innovateuk.gov.uk>
2020-03-11 14:50:41 +0000

    IFS-7194: fix sql

--- ifs-data-layer/ifs-data-service/src/main/resources/db/migration/V160_5_1__Alter_competition_add_international_competition_identifier.sql
+++ ifs-data-layer/ifs-data-service/src/main/resources/db/migration/V160_5_1__Alter_competition_add_international_competition_identifier.sql
@@ -2,4 +2,4 @@
 ALTER TABLE address ADD COLUMN country VARCHAR(255) DEFAULT NULL;
 
 ALTER TABLE organisation ADD COLUMN is_international BIT(1) NOT NULL DEFAULT FALSE;
-ALTER TABLE organisation ADD COLUMN international_registration_number BIT(1) DEFAULT NULL;
+ALTER TABLE organisation ADD COLUMN international_registration_number VARCHAR(255) DEFAULT NULL;
