b62bee710032179820d9d3c6a2efe61d6ef447e2
Kieran Hester <khester@worth.systems>
2020-03-12 10:56:56 +0000

    IFS-7258 delete users before wiping db

--- setup-files/scripts/docker/clean-migrate-db-sync-ldap.sh
+++ setup-files/scripts/docker/clean-migrate-db-sync-ldap.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+. /wipe-ldap-users.sh
+
 # R Popple: Depending on environment variables clean migrate the database and sync it with ldap.
 if ! [ $ONLY_SYNC_LDAP = "true" ] ; then
 . /clean-migrate-db.sh
@@ -7,7 +10,5 @@ fi
 if [ ${PERFORMANCE_APPLICANT_USERS} > 0 ] || [ ${PERFORMANCE_h2020_USERS} ] ; then
 . /add-performance-users.sh
 fi
-
-. /wipe-ldap-users.sh
 
 . /ldap-sync-from-ifs-db.sh
