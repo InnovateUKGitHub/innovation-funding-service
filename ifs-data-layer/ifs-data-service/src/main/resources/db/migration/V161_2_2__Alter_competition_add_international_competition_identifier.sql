2de75257cd908bd39740cc4c17e5f8e08ba28ca7
Kieran Hester <khester@worth.systems>
2020-03-12 15:16:40 +0000

    IFS-7194 add constraint

--- ifs-data-layer/ifs-data-service/src/main/resources/db/migration/V161_2_2__Alter_competition_add_international_competition_identifier.sql
+++ ifs-data-layer/ifs-data-service/src/main/resources/db/migration/V161_2_2__Alter_competition_add_international_competition_identifier.sql
@@ -3,3 +3,5 @@ ALTER TABLE address ADD COLUMN country VARCHAR(255) DEFAULT NULL;
 
 ALTER TABLE organisation ADD COLUMN is_international BIT(1) NOT NULL DEFAULT FALSE;
 ALTER TABLE organisation ADD COLUMN international_registration_number VARCHAR(255) DEFAULT NULL;
+
+ALTER TABLE competition ADD CONSTRAINT fk_competition_organisation_config_id FOREIGN KEY (competition_organisation_config_id) REFERENCES competition_organisation_config (id);
