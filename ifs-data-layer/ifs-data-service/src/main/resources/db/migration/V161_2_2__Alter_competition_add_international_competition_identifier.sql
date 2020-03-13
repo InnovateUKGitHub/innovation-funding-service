72111415947aed46962349560c4de75f09eed766
Grace Williams <grace.williams@innovateuk.gov.uk>
2020-03-13 01:20:41 +0000

    IFS-7194: Address type table updated

--- ifs-data-layer/ifs-data-service/src/main/resources/db/migration/V161_2_2__Alter_competition_add_international_competition_identifier.sql
+++ ifs-data-layer/ifs-data-service/src/main/resources/db/migration/V161_2_2__Alter_competition_add_international_competition_identifier.sql
@@ -3,3 +3,5 @@ ALTER TABLE address ADD COLUMN country VARCHAR(255) DEFAULT NULL;
 
 ALTER TABLE organisation ADD COLUMN is_international BIT(1) NOT NULL DEFAULT FALSE;
 ALTER TABLE organisation ADD COLUMN international_registration_number VARCHAR(255) DEFAULT NULL;
+
+INSERT INTO address_type VALUES (5,'INTERNATIONAL');
