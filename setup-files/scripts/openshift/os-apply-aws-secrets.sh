4c9195b7702d1332d8b1d0cbad719efb17156a54
Kieran Hester <khester@worth.systems>
2020-03-20 09:46:26 +0000

    IFS-7176 add quotes

--- setup-files/scripts/openshift/os-apply-aws-secrets.sh
+++ setup-files/scripts/openshift/os-apply-aws-secrets.sh
@@ -87,8 +87,8 @@ function loadSpDataFromAws() {
     echo "sp" ${SSO_SP}
     export IFS=","
     for sp in "${SSO_SP}"; do
-      echo "$(valueFromAws /CI/IFS/$sp/PROPERTY)" >> $sp.properties
-      echo "$(valueFromAws /CI/IFS/$sp/CERT)" >> $sp.crt
+      echo "$(valueFromAws /CI/IFS/$sp/PROPERTY)" >> "$sp.properties"
+      echo "$(valueFromAws /CI/IFS/$sp/CERT)" >> "$sp.crt"
       text=$text" --from-file="$sp".properties="$sp".properties"
       text=$text" --from-file="$sp".crt="$sp".crt"
     done
