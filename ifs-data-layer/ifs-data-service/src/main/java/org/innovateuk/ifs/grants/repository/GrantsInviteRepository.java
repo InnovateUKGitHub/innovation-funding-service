9b21119708b4640c9907f5bb2c7808814a1edfa5
Kieran Hester <khester@worth.systems>
2020-03-26 13:00:44 +0000

    IFS-7227 fix

--- ifs-data-layer/ifs-data-service/src/main/java/org/innovateuk/ifs/grants/repository/GrantsInviteRepository.java
+++ ifs-data-layer/ifs-data-service/src/main/java/org/innovateuk/ifs/grants/repository/GrantsInviteRepository.java
@@ -3,7 +3,5 @@ package org.innovateuk.ifs.grants.repository;
 import org.innovateuk.ifs.grants.domain.GrantsInvite;
 import org.innovateuk.ifs.invite.repository.InviteRepository;
 
-public interface GrantsInviteRepository<Invite extends GrantsInvite> extends InviteRepository<Invite> {
-
-    GrantsInvite save(Invite invite);
+public interface GrantsInviteRepository extends InviteRepository<GrantsInvite> {
 }
