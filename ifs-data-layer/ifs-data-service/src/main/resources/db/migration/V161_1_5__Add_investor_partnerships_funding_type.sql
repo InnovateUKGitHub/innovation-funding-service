fbc07918a0a2481e84b1194f64c587e43dafd0ea
JonathanLee <jlee@worth.systems>
2020-03-31 15:39:55 +0100

    IFS-7213 adding investor partnerships funding type

---
+++ ifs-data-layer/ifs-data-service/src/main/resources/db/migration/V161_1_5__Add_investor_partnerships_funding_type.sql
@@ -0,0 +1,13 @@
+-- IFS-7213 add INVESTOR_PARTNERSHIP funding type
+ALTER TABLE competition MODIFY COLUMN funding_type enum('GRANT','LOAN','PROCUREMENT','KTP','INVESTOR_PARTNERSHIPS');
+
+SET @system_maintenance_user_id = (
+    SELECT id
+    FROM user
+    WHERE email = 'ifs_system_maintenance_user@innovateuk.org');
+
+SET @new_terms_id = 30;
+
+INSERT INTO terms_and_conditions (id, name, template, version, type, created_by, created_on, modified_by, modified_on)
+VALUES (@new_terms_id, 'Investor Partnerships', 'default-terms-and-conditions-v5', 1, 'GRANT',
+        @system_maintenance_user_id, now(), @system_maintenance_user_id, now());
