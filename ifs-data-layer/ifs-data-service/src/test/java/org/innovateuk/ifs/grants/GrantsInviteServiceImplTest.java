90ee79642c7050789d1a1aa69d6a4574a321a260
Grace Williams <grace.williams@innovateuk.gov.uk>
2020-04-06 12:12:01 +0100

    IFS-7227: More test cases added

--- ifs-data-layer/ifs-data-service/src/test/java/org/innovateuk/ifs/grants/GrantsInviteServiceImplTest.java
+++ ifs-data-layer/ifs-data-service/src/test/java/org/innovateuk/ifs/grants/GrantsInviteServiceImplTest.java
@@ -4,8 +4,12 @@ import org.innovateuk.ifs.BaseServiceUnitTest;
 import org.innovateuk.ifs.activitylog.transactional.ActivityLogService;
 import org.innovateuk.ifs.application.domain.Application;
 import org.innovateuk.ifs.commons.service.ServiceResult;
+import org.innovateuk.ifs.grants.domain.GrantsFinanceContactInvite;
 import org.innovateuk.ifs.grants.domain.GrantsInvite;
+import org.innovateuk.ifs.grants.domain.GrantsMonitoringOfficerInvite;
 import org.innovateuk.ifs.grants.domain.GrantsProjectManagerInvite;
+import org.innovateuk.ifs.grants.repository.GrantsFinanceContactInviteRepository;
+import org.innovateuk.ifs.grants.repository.GrantsMonitoringOfficerInviteRepository;
 import org.innovateuk.ifs.grants.repository.GrantsProjectManagerInviteRepository;
 import org.innovateuk.ifs.grants.transactional.GrantsInviteService;
 import org.innovateuk.ifs.grants.transactional.GrantsInviteServiceImpl;
@@ -18,6 +22,7 @@ import org.innovateuk.ifs.organisation.domain.Organisation;
 import org.innovateuk.ifs.project.core.domain.Project;
 import org.innovateuk.ifs.project.core.repository.ProjectRepository;
 import org.innovateuk.ifs.security.LoggedInUserSupplier;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
@@ -31,6 +36,7 @@ import static org.innovateuk.ifs.commons.service.ServiceResult.serviceSuccess;
 import static org.innovateuk.ifs.grantsinvite.resource.GrantsInviteResource.GrantsInviteRole.GRANTS_PROJECT_MANAGER;
 import static org.innovateuk.ifs.invite.builder.ApplicationInviteBuilder.newApplicationInvite;
 import static org.innovateuk.ifs.invite.builder.InviteOrganisationBuilder.newInviteOrganisation;
+import static org.innovateuk.ifs.invite.constant.InviteStatus.CREATED;
 import static org.innovateuk.ifs.organisation.builder.OrganisationBuilder.newOrganisation;
 import static org.innovateuk.ifs.project.core.builder.PartnerOrganisationBuilder.newPartnerOrganisation;
 import static org.innovateuk.ifs.project.core.builder.ProjectBuilder.newProject;
@@ -55,6 +61,12 @@ public class GrantsInviteServiceImplTest extends BaseServiceUnitTest<GrantsInvit
     private GrantsProjectManagerInviteRepository grantsProjectManagerInviteRepository;
 
     @Mock
+    private GrantsFinanceContactInviteRepository grantsFinanceContactInviteRepository;
+
+    @Mock
+    private GrantsMonitoringOfficerInviteRepository grantsMonitoringOfficerInviteRepository;
+
+    @Mock
     private ActivityLogService activityLogService;
 
     @Mock
@@ -63,31 +75,38 @@ public class GrantsInviteServiceImplTest extends BaseServiceUnitTest<GrantsInvit
     @Mock
     private LoggedInUserSupplier loggedInUserSupplier;
 
-    @Test
-    public void getById() {
+    private long projectId = 1L;
 
-        long projectId = 1L;
-        Organisation organisation = newOrganisation().withName("Ludlow").build();
-        Application application = newApplication().build();
-        Project project = newProject()
+    private Organisation organisation;
+    private Application application;
+    private Project project;
+    private ApplicationInvite applicationInvite;
+    private InviteOrganisation inviteOrganisation;
+    private GrantsInvite grantsInvite;
+
+
+    @Before
+    public void setup() {
+        organisation = newOrganisation().withName("Ludlow").build();
+        application = newApplication().build();
+        project = newProject()
                 .withApplication(application)
                 .withPartnerOrganisations(singletonList(newPartnerOrganisation()
-                .withOrganisation(organisation)
-                .withLeadOrganisation(true)
-                .build()))
+                        .withOrganisation(organisation)
+                        .withLeadOrganisation(true)
+                        .build()))
                 .build();
-        ApplicationInvite applicationInvite = newApplicationInvite().withApplication(application).build();
-        InviteOrganisation inviteOrganisation = newInviteOrganisation()
+        applicationInvite = newApplicationInvite().withApplication(application).build();
+        inviteOrganisation = newInviteOrganisation()
                 .withOrganisation(organisation)
                 .withOrganisationName(organisation.getName())
                 .withInvites(singletonList(applicationInvite))
                 .build();
-        GrantsInvite grantsInvite = new GrantsInvite();
-        grantsInvite.setInviteOrganisation(inviteOrganisation);
-        grantsInvite.setEmail("test@email.com");
-        grantsInvite.setName("Mr.Fly");
-        grantsInvite.setHash("hask3456jk");
-        grantsInvite.setTarget(project);
+        grantsInvite = new GrantsInvite("Mr.Fly", "test@email.com", "hask3456jk", inviteOrganisation, project, CREATED);
+    }
+
+    @Test
+    public void sendInviteGrantsProjectManager() {
         GrantsInviteResource invite = new GrantsInviteResource(grantsInvite.getInviteOrganisation().getOrganisationName(),
                 grantsInvite.getName(),
                 grantsInvite.getEmail(),
@@ -109,4 +128,52 @@ public class GrantsInviteServiceImplTest extends BaseServiceUnitTest<GrantsInvit
 
         assertTrue(result.isSuccess());
     }
+
+    @Test
+    public void sendInviteGrantsProjectFinanceContact() {
+        GrantsInviteResource invite = new GrantsInviteResource(grantsInvite.getInviteOrganisation().getOrganisationName(),
+                grantsInvite.getName(),
+                grantsInvite.getEmail(),
+                GRANTS_PROJECT_MANAGER);
+        GrantsFinanceContactInvite grantsFinanceContactInvite = new GrantsFinanceContactInvite(grantsInvite.getName(),
+                grantsInvite.getEmail(),
+                grantsInvite.getHash(),
+                grantsInvite.getInviteOrganisation(),
+                grantsInvite.getProject(),
+                grantsInvite.getStatus());
+
+        when(projectRepository.findById(projectId)).thenReturn(Optional.of(project));
+        when(inviteOrganisationRepository.save(inviteOrganisation)).thenReturn(inviteOrganisation);
+        when(grantsFinanceContactInviteRepository.save(grantsFinanceContactInvite)).thenReturn(grantsFinanceContactInvite);
+        when(notificationService.sendNotificationWithFlush(any(), any())).thenReturn(serviceSuccess());
+        when(loggedInUserSupplier.get()).thenReturn(newUser().build());
+
+        ServiceResult<Void> result = service.sendInvite(projectId, invite);
+
+        assertTrue(result.isSuccess());
+    }
+
+    @Test
+    public void sendInviteGrantsProjectMonitoringOfficer() {
+        GrantsInviteResource invite = new GrantsInviteResource(grantsInvite.getInviteOrganisation().getOrganisationName(),
+                grantsInvite.getName(),
+                grantsInvite.getEmail(),
+                GRANTS_PROJECT_MANAGER);
+        GrantsMonitoringOfficerInvite grantsMonitoringOfficerInvite = new GrantsMonitoringOfficerInvite(grantsInvite.getName(),
+                grantsInvite.getEmail(),
+                grantsInvite.getHash(),
+                grantsInvite.getProject(),
+                grantsInvite.getStatus());
+
+        when(projectRepository.findById(projectId)).thenReturn(Optional.of(project));
+        when(inviteOrganisationRepository.save(inviteOrganisation)).thenReturn(inviteOrganisation);
+        when(grantsMonitoringOfficerInviteRepository.save(grantsMonitoringOfficerInvite)).thenReturn(grantsMonitoringOfficerInvite);
+        when(notificationService.sendNotificationWithFlush(any(), any())).thenReturn(serviceSuccess());
+        when(loggedInUserSupplier.get()).thenReturn(newUser().build());
+
+        ServiceResult<Void> result = service.sendInvite(projectId, invite);
+
+        assertTrue(result.isSuccess());
+    }
+
 }
