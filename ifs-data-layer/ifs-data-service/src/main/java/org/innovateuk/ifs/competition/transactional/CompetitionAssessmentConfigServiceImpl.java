c591fca47aecbacd580cf9536876f61ef8963e61
Luke Harper <lharper@worth.systems>
2020-04-20 16:02:22 +0100

    IFS-7369 lookup callback.

--- ifs-data-layer/ifs-data-service/src/main/java/org/innovateuk/ifs/competition/transactional/CompetitionAssessmentConfigServiceImpl.java
+++ ifs-data-layer/ifs-data-service/src/main/java/org/innovateuk/ifs/competition/transactional/CompetitionAssessmentConfigServiceImpl.java
@@ -1,6 +1,7 @@
 package org.innovateuk.ifs.competition.transactional;
 
 import org.innovateuk.ifs.commons.ZeroDowntime;
+import org.innovateuk.ifs.commons.error.CommonErrors;
 import org.innovateuk.ifs.commons.service.ServiceResult;
 import org.innovateuk.ifs.competition.domain.CompetitionAssessmentConfig;
 import org.innovateuk.ifs.competition.mapper.CompetitionAssessmentConfigMapper;
@@ -11,9 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Optional;
-
-import static org.innovateuk.ifs.commons.service.ServiceResult.serviceSuccess;
+import static org.innovateuk.ifs.util.EntityLookupCallbacks.find;
 
 @Service
 public class CompetitionAssessmentConfigServiceImpl extends RootTransactionalService implements CompetitionAssessmentConfigService {
@@ -26,38 +25,28 @@ public class CompetitionAssessmentConfigServiceImpl extends RootTransactionalSer
 
     @Override
     public ServiceResult<CompetitionAssessmentConfigResource> findOneByCompetitionId(long competitionId) {
-        Optional<CompetitionAssessmentConfig> config = competitionAssessmentConfigRepository.findOneByCompetitionId(competitionId);
-
-        if (config.isPresent()) {
-            return serviceSuccess(mapper.mapToResource(config.get()));
-        }
-
-        // test this with just one set default
-        return serviceSuccess(new CompetitionAssessmentConfigResource(false));
-//        return serviceSuccess(new CompetitionAssessmentConfigResource(false, 1, BigDecimal.ZERO, true, true, AssessorFinanceView.DETAILED));
+        return find(competitionAssessmentConfigRepository.findOneByCompetitionId(competitionId), CommonErrors.notFoundError(CompetitionAssessmentConfig.class, competitionId))
+                .andOnSuccessReturn(mapper::mapToResource);
     }
-
+    
     @Override
     @Transactional
     @ZeroDowntime(description = "remove setting of data on competition table.", reference = "IFS-7369")
     public ServiceResult<Void> update(long competitionId, CompetitionAssessmentConfigResource competitionAssessmentConfigResource) {
-        Optional<CompetitionAssessmentConfig> config = competitionAssessmentConfigRepository.findOneByCompetitionId(competitionId);
+        return find(competitionAssessmentConfigRepository.findOneByCompetitionId(competitionId), CommonErrors.notFoundError(CompetitionAssessmentConfig.class, competitionId))
+                .andOnSuccessReturnVoid((config) -> {
+                    config.setAverageAssessorScore(competitionAssessmentConfigResource.getAverageAssessorScore());
+                    config.setAssessorPay(competitionAssessmentConfigResource.getAssessorPay());
+                    config.setAssessorCount(competitionAssessmentConfigResource.getAssessorCount());
+                    config.setHasAssessmentPanel(competitionAssessmentConfigResource.getHasAssessmentPanel());
+                    config.setHasInterviewStage(competitionAssessmentConfigResource.getHasInterviewStage());
+                    config.setAssessorFinanceView(competitionAssessmentConfigResource.getAssessorFinanceView());
 
-        if (config.isPresent()) {
-            config.get().setAverageAssessorScore(competitionAssessmentConfigResource.getAverageAssessorScore());
-            config.get().setAssessorPay(competitionAssessmentConfigResource.getAssessorPay());
-            config.get().setAssessorCount(competitionAssessmentConfigResource.getAssessorCount());
-            config.get().setHasAssessmentPanel(competitionAssessmentConfigResource.getHasAssessmentPanel());
-            config.get().setHasInterviewStage(competitionAssessmentConfigResource.getHasInterviewStage());
-            config.get().setAssessorFinanceView(competitionAssessmentConfigResource.getAssessorFinanceView());
-
-            config.get().getCompetition().setAssessorPay(competitionAssessmentConfigResource.getAssessorPay());
-            config.get().getCompetition().setAssessorCount(competitionAssessmentConfigResource.getAssessorCount());
-            config.get().getCompetition().setHasAssessmentPanel(competitionAssessmentConfigResource.getHasAssessmentPanel());
-            config.get().getCompetition().setHasAssessmentPanel(competitionAssessmentConfigResource.getHasAssessmentPanel());
-            config.get().getCompetition().setAssessorFinanceView(competitionAssessmentConfigResource.getAssessorFinanceView());
-        }
-
-        return serviceSuccess();
+                    config.getCompetition().setAssessorPay(competitionAssessmentConfigResource.getAssessorPay());
+                    config.getCompetition().setAssessorCount(competitionAssessmentConfigResource.getAssessorCount());
+                    config.getCompetition().setHasAssessmentPanel(competitionAssessmentConfigResource.getHasAssessmentPanel());
+                    config.getCompetition().setHasAssessmentPanel(competitionAssessmentConfigResource.getHasAssessmentPanel());
+                    config.getCompetition().setAssessorFinanceView(competitionAssessmentConfigResource.getAssessorFinanceView());
+                });
     }
 }
