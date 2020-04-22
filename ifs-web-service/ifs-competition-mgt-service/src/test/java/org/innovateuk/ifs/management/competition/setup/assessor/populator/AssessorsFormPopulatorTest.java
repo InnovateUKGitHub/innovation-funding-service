6f19bcdb4b003f28c4e395845a027650685310e8
Grace Williams <grace.williams@innovateuk.gov.uk>
2020-04-22 17:14:30 +0100

    IFS-7370: Fixed AssessorFormPopulatorTest

--- ifs-web-service/ifs-competition-mgt-service/src/test/java/org/innovateuk/ifs/management/competition/setup/assessor/populator/AssessorsFormPopulatorTest.java
+++ ifs-web-service/ifs-competition-mgt-service/src/test/java/org/innovateuk/ifs/management/competition/setup/assessor/populator/AssessorsFormPopulatorTest.java
@@ -1,27 +1,35 @@
 package org.innovateuk.ifs.management.competition.setup.assessor.populator;
 
+import org.innovateuk.ifs.competition.resource.CompetitionAssessmentConfigResource;
 import org.innovateuk.ifs.competition.resource.CompetitionResource;
+import org.innovateuk.ifs.competition.service.CompetitionAssessmentConfigRestService;
 import org.innovateuk.ifs.management.competition.setup.assessor.form.AssessorsForm;
 import org.innovateuk.ifs.management.competition.setup.core.form.CompetitionSetupForm;
 import org.innovateuk.ifs.management.fixtures.CompetitionFundersFixture;
-import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import java.math.BigDecimal;
 
+import static org.innovateuk.ifs.commons.rest.RestResult.restSuccess;
+import static org.innovateuk.ifs.competition.builder.CompetitionAssessmentConfigResourceBuilder.newCompetitionAssessmentConfigResource;
 import static org.innovateuk.ifs.competition.builder.CompetitionResourceBuilder.newCompetitionResource;
 import static org.innovateuk.ifs.competition.resource.AssessorFinanceView.OVERVIEW;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.when;
 
+@RunWith(MockitoJUnitRunner.Silent.class)
 public class AssessorsFormPopulatorTest {
 
+	@InjectMocks
 	private AssessorsFormPopulator populator;
-	
-	@Before
-	public void setUp() {
-		populator = new AssessorsFormPopulator();
-	}
+
+	@Mock
+	private CompetitionAssessmentConfigRestService competitionAssessmentConfigRestService;
 
 	@Test
 	public void testPopulateForm() {
@@ -37,6 +45,17 @@ public class AssessorsFormPopulatorTest {
 				.withHasAssessmentPanel(Boolean.FALSE)
 				.withHasInterviewStage(Boolean.FALSE)
 				.withAssessorFinanceView(OVERVIEW).build();
+
+		CompetitionAssessmentConfigResource competitionAssessmentConfigResource = newCompetitionAssessmentConfigResource()
+				.withAssessorCount(1)
+				.withAssessorPay(BigDecimal.TEN)
+				.withHasAssessmentPanel(Boolean.FALSE)
+				.withAssessorFinanceView(OVERVIEW)
+				.withHasInterviewStage(Boolean.FALSE)
+				.withAverageAssessorScore(Boolean.FALSE)
+				.build();
+
+		when(competitionAssessmentConfigRestService.findOneByCompetitionId(competition.getId())).thenReturn(restSuccess(competitionAssessmentConfigResource));
 
 		CompetitionSetupForm result = populator.populateForm(competition);
 		
