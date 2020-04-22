eec492f5dcce376260c01a775733d767f6eb74c4
Grace Williams <grace.williams@innovateuk.gov.uk>
2020-04-22 16:39:24 +0100

    IFS-7370: Fixed InterviewPermission unit test failures

--- ifs-web-service/ifs-competition-mgt-service/src/test/java/org/innovateuk/ifs/management/interview/security/InterviewPermissionRulesTest.java
+++ ifs-web-service/ifs-competition-mgt-service/src/test/java/org/innovateuk/ifs/management/interview/security/InterviewPermissionRulesTest.java
@@ -1,16 +1,18 @@
 package org.innovateuk.ifs.management.interview.security;
 
 import org.innovateuk.ifs.BasePermissionRulesTest;
+import org.innovateuk.ifs.competition.resource.CompetitionAssessmentConfigResource;
 import org.innovateuk.ifs.competition.resource.CompetitionCompositeId;
 import org.innovateuk.ifs.competition.resource.CompetitionResource;
 import org.innovateuk.ifs.competition.resource.CompetitionStatus;
+import org.innovateuk.ifs.competition.service.CompetitionAssessmentConfigRestService;
 import org.innovateuk.ifs.competition.service.CompetitionRestService;
-import org.innovateuk.ifs.management.interview.security.InterviewPermissionRules;
 import org.innovateuk.ifs.user.resource.UserResource;
 import org.junit.Test;
 import org.mockito.Mock;
 
 import static org.innovateuk.ifs.commons.rest.RestResult.restSuccess;
+import static org.innovateuk.ifs.competition.builder.CompetitionAssessmentConfigResourceBuilder.newCompetitionAssessmentConfigResource;
 import static org.innovateuk.ifs.competition.builder.CompetitionResourceBuilder.newCompetitionResource;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
@@ -20,6 +22,9 @@ public class InterviewPermissionRulesTest extends BasePermissionRulesTest<Interv
 
     @Mock
     private CompetitionRestService competitionRestService;
+
+    @Mock
+    private CompetitionAssessmentConfigRestService competitionAssessmentConfigRestService;
 
     @Override
     protected InterviewPermissionRules supplyPermissionRulesUnderTest() {
@@ -41,8 +46,18 @@ public class InterviewPermissionRulesTest extends BasePermissionRulesTest<Interv
                     .withCompetitionStatus(competitionStatus)
                     .build();
 
+            final CompetitionAssessmentConfigResource configResourceWithInterview = newCompetitionAssessmentConfigResource()
+                    .withHasInterviewStage(true)
+                    .build();
+            final CompetitionAssessmentConfigResource configResourceWithoutInterview = newCompetitionAssessmentConfigResource()
+                    .withHasInterviewStage(false)
+                    .build();
+
+
             when(competitionRestService.getCompetitionById(competitionWithInterviewStage.getId())).thenReturn(restSuccess(competitionWithInterviewStage));
             when(competitionRestService.getCompetitionById(competitionWithoutInterviewStage.getId())).thenReturn(restSuccess(competitionWithoutInterviewStage));
+            when(competitionAssessmentConfigRestService.findOneByCompetitionId(competitionWithInterviewStage.getId())).thenReturn(restSuccess(configResourceWithInterview));
+            when(competitionAssessmentConfigRestService.findOneByCompetitionId(competitionWithoutInterviewStage.getId())).thenReturn(restSuccess(configResourceWithoutInterview));
 
             switch (competitionStatus) {
                 case ASSESSOR_FEEDBACK: case PROJECT_SETUP: case PREVIOUS:
@@ -72,9 +87,14 @@ public class InterviewPermissionRulesTest extends BasePermissionRulesTest<Interv
                     .withHasInterviewStage(false)
                     .withCompetitionStatus(competitionStatus)
                     .build();
+            final CompetitionAssessmentConfigResource configResourceWithInterview = newCompetitionAssessmentConfigResource().withHasInterviewStage(true).build();
+            final CompetitionAssessmentConfigResource configResourceWithoutInterview = newCompetitionAssessmentConfigResource().withHasInterviewStage(false).build();
+
 
             when(competitionRestService.getCompetitionById(competitionWithInterviewStage.getId())).thenReturn(restSuccess(competitionWithInterviewStage));
             when(competitionRestService.getCompetitionById(competitionWithoutInterviewStage.getId())).thenReturn(restSuccess(competitionWithoutInterviewStage));
+            when(competitionAssessmentConfigRestService.findOneByCompetitionId(competitionWithInterviewStage.getId())).thenReturn(restSuccess(configResourceWithInterview));
+            when(competitionAssessmentConfigRestService.findOneByCompetitionId(competitionWithoutInterviewStage.getId())).thenReturn(restSuccess(configResourceWithoutInterview));
 
             if (competitionStatus == CompetitionStatus.FUNDERS_PANEL) {
                 assertTrue(rules.interviewPanelApplications(CompetitionCompositeId.id(competitionWithInterviewStage.getId()), loggedInUser));
