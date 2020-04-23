7356df8d08fd7c827289a0a3449d7ee1edd8325f
Luke Harper <lharper@worth.systems>
2020-04-23 09:41:48 +0100

    IFS-7370 only migrating comps after assessment before PS.

--- ifs-data-layer/ifs-data-service/src/main/resources/db/migration/V161_4_2__migrate_average_assessor_score_table.sql
+++ ifs-data-layer/ifs-data-service/src/main/resources/db/migration/V161_4_2__migrate_average_assessor_score_table.sql
@@ -12,6 +12,12 @@ FROM assessor_form_input_response afir
     ON fi.id = afir.form_input_id and fi.form_input_type_id=23
     INNER JOIN question q
     ON q.id = fi.question_id
+    INNER JOIN milestone assessment_milestone
+    on q.competition_id = assessment_milestone.compeitition_id and assessment_milestone.type='ASSESSORS_NOTIFIED'
+    INNER JOIN milestone notifications_milestone
+    on q.competition_id = notifications_milestone.competition_id and notifications_milestone.type='NOTIFICATIONS'
 WHERE
-p.activity_state_id = 5 -- submitted
+        p.activity_state_id = 5 -- submitted
+AND     assessment_milestone.date < now()
+AND     notifications_milestone.date > now()
 GROUP BY p.target_id;
