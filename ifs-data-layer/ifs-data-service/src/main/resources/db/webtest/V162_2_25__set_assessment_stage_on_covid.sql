5f7847ef259d20931dd6187d76b4721fdcfdc3a3
Luke Harper <lharper@worth.systems>
2020-04-28 11:08:56 +0100

    webtest fix for finance types.

--- ifs-data-layer/ifs-data-service/src/main/resources/db/webtest/V162_2_25__set_assessment_stage_on_covid.sql
+++ ifs-data-layer/ifs-data-service/src/main/resources/db/webtest/V162_2_25__set_assessment_stage_on_covid.sql
@@ -1,1 +1,5 @@
 UPDATE competition SET has_assessment_stage=false WHERE name = 'Covid-19 competition';
+
+SET @covid = (SELECT id FROM competition WHERE name = 'Covid-19 competition');
+DELETE FROM competition_finance_row_types WHERE competition_id = @covid and finance_row_type='FINANCE';
+INSERT INTO competition_finance_row_types(competition_id, finance_row_type) VALUES (@covid, 'GRANT_CLAIM_AMOUNT');
