c3fcfccc2697fceeb8c3147a4d6fcba49af60093
Luke Harper <lharper@worth.systems>
2020-04-30 10:47:38 +0100

    Add overheads back

--- ifs-data-layer/ifs-data-service/src/main/resources/db/webtest/V163_2_26__covid_setup.sql
+++ ifs-data-layer/ifs-data-service/src/main/resources/db/webtest/V163_2_26__covid_setup.sql
@@ -6,7 +6,6 @@ UPDATE competition SET covid_type='DE_MINIMIS_ROUND_2' WHERE name = '599 Covid d
 UPDATE competition SET has_assessment_stage=false WHERE covid_type = 'ADDITIONAL_FUNDING';
 SET @additional_funding_id = (SELECT id FROM competition WHERE covid_type = 'ADDITIONAL_FUNDING');
 DELETE FROM competition_finance_row_types WHERE competition_id = @additional_funding_id and finance_row_type='FINANCE';
-DELETE FROM competition_finance_row_types WHERE competition_id = @additional_funding_id and finance_row_type='OVERHEADS';
 INSERT INTO competition_finance_row_types(competition_id, finance_row_type) VALUES (@additional_funding_id, 'GRANT_CLAIM_AMOUNT');
 
 
