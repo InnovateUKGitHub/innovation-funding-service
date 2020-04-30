6230201c5045c09278e14b65f1d0dacd7a8b6b58
Luke Harper <lharper@worth.systems>
2020-04-30 13:25:44 +0100

    fixed.

--- ifs-data-layer/ifs-data-service/src/main/resources/db/webtest/V163_2_26__covid_setup.sql
+++ ifs-data-layer/ifs-data-service/src/main/resources/db/webtest/V163_2_26__covid_setup.sql
@@ -3,8 +3,8 @@ UPDATE competition SET covid_type='ADDITIONAL_FUNDING' WHERE name = '596 Covid g
 UPDATE competition SET covid_type='DE_MINIMIS_ROUND_2' WHERE name = '599 Covid de minimis round 2';
 
 -- ADDITIONAL_FUNDING
-UPDATE competition SET has_assessment_stage=false WHERE covid_type = 'ADDITIONAL_FUNDING';
 SET @additional_funding_id = (SELECT id FROM competition WHERE covid_type = 'ADDITIONAL_FUNDING');
+UPDATE competition SET has_assessment_stage=false WHERE id = @additional_funding_id;
 DELETE FROM competition_finance_row_types WHERE competition_id = @additional_funding_id and finance_row_type='FINANCE';
 INSERT INTO competition_finance_row_types(competition_id, finance_row_type) VALUES (@additional_funding_id, 'GRANT_CLAIM_AMOUNT');
 
@@ -18,10 +18,10 @@ INSERT INTO competition_finance_row_types(competition_id, finance_row_type) VALU
 -- TODO
 -- docusign
 
--- These following queries will not be needed on prod.
+-- These following queries will not be needed on prod and will be setup by comp admins.
 UPDATE competition SET covid_type='DE_MINIMIS' WHERE name = 'Project Setup Comp 18';
 
--- This will be done by comp admin
+-- All comps are 100% funding
 SET @current_max_grant_id = (select max(id) from grant_claim_maximum);
 
 INSERT INTO grant_claim_maximum (organisation_size_id, category_id, maximum)
@@ -31,7 +31,6 @@ SELECT s.id   AS organisation_size_id,
 FROM category c
 INNER JOIN organisation_size s
 WHERE c.type='RESEARCH_CATEGORY';
-
 
 DELETE g FROM grant_claim_maximum_competition g
 INNER JOIN competition c
@@ -46,6 +45,6 @@ INNER JOIN grant_claim_maximum m
 WHERE m.id > @current_max_grant_id
 AND c.covid_type IS NOT NULL;
 
--- question score, no feedback
+-- 25 question score, no feedback for covid round 2.
 update form_input set active = 0 where competition_id = @de_minimis_round_2 and form_input_type_id=2 and scope='ASSESSMENT';
 update question set assessor_maximum_score = 25 where competition_id = @de_minimis_round_2;
