6934ca8c9c817615bf2b7ea6742d10e7149ce0e7
Luke Harper <lharper@worth.systems>
2020-05-18 10:28:52 +0100

    IFS-7275 fixed.

--- ifs-data-layer/ifs-data-service/src/main/resources/db/migration/V165_1_1__migrate_bank_details_address.sql
+++ ifs-data-layer/ifs-data-service/src/main/resources/db/migration/V165_1_1__migrate_bank_details_address.sql
@@ -11,9 +11,13 @@ UPDATE bank_details bd
 
 ALTER TABLE bank_details MODIFY COLUMN address_id bigint(20) NOT NULL;
 ALTER TABLE bank_details ADD CONSTRAINT bank_details_to_address_id_fk FOREIGN KEY (address_id) REFERENCES address (id);
-ALTER TABLE bank_details DROP FOREIGN KEY bank_details_to_organisation_address_fk;
-ALTER TABLE bank_details DROP COLUMN organisation_address_id;
+
+-- We can't remove the organisation_address_id column because DW expect it, although they don't use it. remove not null and wipe data.
+ALTER TABLE bank_details MODIFY COLUMN organisation_address_id bigint(20);
+UPDATE bank_details SET organisation_address_id = NULL;
 
 -- remove unused address types.
 DELETE FROM organisation_address WHERE address_type_id in (3,4);
 DELETE FROM address_type where id in (3,4);
+
+
