67dcdcd9a265f6ac1a1bcadf6f8c200e2b3c845b
Luke Harper <lharper@worth.systems>
2020-05-19 08:54:07 +0100

    IFS-7275 new domain.

--- ifs-data-layer/ifs-data-service/src/main/resources/db/migration/V165_1_2__organisation_address_add_app_id.sql
+++ ifs-data-layer/ifs-data-service/src/main/resources/db/migration/V165_1_2__organisation_address_add_app_id.sql
@@ -1,26 +1,18 @@
--- Change organisation_address table to also be linked to an application. This will be used to store international organisation
--- addresses and the historic operating/registered addresses.
+-- Add a date to the organisation_address table to see what the latest date is.
 
 ALTER TABLE organisation_address
-  DROP INDEX UK_g3y4ooi9akaq8e98efgmljigm,
-  ADD COLUMN application_id bigint(20),
-  ADD COLUMN created_on DATETIME,
-  ADD UNIQUE KEY UK_organisation_application_address (organisation_id, address_type_id, application_id);
+  ADD COLUMN created_on DATETIME;
 
-INSERT INTO organisation_address (organisation_id, application_id, address_id, address_type_id, created_on)
-    SELECT DISTINCT
-        pr.organisation_id          AS organisation_id,
-        pr.application_id           AS application_id,
-        oaa.address_id              AS address_id,
-        oaa.address_type_id         AS address_type_id,
-        now()                       AS created_on
-    FROM process_role pr
-    INNER JOIN organisation_address oaa on oaa.organisation_id = pr.organisation_id
-    WHERE pr.organisation_id IS NOT NULL
-    GROUP BY pr.organisation_id, pr.application_id;
+UPDATE organisation_address set created_on = now();
 
-DELETE FROM organisation_address WHERE application_id IS NULL;
-
-ALTER TABLE organisation_address MODIFY COLUMN application_id bigint(20) NOT NULL;
-ALTER TABLE organisation_address ADD CONSTRAINT organisation_application_address_to_application_id FOREIGN KEY (application_id) REFERENCES application (id);
 ALTER TABLE organisation_address MODIFY COLUMN created_on DATETIME NOT NULL;
+
+-- New table to link organisation addresses with applications.
+CREATE TABLE organisation_application_address (
+  id bigint(20) NOT NULL AUTO_INCREMENT,
+  organisation_address_id bigint(20) NOT NULL,
+  application_id bigint(20) NOT NULL,
+  PRIMARY KEY (id),
+  CONSTRAINT organisation_application_address_to_application_fk FOREIGN KEY (application_id) REFERENCES application (id),
+  CONSTRAINT organisation_application_address_to_organisation_address_fk FOREIGN KEY (organisation_address_id) REFERENCES organisation_address (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
