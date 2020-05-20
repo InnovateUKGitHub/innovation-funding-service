45c7b7549f8010cbc8180e648c758edf4806eea9
Jack Jefferies <jjefferies@zaizi.com>
2020-05-20 08:48:32 +0100

    IFS-7197: fix db migration script for test dbs

--- ifs-data-layer/ifs-data-service/src/main/resources/db/migration/V165_1_1__Change_business_type_description.sql
+++ ifs-data-layer/ifs-data-service/src/main/resources/db/migration/V165_1_1__Change_business_type_description.sql
@@ -1,2 +1,2 @@
 -- IFS-7197 update business organisation description
-UPDATE `ifs`.`organisation_type` SET `description` = 'A person or organisation that provides goods or services in exchange for something of value, usually money.' WHERE (`id` = '1');
+UPDATE `organisation_type` SET `description` = 'A person or organisation that provides goods or services in exchange for something of value, usually money.' WHERE (`id` = '1');
