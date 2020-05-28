32cb628044bf354c5cce0e41ead20a9a2ec44662
Kieran Hester <khester@worth.systems>
2020-05-28 14:50:24 +0100

    IFS-7575 add option to pull other values

--- setup-files/scripts/openshift/os-apply-properties-from-secrets.sh
+++ setup-files/scripts/openshift/os-apply-properties-from-secrets.sh
@@ -33,6 +33,8 @@ function applyProperties() {
     echo "$(valueFromAws)" >> "unformatted-properties.gradle"
     sed 's/ext/\'$'\n''&/g' unformatted-properties.gradle > formatted-properties.gradle
 
+    echo formatted-properties.gradle
+
     oc create secret generic properties \
         --from-file=properties=formatted-properties.gradle \
     ${SVC_ACCOUNT_CLAUSE} --dry-run -o yaml | \
