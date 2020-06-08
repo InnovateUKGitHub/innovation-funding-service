3bd97c453be0b9d9795044b2dfab58ce5157cc9a
Prem Jonathan <pjonathan@zaizi.com>
2020-06-09 00:21:29 +0100

    fixed permission issues and fixed issues with the front end

--- ifs-data-layer/ifs-data-service/src/main/java/org/innovateuk/ifs/competition/transactional/CompetitionSetupPostAwardServiceService.java
+++ ifs-data-layer/ifs-data-service/src/main/java/org/innovateuk/ifs/competition/transactional/CompetitionSetupPostAwardServiceService.java
@@ -3,6 +3,8 @@ package org.innovateuk.ifs.competition.transactional;
 import org.innovateuk.ifs.commons.service.ServiceResult;
 import org.innovateuk.ifs.competition.resource.CompetitionPostAwardServiceResource;
 import org.innovateuk.ifs.competition.resource.PostAwardService;
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PostFilter;
 import org.springframework.security.access.prepost.PreAuthorize;
 
 public interface CompetitionSetupPostAwardServiceService {
@@ -10,6 +12,7 @@ public interface CompetitionSetupPostAwardServiceService {
     @PreAuthorize("hasPermission(#competitionId, 'org.innovateuk.ifs.competition.resource.CompetitionResource', 'CHOOSE_POST_AWARD_SERVICE')")
     ServiceResult<Void> configurePostAwardService(long competitionId, PostAwardService postAwardService);
 
-    @PreAuthorize("hasPermission(#competitionId, 'org.innovateuk.ifs.competition.resource.CompetitionResource', 'CHOOSE_POST_AWARD_SERVICE')")
+    /*@PreAuthorize("hasPermission(#competitionId, 'org.innovateuk.ifs.competition.resource.CompetitionResource', 'CHOOSE_POST_AWARD_SERVICE')")*/
+    @PostFilter("hasPermission(filterObject, 'READ')")
     ServiceResult<CompetitionPostAwardServiceResource> getPostAwardService(long competitionId);
 }
