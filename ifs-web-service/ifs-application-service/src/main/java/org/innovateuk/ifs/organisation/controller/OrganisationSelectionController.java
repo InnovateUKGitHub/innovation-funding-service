6139376531aa52a17effa0d2f56d7fb4b1623116
Adam Mapstone <adam.mapstone@innovateuk.gov.uk>
2020-06-09 01:07:25 +0100

    IFS-7639 - Stakeholder role added to PreAuthorize of organisation selection

--- ifs-web-service/ifs-application-service/src/main/java/org/innovateuk/ifs/organisation/controller/OrganisationSelectionController.java
+++ ifs-web-service/ifs-application-service/src/main/java/org/innovateuk/ifs/organisation/controller/OrganisationSelectionController.java
@@ -33,7 +33,7 @@ import static org.innovateuk.ifs.organisation.controller.OrganisationCreationTyp
 @SecuredBySpring(value="Controller", description = "An existing applicant can pick a previous organisation." +
         " An assessor will be passed on to create an organisation for the first time and become an applicant. ",
         securedType = OrganisationSelectionController.class)
-@PreAuthorize("hasAnyAuthority('applicant', 'assessor')")
+@PreAuthorize("hasAnyAuthority('applicant', 'assessor', 'stakeholder')")
 @Controller
 public class OrganisationSelectionController extends AbstractOrganisationCreationController {
 
