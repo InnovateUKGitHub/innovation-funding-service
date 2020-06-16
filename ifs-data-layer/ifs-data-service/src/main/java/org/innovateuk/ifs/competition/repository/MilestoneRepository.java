9184688b8781bf1ab190d50500b356cd3b440194
Kieran Hester <khester@worth.systems>
2020-06-16 10:30:05 +0100

    IFS-7313 tidy

--- ifs-data-layer/ifs-data-service/src/main/java/org/innovateuk/ifs/competition/repository/MilestoneRepository.java
+++ ifs-data-layer/ifs-data-service/src/main/java/org/innovateuk/ifs/competition/repository/MilestoneRepository.java
@@ -2,7 +2,6 @@ package org.innovateuk.ifs.competition.repository;
 
 import org.innovateuk.ifs.competition.domain.Milestone;
 import org.innovateuk.ifs.competition.resource.MilestoneType;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
