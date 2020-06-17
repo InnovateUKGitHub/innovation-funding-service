29de5c1ae8f3b1ce03112fe05d8a188121b3c359
Jonathan Lee <jlee@worth.systems>
2020-06-17 14:44:41 +0100

    IFS-7235 - removing script not needed as test data being generated

--- ifs-data-layer/ifs-data-service/src/main/resources/db/webtest/V166_3_27__investor-partnerships-tandc-questions.sql
+++
@@ -1,4 +0,0 @@
-UPDATE question
-SET name = 'Investor Partnerships terms and conditions', short_name = 'Investor Partnerships terms and conditions', description = 'Investor Partnerships terms and conditions'
-WHERE question_setup_type = 'TERMS_AND_CONDITIONS'
- AND competition_id = (select id from competition where funding_type= 'INVESTOR_PARTNERSHIPS');
