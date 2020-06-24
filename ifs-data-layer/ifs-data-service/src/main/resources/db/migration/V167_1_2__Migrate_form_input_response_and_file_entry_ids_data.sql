baede03cc15e5440959ef6ade9e6a3ff83fc46e7
Adam Mapstone <adam.mapstone@innovateuk.gov.uk>
2020-06-24 08:00:13 +0100

    IFS-7309 - Word count update script.

--- ifs-data-layer/ifs-data-service/src/main/resources/db/migration/V167_1_2__Migrate_form_input_response_and_file_entry_ids_data.sql
+++ ifs-data-layer/ifs-data-service/src/main/resources/db/migration/V167_1_2__Migrate_form_input_response_and_file_entry_ids_data.sql
@@ -1,5 +1,9 @@
--- IFS-7309 - Migration of form_input_response_id and file_entry_id to form_input_file_entry table
+-- IFS-7309 - Migration of form_input_response_id and file_entry_id to form_input_file_entry table and set word_count to 1 for FILEUPLOAD input type.
 INSERT INTO form_input_response_file_entry(form_input_response_id, file_entry_id)
 SELECT fir.id, file_entry_id
 FROM file_entry fe
 INNER JOIN form_input_response fir ON fe.id = fir.file_entry_id;
+
+UPDATE form_input
+SET word_count = 1
+WHERE form_input_type_id = 4;
