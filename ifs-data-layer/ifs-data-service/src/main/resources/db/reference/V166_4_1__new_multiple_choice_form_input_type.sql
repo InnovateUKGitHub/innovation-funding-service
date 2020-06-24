dbf86a6c003b0f6041ead2e512ed845dd33aed42
Luke Harper <lharper@worth.systems>
2020-06-24 14:03:42 +0100

    IFS-7701 comment.

--- ifs-data-layer/ifs-data-service/src/main/resources/db/reference/V166_4_1__new_multiple_choice_form_input_type.sql
+++ ifs-data-layer/ifs-data-service/src/main/resources/db/reference/V166_4_1__new_multiple_choice_form_input_type.sql
@@ -2,7 +2,7 @@
 delete from form_input_type
     where id not in (2,4,21,22,23,29);
 
--- Insert new multiple choice form input.
+-- Insert new multiple choice form input type.
 insert into form_input_type (id, name)
     VALUES (30, 'MULTIPLE_CHOICE');
 
