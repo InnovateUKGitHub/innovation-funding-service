b9d4639c26b8cc8dc04f4eaa8b6e047a93b9e89f
Jonathan Lee <jlee@worth.systems>
2020-07-01 09:03:21 +0100

    IFS-7700 - fixing flyway script

--- ifs-data-layer/ifs-data-service/src/main/resources/db/reference/V168_1_1__Add_edi_questions_to_template_competitions.sql
+++ ifs-data-layer/ifs-data-service/src/main/resources/db/reference/V168_1_1__Add_edi_questions_to_template_competitions.sql
@@ -13,14 +13,14 @@ values (0, '<a href="https://www.surveymonkey.co.uk/r/ifsaccount" target="_blank
 (0, '<a href="https://www.surveymonkey.co.uk/r/ifsaccount" target="_blank" rel="external">Complete the survey (opens in new window).</a><p>We will not use this data when we assess your application. We collect this data anonymously and only use it to help us understand our funding recipients better.</p>', 1, 0, 'Have you completed the EDI survey?', 'Equality, diversity and inclusion', 3, 9, 103, 'GENERAL', 'EQUALITY_DIVERSITY_INCLUSION');
 
 insert into form_input (word_count, form_input_type_id, competition_id, included_in_application_summary, description, priority, question_id, scope, active)
-values (2, 30, 2, 1, 'Equality, diversity and inclusion', 0, (select id from question where competition_id = 2 and name = 'Equality, diversity and inclusion'), 'APPLICATION', 1),
-(2, 30, 3, 1, 'Equality, diversity and inclusion', 0, (select id from question where competition_id = 3 and name = 'Equality, diversity and inclusion'), 'APPLICATION', 1),
-(2, 30, 4, 1, 'Equality, diversity and inclusion', 0, (select id from question where competition_id = 4 and name = 'Equality, diversity and inclusion'), 'APPLICATION', 1),
-(2, 30, 5, 1, 'Equality, diversity and inclusion', 0, (select id from question where competition_id = 5 and name = 'Equality, diversity and inclusion'), 'APPLICATION', 1),
-(2, 30, 6, 1, 'Equality, diversity and inclusion', 0, (select id from question where competition_id = 6 and name = 'Equality, diversity and inclusion'), 'APPLICATION', 1),
-(2, 30, 7, 1, 'Equality, diversity and inclusion', 0, (select id from question where competition_id = 7 and name = 'Equality, diversity and inclusion'), 'APPLICATION', 1),
-(2, 30, 8, 1, 'Equality, diversity and inclusion', 0, (select id from question where competition_id = 8 and name = 'Equality, diversity and inclusion'), 'APPLICATION', 1),
-(2, 30, 9, 1, 'Equality, diversity and inclusion', 0, (select id from question where competition_id = 9 and name = 'Equality, diversity and inclusion'), 'APPLICATION', 1);
+values (2, 30, 2, 1, 'Equality, diversity and inclusion', 0, (select id from question where competition_id = 2 and short_name = 'Equality, diversity and inclusion'), 'APPLICATION', 1),
+(2, 30, 3, 1, 'Equality, diversity and inclusion', 0, (select id from question where competition_id = 3 and short_name = 'Equality, diversity and inclusion'), 'APPLICATION', 1),
+(2, 30, 4, 1, 'Equality, diversity and inclusion', 0, (select id from question where competition_id = 4 and short_name = 'Equality, diversity and inclusion'), 'APPLICATION', 1),
+(2, 30, 5, 1, 'Equality, diversity and inclusion', 0, (select id from question where competition_id = 5 and short_name = 'Equality, diversity and inclusion'), 'APPLICATION', 1),
+(2, 30, 6, 1, 'Equality, diversity and inclusion', 0, (select id from question where competition_id = 6 and short_name = 'Equality, diversity and inclusion'), 'APPLICATION', 1),
+(2, 30, 7, 1, 'Equality, diversity and inclusion', 0, (select id from question where competition_id = 7 and short_name = 'Equality, diversity and inclusion'), 'APPLICATION', 1),
+(2, 30, 8, 1, 'Equality, diversity and inclusion', 0, (select id from question where competition_id = 8 and short_name = 'Equality, diversity and inclusion'), 'APPLICATION', 1),
+(2, 30, 9, 1, 'Equality, diversity and inclusion', 0, (select id from question where competition_id = 9 and short_name = 'Equality, diversity and inclusion'), 'APPLICATION', 1);
 
 insert into multiple_choice_option (text, form_input_id)
 values ('Yes', (select id from form_input where competition_id = 2 and description = 'Equality, diversity and inclusion')),
