d00b90ddc90ecec32557449c9d79e95047ba87fd
abirajdar <abirajdar@zaizi.com>
2020-07-06 11:53:01 +0100

    IFS-7733 referring common deploy functions

--- setup-files/scripts/openshift/os-deploy-services.sh
+++ setup-files/scripts/openshift/os-deploy-services.sh
@@ -6,6 +6,9 @@ PROJECT=$1
 TARGET=$2
 VERSION=$3
 
+. $(dirname $0)/deploy-functions.sh
+. $(dirname $0)/local-deploy-functions.sh
+
 PROJECT=$(getProjectName $PROJECT $TARGET)
 SVC_ACCOUNT_TOKEN=$(getSvcAccountToken)
 HOST=$(getHost $TARGET)
