2e39ba254fcbb5222550919cc5aea81cbf6e8b85
Jonathan Lee <jlee@worth.systems>
2020-07-21 16:27:38 +0100

    IFS-7813 removing unused reference

--- ifs-web-service/ifs-project-setup-service/src/main/java/org/innovateuk/ifs/project/pendingpartner/populator/ProjectYourFundingViewModelPopulator.java
+++ ifs-web-service/ifs-project-setup-service/src/main/java/org/innovateuk/ifs/project/pendingpartner/populator/ProjectYourFundingViewModelPopulator.java
@@ -1,13 +1,10 @@
 package org.innovateuk.ifs.project.pendingpartner.populator;
 
-import org.innovateuk.ifs.application.resource.ApplicationResource;
-import org.innovateuk.ifs.application.service.ApplicationRestService;
 import org.innovateuk.ifs.competition.resource.CompetitionResource;
 import org.innovateuk.ifs.competition.service.CompetitionRestService;
 import org.innovateuk.ifs.finance.resource.ProjectFinanceResource;
 import org.innovateuk.ifs.finance.service.GrantClaimMaximumRestService;
 import org.innovateuk.ifs.organisation.resource.OrganisationResource;
-import org.innovateuk.ifs.organisation.resource.OrganisationTypeEnum;
 import org.innovateuk.ifs.project.finance.service.ProjectFinanceRestService;
 import org.innovateuk.ifs.project.pendingpartner.viewmodel.ProjectYourFundingViewModel;
 import org.innovateuk.ifs.project.projectteam.PendingPartnerProgressRestService;
@@ -16,7 +13,6 @@ import org.innovateuk.ifs.project.resource.ProjectResource;
 import org.innovateuk.ifs.project.service.ProjectRestService;
 import org.innovateuk.ifs.user.service.OrganisationRestService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -30,9 +26,6 @@ public class ProjectYourFundingViewModelPopulator {
 
     @Autowired
     private ProjectRestService projectRestService;
-
-    @Autowired
-    private ApplicationRestService applicationRestService;
 
     @Autowired
     private OrganisationRestService organisationRestService;
