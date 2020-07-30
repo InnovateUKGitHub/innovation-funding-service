bedf0e327924ba0bf4e53176478bf86ddc953cfe
Prem Jonathan <pjonathan@zaizi.com>
2020-07-30 17:05:55 +0100

    added additional case for non-ktp-competition

--- ifs-web-service/ifs-assessment-service/src/test/java/org/innovateuk/ifs/assessment/feedback/viewmodel/AssessmentFeedbackApplicationDetailsViewModelTest.java
+++ ifs-web-service/ifs-assessment-service/src/test/java/org/innovateuk/ifs/assessment/feedback/viewmodel/AssessmentFeedbackApplicationDetailsViewModelTest.java
@@ -5,6 +5,7 @@ import org.innovateuk.ifs.competition.publiccontent.resource.FundingType;
 import org.innovateuk.ifs.competition.resource.CompetitionResource;
 import org.junit.Test;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class AssessmentFeedbackApplicationDetailsViewModelTest {
@@ -16,11 +17,28 @@ public class AssessmentFeedbackApplicationDetailsViewModelTest {
         long daysLeft = 10L;
         long daysLeftPercentage = 5L;
 
-        CompetitionResource competitionResource = CompetitionResourceBuilder.newCompetitionResource().withFundingType(FundingType.KTP).build();
+        CompetitionResource competitionResource = CompetitionResourceBuilder.newCompetitionResource()
+                .withFundingType(FundingType.KTP).build();
 
         AssessmentFeedbackApplicationDetailsViewModel viewModel = new AssessmentFeedbackApplicationDetailsViewModel(applicationId, null, null,
                 applicationDurationInMonths, daysLeft, daysLeftPercentage, null, competitionResource.isKtp());
 
         assertTrue(viewModel.isKtpCompetition());
+    }
+
+    @Test
+    public void testNonKtpCompetition() {
+        long applicationId = 1L;
+        long applicationDurationInMonths = 15L;
+        long daysLeft = 10L;
+        long daysLeftPercentage = 5L;
+
+        CompetitionResource competitionResource = CompetitionResourceBuilder.newCompetitionResource()
+                .withFundingType(FundingType.GRANT).build();
+
+        AssessmentFeedbackApplicationDetailsViewModel viewModel = new AssessmentFeedbackApplicationDetailsViewModel(applicationId, null, null,
+                applicationDurationInMonths, daysLeft, daysLeftPercentage, null, competitionResource.isKtp());
+
+        assertFalse(viewModel.isKtpCompetition());
     }
 }
