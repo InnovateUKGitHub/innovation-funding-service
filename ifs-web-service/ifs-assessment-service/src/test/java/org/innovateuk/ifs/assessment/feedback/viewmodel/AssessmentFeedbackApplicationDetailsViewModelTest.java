3f49f24308d66d714b290071d3223313cdca2c22
Prem Jonathan <pjonathan@zaizi.com>
2020-07-30 16:03:20 +0100

    fixed assessor application details view to hide project start date for ktp competition

---
+++ ifs-web-service/ifs-assessment-service/src/test/java/org/innovateuk/ifs/assessment/feedback/viewmodel/AssessmentFeedbackApplicationDetailsViewModelTest.java
@@ -0,0 +1,26 @@
+package org.innovateuk.ifs.assessment.feedback.viewmodel;
+
+import org.innovateuk.ifs.competition.builder.CompetitionResourceBuilder;
+import org.innovateuk.ifs.competition.publiccontent.resource.FundingType;
+import org.innovateuk.ifs.competition.resource.CompetitionResource;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class AssessmentFeedbackApplicationDetailsViewModelTest {
+
+    @Test
+    public void testKtpCompetition() {
+        long applicationId = 1L;
+        long applicationDurationInMonths = 15L;
+        long daysLeft = 10L;
+        long daysLeftPercentage = 5L;
+
+        CompetitionResource competitionResource = CompetitionResourceBuilder.newCompetitionResource().withFundingType(FundingType.KTP).build();
+
+        AssessmentFeedbackApplicationDetailsViewModel viewModel = new AssessmentFeedbackApplicationDetailsViewModel(applicationId, null, null,
+                applicationDurationInMonths, daysLeft, daysLeftPercentage, null, competitionResource.isKtp());
+
+        assertTrue(viewModel.isKtpCompetition());
+    }
+}
