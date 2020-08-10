b4382c55ab28aa4c506abb1eaeefdd06db43c710
Prem Jonathan <pjonathan@zaizi.com>
2020-08-10 12:01:32 +0100

    fixed unit test failure

--- ifs-web-service/ifs-competition-mgt-service/src/test/java/org/innovateuk/ifs/management/admin/viewmodel/InviteUserViewModelTest.java
+++ ifs-web-service/ifs-competition-mgt-service/src/test/java/org/innovateuk/ifs/management/admin/viewmodel/InviteUserViewModelTest.java
@@ -1,6 +1,7 @@
 package org.innovateuk.ifs.management.admin.viewmodel;
 
 import org.innovateuk.ifs.management.admin.form.InviteUserView;
+import org.innovateuk.ifs.user.resource.Role;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -11,20 +12,22 @@ public class InviteUserViewModelTest {
     @Test
     public void InviteInternalUserViewModel() {
 
-        InviteUserViewModel viewModel = new InviteUserViewModel(InviteUserView.INTERNAL_USER);
+        InviteUserViewModel viewModel = new InviteUserViewModel(InviteUserView.INTERNAL_USER, Role.internalRoles());
 
         assertEquals(InviteUserView.INTERNAL_USER, viewModel.getType());
         assertEquals(InviteUserView.INTERNAL_USER.getName(), viewModel.getTypeName());
         assertTrue(viewModel.isInternal());
+        assertEquals(viewModel.getRoles(), Role.internalRoles());
     }
 
     @Test
     public void InviteExternalUserViewModel() {
 
-        InviteUserViewModel viewModel = new InviteUserViewModel(InviteUserView.EXTERNAL_USER);
+        InviteUserViewModel viewModel = new InviteUserViewModel(InviteUserView.EXTERNAL_USER, Role.externalRolesToInvite());
 
         assertEquals(InviteUserView.EXTERNAL_USER, viewModel.getType());
         assertEquals(InviteUserView.EXTERNAL_USER.getName(), viewModel.getTypeName());
         assertTrue(viewModel.isExternal());
+        assertEquals(viewModel.getRoles(), Role.externalRolesToInvite());
     }
 }
