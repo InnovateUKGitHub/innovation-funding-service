03acca3dd9392c3b55ed99656e22100704787d6c
Kieran Hester <khester@worth.systems>
2020-08-14 15:07:49 +0100

    IFS-7976 advice

--- ifs-resources/src/main/java/org/innovateuk/ifs/user/resource/Role.java
+++ ifs-resources/src/main/java/org/innovateuk/ifs/user/resource/Role.java
@@ -37,7 +37,7 @@ public enum Role implements Identifiable {
     STAKEHOLDER                 (20, "stakeholder",               "Stakeholder"),
     LIVE_PROJECTS_USER          (21, "live_projects_user",        "Live projects user"),
     EXTERNAL_FINANCE            (22, "external_finance",        "External finance reviewer"),
-    KNOWLEDGE_TRANSFER_ADVISOR  (23, "knowledge_transfer_advisor", "Knowledge transfer advisor");
+    KNOWLEDGE_TRANSFER_ADVISOR  (23, "knowledge_transfer_adviser", "Knowledge transfer adviser");
 
     final long id;
     final String name;
