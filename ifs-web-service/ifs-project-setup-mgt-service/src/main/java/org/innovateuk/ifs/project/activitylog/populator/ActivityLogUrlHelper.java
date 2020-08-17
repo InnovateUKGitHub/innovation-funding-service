7ccb797043199118d60b0557ebf021f8246dd2bc
Grace Williams <grace.williams@innovateuk.gov.uk>
2020-08-17 11:26:21 +0100

    IFS-7742: Removed deleted from urlhelp file

--- ifs-web-service/ifs-project-setup-mgt-service/src/main/java/org/innovateuk/ifs/project/activitylog/populator/ActivityLogUrlHelper.java
+++ ifs-web-service/ifs-project-setup-mgt-service/src/main/java/org/innovateuk/ifs/project/activitylog/populator/ActivityLogUrlHelper.java
@@ -59,11 +59,9 @@ public class ActivityLogUrlHelper {
             case SPEND_PROFILE_APPROVED:
             case SPEND_PROFILE_REJECTED:
                 return format("/project-setup-management/project/%d/spend-profile/approval", project.getId());
-            case SPEND_PROFILE_DELETED:
             case GRANT_OFFER_LETTER_UPLOADED:
             case GRANT_OFFER_LETTER_PUBLISHED:
             case GRANT_OFFER_LETTER_SIGNED:
-            case GRANT_OFFER_LETTER_DELETED:
             case GRANT_OFFER_LETTER_APPROVED:
             case GRANT_OFFER_LETTER_REJECTED:
                 return format("/project-setup-management/project/%d/grant-offer-letter/send", project.getId());
