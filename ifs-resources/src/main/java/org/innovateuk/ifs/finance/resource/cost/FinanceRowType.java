549b9b23ac60b996af9d2d00d76e814e133c190e
Prem Jonathan <pjonathan@zaizi.com>
2020-08-20 17:07:51 +0100

    renamed ktp finance row type display name

--- ifs-resources/src/main/java/org/innovateuk/ifs/finance/resource/cost/FinanceRowType.java
+++ ifs-resources/src/main/java/org/innovateuk/ifs/finance/resource/cost/FinanceRowType.java
@@ -33,11 +33,11 @@ public enum FinanceRowType implements CostCategoryGenerator<FinanceRowType> {
     PROCUREMENT_OVERHEADS("procurement_overheads", "Overheads", INCLUDE_IN_SPEND_PROFILE, COST, APPEARS_IN_PROJECT_COSTS_ACCORDION),
 
     /* KTP */
-    ASSOCIATE_SALARY_COSTS("associate_salary_costs", "Associate salary costs", INCLUDE_IN_SPEND_PROFILE, COST, APPEARS_IN_PROJECT_COSTS_ACCORDION),
+    ASSOCIATE_SALARY_COSTS("associate_salary_costs", "Associate Employment", INCLUDE_IN_SPEND_PROFILE, COST, APPEARS_IN_PROJECT_COSTS_ACCORDION),
     ASSOCIATE_DEVELOPMENT_COSTS("associate_development", "Associate development costs", INCLUDE_IN_SPEND_PROFILE, COST, APPEARS_IN_PROJECT_COSTS_ACCORDION),
     CONSUMABLES("consumables", "Consumables", INCLUDE_IN_SPEND_PROFILE, COST, APPEARS_IN_PROJECT_COSTS_ACCORDION),
-    ASSOCIATE_SUPPORT("associate_support", "Associate support costs", INCLUDE_IN_SPEND_PROFILE, COST, APPEARS_IN_PROJECT_COSTS_ACCORDION),
-    KNOWLEDGE_BASE("knowledge_base", "Knowledge base advisor", INCLUDE_IN_SPEND_PROFILE, COST, APPEARS_IN_PROJECT_COSTS_ACCORDION),
+    ASSOCIATE_SUPPORT("associate_support", "Additional associate support", INCLUDE_IN_SPEND_PROFILE, COST, APPEARS_IN_PROJECT_COSTS_ACCORDION),
+    KNOWLEDGE_BASE("knowledge_base", "Knowledge base supervisor", INCLUDE_IN_SPEND_PROFILE, COST, APPEARS_IN_PROJECT_COSTS_ACCORDION),
     ESTATE_COSTS("estate_costs", "Estate", INCLUDE_IN_SPEND_PROFILE, COST, APPEARS_IN_PROJECT_COSTS_ACCORDION),
     ADDITIONAL_COMPANY_COSTS("additional_company_costs", "Additional company costs");
 
