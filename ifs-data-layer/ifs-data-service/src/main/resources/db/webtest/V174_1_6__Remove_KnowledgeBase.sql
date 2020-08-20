c817f377766ead28bd827f83f33f970e77a74487
Kieran Hester <khester@worth.systems>
2020-08-20 19:41:47 +0100

    IFS-7807 web test

--- ifs-data-layer/ifs-data-service/src/main/resources/db/webtest/V174_1_6__Remove_KnowledgeBase.sql
+++ ifs-data-layer/ifs-data-service/src/main/resources/db/webtest/V174_1_6__Remove_KnowledgeBase.sql
@@ -1,5 +1,5 @@
 -- Needed to remove the knowledge base and address's as they are included in the mysqldump, cannot just exclude table as need the other address's for web test
-SET SQL_SAFE_UPDATES = 0;
-delete from knowledge_base;
+SET foreign_key_checks = 0;
+DELETE FROM knowledge_base;
 DELETE FROM address;
-SET SQL_SAFE_UPDATES = 1;
+SET foreign_key_checks = 1;
