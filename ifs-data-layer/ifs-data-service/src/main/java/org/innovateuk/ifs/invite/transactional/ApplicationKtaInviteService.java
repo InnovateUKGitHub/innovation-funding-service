f51f7ffb6cf6b8d3afd116c1dc4f82e7b26ec579
Grace Williams <grace.williams@innovateuk.gov.uk>
2020-08-24 15:46:57 +0100

    IFS-8001: Fixed failing unit tests

--- ifs-data-layer/ifs-data-service/src/main/java/org/innovateuk/ifs/invite/transactional/ApplicationKtaInviteService.java
+++ ifs-data-layer/ifs-data-service/src/main/java/org/innovateuk/ifs/invite/transactional/ApplicationKtaInviteService.java
@@ -1,5 +1,6 @@
 package org.innovateuk.ifs.invite.transactional;
 
+import org.innovateuk.ifs.commons.security.SecuredBySpring;
 import org.innovateuk.ifs.commons.service.ServiceResult;
 import org.innovateuk.ifs.invite.resource.ApplicationKtaInviteResource;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -19,6 +20,9 @@ public interface ApplicationKtaInviteService {
     ServiceResult<Void> removeKtaInviteByApplication(long applicationId);
 
     @PreAuthorize("hasAuthority('system_registrar')")
+    @SecuredBySpring(value = "READ_INVITE_ON_HASH",
+            description = "The System Registration user can read an invite for a given hash",
+            additionalComments = "The hash should be unguessable so the only way to successfully call this method would be to have been given the hash in the first place")
     ServiceResult<ApplicationKtaInviteResource> getKtaInviteByHash(String hash);
 
     @PreAuthorize("hasPermission(#hash, 'org.innovateuk.ifs.invite.resource.ApplicationKtaInviteResource', 'ACCEPT')")
