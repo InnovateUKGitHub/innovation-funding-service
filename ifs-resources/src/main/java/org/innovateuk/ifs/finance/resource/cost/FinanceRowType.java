3d405e7fa5de8f92fea6655034697105dc2cf301
Jonathan Lee <jlee@worth.systems>
2020-08-28 09:18:11 +0100

    IFS-7956 changing display name of previous funding to other funding for the table

--- ifs-resources/src/main/java/org/innovateuk/ifs/finance/resource/cost/FinanceRowType.java
+++ ifs-resources/src/main/java/org/innovateuk/ifs/finance/resource/cost/FinanceRowType.java
@@ -40,7 +40,7 @@ public enum FinanceRowType implements CostCategoryGenerator<FinanceRowType> {
     KNOWLEDGE_BASE("knowledge_base", "Knowledge base advisor", INCLUDE_IN_SPEND_PROFILE, COST, APPEARS_IN_PROJECT_COSTS_ACCORDION),
     ESTATE_COSTS("estate_costs", "Estate", INCLUDE_IN_SPEND_PROFILE, COST, APPEARS_IN_PROJECT_COSTS_ACCORDION),
     ADDITIONAL_COMPANY_COSTS("additional_company_costs", "Additional company costs"),
-    PREVIOUS_FUNDING("previous_funding", "Previous Funding", COST);
+    PREVIOUS_FUNDING("previous_funding", "Other funding", COST);
 
 
     enum FinanceRowOptions {
