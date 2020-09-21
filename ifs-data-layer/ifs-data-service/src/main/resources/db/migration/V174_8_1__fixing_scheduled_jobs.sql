d4c67f0d95eeedcc4e687aea87c0e99ba1af106c
Luke Harper <lharper@worth.systems>
2020-09-21 14:53:37 +0100

    Fixed.

--- ifs-data-layer/ifs-data-service/src/main/resources/db/migration/V174_8_1__fixing_scheduled_jobs.sql
+++ ifs-data-layer/ifs-data-service/src/main/resources/db/migration/V174_8_1__fixing_scheduled_jobs.sql
@@ -1,7 +1,7 @@
 update profile p
-set p.doi_notified_on = now()
 inner join user u on p.id = u.profile_id
-and p.doi_notified_on < '2020-04-06 00:00:00' OR p.doi_notified_on is NULL
+set p.doi_notified_on = now()
+where p.doi_notified_on < '2020-04-06 00:00:00' OR p.doi_notified_on is NULL;
 
 update schedule_status set active = 0
 where job_name in ('ASSESSOR_DOI_EXPIRY', 'DOCUSIGN_IMPORT');
