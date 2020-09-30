c51e5509a214f3086e4f0556f364e821f8e928bf
Jonathan Lee <jlee@worth.systems>
2020-09-30 14:43:13 +0100

    IFS-8212 updating conditions under which to show feedback

--- ifs-web-service/ifs-application-service/src/main/java/org/innovateuk/ifs/application/summary/populator/ApplicationSummaryViewModelPopulator.java
+++ ifs-web-service/ifs-application-service/src/main/java/org/innovateuk/ifs/application/summary/populator/ApplicationSummaryViewModelPopulator.java
@@ -46,7 +46,7 @@ public class ApplicationSummaryViewModelPopulator {
     private UserRestService userRestService;
 
     public ApplicationSummaryViewModel populate(ApplicationResource application, CompetitionResource competition, UserResource user) {
-        ApplicationReadOnlySettings settings = defaultSettings().setIncludeAllAssessorFeedback(shouldDisplayFeedback(competition, application));
+        ApplicationReadOnlySettings settings = defaultSettings().setIncludeAllAssessorFeedback(shouldDisplayFeedback(competition, application, user));
         ApplicationReadOnlyViewModel applicationReadOnlyViewModel = applicationReadOnlyViewModelPopulator.populate(application, competition, user, settings);
 
         final InterviewFeedbackViewModel interviewFeedbackViewModel;
@@ -74,11 +74,14 @@ public class ApplicationSummaryViewModelPopulator {
                                                interviewFeedbackViewModel);
     }
 
-    private boolean shouldDisplayFeedback(CompetitionResource competition, ApplicationResource application) {
+    private boolean shouldDisplayFeedback(CompetitionResource competition, ApplicationResource application, UserResource user) {
+        boolean feedbackReleased = competition.getCompetitionStatus().isFeedbackReleased();
+        if (competition.isKtp()) {
+            return user.hasAnyRoles(Role.KNOWLEDGE_TRANSFER_ADVISER) && feedbackReleased;
+        }
         boolean isApplicationAssignedToInterview = interviewAssignmentRestService.isAssignedToInterview(application.getId()).getSuccess();
-        boolean feedbackAvailable = competition.getCompetitionStatus().isFeedbackReleased() || isApplicationAssignedToInterview;
-        return application.isSubmitted()
-                && feedbackAvailable;
+        boolean feedbackAvailable = feedbackReleased || isApplicationAssignedToInterview;
+        return application.isSubmitted() && feedbackAvailable;
     }
 
     private boolean isProjectWithdrawn(Long applicationId) {
