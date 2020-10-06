cb0d389020acfdd82d1dbb7c6a86f373678a55d7
Adam Mapstone <adam.mapstone@innovateuk.gov.uk>
2020-10-06 11:34:09 +0100

    IFS-8402 - Comment added.

--- ifs-data-layer/ifs-data-service/src/main/java/org/innovateuk/ifs/cofunder/controller/CofunderCompetitionDashboardController.java
+++ ifs-data-layer/ifs-data-service/src/main/java/org/innovateuk/ifs/cofunder/controller/CofunderCompetitionDashboardController.java
@@ -1,5 +1,6 @@
 package org.innovateuk.ifs.cofunder.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,4 +8,5 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/cofunder")
 public class CofunderCompetitionDashboardController {
 
+    @GetMapping("/assessment/assessor/dashboard")
 }
