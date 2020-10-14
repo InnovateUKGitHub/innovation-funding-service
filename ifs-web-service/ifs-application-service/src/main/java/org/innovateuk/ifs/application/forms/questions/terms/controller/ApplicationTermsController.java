edf0d9cad904222d4fb8ae3dc915d33bdb08d588
Prem Jonathan <pjonathan@zaizi.com>
2020-10-14 14:57:30 +0100

    fixed the constructor for Ts & Cs

--- ifs-web-service/ifs-application-service/src/main/java/org/innovateuk/ifs/application/forms/questions/terms/controller/ApplicationTermsController.java
+++ ifs-web-service/ifs-application-service/src/main/java/org/innovateuk/ifs/application/forms/questions/terms/controller/ApplicationTermsController.java
@@ -15,6 +15,7 @@ import org.innovateuk.ifs.controller.ValidationHandler;
 import org.innovateuk.ifs.user.resource.ProcessRoleResource;
 import org.innovateuk.ifs.user.resource.UserResource;
 import org.innovateuk.ifs.user.service.UserRestService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -47,6 +48,7 @@ public class ApplicationTermsController {
 
     }
 
+    @Autowired
     public ApplicationTermsController(UserRestService userRestService,
                                       QuestionStatusRestService questionStatusRestService,
                                       ApplicationRestService applicationRestService,
