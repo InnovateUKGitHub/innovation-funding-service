ffc3ba898b7fc66ce8501af5107df661280d4fe6
Luke Harper <lharper@worth.systems>
2020-10-14 15:09:26 +0100

    IFS-8409 fixed issues.

--- ifs-web-service/ifs-assessment-service/src/main/java/org/innovateuk/ifs/cofunder/form/CofunderResponseForm.java
+++ ifs-web-service/ifs-assessment-service/src/main/java/org/innovateuk/ifs/cofunder/form/CofunderResponseForm.java
@@ -2,14 +2,20 @@ package org.innovateuk.ifs.cofunder.form;
 
 import org.innovateuk.ifs.cofunder.resource.CofunderAssignmentResource;
 import org.innovateuk.ifs.cofunder.resource.CofunderState;
+import org.innovateuk.ifs.commons.validation.constraints.WordCount;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 public class CofunderResponseForm {
 
     private long assignmentId;
+    @NotNull(message = "{validation.cofunder.response.decision.required}")
     private Boolean decision;
     @NotBlank(message = "{validation.cofunder.response.comments.required}")
+    @Size(max = 5000, message = "{validation.field.too.many.characters}")
+    @WordCount(max = 250, message = "{validation.field.max.word.count}")
     private String comments;
 
     public CofunderResponseForm() {
