9c7c375e5c0396a68b632b6846f77af2688b4163
Prem Jonathan <pjonathan@zaizi.com>
2020-10-15 15:59:19 +0100

    updated enum and parent table updates

--- ifs-data-layer/ifs-data-service/src/main/resources/db/migration/V176_1_1__Alter_assessor_finance_view_type.sql
+++ ifs-data-layer/ifs-data-service/src/main/resources/db/migration/V176_1_1__Alter_assessor_finance_view_type.sql
@@ -1,4 +1,7 @@
 -- IFS-8453 - include All finance view for assessor
 
+ALTER TABLE competition
+    MODIFY COLUMN assessor_finance_view enum('OVERVIEW', 'DETAILED', 'ALL');
+
 ALTER TABLE competition_assessment_config
     MODIFY COLUMN assessor_finance_view enum('OVERVIEW', 'DETAILED', 'ALL');
