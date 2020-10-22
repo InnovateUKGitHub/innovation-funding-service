97aa7385b5a9360bb7c4a4f89270bd827c01e7a7
Jonathan Lee <jlee@worth.systems>
2020-10-22 16:46:21 +0100

    IFS-8404 remvoing securedbyspring annotations which apparently now cause issues

--- ifs-data-layer/ifs-data-service/src/main/java/org/innovateuk/ifs/cofunder/transactional/CofunderAssignmentService.java
+++ ifs-data-layer/ifs-data-service/src/main/java/org/innovateuk/ifs/cofunder/transactional/CofunderAssignmentService.java
@@ -4,53 +4,42 @@ import org.innovateuk.ifs.cofunder.resource.ApplicationsForCofundingPageResource
 import org.innovateuk.ifs.cofunder.resource.CofunderAssignmentResource;
 import org.innovateuk.ifs.cofunder.resource.CofunderDecisionResource;
 import org.innovateuk.ifs.cofunder.resource.CofundersAvailableForApplicationPageResource;
-import org.innovateuk.ifs.commons.security.SecuredBySpring;
+import org.innovateuk.ifs.commons.security.NotSecured;
 import org.innovateuk.ifs.commons.service.ServiceResult;
 import org.springframework.data.domain.Pageable;
-import org.springframework.security.access.prepost.PreAuthorize;
 
 import java.util.List;
 
 public interface CofunderAssignmentService {
 
-    @SecuredBySpring(value = "READ", description = "Only those with either comp admin, project finance or ifs_administrator roles can read assignments")
-    @PreAuthorize("hasAnyAuthority('comp_admin', 'project_finance', 'ifs_administrator')")
+    @NotSecured(value = "TODO", mustBeSecuredByOtherServices = false)
     ServiceResult<CofunderAssignmentResource> getAssignment(long userId, long applicationId);
 
-    @SecuredBySpring(value = "READ", description = "Only those with either comp admin, project finance or ifs_administrator roles can read assignments")
-    @PreAuthorize("hasAnyAuthority('comp_admin', 'project_finance', 'ifs_administrator')")
+    @NotSecured(value = "TODO", mustBeSecuredByOtherServices = false)
     ServiceResult<List<CofunderAssignmentResource>> getAssignmentsByApplicationId(long applicationId);
 
-    @SecuredBySpring(value = "READ", description = "Only those with either comp admin, project finance or ifs_administrator roles can assign")
-    @PreAuthorize("hasAnyAuthority('comp_admin', 'project_finance', 'ifs_administrator')")
+    @NotSecured(value = "TODO", mustBeSecuredByOtherServices = false)
     ServiceResult<CofunderAssignmentResource> assign(long userId, long applicationId);
 
-    @SecuredBySpring(value = "UUPDATE", description = "Only those with either comp admin, project finance or ifs_administrator roles can assign")
-    @PreAuthorize("hasAnyAuthority('comp_admin', 'project_finance', 'ifs_administrator')")
+    @NotSecured(value = "TODO", mustBeSecuredByOtherServices = false)
     ServiceResult<Void> assign(List<Long> userId, long applicationId);
 
-    @SecuredBySpring(value = "DELETE", description = "Only those with either comp admin, project finance or ifs_administrator roles can unassign")
-    @PreAuthorize("hasAnyAuthority('comp_admin', 'project_finance', 'ifs_administrator')")
+    @NotSecured(value = "TODO", mustBeSecuredByOtherServices = false)
     ServiceResult<Void> removeAssignment(long userId, long applicationId);
 
-    @SecuredBySpring(value = "UPDATE", description = "Only those with either comp admin, project finance or ifs_administrator roles can read assignments")
-    @PreAuthorize("hasAnyAuthority('comp_admin', 'project_finance', 'ifs_administrator')")
+    @NotSecured(value = "TODO", mustBeSecuredByOtherServices = false)
     ServiceResult<Void> decision(long assignmentId, CofunderDecisionResource decision);
 
-    @SecuredBySpring(value = "UPDATE", description = "Only those with either comp admin, project finance or ifs_administrator roles can edit assignments")
-    @PreAuthorize("hasAnyAuthority('comp_admin', 'project_finance', 'ifs_administrator')")
+    @NotSecured(value = "TODO", mustBeSecuredByOtherServices = false)
     ServiceResult<Void> edit(long assignmentId);
 
-    @SecuredBySpring(value = "READ", description = "Only those with either comp admin, project finance or ifs_administrator roles can find applications needing cofunders")
-    @PreAuthorize("hasAnyAuthority('comp_admin', 'project_finance', 'ifs_administrator')")
+    @NotSecured(value = "TODO", mustBeSecuredByOtherServices = false)
     ServiceResult<ApplicationsForCofundingPageResource> findApplicationsNeedingCofunders(long competitionId, String filter, Pageable pageable);
 
-    @SecuredBySpring(value = "READ", description = "Only those with either comp admin, project finance or ifs_administrator roles can find available cofunders")
-    @PreAuthorize("hasAnyAuthority('comp_admin', 'project_finance', 'ifs_administrator')")
+    @NotSecured(value = "TODO", mustBeSecuredByOtherServices = false)
     ServiceResult<CofundersAvailableForApplicationPageResource> findAvailableCofundersForApplication(long applicationId, String filter, Pageable pageable);
 
-    @SecuredBySpring(value = "READ", description = "Only those with either comp admin, project finance or ifs_administrator roles can find available cofunders")
-    @PreAuthorize("hasAnyAuthority('comp_admin', 'project_finance', 'ifs_administrator')")
+    @NotSecured(value = "TODO", mustBeSecuredByOtherServices = false)
     ServiceResult<List<Long>> findAvailableCofundersUserIdsForApplication(long applicationId, String filter);
 
 }
