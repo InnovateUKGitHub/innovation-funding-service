a374437983e429227ccbefcd1c7fdca91ecd5ddb
Adam Mapstone <adam.mapstone@innovateuk.gov.uk>
2020-10-29 09:10:40 +0000

    IFS-8398 - WIP add of knowledge base and addresses.

---
+++ ifs-data-layer/ifs-data-service/src/main/resources/db/reference/V180_1_2__Add_Knowledge_Base_Addresses.sql
@@ -0,0 +1,40 @@
+INSERT INTO address (address_line1, address_line2, address_line3, town, postcode, county, country) VALUES ('Kydd Building', '40 Bell Street', '', 'Dundee', 'DD1 1HG', '', 'United Kingdom');
+SET @knowledge_base_id = (SELECT id FROM knowledge_base WHERE name = 'Abertay University');
+
+INSERT INTO address (address_line1, address_line2, address_line3, town, postcode, county, country) VALUES ('Visualisation Centre', 'Penglais', '', 'Aberystwyth', 'SY23 3BF', 'Dyfed', 'United Kingdom');
+SET @knowledge_base_id = (SELECT id FROM knowledge_base WHERE name = 'Aberystwyth University (Prifysgol Aberystwyth)');
+
+INSERT INTO address (address_line1, address_line2, address_line3, town, postcode, county, country) VALUES ('College Road', '', '', 'Bangor', 'LL57 2DG', 'Gwynedd', 'United Kingdom');
+SET @knowledge_base_id = (SELECT id FROM knowledge_base WHERE name = 'Bangor University (Prifysgol Bangor)');
+
+INSERT INTO address (address_line1, address_line2, address_line3, town, postcode, county, country) VALUES ('7 Queens Road', '', '', 'Belfast', 'BT3 9FQ', 'Gwynedd', 'United Kingdom');
+SET @knowledge_base_id = (SELECT id FROM knowledge_base WHERE name = 'Belfast Metropolitan College');
+
+INSERT INTO address (address_line1, address_line2, address_line3, town, postcode, county, country) VALUES ('Western Avenue', '', '', 'Cardiff', 'CF5 2YB', '', 'United Kingdom');
+SET @knowledge_base_id = (SELECT id FROM knowledge_base WHERE name = 'Cardiff Metropolitan University (Prifysgol Metropolitan Caerdydd)');
+
+
+
+
+
+
+INSERT INTO address (address_line1, address_line2, address_line3, town, postcode, county, country) VALUES ('', '', '', '', '', '', 'United Kingdom');
+SET @knowledge_base_id = (SELECT id FROM knowledge_base WHERE name = 'Cardiff University (Prifysgol Caerdydd)');
+
+INSERT INTO address (address_line1, address_line2, address_line3, town, postcode, county, country) VALUES ('', '', '', '', '', '', 'United Kingdom');
+SET @knowledge_base_id = (SELECT id FROM knowledge_base WHERE name = '');
+
+INSERT INTO address (address_line1, address_line2, address_line3, town, postcode, county, country) VALUES ('', '', '', '', '', '', 'United Kingdom');
+SET @knowledge_base_id = (SELECT id FROM knowledge_base WHERE name = '');
+
+INSERT INTO address (address_line1, address_line2, address_line3, town, postcode, county, country) VALUES ('', '', '', '', '', '', 'United Kingdom');
+SET @knowledge_base_id = (SELECT id FROM knowledge_base WHERE name = '');
+
+INSERT INTO address (address_line1, address_line2, address_line3, town, postcode, county, country) VALUES ('', '', '', '', '', '', 'United Kingdom');
+SET @knowledge_base_id = (SELECT id FROM knowledge_base WHERE name = '');
+
+INSERT INTO address (address_line1, address_line2, address_line3, town, postcode, county, country) VALUES ('', '', '', '', '', '', 'United Kingdom');
+SET @knowledge_base_id = (SELECT id FROM knowledge_base WHERE name = '');
+
+INSERT INTO address (address_line1, address_line2, address_line3, town, postcode, county, country) VALUES ('', '', '', '', '', '', 'United Kingdom');
+SET @knowledge_base_id = (SELECT id FROM knowledge_base WHERE name = '');
