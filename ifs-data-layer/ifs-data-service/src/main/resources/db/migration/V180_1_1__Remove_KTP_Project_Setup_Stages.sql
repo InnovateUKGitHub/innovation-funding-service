3c45a5378a4ed0af83c699cf572e6c35edbd336b
Kieran Hester <khester@worth.systems>
2020-11-04 12:07:25 +0000

    IFS-8329 fix f

--- ifs-data-layer/ifs-data-service/src/main/resources/db/migration/V180_1_1__Remove_KTP_Project_Setup_Stages.sql
+++ ifs-data-layer/ifs-data-service/src/main/resources/db/migration/V180_1_1__Remove_KTP_Project_Setup_Stages.sql
@@ -4,4 +4,4 @@ DELETE ps
 FROM project_stages ps
 INNER JOIN competition c on c.id = ps.competition_id
 WHERE c.funding_type = "KTP"
-AND ps.project_setup_stage in ("DOCUMENTS", "BANK_DETAILS", "SPEND_PROfILE");
+AND ps.project_setup_stage in ("DOCUMENTS", "BANK_DETAILS", "SPEND_PROFILE");
