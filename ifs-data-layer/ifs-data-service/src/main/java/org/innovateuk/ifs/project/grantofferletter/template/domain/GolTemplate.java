b617c79771d1d2858617fc5043ea89f88d54335a
Luke Harper <lharper@worth.systems>
2020-11-13 14:55:03 +0000

    start

---
+++ ifs-data-layer/ifs-data-service/src/main/java/org/innovateuk/ifs/project/grantofferletter/template/domain/GolTemplate.java
@@ -0,0 +1,12 @@
+package org.innovateuk.ifs.project.grantofferletter.template.domain;
+
+import org.innovateuk.ifs.competition.domain.TermsAndConditions;
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
+
+@Entity
+@DiscriminatorValue("GOL")
+public class GolTemplate extends TermsAndConditions {
+}
---
+++ ifs-data-layer/ifs-data-service/src/main/java/org/innovateuk/ifs/project/grantofferletter/template/repository/GolTemplateRepository.java
@@ -0,0 +1,12 @@
+package org.innovateuk.ifs.project.grantofferletter.template.repository;
+
+import org.innovateuk.ifs.project.grantofferletter.template.domain.GolTemplate;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Repository for GolTemplate
+ */
+public interface GolTemplateRepository extends CrudRepository<GolTemplate, Long> {
+
+    GolTemplate findTopByOrderByVersionDesc();
+}
