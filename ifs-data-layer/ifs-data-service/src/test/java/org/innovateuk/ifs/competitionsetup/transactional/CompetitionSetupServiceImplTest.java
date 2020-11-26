a7234e1f46fa43a5a59d97f97ccdea8fdb271794
Punniya Rajkumar <punniya.rajkumar@innovateuk.ukri.org>
2020-11-26 10:35:30 +0000

    IFS-8496: Unit test case update

--- ifs-data-layer/ifs-data-service/src/test/java/org/innovateuk/ifs/competitionsetup/transactional/CompetitionSetupServiceImplTest.java
+++ ifs-data-layer/ifs-data-service/src/test/java/org/innovateuk/ifs/competitionsetup/transactional/CompetitionSetupServiceImplTest.java
@@ -36,10 +36,12 @@ import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.http.HttpStatus;
 
+import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import static java.util.Arrays.asList;
 import static org.innovateuk.ifs.LambdaMatcher.createLambdaMatcher;
