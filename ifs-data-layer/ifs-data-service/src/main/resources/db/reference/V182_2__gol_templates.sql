f6a487a1d87ac85e321c40becdd6e97572175a29
Luke Harper <lharper@worth.systems>
2020-11-27 10:32:18 +0000

    Fixing patch comment.

--- ifs-data-layer/ifs-data-service/src/main/resources/db/reference/V182_2__gol_templates.sql
+++ ifs-data-layer/ifs-data-service/src/main/resources/db/reference/V182_2__gol_templates.sql
@@ -3,9 +3,8 @@ SELECT id
 FROM user
 WHERE email = 'ifs_system_maintenance_user@innovateuk.org');
 
--- TODO check these ids
-SET @default_gol_template_id = 50;
-SET @KTP_gol_template_id = 51;
+SET @default_gol_template_id = 38;
+SET @KTP_gol_template_id = 39;
 
 INSERT INTO terms_and_conditions (id, name, template, version, type, created_by, created_on, modified_by, modified_on)
 VALUES (@default_gol_template_id, 'Default GOL Template', 'default-gol-template', 1, 'GOL',
