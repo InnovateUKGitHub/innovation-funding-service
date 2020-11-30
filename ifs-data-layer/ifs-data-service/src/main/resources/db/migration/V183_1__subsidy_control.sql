2c26483360382a22cc6603cbe607b818fbc77b32
Jonathan Lee <jlee@worth.systems>
2020-11-30 16:04:18 +0000

    IFS-8787 removing weird comment

--- ifs-data-layer/ifs-data-service/src/main/resources/db/migration/V183_1__subsidy_control.sql
+++ ifs-data-layer/ifs-data-service/src/main/resources/db/migration/V183_1__subsidy_control.sql
@@ -2,6 +2,5 @@
 
 ALTER TABLE competition ADD COLUMN funding_rules ENUM('STATE_AID', 'SUBSIDY_CONTROL', 'NOT_AID') NULL AFTER state_aid;
 
--- Migrate the full_application_finance field, but not for templates
 UPDATE competition SET funding_rules = 'STATE_AID' WHERE state_aid = 1;
 UPDATE competition SET funding_rules = 'NOT_AID' WHERE state_aid = 0;
