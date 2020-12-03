e64875abe77c372c3631267e81d8f7b8b30cbe80
richard <rpopple@worth.systems>
2020-12-03 15:01:16 +0100

    IFS-8516 removing no longer required script

--- ifs-data-layer/ifs-data-service/src/main/resources/db/webtest/V9999_19__Remove_applicant_role.sql
+++
@@ -1,5 +0,0 @@
--- TODO Configure user builder to register stakeholders and monitoring officers and ktas and then this can be deleted
-SET @applicant_role_id =
-(SELECT id FROM role WHERE name = 'applicant');
-SET @assessor_role_id =
-(SELECT id FROM role WHERE name = 'assessor');
