f9bb99e7396d15ee805f1962f8abe4d86b14a53c
prasadduddi <prasad.d.79@gmail.com>
2020-12-03 15:04:05 +0000

    IFS-8751: Updated validation message

--- ifs-web-service/ifs-application-service/src/main/java/org/innovateuk/ifs/application/forms/questions/applicationdetails/form/ApplicationDetailsForm.java
+++ ifs-web-service/ifs-application-service/src/main/java/org/innovateuk/ifs/application/forms/questions/applicationdetails/form/ApplicationDetailsForm.java
@@ -34,6 +34,7 @@ public class ApplicationDetailsForm {
     private LocalDate startDate;
 
     @NotNull
+    @Range(min = 1, message = "{validation.project.duration.range.invalid}")
     private Long durationInMonths;
 
     private Boolean resubmission;
