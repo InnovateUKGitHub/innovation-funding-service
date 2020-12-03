208b6fe234f455e25f3193e4f87cc0646f6b5e01
richard <rpopple@worth.systems>
2020-12-03 16:52:07 +0100

    IFS-8516 removing unused import

--- ifs-data-layer/ifs-data-service/src/test/java/org/innovateuk/ifs/testdata/builders/BaseUserDataBuilder.java
+++ ifs-data-layer/ifs-data-service/src/test/java/org/innovateuk/ifs/testdata/builders/BaseUserDataBuilder.java
@@ -3,7 +3,6 @@ package org.innovateuk.ifs.testdata.builders;
 import org.innovateuk.ifs.testdata.builders.data.BaseUserData;
 import org.innovateuk.ifs.token.domain.Token;
 import org.innovateuk.ifs.token.resource.TokenType;
-import org.innovateuk.ifs.user.command.GrantRoleCommand;
 import org.innovateuk.ifs.user.domain.User;
 import org.innovateuk.ifs.user.resource.Role;
 import org.innovateuk.ifs.user.resource.UserResource;
