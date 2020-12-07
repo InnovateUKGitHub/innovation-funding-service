02144fd2b8f9b2685f50f35d686e97a5a967312b
Sam Grayston <sgrayston@zaizi.com>
2020-12-07 13:55:02 +0000

    IFS-8667: migration script amendments

--- ifs-data-layer/ifs-data-service/src/main/resources/db/migration/V184_1__add_heukar_partner_org_table.sql
+++ ifs-data-layer/ifs-data-service/src/main/resources/db/migration/V184_1__add_heukar_partner_org_table.sql
@@ -1,15 +1,13 @@
-CREATE TABLE `ifs`.`heukar_partner_organisation` (
-  `id` BIGINT(20) NOT NULL AUTO_INCREMENT,
-  `application_id` BIGINT(20) NULL,
-  `organisation_type_id` BIGINT(20) NULL,
-  PRIMARY KEY (`id`),
-  CONSTRAINT `heukar_partner_organisation_application_fk`
-    FOREIGN KEY (`application_id`)
-    REFERENCES `ifs`.`application` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `heukar_partner_organisation_org_type_fk`
-    FOREIGN KEY (`organisation_type_id`)
-    REFERENCES `ifs`.`organisation_type` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION);
+create TABLE heukar_partner_organisation (
+  id BIGINT(20) NOT NULL AUTO_INCREMENT,
+  application_id BIGINT(20) NULL,
+  organisation_type_id BIGINT(20) NULL,
+  PRIMARY KEY (id),
+  CONSTRAINT heukar_partner_organisation_application_fk
+    FOREIGN KEY (application_id)
+    REFERENCES application (id),
+  CONSTRAINT heukar_partner_organisation_org_type_fk
+    FOREIGN KEY (organisation_type_id)
+    REFERENCES organisation_type (id)
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
