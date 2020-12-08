6ed48c9650cc74ce764094f9c04067d483dc2363
Sam Grayston <sgrayston@zaizi.com>
2020-12-08 09:45:22 +0000

    IFS-8667: Added secured annotations to service methods

--- ifs-data-layer/ifs-data-service/src/main/java/org/innovateuk/ifs/heukar/transactional/HeukarPartnerOrganisationService.java
+++ ifs-data-layer/ifs-data-service/src/main/java/org/innovateuk/ifs/heukar/transactional/HeukarPartnerOrganisationService.java
@@ -1,5 +1,6 @@
 package org.innovateuk.ifs.heukar.transactional;
 
+import org.innovateuk.ifs.commons.security.SecuredBySpring;
 import org.innovateuk.ifs.commons.service.ServiceResult;
 import org.innovateuk.ifs.heukar.domain.HeukarPartnerOrganisation;
 import org.innovateuk.ifs.organisation.resource.HeukarPartnerOrganisationResource;
@@ -10,18 +11,23 @@ import java.util.List;
 public interface HeukarPartnerOrganisationService {
 
     @PreAuthorize("hasAuthority('applicant')")
+    @SecuredBySpring(value = "READ", description = "An applicant can view the partner organisations for their application")
     ServiceResult<List<HeukarPartnerOrganisationResource>> findByApplicationId(long applicationId);
 
     @PreAuthorize("hasAuthority('applicant')")
+    @SecuredBySpring(value = "CREATE", description = "An applicant can create partner organisations for their application")
     ServiceResult<HeukarPartnerOrganisation> addNewPartnerOrgToApplication(long applicationId, long organisationTypeId);
 
     @PreAuthorize("hasAuthority('applicant')")
+    @SecuredBySpring(value = "UPDATE", description = "An applicant can make changes to the partner organisations for their application")
     ServiceResult<HeukarPartnerOrganisation> updatePartnerOrganisation(Long id, long organisationTypeId);
 
     @PreAuthorize("hasAuthority('applicant')")
+    @SecuredBySpring(value = "DELETE", description = "An applicant can make changes to the partner organisations for their application")
     ServiceResult<Void> deletePartnerOrganisation(Long id);
 
     @PreAuthorize("hasAuthority('applicant')")
+    @SecuredBySpring(value = "READ", description = "An applicant can view the partner organisations for their application")
     ServiceResult<HeukarPartnerOrganisationResource> findOne(Long id);
 
 }
