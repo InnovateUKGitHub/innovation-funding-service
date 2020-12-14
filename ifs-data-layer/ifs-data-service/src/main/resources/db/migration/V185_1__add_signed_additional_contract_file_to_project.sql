0d5411fdaef64bc910da2a2c3cb77cc6fcb1c61c
richard <rpopple@worth.systems>
2020-12-14 13:19:49 +0100

    IFS-8314 removal of database patch for ZDD. That will be done in a seperate pull request

--- ifs-data-layer/ifs-data-service/src/main/resources/db/migration/V185_1__add_signed_additional_contract_file_to_project.sql
+++
@@ -1,4 +0,0 @@
--- IFS-8737 upload signed annex
-
-ALTER TABLE project ADD COLUMN signed_additional_contract_file_entry_id bigint(20);
-ALTER TABLE project ADD CONSTRAINT `signed_additional_contract_file_entry_id_fk` FOREIGN KEY (`signed_additional_contract_file_entry_id`) REFERENCES `file_entry` (`id`);
