1d2255a27c29897c2c4a3c750edd286a527ad4e1
Prem Jonathan <pjonathan@zaizi.com>
2020-12-22 17:45:17 +0000

    fixed script

--- ifs-data-layer/ifs-data-service/src/main/resources/db/migration/V189_2__Multiple_assessment_periods.sql
+++ ifs-data-layer/ifs-data-service/src/main/resources/db/migration/V189_2__Multiple_assessment_periods.sql
@@ -3,8 +3,3 @@
 ALTER TABLE milestone
         ADD COLUMN parent_id BIGINT(20) DEFAULT NULL,
         ADD CONSTRAINT `FK_milestone_parent_id` FOREIGN KEY (`parent_id`) REFERENCES `milestone` (`id`);
-
-ALTER TABLE process
-        ADD COLUMN milestone_id BIGINT(20) DEFAULT NULL,
-        ADD CONSTRAINT `FK_process_milestone_id` FOREIGN KEY (`milestone_id`) REFERENCES `milestone` (`id`);
-
