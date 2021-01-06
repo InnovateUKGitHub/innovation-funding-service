2331d3476d49640eae9f0e3f44a3c4c5f300b820
Luke Harper <lharper@worth.systems>
2021-01-06 11:31:19 +0000

    IFS-8938 fixed controller test.

--- ifs-web-service/ifs-application-service/src/test/java/org/innovateuk/ifs/application/forms/sections/procurement/milestones/controller/ApplicationProcurementMilestonesControllerTest.java
+++ ifs-web-service/ifs-application-service/src/test/java/org/innovateuk/ifs/application/forms/sections/procurement/milestones/controller/ApplicationProcurementMilestonesControllerTest.java
@@ -3,37 +3,25 @@ package org.innovateuk.ifs.application.forms.sections.procurement.milestones.con
 import org.innovateuk.ifs.BaseControllerMockMVCTest;
 import org.innovateuk.ifs.application.forms.sections.procurement.milestones.form.ProcurementMilestoneForm;
 import org.innovateuk.ifs.application.forms.sections.procurement.milestones.form.ProcurementMilestonesForm;
+import org.innovateuk.ifs.application.forms.sections.procurement.milestones.populator.ApplicationProcurementMilestoneViewModelPopulator;
 import org.innovateuk.ifs.application.forms.sections.procurement.milestones.populator.ProcurementMilestoneFormPopulator;
 import org.innovateuk.ifs.application.forms.sections.procurement.milestones.saver.ApplicationProcurementMilestoneFormSaver;
 import org.innovateuk.ifs.application.forms.sections.procurement.milestones.viewmodel.ApplicationProcurementMilestonesViewModel;
-import org.innovateuk.ifs.application.resource.ApplicationResource;
-import org.innovateuk.ifs.application.service.ApplicationRestService;
-import org.innovateuk.ifs.application.service.SectionService;
 import org.innovateuk.ifs.application.service.SectionStatusRestService;
-import org.innovateuk.ifs.finance.resource.ApplicationFinanceResource;
-import org.innovateuk.ifs.finance.service.ApplicationFinanceRestService;
 import org.innovateuk.ifs.procurement.milestone.resource.ApplicationProcurementMilestoneResource;
 import org.innovateuk.ifs.procurement.milestone.service.ApplicationProcurementMilestoneRestService;
 import org.innovateuk.ifs.user.service.ProcessRoleRestService;
 import org.junit.Test;
 import org.mockito.Mock;
-import org.springframework.test.web.servlet.MvcResult;
 
-import java.math.BigDecimal;
-import java.math.BigInteger;
 import java.util.List;
 
-import static com.google.common.collect.Lists.newArrayList;
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.hamcrest.core.Is.is;
-import static org.innovateuk.ifs.application.builder.ApplicationResourceBuilder.newApplicationResource;
 import static org.innovateuk.ifs.application.forms.ApplicationFormUtil.APPLICATION_BASE_URL;
 import static org.innovateuk.ifs.commons.error.ValidationMessages.noErrors;
 import static org.innovateuk.ifs.commons.rest.RestResult.restSuccess;
 import static org.innovateuk.ifs.commons.service.ServiceResult.serviceSuccess;
 import static org.innovateuk.ifs.procurement.milestone.builder.ApplicationProcurementMilestoneBuilder.newApplicationProcurementMilestoneResource;
 import static org.innovateuk.ifs.user.builder.ProcessRoleResourceBuilder.newProcessRoleResource;
-import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -45,16 +33,13 @@ public class ApplicationProcurementMilestonesControllerTest extends BaseControll
     private static final long SECTION_ID = 2L;
     private static final long PROCESS_ROLE_ID = 3L;
     private static final long ORGANISATION_ID = 4L;
-    private static final String VIEW = "application/sections/procurement-milestones/procurement-milestones";
+    private static final String VIEW = "application/sections/procurement-milestones/application-procurement-milestones";
 
     @Mock
     private ProcurementMilestoneFormPopulator formPopulator;
 
     @Mock
     private ApplicationProcurementMilestoneRestService restService;
-
-    @Mock
-    private ApplicationRestService applicationRestService;
 
     @Mock
     private ApplicationProcurementMilestoneFormSaver saver;
@@ -66,10 +51,7 @@ public class ApplicationProcurementMilestonesControllerTest extends BaseControll
     private ProcessRoleRestService processRoleRestService;
 
     @Mock
-    private SectionService sectionService;
-
-    @Mock
-    private ApplicationFinanceRestService applicationFinanceRestService;
+    private ApplicationProcurementMilestoneViewModelPopulator viewModelPopulator;
 
     @Override
     protected ApplicationProcurementMilestonesController supplyControllerUnderTest() {
@@ -87,20 +69,14 @@ public class ApplicationProcurementMilestonesControllerTest extends BaseControll
         form.getMilestones().put(String.valueOf(milestones.get(0).getId()), milestoneForm);
         when(restService.getByApplicationIdAndOrganisationId(APPLICATION_ID, ORGANISATION_ID)).thenReturn(restSuccess(milestones));
         when(formPopulator.populate(milestones)).thenReturn(form);
-        mockViewModel();
+        ApplicationProcurementMilestonesViewModel model = mockViewModel();
 
-        MvcResult result = mockMvc.perform(get(APPLICATION_BASE_URL + "{applicationId}/form/procurement-milestones/organisation/{organisationId}/section/{sectionId}",
+        mockMvc.perform(get(APPLICATION_BASE_URL + "{applicationId}/form/procurement-milestones/organisation/{organisationId}/section/{sectionId}",
                 APPLICATION_ID, ORGANISATION_ID, SECTION_ID))
                 .andExpect(status().isOk())
                 .andExpect(model().attribute("form", form))
-                .andExpect(view().name(VIEW))
-                .andReturn();
-
-        ApplicationProcurementMilestonesViewModel model = (ApplicationProcurementMilestonesViewModel) result.getModelAndView().getModel().get("model");
-
-        assertThat(model.getDurations(), is(equalTo(newArrayList(1L, 2L, 3L, 4L, 5L))));
-        assertThat(model.getFinancesUrl(), is(equalTo(String.format("/application/%d/form/FINANCE/%d", APPLICATION_ID, ORGANISATION_ID))));
-        assertThat(model.getFundingAmount(), is(equalTo(BigInteger.TEN)));
+                .andExpect(model().attribute("model", model))
+                .andExpect(view().name(VIEW));
     }
 
     @Test
@@ -175,14 +151,9 @@ public class ApplicationProcurementMilestonesControllerTest extends BaseControll
         verify(saver).removeRowFromForm(any(ProcurementMilestonesForm.class), eq(rowToRemove));
     }
 
-    private void mockViewModel() {
-        ApplicationFinanceResource finance = mock(ApplicationFinanceResource.class);
-        ApplicationResource application = newApplicationResource()
-                .withDurationInMonths(5L)
-                .build();
-        when(applicationFinanceRestService.getFinanceDetails(APPLICATION_ID, ORGANISATION_ID)).thenReturn(restSuccess(finance));
-        when(finance.getTotalFundingSought()).thenReturn(BigDecimal.TEN);
-        when(applicationRestService.getApplicationById(APPLICATION_ID)).thenReturn(restSuccess(application));
-        when(sectionService.getCompleted(APPLICATION_ID, ORGANISATION_ID)).thenReturn(newArrayList(SECTION_ID));
+    private ApplicationProcurementMilestonesViewModel mockViewModel() {
+        ApplicationProcurementMilestonesViewModel model = mock(ApplicationProcurementMilestonesViewModel.class);
+        when(viewModelPopulator.populate(getLoggedInUser(), APPLICATION_ID, ORGANISATION_ID, SECTION_ID)).thenReturn(model);
+        return model;
     }
 }
