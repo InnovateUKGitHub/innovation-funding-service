6de25ca1012affbfd86e1c0399ed717f3d878647
Kieran Hester <khester@worth.systems>
2021-01-08 09:37:24 +0000

    IFS-8941 fix

--- ifs-data-layer/ifs-data-service/src/main/java/org/innovateuk/ifs/procurement/milestone/transactional/ProjectProcurementMilestoneServiceImpl.java
+++ ifs-data-layer/ifs-data-service/src/main/java/org/innovateuk/ifs/procurement/milestone/transactional/ProjectProcurementMilestoneServiceImpl.java
@@ -15,6 +15,7 @@ import java.util.List;
 
 import static java.util.stream.Collectors.toList;
 import static org.innovateuk.ifs.commons.error.CommonErrors.notFoundError;
+import static org.innovateuk.ifs.commons.service.ServiceResult.serviceSuccess;
 import static org.innovateuk.ifs.util.EntityLookupCallbacks.find;
 
 @Service
@@ -46,11 +47,10 @@ public class ProjectProcurementMilestoneServiceImpl
     }
 
     @Override
-    public ServiceResult<List<ProjectProcurementMilestoneResource>> getByProjectIdAndOrganisationId(long applicationId, long organisationId) {
-        return find(repository.findByProjectFinanceProjectIdAndProjectFinanceOrganisationIdOrderByMonthAsc(applicationId, organisationId), notFoundError(ProjectProcurementMilestone.class, applicationId, organisationId))
-                .andOnSuccessReturn((milestones) ->
-                        milestones.stream()
-                                .map(mapper::mapToResource)
-                                .collect(toList()));
+    public ServiceResult<List<ProjectProcurementMilestoneResource>> getByProjectIdAndOrganisationId(long projectId, long organisationId) {
+        return serviceSuccess(repository.findByProjectFinanceProjectIdAndProjectFinanceOrganisationIdOrderByMonthAsc(projectId, organisationId)
+                .stream()
+                .map(mapper::mapToResource)
+                .collect(toList()));
     }
 }
