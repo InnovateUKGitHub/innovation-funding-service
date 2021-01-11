f7a83865953a96ae2efd25ce1659b3d602d0fef5
Kieran Hester <khester@worth.systems>
2021-01-11 10:43:01 +0000

    IFS-8941 converter

---
+++ ifs-data-layer/ifs-data-service/src/main/java/org/innovateuk/ifs/project/financechecks/repository/ProcurementMilestoneStateConverter.java
@@ -0,0 +1,25 @@
+package org.innovateuk.ifs.project.financechecks.repository;
+
+import org.innovateuk.ifs.commons.util.IdentifiableEnumConverter;
+import org.innovateuk.ifs.project.finance.resource.PaymentMilestoneState;
+
+import javax.persistence.Converter;
+
+@Converter(autoApply = true)
+@SuppressWarnings(value = "unused")
+public class ProcurementMilestoneStateConverter extends IdentifiableEnumConverter<PaymentMilestoneState> {
+
+    public ProcurementMilestoneStateConverter() {
+        super(PaymentMilestoneState.class);
+    }
+
+    @Override
+    public Long convertToDatabaseColumn(PaymentMilestoneState attribute) {
+        return super.convertToDatabaseColumn(attribute);
+    }
+
+    @Override
+    public PaymentMilestoneState convertToEntityAttribute(Long dbData) {
+        return super.convertToEntityAttribute(dbData);
+    }
+}
