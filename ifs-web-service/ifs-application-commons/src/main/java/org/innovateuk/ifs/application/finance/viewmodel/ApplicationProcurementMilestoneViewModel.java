207dfebf90ed9e88dde36cfac14a22875401dc32
richard <rpopple@worth.systems>
2021-01-11 16:41:41 +0100

    IFS-8958 tidy

--- ifs-web-service/ifs-application-commons/src/main/java/org/innovateuk/ifs/application/finance/viewmodel/ApplicationProcurementMilestoneViewModel.java
+++ ifs-web-service/ifs-application-commons/src/main/java/org/innovateuk/ifs/application/finance/viewmodel/ApplicationProcurementMilestoneViewModel.java
@@ -5,6 +5,7 @@ import org.innovateuk.ifs.procurement.milestone.resource.ApplicationProcurementM
 import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * View model for finance/finance-summary :: payment milestones table.
@@ -22,19 +23,15 @@ public class ApplicationProcurementMilestoneViewModel {
     }
 
     public BigInteger getTotal() {
-        BigInteger total = BigInteger.ZERO;
-
-        for (ApplicationProcurementMilestoneResource resource : applicationProcurementMilestoneResources) {
-            total = total.add(resource.getPayment());
-        }
-
-        return total;
+        return applicationProcurementMilestoneResources.stream()
+                .map(ApplicationProcurementMilestoneResource::getPayment)
+                .filter(Objects::nonNull)
+                .reduce(BigInteger.ZERO, BigInteger::add);
     }
 
     public BigDecimal getPercentage(BigInteger cost) {
         BigDecimal total = new BigDecimal(getTotal());
         BigDecimal costAsDecimal = new BigDecimal(cost);
-
         return costAsDecimal.multiply(new BigDecimal(100)).divide(total, 2, BigDecimal.ROUND_HALF_EVEN);
     }
 }
