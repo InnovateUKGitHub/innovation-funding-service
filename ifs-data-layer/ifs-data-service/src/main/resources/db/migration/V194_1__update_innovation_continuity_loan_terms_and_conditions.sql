19185fed15c5b0a0e2d1c961d9faec9580292baf
richard <rpopple@worth.systems>
2021-01-12 16:49:45 +0100

    IFS-9071 inserting, not updating

--- ifs-data-layer/ifs-data-service/src/main/resources/db/migration/V194_1__update_innovation_continuity_loan_terms_and_conditions.sql
+++ ifs-data-layer/ifs-data-service/src/main/resources/db/migration/V194_1__update_innovation_continuity_loan_terms_and_conditions.sql
@@ -1,1 +1,9 @@
-UPDATE terms_and_conditions SET version = 2, modified_on = NOW() WHERE template = "innovation-continuity-loan";
+SET @new_terms_id = 43;
+SET @system_maintenance_user_id = (
+    SELECT id
+    FROM user
+    WHERE email = 'ifs_system_maintenance_user@innovateuk.org');
+
+INSERT INTO terms_and_conditions (id, name, template, version, type, created_by, created_on, modified_by, modified_on)
+VALUES (@new_terms_id, 'Loans', 'loans-terms-and-conditions-v2', 2, 'GRANT',
+        @system_maintenance_user_id, now(), @system_maintenance_user_id, now());
