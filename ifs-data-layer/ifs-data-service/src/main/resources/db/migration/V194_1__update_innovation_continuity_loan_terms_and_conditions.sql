586421aa7149e30dbd43df73f7d1467e424e6bd9
richard <rpopple@worth.systems>
2021-01-12 17:19:47 +0100

    IFS-9071 fix script

--- ifs-data-layer/ifs-data-service/src/main/resources/db/migration/V194_1__update_innovation_continuity_loan_terms_and_conditions.sql
+++ ifs-data-layer/ifs-data-service/src/main/resources/db/migration/V194_1__update_innovation_continuity_loan_terms_and_conditions.sql
@@ -5,5 +5,5 @@ SET @system_maintenance_user_id = (
     WHERE email = 'ifs_system_maintenance_user@innovateuk.org');
 
 INSERT INTO terms_and_conditions (id, name, template, version, type, created_by, created_on, modified_by, modified_on)
-VALUES (@new_terms_id, 'Loans', 'loans-terms-and-conditions-v2', 2, 'GRANT',
+VALUES (@new_terms_id, 'Innovation Continuity Loan', 'innovation-continuity-loan-v2', 2, 'GRANT',
         @system_maintenance_user_id, now(), @system_maintenance_user_id, now());
