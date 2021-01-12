561f4238d6b28a22d7ee95a4869c02bd013d8d95
Kieran Hester <khester@worth.systems>
2021-01-12 09:13:18 +0000

    IFS-8941 permission

--- ifs-data-layer/ifs-data-service/src/main/java/org/innovateuk/ifs/procurement/milestone/transactional/ProjectProcurementMilestoneService.java
+++ ifs-data-layer/ifs-data-service/src/main/java/org/innovateuk/ifs/procurement/milestone/transactional/ProjectProcurementMilestoneService.java
@@ -1,15 +1,17 @@
 package org.innovateuk.ifs.procurement.milestone.transactional;
 
+import org.innovateuk.ifs.commons.security.SecuredBySpring;
 import org.innovateuk.ifs.commons.service.ServiceResult;
 import org.innovateuk.ifs.procurement.milestone.resource.ProjectProcurementMilestoneId;
 import org.innovateuk.ifs.procurement.milestone.resource.ProjectProcurementMilestoneResource;
+import org.springframework.security.access.prepost.PreAuthorize;
 
 import java.util.List;
 
 public interface ProjectProcurementMilestoneService extends ProcurementMilestoneService<ProjectProcurementMilestoneResource, ProjectProcurementMilestoneId> {
 
-//    @PreAuthorize("hasAuthority('project_finance')")
-//    @SecuredBySpring(value = "VIEW", securedType = ProjectProcurementMilestoneResource.class, description = "Project finance user should be able to view any project milestones")
+    @PreAuthorize("hasAuthority('project_finance')")
+    @SecuredBySpring(value = "VIEW", securedType = ProjectProcurementMilestoneResource.class, description = "Project finance user should be able to view any project milestones")
     ServiceResult<List<ProjectProcurementMilestoneResource>> getByProjectIdAndOrganisationId(long projectId, long organisationId);
 
 }
