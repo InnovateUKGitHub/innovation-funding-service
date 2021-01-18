de264f89dac0dc6d9cc2f40bff4d383012419e47
Prem Jonathan <pjonathan@zaizi.com>
2021-01-18 13:24:25 +0000

    fixed save issue

--- ifs-web-service/ifs-competition-mgt-service/src/main/java/org/innovateuk/ifs/management/competition/setup/milestone/populator/MilestonesModelPopulator.java
+++ ifs-web-service/ifs-competition-mgt-service/src/main/java/org/innovateuk/ifs/management/competition/setup/milestone/populator/MilestonesModelPopulator.java
@@ -2,10 +2,12 @@ package org.innovateuk.ifs.management.competition.setup.milestone.populator;
 
 import org.innovateuk.ifs.competition.resource.CompetitionResource;
 import org.innovateuk.ifs.competition.resource.CompetitionSetupSection;
+import org.innovateuk.ifs.management.competition.setup.completionstage.util.CompletionStageUtils;
 import org.innovateuk.ifs.management.competition.setup.core.populator.CompetitionSetupSectionModelPopulator;
 import org.innovateuk.ifs.management.competition.setup.core.viewmodel.CompetitionSetupViewModel;
 import org.innovateuk.ifs.management.competition.setup.core.viewmodel.GeneralSetupViewModel;
 import org.innovateuk.ifs.management.competition.setup.milestone.viewmodel.MilestonesViewModel;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
@@ -13,6 +15,9 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class MilestonesModelPopulator implements CompetitionSetupSectionModelPopulator {
+
+    @Autowired
+    private CompletionStageUtils completionStageUtils;
 
     @Override
     public CompetitionSetupSection sectionToPopulateModel() {
@@ -22,6 +27,6 @@ public class MilestonesModelPopulator implements CompetitionSetupSectionModelPop
     @Override
     public CompetitionSetupViewModel populateModel(GeneralSetupViewModel generalViewModel, CompetitionResource competitionResource) {
 
-        return new MilestonesViewModel(generalViewModel);
+        return new MilestonesViewModel(generalViewModel, completionStageUtils);
     }
 }
--- ifs-web-service/ifs-competition-mgt-service/src/main/java/org/innovateuk/ifs/management/competition/setup/milestone/viewmodel/MilestonesViewModel.java
+++ ifs-web-service/ifs-competition-mgt-service/src/main/java/org/innovateuk/ifs/management/competition/setup/milestone/viewmodel/MilestonesViewModel.java
@@ -1,10 +1,19 @@
 package org.innovateuk.ifs.management.competition.setup.milestone.viewmodel;
 
+import org.innovateuk.ifs.management.competition.setup.completionstage.util.CompletionStageUtils;
 import org.innovateuk.ifs.management.competition.setup.core.viewmodel.CompetitionSetupViewModel;
 import org.innovateuk.ifs.management.competition.setup.core.viewmodel.GeneralSetupViewModel;
 
 public class MilestonesViewModel extends CompetitionSetupViewModel {
-    public MilestonesViewModel(GeneralSetupViewModel generalSetupViewModel) {
+
+    private CompletionStageUtils completionStageUtils;
+
+    public MilestonesViewModel(GeneralSetupViewModel generalSetupViewModel, CompletionStageUtils completionStageUtils) {
         this.generalSetupViewModel = generalSetupViewModel;
+        this.completionStageUtils = completionStageUtils;
+    }
+
+    public boolean isApplicationSubmissionEnabled() {
+        return completionStageUtils.isApplicationSubmissionEnabled(generalSetupViewModel.getCompetition().getCompletionStage());
     }
 }
