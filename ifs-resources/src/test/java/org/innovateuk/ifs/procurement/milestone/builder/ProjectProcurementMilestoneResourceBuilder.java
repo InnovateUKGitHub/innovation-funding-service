03c905961a91fd80d2e2f04a8897086322acd743
Kieran Hester <khester@worth.systems>
2021-01-18 15:26:35 +0000

    IFS-8941 tests

--- ifs-resources/src/test/java/org/innovateuk/ifs/procurement/milestone/builder/ProjectProcurementMilestoneResourceBuilder.java
+++ ifs-resources/src/test/java/org/innovateuk/ifs/procurement/milestone/builder/ProjectProcurementMilestoneResourceBuilder.java
@@ -1,20 +1,20 @@
 package org.innovateuk.ifs.procurement.milestone.builder;
 
-import org.innovateuk.ifs.procurement.milestone.resource.ProjectProcurementMilestoneResource;
+import org.innovateuk.ifs.procurement.milestone.resource.PaymentMilestoneResource;
 
 import java.util.List;
 import java.util.function.BiConsumer;
 
 import static java.util.Collections.emptyList;
 
-public class ProjectProcurementMilestoneResourceBuilder extends AbstractProcurementMilestoneResourceBuilder<ProjectProcurementMilestoneResource, ProjectProcurementMilestoneResourceBuilder> {
+public class ProjectProcurementMilestoneResourceBuilder extends AbstractProcurementMilestoneResourceBuilder<PaymentMilestoneResource, ProjectProcurementMilestoneResourceBuilder> {
 
-    private ProjectProcurementMilestoneResourceBuilder(final List<BiConsumer<Integer, ProjectProcurementMilestoneResource>> newActions) {
+    private ProjectProcurementMilestoneResourceBuilder(final List<BiConsumer<Integer, PaymentMilestoneResource>> newActions) {
         super(newActions);
     }
 
     @Override
-    protected ProjectProcurementMilestoneResourceBuilder createNewBuilderWithActions(List<BiConsumer<Integer, ProjectProcurementMilestoneResource>> actions) {
+    protected ProjectProcurementMilestoneResourceBuilder createNewBuilderWithActions(List<BiConsumer<Integer, PaymentMilestoneResource>> actions) {
         return new ProjectProcurementMilestoneResourceBuilder(actions);
     }
 
@@ -23,8 +23,8 @@ public class ProjectProcurementMilestoneResourceBuilder extends AbstractProcurem
     }
 
     @Override
-    protected ProjectProcurementMilestoneResource createInitial() {
-        return new ProjectProcurementMilestoneResource();
+    protected PaymentMilestoneResource createInitial() {
+        return new PaymentMilestoneResource();
     }
 
     public ProjectProcurementMilestoneResourceBuilder withProjectId(long... projectIds) {
