f467ac825491144ce1f83c95a28880aca833a24f
Prem Jonathan <pjonathan@zaizi.com>
2021-01-22 12:48:36 +0000

    fixed migration query

--- ifs-data-layer/ifs-data-service/src/main/resources/db/migration/V196_1__Assessment_period_migration.sql
+++ ifs-data-layer/ifs-data-service/src/main/resources/db/migration/V196_1__Assessment_period_migration.sql
@@ -6,13 +6,13 @@ FROM competition c
 INNER JOIN milestone m ON c.id = m.competition_id
 WHERE c.always_open IS NULL OR c.always_open = 0 AND m.type = 'ASSESSOR_BRIEFING';
 
-UPDATE Milestone m
+UPDATE milestone m
 INNER JOIN competition c ON m.competition_id = c.id
 INNER JOIN assessment_period ap ON m.competition_id = ap.competition_id
 SET m.parent_id = ap.id
 WHERE m.type IN ('ASSESSOR_BRIEFING', 'ASSESSOR_ACCEPTS', 'ASSESSOR_DEADLINE');
 
-UPDATE Application a
+UPDATE application a
 INNER JOIN competition c ON a.competition = c.id
 INNER JOIN assessment_period ap ON c.id = ap.competition_id
 SET a.assessment_period_id = ap.id;
