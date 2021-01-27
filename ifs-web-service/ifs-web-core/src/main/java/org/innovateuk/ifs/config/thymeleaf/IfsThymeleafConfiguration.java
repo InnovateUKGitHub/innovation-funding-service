cbd1242a5f4e865710ad41e42ef3d204a0d3063d
mark.dewey <mark.dewey@ukri.org>
2021-01-27 13:48:58 +0000

    [IFS-9141] see if we can hook into existing config a bit better

---
+++ ifs-web-service/ifs-web-core/src/main/java/org/innovateuk/ifs/config/thymeleaf/IfsThymeleafConfiguration.java
@@ -0,0 +1,47 @@
+package org.innovateuk.ifs.config.thymeleaf;
+
+import org.innovateuk.ifs.config.thymeleaf.dialect.IfSThymeleafDialect;
+import org.innovateuk.ifs.config.thymeleaf.postprocessor.IfsThymeleafPostProcessorDialect;
+import org.springframework.boot.autoconfigure.AutoConfigureBefore;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.thymeleaf.ThymeleafAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.thymeleaf.dialect.IDialect;
+import org.thymeleaf.templatemode.TemplateMode;
+
+/**
+ * ThymeleafAutoConfiguration dialect refinements
+ * {@link org.springframework.boot.autoconfigure.thymeleaf.ThymeleafAutoConfiguration}
+ */
+@Configuration
+@ConditionalOnClass(TemplateMode.class)
+@AutoConfigureBefore({ThymeleafAutoConfiguration.class})
+public class IfsThymeleafConfiguration {
+
+    @Configuration
+    @ConditionalOnClass(IfSThymeleafDialect.class)
+    protected static class IfSThymeleafDialectDialectConfiguration {
+
+        @Bean
+        @ConditionalOnMissingBean
+        public IDialect ifSThymeleafDialect() {
+            return new IfSThymeleafDialect();
+        }
+    }
+
+    @Configuration
+    @ConditionalOnClass(IfsThymeleafPostProcessorDialect.class)
+    @Profile("debug")
+    protected static class IfsThymeleafPostProcessorDialectConfiguration {
+
+        @Bean
+        @ConditionalOnMissingBean
+        public IDialect ifsThymeleafPostProcessorDialect() {
+            return new IfsThymeleafPostProcessorDialect();
+        }
+    }
+
+}
