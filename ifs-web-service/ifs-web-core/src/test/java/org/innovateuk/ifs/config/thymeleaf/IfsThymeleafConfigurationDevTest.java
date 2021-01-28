152796868de18b8211893a0f219c110c487460e2
mark.dewey <mark.dewey@ukri.org>
2021-01-28 12:17:41 +0000

    [IFS-9141] config seems ok

--- ifs-web-service/ifs-web-core/src/test/java/org/innovateuk/ifs/config/thymeleaf/IfsThymeleafConfigurationDevTest.java
+++ ifs-web-service/ifs-web-core/src/test/java/org/innovateuk/ifs/config/thymeleaf/IfsThymeleafConfigurationDevTest.java
@@ -7,13 +7,14 @@ import org.innovateuk.ifs.config.thymeleaf.postprocessor.IfsThymeleafPostProcess
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.thymeleaf.dialect.IDialect;
 import org.thymeleaf.extras.java8time.dialect.Java8TimeDialect;
 import org.thymeleaf.extras.springsecurity5.dialect.SpringSecurityDialect;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.dialect.SpringStandardDialect;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -21,23 +22,27 @@ import java.util.stream.Collectors;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
-@SpringBootTest
-@EnableAutoConfiguration
 @RunWith(SpringRunner.class)
+@SpringBootTest
 @ActiveProfiles({"debug"})
 public class IfsThymeleafConfigurationDevTest {
 
     @Autowired
-    private List<IDialect> dialects;
+    private SpringTemplateEngine springTemplateEngine;
 
     @Test
     public void testAvailableDialects() {
-        assertThat(dialects.size(), equalTo(4));
+        assertThat(springTemplateEngine.getDialects().size(), equalTo(5));
 
         // Cross reference expected classes and actual classes
         assertThat(Sets.symmetricDifference(
-                dialects.stream().map(d->d.getClass()).collect(Collectors.toSet()),
-                ImmutableSet.of(SpringSecurityDialect.class, IfSThymeleafDialect.class, IfsThymeleafPostProcessorDialect.class, Java8TimeDialect.class)
+                springTemplateEngine.getDialects().stream().map(d->d.getClass()).collect(Collectors.toSet()),
+                ImmutableSet.of(
+                        SpringStandardDialect.class,
+                        SpringSecurityDialect.class,
+                        IfSThymeleafDialect.class,
+                        IfsThymeleafPostProcessorDialect.class,
+                        Java8TimeDialect.class)
         ).size(), equalTo(0));
     }
 
--- ifs-web-service/ifs-web-core/src/test/java/org/innovateuk/ifs/config/thymeleaf/IfsThymeleafConfigurationTest.java
+++ ifs-web-service/ifs-web-core/src/test/java/org/innovateuk/ifs/config/thymeleaf/IfsThymeleafConfigurationTest.java
@@ -6,43 +6,64 @@ import org.innovateuk.ifs.config.thymeleaf.dialect.IfSThymeleafDialect;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.thymeleaf.ThymeleafProperties;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.thymeleaf.dialect.IDialect;
 import org.thymeleaf.extras.java8time.dialect.Java8TimeDialect;
 import org.thymeleaf.extras.springsecurity5.dialect.SpringSecurityDialect;
 import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.dialect.SpringStandardDialect;
 import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring5.view.ThymeleafViewResolver;
+import org.thymeleaf.templatemode.TemplateMode;
 
-import java.util.List;
+import java.nio.charset.StandardCharsets;
 import java.util.stream.Collectors;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
-@SpringBootTest
 @RunWith(SpringRunner.class)
-@EnableAutoConfiguration
+@SpringBootTest
+@ActiveProfiles({"notdebug"})
 public class IfsThymeleafConfigurationTest {
 
     @Autowired
-    private List<IDialect> dialects;
+    private ThymeleafProperties thymeleafProperties;
 
     @Autowired
     private SpringResourceTemplateResolver springResourceTemplateResolver;
 
     @Autowired
+    private ThymeleafViewResolver thymeleafViewResolver;
+
+    @Autowired
     private SpringTemplateEngine springTemplateEngine;
 
     @Test
+    public void testContext() {
+        assertThat(thymeleafViewResolver.getCharacterEncoding(), equalTo(StandardCharsets.UTF_8.name()));
+        assertThat(springTemplateEngine.getEnableSpringELCompiler(), equalTo(true));
+
+        assertThat(springResourceTemplateResolver.getPrefix(), equalTo(ThymeleafProperties.DEFAULT_PREFIX));
+        assertThat(springResourceTemplateResolver.getSuffix(), equalTo(ThymeleafProperties.DEFAULT_SUFFIX));
+        assertThat(springResourceTemplateResolver.isCacheable(), equalTo(true));
+        assertThat(springResourceTemplateResolver.getTemplateMode(), equalTo(TemplateMode.HTML));
+    }
+
+    @Test
     public void testAvailableDialects() {
-        assertThat(dialects.size(), equalTo(3));
+        assertThat(springTemplateEngine.getDialects().size(), equalTo(4));
 
         // Cross reference expected classes and actual classes
         assertThat(Sets.symmetricDifference(
-                dialects.stream().map(d->d.getClass()).collect(Collectors.toSet()),
-                ImmutableSet.of(SpringSecurityDialect.class, IfSThymeleafDialect.class, Java8TimeDialect.class)
+                springTemplateEngine.getDialects().stream().map(d->d.getClass()).collect(Collectors.toSet()),
+                ImmutableSet.of(
+                        SpringStandardDialect.class,
+                        SpringSecurityDialect.class,
+                        IfSThymeleafDialect.class,
+                        Java8TimeDialect.class)
         ).size(), equalTo(0));
     }
 
