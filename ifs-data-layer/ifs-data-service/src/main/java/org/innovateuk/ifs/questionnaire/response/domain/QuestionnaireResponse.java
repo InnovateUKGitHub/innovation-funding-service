8e70eb03858720c7bc8e5f64b2e26ffadf29bd8f
Luke Harper <lharper@worth.systems>
2021-02-05 11:16:53 +0000

    start.

---
+++ ifs-data-layer/ifs-data-service/src/main/java/org/innovateuk/ifs/questionnaire/response/domain/QuestionnaireResponse.java
@@ -0,0 +1,10 @@
+package org.innovateuk.ifs.questionnaire.response.domain;
+
+import org.innovateuk.ifs.questionnaire.config.domain.Questionnaire;
+
+public class QuestionnaireResponse {
+
+    private Questionnaire questionnaire;
+
+
+}
