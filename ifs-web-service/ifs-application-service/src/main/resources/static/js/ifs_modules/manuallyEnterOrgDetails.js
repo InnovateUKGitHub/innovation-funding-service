fb001b06bde835748da2a6323461dce9c372bd1d
Sam Grayston <sgrayston@zaizi.com>
2021-02-22 11:04:32 +0000

    IFS-7724: layout padding tweaks

--- ifs-web-service/ifs-application-service/src/main/resources/static/js/ifs_modules/manuallyEnterOrgDetails.js
+++ ifs-web-service/ifs-application-service/src/main/resources/static/js/ifs_modules/manuallyEnterOrgDetails.js
@@ -38,10 +38,10 @@ IFS.manuallyEnter = (function () {
           return
         }
       }
-      var html = '<div class="govuk-grid-row  sic-code-row govuk-!-margin-top-6" id="sic-code-row-' + idCount + '">' +
+      var html = '<div class="govuk-grid-row sic-code-row" id="sic-code-row-' + idCount + '">' +
                                      '<div class="govuk-grid-column">' +
                                          '<div class="govuk-grid-column-two-thirds">' +
-                                         '<input class="govuk-input govuk-input--width-5 govuk-!-margin-bottom-3"' +
+                                         '<input class="govuk-input govuk-input--width-5 govuk-!-margin-bottom-2"' +
                                          'id="sicCode"' +
                                           'type="text" ' +
                                           'maxlength="5" ' +
@@ -54,7 +54,7 @@ IFS.manuallyEnter = (function () {
                                                          'th:id="remove-sic-code-row"' + idCount + ' ">Remove' +
                                               '</button>' +
                                             '</div>' +
-                                           '<hr class="govuk-section-break govuk-section-break--l govuk-section-break--visible govuk-!-margin-top-3 govuk-!-margin-bottom-3  govuk-!-margin-left-3">' +
+                                           '<hr class="govuk-section-break govuk-section-break--l govuk-section-break--visible govuk-!-margin-top-2 govuk-!-margin-bottom-2  govuk-!-margin-left-3">' +
                                       '</div>'
       jQuery('.sic-code-row').last().after(html)
       jQuery('.sic-code-' + idCount).val('')
@@ -81,10 +81,10 @@ IFS.manuallyEnter = (function () {
       if (jQuery('.exec-officer-row').length) {
         idCount = parseInt(jQuery('.exec-officer-row[id^=exec-officer-row-]').last().attr('id').split('exec-officer-row-')[1], 10) + 1
       }
-      var html = '<div class="govuk-grid-row exec-officer-row govuk-!-margin-top-6" id="exec-officer-row-' + idCount + '">' +
+      var html = '<div class="govuk-grid-row exec-officer-row" id="exec-officer-row-' + idCount + '">' +
                                '<div class="govuk-grid-column">' +
                                    '<div class="govuk-grid-column-two-thirds">' +
-                                   '<input class="govuk-input govuk-input--width-23 govuk-!-margin-bottom-3"' +
+                                   '<input class="govuk-input govuk-input--width-23 govuk-!-margin-bottom-2"' +
                                    'id="execOfficer"' +
                                     'type="text" ' +
                                     'maxlength="255" ' +
@@ -97,7 +97,7 @@ IFS.manuallyEnter = (function () {
                                                    'th:id="remove-exec-officer-row"' + idCount + ' ">Remove' +
                                         '</button>' +
                                       '</div>' +
-                                      '<hr class="govuk-section-break govuk-section-break--l govuk-section-break--visible govuk-!-margin-top-3 govuk-!-margin-bottom-3  govuk-!-margin-left-3">' +
+                                      '<hr class="govuk-section-break govuk-section-break--l govuk-section-break--visible govuk-!-margin-top-2 govuk-!-margin-bottom-2  govuk-!-margin-left-3">' +
                                 '</div>'
       jQuery('.exec-officer-row').last().after(html)
       jQuery('.exec-officer-' + idCount).val('')
