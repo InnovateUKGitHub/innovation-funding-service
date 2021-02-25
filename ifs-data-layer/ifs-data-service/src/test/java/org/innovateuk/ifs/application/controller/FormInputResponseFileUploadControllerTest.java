695506ca0b94e58de738539f6f9ff1187816350b
mark.dewey <mark.dewey@ukri.org>
2021-02-25 17:36:32 +0000

    [IFS-9108] test fix

--- ifs-data-layer/ifs-data-service/src/test/java/org/innovateuk/ifs/application/controller/FormInputResponseFileUploadControllerTest.java
+++ ifs-data-layer/ifs-data-service/src/test/java/org/innovateuk/ifs/application/controller/FormInputResponseFileUploadControllerTest.java
@@ -15,7 +15,7 @@ import org.innovateuk.ifs.file.service.FilesizeAndTypeFileValidator;
 import org.innovateuk.ifs.file.transactional.FileHeaderAttributes;
 import org.innovateuk.ifs.form.domain.FormInput;
 import org.innovateuk.ifs.user.domain.ProcessRole;
-import org.junit.Ignore;
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
 import org.springframework.http.HttpStatus;
@@ -70,6 +70,13 @@ public class FormInputResponseFileUploadControllerTest extends BaseControllerMoc
 
     @Mock(name = "fileValidator")
     private FilesizeAndTypeFileValidator<Long> fileValidatorMock;
+
+    private InputStream stream = null;
+
+    @Before
+    public void resetStreamCache() {
+        stream = null;
+    }
 
     @Override
     protected FormInputResponseFileUploadController supplyControllerUnderTest() {
@@ -130,7 +137,7 @@ public class FormInputResponseFileUploadControllerTest extends BaseControllerMoc
         assertEquals(1111L, createdResponse.getFileEntryId());
 
         verify(fileValidatorMock).validateFileHeaders("application/pdf", "1000", "original.pdf", formInputId, 1234L);
-        //verify(applicationFormInputUploadService).uploadResponse(createFileEntryResourceExpectations("original.pdf"), createInputStreamExpectations(dummyContent));
+        verify(applicationFormInputUploadService).uploadResponse(createFileEntryResourceExpectations("original.pdf"), createInputStreamExpectations(dummyContent));
     }
 
     private FormInputResponseFileEntryResource createFileEntryResourceExpectations(String expectedFilename) {
@@ -148,7 +155,10 @@ public class FormInputResponseFileUploadControllerTest extends BaseControllerMoc
 
     private Supplier<InputStream> createInputStreamExpectations(String dummyContent) {
         return createLambdaMatcher(is -> {
-            assertInputStreamContents(is.get(), dummyContent);
+            if (stream == null) {
+                stream = is.get();
+            }
+            assertInputStreamContents(stream, dummyContent);
         });
     }
 
@@ -219,10 +229,10 @@ public class FormInputResponseFileUploadControllerTest extends BaseControllerMoc
     }
 
     @Test
-    @Ignore("If you can figure this out, you welcome to re-enable it")
     public void testCreateFileButContentLengthHeaderMissing() throws Exception {
 
-        when(fileValidatorMock.validateFileHeaders("application/pdf", null, "original.pdf", formInputId, maxFilesize)).thenReturn(serviceFailure(lengthRequiredError(5000L)));
+        // Spring updates causes the contentLengthValue to be set even if not specified on post.
+        when(fileValidatorMock.validateFileHeaders("application/pdf", "14", "original.pdf", formInputId, maxFilesize)).thenReturn(serviceFailure(lengthRequiredError(5000L)));
 
         MvcResult response = mockMvc.
                 perform(
