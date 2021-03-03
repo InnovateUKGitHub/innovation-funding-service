c421a44d4a1a073b339cab3837db312e4868d25d
Jonathan Lee <jlee@worth.systems>
2021-03-03 10:03:20 +0000

    IFS-9127 url for funding rules activities

--- ifs-web-service/ifs-project-setup-mgt-service/src/main/java/org/innovateuk/ifs/project/activitylog/populator/ActivityLogUrlHelper.java
+++ ifs-web-service/ifs-project-setup-mgt-service/src/main/java/org/innovateuk/ifs/project/activitylog/populator/ActivityLogUrlHelper.java
@@ -69,6 +69,10 @@ public class ActivityLogUrlHelper {
                 return format("/project-setup-management/project/%d/finance-check", project.getId());
             case PAYMENT_MILESTONES_RESET:
                 return format("/project-setup-management/project/%d/finance-check", project.getId());
+            case FUNDING_RULES_UPDATED:
+                return format("/project-setup-management/project/%d/finance-check", project.getId());
+            case FUNDING_RULES_APPROVED:
+                return format("/project-setup-management/project/%d/finance-check", project.getId());
             default:
                 return null;
         }
