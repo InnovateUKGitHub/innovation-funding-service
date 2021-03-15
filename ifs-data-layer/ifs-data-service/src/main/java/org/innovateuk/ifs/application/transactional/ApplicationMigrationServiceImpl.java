6662ba7a23d39f79b7629b696c71472bdf5a7a9e
Prem Jonathan <pjonathan@zaizi.com>
2021-03-15 08:43:08 +0000

    added logging

--- ifs-data-layer/ifs-data-service/src/main/java/org/innovateuk/ifs/application/transactional/ApplicationMigrationServiceImpl.java
+++ ifs-data-layer/ifs-data-service/src/main/java/org/innovateuk/ifs/application/transactional/ApplicationMigrationServiceImpl.java
@@ -22,6 +22,8 @@ import org.innovateuk.ifs.project.core.repository.ProjectToBeCreatedRepository;
 import org.innovateuk.ifs.review.repository.ReviewRepository;
 import org.innovateuk.ifs.supporter.repository.SupporterAssignmentRepository;
 import org.innovateuk.ifs.user.repository.ProcessRoleRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -35,6 +37,8 @@ import static org.innovateuk.ifs.commons.service.ServiceResult.serviceSuccess;
 
 @Service
 public class ApplicationMigrationServiceImpl implements ApplicationMigrationService {
+
+    private static final Logger LOG = LoggerFactory.getLogger(ApplicationMigrationServiceImpl.class);
 
     @Autowired
     private ApplicationMigrationRepository applicationMigrationRepository;
@@ -114,6 +118,8 @@ public class ApplicationMigrationServiceImpl implements ApplicationMigrationServ
                 .andOnSuccess(application -> {
                     Application migratedApplication = applicationRepository.save(new Application(application));
 
+                    LOG.debug("Migrated application : " + application.getId());
+
                     activityLogRepository.findByApplicationId(application.getId()).stream()
                             .forEach(activityLog -> {
                                 ActivityLog migratedActivityLog = new ActivityLog(migratedApplication, activityLog.getType(),
@@ -124,11 +130,15 @@ public class ApplicationMigrationServiceImpl implements ApplicationMigrationServ
                                 activityLogRepository.save(migratedActivityLog);
                             });
 
+                    LOG.debug("Migrated activity log for application : " + application.getId());
+
                     applicationFinanceRepository.findByApplicationId(application.getId()).stream()
                             .forEach(applicationFinance -> {
                                 applicationFinance.setApplication(migratedApplication);
                                 applicationFinanceRepository.save(applicationFinance);
                             });
+
+                    LOG.debug("Migrated application finance for application : " + application.getId());
 
                     applicationHiddenFromDashboardRepository.findByApplicationId(application.getId()).stream()
                             .forEach(applicationHiddenFromDashboard -> {
@@ -136,17 +146,23 @@ public class ApplicationMigrationServiceImpl implements ApplicationMigrationServ
                                 applicationHiddenFromDashboardRepository.save(applicationHiddenFromDashboard);
                             });
 
+                    LOG.debug("Migrated application hidden from dashboard for application : " + application.getId());
+
                     applicationOrganisationAddressRepository.findByApplicationId(application.getId()).stream()
                             .forEach(applicationOrganisationAddress -> {
                                 applicationOrganisationAddress.setApplication(migratedApplication);
                                 applicationOrganisationAddressRepository.save(applicationOrganisationAddress);
                             });
 
+                    LOG.debug("Migrated application organisation address for application : " + application.getId());
+
                     averageAssessorScoreRepository.findByApplicationId(application.getId()).ifPresent(
                             averageAssessorScore -> {
                                 averageAssessorScore.setApplication(migratedApplication);
                                 averageAssessorScoreRepository.save(averageAssessorScore);
                             });
+
+                    LOG.debug("Migrated average assessor score for application : " + application.getId());
 
                     serviceSuccess(euGrantTransferRepository.findByApplicationId(application.getId()))
                             .andOnSuccessReturnVoid(euGrantTransfer -> {
@@ -156,17 +172,23 @@ public class ApplicationMigrationServiceImpl implements ApplicationMigrationServ
                                 }
                             });
 
+                    LOG.debug("Migrated eu grant transfer for application : " + application.getId());
+
                     formInputResponseRepository.findByApplicationId(application.getId()).stream()
                             .forEach(formInputResponse -> {
                                 formInputResponse.setApplication(migratedApplication);
                                 formInputResponseRepository.save(formInputResponse);
                             });
 
+                    LOG.debug("Migrated form input response for application : " + application.getId());
+
                     processRoleRepository.findByApplicationId(application.getId()).stream()
                             .forEach(processRole -> {
                                 processRole.setApplicationId(migratedApplication.getId());
                                 processRoleRepository.save(processRole);
                             });
+
+                    LOG.debug("Migrated process role for application : " + application.getId());
 
                     projectRepository.findByApplicationId(application.getId()).ifPresent(
                             project -> {
@@ -175,6 +197,8 @@ public class ApplicationMigrationServiceImpl implements ApplicationMigrationServ
                             }
                     );
 
+                    LOG.debug("Migrated project for application : " + application.getId());
+
                     projectToBeCreatedRepository.findByApplicationId(application.getId()).ifPresent(
                             projectToBeCreated -> {
                                 projectToBeCreated.setApplication(migratedApplication);
@@ -182,11 +206,15 @@ public class ApplicationMigrationServiceImpl implements ApplicationMigrationServ
                             }
                     );
 
+                    LOG.debug("Migrated project to be created for application : " + application.getId());
+
                     questionStatusRepository.findByApplicationId(application.getId()).stream()
                             .forEach(questionStatus -> {
                                 questionStatus.setApplication(migratedApplication);
                                 questionStatusRepository.save(questionStatus);
                             });
+
+                    LOG.debug("Migrated question status for application : " + application.getId());
 
                     serviceSuccess(grantProcessRepository.findOneByApplicationId(application.getId()))
                             .andOnSuccessReturnVoid(grantProcess -> {
@@ -200,11 +228,15 @@ public class ApplicationMigrationServiceImpl implements ApplicationMigrationServ
                                 }
                             });
 
+                    LOG.debug("Migrated grant process for application : " + application.getId());
+
                     applicationProcessRepository.findByTargetId(application.getId()).stream()
                             .forEach(applicationProcess -> {
                                 applicationProcess.setTarget(migratedApplication);
                                 applicationProcessRepository.save(applicationProcess);
                             });
+
+                    LOG.debug("Migrated application process for application : " + application.getId());
 
                     assessmentRepository.findByTargetId(application.getId()).stream()
                             .forEach(assessmentProcess -> {
@@ -212,11 +244,15 @@ public class ApplicationMigrationServiceImpl implements ApplicationMigrationServ
                                 assessmentRepository.save(assessmentProcess);
                             });
 
+                    LOG.debug("Migrated assessment process for application : " + application.getId());
+
                     interviewRepository.findByTargetId(application.getId()).stream()
                             .forEach(interviewProcess -> {
                                 interviewProcess.setTarget(migratedApplication);
                                 interviewRepository.save(interviewProcess);
                             });
+
+                    LOG.debug("Migrated interview process for application : " + application.getId());
 
                     interviewAssignmentRepository.findByTargetId(application.getId()).stream()
                             .forEach(interviewAssignmentProcess -> {
@@ -224,11 +260,15 @@ public class ApplicationMigrationServiceImpl implements ApplicationMigrationServ
                                 interviewAssignmentRepository.save(interviewAssignmentProcess);
                             });
 
+                    LOG.debug("Migrated interview assignment process for application : " + application.getId());
+
                     reviewRepository.findByTargetId(application.getId()).stream()
                             .forEach(reviewProcess -> {
                                 reviewProcess.setTarget(migratedApplication);
                                 reviewRepository.save(reviewProcess);
                             });
+
+                    LOG.debug("Migrated review process for application : " + application.getId());
 
                     supporterAssignmentRepository.findByTargetId(application.getId()).stream()
                             .forEach(supporterAssignmentProcess -> {
@@ -236,11 +276,15 @@ public class ApplicationMigrationServiceImpl implements ApplicationMigrationServ
                                 supporterAssignmentRepository.save(supporterAssignmentProcess);
                             });
 
+                    LOG.debug("Migrated supporter assignment process for application : " + application.getId());
+
                     applicationInviteRepository.findByApplicationId(application.getId()).stream()
                             .forEach(applicationInvite -> {
                                 applicationInvite.setTarget(migratedApplication);
                                 applicationInviteRepository.save(applicationInvite);
                             });
+
+                    LOG.debug("Migrated application invite for application : " + application.getId());
 
                     applicationKtaInviteRepository.findByApplicationId(application.getId()).ifPresent(
                             applicationKtaInvite -> {
@@ -248,6 +292,8 @@ public class ApplicationMigrationServiceImpl implements ApplicationMigrationServ
                                 applicationKtaInviteRepository.save(applicationKtaInvite);
                             }
                     );
+
+                    LOG.debug("Migrated application kta invite for application : " + application.getId());
 
                     deleteApplication(application);
 
@@ -259,6 +305,7 @@ public class ApplicationMigrationServiceImpl implements ApplicationMigrationServ
         activityLogRepository.deleteByApplicationId(application.getId());
         grantProcessRepository.deleteByApplicationId(application.getId());
         applicationRepository.delete(application);
+        LOG.debug("Deleted application : " + application.getId());
     }
 
     @Override
