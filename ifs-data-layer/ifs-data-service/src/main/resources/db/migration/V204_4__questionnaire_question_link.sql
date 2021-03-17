12ec6d57b9353aea84c8cfef97f0e740c29bf326
richard <rpopple@worth.systems>
2021-03-17 17:42:34 +0100

    IFS-9289 resolving merge conflicts

--- ifs-data-layer/ifs-data-service/src/main/resources/db/migration/V204_4__questionnaire_question_link.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE question ADD questionnaire_id BIGINT(20) NULL;
-ALTER TABLE question ADD CONSTRAINT question_questionnaire_fk FOREIGN KEY (questionnaire_id) REFERENCES questionnaire(id);
