565d8876a70ba94fcc74770031ea69b792b0f21c
richard <rpopple@worth.systems>
2021-03-18 17:29:12 +0100

    IFS-9467

--- ifs-data-layer/ifs-data-service/src/main/resources/db/migration/V206_1__Update_Subsidy_Control_Terms_And_Conditions.sql
+++ ifs-data-layer/ifs-data-service/src/main/resources/db/migration/V206_1__Update_Subsidy_Control_Terms_And_Conditions.sql
@@ -6,4 +6,4 @@ SET @system_maintenance_user_id = (
     WHERE email = 'ifs_system_maintenance_user@innovateuk.org');
 
 INSERT INTO terms_and_conditions (name, template, version, type, created_by, created_on, modified_on, modified_by)
-VALUES ('Aerospace Technology Institute (ATI) - Subsidy control', 'ati-subsidy-control-terms-and-conditions-v3', 2, 'GRANT', @system_maintenance_user_id, NOW(), NOW(), @system_maintenance_user_id);
+VALUES ('Aerospace Technology Institute (ATI) - Subsidy control', 'ati-subsidy-control-terms-and-conditions-v3', 3, 'GRANT', @system_maintenance_user_id, NOW(), NOW(), @system_maintenance_user_id);
