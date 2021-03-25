c13e67f66772eb103cfa76b735ad2190f3669575
Jonathan Lee <jlee@worth.systems>
2021-03-25 16:07:13 +0000

    IFS-9464 WIP

--- ifs-web-service/ifs-project-setup-mgt-service/src/main/java/org/innovateuk/ifs/project/grantofferletter/populator/ProcurementGrantOfferLetterTemplatePopulator.java
+++ ifs-web-service/ifs-project-setup-mgt-service/src/main/java/org/innovateuk/ifs/project/grantofferletter/populator/ProcurementGrantOfferLetterTemplatePopulator.java
@@ -1,7 +1,10 @@
 package org.innovateuk.ifs.project.grantofferletter.populator;
 
 import org.innovateuk.ifs.competition.resource.CompetitionResource;
+import org.innovateuk.ifs.procurement.milestone.resource.ProjectProcurementMilestoneResource;
+import org.innovateuk.ifs.procurement.milestone.service.ProjectProcurementMilestoneRestService;
 import org.innovateuk.ifs.project.grantofferletter.viewmodel.ProcurementGrantOfferLetterTemplateViewModel;
+import org.innovateuk.ifs.project.grantofferletter.viewmodel.ProcurementGrantOfferLetterTemplateViewModel.ProcurementGrantOfferLetterTemplateMilestoneEntryViewModel;
 import org.innovateuk.ifs.project.resource.PartnerOrganisationResource;
 import org.innovateuk.ifs.project.resource.ProjectResource;
 import org.innovateuk.ifs.project.service.PartnerOrganisationRestService;
@@ -9,7 +12,9 @@ import org.innovateuk.ifs.project.service.ProjectRestService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 public class ProcurementGrantOfferLetterTemplatePopulator {
@@ -20,9 +25,27 @@ public class ProcurementGrantOfferLetterTemplatePopulator {
     @Autowired
     private PartnerOrganisationRestService partnerOrganisationRestService;
 
+    @Autowired
+    private ProjectProcurementMilestoneRestService projectProcurementMilestoneRestService;
+
     public ProcurementGrantOfferLetterTemplateViewModel populate(ProjectResource project, CompetitionResource competition) {
         long applicationId = project.getApplication();
         List<PartnerOrganisationResource> organisations = partnerOrganisationRestService.getProjectPartnerOrganisations(project.getId()).getSuccess();
-        return new ProcurementGrantOfferLetterTemplateViewModel(applicationId, organisations.get(0).getOrganisationName());
+
+        List<ProcurementGrantOfferLetterTemplateMilestoneEntryViewModel> milestones = projectProcurementMilestoneRestService.getByProjectId(project.getId())
+                .getSuccess().stream()
+                .map(m -> entryForMilestone(project, m))
+                .collect(Collectors.toList());
+
+        return new ProcurementGrantOfferLetterTemplateViewModel(applicationId,
+                organisations.get(0).getOrganisationName(),
+                milestones);
+    }
+
+    private ProcurementGrantOfferLetterTemplateMilestoneEntryViewModel entryForMilestone(ProjectResource project, ProjectProcurementMilestoneResource milestone) {
+        String description = milestone.getTaskOrActivity() + " " + milestone.getDescription() + " " + milestone.getDeliverable();
+        String successCriteria = milestone.getSuccessCriteria();
+        LocalDate completionDate = project.getTargetStartDate().plusMonths(milestone.getMonth());
+        return new ProcurementGrantOfferLetterTemplateMilestoneEntryViewModel(description, successCriteria, completionDate);
     }
 }
--- ifs-web-service/ifs-project-setup-mgt-service/src/main/java/org/innovateuk/ifs/project/grantofferletter/viewmodel/ProcurementGrantOfferLetterTemplateViewModel.java
+++ ifs-web-service/ifs-project-setup-mgt-service/src/main/java/org/innovateuk/ifs/project/grantofferletter/viewmodel/ProcurementGrantOfferLetterTemplateViewModel.java
@@ -1,13 +1,19 @@
 package org.innovateuk.ifs.project.grantofferletter.viewmodel;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public class ProcurementGrantOfferLetterTemplateViewModel {
 
     private final long applicationId;
     private final String organisationName;
+    private final List<ProcurementGrantOfferLetterTemplateMilestoneEntryViewModel> milestones;
 
-    public ProcurementGrantOfferLetterTemplateViewModel(long applicationId, String organisationName) {
+    public ProcurementGrantOfferLetterTemplateViewModel(long applicationId, String organisationName,
+                                                        List<ProcurementGrantOfferLetterTemplateMilestoneEntryViewModel> milestones) {
         this.applicationId = applicationId;
         this.organisationName = organisationName;
+        this.milestones = milestones;
     }
 
     public long getApplicationId() {
@@ -16,5 +22,33 @@ public class ProcurementGrantOfferLetterTemplateViewModel {
 
     public String getOrganisationName() {
         return organisationName;
+    }
+
+    public List<ProcurementGrantOfferLetterTemplateMilestoneEntryViewModel> getMilestones() {
+        return milestones;
+    }
+
+    public static class ProcurementGrantOfferLetterTemplateMilestoneEntryViewModel {
+        private String description;
+        private String successCriteria;
+        private LocalDate completionDate;
+
+        public ProcurementGrantOfferLetterTemplateMilestoneEntryViewModel(String description, String successCriteria, LocalDate completionDate) {
+            this.description = description;
+            this.successCriteria = successCriteria;
+            this.completionDate = completionDate;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+
+        public String getSuccessCriteria() {
+            return successCriteria;
+        }
+
+        public LocalDate getCompletionDate() {
+            return completionDate;
+        }
     }
 }
