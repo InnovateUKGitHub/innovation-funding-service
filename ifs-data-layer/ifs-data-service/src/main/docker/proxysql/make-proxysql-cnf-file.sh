23f67517ff5438fdb833bb1470e54dce9a1840f9
abirajdar <abirajdar@zaizi.com>
2021-03-25 13:15:18 +0000

    IFS-9508 injecting vars

--- ifs-data-layer/ifs-data-service/src/main/docker/proxysql/make-proxysql-cnf-file.sh
+++ ifs-data-layer/ifs-data-service/src/main/docker/proxysql/make-proxysql-cnf-file.sh
@@ -93,7 +93,7 @@ function inject_query_rules_into_proxysql_cnf() {
 
 # a function to replace database configuration replacement tokens in proxysql.cnf with real values
 function inject_db_configuration_into_proxysql_cnf() {
-    sed -i "s#<<DB_USER>>#$DB_USER#g;s#<<DB_PASS>>#$DB_PASS#g;s#<<DB_NAME>>#$DB_NAME#g;s#<<DB_HOST>>#$DB_HOST#g;s#<<DB_PORT>>#$DB_PORT#g" /etc/proxysql.cnf
+    sed -i '' "s/<<DB_USER>>/$DB_USER/g;s/<<DB_HOST>>/$DB_HOST/g;s/<<DB_PASS>>/$DB_PASS/g;s/<<DB_PORT>>/$DB_PORT/g;s/<<DB_NAME>>/$DB_NAME/g" /etc/proxysql.cnf
 }
 
 # the entrypoint into this script
