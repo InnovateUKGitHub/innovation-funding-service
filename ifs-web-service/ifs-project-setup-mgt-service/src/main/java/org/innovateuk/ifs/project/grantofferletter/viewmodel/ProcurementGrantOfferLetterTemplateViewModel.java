b45015ff0767b49ceec7fd309ee2660c42b8a39a
Luke Harper <lharper@worth.systems>
2021-03-31 10:56:30 +0100

    IFS-9173 fixed test issues.

--- ifs-web-service/ifs-project-setup-mgt-service/src/main/java/org/innovateuk/ifs/project/grantofferletter/viewmodel/ProcurementGrantOfferLetterTemplateViewModel.java
+++ ifs-web-service/ifs-project-setup-mgt-service/src/main/java/org/innovateuk/ifs/project/grantofferletter/viewmodel/ProcurementGrantOfferLetterTemplateViewModel.java
@@ -4,10 +4,16 @@ public class ProcurementGrantOfferLetterTemplateViewModel {
 
     private final long applicationId;
     private final String organisationName;
+    private final String projectName;
+    private final String competitionName;
+    private final String projectManagerName;
 
-    public ProcurementGrantOfferLetterTemplateViewModel(long applicationId, String organisationName) {
+    public ProcurementGrantOfferLetterTemplateViewModel(long applicationId, String organisationName, String projectName, String competitionName, String projectManagerName) {
         this.applicationId = applicationId;
         this.organisationName = organisationName;
+        this.projectName = projectName;
+        this.competitionName = competitionName;
+        this.projectManagerName = projectManagerName;
     }
 
     public long getApplicationId() {
@@ -16,5 +22,17 @@ public class ProcurementGrantOfferLetterTemplateViewModel {
 
     public String getOrganisationName() {
         return organisationName;
+    }
+
+    public String getProjectName() {
+        return projectName;
+    }
+
+    public String getCompetitionName() {
+        return competitionName;
+    }
+
+    public String getProjectManagerName() {
+        return projectManagerName;
     }
 }
