83df3259aad6a51bc9be249879278ed61107e083
richard <rpopple@worth.systems>
2021-04-01 12:36:32 +0100

    IFS-9173 fixing unit test

--- ifs-web-service/ifs-project-setup-mgt-service/src/test/java/org/innovateuk/ifs/project/grantofferletter/populator/ProcurementGrantOfferLetterTemplatePopulatorTest.java
+++ ifs-web-service/ifs-project-setup-mgt-service/src/test/java/org/innovateuk/ifs/project/grantofferletter/populator/ProcurementGrantOfferLetterTemplatePopulatorTest.java
@@ -5,7 +5,9 @@ import org.innovateuk.ifs.competition.resource.CompetitionResource;
 import org.innovateuk.ifs.project.grantofferletter.viewmodel.ProcurementGrantOfferLetterTemplateViewModel;
 import org.innovateuk.ifs.project.resource.PartnerOrganisationResource;
 import org.innovateuk.ifs.project.resource.ProjectResource;
+import org.innovateuk.ifs.project.resource.ProjectUserResource;
 import org.innovateuk.ifs.project.service.PartnerOrganisationRestService;
+import org.innovateuk.ifs.project.service.ProjectRestService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -13,12 +15,11 @@ import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import static com.google.common.collect.Lists.newArrayList;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.is;
 import static org.innovateuk.ifs.competition.builder.CompetitionResourceBuilder.newCompetitionResource;
 import static org.innovateuk.ifs.project.builder.PartnerOrganisationResourceBuilder.newPartnerOrganisationResource;
 import static org.innovateuk.ifs.project.builder.ProjectResourceBuilder.newProjectResource;
-import static org.junit.Assert.assertThat;
+import static org.innovateuk.ifs.project.builder.ProjectUserResourceBuilder.newProjectUserResource;
+import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -30,6 +31,9 @@ public class ProcurementGrantOfferLetterTemplatePopulatorTest {
     @Mock
     private PartnerOrganisationRestService partnerOrganisationRestService;
 
+    @Mock
+    private ProjectRestService projectRestService;
+
     @Test
     public void populate() {
         ProjectResource project = newProjectResource()
@@ -39,11 +43,15 @@ public class ProcurementGrantOfferLetterTemplatePopulatorTest {
         PartnerOrganisationResource org = newPartnerOrganisationResource()
                 .withOrganisationName("Organisation")
                 .build();
-        when(partnerOrganisationRestService.getProjectPartnerOrganisations(project.getId())).thenReturn(RestResult.restSuccess(newArrayList(org)));
+        ProjectUserResource projectUser = newProjectUserResource().withUserName("projectUserUserName").build();
 
+
+        when(partnerOrganisationRestService.getProjectPartnerOrganisations(project.getId())).thenReturn(RestResult.restSuccess(newArrayList(org)));
+        when(projectRestService.getProjectManager(project.getId())).thenReturn(RestResult.restSuccess(projectUser));
         ProcurementGrantOfferLetterTemplateViewModel viewModel = populator.populate(project, competition);
 
-        assertThat(viewModel.getApplicationId(), is(equalTo(project.getApplication())));
-        assertThat(viewModel.getOrganisationName(), is(equalTo(org.getOrganisationName())));
+        assertEquals(project.getApplication(), viewModel.getApplicationId());
+        assertEquals(org.getOrganisationName(), viewModel.getOrganisationName());
+        assertEquals(projectUser.getUserName(), viewModel.getProjectManagerName());
     }
 }
