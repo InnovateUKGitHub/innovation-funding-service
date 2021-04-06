bc45bafb3ee0395efda47a82c583e9edd0f9a7aa
Sebastian Faull <sfaull@zaizi.com>
2021-04-06 15:13:08 +0100

    IFS-9246: Update webtest data.

--- ifs-data-layer/ifs-data-service/src/test/java/org/innovateuk/ifs/testdata/data/CompetitionWebTestData.java
+++ ifs-data-layer/ifs-data-service/src/test/java/org/innovateuk/ifs/testdata/data/CompetitionWebTestData.java
@@ -373,7 +373,9 @@ public class CompetitionWebTestData {
                         .withName("KTP dashboard competition"),
                 grantCompetition()
                         .withName("Subsidy control t and c competition")
-                        .withFundingRules(FundingRules.SUBSIDY_CONTROL)
+                        .withFundingRules(FundingRules.SUBSIDY_CONTROL),
+               ktpCompetition()
+                        .withName("KTP new competition duplicate")
         )
                 .stream()
                 .map(competitionLineBuilder -> competitionLineBuilder.withCompetitionStatus(CompetitionStatus.OPEN))
