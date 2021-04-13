9492e5e433a7495da0b597bdbdfa0a669beabc65
Prem Jonathan <pjonathan@zaizi.com>
2021-04-13 22:05:41 +0530

    added ktp fec filter

---
+++ ifs-data-layer/ifs-data-service/src/main/java/org/innovateuk/ifs/util/KtpFecFilter.java
@@ -0,0 +1,29 @@
+package org.innovateuk.ifs.util;
+
+import org.apache.commons.lang3.BooleanUtils;
+import org.innovateuk.ifs.finance.domain.Finance;
+import org.innovateuk.ifs.finance.domain.FinanceRow;
+import org.innovateuk.ifs.finance.resource.cost.FinanceRowType;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * A util class to provide common ktp fec model services
+ */
+@Component
+public class KtpFecFilter {
+
+    public List<? extends FinanceRow> filterKtpFecCostCategoriesIfRequired(Finance finance, List<? extends FinanceRow> financeRows) {
+        if (finance.getApplication().getCompetition().isKtp()) {
+            financeRows = financeRows.stream()
+                    .filter(financeRow -> BooleanUtils.isFalse(finance.getFecModelEnabled())
+                            ? !FinanceRowType.getFecSpecificFinanceRowTypes().contains(financeRow.getType())
+                            : !FinanceRowType.getNonFecSpecificFinanceRowTypes().contains(financeRow.getType()))
+                    .collect(Collectors.toList());
+        }
+
+        return financeRows;
+    }
+}
