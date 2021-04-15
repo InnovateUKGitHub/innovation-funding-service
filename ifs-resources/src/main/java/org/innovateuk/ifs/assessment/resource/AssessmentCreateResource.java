0d58240bfeebcee78e8f40b8b7fbfee2c38f353f
Jonathan Lee <jlee@worth.systems>
2021-04-15 16:28:03 +0100

    IFS-8852 tidyup after merge

--- ifs-resources/src/main/java/org/innovateuk/ifs/assessment/resource/AssessmentCreateResource.java
+++ ifs-resources/src/main/java/org/innovateuk/ifs/assessment/resource/AssessmentCreateResource.java
@@ -24,12 +24,6 @@ public class AssessmentCreateResource {
         this.assessorId = assessorId;
     }
 
-    public AssessmentCreateResource(Long applicationId, Long assessorId, Long assessmentPeriod) {
-        this.applicationId = applicationId;
-        this.assessorId = assessorId;
-        this.assessmentPeriod = assessmentPeriod;
-    }
-
     public Long getApplicationId() {
         return applicationId;
     }
