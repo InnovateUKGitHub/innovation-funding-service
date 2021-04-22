25c5d1c2bf82824a9502ace780dd1c20be5ae15b
Jonathan Lee <jlee@worth.systems>
2021-04-22 11:19:25 +0100

    IFS-8847 removing duplicated method unused with other name

--- ifs-resources/src/main/java/org/innovateuk/ifs/competition/resource/CompetitionCompletionStage.java
+++ ifs-resources/src/main/java/org/innovateuk/ifs/competition/resource/CompetitionCompletionStage.java
@@ -35,10 +35,4 @@ public enum CompetitionCompletionStage {
                 .collect(Collectors.toList());
     }
 
-    public static List<CompetitionCompletionStage> assessmentValues() {
-        return Stream.of(values())
-                .filter(competitionCompletionStage -> (competitionCompletionStage == CompetitionCompletionStage.PROJECT_SETUP
-                        || competitionCompletionStage == CompetitionCompletionStage.RELEASE_FEEDBACK))
-                .collect(Collectors.toList());
-    }
 }
