ea7bcb64182be794ce9a92f3981fb20d8301b006
Punniya Rajkumar <punniya.rajkumar@innovateuk.ukri.org>
2021-05-06 17:44:14 +0100

    IFS-9619: Sample competitions.csv and domain model update

---
+++ ifs-data-layer/ifs-data-service/src/main/resources/db/migration/V219_1__Add_upload_files_for_assessor_service.sql
@@ -0,0 +1,9 @@
+-- IFS-9619 - Assessment as a service PoC
+-- Add a new table for upload files.
+
+CREATE TABLE `upload_files` (
+	`id` bigint(20) NOT NULL AUTO_INCREMENT,
+	`type` varchar(255) NOT NULL,
+	`file_entry_id` bigint(20) DEFAULT NULL,
+ 	 PRIMARY KEY (`id`)
+);
---
+++ ifs-data-layer/ifs-data-service/src/main/resources/db/migration/V220_1__Add_constraint_for_upload_files.sql
@@ -0,0 +1,4 @@
+-- IFS-9619 - Assessment as a service PoC
+-- Adding constraint for uploading the files in the file entry table
+
+ALTER TABLE upload_files ADD CONSTRAINT `file_entry_id_fk` FOREIGN KEY (`file_entry_id`) REFERENCES `file_entry` (`id`);
