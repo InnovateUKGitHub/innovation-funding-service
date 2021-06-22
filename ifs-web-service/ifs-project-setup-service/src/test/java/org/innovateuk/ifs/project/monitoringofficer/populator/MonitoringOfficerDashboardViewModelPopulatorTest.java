6a380b3b4a46e246eb73101a63acfa59210655da
Prem Jonathan <pjonathan@zaizi.com>
2021-06-22 14:51:19 +0530

    added more tests

--- ifs-web-service/ifs-project-setup-service/src/test/java/org/innovateuk/ifs/project/monitoringofficer/populator/MonitoringOfficerDashboardViewModelPopulatorTest.java
+++ ifs-web-service/ifs-project-setup-service/src/test/java/org/innovateuk/ifs/project/monitoringofficer/populator/MonitoringOfficerDashboardViewModelPopulatorTest.java
@@ -10,8 +10,10 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 
+import static java.util.Arrays.asList;
 import static java.util.Collections.singletonList;
 import static org.innovateuk.ifs.commons.rest.RestResult.restSuccess;
 import static org.innovateuk.ifs.project.builder.ProjectResourceBuilder.newProjectResource;
@@ -43,22 +45,63 @@ public class MonitoringOfficerDashboardViewModelPopulatorTest {
                 .withProjectState(ProjectState.UNSUCCESSFUL)
                 .build();
 
-        MonitoringOfficerSummaryViewModel monitoringOfficerSummaryViewModel = new MonitoringOfficerSummaryViewModel(0, 1);
+        MonitoringOfficerSummaryViewModel monitoringOfficerSummaryViewModel = Mockito.mock(MonitoringOfficerSummaryViewModel.class);
 
         when(monitoringOfficerRestService.getProjectsForMonitoringOfficer(user.getId())).thenReturn(restSuccess(singletonList(projectResource)));
         when(monitoringOfficerSummaryViewModelPopulator.populate(anyList())).thenReturn(monitoringOfficerSummaryViewModel);
 
         MonitoringOfficerDashboardViewModel viewModel = populator.populate(user);
-        assertEquals(viewModel.getProjects().size(), 1);
-        assertEquals(viewModel.getProjects().get(0).getProjectId(), (long) projectResource.getId());
-        assertEquals(viewModel.getProjects().get(0).getApplicationNumber(), projectResource.getApplication());
-        assertEquals(viewModel.getProjects().get(0).getCompetitionTitle(), "Competition name");
-        assertEquals(viewModel.getProjects().get(0).getLinkUrl(), String.format("/project-setup/project/%d", projectResource.getId()));
-        assertEquals(viewModel.getProjects().get(0).getProjectTitle(), "Project name");
+        assertEquals(1, viewModel.getProjects().size());
+
+        assertEquals((long) projectResource.getId(), viewModel.getProjects().get(0).getProjectId());
+        assertEquals(projectResource.getApplication(), viewModel.getProjects().get(0).getApplicationNumber());
+        assertEquals("Competition name", viewModel.getProjects().get(0).getCompetitionTitle());
+        assertEquals(String.format("/project-setup/project/%d", projectResource.getId()), viewModel.getProjects().get(0).getLinkUrl());
+        assertEquals("Project name", viewModel.getProjects().get(0).getProjectTitle());
         assertTrue(viewModel.getProjects().get(0).isUnsuccessful());
         assertFalse(viewModel.getProjects().get(0).isLiveOrCompletedOffline());
         assertFalse(viewModel.getProjects().get(0).isWithdrawn());
-        assertEquals(0, viewModel.getMonitoringOfficerSummaryView().getInSetupProjectCount());
-        assertEquals(1, viewModel.getMonitoringOfficerSummaryView().getPreviousProjectCount());
+    }
+
+    @Test
+    public void populateApplyFilterAndSorting() {
+        UserResource user = newUserResource().build();
+        ProjectResource projectResourceInSetup = newProjectResource()
+                .withCompetition(1L)
+                .withCompetitionName("Competition name")
+                .withApplication(2L)
+                .withName("Project name")
+                .withProjectState(ProjectState.SETUP)
+                .build();
+        ProjectResource projectResourceInLive = newProjectResource()
+                .withCompetition(1L)
+                .withCompetitionName("Competition name")
+                .withApplication(2L)
+                .withName("Project name")
+                .withProjectState(ProjectState.LIVE)
+                .build();
+
+        MonitoringOfficerSummaryViewModel monitoringOfficerSummaryViewModel = new MonitoringOfficerSummaryViewModel(1, 1);
+
+        when(monitoringOfficerRestService.filterProjectsForMonitoringOfficer(user.getId(), true, true))
+                .thenReturn(restSuccess(asList(projectResourceInLive, projectResourceInSetup)));
+        when(monitoringOfficerSummaryViewModelPopulator.populate(user)).thenReturn(monitoringOfficerSummaryViewModel);
+
+        MonitoringOfficerDashboardViewModel viewModel = populator.populate(user, true, true);
+        assertEquals(2, viewModel.getProjects().size());
+
+        assertEquals((long) projectResourceInSetup.getId(), viewModel.getProjects().get(0).getProjectId());
+        assertEquals(projectResourceInSetup.getApplication(), viewModel.getProjects().get(0).getApplicationNumber());
+        assertEquals("Competition name", viewModel.getProjects().get(0).getCompetitionTitle());
+        assertEquals(String.format("/project-setup/project/%d", projectResourceInSetup.getId()), viewModel.getProjects().get(0).getLinkUrl());
+        assertEquals("Project name", viewModel.getProjects().get(0).getProjectTitle());
+        assertEquals(ProjectState.SETUP, viewModel.getProjects().get(0).getProjectState());
+
+        assertEquals((long) projectResourceInLive.getId(), viewModel.getProjects().get(1).getProjectId());
+        assertEquals(projectResourceInLive.getApplication(), viewModel.getProjects().get(1).getApplicationNumber());
+        assertEquals("Competition name", viewModel.getProjects().get(1).getCompetitionTitle());
+        assertEquals(String.format("/project-setup/project/%d", projectResourceInLive.getId()), viewModel.getProjects().get(1).getLinkUrl());
+        assertEquals("Project name", viewModel.getProjects().get(1).getProjectTitle());
+        assertEquals(ProjectState.LIVE, viewModel.getProjects().get(1).getProjectState());
     }
 }
