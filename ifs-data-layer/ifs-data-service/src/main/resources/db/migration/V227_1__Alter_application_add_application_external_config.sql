cbcdc229cf383e83f6a9253d016de5e82f779df9
Punniya Rajkumar <punniya.rajkumar@innovateuk.ukri.org>
2021-06-28 16:39:57 +0100

    IFS-9961: Web test data generation

--- ifs-data-layer/ifs-data-service/src/main/resources/db/migration/V227_1__Alter_application_add_application_external_config.sql
+++ ifs-data-layer/ifs-data-service/src/main/resources/db/migration/V227_1__Alter_application_add_application_external_config.sql
@@ -1,3 +1,3 @@
 ALTER TABLE application ADD COLUMN application_external_config_id BIGINT(20) DEFAULT NULL;
 ALTER TABLE application ADD UNIQUE KEY  application_external_config_id__UNIQUE (application_external_config_id);
-ALTER TABLE application ADD CONSTRAINT fk_applikcation_external_config FOREIGN KEY(application_external_config_id) REFERENCES application_external_config(id);
+ALTER TABLE application ADD CONSTRAINT fk_application_external_config FOREIGN KEY(application_external_config_id) REFERENCES application_external_config(id);
