17982a3178097c53723d574020f5f0ef54cd8922
Ashish Uniyal <ashish.uniyal@innovateuk.ukri.org>
2021-07-13 18:13:13 +0100

    IFS-9966 simplification and taken out junit5

--- ifs-sil-resources/src/test/java/org/innovateuk/ifs/sil/crm/resource/json/SilContactJsonTest.java
+++ ifs-sil-resources/src/test/java/org/innovateuk/ifs/sil/crm/resource/json/SilContactJsonTest.java
@@ -4,24 +4,48 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.innovateuk.ifs.sil.crm.resource.SilAddress;
 import org.innovateuk.ifs.sil.crm.resource.SilContact;
 import org.innovateuk.ifs.sil.crm.resource.SilOrganisation;
-import org.junit.jupiter.api.Test;
-
-
-import static org.hamcrest.Matchers.is;
-import static org.junit.jupiter.api.Assertions.assertAll;
+import org.junit.Test;
 
 import java.io.IOException;
-import java.time.LocalDate;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.equalTo;
 
 public class SilContactJsonTest {
-
-    private static final LocalDate DATE = LocalDate.of(2020, 12, 10);
+    private static final String crmJsonPayload = "{\"ifsUuid\":null," +
+            "\"experienceType\":null," +
+            "\"ifsAppID\":null," +
+            "\"email\":\"email\"," +
+            "\"lastName\":\"last name\"," +
+            "\"firstName\":\"first name\"," +
+            "\"title\":\"title\"," +
+            "\"jobTitle\":\"job title\"," +
+            "\"organisation\":{" +
+            "\"name\":\"name\"," +
+            "\"registrationNumber\":\"registration number\"," +
+            "\"registeredAddress\":{" +
+            "\"buildingName\":\"building name\"," +
+            "\"street\":\"street\"," +
+            "\"locality\":\"locality\"," +
+            "\"town\":\"town\"," +
+            "\"postcode\":\"postcode\"," +
+            "\"country\":\"country\"" +
+            "}," +
+            "\"srcSysOrgId\":\"sys org id\"}," +
+            "\"sourceSystem\":\"IFS\"," +
+            "\"srcSysContactId\":\"sys contact id\"," +
+            "\"Address\":{" +
+            "\"buildingName\":\"building name\"," +
+            "\"street\":\"street\"," +
+            "\"locality\":\"locality\"," +
+            "\"town\":\"town\"," +
+            "\"postcode\":\"postcode\"," +
+            "\"country\":\"country\"}" +
+            "}";
+    private static final String contactToString = "SilContact(ifsUuid=null, experienceType=null, ifsAppID=null, email=email, lastName=last name, firstName=first name, title=title, jobTitle=job title, address=SilAddress(buildingName=building name, street=street, locality=locality, town=town, postcode=postcode, country=country), organisation=SilOrganisation(name=name, registrationNumber=registration number, registeredAddress=SilAddress(buildingName=building name, street=street, locality=locality, town=town, postcode=postcode, country=country), srcSysOrgId=sys org id), sourceSystem=IFS, srcSysContactId=sys contact id)";
 
     @Test
-    public void shouldSerializeAndDeserialize() throws IOException {
+    public void shouldSerialize() throws IOException {
         SilAddress address = new SilAddress();
         address.setBuildingName("building name");
         address.setStreet("street");
@@ -45,45 +69,16 @@ public class SilContactJsonTest {
         silContact.setJobTitle("job title");
         silContact.setOrganisation(organisation);
         silContact.setSrcSysContactId("sys contact id");
-        silContact.setIfsUuid("0ba486fa-8deb-466f-9331-7cbcfb864dd8");
-        silContact.setIfsAppID("657");
-        silContact.setExperienceType("LOANS");
-
 
         String json = new ObjectMapper().writeValueAsString(silContact);
-        assertAll(
-                () -> assertThat(json, containsString("\"title\":\"title\"")),
-                () -> assertThat(json, containsString("\"firstName\":\"first name\"")),
-                () -> assertThat(json, containsString("\"lastName\":\"last name\"")),
-                () -> assertThat(json, containsString("\"ifsUuid\":\"0ba486fa-8deb-466f-9331-7cbcfb864dd8\"")),
-                () -> assertThat(json, containsString("\"ifsAppID\":\"657\"")),
-                () -> assertThat(json, containsString("\"experienceType\":\"LOANS\"")),
-                () -> assertThat(json, containsString("\"email\":\"email\"")),
-                () -> assertThat(json, containsString("\"Address\":" +
-                        "{\"buildingName\":\"building name\"" +
-                        ",\"street\":\"street\"," +
-                        "\"locality\":\"locality\"" +
-                        ",\"town\":\"town\"" +
-                        ",\"postcode\":\"postcode\"" +
-                        ",\"country\":\"country\"}")),
-                () -> assertThat(json, containsString("\"organisation\":" +
-                        "{\"name\":\"name\"" +
-                        ",\"registrationNumber\":\"registration number\"," +
-                        "\"registeredAddress\":" +
-                        "{\"buildingName\":\"building name\"" +
-                        ",\"street\":\"street\"" +
-                        ",\"locality\":\"locality\"" +
-                        ",\"town\":\"town\"" +
-                        ",\"postcode\":\"postcode\"" +
-                        ",\"country\":\"country\"}" +
-                        ",\"srcSysOrgId\":\"sys org id\"}")),
-                () -> assertThat(json, containsString("\"srcSysContactId\":\"sys contact id\"")),
-                () -> assertThat(json, containsString("\"sourceSystem\":\"IFS\""))
+        assertThat(json, equalTo(crmJsonPayload));
+    }
 
-        );
+    @Test
+    public void shouldDeserialize() throws IOException {
 
+        SilContact out = new ObjectMapper().readValue(crmJsonPayload, SilContact.class);
+        assertThat(out.toString(), equalTo(contactToString));
 
-        SilContact out = new ObjectMapper().readValue(json, SilContact.class);
-        assertThat(out.toString(), is("SilContact(ifsUuid=0ba486fa-8deb-466f-9331-7cbcfb864dd8, experienceType=LOANS, ifsAppID=657, email=email, lastName=last name, firstName=first name, title=title, jobTitle=job title, address=SilAddress(buildingName=building name, street=street, locality=locality, town=town, postcode=postcode, country=country), organisation=SilOrganisation(name=name, registrationNumber=registration number, registeredAddress=SilAddress(buildingName=building name, street=street, locality=locality, town=town, postcode=postcode, country=country), srcSysOrgId=sys org id), sourceSystem=IFS, srcSysContactId=sys contact id)"));
     }
 }
