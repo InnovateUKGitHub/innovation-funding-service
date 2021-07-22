2a36f394c6019728648fb1baa3e20ea1053caa3d
Prem Jonathan <pjonathan@zaizi.com>
2021-07-22 19:06:39 +0530

    added domain model change to store third party config

---
+++ ifs-data-layer/ifs-data-service/src/main/resources/db/migration/V232_1__Add_new_competition_third_party_procurement_config_table.sql
@@ -0,0 +1,13 @@
+-- IFS-10104: Add a new table for competition third party config
+CREATE TABLE competition_third_party_config (
+  id bigint(20) NOT NULL AUTO_INCREMENT,
+  terms_and_conditions_label varchar(255) NOT NULL,
+  project_cost_guidance_url varchar(255) NOT NULL,
+  terms_and_conditions_guidance longtext,
+  PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Update competition to have third party config mapping
+ALTER TABLE competition ADD COLUMN competition_third_party_config_id BIGINT(20) DEFAULT NULL;
+ALTER TABLE competition ADD UNIQUE KEY competition_third_party_config_id_UNIQUE (competition_third_party_config_id);
+ALTER TABLE competition ADD CONSTRAINT fk_competition_third_party_config FOREIGN KEY(competition_third_party_config_id) REFERENCES competition_third_party_config(id);
