9e2c08a0988fc8975f88d2ac854e5ffa55d7ea3c
Ashish Uniyal <ashish.uniyal@innovateuk.ukri.org>
2021-07-23 16:51:18 +0100

    IFS-10109 initial commit

--- ifs-data-layer/ifs-data-service/src/test/java/org/innovateuk/ifs/testdata/builders/BaseUserDataBuilder.java
+++ ifs-data-layer/ifs-data-service/src/test/java/org/innovateuk/ifs/testdata/builders/BaseUserDataBuilder.java
@@ -37,7 +37,15 @@ public abstract class BaseUserDataBuilder<T extends BaseUserData, S> extends Bas
                 Optional<Token> verifyToken = tokenRepository.findByTypeAndClassNameAndClassPk(TokenType.VERIFY_EMAIL_ADDRESS, User.class.getName(), user.getId());
 
                 verifyToken.map(token -> registrationService.activateUser(token.getClassPk()).andOnSuccessReturnVoid(v -> {
-                    tokenService.handleExtraAttributes(token);
+//                    tokenService.handleExtraAttributes(token)
+//                            .andOnFailure(
+//                            failure -> {
+//                                //no Application Resource created
+//                                System.out.println(""+failure);
+//
+//
+//                            }
+//                    );
                     tokenService.removeToken(token);
                 })).orElseThrow(() -> new RuntimeException("No Invite Token exists for user " + user.getId()));
             });
