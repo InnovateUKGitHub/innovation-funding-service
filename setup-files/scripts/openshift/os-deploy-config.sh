19d0a7c7d8e12b743293bbdaa19c41bbce8ad491
Ashish Uniyal <ashish.uniyal@innovateuk.ukri.org>
2021-07-29 12:34:07 +0100

    IFS-7686 more cleanup

--- setup-files/scripts/openshift/os-deploy-config.sh
+++ setup-files/scripts/openshift/os-deploy-config.sh
@@ -22,7 +22,6 @@ function applyConfigMaps {
     oc apply -f $(getBuildLocation)/config-maps/docusign-config.yml ${SVC_ACCOUNT_CLAUSE}
     oc apply -f $(getBuildLocation)/config-maps/finance-db-config.yml ${SVC_ACCOUNT_CLAUSE}
     oc apply -f $(getBuildLocation)/config-maps/flyway-config.yml ${SVC_ACCOUNT_CLAUSE}
-    oc apply -f $(getBuildLocation)/config-maps/ldap-config.yml ${SVC_ACCOUNT_CLAUSE}
     oc apply -f $(getBuildLocation)/config-maps/new-relic-config.yml ${SVC_ACCOUNT_CLAUSE}
     oc apply -f $(getBuildLocation)/config-maps/performance-config.yml ${SVC_ACCOUNT_CLAUSE}
     oc apply -f $(getBuildLocation)/config-maps/shibboleth-config.yml ${SVC_ACCOUNT_CLAUSE}
