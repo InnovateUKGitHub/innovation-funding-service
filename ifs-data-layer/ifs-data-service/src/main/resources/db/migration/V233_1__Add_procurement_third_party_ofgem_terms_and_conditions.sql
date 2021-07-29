ecfc6bd673b26d090201a923d61b043c284ba6c7
Punniya Rajkumar <punniya.rajkumar@innovateuk.ukri.org>
2021-07-29 22:15:33 +0100

    IFS-10081: template name updated

--- ifs-data-layer/ifs-data-service/src/main/resources/db/migration/V233_1__Add_procurement_third_party_ofgem_terms_and_conditions.sql
+++ ifs-data-layer/ifs-data-service/src/main/resources/db/migration/V233_1__Add_procurement_third_party_ofgem_terms_and_conditions.sql
@@ -7,5 +7,5 @@ WHERE email = 'ifs_system_maintenance_user@innovateuk.org');
 SET @new_terms_id = 55;
 
 INSERT INTO terms_and_conditions (id, name, template, version, type, created_by, created_on, modified_by, modified_on)
-VALUES (@new_terms_id, 'Procurement Third Party', 'procurement-third-party-terms-and-conditions', 1, 'GRANT',
+VALUES (@new_terms_id, 'Procurement Third Party', 'third-party-terms-and-conditions', 1, 'GRANT',
 @system_maintenance_user_id, now(), @system_maintenance_user_id, now());
