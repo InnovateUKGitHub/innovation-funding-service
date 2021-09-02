7a0c0b24c8f31d78edd954b8f4480d7d57c6e84c
abirajdar <abirajdar@zaizi.com>
2021-09-02 16:27:16 +0100

    IFS-10227 extract ifs-rest-api-service and ifs-survey-rest-api-service

--- ifs-rest-api-service/src/main/java/org/innovateuk/ifs/finance/service/ProjectFinanceRowRestService.java
+++
@@ -1,9 +0,0 @@
-package org.innovateuk.ifs.finance.service;
-
-import org.innovateuk.ifs.finance.resource.cost.FinanceRowItem;
-
-/**
- * Interface for CRUD operations on {@link FinanceRowItem} related data.
- */
-public interface ProjectFinanceRowRestService extends FinanceRowRestService {
-}
