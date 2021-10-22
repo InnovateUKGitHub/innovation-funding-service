1e883111d3965ee104a806d3e5ae59a61407f21e
Punniya Rajkumar <punniya.rajkumar@innovateuk.ukri.org>
2021-10-22 16:33:34 +0100

    IFS-10677: unit test fixed

--- ifs-web-service/ifs-project-setup-service/src/test/java/org/innovateuk/ifs/project/monitoringofficer/controller/MonitoringOfficerDashboardControllerTest.java
+++ ifs-web-service/ifs-project-setup-service/src/test/java/org/innovateuk/ifs/project/monitoringofficer/controller/MonitoringOfficerDashboardControllerTest.java
@@ -1,40 +1,55 @@
 package org.innovateuk.ifs.project.monitoringofficer.controller;
 
 import org.innovateuk.ifs.BaseControllerMockMVCTest;
+import org.innovateuk.ifs.project.monitoringofficer.form.MonitoringOfficerDashboardForm;
 import org.innovateuk.ifs.project.monitoringofficer.populator.MonitoringOfficerDashboardViewModelPopulator;
+import org.innovateuk.ifs.project.monitoringofficer.service.MonitoringOfficerDashBoardCookieService;
 import org.innovateuk.ifs.project.monitoringofficer.viewmodel.MonitoringOfficerDashboardViewModel;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@RunWith(MockitoJUnitRunner.class)
+@RunWith(MockitoJUnitRunner.Silent.class)
 public class MonitoringOfficerDashboardControllerTest extends BaseControllerMockMVCTest<MonitoringOfficerDashboardController> {
 
     @Mock
     private MonitoringOfficerDashboardViewModelPopulator populator;
+
+    @Mock
+    private MonitoringOfficerDashBoardCookieService cookieService;
+
+    private MonitoringOfficerDashboardForm monitoringOfficerDashboardForm;
 
     @Override
     protected MonitoringOfficerDashboardController supplyControllerUnderTest() {
         return new MonitoringOfficerDashboardController();
     }
 
+    @Before
+    public void setUpForm() {
+        monitoringOfficerDashboardForm = new MonitoringOfficerDashboardForm();
+    }
     @Test
     public void viewDashboard() throws Exception {
         MonitoringOfficerDashboardViewModel model = mock(MonitoringOfficerDashboardViewModel.class);
+
+        when(cookieService.getMODashboardFormCookieValue(any(), any(), any())).thenReturn(monitoringOfficerDashboardForm);
         when(populator.populate(loggedInUser, null, true, false, false,
                 false, false, false, false,
                 false,0, 10)).thenReturn(model);
-
         mockMvc.perform(get("/monitoring-officer/dashboard"))
+                .andExpect(status().is2xxSuccessful())
                 .andExpect(view().name("monitoring-officer/dashboard"))
-                .andExpect(model().attribute("model", model));
+                .andExpect(model().attributeExists("form"));
     }
 
     @Test
