16bfb4b07de6ff13c02aac4f1789b8c84eefd5e2
Grace Williams <grace.williams@innovateuk.gov.uk>
2021-11-01 21:19:03 +0000

    IFS-10677: Removed used of cookies

--- ifs-web-service/ifs-project-setup-service/src/main/java/org/innovateuk/ifs/project/monitoringofficer/controller/MonitoringOfficerDashboardController.java
+++ ifs-web-service/ifs-project-setup-service/src/main/java/org/innovateuk/ifs/project/monitoringofficer/controller/MonitoringOfficerDashboardController.java
@@ -4,7 +4,6 @@ import org.innovateuk.ifs.commons.security.SecuredBySpring;
 import org.innovateuk.ifs.controller.ValidationHandler;
 import org.innovateuk.ifs.project.monitoringofficer.form.MonitoringOfficerDashboardForm;
 import org.innovateuk.ifs.project.monitoringofficer.populator.MonitoringOfficerDashboardViewModelPopulator;
-import org.innovateuk.ifs.project.monitoringofficer.service.MonitoringOfficerDashBoardCookieService;
 import org.innovateuk.ifs.user.resource.UserResource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -13,10 +12,10 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 import java.util.function.Supplier;
+
+import static java.lang.String.format;
 
 @RequestMapping("/monitoring-officer/dashboard")
 @Controller
@@ -30,10 +29,9 @@ public class MonitoringOfficerDashboardController {
     private static final String PAGE_SIZE_KEY = "size";
     private static final String DEFAULT_PAGE_NUMBER = "0";
     private static final String DEFAULT_PAGE_SIZE = "10";
+
     @Autowired
     private MonitoringOfficerDashboardViewModelPopulator monitoringOfficerDashboardViewModelPopulator;
-    @Autowired
-    private MonitoringOfficerDashBoardCookieService monitoringOfficerDashBoardCookieService;
 
     public MonitoringOfficerDashboardController() {
 
@@ -44,24 +42,12 @@ public class MonitoringOfficerDashboardController {
                                 @ModelAttribute(name = FORM_ATTR_NAME, binding = false) MonitoringOfficerDashboardForm form,
                                 UserResource user,
                                 @RequestParam(value = PAGE_NUMBER_KEY, defaultValue = DEFAULT_PAGE_NUMBER) int pageNumber,
-                                @RequestParam(value = PAGE_SIZE_KEY, defaultValue = DEFAULT_PAGE_SIZE) int pageSize,
-                                HttpServletResponse response) {
-
-        monitoringOfficerDashBoardCookieService.deleteMODashBoardDataFromCookie(response);
+                                @RequestParam(value = PAGE_SIZE_KEY, defaultValue = DEFAULT_PAGE_SIZE) int pageSize) {
 
         form.setProjectInSetup(true);
 
         model.addAttribute(FORM_ATTR_NAME, form);
-        model.addAttribute("model", monitoringOfficerDashboardViewModelPopulator.populate(user
-                , form.getKeywordSearch()
-                , form.isProjectInSetup()
-                , form.isPreviousProject()
-                , form.isDocumentsComplete()
-                , form.isDocumentsIncomplete()
-                , form.isDocumentsAwaitingReview()
-                , form.isSpendProfileComplete()
-                , form.isSpendProfileIncomplete()
-                , form.isSpendProfileAwaitingReview(), pageNumber, pageSize));
+        model.addAttribute("model", monitoringOfficerDashboardViewModelPopulator.populate(user, form, pageNumber, pageSize));
 
         return "monitoring-officer/dashboard";
     }
@@ -73,66 +59,48 @@ public class MonitoringOfficerDashboardController {
                                   ValidationHandler validationHandler,
                                   UserResource user,
                                   @RequestParam(value = PAGE_NUMBER_KEY, defaultValue = DEFAULT_PAGE_NUMBER) int pageNumber,
-                                  @RequestParam(value = PAGE_SIZE_KEY, defaultValue = DEFAULT_PAGE_SIZE) int pageSize,
-                                  HttpServletResponse response) {
-        final Supplier<String> failureView = () -> viewDashboard(model, form, user, pageNumber, pageSize, response);
+                                  @RequestParam(value = PAGE_SIZE_KEY, defaultValue = DEFAULT_PAGE_SIZE) int pageSize) {
+        final Supplier<String> failureView = () -> viewDashboard(model, form, user, pageNumber, pageSize);
+        final Supplier<String> successView = () -> format("redirect:/monitoring-officer/dashboard/results?keywordSearch=%s&inSetup=%s&inPrevious=%s", form.getKeywordSearch(), form.isProjectInSetup(), form.isPreviousProject());
 
-        return validationHandler.failNowOrSucceedWith(failureView,
-                () -> {
-                    monitoringOfficerDashBoardCookieService.saveMODashboardDataIntoCookie(form, response);
-                    return "redirect:/monitoring-officer/dashboard/results";
-                });
+        return validationHandler.failNowOrSucceedWith(failureView, successView);
     }
 
     @GetMapping("/results")
     public String getResultsDashboard(Model model,
-                                UserResource user,
-                                @RequestParam(value = PAGE_NUMBER_KEY, defaultValue = DEFAULT_PAGE_NUMBER) int pageNumber,
-                                @RequestParam(value = PAGE_SIZE_KEY, defaultValue = DEFAULT_PAGE_SIZE) int pageSize,
-                                HttpServletRequest request) {
+                                      UserResource user,
+                                      @Valid @ModelAttribute(FORM_ATTR_NAME) MonitoringOfficerDashboardForm form,
+                                      @SuppressWarnings("unused") BindingResult bindingResult,
+                                      ValidationHandler validationHandler,
+                                      @RequestParam(value = PAGE_NUMBER_KEY, defaultValue = DEFAULT_PAGE_NUMBER) int pageNumber,
+                                      @RequestParam(value = PAGE_SIZE_KEY, defaultValue = DEFAULT_PAGE_SIZE) int pageSize,
+                                      @RequestParam(defaultValue= "") String keywordSearch,
+                                      @RequestParam(defaultValue = "true") boolean inSetup,
+                                      @RequestParam(defaultValue = "false") boolean inPrevious) {
+        form.setKeywordSearch(keywordSearch);
+        form.setProjectInSetup(inSetup);
+        form.setPreviousProject(inPrevious);
 
-        MonitoringOfficerDashboardForm moDashboardForm = monitoringOfficerDashBoardCookieService.getMODashboardFormCookieValue(request);
-            model.addAttribute(FORM_ATTR_NAME, moDashboardForm);
-            model.addAttribute("model", monitoringOfficerDashboardViewModelPopulator.populate(user
-                    , moDashboardForm.getKeywordSearch()
-                    , moDashboardForm.isProjectInSetup()
-                    , moDashboardForm.isPreviousProject()
-                    , moDashboardForm.isDocumentsComplete()
-                    , moDashboardForm.isDocumentsIncomplete()
-                    , moDashboardForm.isDocumentsAwaitingReview()
-                    , moDashboardForm.isSpendProfileComplete()
-                    , moDashboardForm.isSpendProfileIncomplete()
-                    , moDashboardForm.isSpendProfileAwaitingReview(), pageNumber, pageSize));
+        model.addAttribute(FORM_ATTR_NAME, form);
+        model.addAttribute("model", monitoringOfficerDashboardViewModelPopulator.populate(user, form, pageNumber, pageSize));
 
         return "monitoring-officer/dashboard";
     }
 
     @PostMapping("/results")
     public String postResultsDashboard(Model model,
-                                  @Valid @ModelAttribute(FORM_ATTR_NAME) MonitoringOfficerDashboardForm form,
-                                  @SuppressWarnings("unused") BindingResult bindingResult,
-                                  ValidationHandler validationHandler,
-                                  UserResource user,
-                                  @RequestParam(value = PAGE_NUMBER_KEY, defaultValue = DEFAULT_PAGE_NUMBER) int pageNumber,
-                                  @RequestParam(value = PAGE_SIZE_KEY, defaultValue = DEFAULT_PAGE_SIZE) int pageSize,
-                                  HttpServletRequest request) {
-        final Supplier<String> failureView = () -> getResultsDashboard(model, user, pageNumber, pageSize, request);
+                                       @Valid @ModelAttribute(FORM_ATTR_NAME) MonitoringOfficerDashboardForm form,
+                                       @SuppressWarnings("unused") BindingResult bindingResult,
+                                       ValidationHandler validationHandler,
+                                       UserResource user,
+                                       @RequestParam(value = PAGE_NUMBER_KEY, defaultValue = DEFAULT_PAGE_NUMBER) int pageNumber,
+                                       @RequestParam(value = PAGE_SIZE_KEY, defaultValue = DEFAULT_PAGE_SIZE) int pageSize) {
+        final Supplier<String> failureView = () -> getResultsDashboard(model, user, form, bindingResult, validationHandler, pageNumber, pageSize, form.getKeywordSearch(), form.isProjectInSetup(), form.isPreviousProject());
 
         return validationHandler.failNowOrSucceedWith(failureView,
                 () -> {
-                    model.addAttribute("model", monitoringOfficerDashboardViewModelPopulator.populate(user
-                            , form.getKeywordSearch()
-                            , form.isProjectInSetup()
-                            , form.isPreviousProject()
-                            , form.isDocumentsComplete()
-                            , form.isDocumentsIncomplete()
-                            , form.isDocumentsAwaitingReview()
-                            , form.isSpendProfileComplete()
-                            , form.isSpendProfileIncomplete()
-                            , form.isSpendProfileAwaitingReview(), pageNumber, pageSize));
-
+                    model.addAttribute("model", monitoringOfficerDashboardViewModelPopulator.populate(user, form, pageNumber, pageSize));
                     return "monitoring-officer/dashboard";
                 });
     }
-
 }
--- ifs-web-service/ifs-project-setup-service/src/test/java/org/innovateuk/ifs/project/monitoringofficer/controller/MonitoringOfficerDashboardControllerTest.java
+++ ifs-web-service/ifs-project-setup-service/src/test/java/org/innovateuk/ifs/project/monitoringofficer/controller/MonitoringOfficerDashboardControllerTest.java
@@ -3,7 +3,6 @@ package org.innovateuk.ifs.project.monitoringofficer.controller;
 import org.innovateuk.ifs.BaseControllerMockMVCTest;
 import org.innovateuk.ifs.project.monitoringofficer.form.MonitoringOfficerDashboardForm;
 import org.innovateuk.ifs.project.monitoringofficer.populator.MonitoringOfficerDashboardViewModelPopulator;
-import org.innovateuk.ifs.project.monitoringofficer.service.MonitoringOfficerDashBoardCookieService;
 import org.innovateuk.ifs.project.monitoringofficer.viewmodel.MonitoringOfficerDashboardViewModel;
 import org.junit.Before;
 import org.junit.Test;
@@ -11,7 +10,6 @@ import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -23,9 +21,6 @@ public class MonitoringOfficerDashboardControllerTest extends BaseControllerMock
 
     @Mock
     private MonitoringOfficerDashboardViewModelPopulator populator;
-
-    @Mock
-    private MonitoringOfficerDashBoardCookieService cookieService;
 
     private MonitoringOfficerDashboardForm monitoringOfficerDashboardForm;
 
@@ -42,10 +37,7 @@ public class MonitoringOfficerDashboardControllerTest extends BaseControllerMock
     public void viewDashboard() throws Exception {
         MonitoringOfficerDashboardViewModel model = mock(MonitoringOfficerDashboardViewModel.class);
 
-        when(cookieService.getMODashboardFormCookieValue(any())).thenReturn(monitoringOfficerDashboardForm);
-        when(populator.populate(loggedInUser, null, true, false, false,
-                false, false, false, false,
-                false,0, 10)).thenReturn(model);
+        when(populator.populate(loggedInUser,monitoringOfficerDashboardForm,0, 10)).thenReturn(model);
         mockMvc.perform(get("/monitoring-officer/dashboard"))
                 .andExpect(status().is2xxSuccessful())
                 .andExpect(view().name("monitoring-officer/dashboard"))
@@ -55,13 +47,13 @@ public class MonitoringOfficerDashboardControllerTest extends BaseControllerMock
     @Test
     public void filterDashboard() throws Exception {
         MonitoringOfficerDashboardViewModel model = mock(MonitoringOfficerDashboardViewModel.class);
-        when(populator.populate(loggedInUser, "keyword", false, false, false,
-                false, false, false, false,
-                false,0, 10)).thenReturn(model);
+        monitoringOfficerDashboardForm.setKeywordSearch("keyword");
+        when(populator.populate(loggedInUser, monitoringOfficerDashboardForm,0, 10)).thenReturn(model);
 
         mockMvc.perform(post("/monitoring-officer/dashboard")
                 .param("keywordSearch", "keyword"))
-                .andExpect(view().name("monitoring-officer/dashboard"))
-                .andExpect(model().attribute("model", model));
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/monitoring-officer/dashboard/results?keywordSearch=keyword&inSetup=false&inPrevious=false"));
+
     }
 }
