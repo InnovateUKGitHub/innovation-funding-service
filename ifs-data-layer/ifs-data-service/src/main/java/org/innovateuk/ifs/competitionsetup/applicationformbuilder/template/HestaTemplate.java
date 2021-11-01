d672a3d2634de9b23ba022e1c9f64dbf0073e9ad
Hiral.vaghasiya <hiral.vaghasiya@innovateuk.ukri.org>
2021-11-01 14:43:36 +0000

    IFS-10695 AT's added

--- ifs-data-layer/ifs-data-service/src/main/java/org/innovateuk/ifs/competitionsetup/applicationformbuilder/template/HestaTemplate.java
+++ ifs-data-layer/ifs-data-service/src/main/java/org/innovateuk/ifs/competitionsetup/applicationformbuilder/template/HestaTemplate.java
@@ -1,35 +1,20 @@
 package org.innovateuk.ifs.competitionsetup.applicationformbuilder.template;
 
-import org.apache.commons.lang3.StringUtils;
 import org.innovateuk.ifs.competition.domain.Competition;
 import org.innovateuk.ifs.competition.repository.GrantTermsAndConditionsRepository;
-import org.innovateuk.ifs.competition.resource.CompetitionCompletionStage;
 import org.innovateuk.ifs.competition.resource.CompetitionTypeEnum;
 import org.innovateuk.ifs.competition.resource.FundingRules;
 import org.innovateuk.ifs.competitionsetup.applicationformbuilder.builder.QuestionBuilder;
 import org.innovateuk.ifs.competitionsetup.applicationformbuilder.builder.SectionBuilder;
-import org.innovateuk.ifs.form.domain.FormInput;
-import org.innovateuk.ifs.form.resource.FormInputScope;
-import org.innovateuk.ifs.form.resource.FormInputType;
-import org.innovateuk.ifs.form.resource.QuestionType;
-import org.innovateuk.ifs.form.resource.SectionType;
-import org.innovateuk.ifs.question.resource.QuestionSetupType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.Collections;
 import java.util.List;
-import java.util.Optional;
 
 import static com.google.common.collect.Lists.newArrayList;
-import static java.lang.Boolean.*;
-import static org.innovateuk.ifs.competition.resource.CompetitionCompletionStage.*;
+import static org.innovateuk.ifs.competition.resource.CompetitionCompletionStage.PROJECT_SETUP;
 import static org.innovateuk.ifs.competitionsetup.applicationformbuilder.CommonBuilders.*;
-import static org.innovateuk.ifs.competitionsetup.applicationformbuilder.builder.FormInputBuilder.aFormInput;
-import static org.innovateuk.ifs.competitionsetup.applicationformbuilder.builder.QuestionBuilder.*;
-import static org.innovateuk.ifs.form.resource.FormInputScope.APPLICATION;
 import static org.innovateuk.ifs.form.resource.FormInputScope.ASSESSMENT;
-import static org.innovateuk.ifs.util.CollectionFunctions.simpleFilter;
 
 @Component
 public class HestaTemplate implements CompetitionTemplate {
@@ -53,8 +38,6 @@ public class HestaTemplate implements CompetitionTemplate {
         competition.setAcademicGrantPercentage(100);
         competition.setMinProjectDuration(1);
         competition.setMaxProjectDuration(84);
-        competition.setAlwaysOpen(TRUE);
-        competition.setCompletionStage(PROJECT_SETUP);
         return competition;
     }
 
