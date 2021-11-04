0da5e9d40cba758cdab2673ecc856fc35f9dae1e
Ashish Uniyal <ashish.uniyal@innovateuk.ukri.org>
2021-11-04 16:53:32 +0000

    IFS-10692 resolve merge conflict

--- ifs-sil-stub/src/main/java/org/innovateuk/ifs/sil/crm/controller/CrmEndpointController.java
+++ ifs-sil-stub/src/main/java/org/innovateuk/ifs/sil/crm/controller/CrmEndpointController.java
@@ -4,15 +4,15 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.innovateuk.ifs.commons.rest.RestResult;
 import org.innovateuk.ifs.sil.crm.resource.SilContact;
-import org.innovateuk.ifs.sil.crm.resource.SilLoanAssessment;
 import org.innovateuk.ifs.sil.crm.resource.SilLoanApplication;
+import org.innovateuk.ifs.sil.crm.resource.SilLoanAssessment;
 import org.innovateuk.ifs.util.JsonMappingUtil;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import static org.innovateuk.ifs.commons.rest.RestResult.restFailure;
 import static org.innovateuk.ifs.commons.rest.RestResult.restSuccess;
-import static org.innovateuk.ifs.commons.rest.RestResult.restFailure;
+
 
 /**
  * A simple endpoint to allow stubbing of the SIL outbound CRM updates.
