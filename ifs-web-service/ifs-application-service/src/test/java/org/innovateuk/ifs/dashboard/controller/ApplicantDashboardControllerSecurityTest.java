521db6cbc5d645ed5cdfa07fd05c1467e69a1a9a
Grace Williams <grace.williams@innovateuk.gov.uk>
2021-11-25 12:21:58 +0000

    IFS-10761: Fixed failing test

--- ifs-web-service/ifs-application-service/src/test/java/org/innovateuk/ifs/dashboard/controller/ApplicantDashboardControllerSecurityTest.java
+++ ifs-web-service/ifs-application-service/src/test/java/org/innovateuk/ifs/dashboard/controller/ApplicantDashboardControllerSecurityTest.java
@@ -3,9 +3,13 @@ package org.innovateuk.ifs.dashboard.controller;
 import org.innovateuk.ifs.security.BaseControllerSecurityTest;
 import org.innovateuk.ifs.user.resource.Role;
 import org.junit.Test;
+import org.springframework.mock.web.MockHttpServletResponse;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 import java.util.List;
+
+import static org.mockito.Mockito.mock;
 
 public class ApplicantDashboardControllerSecurityTest extends BaseControllerSecurityTest<ApplicantDashboardController> {
 
@@ -20,6 +24,9 @@ public class ApplicantDashboardControllerSecurityTest extends BaseControllerSecu
         roles.add(Role.APPLICANT);
         roles.add(Role.KNOWLEDGE_TRANSFER_ADVISER);
 
-        assertRolesCanPerform(() -> classUnderTest.dashboard(null, null), roles);
+        HttpServletRequest request = mock(HttpServletRequest.class);
+        MockHttpServletResponse response = new MockHttpServletResponse();
+
+        assertRolesCanPerform(() -> classUnderTest.dashboard(null, null, request, response), roles);
     }
 }
