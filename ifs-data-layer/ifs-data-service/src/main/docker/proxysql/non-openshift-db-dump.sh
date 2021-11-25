c068d92ef034ef90d6ab055f6b992c4e14a5ccb5
mark.dewey <mark.dewey@ukri.org>
2021-11-25 06:01:17 -0400

    db-reset

---
+++ ifs-data-layer/ifs-data-service/src/main/docker/proxysql/non-openshift-db-dump.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+#
+# This script is for testing anonymised dumps against a local docker-compose environment as opposed to a local or remote
+# OpenShift project
+#
+set -e
+
+cd ../../../../../..
+
+./gradlew :ifs-data-layer:ifs-data-service:dbAnonymisedDumpDocker -x test
+
+docker run --name anonymised-data-service --net ifs -d \
+  -e DB_NAME='ifs' \
+  -e DB_USER='root' \
+  -e DB_PASS='password' \
+  -e DB_HOST='ifs-database' \
+  -e DB_PORT='3306' \
+  -e DB_ANON_PASS='password' \
+  innovateuk/db-anonymised-data
+
+# Sleep in order to allow time to establish a connection with the database
+sleep 10
+
+docker exec -it anonymised-data-service /dump/make-mysqldump.sh
+
+sleep 1
+
+docker cp anonymised-data-service:/dump/anonymised-dump.sql.gpg /tmp/anonymised.sql.gpg
+
+docker kill anonymised-data-service && docker rm anonymised-data-service
+
+echo "Now simply run 'gpg --decrypt /tmp/anonymised.sql.gpg > /tmp/anonymised.sql' specifying the password 'password' when prompted"
