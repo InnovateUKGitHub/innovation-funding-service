2abeb580c83ee6dfe014ba74ad781ef0e90492db
Arun.Birajdar <Foanax)bluothWecachJoch0>
2021-12-10 08:35:19 +0000

    IFS-10200 remove unused files

--- robot-tests/run_tests_repeatedly.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# A simple script to run a test suite several times in a row, to aid with tracking down sporadic failures
-
-for ((i=0;i<=$2;i++))
-do 
-    ./micro_run_tests.sh -o -d $1
-done
-
