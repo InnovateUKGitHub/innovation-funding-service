b225c3f0e0f56afc9f45695f8365bff1723c3f0e
Arun.Birajdar <Foanax)bluothWecachJoch0>
2021-12-10 09:43:59 +0000

    IFS-10200 remove var

--- robot-tests/micro_run_tests.sh
+++ robot-tests/micro_run_tests.sh
@@ -357,7 +357,7 @@ dryRun=0
 openReports=1
 
 testDirectory='IFS_acceptance_tests/tests'
-while getopts ":q :h :t :r :c :w :d: :x :R :B :I: :E: :a :o :p" opt ; do
+while getopts ":q :h :t :r :c :w :d: :x :R :B :I: :E: :a :o" opt ; do
     case ${opt} in
         q)
             quickTest=1
