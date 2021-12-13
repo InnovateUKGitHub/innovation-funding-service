d03ebbbedc38e5148f6c175da2cf57168f62d60d
Ashish Uniyal <ashish.uniyal@innovateuk.ukri.org>
2021-12-13 20:04:35 +0530

    IFS-10755 initial commit

---
+++ ifs-data-layer/ifs-data-service/src/main/java/org/innovateuk/ifs/invite/domain/InviteHistory.java
@@ -0,0 +1,30 @@
+package org.innovateuk.ifs.invite.domain;
+
+import lombok.Data;
+import org.innovateuk.ifs.invite.constant.InviteStatus;
+import org.innovateuk.ifs.user.domain.User;
+
+import javax.persistence.*;
+import java.time.ZonedDateTime;
+
+@Data
+@Entity
+public class InviteHistory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name="inviteId", referencedColumnName="id")
+    private Invite invite;
+
+    @Enumerated(EnumType.STRING)
+    private InviteStatus status;
+
+    @Column
+    private ZonedDateTime updatedOn;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="updatedBy", referencedColumnName="id")
+    private User updatedBy;
+}
