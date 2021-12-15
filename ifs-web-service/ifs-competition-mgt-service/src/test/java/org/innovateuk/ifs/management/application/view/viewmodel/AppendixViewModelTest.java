1e7f1027c5bfaa22ac46861af8393120e632d2eb
markd@iuk.ukri.org <markd@iuk.ukri.org>
2021-12-15 17:51:09 -0400

    junit4...

--- ifs-web-service/ifs-competition-mgt-service/src/test/java/org/innovateuk/ifs/management/application/view/viewmodel/AppendixViewModelTest.java
+++ ifs-web-service/ifs-competition-mgt-service/src/test/java/org/innovateuk/ifs/management/application/view/viewmodel/AppendixViewModelTest.java
@@ -1,15 +1,15 @@
 package org.innovateuk.ifs.management.application.view.viewmodel;
 
 import org.innovateuk.ifs.file.resource.FileEntryResource;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
-class AppendixViewModelTest {
+public class AppendixViewModelTest {
 
     @Test
-    void getHumanReadableFileSize() {
+    public void getHumanReadableFileSize() {
         assertThat(buildAppendixViewModel(1048576L).getHumanReadableFileSize(), equalTo("1"));
         assertThat(buildAppendixViewModel(1000000L).getHumanReadableFileSize(), equalTo("0"));
         assertThat(buildAppendixViewModel(5242880L).getHumanReadableFileSize(), equalTo("5"));
