e9b5f8726f1f432a7b809a3a366b5748abbac227
Punniya Rajkumar <punniya.rajkumar@innovateuk.ukri.org>
2022-01-05 17:59:45 +0000

    checking the cost data

--- ifs-data-layer/ifs-data-service/src/main/resources/db/webtest/V9999_14__Update_value_by_comp_name.sql
+++ ifs-data-layer/ifs-data-service/src/main/resources/db/webtest/V9999_14__Update_value_by_comp_name.sql
@@ -3,7 +3,7 @@ SELECT @org_id := id FROM organisation WHERE name = 'Aberystwyth University';
 SELECT @finance_id := id FROM project_finance WHERE organisation_id = @org_id AND project_id = @project_id;
 UPDATE finance_row SET cost = IF(cost IS NOT NULL, 30000, NULL) WHERE target_id = @finance_id;
 
-UPDATE finance_row SET cost = IF(cost IS NOT NULL, 1000, NULL) WHERE target_id = 4;
+UPDATE finance_row SET cost = IF(cost IS NOT NULL, 100, NULL) WHERE target_id = 4;
 
 
 SELECT @org_id_2 := id FROM organisation WHERE name = 'Big Riffs And Insane Solos Ltd';
