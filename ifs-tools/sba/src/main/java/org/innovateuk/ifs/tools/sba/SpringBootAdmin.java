2ededd68778d11820350ea19b05042d0b38d7df5
MarkD <MarkD@iuk.ukri.org>
2022-01-24 17:32:51 +0000

    IFS-11091: Enable k8s client

--- ifs-tools/sba/src/main/java/org/innovateuk/ifs/tools/sba/SpringBootAdmin.java
+++ ifs-tools/sba/src/main/java/org/innovateuk/ifs/tools/sba/SpringBootAdmin.java
@@ -4,10 +4,12 @@ package org.innovateuk.ifs.tools.sba;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableAdminServer
+@EnableDiscoveryClient
 @EnableScheduling
 public class SpringBootAdmin {
 
