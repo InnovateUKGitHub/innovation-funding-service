6ea500495ad5264f4e5353206a50e3afa6d5bdab
Dexter Ng <Dexter.Ng@ukri.org>
2022-01-27 16:06:23 +0000

    IFS-11202 update comment

--- ifs-data-layer/ifs-data-service/src/main/resources/db/migration/V245_1__Add_project_finance_fEC_cert_expiry_date.sql
+++ ifs-data-layer/ifs-data-service/src/main/resources/db/migration/V245_1__Add_project_finance_fEC_cert_expiry_date.sql
@@ -1,9 +1,7 @@
--- 11116
+-- IFS-11116 / IFS-11202
 
--- IFS-10736
+-- realated ticket IFS-10736
 ALTER TABLE application_finance
     ADD COLUMN fec_cert_expiry_date DATE NULL DEFAULT NULL AFTER fec_file_entry_id;
 ALTER TABLE project_finance
     ADD COLUMN fec_cert_expiry_date DATE NULL DEFAULT NULL AFTER fec_file_entry_id;
-
--- IFS-10738
