d2b98f94c7ac4079f1c6dc781c5c9bab708cf23c
Ashish Uniyal <ashish.uniyal@innovateuk.ukri.org>
2022-02-09 12:46:16 +0000

    IFS-11303 remove update script

--- ifs-data-layer/ifs-data-service/src/main/resources/db/migration/V246_1__Update_User.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE `user`
-    ADD COLUMN `edi_status` ENUM('COMPLETE', 'INCOMPLETE', 'INPROGRESS') NULL AFTER `modified_on`,
-    ADD COLUMN `edi_review_date` DATETIME NULL AFTER `edi_status`;
-
