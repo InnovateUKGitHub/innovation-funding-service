1e915632815f4474174204292721b8facf090c0b
adam-mapstone <adam.mapstone@ukri.org>
2022-02-16 14:40:39 +0000

    IFS-11299 - New funding type added.

--- common/ifs-resources/src/main/java/org/innovateuk/ifs/competition/publiccontent/resource/FundingType.java
+++ common/ifs-resources/src/main/java/org/innovateuk/ifs/competition/publiccontent/resource/FundingType.java
@@ -11,7 +11,8 @@ public enum FundingType {
     INVESTOR_PARTNERSHIPS("Investor Partnerships", "Investor Partnerships"),
     KTP("Knowledge Transfer Partnership (KTP)", "Knowledge Transfer Partnership (KTP)", "KTP GOL Template"),
     LOAN("Loan", "Loans"),
-    PROCUREMENT("Procurement", "Procurement", "Procurement GOL Template");
+    PROCUREMENT("Procurement", "Procurement", "Procurement GOL Template"),
+    HECP("Horizon Europe Guarantee", "Horizon Europe Guarantee", "Horizon Europe Guarantee GOL Template");
 
     private final String displayName;
     private final String defaultTermsName;
