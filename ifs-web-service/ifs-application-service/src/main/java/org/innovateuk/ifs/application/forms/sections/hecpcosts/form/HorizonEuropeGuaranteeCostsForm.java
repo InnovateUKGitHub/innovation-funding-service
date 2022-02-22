dbf44d2597e7fd685e61e8145baa4b495ef336f8
Adam Mapstone <adam.mapstone@ukri.org>
2022-02-22 23:54:45 +0000

    IFS-11269 - Lombok added to costs form.

--- ifs-web-service/ifs-application-service/src/main/java/org/innovateuk/ifs/application/forms/sections/hecpcosts/form/HorizonEuropeGuaranteeCostsForm.java
+++ ifs-web-service/ifs-application-service/src/main/java/org/innovateuk/ifs/application/forms/sections/hecpcosts/form/HorizonEuropeGuaranteeCostsForm.java
@@ -1,7 +1,12 @@
 package org.innovateuk.ifs.application.forms.sections.hecpcosts.form;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.math.BigInteger;
 
+@Getter
+@Setter
 public class HorizonEuropeGuaranteeCostsForm {
 
     private BigInteger labour;
@@ -11,62 +16,6 @@ public class HorizonEuropeGuaranteeCostsForm {
     private BigInteger subcontracting;
     private BigInteger travel;
     private BigInteger other;
-
-    public BigInteger getLabour() {
-        return labour;
-    }
-
-    public void setLabour(BigInteger labour) {
-        this.labour = labour;
-    }
-
-    public BigInteger getOverhead() {
-        return overhead;
-    }
-
-    public void setOverhead(BigInteger overhead) {
-        this.overhead = overhead;
-    }
-
-    public BigInteger getMaterial() {
-        return material;
-    }
-
-    public void setMaterial(BigInteger material) {
-        this.material = material;
-    }
-
-    public BigInteger getCapital() {
-        return capital;
-    }
-
-    public void setCapital(BigInteger capital) {
-        this.capital = capital;
-    }
-
-    public BigInteger getSubcontracting() {
-        return subcontracting;
-    }
-
-    public void setSubcontracting(BigInteger subcontracting) {
-        this.subcontracting = subcontracting;
-    }
-
-    public BigInteger getTravel() {
-        return travel;
-    }
-
-    public void setTravel(BigInteger travel) {
-        this.travel = travel;
-    }
-
-    public BigInteger getOther() {
-        return other;
-    }
-
-    public void setOther(BigInteger other) {
-        this.other = other;
-    }
 
     /* view logic. */
     public BigInteger getOrganisationFinanceTotal() {
