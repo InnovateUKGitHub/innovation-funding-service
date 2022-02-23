fbef0af05e04f441ef31a148893f6c4bbe8bdc87
Prem Jonathan <prem.jonathan@iuk.ukri.org>
2022-02-23 23:18:42 +0530

    draft changes to display edi_status in application read-only views

--- common/ifs-application-commons/src/main/java/org/innovateuk/ifs/application/readonly/viewmodel/ApplicationTeamUserReadOnlyViewModel.java
+++ common/ifs-application-commons/src/main/java/org/innovateuk/ifs/application/readonly/viewmodel/ApplicationTeamUserReadOnlyViewModel.java
@@ -1,6 +1,7 @@
 package org.innovateuk.ifs.application.readonly.viewmodel;
 
 import org.innovateuk.ifs.invite.resource.ApplicationInviteResource;
+import org.innovateuk.ifs.user.resource.EDIStatus;
 import org.innovateuk.ifs.user.resource.ProcessRoleResource;
 
 import java.time.ZonedDateTime;
@@ -13,22 +14,27 @@ public class ApplicationTeamUserReadOnlyViewModel {
     private final boolean lead;
     private final boolean invite;
     private final ZonedDateTime pendingSince;
+    private final EDIStatus ediStatus;
 
-    public ApplicationTeamUserReadOnlyViewModel(String name, String email, String phone, boolean lead, boolean invite, ZonedDateTime pendingSince) {
+    public ApplicationTeamUserReadOnlyViewModel(String name, String email, String phone, boolean lead, boolean invite,
+                                                ZonedDateTime pendingSince, EDIStatus ediStatus) {
         this.name = name;
         this.email = email;
         this.phone = phone;
         this.lead = lead;
         this.invite = invite;
         this.pendingSince = pendingSince;
+        this.ediStatus = ediStatus;
     }
 
-    public static ApplicationTeamUserReadOnlyViewModel fromProcessRole(ProcessRoleResource pr, String phone) {
-        return new ApplicationTeamUserReadOnlyViewModel(pr.getUserName(), pr.getUserEmail(), phone, pr.getRole().isLeadApplicant(), false, null);
+    public static ApplicationTeamUserReadOnlyViewModel fromProcessRole(ProcessRoleResource pr, String phone, EDIStatus ediStatus) {
+        return new ApplicationTeamUserReadOnlyViewModel(pr.getUserName(), pr.getUserEmail(), phone, pr.getRole().isLeadApplicant(),
+                false, null, ediStatus);
     }
 
     public static ApplicationTeamUserReadOnlyViewModel fromInvite(ApplicationInviteResource invite) {
-        return new ApplicationTeamUserReadOnlyViewModel(invite.getName(), invite.getEmail(), null, false, true, invite.getSentOn());
+        return new ApplicationTeamUserReadOnlyViewModel(invite.getName(), invite.getEmail(), null, false,
+                true, invite.getSentOn(), null);
     }
 
     public String getEmail() {
@@ -55,5 +61,16 @@ public class ApplicationTeamUserReadOnlyViewModel {
 
     private long getDaysPending() {
         return ChronoUnit.DAYS.between(pendingSince, ZonedDateTime.now());
+    }
+
+    public boolean isEdiCompleted() {
+        return ediStatus != null
+            && EDIStatus.COMPLETE == ediStatus;
+    }
+
+    public String getEdiStatus() {
+        return ediStatus == null
+                ? EDIStatus.INCOMPLETE.getDisplayName()
+                : ediStatus.getDisplayName();
     }
 }
