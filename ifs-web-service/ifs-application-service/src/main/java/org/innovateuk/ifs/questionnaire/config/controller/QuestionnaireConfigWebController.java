04ba9642f82a9e7576370146e4f67a1f6127ce4a
MarkD <MarkD@iuk.ukri.org>
2022-02-24 14:58:16 +0000

    revert for now

--- ifs-web-service/ifs-application-service/src/main/java/org/innovateuk/ifs/questionnaire/config/controller/QuestionnaireConfigWebController.java
+++ ifs-web-service/ifs-application-service/src/main/java/org/innovateuk/ifs/questionnaire/config/controller/QuestionnaireConfigWebController.java
@@ -1,7 +1,6 @@
 package org.innovateuk.ifs.questionnaire.config.controller;
 
 
-import org.innovateuk.ifs.IfsProfileConstants;
 import org.innovateuk.ifs.controller.ValidationHandler;
 import org.innovateuk.ifs.questionnaire.config.form.QuestionnaireQuestionConfigForm;
 import org.innovateuk.ifs.questionnaire.config.form.QuestionnaireQuestionOptionForm;
@@ -29,6 +28,7 @@ import java.util.Objects;
 @Controller
 @RequestMapping("/questionnaire-configure")
 @PreAuthorize("permitAll")
+@Profile("questionnaire-configure")
 public class QuestionnaireConfigWebController {
 
     @Autowired
