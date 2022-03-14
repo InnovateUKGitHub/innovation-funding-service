33ed3613bd44d1dd02c6bf843c8fa8edc85c4bb3
MarkD <MarkD@iuk.ukri.org>
2022-03-14 16:34:15 +0000

    try and make sense of the configuration

--- common/starter/ifs-starter-cache/src/main/java/org/innovateuk/ifs/starters/cache/cfg/RedisConfiguration.java
+++ common/starter/ifs-starter-cache/src/main/java/org/innovateuk/ifs/starters/cache/cfg/RedisConfiguration.java
@@ -11,39 +11,18 @@ import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.cache.annotation.CachingConfigurerSupport;
 import org.springframework.context.annotation.Bean;
 
-import javax.annotation.PostConstruct;
-
 @Slf4j
 public class RedisConfiguration extends CachingConfigurerSupport {
 
     @Autowired
     private RedisProperties redisProperties;
 
-    /*
-        Modify the redis properties. We set the redis connection with env variables. We want to use env variables
-        to configure either an standalone or cluster configuration. The default autoconfigurer will assume we're using
-        a cluster configuration if the cluster nodes property is defined (Even if its empty!).
-
-        Here we are setting the cluster configuration to be null if the nodes property is empty.
-     */
-    @PostConstruct
-    public void redisConfiguration() {
-        if (redisProperties.getCluster() != null && redisProperties.getCluster().getNodes().isEmpty()) {
-            redisProperties.setCluster(null);
-        }
-    }
-
     @Bean
     public LettuceClientConfigurationBuilderCustomizer lettuceClientConfigurationBuilderCustomizer() {
-        final ClientOptions.Builder options;
-        if (redisProperties.getCluster() != null) {
-            options = ClusterClientOptions.builder()
-                    .validateClusterNodeMembership(false);
-        } else {
-            options = ClientOptions.builder();
-        }
-        return builder -> builder.clientOptions(options
-                .disconnectedBehavior(DisconnectedBehavior.REJECT_COMMANDS).build());
+        final ClientOptions.Builder options = ClusterClientOptions.builder()
+//                    .validateClusterNodeMembership(false)
+                .disconnectedBehavior(DisconnectedBehavior.REJECT_COMMANDS);
+        return builder -> builder.clientOptions(options.build());
     }
 
 }
