5ca3ffcc2af6d59d95a5a1670097dbe4813e387e
MarkD <MarkD@iuk.ukri.org>
2022-03-24 15:24:33 +0000

    new-relic changes

--- os-files/kustomize/kubeval.sh
+++ os-files/kustomize/kubeval.sh
@@ -9,5 +9,3 @@ kustomize build env/stub/custom | kubeval --strict --openshift
 
 kustomize build env/stub/local/ext  | kubeval --strict
 kustomize build env/stub/local/dev | kubeval --strict
-kustomize build env/stub/local/custom | kubeval --strict
-kustomize build env/stub/local/debug | kubeval --strict
