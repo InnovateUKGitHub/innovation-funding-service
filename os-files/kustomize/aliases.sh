9bfd97e4d762b4bc7f49e58d98dd4d49671528a7
Arun.Birajdar <Foanax)bluothWecachJoch0>
2022-03-24 12:05:05 +0000

    IFS-11468 delete redundant files

--- os-files/kustomize/aliases.sh
+++ os-files/kustomize/aliases.sh
@@ -9,7 +9,6 @@
 alias skaffold_e="skaffold dev -f skaffold-EXT.yml --rpc-http-port=50054 --rpc-port=50053 --auto-build=false --auto-sync=false --auto-deploy=false --status-check=true --wait-for-deletions=true --tail=false"
 # Use sensible defaults to deploy dev and custom builds in a faster mode (use one at a time)
 alias skaffold_dx="skaffold dev --watch-image='[]' --cache-artifacts=false --auto-build=false --auto-sync=false --auto-deploy=false --status-check=false --wait-for-deletions=true --tail=false"
-alias skaffold_cx="skaffold dev -f skaffold-CUSTOM.yml --cache-artifacts=false --watch-image='[]' --auto-build=false --auto-sync=false --auto-deploy=false --status-check=false --wait-for-deletions=true --tail=false"
 # View state/events for dev/custom in firefox
 alias skaffold_state="open -a Firefox http://localhost:50052/v1/state"
 alias skaffold_events="open -a Firefox http://localhost:50052/v1/events"
@@ -180,7 +179,6 @@ skaffold_help () {
     echo '    skaffold_dev [file] runs fast dev mode on specified skaffold file'
     echo '    skaffold_debug [file] runs fast debug mode on specified skaffold file'
     echo ''
-    echo '    skaffold_cx - runs services configured via skaffold-CUSTOM.yml (editable)'
     echo '    It is quite easy to create ad-hoc configurations for any dev/ops purpose'
     echo '<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<->>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>'
 }
