cd7d2fcfe33e81f9bbfcbbe4136bdc75aac57c65
Adam Mapstone <adam.mapstone@ukri.org>
2022-03-25 12:36:06 +0000

    IFS-11366 - Has permission added to service method.

--- ifs-data-layer/ifs-data-service/src/main/java/org/innovateuk/ifs/horizon/transactional/HorizonWorkProgrammeService.java
+++ ifs-data-layer/ifs-data-service/src/main/java/org/innovateuk/ifs/horizon/transactional/HorizonWorkProgrammeService.java
@@ -3,11 +3,13 @@ package org.innovateuk.ifs.horizon.transactional;
 import org.innovateuk.ifs.commons.service.ServiceResult;
 import org.innovateuk.ifs.horizon.resource.ApplicationHorizonWorkProgrammeResource;
 import org.innovateuk.ifs.horizon.resource.HorizonWorkProgramme;
+import org.springframework.security.access.prepost.PreAuthorize;
 
 import java.util.List;
 
 public interface HorizonWorkProgrammeService {
 
+    @PreAuthorize("hasPermission(#applicationId, 'org.innovateuk.ifs.application.resource.ApplicationResource', 'UPDATE')")
     ServiceResult<Void> updateWorkProgrammesForApplication(List<HorizonWorkProgramme> programmes, long applicationId);
 
     ServiceResult<List<ApplicationHorizonWorkProgrammeResource>> findSelectedForApplication(long applicationId);
