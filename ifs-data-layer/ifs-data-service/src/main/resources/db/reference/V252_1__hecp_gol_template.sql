5f7136def4e353d8cd21ec7b177e8436b1f17d35
Adam Mapstone <adam.mapstone@ukri.org>
2022-04-05 21:38:03 +0100

    IFS-11366 - Indent on script updated.

--- ifs-data-layer/ifs-data-service/src/main/resources/db/reference/V252_1__hecp_gol_template.sql
+++ ifs-data-layer/ifs-data-service/src/main/resources/db/reference/V252_1__hecp_gol_template.sql
@@ -8,12 +8,12 @@ SET @hecp_gol_template_id = 65;
 
 INSERT INTO terms_and_conditions (id, name, template, version, type, created_by, created_on, modified_by, modified_on)
 VALUES (
-           @hecp_gol_template_id,
-           'Horizon Europe Guarantee GOL Template',
-           'hecp-gol-template',
-           1,
-           'GOL',
-           @system_maintenance_user_id,
-           NOW(),
-           @system_maintenance_user_id,
-           now());
+        @hecp_gol_template_id,
+        'Horizon Europe Guarantee GOL Template',
+        'hecp-gol-template',
+        1,
+        'GOL',
+        @system_maintenance_user_id,
+        NOW(),
+        @system_maintenance_user_id,
+        now());
