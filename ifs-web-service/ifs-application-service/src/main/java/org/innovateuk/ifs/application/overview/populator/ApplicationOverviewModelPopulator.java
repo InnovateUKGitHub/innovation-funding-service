1cfd81eb3e186c7c72ce114b43d4ea1e65e4e970
Prem Jonathan <prem.jonathan@iuk.ukri.org>
2022-04-06 16:29:36 +0530

    override appication description when there is no assessment stage

--- ifs-web-service/ifs-application-service/src/main/java/org/innovateuk/ifs/application/overview/populator/ApplicationOverviewModelPopulator.java
+++ ifs-web-service/ifs-application-service/src/main/java/org/innovateuk/ifs/application/overview/populator/ApplicationOverviewModelPopulator.java
@@ -169,10 +169,7 @@ public class ApplicationOverviewModelPopulator extends AsyncAdaptor {
                 }
                 break;
             case APPLICATION_QUESTIONS:
-                if (competition.isHorizonEuropeGuarantee()) {
-                    messageCode = "ifs.section.hecpApplicationQuestions.description";
-                    break;
-                } else if (!competition.isKtp() || competition.isHasAssessmentStage()) {
+                if (!competition.isKtp() && competition.isHasAssessmentStage()) {
                     messageCode = "ifs.section.applicationQuestions.description";
                     break;
                 }
