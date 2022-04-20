8e8d777a1a1adfba189ece212bceee9545232a48
Dexter Ng <Dexter.Ng@ukri.org>
2022-04-20 10:55:29 +0100

    IFS-11664 - change comp setup tnc selection name - change tnc title - remove hydrogen annexes

--- ifs-data-layer/ifs-data-service/src/main/resources/db/migration/V256_1_1_Add_hydrogen_terms_and_conditions.sql
+++ ifs-data-layer/ifs-data-service/src/main/resources/db/migration/V256_1_1_Add_hydrogen_terms_and_conditions.sql
@@ -3,5 +3,7 @@ SET @system_maintenance_user_id = (
     FROM user
     WHERE email = 'ifs_system_maintenance_user@innovateuk.org');
 
-INSERT INTO terms_and_conditions (name, template, version, type, created_by, created_on, modified_on, modified_by)
-values('Hydrogen Grant', 'hydrogen-terms-and-conditions-v1', 1, 'GRANT', @system_maintenance_user_id, now(), now(), @system_maintenance_user_id);
+SET @new_terms_id = 66;
+
+INSERT INTO terms_and_conditions (id, name, template, version, type, created_by, created_on, modified_on, modified_by)
+values(new_terms_id, 'BEIS Net Zero', 'hydrogen-terms-and-conditions-v1', 1, 'GRANT', @system_maintenance_user_id, now(), now(), @system_maintenance_user_id);
