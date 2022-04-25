2e858fd666cb5a643b4ab681cf7ecd18fff45aa7
Punniya Rajkumar <punniya.rajkumar@innovateuk.ukri.org>
2022-04-25 16:04:52 +0100

    IFS-11787 Review comment updated

--- ifs-data-layer/ifs-data-service/src/main/java/org/innovateuk/ifs/invite/controller/InviteUserController.java
+++ ifs-data-layer/ifs-data-service/src/main/java/org/innovateuk/ifs/invite/controller/InviteUserController.java
@@ -33,12 +33,8 @@ public class InviteUserController {
 
     @PostMapping("/save-invite")
     public RestResult<Void> saveUserInvite(@RequestBody InviteUserResource inviteUserResource) {
-        if (inviteUserResource.getRole().isAssessor()) {
-            return inviteUserService.saveAssessorInvite(
-                    inviteUserResource.getInvitedUser(), inviteUserResource.getRole(), inviteUserResource.getInnovationAreaId()).toPostResponse();
-        }
-        return inviteUserService.saveUserInvite(
-                inviteUserResource.getInvitedUser(), inviteUserResource.getRole(), inviteUserResource.getOrganisation()).toPostResponse();
+         return inviteUserResource.getRole().isAssessor() ? inviteUserService.saveAssessorInvite(inviteUserResource.getInvitedUser(), inviteUserResource.getRole(), inviteUserResource.getInnovationAreaId()).toPostResponse()
+               : inviteUserService.saveUserInvite(inviteUserResource.getInvitedUser(), inviteUserResource.getRole(), inviteUserResource.getOrganisation()).toPostResponse();
     }
 
     @GetMapping("/get-invite/{inviteHash}")
