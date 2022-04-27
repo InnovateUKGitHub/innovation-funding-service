468f44c65a0e4a2ec539da70a0970c5b024fb113
Adam Mapstone <adam.mapstone@ukri.org>
2022-04-27 22:33:10 +0100

    IFS-11682 - Direct award competition template added.

---
+++ ifs-data-layer/ifs-data-service/src/main/java/org/innovateuk/ifs/competitionsetup/applicationformbuilder/template/DirectAwardTemplate.java
@@ -0,0 +1,69 @@
+package org.innovateuk.ifs.competitionsetup.applicationformbuilder.template;
+
+import org.innovateuk.ifs.competition.domain.Competition;
+import org.innovateuk.ifs.competition.repository.GrantTermsAndConditionsRepository;
+import org.innovateuk.ifs.competition.resource.CompetitionTypeEnum;
+import org.innovateuk.ifs.competition.resource.FundingRules;
+import org.innovateuk.ifs.competitionsetup.applicationformbuilder.builder.QuestionBuilder;
+import org.innovateuk.ifs.competitionsetup.applicationformbuilder.builder.SectionBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+import static com.google.common.collect.Lists.newArrayList;
+import static org.innovateuk.ifs.competitionsetup.applicationformbuilder.CommonBuilders.*;
+import static org.innovateuk.ifs.competitionsetup.applicationformbuilder.CommonBuilders.genericQuestion;
+
+@Component
+public class DirectAwardTemplate implements CompetitionTemplate {
+
+    @Autowired
+    private GrantTermsAndConditionsRepository grantTermsAndConditionsRepository;
+
+    @Override
+    public CompetitionTypeEnum type() {
+        return CompetitionTypeEnum.DIRECT_AWARD;
+    }
+
+    @Override
+    public Competition copyTemplatePropertiesToCompetition(Competition competition) {
+        if (FundingRules.SUBSIDY_CONTROL == competition.getFundingRules()) {
+            competition.setTermsAndConditions(grantTermsAndConditionsRepository.findFirstByNameOrderByVersionDesc("Innovate UK - Subsidy control"));
+            competition.setOtherFundingRulesTermsAndConditions(grantTermsAndConditionsRepository.findFirstByNameOrderByVersionDesc("Innovate UK"));
+        } else {
+            competition.setTermsAndConditions(grantTermsAndConditionsRepository.findFirstByNameOrderByVersionDesc("Innovate UK"));
+        }
+
+        competition.setAcademicGrantPercentage(100);
+        competition.setAlwaysOpen(true);
+        return competition;
+    }
+
+    @Override
+    public List<SectionBuilder> sections() {
+        return newArrayList(
+                projectDetails()
+                        .withQuestions(newArrayList(
+                                applicationTeam(),
+                                applicationDetails(),
+                                researchCategory(),
+                                equalityDiversityAndInclusion(),
+                                projectSummary(),
+                                publicDescription(),
+                                scope()
+                        )),
+                applicationQuestions()
+                        .withQuestions(newArrayList(
+                                directAwardDefaultQuestion()
+                        )),
+                finances(),
+                termsAndConditions()
+        );
+    }
+
+    public static QuestionBuilder directAwardDefaultQuestion() {
+        return genericQuestion()
+                .withName("Direct award place holder question");
+    }
+}
