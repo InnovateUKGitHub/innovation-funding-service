65cb322165df7a9885d5a61d5a00a7b212a358ee
Dexter Ng <Dexter.Ng@ukri.org>
2022-04-29 09:39:49 +0100

    IFS-11911 default tnc content change

---
+++ ifs-data-layer/ifs-data-service/src/main/resources/db/migration/V258_1_1__Update_default_terms_and_conditions.sql
@@ -0,0 +1,10 @@
+-- IFS-11911 Innovate UK T&C update
+
+SET @system_maintenance_user_id = (
+    SELECT id
+    FROM user
+    WHERE email = 'ifs_system_maintenance_user@innovateuk.org');
+
+INSERT INTO terms_and_conditions (name, template, version, type, created_by, created_on, modified_by, modified_on)
+VALUES ('Innovate UK', 'default-terms-and-conditions-v9', 9, 'GRANT',
+        @system_maintenance_user_id, now(), @system_maintenance_user_id, now());
