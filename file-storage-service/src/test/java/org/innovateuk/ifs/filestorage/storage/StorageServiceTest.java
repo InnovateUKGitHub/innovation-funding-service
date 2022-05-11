5e72042bffd9f3b5a6de5e58281ea0b9f16534e5
MarkD <MarkD@iuk.ukri.org>
2022-05-11 16:36:58 +0100

    feign starter

--- file-storage-service/src/test/java/org/innovateuk/ifs/filestorage/storage/StorageServiceTest.java
+++ file-storage-service/src/test/java/org/innovateuk/ifs/filestorage/storage/StorageServiceTest.java
@@ -50,7 +50,7 @@ class StorageServiceTest {
         FileStorageRecord fileStorageRecord = fileStorageRecordRepository.findById(uuid.toString()).get();
         assertThat(fileStorageRecord.fileUuid(), equalTo(uuid.toString()));
         FileDownloadResponse fileDownloadResponse = storageService.fileByUuid(uuid.toString()).get();
-        assertThat(fileDownloadResponse.fileId(), equalTo(uuid.toString()));
+        assertThat(fileDownloadResponse.getFileId(), equalTo(uuid.toString()));
     }
 
 }
