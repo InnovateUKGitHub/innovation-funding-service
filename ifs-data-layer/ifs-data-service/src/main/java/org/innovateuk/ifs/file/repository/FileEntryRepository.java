941596596d80dabb4d02589a1ac30c69bf84b37f
Arun Birajdar <arun.birajdar@innovateuk.ukri.org>
2022-05-25 11:09:23 +0100

    IFS-11863 subselect query

--- ifs-data-layer/ifs-data-service/src/main/java/org/innovateuk/ifs/file/repository/FileEntryRepository.java
+++ ifs-data-layer/ifs-data-service/src/main/java/org/innovateuk/ifs/file/repository/FileEntryRepository.java
@@ -3,14 +3,14 @@ package org.innovateuk.ifs.file.repository;
 import org.innovateuk.ifs.file.domain.FileEntry;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import java.util.List;
 
 /**
  *
  */
-public interface FileEntryRepository extends CrudRepository<FileEntry, Long> {
+public interface FileEntryRepository extends PagingAndSortingRepository<FileEntry, Long> {
 
     @Query("SELECT a from FileEntry a WHERE a.fileUuid IS NULL AND a.id NOT IN (SELECT b.fileEntryId from GlusterMigrationStatus b WHERE b.status = 'FILE_NOT_FOUND' )")
     List<FileEntry> findFileEntryByStatusAndFileUUIDIsNUll(Pageable pageable);
