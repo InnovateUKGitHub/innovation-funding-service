550fbf17bc1221799196bcc346873309a44e746f
Arun Birajdar <arun.birajdar@innovateuk.ukri.org>
2022-05-27 09:06:59 +0100

    IFS-11863 not secured mustBeSecuredByOtherServices false

--- ifs-data-layer/ifs-data-service/src/main/java/org/innovateuk/ifs/glustermigration/service/GlusterMigrationService.java
+++ ifs-data-layer/ifs-data-service/src/main/java/org/innovateuk/ifs/glustermigration/service/GlusterMigrationService.java
@@ -1,5 +1,6 @@
 package org.innovateuk.ifs.glustermigration.service;
 
+import org.innovateuk.ifs.commons.security.NotSecured;
 import org.innovateuk.ifs.commons.service.ServiceResult;
 import org.innovateuk.ifs.schedule.transactional.ScheduleResponse;
 
@@ -7,6 +8,7 @@ import java.io.IOException;
 
 public interface GlusterMigrationService {
 
+    @NotSecured(value = "Gluster migration scheduler", mustBeSecuredByOtherServices = false)
     ServiceResult<ScheduleResponse> processGlusterFiles() throws IOException;
 
 }
