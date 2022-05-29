b5e9cba360d293886adc47ff048cb3480f1b0b61
Adam Mapstone <adam.mapstone@ukri.org>
2022-05-29 02:22:17 +0100

    IFS-12012 - Fix to unit tests.

--- ifs-web-service/ifs-project-setup-mgt-service/src/test/java/org/innovateuk/ifs/project/eligibility/saver/FinanceChecksEligibilityHecpCostsSaverTest.java
+++ ifs-web-service/ifs-project-setup-mgt-service/src/test/java/org/innovateuk/ifs/project/eligibility/saver/FinanceChecksEligibilityHecpCostsSaverTest.java
@@ -18,21 +18,18 @@ import static java.util.Arrays.asList;
 import static java.util.Collections.emptyList;
 import static java.util.Collections.singletonList;
 import static org.innovateuk.ifs.commons.rest.RestResult.restSuccess;
-import static org.innovateuk.ifs.finance.builder.CapitalUsageBuilder.newCapitalUsage;
 import static org.innovateuk.ifs.finance.builder.DefaultCostCategoryBuilder.newDefaultCostCategory;
 import static org.innovateuk.ifs.finance.builder.EquipmentCostBuilder.newEquipment;
 import static org.innovateuk.ifs.finance.builder.HecpIndirectCostsBuilder.newHecpIndirectCosts;
 import static org.innovateuk.ifs.finance.builder.HecpIndirectCostsCategoryBuilder.newHecpIndirectCostsCostCategory;
-import static org.innovateuk.ifs.finance.builder.LabourCostBuilder.newLabourCost;
-import static org.innovateuk.ifs.finance.builder.LabourCostCategoryBuilder.newLabourCostCategory;
-import static org.innovateuk.ifs.finance.builder.MaterialsCostBuilder.newMaterials;
 import static org.innovateuk.ifs.finance.builder.OtherCostBuilder.newOtherCost;
 import static org.innovateuk.ifs.finance.builder.OtherGoodsBuilder.newOtherGoods;
-import static org.innovateuk.ifs.finance.builder.OverheadCostCategoryBuilder.newOverheadCostCategory;
+import static org.innovateuk.ifs.finance.builder.PersonnelCostBuilder.newPersonnelCost;
+import static org.innovateuk.ifs.finance.builder.PersonnelCostCategoryBuilder.newPersonnelCostCategory;
 import static org.innovateuk.ifs.finance.builder.ProjectFinanceResourceBuilder.newProjectFinanceResource;
 import static org.innovateuk.ifs.finance.builder.SubcontractingCostBuilder.newSubContractingCost;
 import static org.innovateuk.ifs.finance.builder.TravelCostBuilder.newTravelCost;
-import static org.innovateuk.ifs.finance.resource.category.LabourCostCategory.WORKING_DAYS_PER_YEAR;
+import static org.innovateuk.ifs.finance.resource.category.PersonnelCostCategory.WORKING_DAYS_PER_YEAR;
 import static org.innovateuk.ifs.util.MapFunctions.asMap;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
@@ -56,16 +53,18 @@ public class FinanceChecksEligibilityHecpCostsSaverTest {
 
     @Test
     public void save() {
-        LabourCost workingDays = newLabourCost()
+        PersonnelCost workingDays = newPersonnelCost()
                 .withGrossEmployeeCost(BigDecimal.ZERO)
                 .withDescription(WORKING_DAYS_PER_YEAR)
                 .withLabourDays(1)
                 .build();
+
         HecpIndirectCosts hecpIndirectCosts = newHecpIndirectCosts()
                 .withRateType(OverheadRateType.HORIZON_EUROPE_GUARANTEE_TOTAL)
                 .build();
+
         ProjectFinanceResource finance = newProjectFinanceResource().withFinanceOrganisationDetails(asMap(
-                FinanceRowType.LABOUR, newLabourCostCategory().withCosts(singletonList(workingDays)).build(),
+                FinanceRowType.PERSONNEL, newPersonnelCostCategory().withCosts(singletonList(workingDays)).build(),
                 FinanceRowType.HECP_INDIRECT_COSTS, newHecpIndirectCostsCostCategory().withCosts(singletonList(hecpIndirectCosts)).build(),
                 FinanceRowType.EQUIPMENT, newDefaultCostCategory().withCosts(emptyList()).build(),
                 FinanceRowType.OTHER_GOODS, newDefaultCostCategory().withCosts(emptyList()).build(),
@@ -74,16 +73,16 @@ public class FinanceChecksEligibilityHecpCostsSaverTest {
                 FinanceRowType.OTHER_COSTS, newDefaultCostCategory().withCosts(emptyList()).build()
         )).build();
 
-        LabourCost newLabourCost = newLabourCost().build();
+        PersonnelCost newPersonnelCost = newPersonnelCost().build();
         Equipment newEquipment = newEquipment().build();
         OtherGoods newOtherGoods = newOtherGoods().build();
         SubContractingCost newSubcontracting = newSubContractingCost().build();
         TravelCost newTravel = newTravelCost().build();
         OtherCost newOther = newOtherCost().build();
 
-        when(financeRowRestService.create(any(LabourCost.class))).thenReturn(restSuccess(newLabourCost));
-        when(financeRowRestService.create(any(Materials.class))).thenReturn(restSuccess(newEquipment));
-        when(financeRowRestService.create(any(CapitalUsage.class))).thenReturn(restSuccess(newOtherGoods));
+        when(financeRowRestService.create(any(PersonnelCost.class))).thenReturn(restSuccess(newPersonnelCost));
+        when(financeRowRestService.create(any(Equipment.class))).thenReturn(restSuccess(newEquipment));
+        when(financeRowRestService.create(any(OtherGoods.class))).thenReturn(restSuccess(newOtherGoods));
         when(financeRowRestService.create(any(SubContractingCost.class))).thenReturn(restSuccess(newSubcontracting));
         when(financeRowRestService.create(any(TravelCost.class))).thenReturn(restSuccess(newTravel));
         when(financeRowRestService.create(any(OtherCost.class))).thenReturn(restSuccess(newOther));
@@ -104,14 +103,14 @@ public class FinanceChecksEligibilityHecpCostsSaverTest {
         verify(financeRowRestService).update(workingDays);
         verify(financeRowRestService).update(hecpIndirectCosts);
 
-        verify(financeRowRestService).create(any(LabourCost.class));
+        verify(financeRowRestService).create(any(PersonnelCost.class));
         verify(financeRowRestService).create(any(Equipment.class));
         verify(financeRowRestService).create(any(OtherGoods.class));
         verify(financeRowRestService).create(any(SubContractingCost.class));
         verify(financeRowRestService).create(any(TravelCost.class));
         verify(financeRowRestService).create(any(OtherCost.class));
 
-        verify(financeRowRestService).update(newLabourCost);
+        verify(financeRowRestService).update(newPersonnelCost);
         verify(financeRowRestService).update(newEquipment);
         verify(financeRowRestService).update(newOtherGoods);
         verify(financeRowRestService).update(newSubcontracting);
@@ -119,33 +118,35 @@ public class FinanceChecksEligibilityHecpCostsSaverTest {
         verify(financeRowRestService).update(newOther);
         verifyNoMoreInteractions(financeRowRestService);
 
-        assertEquals(newLabourCost.getTotal(workingDays.getLabourDays()).toBigInteger().longValue(), 1L);
-        assertEquals(hecpIndirectCosts.getRateType(), OverheadRateType.HORIZON_EUROPE_GUARANTEE_TOTAL);
+        assertEquals(1L, newPersonnelCost.getTotal(workingDays.getLabourDays()).toBigInteger().longValue());
+        assertEquals(OverheadRateType.HORIZON_EUROPE_GUARANTEE_TOTAL, hecpIndirectCosts.getRateType());
         assertEquals(hecpIndirectCosts.getRate(), (Integer) 2);
-        assertEquals(newEquipment.getTotal().toBigInteger().longValue(), 3L);
-        assertEquals(newOtherGoods.getTotal().toBigInteger().longValue(), 4L);
-        assertEquals(newSubcontracting.getTotal().toBigInteger().longValue(), 5L);
-        assertEquals(newTravel.getTotal().toBigInteger().longValue(), 6L);
-        assertEquals(newOther.getTotal().toBigInteger().longValue(), 7L);
+        assertEquals(3L, newEquipment.getTotal().toBigInteger().longValue());
+        assertEquals(4L, newOtherGoods.getTotal().toBigInteger().longValue());
+        assertEquals(5L, newSubcontracting.getTotal().toBigInteger().longValue());
+        assertEquals(6L, newTravel.getTotal().toBigInteger().longValue());
+        assertEquals(7L, newOther.getTotal().toBigInteger().longValue());
 
         assertEquals(workingDays.getLabourDays(), (Integer) 1);
     }
 
     @Test
     public void save_zeros() {
-        LabourCost workingDays = newLabourCost()
+        PersonnelCost workingDays = newPersonnelCost()
                 .withGrossEmployeeCost(BigDecimal.ZERO)
                 .withDescription(WORKING_DAYS_PER_YEAR)
                 .withLabourDays(1)
                 .build();
+
         HecpIndirectCosts hecpIndirectCosts = newHecpIndirectCosts()
                 .withRateType(OverheadRateType.HORIZON_EUROPE_GUARANTEE_TOTAL)
                 .build();
+
         ProjectFinanceResource finance = newProjectFinanceResource().withFinanceOrganisationDetails(asMap(
-                FinanceRowType.LABOUR, newLabourCostCategory().withCosts(asList(workingDays, newLabourCost().build())).build(),
-                FinanceRowType.HECP_INDIRECT_COSTS, newOverheadCostCategory().withCosts(singletonList(hecpIndirectCosts)).build(),
-                FinanceRowType.EQUIPMENT, newDefaultCostCategory().withCosts(newMaterials().build(1)).build(),
-                FinanceRowType.OTHER_GOODS, newDefaultCostCategory().withCosts(newCapitalUsage().build(1)).build(),
+                FinanceRowType.PERSONNEL, newPersonnelCostCategory().withCosts(asList(workingDays, newPersonnelCost().build())).build(),
+                FinanceRowType.HECP_INDIRECT_COSTS, newHecpIndirectCostsCostCategory().withCosts(singletonList(hecpIndirectCosts)).build(),
+                FinanceRowType.EQUIPMENT, newDefaultCostCategory().withCosts(newEquipment().build(1)).build(),
+                FinanceRowType.OTHER_GOODS, newDefaultCostCategory().withCosts(newOtherGoods().build(1)).build(),
                 FinanceRowType.SUBCONTRACTING_COSTS, newDefaultCostCategory().withCosts(newSubContractingCost().build(1)).build(),
                 FinanceRowType.TRAVEL, newDefaultCostCategory().withCosts(newTravelCost().build(1)).build(),
                 FinanceRowType.OTHER_COSTS, newDefaultCostCategory().withCosts(newOtherCost().build(1)).build()
