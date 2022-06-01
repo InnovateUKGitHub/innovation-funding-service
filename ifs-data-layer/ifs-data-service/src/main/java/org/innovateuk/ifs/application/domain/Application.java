b190aa95c8a02901e8d1d433397586182465f1a5
Grace Williams <grace.williams@ukri.org>
2022-06-01 14:59:42 +0100

    IFS-11992: Default removed

--- ifs-data-layer/ifs-data-service/src/main/java/org/innovateuk/ifs/application/domain/Application.java
+++ ifs-data-layer/ifs-data-service/src/main/java/org/innovateuk/ifs/application/domain/Application.java
@@ -121,7 +121,7 @@ public class Application implements ProcessActivity {
     private ApplicationExternalConfig applicationExternalConfig;
 
     @Column(name = "pre_registration", nullable = false)
-    private boolean enableForEOI = false;
+    private boolean enableForEOI;
 
     public Application() {
     }
