7083809206813fbc7c0fb1b1c693f8c3bfbf368f
Dexter Ng <Dexter.Ng@ukri.org>
2022-06-21 12:28:22 +0100

    IFS-12174 fix unit test

--- common/ifs-rest-api-service/src/test/java/org/innovateuk/ifs/horizon/service/HorizonWorkProgrammeResourceRestServiceImplTest.java
+++ common/ifs-rest-api-service/src/test/java/org/innovateuk/ifs/horizon/service/HorizonWorkProgrammeResourceRestServiceImplTest.java
@@ -8,6 +8,7 @@ import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static java.lang.String.format;
 import static org.innovateuk.ifs.util.CollectionFunctions.simpleJoiner;
@@ -27,10 +28,14 @@ public class HorizonWorkProgrammeResourceRestServiceImplTest extends BaseRestSer
     public void updateWorkProgrammeForApplication() {
         long applicationId = 1L;
         List<HorizonWorkProgrammeResource> programmes = new ArrayList();
+        HorizonWorkProgrammeResource workProgramme = new HorizonWorkProgrammeResource(1, "CL2", true);
+        HorizonWorkProgrammeResource callerId = new HorizonWorkProgrammeResource(15, "HORIZON-CL2-2021-DEMOCRACY-01", workProgramme, true);
+        programmes.add(workProgramme);
+        programmes.add(callerId);
 
         String baseUrl = format("%s/%s/%s", REST_URL, "update-work-programmes", applicationId);
         UriComponentsBuilder builder = UriComponentsBuilder.fromPath(baseUrl)
-                .queryParam("workProgrammes", simpleJoiner(programmes, ","));
+                .queryParam("workProgrammeIds",programmes.stream().map(p -> Long.toString(p.getId())).collect(Collectors.joining(",")));
 
         setupPostWithRestResultExpectations(builder.toUriString(), OK);
 
