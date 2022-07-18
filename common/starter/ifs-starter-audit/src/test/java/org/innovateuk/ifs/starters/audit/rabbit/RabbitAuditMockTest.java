a96fbcb09730cd7c71d279c390e422b38988f7ca
MarkD <MarkD@iuk.ukri.org>
2022-07-18 17:07:47 +0100

    test lock

--- common/starter/ifs-starter-audit/src/test/java/org/innovateuk/ifs/starters/audit/rabbit/RabbitAuditMockTest.java
+++ common/starter/ifs-starter-audit/src/test/java/org/innovateuk/ifs/starters/audit/rabbit/RabbitAuditMockTest.java
@@ -10,6 +10,7 @@ import org.innovateuk.ifs.starters.audit.cfg.AuditConfigurationProperties;
 import org.innovateuk.ifs.starters.audit.cfg.testcfg.RabbitAuditTestConfiguration;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.parallel.ResourceLock;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -17,6 +18,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 
+import static org.innovateuk.ifs.starters.audit.cfg.testcfg.RabbitAuditTestConfiguration.CONTEXT_RESOURCE_LOCK;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
@@ -31,6 +33,7 @@ class RabbitAuditMockTest {
     private Audit audit;
 
     @Test
+    @ResourceLock(CONTEXT_RESOURCE_LOCK)
     void audit() throws JsonProcessingException {
         when(objectMapper.writeValueAsString(any())).thenThrow(JsonProcessingException.class);
         RuntimeException thrown = Assertions.assertThrows(RuntimeException.class, () -> {
--- common/starter/ifs-starter-audit/src/test/java/org/innovateuk/ifs/starters/audit/rabbit/RabbitAuditTest.java
+++ common/starter/ifs-starter-audit/src/test/java/org/innovateuk/ifs/starters/audit/rabbit/RabbitAuditTest.java
@@ -6,9 +6,12 @@ import org.innovateuk.ifs.api.audit.AuditType;
 import org.innovateuk.ifs.starters.audit.cfg.AuditConfigurationProperties;
 import org.innovateuk.ifs.starters.audit.cfg.testcfg.RabbitAuditTestConfiguration;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.parallel.ResourceLock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.innovateuk.ifs.starters.audit.cfg.testcfg.RabbitAuditTestConfiguration.CONTEXT_RESOURCE_LOCK;
 
 @SpringBootTest(classes = {RabbitAuditTestConfiguration.class})
 @EnableConfigurationProperties(AuditConfigurationProperties.class) class RabbitAuditTest {
@@ -16,6 +19,7 @@ import org.springframework.boot.test.context.SpringBootTest;
     private Audit audit;
 
     @Test
+    @ResourceLock(CONTEXT_RESOURCE_LOCK)
     void audit() {
         for(int i=0;i<100;i++) {
             audit.audit(AuditMessageBuilder.builder(AuditType.MISC).payload("{json: 'ddd'}").build());
