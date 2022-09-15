00d9a05526baa76e1c56d0b87da02f7fe2f2a545
Punniya.Rajkumar <punniya.rajkmar@iuk.ukri.org>
2022-09-15 13:38:45 +0100

    unique id updated

--- ifs-data-layer/ifs-data-service/src/main/resources/db/migration/V285_1__add_farming_innovation_terms_and_conditions.sql
+++ ifs-data-layer/ifs-data-service/src/main/resources/db/migration/V285_1__add_farming_innovation_terms_and_conditions.sql
@@ -3,7 +3,7 @@ SET @system_maintenance_user_id = (
     FROM user
     WHERE email = 'ifs_system_maintenance_user@innovateuk.org');
 
-SET @new_terms_id = 73;
+SET @new_terms_id = 74;
 
 INSERT INTO terms_and_conditions (id, name, template, version, type, created_by, created_on, modified_on, modified_by)
 values(@new_terms_id, 'Farming Innovation Programme', 'farming-innovation-programme-terms-and-conditions-v1', 1, 'GRANT', @system_maintenance_user_id, now(), now(), @system_maintenance_user_id);
