57c84b69b898df6a1cf1216898136c9ed18f9f8f
Adam Mapstone <adam.mapstone@ukri.org>
2022-10-01 18:35:53 +0100

    IFS-11365 - Lombok annotations added to viewmodel class.

--- ifs-web-service/ifs-competition-mgt-service/src/main/java/org/innovateuk/ifs/management/assessment/viewmodel/ApplicationAssessmentProgressUnsubmitViewModel.java
+++ ifs-web-service/ifs-competition-mgt-service/src/main/java/org/innovateuk/ifs/management/assessment/viewmodel/ApplicationAssessmentProgressUnsubmitViewModel.java
@@ -1,13 +1,15 @@
 package org.innovateuk.ifs.management.assessment.viewmodel;
 
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
-import org.apache.commons.lang3.builder.EqualsBuilder;
-import org.apache.commons.lang3.builder.HashCodeBuilder;
 import org.innovateuk.ifs.competition.resource.AvailableAssessorsSortFieldType;
 
 /**
  * Holder of model attributes for the Application Progress view.
  */
+@EqualsAndHashCode
+@AllArgsConstructor
 @Getter
 public class ApplicationAssessmentProgressUnsubmitViewModel {
 
@@ -16,45 +18,4 @@ public class ApplicationAssessmentProgressUnsubmitViewModel {
     private long assessmentId;
     private long assessmentPeriodId;
     private AvailableAssessorsSortFieldType sortField;
-
-    public ApplicationAssessmentProgressUnsubmitViewModel(
-            long competitionId,
-            long applicationId,
-            long assessmentId,
-            long assessmentPeriodId,
-            AvailableAssessorsSortFieldType sortField) {
-        this.competitionId = competitionId;
-        this.applicationId = applicationId;
-        this.assessmentId = assessmentId;
-        this.assessmentPeriodId = assessmentPeriodId;
-        this.sortField = sortField;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-
-        if (o == null || getClass() != o.getClass()) return false;
-
-        ApplicationAssessmentProgressUnsubmitViewModel that = (ApplicationAssessmentProgressUnsubmitViewModel) o;
-
-        return new EqualsBuilder()
-                .append(competitionId, that.competitionId)
-                .append(applicationId, that.applicationId)
-                .append(assessmentId, that.assessmentId)
-                .append(assessmentPeriodId, that.assessmentPeriodId)
-                .append(sortField, that.sortField)
-                .isEquals();
-    }
-
-    @Override
-    public int hashCode() {
-        return new HashCodeBuilder(17, 37)
-                .append(competitionId)
-                .append(applicationId)
-                .append(assessmentId)
-                .append(assessmentPeriodId)
-                .append(sortField)
-                .toHashCode();
-    }
 }
