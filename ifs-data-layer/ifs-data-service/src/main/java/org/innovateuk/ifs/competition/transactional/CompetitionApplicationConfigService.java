e4c573c3d26aaccd2a942bc539466eb0f5e20273
Ashish Uniyal <ashish.uniyal@innovateuk.ukri.org>
2022-10-03 18:51:08 +0100

    IFS-12942 edit and readonly views

--- ifs-data-layer/ifs-data-service/src/main/java/org/innovateuk/ifs/competition/transactional/CompetitionApplicationConfigService.java
+++ ifs-data-layer/ifs-data-service/src/main/java/org/innovateuk/ifs/competition/transactional/CompetitionApplicationConfigService.java
@@ -3,6 +3,7 @@ package org.innovateuk.ifs.competition.transactional;
 import org.innovateuk.ifs.commons.security.NotSecured;
 import org.innovateuk.ifs.commons.service.ServiceResult;
 import org.innovateuk.ifs.competition.resource.CompetitionApplicationConfigResource;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface CompetitionApplicationConfigService {
 
@@ -11,4 +12,7 @@ public interface CompetitionApplicationConfigService {
 
     @NotSecured(value = "TODO", mustBeSecuredByOtherServices = false)
     ServiceResult<Void> update(long competitionId, CompetitionApplicationConfigResource competitionApplicationConfigResource);
+
+    @Transactional
+    ServiceResult<Void> updateImpactSurvey(long competitionId, CompetitionApplicationConfigResource competitionApplicationConfigResource);
 }
--- ifs-web-service/ifs-competition-mgt-service/src/main/java/org/innovateuk/ifs/management/competition/setup/projectimpact/controller/SupportingDocumentController.java
+++ ifs-web-service/ifs-competition-mgt-service/src/main/java/org/innovateuk/ifs/management/competition/setup/projectimpact/controller/SupportingDocumentController.java
@@ -1,8 +1,13 @@
 package org.innovateuk.ifs.management.competition.setup.projectimpact.controller;
 
+import org.innovateuk.ifs.application.service.SectionRestService;
 import org.innovateuk.ifs.commons.security.SecuredBySpring;
+import org.innovateuk.ifs.commons.service.ServiceResult;
+import org.innovateuk.ifs.competition.resource.CompetitionApplicationConfigResource;
 import org.innovateuk.ifs.competition.resource.CompetitionResource;
+import org.innovateuk.ifs.competition.service.CompetitionApplicationConfigRestService;
 import org.innovateuk.ifs.competition.service.CompetitionRestService;
+import org.innovateuk.ifs.competition.service.CompetitionSetupRestService;
 import org.innovateuk.ifs.controller.ValidationHandler;
 import org.innovateuk.ifs.management.competition.setup.core.service.CompetitionSetupService;
 import org.innovateuk.ifs.management.competition.setup.projectimpact.form.ProjectImpactForm;
@@ -12,15 +17,15 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.function.Supplier;
 
+import static java.lang.String.format;
 import static org.innovateuk.ifs.competition.resource.CompetitionSetupSection.PROJECT_IMPACT;
+import static org.innovateuk.ifs.controller.ErrorToObjectErrorConverterFactory.asGlobalErrors;
+import static org.innovateuk.ifs.controller.ErrorToObjectErrorConverterFactory.fieldErrorsToFieldErrors;
 import static org.innovateuk.ifs.management.competition.setup.CompetitionSetupController.COMPETITION_SETUP_FORM_KEY;
 
 /**
@@ -38,13 +43,35 @@ public class SupportingDocumentController {
     private final CompetitionRestService competitionRestService;
 
     private final CompetitionSetupService competitionSetupService;
+    private final CompetitionApplicationConfigRestService competitionApplicationConfigRestService;
+
+    private CompetitionSetupRestService competitionSetupRestService;
 
     @Autowired
-    public SupportingDocumentController(CompetitionRestService competitionRestService, CompetitionSetupService competitionSetupService) {
+   public SupportingDocumentController(CompetitionRestService competitionRestService, CompetitionSetupService competitionSetupService, CompetitionApplicationConfigRestService competitionApplicationConfigRestService, CompetitionSetupRestService competitionSetupRestService) {
         this.competitionRestService = competitionRestService;
         this.competitionSetupService = competitionSetupService;
+        this.competitionApplicationConfigRestService = competitionApplicationConfigRestService;
+        this.competitionSetupRestService = competitionSetupRestService;
     }
 
+
+    @GetMapping
+    public String organisationalEligibilityPageDetails(Model model,
+                                                       @PathVariable long competitionId,
+                                                       UserResource loggedInUser) {
+
+        CompetitionResource competition = competitionRestService.getCompetitionById(competitionId).getSuccess();
+
+        if (!competitionSetupService.hasInitialDetailsBeenPreviouslySubmitted(competitionId)) {
+            return "redirect:/competition/setup/" + competition.getId();
+        }
+
+        model.addAttribute(MODEL, competitionSetupService.populateCompetitionSectionModelAttributes(competition, loggedInUser, PROJECT_IMPACT));
+        model.addAttribute(COMPETITION_SETUP_FORM_KEY, competitionSetupService.getSectionFormPopulator(PROJECT_IMPACT).populateForm(competition));
+
+        return "competition/setup";
+    }
 
     @PostMapping
     public String submitOrganisationalEligibilitySectionDetails(@Valid @ModelAttribute("competitionSetupForm") ProjectImpactForm projectImpactForm,
@@ -56,12 +83,26 @@ public class SupportingDocumentController {
 
         CompetitionResource competition = competitionRestService.getCompetitionById(competitionId).getSuccess();
 
+
         Supplier<String> failureView = () -> {
             model.addAttribute(MODEL, competitionSetupService.populateCompetitionSectionModelAttributes(competition, loggedInUser, PROJECT_IMPACT));
             model.addAttribute(COMPETITION_SETUP_FORM_KEY, projectImpactForm);
             return "competition/setup";
         };
-        return validationHandler.failNowOrSucceedWith(failureView, null);
+        Supplier<String> successView = () -> {
+            model.addAttribute("model.general.editable",false);
+            return validationHandler.addAnyErrors(saveImpactManagementConfig(competition, projectImpactForm), fieldErrorsToFieldErrors(), asGlobalErrors())
+                    .failNowOrSucceedWith(failureView, () ->
+                            format("redirect:/competition/setup/%d/section/%s", competition.getId(), PROJECT_IMPACT.getPostMarkCompletePath()));
+        };
+        return validationHandler.failNowOrSucceedWith(failureView, successView);
+    }
+
+    private ServiceResult<Void> saveImpactManagementConfig(CompetitionResource competition, @Valid ProjectImpactForm projectImpactForm) {
+        competitionSetupRestService.markSectionComplete(competition.getId(),PROJECT_IMPACT);
+        CompetitionApplicationConfigResource competitionApplicationConfigResource = competitionApplicationConfigRestService.findOneByCompetitionId(competition.getId()).getSuccess();
+        competitionApplicationConfigResource.setImSurveyRequired(projectImpactForm.getProjectImpactSurveyApplicable());
+        return competitionApplicationConfigRestService.updateImpactSurvey(competition.getId(), competitionApplicationConfigResource).toServiceResult().andOnSuccessReturnVoid();
     }
 
 }
