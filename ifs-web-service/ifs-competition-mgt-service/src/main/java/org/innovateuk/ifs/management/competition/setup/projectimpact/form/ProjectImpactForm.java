8cd0196e23372065288c84aa2cee68b8c6db2856
Ashish Uniyal <ashish.uniyal@innovateuk.ukri.org>
2022-10-05 11:13:25 +0100

    IFS-12942 more UT

--- ifs-web-service/ifs-competition-mgt-service/src/main/java/org/innovateuk/ifs/management/competition/setup/projectimpact/form/ProjectImpactForm.java
+++ ifs-web-service/ifs-competition-mgt-service/src/main/java/org/innovateuk/ifs/management/competition/setup/projectimpact/form/ProjectImpactForm.java
@@ -1,5 +1,7 @@
 package org.innovateuk.ifs.management.competition.setup.projectimpact.form;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.innovateuk.ifs.management.competition.setup.core.form.CompetitionSetupForm;
 
 import javax.validation.constraints.NotNull;
@@ -9,19 +11,11 @@ import static java.util.Objects.isNull;
 /**
  * Form for the Project Impact competition setup section.
  */
+@Getter
+@Setter
 public class ProjectImpactForm extends CompetitionSetupForm {
 
     @NotNull(message = "{validation.projectImpactForm.projectImpactSurveyApplicable.required}")
     private Boolean projectImpactSurveyApplicable;
-
-
-    public Boolean getProjectImpactSurveyApplicable() {
-        return projectImpactSurveyApplicable;
-    }
-
-    public void setProjectImpactSurveyApplicable(Boolean projectImpactSurveyApplicable) {
-        this.projectImpactSurveyApplicable = projectImpactSurveyApplicable;
-    }
-
 
 }
---
+++ ifs-web-service/ifs-competition-mgt-service/src/test/java/org/innovateuk/ifs/management/competition/setup/projectimpact/controller/populator/ProjectImpactFormPopulatorTest.java
@@ -0,0 +1,55 @@
+package org.innovateuk.ifs.management.competition.setup.projectimpact.controller.populator;
+
+import org.innovateuk.ifs.BaseUnitTest;
+import org.innovateuk.ifs.competition.builder.CompetitionApplicationConfigResourceBuilder;
+import org.innovateuk.ifs.competition.resource.CompetitionApplicationConfigResource;
+import org.innovateuk.ifs.competition.resource.CompetitionResource;
+import org.innovateuk.ifs.competition.service.CompetitionApplicationConfigRestService;
+import org.innovateuk.ifs.management.competition.setup.projectimpact.form.ProjectImpactForm;
+import org.innovateuk.ifs.management.competition.setup.projectimpact.populator.ProjectImpactFormPopulator;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
+import static org.innovateuk.ifs.commons.rest.RestResult.restSuccess;
+import static org.innovateuk.ifs.competition.builder.CompetitionResourceBuilder.newCompetitionResource;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.when;
+
+public class ProjectImpactFormPopulatorTest extends BaseUnitTest {
+
+    @InjectMocks
+    private ProjectImpactFormPopulator service;
+
+    @Mock
+    private CompetitionApplicationConfigRestService competitionApplicationConfigRestService;
+
+    @Test
+    public void populateFormWithRequiredTrue() {
+        long competitionId = 100L;
+        CompetitionResource competitionResource = newCompetitionResource().withId(competitionId).build();
+        CompetitionApplicationConfigResource competitionApplicationConfigResource = CompetitionApplicationConfigResourceBuilder
+                .newCompetitionApplicationConfigResource()
+                .withIMSurveyRequired(true).build();
+
+        when(competitionApplicationConfigRestService.findOneByCompetitionId(competitionResource.getId())).thenReturn(restSuccess(competitionApplicationConfigResource));
+        ProjectImpactForm result = (ProjectImpactForm) service.populateForm(competitionResource);
+        assertTrue(result.getProjectImpactSurveyApplicable());
+    }
+
+    @Test
+    public void populateFormWithRequiredFalse() {
+        long competitionId = 100L;
+        CompetitionResource competitionResource = newCompetitionResource().withId(competitionId).build();
+        CompetitionApplicationConfigResource competitionApplicationConfigResource = CompetitionApplicationConfigResourceBuilder
+                .newCompetitionApplicationConfigResource()
+                .withIMSurveyRequired(false).build();
+
+        when(competitionApplicationConfigRestService.findOneByCompetitionId(competitionResource.getId())).thenReturn(restSuccess(competitionApplicationConfigResource));
+        ProjectImpactForm result = (ProjectImpactForm) service.populateForm(competitionResource);
+        assertFalse(result.getProjectImpactSurveyApplicable());
+    }
+
+
+}
